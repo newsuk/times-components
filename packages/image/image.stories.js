69f4c69563b79ce8b6cf29f687825a70b040f35c
Fábio Santos <fabiosantosart@gmail.com>
2017-06-13 13:47:18 +0100

    chore: Add prettier

--- packages/image/image.stories.js
+++ packages/image/image.stories.js
@@ -4,8 +4,8 @@ import { storiesOf } from "@storybook/react";
 import Image from "./image";
 
 const exampleImage = {
-  uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==',
+  uri:
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
 };
 
-storiesOf("Image", module)
-  .add("Image", () => <Image source={exampleImage} />);
+storiesOf("Image", module).add("Image", () => <Image source={exampleImage} />);
