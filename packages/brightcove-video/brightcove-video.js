e94526571c734788dd5ac4a3e57a33eba7cac095
Ritchie Comley <richard.comley@news.co.uk>
2017-06-15 15:07:20 +0100

    stubs brightcove-video component

---
+++ packages/brightcove-video/brightcove-video.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class BrightcoveVideo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Author Bios!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5EFEB"
+  }
+});
---
+++ packages/brightcove-video/brightcove-video.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import BrightcoveVideo from "./brightcove-video";
+
+storiesOf("BrightcoveVideo", module).add("BrightcoveVideo", () => <BrightcoveVideo />);
