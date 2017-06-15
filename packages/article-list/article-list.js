433a82bc87d2880e818bdc1de345f5fbb2bcbc2f
Fábio Santos <fabiosantosart@gmail.com>
2017-06-15 16:54:53 +0100

    feat: Change from .map() to ListView for listing

--- packages/article-list/article-list.js
+++ packages/article-list/article-list.js
@@ -1,24 +1,48 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Card from "@newsint/card";
+import { View, ListView, StyleSheet } from "react-native";
+import Card from "@times-components/card";
 
-const rowStyle = isLast => {
-  if (isLast) {
-    return {};
+const styles = StyleSheet.create({
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  },
+  container: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
   }
-  return {
-    borderBottomWidth: isLast ? 0 : 1,
-    borderBottomColor: "#DBDBDB",
-    paddingBottom: 12,
-    marginBottom: 15
-  };
-};
+})
 
-export default function ArticleList({ articles }) {
-  const cards = articles.map((props, i) =>
-    <View style={rowStyle(i === articles.length - 1)}>
-      <Card {...props} key={`articlelist-article-${i}`} />
-    </View>
-  );
-  return <View>{cards}</View>;
+const Row = article => (
+  <View style={styles.container}>
+    <Card {...article} />
+  </View>
+);
+
+export default class ArticleList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const articlesDataSource = new ListView.DataSource(
+      { rowHasChanged: (r1, r2) => r1 !== r2 }
+    );
+
+    this.state = {
+      articles: articlesDataSource.cloneWithRows(props.articles),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.articles}
+        renderRow={(data) => <Row {...data} />}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+      />
+    );
+  }
 }
