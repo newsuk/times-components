038a39f4b2f439d664347e91b38c3d483a4c5f31
Ritchie Comley <richard.comley@news.co.uk>
2017-06-16 17:32:43 +0100

    made lib for brightcove stuff

---
+++ android/rntbrightcovevideo/src/main/java/uk/co/news/rntbrightcovevideo/RNTBrightcoveView.java
@@ -0,0 +1,51 @@
+package uk.co.news.rntbrightcovevideo;
+
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.mediacontroller.BrightcoveMediaController;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.facebook.react.uimanager.ThemedReactContext;
+
+public class RNTBrightcoveView extends BrightcoveExoPlayerVideoView {
+  private String mVideoId, mAccountId, mPolicyId;
+
+  public RNTBrightcoveView(ThemedReactContext context) {
+    super(context, null);
+    finishInitialization();
+  }
+
+  public void setVideoId(String videoId) {
+    mVideoId = videoId;
+    initVideo();
+  }
+
+  public void setAccountId(String accountId) {
+    mAccountId = accountId;
+    initVideo();
+  }
+
+  public void setPolicyId(String policyId) {
+    mPolicyId = policyId;
+    initVideo();
+  }
+
+  private void initVideo() {
+    if (mVideoId != null && mAccountId != null && mPolicyId != null) {
+      EventEmitter eventEmitter = getEventEmitter();
+
+      Catalog catalog = new Catalog(eventEmitter, mAccountId, mPolicyId);
+
+      this.setMediaController(new BrightcoveMediaController(this));
+
+      catalog.findVideoByID(mVideoId, new VideoListener() {
+        @Override
+        public void onVideo(Video video) {
+          RNTBrightcoveView.this.add(video);
+          RNTBrightcoveView.this.start();
+        }
+      });
+    }
+  }
+}
