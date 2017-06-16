f171943a8f89cbf32ce62a3628e7a32dce859c35
Fábio Santos <fabiosantosart@gmail.com>
2017-06-16 15:10:54 +0100

    feat: improve Image story to better explain what it does

--- packages/image/image.stories.js
+++ packages/image/image.stories.js
@@ -1,11 +1,29 @@
-import "react-native";
 import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 import { storiesOf } from "@storybook/react";
 import Image from "./image";
 
 const exampleImage = {
   uri:
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
+    "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F9242e576-4dfc-11e7-a20e-a11097d3353d.jpg?crop=1463%2C975%2C293%2C12&resize=320"
 };
 
-storiesOf("Image", module).add("Image", () => <Image source={exampleImage} />);
+const styles = StyleSheet.create({
+  halfWidthView: {
+    width: "100%"
+  },
+  customSize: {
+    width: "100px"
+  }
+});
+
+storiesOf("Image", module)
+.add("Stretched to full width keeping aspect ratio", () =>
+  <View style={styles.halfWidthView}>
+    <Image source={exampleImage} />
+  </View>
+).add("With custom width of 100px", () =>
+  <View style={styles.customSize}>
+    <Image source={exampleImage} />
+  </View>
+);
