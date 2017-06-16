04fb050914a75625c8f1497552fd0ba76fd8f262
Aron Shamash <aronshamash@gmail.com>
2017-06-16 16:38:24 +0100

    Basic brightcove video component

--- packages/brightcove-video/brightcove-video.web.js
+++ packages/brightcove-video/brightcove-video.web.js
@@ -1,37 +1,114 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import Script from "react-load-script";
+import PropTypes from "prop-types";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5EFEB"
+  },
+  outerContainer: {
+    display: "block",
+    position: "relative",
+    maxWidth: "100%"
+  },
+  innerContainer: {
+    paddingTop: "56.25%"
+  }
+});
 
 class Brightcove extends Component {
-  componentDidMount () {
-    // add and execute the player script tag
-    const s = document.createElement('script');
-    s.src = '//players.brightcove.net/' + this.props.accountId + '/' + (this.props.playerId || 'default') + '_default/index.min.js';
-    document.body.appendChild(s);
+  constructor(props) {
+    super(props);
+    this.state = {
+      scriptLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  handleScriptLoad() {
+    this.setState({ scriptLoaded: true });
+
+    this.mountVideoPlayer();
+  }
+
+  mountVideoPlayer() {
+    this._player = videojs(this.getVideoElement());
+  }
+
+  getVideoPlayer() {
+    return this._player;
+  }
+
+  getVideoElement() {
+    return `${this.props.video_id}-${this.props.account_id}`;
+  }
+
+  pauseVideo() {
+    this._player.pause();
+  }
+
+  playVideo() {
+    this._player.play();
+  }
+
+  restartVideo() {
+    this._player.currentTime(0).play();
   }
 
   render () {
-    let height = '100px';
-    let width = '150px';
+    const scriptUrl = `//players.brightcove.net/${this.props.account_id}/${this.props.player_id}_default/index.min.js`;
 
-    if (this.props.height) {
-      height = this.props.height + 'px';
-    }
-
-    if (this.props.width) {
-      width = this.props.width + 'px';
-    }
+    const attribs = {
+      id: this.getVideoElement(),
+      "data-embed": "default",
+      "data-video-id": this.props.video_id,
+      "data-account": this.props.account_id,
+      "data-player": this.props.player_id,
+      "className": "video-js"
+    };
 
     return (
-      <video
-        style={{ height: height, width: width }}
-        data-video-id={this.props.videoId}
-        data-account={this.props.accountId}
-        data-player={this.props.playerId || 'default'}
-        data-embed='default'
-        data-application-id
-        className='video-js'
-        controls />
+      <View style={styles.outerContainer}>
+        <View style={styles.innerContainer}>
+          <video
+            style={{width: this.props.width, height: this.props.height, position: "absolute", top: "0", left: "0"}}
+            {...attribs}
+            data-application-id
+            controls>
+          </video>
+          <Script
+            url={scriptUrl}
+            onLoad={this.handleScriptLoad.bind(this)}
+          />
+        </View>
+      </View>
     );
   }
 }
+
+Brightcove.defaultProps = {
+  account_id: "5436121857001",
+  video_id: "5464373931001",
+  player_id: "default",
+  paidOnly: false,
+  width: "100%",
+  height: "100%"
+};
+
+Brightcove.propTypes = {
+  account_id: PropTypes.string.isRequired,
+  video_id: PropTypes.string.isRequired,
+  player_id: PropTypes.string,
+  paidOnly: PropTypes.bool,
+  width: PropTypes.string,
+  height: PropTypes.string
+};
 
 export default Brightcove;
