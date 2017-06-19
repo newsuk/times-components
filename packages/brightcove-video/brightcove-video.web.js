d3dfaf42dace61d8db946da34729e67eee440b7c
Jack Harvey <jack.harvey@news.co.uk>
2017-06-19 17:06:42 +0100

    Add click to add storybook story

--- packages/brightcove-video/brightcove-video.web.js
+++ packages/brightcove-video/brightcove-video.web.js
@@ -1,79 +1,24 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import Script from "react-load-script";
-import PropTypes from "prop-types";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5EFEB"
-  },
-  outerContainer: {
-    display: "block",
-    position: "relative",
-    maxWidth: "100%"
-  },
-  innerContainer: {
-    paddingTop: "56.25%"
-  }
-});
+let id = 0;
 
 class Brightcove extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      scriptLoaded: false
-    };
-  }
-
   componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  handleScriptLoad() {
-    this.setState({ scriptLoaded: true });
-
-    this.mountVideoPlayer();
-  }
-
-  mountVideoPlayer() {
-    this._player = videojs(this.getVideoElementId());
-  }
-
-  getVideoPlayer() {
-    return this._player;
-  }
-
-  getVideoElementId() {
-    return `${this.props.video_id}-${this.props.account_id}`;
-  }
-
-  pauseVideo() {
-    this._player.pause();
-  }
-
-  playVideo() {
-    this._player.play();
-  }
-
-  restartVideo() {
-    this._player.currentTime(0).play();
+    // add and execute the player script tag
+    const s = document.createElement("script");
+    s.src =
+      "//players.brightcove.net/" +
+      this.props.accountId +
+      "/" +
+      (this.props.playerId || "default") +
+      "_default/index.min.js";
+    document.body.appendChild(s);
   }
 
   render() {
-    const scriptUrl = `//players.brightcove.net/${this.props.account_id}/${this
-      .props.player_id}_default/index.min.js`;
-
-    const attribs = {
-      id: this.getVideoElementId(),
-      "data-embed": "default",
-      "data-video-id": this.props.video_id,
-      "data-account": this.props.account_id,
-      "data-player": this.props.player_id,
-      className: "video-js"
-    };
+    let height = "100px";
+    let width = "150px";
 
     if (this.props.height) {
       height = this.props.height + "px";
@@ -84,43 +29,20 @@ class Brightcove extends Component {
     }
 
     return (
-      <View style={styles.outerContainer}>
-        <View style={styles.innerContainer}>
-          <video
-            style={{
-              width: this.props.width,
-              height: this.props.height,
-              position: "absolute",
-              top: "0",
-              left: "0"
-            }}
-            {...attribs}
-            data-application-id
-            controls
-          />
-          <Script url={scriptUrl} onLoad={this.handleScriptLoad.bind(this)} />
-        </View>
+      <View>
+        <video
+          id={"_" + id++}
+          style={{ height, width }}
+          data-video-id={this.props.videoId}
+          data-account={this.props.accountId}
+          data-player="default"
+          data-embed="default"
+          data-application-id
+          controls
+        />
       </View>
     );
   }
 }
-
-Brightcove.defaultProps = {
-  account_id: "5436121857001",
-  video_id: "5464373931001",
-  player_id: "default",
-  paidOnly: false,
-  width: 800,
-  height: 450
-};
-
-Brightcove.propTypes = {
-  account_id: PropTypes.string.isRequired,
-  video_id: PropTypes.string.isRequired,
-  player_id: PropTypes.string,
-  paidOnly: PropTypes.bool,
-  width: PropTypes.number,
-  height: PropTypes.number
-};
 
 export default Brightcove;
