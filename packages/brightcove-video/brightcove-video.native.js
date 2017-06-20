3c803f5bd4d5a291dac959bb15a310cbd6309674
Ritchie Comley <richard.comley@news.co.uk>
2017-06-20 09:42:24 +0100

    prettier

--- packages/brightcove-video/brightcove-video.native.js
+++ packages/brightcove-video/brightcove-video.native.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import { requireNativeComponent } from 'react-native';
+import { requireNativeComponent } from "react-native";
 
 class BrightcoveVideo extends Component {
-    render () {
-        const height = this.props.height || '100%';
-        const width = this.props.width || '100%';
+  render() {
+    const height = this.props.height || "100%";
+    const width = this.props.width || "100%";
 
-        return (<RNTBrightcove
-            style={{height, width}}
-            policyId={this.props.policyId}
-            accountId={this.props.accountId}
-            videoId={this.props.videoId} />
-        );
-    }
+    return (
+      <RNTBrightcove
+        style={{ height, width }}
+        policyId={this.props.policyId}
+        accountId={this.props.accountId}
+        videoId={this.props.videoId}
+      />
+    );
+  }
 }
 
-const RNTBrightcove = requireNativeComponent('RNTBrightcove', null);
+const RNTBrightcove = requireNativeComponent("RNTBrightcove", null);
 
 export default BrightcoveVideo;
