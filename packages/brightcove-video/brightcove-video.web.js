12b89d0bca678b3d91acc4cdbc35202e731473f9
Jack Harvey <jack.harvey@news.co.uk>
2017-06-20 16:54:55 +0100

    Make web component use only one script tag

--- packages/brightcove-video/brightcove-video.web.js
+++ packages/brightcove-video/brightcove-video.web.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 
+let id = 0,
+  hasLoadedScript = false;
+
 class BrightcoveVideo extends Component {
   componentDidMount() {
-    const s = document.createElement("script");
-    s.src = `//players.brightcove.net/${this.props
-      .accountId}/default_default/index.min.js`;
-    document.body.appendChild(s);
+    if (!hasLoadedScript) {
+      const s = document.createElement("script");
+      hasLoadedScript = true;
+
+      s.src = `//players.brightcove.net/${this.props
+        .accountId}/default_default/index.min.js`;
+      document.body.appendChild(s);
+    } else {
+      this.init();
+    }
+  }
+
+  init() {
+    if (window.bc) {
+      bc(document.getElementById(this.id));
+      videojs(this.id);
+    }
   }
 
   render() {
     const width = this.props.width || 320;
     const height = this.props.height || 180;
+    this.id = `_${id++}`;
 
     return (
       <View width="100%" height="100%">
         <video
+          id={this.id}
           width={width}
           height={height}
           data-embed="default"
