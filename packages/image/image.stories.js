52ccfe4e3cb3a7d1f1d453440b960e2bd4cf9cca
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2017-06-27 18:50:10 +0100

    fix: correct dependencies versions

--- packages/image/image.stories.js
+++ packages/image/image.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { storiesOf } from "@storybook/react";
+import { storiesOf } from "@storybook/react-native";
 import Image from "./image";
 
 const exampleImage = {
