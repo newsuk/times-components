769b1107b606ae24fb8c8f646e78f12e86e3e5cd
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-06-29 09:57:49 +0100

    snapshot test

--- packages/gpt/gpt.test.js
+++ packages/gpt/gpt.test.js
@@ -4,16 +4,30 @@ import GPT from "./gpt.web";
 import AdManager from "./ad-manager";
 import renderer from "react-test-renderer";
 
-it("renders correctly", () => {
-  const adManager = new AdManager({
-    networkId: "25436805",
-    adUnit: "d.thetimes.co.uk",
-    section: "article"
+describe("Gpt test", () => {
+  let adManager;
+
+  beforeEach(() => {
+    adManager = new AdManager({
+      networkId: "25436805",
+      adUnit: "d.thetimes.co.uk",
+      section: "article"
+    });
   });
 
-  const tree = renderer
-    .create(<GPT adManager={adManager} code="ad-header" />)
-    .toJSON();
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<GPT adManager={adManager} code="ad-header" />)
+      .toJSON();
 
-  expect(tree).toBeTruthy();
+    expect(tree).toBeTruthy();
+  });
+
+  it("renders a snapshot", () => {
+    const tree = renderer
+      .create(<GPT adManager={adManager} code="ad-header" />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
