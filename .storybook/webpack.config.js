c905ea099d401d3b80e14796cbef18048f4cdf49
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-07-03 11:32:40 +0100

    add babel transform for obj spread operator

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,28 @@
 module.exports = {
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["transform-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  },
 
   resolve: {
     // Maps the 'react-native' import to 'react-native-web'.
     alias: {
-      'react-native': 'react-native-web',
-      '@storybook/react-native': '@storybook/react',
+      "react-native": "react-native-web",
+      "@storybook/react-native": "@storybook/react"
     },
     // If you're working on a multi-platform React Native app, web-specific
     // module implementations should be written in files using the extension
     // `.web.js`.
-    extensions: ['.web.js', '.js']
+    extensions: [".web.js", ".js"]
   }
 };
