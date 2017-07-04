60d26264ef82f2c356766a998fffb8433eeaa86b
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2017-07-04 19:01:22 +0100

    feat: add stories for autho header

--- packages/author-head/author-head.stories.js
+++ packages/author-head/author-head.stories.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import {
-  Header,
-  Photo,
-  Name,
-  Title
-} from './author-head';
+import { Header, Photo } from './author-head';
 
 const data = {
-  uri: 'https://feeds.thetimes.co.uk/web/imageserver/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F0694e84e-04ff-11e7-976a-0b4b9a1a67a3.jpg',
+  uri: 'https://feeds.thetimes.co.uk/web/imageserver/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F0694e84e-04ff-11e7-976a-0b4b9a1a67a3.jpg?crop=854,854,214,0&resize=200',
   name: 'Carol Midgley',
-  title: 'Columnist'
+  title: 'Columnist',
+  twitter: '@carolmidgley',
+  bio: 'Carol Midgley joined The Times in 1996, and won feature writer of the year in 2004. Find her Wednesday column in Times2 each week and a weekly comment piece on Fridays.'
 };
 
 storiesOf('AuthorHead', module)
-  .add('Profile Photo', () => (
-    <Photo source={data.uri} />
-  ))
-  .add('Profile Name', () => (
-    <Name></Name>
-  ))
-  .add('Profile Title', () => (
-    <Title>{data.title}</Title>
+  .add('Author Avatar', () => (
+    <Photo uri={data.uri} />
   ))
   .add('Full Header', () => (
     <Header {...data} />
