1dae084723b3dcaaf0560c45763139eeb4b125ee
Craig Bilner <craig.bilner@gmail.com>
2017-07-06 13:56:03 +0100

    chore: add stateful test

--- packages/image/image.test.js
+++ packages/image/image.test.js
@@ -1,39 +1,20 @@
 /* eslint-env jest */
 
-import "react-native";
+import { shallow } from "enzyme";
 import React from "react";
 import renderer from "react-test-renderer";
 import Image from "./image";
 import placeholder from "./placeholder";
 
-it("renders snapshot correctly", () => {
+it("renders correctly with no dimensions and given URI", () => {
   const props = {
     source: {
       uri: "http://example.com/image.jpg"
     },
-    width: 200,
-    height: 75
+    aspectRatio: 3 / 2
   };
   const tree = renderer.create(<Image {...props} />).toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-it("lays out image with correct aspect ratio", done => {
-  const comp = new Image({
-    source: {
-      uri: "http://httpstat.us/404"
-    },
-    aspectRatio: 0.5
-  });
-
-  comp.setState = ({ width, height }) => {
-    expect(width).toEqual(20);
-    expect(height).toEqual(10);
-
-    return done();
-  };
-
-  comp.handleLayout({ nativeEvent: { layout: { width: 20 } } });
 });
 
 it("use placeholder image when image is not reachable", done => {
@@ -53,15 +34,37 @@ it("use placeholder image when image is not reachable", done => {
   comp.handleError();
 });
 
-it("loads the correct url", () => {
+it("lays out the image with the correct aspect ratio", done => {
+  const comp = new Image({
+    source: {
+      uri: "http://httpstat.us/404"
+    },
+    aspectRatio: 0.5
+  });
+
+  comp.setState = ({ width, height }) => {
+    expect(width).toEqual(20);
+    expect(height).toEqual(10);
+
+    return done();
+  };
+
+  comp.handleLayout({ nativeEvent: { layout: { width: 20 } } });
+});
+
+it("sets the expected dimensions for a given state", () => {
   const props = {
     source: {
-      uri: "http://httpstat.us/200"
+      uri: "http://example.com/image.jpg"
     }
   };
 
-  const comp = renderer.create(<Image {...props} />).toJSON();
-  // Image is inside a View to handle resizing events
-  const target = comp.children[0].props.source.uri;
-  expect(target).toBe(props.source.uri);
+  const wrapper = shallow(<Image {...props} />);
+
+  wrapper.setState({
+    height: 300,
+    width: 100
+  });
+
+  expect(wrapper).toMatchSnapshot();
 });
