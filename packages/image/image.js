4a00802f3a722d864147702def9bd6292c54ef60
Fábio Oliveira <foliveira@users.noreply.github.com>
2017-07-06 17:34:51 +0100

    [Component] Image (#12)
    * feat: add Image component
    
    * feat: make Image stretch to the width of the container and keep its aspect ratio
    
    * feat: add layout tests to Image
    
    * feat: improve Image story to better explain what it does
    
    * chore: update package scope and dependencies
    
    * fix: correct dependencies versions
    
    * fix: removed redundant story
    
    * fix: update image components stories
    
    * feat: add snapshot tests and tests - also corrects image sizing
    
    * fix: change parameter passing
    
    * fix: correct dependencies version
    
    * fix: update with output from prettier
    
    * fix: update with prettier settings
    
    * chore: update test file path
    
    * chore: use default image
    
    * fix: set exact
    
    * fix: use base64 placeholder image
    
    * chore: remove package-lock file
    
    * fix: linting issues
    
    * fix: set source to placeholder on fail
    
    * BREAKING CHANGE: use aspect ratio instead of width and height
    
    * chore: test error case
    
    * chore: add stateful test
    
    * fix: fix yarn.lock
    
    * fix: update package scope and jest config
    
    * fix: revert changes to author

---
+++ packages/image/image.js
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import stylePropType from "react-style-proptype";
+import { View, Image } from "react-native";
+import merge from "lodash.merge";
+import placeholder from "./placeholder";
+
+class ImageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      source: props.source,
+      width: 0,
+      height: 0
+    };
+
+    this.handleLayout = this.handleLayout.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  handleLayout(event) {
+    const containerWidth = event.nativeEvent.layout.width;
+    this.setState({
+      width: containerWidth,
+      height: containerWidth * this.props.aspectRatio
+    });
+  }
+
+  handleError() {
+    this.setState({
+      source: {
+        uri: placeholder
+      }
+    });
+  }
+
+  render() {
+    const style = merge(this.props.style, {
+      width: this.state.width,
+      height: this.state.height
+    });
+
+    return (
+      <View onLayout={this.handleLayout}>
+        <Image
+          onError={this.handleError}
+          source={this.state.source}
+          style={style}
+        />
+      </View>
+    );
+  }
+}
+
+ImageComponent.propTypes = {
+  aspectRatio: PropTypes.number,
+  source: PropTypes.shape({
+    uri: PropTypes.string.isRequired
+  }).isRequired,
+  style: stylePropType
+};
+
+ImageComponent.defaultProps = {
+  aspectRatio: 0.75,
+  style: {}
+};
+
+export default ImageComponent;
---
+++ packages/image/image.test.js
@@ -0,0 +1,70 @@
+/* eslint-env jest */
+
+import { shallow } from "enzyme";
+import React from "react";
+import renderer from "react-test-renderer";
+import Image from "./image";
+import placeholder from "./placeholder";
+
+it("renders correctly with no dimensions and given URI", () => {
+  const props = {
+    source: {
+      uri: "http://example.com/image.jpg"
+    },
+    aspectRatio: 3 / 2
+  };
+  const tree = renderer.create(<Image {...props} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("use placeholder image when image is not reachable", done => {
+  const comp = new Image({
+    source: {
+      uri: "http://httpstat.us/404"
+    },
+    aspectRatio: 0.5
+  });
+
+  comp.setState = ({ source }) => {
+    expect(source.uri).toEqual(placeholder);
+
+    return done();
+  };
+
+  comp.handleError();
+});
+
+it("lays out the image with the correct aspect ratio", done => {
+  const comp = new Image({
+    source: {
+      uri: "http://httpstat.us/404"
+    },
+    aspectRatio: 0.5
+  });
+
+  comp.setState = ({ width, height }) => {
+    expect(width).toEqual(20);
+    expect(height).toEqual(10);
+
+    return done();
+  };
+
+  comp.handleLayout({ nativeEvent: { layout: { width: 20 } } });
+});
+
+it("sets the expected dimensions for a given state", () => {
+  const props = {
+    source: {
+      uri: "http://example.com/image.jpg"
+    }
+  };
+
+  const wrapper = shallow(<Image {...props} />);
+
+  wrapper.setState({
+    height: 300,
+    width: 100
+  });
+
+  expect(wrapper).toMatchSnapshot();
+});
