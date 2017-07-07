3b2c0fbcea62f8ed8af76425e3d0f113924343f9
Filipe Pinheiro <me@fampinheiro.com>
2017-07-07 16:11:00 +0100

    chore: adapt article-list code base to linting rules

--- packages/article-list/article-list.js
+++ packages/article-list/article-list.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { View, ListView, StyleSheet } from "react-native";
 import Card from "@times-components/card";
+import PropTypes from "prop-types";
 
 const styles = StyleSheet.create({
   separator: {
     flex: 1,
     height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
+    backgroundColor: "#8E8E8E"
   },
   container: {
     flex: 1,
     padding: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center"
   }
-})
+});
 
-const Row = article => (
+const Row = article =>
   <View>
     <Card {...article} />
-  </View>
-);
+  </View>;
 
-export default class ArticleList extends React.Component {
+class ArticleList extends React.Component {
   constructor(props) {
     super(props);
 
-    const articlesDataSource = new ListView.DataSource(
-      { rowHasChanged: (r1, r2) => r1 !== r2 }
-    );
+    const articlesDataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
     this.state = {
-      articles: articlesDataSource.cloneWithRows(props.articles),
+      articles: articlesDataSource.cloneWithRows(props.articles)
     };
   }
 
@@ -40,9 +40,16 @@ export default class ArticleList extends React.Component {
       <ListView
         contentContainerStyle={styles.container}
         dataSource={this.state.articles}
-        renderRow={(data) => <Row {...data} />}
-        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderRow={data => <Row {...data} />}
+        renderSeparator={(sectionId, rowId) =>
+          <View key={rowId} style={styles.separator} />}
       />
     );
   }
 }
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.shape(Card.propTypes))
+};
+
+export default ArticleList;
--- packages/article-list/article-list.test.js
+++ packages/article-list/article-list.test.js
@@ -1,7 +1,9 @@
+/* eslint-env jest */
+
 import "react-native";
 import React from "react";
-import ArticleList from "./article-list";
 import renderer from "react-test-renderer";
+import ArticleList from "./article-list";
 
 it("renders correctly", () => {
   const tree = renderer.create(<ArticleList />).toJSON();
@@ -9,7 +11,7 @@ it("renders correctly", () => {
   expect(tree).toBeTruthy();
 });
 
-it('renders a snapshot', () => {
+it("renders a snapshot", () => {
   const tree = renderer.create(<ArticleList />).toJSON();
 
   expect(tree).toMatchSnapshot();
