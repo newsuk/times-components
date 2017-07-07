7a9642347c6eb83ac1ec1131576e0998b0a8e330
Andy Trevorah <a.trevorah@gmail.com>
2017-07-07 16:08:17 +0100

    chore: more content in author profiles

--- packages/author-profile/author-profile.js
+++ packages/author-profile/author-profile.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, View, Text } from "react-native";
+import Card from "@times-components/card"
 
-const Header = ({ name, jobTitle, biography, image, twitter }) =>
-  <Text>HEADER</Text>;
+const Header = ({
+  name,
+  jobTitle,
+  biography,
+  image,
+  twitter
+}) =>
+  <View>
+    <Text>{name}</Text>
+    <Text>{jobTitle}</Text>
+    <Text>{biography}</Text>
+    <Text>{twitter}</Text>
+    <Text>HEADER PAGE CONTROL</Text>
+  </View>
 
-const Footer = () => <Text>FOOTER</Text>;
+const Footer = () => <Text>FOOTER PAGE CONTROL</Text>
 
 export default function AuthorProfile({
   name,
@@ -15,15 +28,32 @@ export default function AuthorProfile({
   articleCount,
   currentPageOfArticles,
   currentPageOffset,
-  pageSize
-}) {
+  pageSize}) {
   return (
     <FlatList
-      data={currentPageOfArticles}
-      keyExtractor={article => article.id}
-      ListHeaderComponent={() => <Header />}
-      renderItem={({ item }) => <Text>{item.title}</Text>}
-      ListFooterComponent={() => <Footer />}
-    />
+    data={currentPageOfArticles}
+    keyExtractor={article => article.id}
+    ListHeaderComponent={() =>
+      <Header
+        name={name}
+        jobTitle={jobTitle}
+        biography={biography}
+        image={image}
+        twitter={twitter}
+        />
+    }
+    renderItem={({ item }) => {
+      const {title, label, publicationName, publishedTime, teaser} = item;
+      return <Card
+        headline={title}
+        label={label || "null"}
+        publication={publicationName}
+        date={publishedTime}
+        image={{uri: "https://facebook.github.io/react/img/logo_og.png" }}
+        text={teaser}
+        />
+    }}
+    ListFooterComponent={() => <Footer/>}
+  />
   );
 }
