6745097dba4fc4fd960cc171a09ff7018bc6c24e
Jack Harvey <jack.harvey@news.co.uk>
2017-07-07 15:49:44 +0100

    Changes to Tealium component

--- packages/brightcove-video/brightcove-video.stories.js
+++ packages/brightcove-video/brightcove-video.stories.js
@@ -7,33 +7,61 @@ import { storiesOf } from "@storybook/react-native";
 import { decorateAction } from "@storybook/addon-actions";
 import BrightcoveVideo from "./brightcove-video";
 
+const TestTealiumVideo = (() => {
+  /* eslint react/prop-types: "off" */
+  let hasTealiumScriptLoaded = false;
+
+  class TealiumVideo extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        tealiumLoaded: hasTealiumScriptLoaded
+      };
+    }
+
+    componentDidMount() {
+      if (hasTealiumScriptLoaded) {
+        return;
+      }
+      const account = "newsinternational";
+      const profile = "thetimes.d.desktop";
+      const env = "dev";
+      const scriptElem = document.createElement("script");
+
+      scriptElem.src = `https://tags.tiqcdn.com/utag/${account}/${profile}/${env}/utag.js`;
+
+      scriptElem.onload = () => {
+        this.setState({ tealiumLoaded: true });
+      };
+
+      document.head.appendChild(scriptElem);
+      hasTealiumScriptLoaded = true;
+    }
+
+    render() {
+      if (this.state.tealiumLoaded) {
+        return (
+          <BrightcoveVideo
+            policyId={this.props.policyId}
+            videoId={this.props.videoId}
+            accountId={this.props.accountId}
+          />
+        );
+      }
+      return null;
+    }
+  }
+
+  return TealiumVideo;
+})();
+
 const policyId =
   "BCpkADawqM0NK0Rq8n6sEQyWykemrqeSmIQqqVt3XBrdpl8TYlvqN3hwKphBJRnkPgx6WAbozCW_VgTOBCNf1AQRh8KnmXSXfveQalRc5-pyNlSod5XzP99If2U";
 const accountId = "57838016001";
 const videoId = "4084164751001";
 
 const firstArgJSONAction = decorateAction([args => [JSON.stringify(args[0])]]);
-let hasTealiumScriptLoaded = false;
-
-class TealiumScript extends Component {
-  componentDidMount() {
-    if (hasTealiumScriptLoaded) {
-      return;
-    }
-    const account = "newsinternational";
-    const profile = "thetimes.d.desktop";
-    const env = "dev";
-    const script = document.createElement("script");
-
-    script.src = `https://tags.tiqcdn.com/utag/${account}/${profile}/${env}/utag.js`;
-    document.body.appendChild(script);
-    hasTealiumScriptLoaded = true;
-  }
-
-  render() {
-    return null;
-  }
-}
 
 class VideoAddTest extends Component {
   static getVideos(count) {
@@ -173,12 +201,9 @@ storiesOf("BrightcoveVideo", module)
     />
   )
   .add("With tealium on the page", () =>
-    <View>
-      <TealiumScript />
-      <BrightcoveVideo
-        policyId={policyId}
-        videoId={videoId}
-        accountId={accountId}
-      />
-    </View>
+    <TestTealiumVideo
+      policyId={policyId}
+      videoId={videoId}
+      accountId={accountId}
+    />
   );
