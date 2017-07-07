bc6c89de502a71abf403d8cdab8572e37b6a029e
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2017-07-07 15:55:05 +0100

    fix: add stateless component prop types

--- packages/author-head/author-head.js
+++ packages/author-head/author-head.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 import PropTypes from "prop-types";
 
-import Image from '@times-components/image';
+import Image from "@times-components/image";
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F9F8F3',
+    backgroundColor: "#F9F8F3",
     paddingTop: 15,
     paddingLeft: 15,
     paddingRight: 15
   },
   photoContainer: {
-    left: '100%',
+    left: "100%",
     bottom: -50
   },
   roundImage: {
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: '#EFEFEF',
+    backgroundColor: "#EFEFEF",
     borderWidth: 5,
     // borderStyle: 'solid',
-    borderColor: '#FFF',
-    left: '-66%',
-    position: 'relative'
+    borderColor: "#FFF",
+    left: "-66%",
+    position: "relative"
   },
   name: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 30,
     lineHeight: 30,
-    fontWeight: '400',
-    color: '#1D1D1B'
+    fontWeight: "400",
+    color: "#1D1D1B"
   },
   title: {
-    textAlign: "center",
+    textAlign: "center"
   },
   twitter: {
-    textAlign: 'center',
-    "fontSize": 15,
-    'color': "#069"
+    textAlign: "center",
+    fontSize: 15,
+    color: "#069"
   },
   bio: {
     textAlign: "center",
@@ -50,43 +50,49 @@ const styles = StyleSheet.create({
 });
 
 const Photo = ({ uri }) => <Image source={{ uri }} style={styles.roundImage} />;
-const Name = ({ children }) => <Text style={styles.name}>{ children }</Text>
-const Title = ({ children }) => <Text style={styles.title}>{ children }</Text>
-const TwitterHandle = ({ children }) => <Text style={styles.twitter}>{ children }</Text>
-const Biography = ({ children }) => <Text style={styles.bio}>{ children }</Text>
+Photo.propTypes = { uri: PropTypes.string.isRequired };
 
-const AuthorHead = (props) => {
-  const {
-    name,
-    title,
-    twitter,
-    bio,
-    uri
-  } = props;
+const Name = ({ children }) => <Text style={styles.name}>{children}</Text>;
+Name.propTypes = { children: PropTypes.string.isRequired };
+
+const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
+Title.propTypes = { children: PropTypes.string.isRequired };
+
+const TwitterHandle = ({ children }) =>
+  <Text style={styles.twitter}>{children}</Text>;
+TwitterHandle.propTypes = { children: PropTypes.string.isRequired };
+
+const Biography = ({ children }) => <Text style={styles.bio}>{children}</Text>;
+Biography.propTypes = { children: PropTypes.string.isRequired };
+
+const AuthorHead = props => {
+  const { name, title, twitter, bio, uri } = props;
 
   return (
     <View accessibilityRole="banner" style={styles.container}>
       <View>
-        <Name accessibilityRole="heading" aria-level="1">{ name }</Name>
-        <Title accessibilityRole="heading" aria-level="2">{ title.toLowerCase() }</Title>
-        <TwitterHandle>{ twitter }</TwitterHandle>
+        <Name accessibilityRole="heading" aria-level="1">{name}</Name>
+        <Title accessibilityRole="heading" aria-level="2">
+          {title.toLowerCase()}
+        </Title>
+        <TwitterHandle>{twitter}</TwitterHandle>
       </View>
       <View>
-        <Biography>{ bio }</Biography>
+        <Biography>{bio}</Biography>
       </View>
       <View style={styles.photoContainer}>
-        <Photo uri={ uri } />
+        <Photo uri={uri} />
       </View>
     </View>
-  )
-}
+  );
+};
 
 AuthorHead.defaultProps = {
-  name: '',
-  title: '',
-  bio: '',
-  uri: '',
-  twitter: ''
+  name: "",
+  title: "",
+  bio: "",
+  uri: "",
+  twitter: ""
 };
 
 AuthorHead.propTypes = {
@@ -99,11 +105,4 @@ AuthorHead.propTypes = {
 
 export default AuthorHead;
 
-export {
-  Photo,
-  Name,
-  Title,
-  TwitterHandle,
-  Biography,
-  AuthorHead as Header
-}
+export { Photo, Name, Title, TwitterHandle, Biography, AuthorHead as Header };
