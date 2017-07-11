9307e89aa558d4845cbe247f952fb6b288e4efa1
Filipe Pinheiro <me@fampinheiro.com>
2017-07-11 15:56:16 +0100

    fix: android css style

--- packages/author-head/author-head.js
+++ packages/author-head/author-head.js
@@ -6,20 +6,22 @@ import Image from "@times-components/image";
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
     display: "flex",
     alignItems: "center",
     flexDirection: "column",
-    backgroundColor: "#F9F8F3"
+    backgroundColor: "#F9F8F3",
+    paddingBottom: 50
   },
   photoContainer: {
     width: 100,
-    height: 50,
-    position: "relative"
+    height: 100,
+    bottom: 0,
+    position: "absolute"
   },
   roundImage: {
     width: 100,
     height: 100,
-    backgroundColor: "#EFEFEF",
     borderColor: "#FFF",
     borderRadius: 50,
     borderWidth: 5
@@ -45,6 +47,12 @@ const styles = StyleSheet.create({
     lineHeight: 25,
     marginTop: 6,
     color: "#333"
+  },
+  wrapper: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    display: "flex",
+    paddingBottom: 50
   }
 });
 
@@ -52,15 +60,17 @@ const AuthorHead = props => {
   const { name, title, twitter, bio, uri } = props;
 
   return (
-    <View accessibilityRole="banner" style={styles.container}>
-      <Text accessibilityRole="heading" aria-level="1" style={styles.name}>
-        {name}
-      </Text>
-      <Text accessibilityRole="heading" aria-level="2" style={styles.title}>
-        {title.toLowerCase()}
-      </Text>
-      <Text style={styles.twitter}>{twitter}</Text>
-      <Text style={styles.bio}>{bio}</Text>
+    <View style={styles.wrapper} pointerEvents="box-none">
+      <View accessibilityRole="banner" style={styles.container}>
+        <Text accessibilityRole="heading" aria-level="1" style={styles.name}>
+          {name}
+        </Text>
+        <Text accessibilityRole="heading" aria-level="2" style={styles.title}>
+          {title.toLowerCase()}
+        </Text>
+        <Text style={styles.twitter}>{twitter}</Text>
+        <Text style={styles.bio}>{bio}</Text>
+      </View>
       <View style={styles.photoContainer}>
         <Image aspectRatio={1} source={{ uri }} style={styles.roundImage} />
       </View>
