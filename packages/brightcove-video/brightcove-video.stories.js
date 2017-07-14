223a5692edee7aabe9efd110445e7baea2fce41c
Aron Shamash <aronshamash@gmail.com>
2017-07-14 15:55:31 +0100

    Fix rebase conflicts

--- packages/brightcove-video/brightcove-video.stories.js
+++ packages/brightcove-video/brightcove-video.stories.js
@@ -52,7 +52,7 @@ storiesOf("BrightcoveVideo", module)
     </View>
   )
   .add("Lazy load players", () =>
-    <VideoAdder policyId={policyId} videoId={videoId} accountId={accountId} />
+    <VideoAdder policyKey={policyKey} videoId={videoId} accountId={accountId} />
   )
   .add("With different videos", () =>
     <View>
@@ -60,7 +60,6 @@ storiesOf("BrightcoveVideo", module)
         width={320}
         height={200}
         policyKey={policyKey}
-        videoId={videoId}
         accountId={accountId}
       />
       <BrightcoveVideo
@@ -102,7 +101,7 @@ storiesOf("BrightcoveVideo", module)
   )
   .add("With external controls", () =>
     <VideoWithExternalControls
-      policyId={policyId}
+      policyKey={policyKey}
       videoId={videoId}
       accountId={accountId}
     />
