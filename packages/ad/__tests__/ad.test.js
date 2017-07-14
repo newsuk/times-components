08e4a695642069bc2c4dbe05c631ee82086ebf48
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-07-14 16:08:32 +0100

    chore: tests for native ads [WIP]; jest mock broken

--- packages/ad/__tests__/ad.test.js
+++ packages/ad/__tests__/ad.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import TestUtils from "react-dom/test-utils";
+import { Linking } from "react-native";
 
 import Ad from "../ad";
 
@@ -15,9 +17,19 @@ describe("Native Ad test", () => {
 
   beforeEach(() => {
     ad = <Ad {...adProps} />;
+    jest.unmock("ScrollView");
+    jest.mock("WebView", () => "WebView");
+    // jest.mock('Linking', () => 'Linking');
+    jest.dontMock("../ad");
   });
 
-  it("renders an native ad slot", () => {
+  afterEach(() => {
+    jest.unmock("Linking");
+    // jest.clearAllMocks();
+    // jest.resetAllMocks();
+  });
+
+  it("renders a native ad slot", () => {
     const tree = renderer.create(ad).toJSON();
 
     expect(tree).toMatchSnapshot();
@@ -25,7 +37,7 @@ describe("Native Ad test", () => {
 
   it("hasDifferentOrigin does not allow null origins", () => {
     const result = Ad.hasDifferentOrigin(null, adProps.baseUrl);
-    expect(result).toEqual(false);
+    expect(result).toEqual(null);
   });
 
   it("hasDifferentOrigin does not allow equal origins", () => {
@@ -45,15 +57,47 @@ describe("Native Ad test", () => {
     expect(result).toEqual(false);
   });
 
-  it("onOriginChange should try to open a link", () => {
+  it("onOriginChange should try to open a link", done => {
     const fakeUrl = "http://another-mock-url.com";
-    const Linking = {
-      canOpenURL: jest.fn(),
-      openURL: jest.fn()
-    };
+    jest.mock("Linking", () => ({
+      canOpenURL: jest
+        .fn()
+        .mockImplementation(() => new Promise(resolve => resolve(true))),
+      openURL: jest
+        .fn()
+        .mockImplementation(() => new Promise(resolve => resolve()))
+    }));
+
     Ad.onOriginChange(fakeUrl);
-    expect(Linking.canOpenURL).toHaveBeenCalled();
-    expect(Linking.openURL).toHaveBeenCalled();
+    expect(Linking.canOpenURL).toHaveBeenCalledWith(fakeUrl);
+    Linking.canOpenURL().then(() => {
+      expect(Linking.openURL).toHaveBeenCalled();
+      done();
+    });
+    expect(Linking.canOpenURL.mock.calls.length).toEqual(2);
+  });
+
+  it("onOriginChange should return error if url is not supported", done => {
+    const fakeUrl = "http://another-mock-url.com";
+    jest.mock("Linking", () => ({
+      canOpenURL: jest
+        .fn()
+        .mockImplementation(() => new Promise(resolve => resolve(false)))
+    }));
+
+    const consoleErrorSpy = jest.spyOn(console, "error");
+    Ad.onOriginChange(fakeUrl);
+    expect(Linking.canOpenURL.mock.calls.length).toEqual(1); // BUG found (retrives 3 instead of 1)
+    Linking.canOpenURL().then(supported => {
+      expect(supported).toEqual(false);
+      expect(consoleErrorSpy).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it("getMaxHeight is 0 is no size is sent", () => {
+    const result = Ad.getMaxHeight(null);
+    expect(result).toEqual(0);
   });
 
   it("getMaxHeight should return the maximum height from an array of arrays of sizes", () => {
@@ -63,30 +107,43 @@ describe("Native Ad test", () => {
   });
 
   it("handleOriginChange should not do anything if the origin is the same", () => {
-    const tree = renderer.create(ad);
+    const tree = TestUtils.renderIntoDocument(ad);
+    const hasDifferentOriginSpy = jest.spyOn(Ad, "hasDifferentOrigin");
+    const onOriginChangeSpy = jest.spyOn(Ad, "onOriginChange");
     tree.handleOriginChange(adProps.baseUrl);
-    expect(Ad.hasDifferentOrigin).toHaveBeenCalled();
-    expect(Ad.onOriginChange).not.toHaveBeenCalled();
+    expect(hasDifferentOriginSpy).toHaveBeenCalled();
+    expect(onOriginChangeSpy).not.toHaveBeenCalled();
   });
 
   it("handleOriginChange should prevent webview from loading and handle different origin if the origin is different", () => {
     const fakeUrl = "http://another-mock-url.com";
-    const tree = renderer.create(ad);
+    const tree = TestUtils.renderIntoDocument(ad);
     tree.webview = {
       stopLoading: jest.fn()
     };
+    jest.mock("Linking", () => ({
+      canOpenURL: jest
+        .fn()
+        .mockImplementation(() => new Promise(resolve => resolve(true))),
+      openURL: jest
+        .fn()
+        .mockImplementation(() => new Promise(resolve => resolve()))
+    }));
+    const hasDifferentOriginSpy = jest.spyOn(Ad, "hasDifferentOrigin");
+    const onOriginChangeSpy = jest.spyOn(Ad, "onOriginChange");
     tree.handleOriginChange(fakeUrl);
-    expect(Ad.hasDifferentOrigin).toHaveBeenCalled();
+    expect(hasDifferentOriginSpy).toHaveBeenCalled();
     expect(tree.webview.stopLoading).toHaveBeenCalled();
-    expect(Ad.onOriginChange).toHaveBeenCalled();
+    expect(onOriginChangeSpy).toHaveBeenCalled();
   });
 
   it("handleNavigationChange should verify if origin is different", () => {
-    const tree = renderer.create(ad);
+    const tree = TestUtils.renderIntoDocument(ad);
     const navState = {
       url: "http://another-mock-url.com"
     };
-    tree.handleOriginChange(navState);
+    tree.handleOriginChange = jest.fn();
+    tree.handleNavigationChange(navState);
     expect(tree.handleOriginChange).toHaveBeenCalledWith(navState.url);
   });
 });
