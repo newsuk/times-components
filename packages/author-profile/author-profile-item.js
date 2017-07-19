e4e7ff8b40a3fcde666efa1fd02455e3e7440c50
Filipe Pinheiro <me@fampinheiro.com>
2017-07-19 13:52:14 +0100

    feat: parse ast teaser

--- packages/author-profile/author-profile-item.js
+++ packages/author-profile/author-profile-item.js
@@ -9,7 +9,7 @@ const AuthorProfileItem = item => {
       uri:
         "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F9242e576-4dfc-11e7-a20e-a11097d3353d.jpg?crop=1463%2C975%2C293%2C12&resize=320"
     },
-    text: item.teaser,
+    text: JSON.parse(item.teaser),
     label: item.label,
     publication: item.publicationName
   };
