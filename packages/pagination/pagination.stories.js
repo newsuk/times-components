64bd32ed564aaa9c43d87741517922456e14ea7e
Filipe Pinheiro <me@fampinheiro.com>
2017-07-19 17:47:14 +0100

    feat: apply style to pagination

--- packages/pagination/pagination.stories.js
+++ packages/pagination/pagination.stories.js
@@ -1,9 +1,11 @@
-import "react-native";
 import React from "react";
+import { View } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import Pagination from "./pagination";
 
+const story = m => <View style={{ paddingTop: 20 }}>{m}</View>;
+
 storiesOf("Pagination", module)
-  .add("First page", () => <Pagination page={0} total={60} />)
-  .add("Another page", () => <Pagination page={1} total={60} />)
-  .add("Last page", () => <Pagination page={2} total={60} />);
+  .add("First page", () => story(<Pagination page={1} count={60} />))
+  .add("Another page", () => story(<Pagination page={2} count={60} />))
+  .add("Last page", () => story(<Pagination page={3} count={60} />));
