07af766eea2af8fed4121dae22572aff211ae1f1
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-07-27 16:29:08 +0100

    fix: revert adding the flag here

--- packages/article/article-content.js
+++ packages/article/article-content.js
@@ -2,12 +2,10 @@ import React from "react";
 import { ScrollView, Text } from "react-native";
 import PropTypes from "prop-types";
 import Ad from "@times-components/ad";
-import { NewArticleFlag } from "@times-components/article-flag";
 
 const ArticleContent = ({ code }) =>
   <ScrollView>
     <Text>Default Article.</Text>
-    <NewArticleFlag />
     <Ad code={code} section="article" />
   </ScrollView>;
 
