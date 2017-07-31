c1a62f9d6abc7a2c7af98884065cd7897333ce0b
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-07-31 12:18:14 +0100

    feat: beautify title

--- packages/article-flag/article-flag.js
+++ packages/article-flag/article-flag.js
@@ -13,15 +13,21 @@ const styles = {
     alignItems: "center"
   },
   diamond: {
-    marginRight: 3,
+    marginRight: 5,
     marginBottom: 1
   },
   title: {
     fontFamily: "TimesDigital-RegularSC",
     fontSize: 10,
-    fontWeight: "400",
-    letterSpacing: 1
+    fontWeight: "400"
   }
+};
+
+const beaufityTitle = function(title) {
+  // apply transformations to add uppercase and letter spacing.
+  // letterSpacing CSS prop does not work on android:
+  // https://github.com/facebook/react-native/pull/13199
+  return title.toUpperCase().split("").join(" ");
 };
 
 const ArticleFlag = ({ title, color }) => {
@@ -34,7 +40,7 @@ const ArticleFlag = ({ title, color }) => {
       <View style={styles.diamond}>
         <Diamond height={7} width={7} color={color} />
       </View>
-      <Text style={[styles.title, { color }]}>{title.toUpperCase()}</Text>
+      <Text style={[styles.title, { color }]}>{beaufityTitle(title)}</Text>
     </View>
   );
 };
