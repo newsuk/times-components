2b402e2fcffe68605f1787c4963d54cc1a446211
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-08-01 11:12:14 +0100

    feat: article label letter spacing

--- packages/article-label/article-label.js
+++ packages/article-label/article-label.js
@@ -10,12 +10,19 @@ const styles = {
   }
 };
 
+const beautifyTitle = function(title) {
+  // apply transformations to add uppercase and letter spacing.
+  // letterSpacing CSS prop does not work on android:
+  // https://github.com/facebook/react-native/pull/13199
+  return title.toUpperCase().split("").join(" ");
+};
+
 const ArticleLabel = ({ title, color }) => {
   if (!title) {
     return null;
   }
 
-  return <Text style={[styles.title, { color }]}>{title.toUpperCase()}</Text>;
+  return <Text style={[styles.title, { color }]}>{beautifyTitle(title)}</Text>;
 };
 
 ArticleLabel.propTypes = {
