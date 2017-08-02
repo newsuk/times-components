c9b061b51640be1ebb4e2bbb93de44ca844c41c6
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-08-02 15:43:50 +0100

    feat: add article label to article

--- packages/article/article-content.js
+++ packages/article/article-content.js
@@ -3,9 +3,11 @@ import { ScrollView, Text } from "react-native";
 import PropTypes from "prop-types";
 import Ad from "@times-components/ad";
 import { NewArticleFlag } from "@times-components/article-flag";
+import ArticleLabel from "@times-components/article-label";
 
 const ArticleContent = ({ code }) =>
   <ScrollView>
+    <ArticleLabel title="swimming" color="#008347" />
     <Text>Default Article.</Text>
     <NewArticleFlag />
     <Ad code={code} section="article" />
