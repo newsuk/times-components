8e8a14a9565b9261a3f0cc1eb4c9e69313c3f929
Daniela Borges Matos de Carvalho <alunassertiva@gmail.com>
2017-08-02 13:27:13 +0100

    Feat: article label (#118)
    * feat: article label
    
    * feat: article label letter spacing

---
+++ packages/article-label/article-label.stories.js
@@ -0,0 +1,8 @@
+import "react-native";
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import ArticleLabel from "./article-label";
+
+storiesOf("ArticleLabel", module).add("ArticleLabel", () =>
+  <ArticleLabel title="swimming" color="#008347" />
+);
