75b074e7c7daa8a6c8a30f9dc6352cac5c7ea627
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-08-03 10:27:48 +0100

    feat: add custom solution for android using webview

--- packages/article-headline/article-headline.js
+++ packages/article-headline/article-headline.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text } from "react-native";
 import PropTypes from "prop-types";
 
-const styles = StyleSheet.create({
+const styles = {
   title: {
     color: "#1D1D1B",
     fontSize: 22,
     lineHeight: 22,
     marginBottom: 8,
     fontFamily: "TimesModern-Bold",
-    fontWeight: "400"
+    fontWeight: "400",
+    letterSpacing: 1
   }
-});
+};
 
 const ArticleHeadline = ({ title, style }) =>
   <Text style={[styles.title, style]}>{title}</Text>;
