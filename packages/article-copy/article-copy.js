3f42fe41332c7cccc3db0c95befd28242e031ea0
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-08-03 17:23:36 +0100

    feat: WIP

--- packages/article-copy/article-copy.js
+++ packages/article-copy/article-copy.js
@@ -2,23 +2,27 @@ import React from "react";
 import { ScrollView, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-import HTMLView from "react-native-htmlview";
-
-const styles = StyleSheet.create({
-  p: {
-    color: "#333",
-    fontFamily: 'TimesDigital-Regular',
-    fontSize: 13,
-    lineHeight: 20,
-    marginTop: 10,
-    marginBottom: 10
-  }
-});
+import Image from '@times-components/image';
+import CopyParagraph, { PullQuoteParagraph } from './copy-paragraph';
 
 const ArticleCopy = ({ content }) => {
   const data = content.map((item, index) => {
-    if (item.type === 'paragraph') {
-      return (<HTMLView key={index} stylesheet={styles} value={'<p>' + item.data.text + '</p>'} />);
+    switch (item.type) {
+      case('paragraph'): {
+        return <CopyParagraph content={item.data.text} index={index} />;
+      }
+      case('image'): {
+        const imageSrc = {
+          uri: `https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F${item.data.id}.jpg`
+        }
+        return <Image source={imageSrc} />
+      }
+      case('pull-quote'): {
+        return <PullQuoteParagraph content={item.data.text} index={index} />;
+      }
+      default: {
+        return null;
+      }
     }
   });
 
@@ -30,3 +34,7 @@ ArticleCopy.propTypes = {
 }
 
 export default ArticleCopy;
+
+export {
+  CopyParagraph
+}
--- packages/article-copy/article-copy.stories.js
+++ packages/article-copy/article-copy.stories.js
@@ -4,7 +4,12 @@ import { storiesOf } from "@storybook/react-native";
 import ArticleCopy from "./article-copy";
 
 const multiParagraph = require("./fixtures/multi-paragraph.json").fixture;
+const differentTypes = require("./fixtures/different-types.json").fixture;
 
-storiesOf("ArticleCopy", module).add("ArticleCopy", () =>
-  <ArticleCopy content={multiParagraph} />
-);
+storiesOf("ArticleCopy", module)
+  .add("ArticleCopy with multiple paragraphs", () =>
+    <ArticleCopy content={multiParagraph} />
+  )
+  .add("ArticleCopy with different types (image, pull-quote, key-facts, paragraph)", () =>
+    <ArticleCopy content={differentTypes} />
+  );
