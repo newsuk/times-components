5329c486e85e2217a86a51c69c756ba6bd3632f1
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-08-03 11:31:27 +0100

    feat: article-copy

---
+++ packages/article-copy/article-copy.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import PropTypes from "prop-types";
+
+const ArticleCopy = () => {
+  return <Text>ArticleCopy</Text>;
+}
+
+ArticleCopy.propTypes = {
+  content: PropTypes.array
+}
+
+export default ArticleCopy;
---
+++ packages/article-copy/article-copy.stories.js
@@ -0,0 +1,8 @@
+import "react-native";
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import ArticleCopy from "./article-copy";
+
+storiesOf("ArticleCopy", module).add("ArticleCopy", () =>
+  <ArticleCopy />
+);
