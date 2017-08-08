765784eb2fe7afe3167a927f43fd0964ec76e5a2
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-08 12:04:13 +0100

    chore: update fructose (#133)
    * chore: update fructose to v 1.0.0
    
    * issue: only reference e2e test jest setup file in e2e tests

--- .fructose/setup.js
+++ .fructose/setup.js
@@ -1,6 +1,18 @@
-import setup from "@times-components/fructose/setup";
+/* globals beforeAll afterAll */
+import fructose from "@times-components/fructose/setup";
+import detox from "detox";
 import config from "../package";
 
 if (process.env.FRUCTOSE) {
-  setup(config.fructose);
+  fructose.withComponent();
+
+  beforeAll(async () => {
+    await fructose.hooks.setup();
+    await detox.init(config.detox);
+  }, 180000);
+
+  afterAll(async () => {
+    await detox.cleanup();
+    await fructose.hooks.cleanup();
+  });
 }
