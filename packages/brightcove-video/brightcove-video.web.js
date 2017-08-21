4e1934a34150219e9d4d70195e6d0a86acbc270e
Jack Harvey <jack.harvey@news.co.uk>
2017-08-21 10:59:23 +0100

    Remove times-brightcove component, use inline style on bc component

--- packages/brightcove-video/brightcove-video.web.js
+++ packages/brightcove-video/brightcove-video.web.js
@@ -180,8 +180,7 @@ class BrightcoveVideo extends Component {
       <div>
         <video
           id={this.state.id}
-          width={this.props.width}
-          height={this.props.height}
+          style={{width: this.props.width, height: this.props.height }}
           poster={this.props.poster}
           data-embed="default"
           data-video-id={this.props.videoId}
