4b8804631a0f081bbc8f1bb773ca0a0e8b40510c
killanaca <killanaca@gmail.com>
2017-08-22 16:51:20 +0100

    styling

--- packages/article/article-style.js
+++ packages/article/article-style.js
@@ -1,3 +1,43 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  ArticleAd: {
+    borderBottomWidth: 1,
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderBottomColor: "#DBDBDB"
+  },
+  ArticleBodyContainer: {
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  ArticleHeader: {
+    paddingTop: 15
+  },
+  ArticleHeadline: {
+    marginBottom: 15
+  },
+  ArticleHeadLineText: {
+    fontSize: 45,
+    lineHeight: 45,
+    color: "#333333",
+    marginBottom: 0
+  },
+  ArticleFlag: {
+    marginTop: 6,
+    marginBottom: 3
+  },
+  ArticleFlagContainer: {},
+  ArticleMeta: {
+    marginBottom: 10
+  },
+  ArticleMetaElement: {
+    borderTopColor: "#DBDBDB",
+    borderTopWidth: 1,
+    paddingTop: 6,
+    paddingBottom: 6
+  },
+  LeadAsset: {
+    marginBottom: 20
+  }
+});
