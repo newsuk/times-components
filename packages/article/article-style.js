5d080b499e4ae2fc81ee48e23b1fc484e8916410
Neha Srivastava <neha.srivastava1@news.co.uk>
2017-08-22 18:57:08 +0100

    Styling for web

--- packages/article/article-style.js
+++ packages/article/article-style.js
@@ -1,6 +1,62 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
+const webStyles = StyleSheet.create({
+  ArticleContainer: {
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  ArticleAd: {
+    marginLeft: -20,
+    marginRight: -20,
+    borderBottomWidth: 1,
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderBottomColor: "#DBDBDB"
+  },
+  ArticleBodyContainer: {
+    width: "58.33333%",
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  ArticleHeader: {
+    paddingTop: 15
+  },
+  ArticleHeadline: {
+    marginBottom: 15
+  },
+  ArticleHeadLineText: {
+    fontSize: 45,
+    lineHeight: 45,
+    color: "#333333",
+    marginBottom: 0
+  },
+  ArticleFlag: {
+    marginTop: 6,
+    marginBottom: 3
+  },
+  ArticleFlagContainer: {},
+  ArticleMeta: {
+    marginBottom: 20,
+    paddingRight: 20,
+    width: "35.71429%",
+    left: "-35.71429%",
+    position: "absolute"
+  },
+  ArticleMetaElement: {
+    borderTopColor: "#DBDBDB",
+    borderTopWidth: 1,
+    paddingTop: 6,
+    paddingBottom: 6
+  },
+  LeadAsset: {
+    marginBottom: 30
+  },
+  ArticleText: {
+    color: "#333"
+  }
+});
+
+const styles = StyleSheet.create({
   ArticleAd: {
     borderBottomWidth: 1,
     paddingBottom: 15,
@@ -44,3 +100,6 @@ export default StyleSheet.create({
     color: "#333"
   }
 });
+
+export default styles;
+export { webStyles };
