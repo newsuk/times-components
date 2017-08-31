fd01d9625b7414b0a2052581bb0711688e00c4a2
Ritchie Comley <richard.comley@news.co.uk>
2017-08-31 09:28:23 +0100

    feat: basic working launcher

--- packages/brightcove-video-launcher/brightcove-video-launcher.js
+++ packages/brightcove-video-launcher/brightcove-video-launcher.js
@@ -1,38 +1,53 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { Component } from "react";
+import { TouchableWithoutFeedback } from "react-native";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
+import BrightcoveVideo from "@times-components/brightcove-video";
 
-const BrightcoveVideoLauncher = ({ poster, width, height }) => <Image source={poster} style={{ width, height }}/>;
+class BrightcoveVideoLauncher extends Component {
+  constructor(props) {
+    super(props);
 
-const numberOrString = PropTypes.oneOfType([
-  PropTypes.string,
-  PropTypes.number
-]);
+    this.state = {
+      launched: false
+    };
+  }
 
-BrightcoveVideoLauncher.propTypes = {
-  videoId: PropTypes.string.isRequired,
-  accountId: PropTypes.string.isRequired,
-  policyKey: PropTypes.string,
-  playerId: PropTypes.string,
-  width: numberOrString,
-  height: numberOrString,
-  onError: PropTypes.func,
-  onChange: PropTypes.func,
-  autoplay: PropTypes.bool,
-  hideFullScreenButton: PropTypes.bool,
-  poster: PropTypes.string
-};
+  render() {
+    if (this.state.launched) {
+      return <BrightcoveVideo {...this.props} />;
+    }
 
-BrightcoveVideoLauncher.defaultProps = {
-  width: 320,
-  height: 180,
-  playerId: "default",
-  onError: () => {},
-  onChange: () => {},
-  autoplay: false,
-  hideFullScreenButton: false,
-  poster: "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
-};
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          this.setState({ launched: true });
+        }}
+      >
+        <Image
+          source={this.props.poster}
+          style={{ width: this.props.width, height: this.props.height }}
+        />
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+BrightcoveVideoLauncher.propTypes = Object.assign(
+  {
+    poster: PropTypes.string
+  },
+  BrightcoveVideo.propTypes
+);
+
+BrightcoveVideoLauncher.defaultProps = Object.assign(
+  {
+    poster:
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
+  },
+  BrightcoveVideo.defaultProps
+);
+
+BrightcoveVideoLauncher.defaultProps.autoplay = true;
 
 export default BrightcoveVideoLauncher;
--- packages/brightcove-video-launcher/brightcove-video-launcher.stories.js
+++ packages/brightcove-video-launcher/brightcove-video-launcher.stories.js
@@ -4,6 +4,18 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import BrightcoveVideoLauncher from "./brightcove-video-launcher";
 
-storiesOf("BrightcoveVideoLauncher", module).add("BrightcoveVideoLauncher", () =>
-  <BrightcoveVideoLauncher />
+const policyKey =
+  "BCpkADawqM0NK0Rq8n6sEQyWykemrqeSmIQqqVt3XBrdpl8TYlvqN3hwKphBJRnkPgx6WAbozCW_VgTOBCNf1AQRh8KnmXSXfveQalRc5-pyNlSod5XzP99If2U";
+const accountId = "57838016001";
+const videoId = "4084164751001";
+
+storiesOf(
+  "BrightcoveVideoLauncher",
+  module
+).add("BrightcoveVideoLauncher", () =>
+  <BrightcoveVideoLauncher
+    policyKey={policyKey}
+    videoId={videoId}
+    accountId={accountId}
+  />
 );
