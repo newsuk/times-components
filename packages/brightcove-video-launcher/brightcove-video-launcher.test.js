09ebcacb0a4a87b369635721496683d328376388
Ritchie Comley <richard.comley@news.co.uk>
2017-08-31 09:43:42 +0100

    test: pass in dummy video info

--- packages/brightcove-video-launcher/brightcove-video-launcher.test.js
+++ packages/brightcove-video-launcher/brightcove-video-launcher.test.js
@@ -5,8 +5,20 @@ import React from "react";
 import renderer from "react-test-renderer";
 import BrightcoveVideoLauncher from "./brightcove-video-launcher";
 
+const policyKey = "[POLICY_KEY]";
+const accountId = "[ACCOUNT_ID]";
+const videoId = "[VIDEO_ID]";
+
 it("renders correctly", () => {
-  const tree = renderer.create(<BrightcoveVideoLauncher />).toJSON();
+  const tree = renderer
+    .create(
+      <BrightcoveVideoLauncher
+        policyKey={policyKey}
+        videoId={videoId}
+        accountId={accountId}
+      />
+    )
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
