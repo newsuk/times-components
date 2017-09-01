b783949606a5735fdc3c4fb85471202e33c25ce4
killanaca <killanaca@gmail.com>
2017-09-01 11:51:51 +0100

    strict control on the props

--- packages/article-flag/article-flag.js
+++ packages/article-flag/article-flag.js
@@ -11,11 +11,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center"
   },
-  diamond: {
-    marginRight: 5,
-    marginBottom: 1
-  },
   title: {
+    marginTop: 1,
+    marginLeft: 5,
     fontFamily: "TimesDigital-RegularSC",
     fontSize: 12,
     fontWeight: "400"
@@ -27,16 +25,14 @@ const styles = StyleSheet.create({
 // https://github.com/facebook/react-native/pull/13199
 const beautifyTitle = title => title.toUpperCase().split("").join(" ");
 
-const ArticleFlag = ({ title, color, containerStyle }) => {
+const ArticleFlag = ({ title, color, style }) => {
   if (!title) {
     return null;
   }
 
   return (
-    <View style={[styles.view, containerStyle]}>
-      <View style={styles.diamond}>
-        <Diamond height={7} width={7} color={color} />
-      </View>
+    <View style={[styles.view, style]}>
+      <Diamond height={7} width={7} color={color} />
       <Text style={[styles.title, { color }]}>
         {beautifyTitle(title)}
       </Text>
@@ -47,28 +43,52 @@ const ArticleFlag = ({ title, color, containerStyle }) => {
 ArticleFlag.propTypes = {
   title: PropTypes.string.isRequired,
   color: PropTypes.string,
-  containerStyle: PropTypes.number
+  style: PropTypes.number
 };
 
 ArticleFlag.defaultProps = {
   color: "black",
-  containerStyle: undefined
+  style: undefined
 };
 
-const NewArticleFlag = props =>
-  <ArticleFlag title="new" color="#E34605" {...props} />;
-const UpdatedArticleFlag = props =>
-  <ArticleFlag title="updated" color="#3C81BE" {...props} />;
-const ExclusiveArticleFlag = props =>
-  <ArticleFlag title="exclusive" color="#C51D24" {...props} />;
-const SponsoredArticleFlag = props =>
-  <ArticleFlag title="sponsored" color="#4D4D4D" {...props} />;
+export const NewArticleFlag = props =>
+  <ArticleFlag title="new" color="#E34605" style={props.style} />;
+
+NewArticleFlag.propTypes = {
+  style: PropTypes.string
+};
+NewArticleFlag.defaultProps = {
+  style: undefined
+};
+
+export const UpdatedArticleFlag = props =>
+  <ArticleFlag title="updated" color="#3C81BE" style={props.style} />;
+
+UpdatedArticleFlag.propTypes = {
+  style: PropTypes.string
+};
+UpdatedArticleFlag.defaultProps = {
+  style: undefined
+};
+
+export const ExclusiveArticleFlag = props =>
+  <ArticleFlag title="exclusive" color="#C51D24" style={props.style} />;
+
+ExclusiveArticleFlag.propTypes = {
+  style: PropTypes.string
+};
+ExclusiveArticleFlag.defaultProps = {
+  style: undefined
+};
+
+export const SponsoredArticleFlag = props =>
+  <ArticleFlag title="sponsored" color="#4D4D4D" style={props.style} />;
+
+SponsoredArticleFlag.propTypes = {
+  style: PropTypes.string
+};
+SponsoredArticleFlag.defaultProps = {
+  style: undefined
+};
 
 export default ArticleFlag;
-
-export {
-  NewArticleFlag,
-  UpdatedArticleFlag,
-  ExclusiveArticleFlag,
-  SponsoredArticleFlag
-};
