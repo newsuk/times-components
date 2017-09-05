a9a7221d61da488a0e906a2b86785eef12850284
Andy Trevorah <andrew.trevorah@news.co.uk>
2017-09-05 17:23:56 +0100

    BREAKING CHANGE: separated text links from block links

--- packages/link/link.web.js
+++ packages/link/link.web.js
@@ -1,32 +1,13 @@
 import React from "react";
-import pick from "lodash.pick";
-import { Text } from "react-native";
+import propTypes from "./link.proptypes";
 
-import { defaultProps, propTypes } from "./link.proptypes";
-
-const styles = {
-  color: "#069",
-  textDecorationLine: "underline"
-};
-
-const Link = props => {
-  const { children, onPress = () => {}, style, url } = props;
-
-  return (
-    <Text
-      {...pick(props, Object.keys(Text.propTypes))}
-      accessibilityRole="link"
-      href={url}
-      onPress={onPress}
-      style={[styles, style]}
-    >
-      {children}
-    </Text>
-  );
-};
-
-Link.defaultProps = defaultProps;
+const Link = ({ url, onPress, children }) => (
+  <a href={url} onClick={onPress}>
+    {children}
+  </a>
+);
 
 Link.propTypes = propTypes;
 
 export default Link;
+export { default as TextLink } from "./text-link";
