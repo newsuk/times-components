aa8e7d7cf0c64d26d6b53c22b11db494ee59d2bf
killanaca <killanaca@gmail.com>
2017-09-07 12:27:21 +0100

    update the fixtures

--- packages/article/__tests__/article.test.js
+++ packages/article/__tests__/article.test.js
@@ -3,18 +3,23 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Article from "../article";
 
-const articleFixtureFull = require("../fixtures/full-article.json");
-const articleFixtureNoFlags = require("../fixtures/article-no-flags.json");
-const articleFixtureNoFlagsNoStandfirst = require("../fixtures/article-no-flags-no-standfirst.json");
+const fullArticleFixture = require("../fixtures/full-article.json");
+const articleFixtureNoLabel = require("../fixtures/no-label.json");
+const articleFixtureNoFlags = require("../fixtures/no-flags.json");
+const articleFixtureNoStandfirst = require("../fixtures/no-standfirst.json");
+const articleFixtureNoStandfirstNoLabel = require("../fixtures/no-standfirst-no-label.json");
+const articleFixtureNoStandfirstNoFlags = require("../fixtures/no-standfirst-no-flags.json");
+const articleFixtureNoLabelNoFlags = require("../fixtures/no-label-no-flags.json");
+const articleFixtureNoLabelNoFlagsNoStandFirst = require("../fixtures/no-label-no-flags-no-standfirst.json");
 
 describe("Article test", () => {
-  it("renders native correctly", () => {
+  it("renders empty article", () => {
     const tree = renderer.create(<Article />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders full article correctly", () => {
-    const tree = renderer.create(<Article {...articleFixtureFull} />).toJSON();
+  it("renders full article", () => {
+    const tree = renderer.create(<Article {...fullArticleFixture} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
@@ -25,9 +30,42 @@ describe("Article test", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders article no flags no standfirst", () => {
+  it("renders article no label", () => {
     const tree = renderer
-      .create(<Article {...articleFixtureNoFlagsNoStandfirst} />)
+      .create(<Article {...articleFixtureNoLabel} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders article no standfirst", () => {
+    const tree = renderer
+      .create(<Article {...articleFixtureNoStandfirst} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders article no standfirst no flags", () => {
+    const tree = renderer
+      .create(<Article {...articleFixtureNoStandfirstNoFlags} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders article no standfirst no label", () => {
+    const tree = renderer
+      .create(<Article {...articleFixtureNoStandfirstNoLabel} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders article no label no flags", () => {
+    const tree = renderer
+      .create(<Article {...articleFixtureNoLabelNoFlags} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders article no label no flags no standfirst", () => {
+    const tree = renderer
+      .create(<Article {...articleFixtureNoLabelNoFlagsNoStandFirst} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
--- packages/article/article.stories.js
+++ packages/article/article.stories.js
@@ -1,60 +1,33 @@
 /* eslint-env browser */
-import {
-  ApolloClient,
-  ApolloProvider,
-  createNetworkInterface
-} from "react-apollo";
 import { storiesOf } from "@storybook/react-native";
-import ArticleProvider from "@times-components/provider";
 import React from "react";
 import Article from "./article";
 
 const fullArticleFixture = require("./fixtures/full-article.json");
-
-const networkInterface = createNetworkInterface({
-  uri: "http://localhost:4000/graphql"
-});
-
-networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      if (!req.options.headers) {
-        req.options.headers = {};
-      }
-      req.options.headers["X-authorization"] = "DO_NOT_USE_IN_PROD";
-      next();
-    }
-  }
-]);
-
-const client = new ApolloClient({
-  networkInterface
-});
+const articleFixtureNoStandfirst = require("./fixtures/no-standfirst.json");
+const articleFixtureNoLabel = require("./fixtures/no-label.json");
+const articleFixtureNoFlags = require("./fixtures/no-flags.json");
+const articleFixtureNoStandfirstNoLabel = require("./fixtures/no-standfirst-no-label.json");
+const articleFixtureNoStandfirstNoFlags = require("./fixtures/no-standfirst-no-flags.json");
+const articleFixtureNoLabelNoFlags = require("./fixtures/no-label-no-flags.json");
+const articleFixtureNoLabelNoFlagsNoStandFirst = require("./fixtures/no-label-no-flags-no-standfirst.json");
 
 storiesOf("Article", module)
   .add("Fixtures - Full", () => <Article {...fullArticleFixture} />)
-  .add("Apollo - long article with italic and bold", () =>
-    <ApolloProvider client={client}>
-      <ArticleProvider id="ccf235b7-bc11-4c3f-bbe5-b0c5c6fc2b29" />(Article)
-    </ApolloProvider>
+  .add("Fixtures - No standfirst", () =>
+    <Article {...articleFixtureNoStandfirst} />
   )
-  .add("Apollo - Label, Flags, Standfirst", () =>
-    <ApolloProvider client={client}>
-      <ArticleProvider id="198c4b2f-ecec-4f34-be53-c89f83bc1b44" />(Article)
-    </ApolloProvider>
+  .add("Fixtures - No label", () => <Article {...articleFixtureNoLabel} />)
+  .add("Fixtures - No flags", () => <Article {...articleFixtureNoFlags} />)
+  .add("Fixtures - No standfirst, no label", () =>
+    <Article {...articleFixtureNoStandfirstNoLabel} />
   )
-  .add("Apollo - Multiple Label and italics", () =>
-    <ApolloProvider client={client}>
-      <ArticleProvider id="be725c01-dd7e-4c46-85b5-16ffc30c0b98" />(Article)
-    </ApolloProvider>
+  .add("Fixtures - No standfirst, no flags", () =>
+    <Article {...articleFixtureNoStandfirstNoFlags} />
   )
-  .add("Apollo - Production Article", () =>
-    <ApolloProvider client={client}>
-      <ArticleProvider id="5cbac836-8eea-11e7-86bd-27eb324693e0" />(Article)
-    </ApolloProvider>
+  .add("Fixtures - No label, no flags", () =>
+    <Article {...articleFixtureNoLabelNoFlags} />
   )
-  .add("Apollo - Production Article bold", () =>
-    <ApolloProvider client={client}>
-      <ArticleProvider id="978ecf38-8eff-11e7-86bd-27eb324693e0" />(Article)
-    </ApolloProvider>
+  .add("Fixtures - No label, no flags, no standfirst", () =>
+    <Article {...articleFixtureNoLabelNoFlagsNoStandFirst} />
   );
