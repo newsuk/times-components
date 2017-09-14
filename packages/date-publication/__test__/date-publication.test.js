76786e83e51d9dbaa5b1b23ba9eabce42353402c
killanaca <killanaca@gmail.com>
2017-09-14 17:28:39 +0100

    remove shared

--- packages/date-publication/__test__/date-publication.test.js
+++ packages/date-publication/__test__/date-publication.test.js
@@ -1,7 +1,30 @@
 /* eslint-env jest */
 
-import shared from "./shared";
+import "react-native";
+import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from "react-test-renderer";
+import DatePublication from "../date-publication";
 
-describe("Date Publication test on ios/android", () => {
-  shared();
+describe("Date Publication test", () => {
+  const props = {
+    date: new Date("2017-07-01T14:32:00.000Z"),
+    publication: "TIMES"
+  };
+
+  it("renders a DatePublication component with content", () => {
+    const tree = renderer.create(<DatePublication {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a DatePublication component with red font color", () => {
+    const styleProps = {
+      ...props,
+      style: {
+        color: "red"
+      }
+    };
+    const tree = renderer.create(<DatePublication {...styleProps} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
