a18db6f65902a93c04f480e88e3083ab24e84b2f
Lowry99 <matthew.lowry@hotmail.co.uk>
2017-09-15 16:39:12 +0100

    removing used dependancies

--- .fructose/setup.js
+++ .fructose/setup.js
@@ -1,12 +1,7 @@
 /* globals beforeAll afterAll */
 import fructose from "@times-components/fructose/setup";
 import detox from "detox";
-import webdriverio from "webdriverio";
 import { spawn } from "child_process";
-import { chromeless } from "chromeless";
-import path from "path";
-import portscanner from "portscanner";
-import EventEmitter from "events";
 import config from "../package";
 
 // this sets up the with component global, issue logged in fructose
