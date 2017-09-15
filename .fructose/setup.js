8bd8fa55ff0a2d544845e599fbc5918352dd35be
Lowry99 <matthew.lowry@hotmail.co.uk>
2017-09-15 15:39:22 +0100

    fix: removing android and web logic

--- .fructose/setup.js
+++ .fructose/setup.js
@@ -9,81 +9,15 @@ import portscanner from "portscanner";
 import EventEmitter from "events";
 import config from "../package";
 
-const isPortTaken = port =>
-  new Promise(resolve => {
-    portscanner.checkPortStatus(port, "127.0.0.1", (error, status) => {
-      // Status is 'open' if currently in use or 'closed' if available
-      const isTaken = status === "open";
-      resolve(isTaken);
-    });
-  });
-
-const checkIfPortTaken = x =>
-  new Promise(resolve => {
-    const event = new EventEmitter();
-    event.on("taken", taken => {
-      resolve(taken);
-    });
-    let taken;
-    let repeatTimes = x;
-    const interval = setInterval(async () => {
-      repeatTimes -= 1;
-      taken = await isPortTaken(3000);
-      if (taken) {
-        event.emit("taken", true);
-        clearInterval(interval);
-      }
-      if (!taken && repeatTimes === 0) {
-        event.emit("taken", false);
-        clearInterval(interval);
-      }
-    }, 1000);
-  });
-
-let appium;
+// this sets up the with component global, issue logged in fructose
 fructose.withComponent();
 
 beforeAll(async () => {
-  if (process.env.FRUCTOSE) {
-    if (process.env.ANDROID || process.env.IOS) {
-      await fructose.hooks.mobile.setup();
-      if (process.env.ANDROID) {
-        appium = await new Promise(resolve => {
-          const proc = spawn("appium");
-          proc.stdout.on("data", d => {
-            if (d.toString("utf8").includes("started on 0.0.0.0:4723")) {
-              resolve(proc);
-            }
-          });
-        });
-        const options = {
-          desiredCapabilities: {
-            platformName: "Android",
-            platformVersion: "7.0",
-            deviceName: "Android Emulator",
-            app: path.join(
-              __dirname,
-              "../android/app/build/outputs/apk/app-debug.apk"
-            )
-          },
-          host: "localhost",
-          port: 4723
-        };
-        global.driver = webdriverio.remote(options);
-        await global.driver.init();
-        await global.driver.waitForVisible('//*[@text="Fructose"]', 45000);
-      } else {
-        await detox.init(config.detox);
-      }
-    } else if (process.env.WEB) {
-      await fructose.hooks.web.setup();
-      const taken = await checkIfPortTaken(10);
-      if (!taken) {
-        console.error("The fructose app did not start correctly");
-        process.exit(1);
-      }
-      global.Chromeless = Chromeless;
-    }
+  if (process.env.IOS) {
+    await fructose.hooks.mobile.setup();
+    await detox.init(config.detox);
+  } else {
+    throw new Error("invalid platform - Could not run tests");
   }
 }, 180000);
 
@@ -91,12 +25,5 @@ afterAll(async () => {
   if (process.env.IOS) {
     await detox.cleanup();
     await fructose.hooks.mobile.cleanup();
-  }
-  if (process.env.ANDROID) {
-    appium.kill();
-    await fructose.hooks.mobile.cleanup();
-  }
-  if (process.env.WEB) {
-    await fructose.hooks.web.cleanup();
   }
 });
