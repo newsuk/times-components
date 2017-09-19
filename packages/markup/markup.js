b4f39d8b32c8ac99e1707439a7696b7dcb2219d5
Andy Trevorah <andrew.trevorah@news.co.uk>
2017-09-19 11:58:03 +0100

    chore: removed unused variable

--- packages/markup/markup.js
+++ packages/markup/markup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import renderTreeWithoutDefaults from "./render-tree-without-defaults";
 
 const styles = {
