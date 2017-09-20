cbeb2bdb48782ae062250ff55267cce639e3aa82
Lowry99 <matthew.lowry@hotmail.co.uk>
2017-09-20 15:41:19 +0100

    test: adding first chromeless test for authorProfile

--- packages/author-profile/author-profile.fructose.web.js
+++ packages/author-profile/author-profile.fructose.web.js
@@ -1,6 +1,4 @@
-
-
-/* globals withComponent test expect beforeEach */
+/* globals withComponent Chromeless test expect beforeEach */
 import React from "react";
 import AuthorProfile from "./author-profile";
 import authorProfileGenerator from "./fixture-generator";
@@ -10,7 +8,7 @@ const setup = () => {
   chromeless = new Chromeless();
 };
 
-const examples = authorProfileGenerator(10);
+const examples = authorProfileGenerator(11);
 
 const props = {
   data: Object.assign({}, examples, {
@@ -22,25 +20,25 @@ const props = {
 };
 
 withComponent(
-  <AuthorProfile fructoseID='authorProfile' {...props} />,
+  <AuthorProfile fructoseID="authorProfile" {...props} />,
   "author profile",
   async fructose => {
     beforeEach(setup);
     test(
       "renders and scrolls",
       async () => {
-
         await chromeless
-        .goto("http://localhost:3000")
-        .exists("[data-testid='fructose']");
+          .goto("http://localhost:3000")
+          .exists("[data-testid='fructose']");
 
         await fructose.loadComponent();
         await chromeless.exists('[data-testid="author-name"]');
-        await chromeless.scrollToElement('[data-testid="articleList-8"]')
 
-          // exists needs to be replaced with .visible currently in chromeless backlog
-          // https://github.com/graphcool/chromeless/issues/33
-        const result = await chromeless.exists('[data-testid="articleList-8"]')
+        await chromeless.scrollToElement('[data-testid="articleList-8"]');
+
+        // exists needs to be replaced with .visible currently in chromeless backlog
+        // https://github.com/graphcool/chromeless/issues/33
+        const result = await chromeless.exists('[data-testid="articleList-8"]');
         expect(result).toBe(true);
 
         await chromeless.end();
