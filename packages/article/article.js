a5cc30afee777689a22c74dbb924274713f525ee
killanaca <killanaca@gmail.com>
2017-09-21 23:25:22 +0100

    default article component

--- packages/article/article.js
+++ packages/article/article.js
@@ -1,11 +1,144 @@
 import React from "react";
-import { View } from "react-native";
-import ArticleContent from "./article-content";
+import { Text, View, ListView, ActivityIndicator } from "react-native";
+import PropTypes from "prop-types";
+import { renderTrees } from "@times-components/markup";
+import Image from "@times-components/image";
+import pick from "lodash.pick";
 
-const Article = () => (
-  <View>
-    <ArticleContent code="intervention" />
-  </View>
-);
+import styles from "./article-style";
+import ArticleHeader from "./article-header";
+import ArticleMeta from "./article-meta";
 
-export default Article;
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+const listViewPageSize = 1;
+const listViewSize = 10;
+const listViewScrollRenderAheadDistance = 10;
+
+class ArticlePage extends React.Component {
+  static prepareDataForListView(props) {
+    const articleData = props.data.article;
+    const leadAssetData = pick(articleData, ["leadAsset"]);
+    const articleHeaderData = pick(articleData, [
+      "label",
+      "title",
+      "standfirst",
+      "flags"
+    ]);
+
+    const articleMidContainerData = pick(articleData, [
+      "publicationName",
+      "publishedTime",
+      "byline"
+    ]);
+    return Array.prototype.concat(
+      [
+        { type: "leadAsset", data: leadAssetData },
+        { type: "header", data: articleHeaderData },
+        { type: "middleContainer", data: articleMidContainerData }
+      ],
+      articleData.content.map(i => ({
+        type: "article_body_row",
+        data: i
+      }))
+    );
+  }
+
+  static renderRow(rowData) {
+    if (rowData.type === "leadAsset") {
+      return (
+        <View style={styles.LeadAsset}>
+          <Image source={{ uri: rowData.data.leadAsset.crop.url }} />
+        </View>
+      );
+    } else if (rowData.type === "header") {
+      const { title, flags, standfirst, label } = rowData.data;
+      return (
+        <ArticleHeader
+          title={title}
+          flags={flags}
+          standfirst={standfirst}
+          label={label}
+        />
+      );
+    } else if (rowData.type === "middleContainer") {
+      const { byline, publishedTime, publicationName } = rowData.data;
+      return (
+        <ArticleMeta
+          byline={byline}
+          publishedTime={publishedTime}
+          publicationName={publicationName}
+        />
+      );
+    } else if (rowData.type === "article_body_row") {
+      return (
+        <View style={[styles.ArticleMainContentRow]}>
+          {renderTrees([rowData.data], {
+            paragraph(key, attributes, children) {
+              return (
+                <Text key={key} style={styles.ArticleTextElement}>
+                  {children}
+                </Text>
+              );
+            }
+          })}
+        </View>
+      );
+    }
+
+    return null;
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: ds.cloneWithRows({})
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.data.loading) {
+      this.setState({
+        dataSource: ds.cloneWithRows(
+          ArticlePage.prepareDataForListView(nextProps)
+        )
+      });
+    }
+  }
+
+  render() {
+    if (this.props.data.loading) {
+      return (
+        <View style={styles.Container}>
+          <ActivityIndicator size={"large"} />
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.PageWrapper}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={ArticlePage.renderRow}
+          initialListSize={listViewSize}
+          scrollRenderAheadDistance={listViewScrollRenderAheadDistance}
+          pageSize={listViewPageSize}
+        />
+      </View>
+    );
+  }
+}
+
+ArticlePage.propTypes = {
+  data: PropTypes.shape({
+    article: PropTypes.object,
+    loading: PropTypes.boolean
+  })
+};
+
+ArticlePage.defaultProps = {
+  data: {
+    loading: true,
+    article: {}
+  }
+};
+
+export default ArticlePage;
