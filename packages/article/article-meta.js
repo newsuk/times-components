85d23b2fe7d1cd7ea42429a03a15d8de7cf2e1d7
killanaca <killanaca@gmail.com>
2017-09-21 23:25:22 +0100

    default article component

---
+++ packages/article/article-meta.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+
+import ArticleByline from "@times-components/article-byline";
+import DatePublication from "@times-components/date-publication";
+
+import styles from "./article-style";
+
+const ArticleHeader = ({ byline, publishedTime, publicationName }) => (
+  <View style={[styles.ArticleMainContentRow, styles.ArticleMiddleContainer]}>
+    <View style={styles.ArticleMeta}>
+      <View style={[styles.ArticleMetaElement]}>
+        <ArticleByline ast={byline} />
+      </View>
+      <View style={[styles.ArticleMetaElement]}>
+        <DatePublication
+          date={new Date(publishedTime)}
+          publication={publicationName}
+        />
+      </View>
+    </View>
+  </View>
+);
+
+ArticleHeader.propTypes = {
+  byline: PropTypes.arrayOf(PropTypes.object),
+  publishedTime: PropTypes.string,
+  publicationName: PropTypes.string
+};
+
+ArticleHeader.defaultProps = {
+  byline: [],
+  publishedTime: null,
+  publicationName: null
+};
+
+export default ArticleHeader;
--- packages/article/article.stories.js
+++ packages/article/article.stories.js
@@ -1,30 +1,33 @@
 /* eslint-env browser */
-
-import { View, Platform } from "react-native";
-import React from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { storiesOf } from "@storybook/react-native";
-import Article from "../article";
+import React from "react";
+import Article from "./article";
 
-const renderIframe = () => {
-  if (Platform.OS !== "web") {
-    return null;
-  }
+const fullArticleFixture = require("./fixtures/full-article.json");
+const articleFixtureNoStandfirst = require("./fixtures/no-standfirst.json");
+const articleFixtureNoLabel = require("./fixtures/no-label.json");
+const articleFixtureNoFlags = require("./fixtures/no-flags.json");
+const articleFixtureNoStandfirstNoLabel = require("./fixtures/no-standfirst-no-label.json");
+const articleFixtureNoStandfirstNoFlags = require("./fixtures/no-standfirst-no-flags.json");
+const articleFixtureNoLabelNoFlags = require("./fixtures/no-label-no-flags.json");
+const articleFixtureNoLabelNoFlagsNoStandFirst = require("./fixtures/no-label-no-flags-no-standfirst.json");
 
-  return (
-    <a
-      href={`/iframe.html${window.top.location.search}`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Render ads
-    </a>
-  );
-};
-
-storiesOf("Article", module).add("Article", () => (
-  <View>
-    {renderIframe()}
-    <Article />
-  </View>
-));
+storiesOf("Article", module)
+  .add("Fixtures - Full", () => <Article {...fullArticleFixture} />)
+  .add("Fixtures - No standfirst", () => (
+    <Article {...articleFixtureNoStandfirst} />
+  ))
+  .add("Fixtures - No label", () => <Article {...articleFixtureNoLabel} />)
+  .add("Fixtures - No flags", () => <Article {...articleFixtureNoFlags} />)
+  .add("Fixtures - No standfirst, no label", () => (
+    <Article {...articleFixtureNoStandfirstNoLabel} />
+  ))
+  .add("Fixtures - No standfirst, no flags", () => (
+    <Article {...articleFixtureNoStandfirstNoFlags} />
+  ))
+  .add("Fixtures - No label, no flags", () => (
+    <Article {...articleFixtureNoLabelNoFlags} />
+  ))
+  .add("Fixtures - No label, no flags, no standfirst", () => (
+    <Article {...articleFixtureNoLabelNoFlagsNoStandFirst} />
+  ));
