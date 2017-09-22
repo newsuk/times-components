8a0b85ddd98377f721ac7dc2a4bcb3fd6a906c37
JGAntunes <j.goncalo.antunes@gmail.com>
2017-09-22 17:41:56 +0100

    chore: fix linting for article.web.test

--- packages/article/__tests__/article.web.test.js
+++ packages/article/__tests__/article.web.test.js
@@ -3,8 +3,8 @@
 import shared from "./shared";
 
 // Due to https://github.com/facebook/react/issues/7371
-jest.mock('react-dom', () => ({
-  findDOMNode: () => ({}),
+jest.mock("react-dom", () => ({
+  findDOMNode: () => ({})
 }));
 
 describe("Article test on web", () => {
