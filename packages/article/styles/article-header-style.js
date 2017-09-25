097dbbe9deb5ec142a020040fecb1819bd5cb891
Neha <neha.srivastava84525@gmail.com>
2017-09-25 12:23:47 +0100

    Prettifying

--- packages/article/styles/article-header-style.js
+++ packages/article/styles/article-header-style.js
@@ -1,7 +1,4 @@
-import {
-    StyleSheet,
-    Platform
-} from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import globalStyle from "./article-global-style";
 
 const standFirstFontFamily = "TimesModern-Regular";
@@ -9,42 +6,42 @@ const standFirstFontFamily = "TimesModern-Regular";
 const styles = {};
 
 const webStyles = StyleSheet.create({
-    articleHeadLineText: {
-        fontSize: 30,
-        lineHeight: 32,
-        color: "#000000",
-        marginBottom: 0
-    },
-    standFirst: {
-        fontSize: 21,
-        lineHeight: 26,
-        fontFamily: standFirstFontFamily,
-        color: "#333333",
-        paddingBottom: 9
-    }
+  articleHeadLineText: {
+    fontSize: 30,
+    lineHeight: 32,
+    color: "#000000",
+    marginBottom: 0
+  },
+  standFirst: {
+    fontSize: 21,
+    lineHeight: 26,
+    fontFamily: standFirstFontFamily,
+    color: "#333333",
+    paddingBottom: 9
+  }
 });
 
 const nativeStyles = StyleSheet.create({
-    articleHeadLineText: {
-        fontSize: Platform.OS === "android" ? 28 : 30,
-        lineHeight: Platform.OS === "android" ? 37 : 32,
-        color: "#000000",
-        fontWeight: Platform.OS === "android" ? "400" : "700",
-        marginBottom: 0
-    },
-    standFirst: {
-        fontSize: 21,
-        lineHeight: Platform.OS === "android" ? 29 : 25,
-        color: "#333333",
-        fontFamily: standFirstFontFamily,
-        paddingBottom: 9
-    },
+  articleHeadLineText: {
+    fontSize: Platform.OS === "android" ? 28 : 30,
+    lineHeight: Platform.OS === "android" ? 37 : 32,
+    color: "#000000",
+    fontWeight: Platform.OS === "android" ? "400" : "700",
+    marginBottom: 0
+  },
+  standFirst: {
+    fontSize: 21,
+    lineHeight: Platform.OS === "android" ? 29 : 25,
+    color: "#333333",
+    fontFamily: standFirstFontFamily,
+    paddingBottom: 9
+  }
 });
 
 if (Platform.OS !== "web") {
-    Object.assign(styles, globalStyle, nativeStyles);
+  Object.assign(styles, globalStyle, nativeStyles);
 } else {
-    Object.assign(styles, globalStyle, webStyles);
+  Object.assign(styles, globalStyle, webStyles);
 }
 
 export default styles;
--- packages/article/styles/article-style.js
+++ packages/article/styles/article-style.js
@@ -1,7 +1,4 @@
-import {
-    StyleSheet,
-    Platform
-} from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import globalStyle from "./article-global-style";
 
 const bodyFontFamilyWebIos = "TimesDigitalW04";
@@ -10,37 +7,38 @@ const bodyFontFamilyAndroid = "TimesDigitalW04-Regular";
 const styles = {};
 
 const webStyles = StyleSheet.create({
-    leadAsset: {
-        marginBottom: 10
-    },
-    articleTextElement: {
-        fontFamily: bodyFontFamilyWebIos,
-        lineHeight: 26,
-        fontSize: 17,
-        marginBottom: 25,
-        marginTop: 0,
-        color: "#333"
-    }
+  leadAsset: {
+    marginBottom: 10
+  },
+  articleTextElement: {
+    fontFamily: bodyFontFamilyWebIos,
+    lineHeight: 26,
+    fontSize: 17,
+    marginBottom: 25,
+    marginTop: 0,
+    color: "#333"
+  }
 });
 
 const nativeStyles = StyleSheet.create({
-    leadAsset: {
-        marginBottom: Platform.OS === "android" ? 6 : 10
-    },
-    articleTextElement: {
-        fontFamily: Platform.OS === "android" ? bodyFontFamilyAndroid : bodyFontFamilyWebIos,
-        fontStyle: "normal",
-        lineHeight: 26,
-        marginBottom: 20,
-        fontSize: Platform.OS === "android" ? 16 : 17,
-        color: "#333333"
-    }
+  leadAsset: {
+    marginBottom: Platform.OS === "android" ? 6 : 10
+  },
+  articleTextElement: {
+    fontFamily:
+      Platform.OS === "android" ? bodyFontFamilyAndroid : bodyFontFamilyWebIos,
+    fontStyle: "normal",
+    lineHeight: 26,
+    marginBottom: 20,
+    fontSize: Platform.OS === "android" ? 16 : 17,
+    color: "#333333"
+  }
 });
 
 if (Platform.OS !== "web") {
-    Object.assign(styles, globalStyle, nativeStyles);
+  Object.assign(styles, globalStyle, nativeStyles);
 } else {
-    Object.assign(styles, globalStyle, webStyles);
+  Object.assign(styles, globalStyle, webStyles);
 }
 
 export default styles;
