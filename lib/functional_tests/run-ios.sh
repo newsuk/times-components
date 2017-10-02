1b5f6c5acef47aa1d2009a93e99221484ed3674d
Rohan Janjua <rohan.janjua@gmail.com>
2017-10-02 17:22:27 +0100

    refactor: split out setup scripts, and more relevant names for scripts

--- lib/functional_tests/run-ios.sh
+++ lib/functional_tests/run-ios.sh
@@ -2,4 +2,4 @@
 npm run fetch-fonts
 npx rnstl --searchDir ./packages --pattern './*/*.fructose.ios.js' --outputFile ./.fructose/components.js
 npx compile-tests
-IOS=true jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/setup.js --forceExit --verbose
+jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/setup.ios.js --forceExit --verbose
--- lib/functional_tests/run-web.sh
+++ lib/functional_tests/run-web.sh
@@ -5,5 +5,5 @@ npx compile-tests # single file for all the tests
 npx webpack --config .fructose/vendor.webpack.config.js # build vendor package
 npx fructose-web --build-dir dist/public & # start the fructose web app
 FRUCTOSE_WEB_PID=$!
-WEB=true jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/setup.js --verbose --forceExit # run the tests
+jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/setup.web.js --verbose --forceExit # run the tests
 kill -9 $FRUCTOSE_WEB_PID # close the web app
