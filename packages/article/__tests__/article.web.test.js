95044e606f44df9776bcae561185959e702cb45b
killanaca <killanaca@gmail.com>
2017-10-03 09:40:52 +0100

    remove mock for ads

--- packages/article/__tests__/article.web.test.js
+++ packages/article/__tests__/article.web.test.js
@@ -2,11 +2,6 @@
 
 import shared from "./shared";
 
-// Due to https://github.com/facebook/react/issues/7371
-jest.mock("react-dom", () => ({
-  findDOMNode: () => ({})
-}));
-
 describe("Article test on web", () => {
   shared();
 });
