bb3e4881ff7bb91ce843b3608f1891a440754b9b
killanaca <killanaca@gmail.com>
2017-10-04 15:14:09 +0100

    update to react-native 0.49

--- android/app/src/main/java/com/storybooknative/MainApplication.java
+++ android/app/src/main/java/com/storybooknative/MainApplication.java
@@ -32,6 +32,11 @@ public class MainApplication extends Application implements ReactApplication {
   };
 
   @Override
+  protected String getJSMainModuleName() {
+    return "index";
+  }
+
+  @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
