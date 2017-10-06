043f091f63781de77070d20badbb69085de2dc39
Rohan Janjua <rohan.janjua@gmail.com>
2017-10-06 16:03:04 +0100

    fix: improve alignment of icon and text

--- packages/pagination/__tests__/test-icons-helper.js
+++ packages/pagination/__tests__/test-icons-helper.js
@@ -1,18 +1,15 @@
 /* eslint-env jest */
-import React from 'react';
-import {Text} from 'react-native';
+import React from "react";
 import { shallow } from "enzyme";
-
-import tests from "./test-helper";
-import {PreviousPageIcon, NextPageIcon} from "../pagination-icons";
+import { PreviousPageIcon, NextPageIcon } from "../pagination-icons";
 
 export default () => {
   describe("Previous page icon", () => {
-  it("renders out an svg and the passed in label", () => {
-    const icon = shallow(<PreviousPageIcon label="a label" />);
-    expect(icon).toMatchSnapshot();
+    it("renders out an svg and the passed in label", () => {
+      const icon = shallow(<PreviousPageIcon label="a label" />);
+      expect(icon).toMatchSnapshot();
+    });
   });
-});
 
   describe("Next page icon", () => {
     it("renders out an svg and the passed in label", () => {
@@ -20,4 +17,4 @@ export default () => {
       expect(icon).toMatchSnapshot();
     });
   });
-}
+};
