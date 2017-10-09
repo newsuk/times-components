1a3caba9403db2d36ef9e1fe1a853a67db4f0702
Craig Bilner <craig.bilner@gmail.com>
2017-10-09 21:39:33 +0200

    chore: refactor

--- packages/author-profile/author-profile-content.js
+++ packages/author-profile/author-profile-content.js
@@ -1,34 +1,58 @@
-import React from "react";
-import { ScrollView, View } from "react-native";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { ListView } from "react-native";
+import AuthorHead from "@times-components/author-head";
 import AuthorProfileItem from "./author-profile-item";
 import AuthorProfileItemSeparator from "./author-profile-item-separator";
 
-const AuthorProfile = props =>
-  <ScrollView testID="scroll-view">
-    {props.articles.list.map((article, key) => {
-      const { id, url } = article;
-      const separatorComponent = key > 0
-        ? <AuthorProfileItemSeparator />
-        : null;
+export default class AuthorProfileContent extends Component {
+  constructor(props) {
+    super();
 
-      return (
-        <View key={id}>
-          {separatorComponent}
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.state = {
+      dataSource: ds.cloneWithRows(props.articles)
+    };
+  }
+
+  render() {
+    const {
+      name,
+      biography,
+      uri,
+      jobTitle,
+      twitter,
+      onTwitterLinkPress,
+      onNext,
+      pageSize,
+      onArticlePress
+    } = this.props;
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={({ article }) =>
           <AuthorProfileItem
             {...article}
-            onPress={e => props.onArticlePress(e, { id, url })}
-          />
-        </View>
-      );
-    })}
-  </ScrollView>;
-
-AuthorProfile.propTypes = Object.assign({
-  onArticlePress: PropTypes.func.isRequired,
-  articles: PropTypes.shape({
-    list: PropTypes.arrayOf(PropTypes.shape(AuthorProfileItem.propTypes))
-  })
-});
-
-export default AuthorProfile;
+            onPress={e =>
+              onArticlePress(e, { id: article.id, url: article.url })}
+          />}
+        initialListSize={pageSize}
+        scrollRenderAheadDistance={2}
+        pageSize={pageSize}
+        renderHeader={() =>
+          <AuthorHead
+            name={name}
+            bio={biography}
+            uri={uri}
+            title={jobTitle}
+            twitter={twitter}
+            onTwitterLinkPress={onTwitterLinkPress}
+          />}
+        renderSeparator={() => <AuthorProfileItemSeparator />}
+        onEndReached={() => onNext()}
+      />
+    );
+  }
+}
