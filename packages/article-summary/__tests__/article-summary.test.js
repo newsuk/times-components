2402a2e6a672bd3a3f31bd4cc98ebcc12ecfad64
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-10-13 18:15:38 +0100

    fixed summary paragraphs and elipses

--- packages/article-summary/__tests__/article-summary.test.js
+++ packages/article-summary/__tests__/article-summary.test.js
@@ -6,12 +6,6 @@ import renderer from "react-test-renderer";
 import ArticleSummary from "../article-summary";
 import props from "../fixtures/article.json";
 
-it("renders an article-summary component", () => {
-  const tree = renderer.create(<ArticleSummary />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
 it("renders an article-summary component with content", () => {
   props.date = new Date("2017-07-01T14:32:00.000Z");
   const tree = renderer.create(<ArticleSummary {...props} />).toJSON();
