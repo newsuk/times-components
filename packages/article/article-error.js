6d9f090ea2be4fb39bfdf9d63032b3b329375c42
Craig Bilner <craig.bilner@news.co.uk>
2017-10-21 11:50:36 +0100

    chore: lint article

--- packages/article/article-error.js
+++ packages/article/article-error.js
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 const ArticleError = props => (
   <View>
-    <Text>An error ocurred</Text>
+    <Text>An error occurred</Text>
     <Text>{JSON.stringify(props, null, 2)}</Text>
   </View>
 );
