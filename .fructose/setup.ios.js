afbff2725decd86ccd5abd2b0ee7544797f7ea29
Rohan Janjua <rohan.janjua@gmail.com>
2017-10-23 15:36:52 +0100

    fix: make timeout for setup longer

--- .fructose/setup.ios.js
+++ .fructose/setup.ios.js
@@ -3,12 +3,13 @@ import fructose from "@times-components/fructose/setup";
 import detox from "detox";
 import config from "../package";
 
-beforeAll(async() => {
-    // increasing timeout to deal with long running detox commands
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    await fructose.hooks.mobile.setup();
-    await detox.init(config.detox);
-}, 180000);
+beforeAll(async () => {
+  // increasing timeout to deal with long running detox commands
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  await fructose.hooks.mobile.setup();
+  await detox.init(config.detox);
+  console.log("loaded Detox")
+}, 240000);
 
 afterAll(async() => {
   await detox.cleanup();
