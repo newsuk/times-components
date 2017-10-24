1b7b05f49e6bbb7626d0672780eb3dae1b554df2
Andy Trevorah <a.trevorah@gmail.com>
2017-10-24 14:53:56 +0100

    fix: simple brightcove activity with fullscreen icon hidden

--- packages/brightcove-video/android/src/main/java/uk/co/news/rntbrightcovevideo/BrightcovePlayerActivity.java
+++ packages/brightcove-video/android/src/main/java/uk/co/news/rntbrightcovevideo/BrightcovePlayerActivity.java
@@ -1,11 +1,16 @@
 package uk.co.news.rntbrightcovevideo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-public class BrightcovePlayerActivity extends AppCompatActivity {
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcovePlayer;
+
+public class BrightcovePlayerActivity extends BrightcovePlayer {
     private final String TAG = this.getClass().getSimpleName();
 
     private static final String PROP_ACCOUNT_ID = "accountId";
@@ -24,16 +29,25 @@ public class BrightcovePlayerActivity extends AppCompatActivity {
         final String videoId = extra.getString(PROP_VIDEO_ID);
 
         // Hide the status bar.
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
+        // When extending the BrightcovePlayer, we must assign the BrightcoveVideoView before
+        // entering the superclass. This allows for some stock video player lifecycle
+        // management.  Establish the video object and use its event emitter to get important
+        // notifications and to control logging.
+        setContentView(R.layout.brightcove_player_activity);
+        brightcoveVideoView = findViewById(R.id.brightcove_video_view);
         super.onCreate(savedInstanceState);
 
-        BrightcovePlayerView playerView = new BrightcovePlayerView(this);
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        Catalog catalog = new Catalog(eventEmitter, accountId, policyKey);
 
-        playerView.initVideo(videoId, accountId, policyKey, true, true);
-
-        setContentView(playerView);
+        catalog.findVideoByID(videoId, new VideoListener() {
+            @Override
+            public void onVideo(Video video) {
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
     }
 }
