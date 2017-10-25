ec8e445f014afd471f7db2ec5bcc7493d1dbc1bd
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-10-25 12:15:07 +0100

    lint

--- packages/article-summary/__tests__/article-summary-helper.js
+++ packages/article-summary/__tests__/article-summary-helper.js
@@ -10,13 +10,17 @@ import blankFixture from "../fixtures/blank.json";
 export default ArticleSummary => {
   it("renders an article-summary component with content", () => {
     articleFixture.date = new Date("2017-07-01T14:32:00.000Z");
-    const tree = renderer.create(<ArticleSummary {...articleFixture} />).toJSON();
+    const tree = renderer
+      .create(<ArticleSummary {...articleFixture} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders an article-summary component with content including line breaks", () => {
     reviewFixture.date = new Date("2017-07-01T14:32:00.000Z");
-    const tree = renderer.create(<ArticleSummary {...reviewFixture} />).toJSON();
+    const tree = renderer
+      .create(<ArticleSummary {...reviewFixture} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
