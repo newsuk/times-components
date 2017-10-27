9a233cc41fb5ad4ea1d78e6ed7428672ad968773
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-10-27 11:27:06 +0100

    chore: move render logic to renderer

--- packages/article-summary/article-summary-renderer.js
+++ packages/article-summary/article-summary-renderer.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Text } from "react-native";
 
 export default {
-  paragraph(key, attributes, renderedChildren) {
-    return <Text key={key}>{renderedChildren} </Text>;
+  paragraph(key, attributes, renderedChildren, indx) {
+    const content = renderedChildren.length
+      ? `${indx === 0 ? "" : " "}${renderedChildren}`
+      : "";
+    return <Text key={key}>{content}</Text>;
   },
   text(key, { value }) {
     return value.trim();
--- packages/markup/render-tree-without-defaults.js
+++ packages/markup/render-tree-without-defaults.js
@@ -1,12 +1,12 @@
-const renderTree = (tree, renderers, key) => {
+const renderTree = (tree, renderers, key, indx = 0) => {
   const { name, attributes, children } = tree;
 
   const renderer = renderers[name];
   if (!renderer) return null;
 
   const renderedChildren = children.map((child, index) =>
-    renderTree(child, renderers, `${key}.${index}`)
+    renderTree(child, renderers, `${key}.${index}`, index)
   );
-  return renderer(key, attributes, renderedChildren);
+  return renderer(key, attributes, renderedChildren, indx);
 };
 export default renderTree;
