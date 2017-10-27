7cee0bb431660714838d840ced29dde003acf224
Filipe Pinheiro <fampinheiro@users.noreply.github.com>
2017-10-27 12:13:24 +0100

    feat: add gradient package (#333)

---
+++ packages/gradient/gradient.web.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { View } from "react-native";
+import StylePropTypes from "react-style-proptype";
+
+const { supportingArrays } = StylePropTypes;
+const Gradient = ({ degrees, children, style }) => (
+  <View
+    style={[
+      {
+        backgroundImage: `linear-gradient(${degrees}deg, #f9f9f9 0%, #ededed 100%)`
+      },
+      style
+    ]}
+  >
+    {children}
+  </View>
+);
+
+Gradient.defaultProps = {
+  degrees: 265,
+  children: null,
+  style: null
+};
+
+Gradient.propTypes = {
+  degrees: PropTypes.number,
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ]),
+  style: supportingArrays
+};
+
+export default Gradient;
