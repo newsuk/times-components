7161ff1c57b0e44fd44e9dfb2e1895484dba5695
Filipe Pinheiro <me@fampinheiro.com>
2017-11-03 14:31:30 +0000

    fix: use react-test-renderer

--- packages/card/__tests__/card-loading.test.js
+++ packages/card/__tests__/card-loading.test.js
@@ -1,15 +1,12 @@
 /* eslint-env jest */
 
 import "react-native";
-import Enzyme, { shallow } from "enzyme";
-import React16Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 import React from "react";
 import CardLoading from "../card-loading";
 
-Enzyme.configure({ adapter: new React16Adapter() });
-
 it("renders loading card state", () => {
-  const component = shallow(<CardLoading />);
+  const tree = renderer.create(<CardLoading />);
 
-  expect(component).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
