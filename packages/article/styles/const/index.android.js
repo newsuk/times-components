75dc752aeeb6e6ccf8f7aeaaeef14da10541e900
James Miller <james@james-miller.co.uk>
2017-11-06 16:54:45 +0000

    TAP-103: Formatted files

--- packages/article/styles/const/index.android.js
+++ packages/article/styles/const/index.android.js
@@ -1,7 +1,7 @@
 import sharedConst from "./shared";
 
 const androidConst = Object.assign({}, sharedConst, {
-    fontFamilyBody: "TimesDigitalW04-Regular",
-})
+  fontFamilyBody: "TimesDigitalW04-Regular"
+});
 
 export default androidConst;
--- packages/article/styles/const/shared.js
+++ packages/article/styles/const/shared.js
@@ -1,7 +1,7 @@
 export default {
-    fontFamilyBody: "TimesDigitalW04",
-    fontFamilyStandFirst: "TimesModern-Regular",
-    colourText: "#333333",
-    colourHeadline: "#000000",
-    colourBorder: "#D0CECE",
-}
+  fontFamilyBody: "TimesDigitalW04",
+  fontFamilyStandFirst: "TimesModern-Regular",
+  colourText: "#333333",
+  colourHeadline: "#000000",
+  colourBorder: "#D0CECE"
+};
