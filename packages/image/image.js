88c2e09517902831002c592faf7f0642afa33ebf
Ritchie Comley <richard.comley@news.co.uk>
2017-11-08 09:22:34 +0000

    chore: make image component compatible with RN 0.50

--- packages/image/image.js
+++ packages/image/image.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-import { Image } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import Placeholder from "./placeholder";
 import imagePropTypes from "./image-prop-types";
 
 const addMissingProtocol = uri => (uri.startsWith("//") ? `https:${uri}` : uri);
+
+const styles = StyleSheet.create({
+  placeholder: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+  }
+});
 
 class TimesImage extends Component {
   constructor(props) {
@@ -24,15 +34,17 @@ class TimesImage extends Component {
     const { isLoaded } = this.state;
     // web handles missing protocols just fine, native doesnt. This evens out support.
     const uri = addMissingProtocol(dirtyUri);
+
     return (
-      <Image
-        style={style}
-        source={{ uri }}
-        aspectRatio={aspectRatio}
-        onLoad={this.handleLoad}
-      >
-        {isLoaded ? null : <Placeholder style={style} />}
-      </Image>
+      <View>
+        <Image
+          style={style}
+          source={{ uri }}
+          aspectRatio={aspectRatio}
+          onLoad={this.handleLoad}
+        />
+        {isLoaded ? null : <Placeholder style={styles.placeholder} />}
+      </View>
     );
   }
 }
