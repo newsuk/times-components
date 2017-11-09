04d157197ab24376d62c1367d664130b3ee9f2b0
Kouros Aliabadi <kaliabadi@qaworks.com>
2017-11-09 15:53:45 +0000

    fixing some android tests

--- fructose/components.js
+++ fructose/components.js
@@ -7,6 +7,8 @@
 function loadStories() {
   
   require('../packages/article/article.fructose.android.js');
+  require('../packages/author-profile/author-profile.fructose.android.js');
+  require('../packages/brightcove-video/brightcove-player.fructose.android.js');
   
 }
 
--- lib/functional_tests/run-android.sh
+++ lib/functional_tests/run-android.sh
@@ -12,7 +12,6 @@ react-native run-android --variant=release --no-packager
 npm run fetch-fonts
 npx rnstl --searchDir ./packages --pattern './*/*.fructose.android.js' --outputFile ./fructose/components.js
 npx compile-tests
-kill -9 $(lsof -ti :8081)
 jest fructose/components.test.js --setupTestFrameworkScriptFile ./fructose/setup.android.js --forceExit --verbose
 kill -9 $EMU_PID
 kill -9 $APPIUM_PID
