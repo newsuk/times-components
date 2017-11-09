9af71a9bb761caa771ad387c82b373075488b6fb
Kouros Aliabadi <kaliabadi@qaworks.com>
2017-11-09 19:28:53 +0000

    adding script to run android tests on sauce labs

--- lib/functional_tests/run-android.sh
+++ lib/functional_tests/run-android.sh
@@ -2,7 +2,7 @@
 npx appium &
 APPIUM_PID=$!
 LOCAL=true npx fructose-tunnel
-# emulator @NEXUS_5X_API_22 -no-boot-anim &
+emulator @NEXUS_5X_API_22 -no-boot-anim &
 EMU_PID=$!
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
