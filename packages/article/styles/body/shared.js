3f762dad53653915f320c4468323bd730ea512af
James Miller <james@james-miller.co.uk>
2017-11-09 11:59:23 +0000

    TAP-103: Attempt to put things back

--- packages/article/styles/body/shared.js
+++ packages/article/styles/body/shared.js
@@ -2,17 +2,17 @@ const sharedStyles = {
   leadAsset: {
     marginBottom: 10
   },
-  articleTextElement: {
-    fontFamily: "TimesDigitalW04",
-    lineHeight: 26,
-    fontSize: 17,
-    marginBottom: 25,
-    color: "#333333"
-  },
-  articleMainContentRow: { // article.js
-    paddingLeft: 10,
-    paddingRight: 10
-  }
+  // articleTextElement: {
+  //   fontFamily: "TimesDigitalW04",
+  //   lineHeight: 26,
+  //   fontSize: 17,
+  //   marginBottom: 25,
+  //   color: "#333333"
+  // },
+  // articleMainContentRow: { // article.js
+  //   paddingLeft: 10,
+  //   paddingRight: 10
+  // }
 };
 
 export default sharedStyles;
--- packages/article/styles/meta/shared.js
+++ packages/article/styles/meta/shared.js
@@ -1,14 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const sharedStyles = {
-  articleMeta: {
-    marginBottom: 20,
-    borderBottomColor: "#d0cece",
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  articleMiddleContainer: { // meta
-    paddingTop: 9
-  },
+  // articleMeta: {
+  //   marginBottom: 20,
+  //   borderBottomColor: "#d0cece",
+  //   borderBottomWidth: StyleSheet.hairlineWidth
+  // },
+  // articleMiddleContainer: { // meta
+  //   paddingTop: 9
+  // },
   articleMetaElement: {
     borderTopColor: "#d0cece",
     borderTopWidth: StyleSheet.hairlineWidth
