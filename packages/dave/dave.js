2175d7cd47c1852d8ed58600fad2199d76fcd380
James Miller <james@james-miller.co.uk>
2017-11-10 10:43:02 +0000

    TAP-103: Fixed tests and a few additions.

--- packages/dave/dave.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-import styles from "./styles";
-
-export default function Dave() {
-  return <Text style={styles.daveBody}>Dave</Text>;
-}
