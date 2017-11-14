2c432d2f3cce3063fcbbda6a4e39dd8ea936e8b0
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-11-14 14:52:44 +0000

    feat: replace ListView with ScrollView

--- packages/article/article-content.web.js
+++ packages/article/article-content.web.js
@@ -1,41 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ListView } from "react-native";
-
-const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+import { ScrollView, View } from "react-native";
 
 const ArticleContent = ({
   data,
-  renderRow,
-  initialListSize,
-  scrollRenderAheadDistance,
-  pageSize
+  renderRow
 }) => (
-  <ListView
-    testID="listView"
-    dataSource={ds.cloneWithRows(data)}
-    renderRow={renderRow}
-    initialListSize={initialListSize}
-    scrollRenderAheadDistance={scrollRenderAheadDistance}
-    pageSize={pageSize}
-    enableEmptySections
-  />
+  <ScrollView>
+    { data.map((item) => {
+      return renderRow(item)
+    }) }
+  </ScrollView>
 );
 
 ArticleContent.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
-  renderRow: PropTypes.func,
-  initialListSize: PropTypes.number,
-  scrollRenderAheadDistance: PropTypes.number,
-  pageSize: PropTypes.number
+  renderRow: PropTypes.func
 };
 
 ArticleContent.defaultProps = {
   data: [],
-  renderRow: null,
-  initialListSize: 0,
-  scrollRenderAheadDistance: 0,
-  pageSize: 0
+  renderRow: null
 };
 
 export default ArticleContent;
