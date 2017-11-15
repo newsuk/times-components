1092fffe315866ccbd8780722bf8dd4da1d84b16
killanaca <killanaca@gmail.com>
2017-11-15 13:44:00 +0000

    run lint

--- storybook/decorators/white-bg-color.js
+++ storybook/decorators/white-bg-color.js
@@ -4,12 +4,19 @@ import React from "react";
 import { View } from "react-native";
 
 const WhiteBgColorDecorator = storyFn => (
-  <View style={{ backgroundColor: "white", flex: 1,
-  position: "absolute",
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0 }}>{storyFn()}</View>
+  <View
+    style={{
+      backgroundColor: "white",
+      flex: 1,
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    }}
+  >
+    {storyFn()}
+  </View>
 );
 
 export default WhiteBgColorDecorator;
