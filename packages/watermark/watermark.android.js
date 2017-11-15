bb41f6bc60c9ba5b7731efb8154164b97da8bfd2
killanaca <killanaca@gmail.com>
2017-11-15 13:45:24 +0000

    run lint

--- packages/watermark/watermark.android.js
+++ packages/watermark/watermark.android.js
@@ -2,11 +2,21 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-const Watermark = ({ width, height }) => <View style={{ height, width, borderColor: "#646464", borderWidth: StyleSheet.hairlineWidth, backgroundColor: "#e8e8e8" }}/>;
+const Watermark = ({ width, height }) => (
+  <View
+    style={{
+      height,
+      width,
+      borderColor: "#646464",
+      borderWidth: StyleSheet.hairlineWidth,
+      backgroundColor: "#e8e8e8"
+    }}
+  />
+);
 
 Watermark.propTypes = {
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired
-  };
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+};
 
-  export default Watermark;
+export default Watermark;
