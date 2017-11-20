2b30b08e1da5da5df302d397012a100468a33705
Craig Bilner <craig.bilner@gmail.com>
2017-11-20 16:00:41 +0000

    fix: remove JS (#403)

--- packages/image/image.web.js
+++ packages/image/image.web.js
@@ -1,62 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { View } from "react-native";
 import Placeholder from "./placeholder";
 import { defaultProps, propTypes } from "./image-prop-types";
 
-class TimesImage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoaded: false
-    };
-    this.handleLoad = this.handleLoad.bind(this);
-  }
-
-  handleLoad() {
-    this.setState({ isLoaded: true });
-  }
-
-  render() {
-    const { uri, aspectRatio, style } = this.props;
-    const { isLoaded } = this.state;
-
-    const aspectRatioStyle = {
+const TimesImage = ({ uri, aspectRatio, style }) => {
+  const styles = {
+    wrapper: {
       height: 0,
       overflow: "hidden",
       paddingBottom: `${100 / aspectRatio}%`
-    };
-
-    const placeholderStyle = {
-      flex: 1,
+    },
+    placeholder: {
       position: "absolute",
       top: 0,
       bottom: 0,
       left: 0,
-      right: 0
-    };
+      right: 0,
+      zIndex: 0
+    },
+    img: { display: "block", width: "100%", zIndex: 1, position: "absolute" }
+  };
 
-    const placeholderComponent = isLoaded ? null : (
-      <Placeholder style={placeholderStyle} />
-    );
+  const boundedImg = (
+    <div style={styles.wrapper}>
+      <img src={uri} style={styles.img} alt="" />
+      <Placeholder style={styles.placeholder} />
+    </div>
+  );
 
-    const boundedImg = (
-      <div style={aspectRatioStyle}>
-        <img
-          src={uri}
-          style={{ display: "block", width: "100%" }}
-          onLoad={this.handleLoad}
-          alt=""
-        />
-        {placeholderComponent}
-      </div>
-    );
-
-    // divs cannot be styled with the output of Stylesheet.create()
-    // only react native Views accept those ids
-    return style ? <View style={style}>{boundedImg}</View> : boundedImg;
-  }
-}
+  // divs cannot be styled with the output of Stylesheet.create()
+  // only react native Views accept those ids
+  return style ? <View style={style}>{boundedImg}</View> : boundedImg;
+};
 
 TimesImage.defaultProps = defaultProps;
 
