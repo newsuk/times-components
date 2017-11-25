bdc3905ff417631c8e08f1d10fb4aa3a2a950969
James Miller <james@james-miller.co.uk>
2017-11-25 21:29:42 +0000

    REPLAT-604: Fix for native header and meta

--- packages/article/article-header.js
+++ packages/article/article-header.js
@@ -49,24 +49,28 @@ const renderLabel = label => {
   );
 };
 
-const ArticleHeader = ({ label, headline, standfirst, flags }) => [
-  renderLabel(label),
-  <ArticleHeadline text={headline} style={styles.articleHeadLineText} />,
-  renderStandfirst(standfirst),
-  renderFlags(flags)
-];
+const ArticleHeader = ({ label, headline, standfirst, flags, style }) => (
+  <View style={[...style]}>
+    {renderLabel(label)}
+    <ArticleHeadline text={headline} style={styles.articleHeadLineText} />
+    {renderStandfirst(standfirst)}
+    {renderFlags(flags)}
+  </View>
+);
 
 ArticleHeader.propTypes = {
   headline: PropTypes.string.isRequired,
   label: PropTypes.string,
   standfirst: PropTypes.string,
-  flags: PropTypes.arrayOf(PropTypes.string)
+  flags: PropTypes.arrayOf(PropTypes.string),
+  style: PropTypes.arrayOf(PropTypes.number),
 };
 
 ArticleHeader.defaultProps = {
   label: null,
   standfirst: null,
-  flags: []
+  flags: [],
+  style: []
 };
 
 export default ArticleHeader;
