37d99e2b2217e268defe7d1f9d54d8bba6c4fe05
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-30 14:30:12 +0000

    test: dextrose android setup updates

--- lib/functional_tests/run-android-dextrose.sh
+++ lib/functional_tests/run-android-dextrose.sh
@@ -2,7 +2,7 @@
 npx appium &
 APPIUM_PID=$!
 LOCAL=true npx fructose-tunnel
-/Users/kourosaliabadi/Library/Android/sdk/tools/emulator @NEXUS_5X_API_22 -no-boot-anim &
+/Users/kourosaliabadi/Library/Android/sdk/tools/emulator @PIXEL_XL_API_26 -no-boot-anim &
 EMU_PID=$!
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
@@ -11,7 +11,9 @@ adb reverse tcp:4723 tcp:4723
 npm run fetch-fonts
 npx compile-tests -d fructose
 npx rnstl --searchDir ./packages --pattern './*/*.stories.js' --outputFile ./fructose/components.js
-react-native run-android --variant=release --no-packager
-LOGLEVEL=verbose npx dextrose --config ./dextrose/dextrose.android.js
+react-native run-android --variant=debug --no-packager
+react-native start --root fructose &
+sleep 8
+LOGLEVEL=verbose npx dextrose --config ./dextrose/dextrose.android.js --snapshotWait 2000
 kill -9 $EMU_PID
 kill -9 $APPIUM_PID
