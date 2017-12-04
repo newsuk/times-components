80a0f735e08e880c3ca6f218240ac30162f98b59
toboid <toboid@users.noreply.github.com>
2017-12-04 17:48:04 +0000

    feat: make bio responsive

---
+++ packages/author-head/responsive-styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const withResponsiveStyles = (Component, styles = {}) => styled(Component)`
+  @media (min-width: 520px) {
+    ${styles.small};
+  }
+  @media (min-width: 768px) {
+    ${styles.medium};
+  }
+  @media (min-width: 1024px) {
+    ${styles.wide};
+  }
+  @media (min-width: 1320px) {
+    ${styles.huge};
+  }
+`;
+
+export default withResponsiveStyles;
