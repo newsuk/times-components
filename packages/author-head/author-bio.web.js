cd3a9d102e62916d5737cd8743eddcd272973d88
toboid <toboid@users.noreply.github.com>
2017-12-05 12:27:52 +0000

    chore: use responsive styles component in author-head

--- packages/author-head/author-bio.web.js
+++ packages/author-head/author-bio.web.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View } from "react-native";
+import withResponsiveStyles from "@times-components/responsive-styles";
 import Bio from "./author-bio.base";
-import withResponsiveStyles from "./responsive-styles";
 
 const BioContainer = withResponsiveStyles(View, {
   base: () => `
     max-width: 88%;
     padding-bottom: 32px;
   `,
-  medium: () => "max-width: 680px",
-  huge: () => "max-width: 760px"
+  mediumUp: () => "max-width: 680px",
+  hugeUp: () => "max-width: 760px"
 });
 
 export default props => (
