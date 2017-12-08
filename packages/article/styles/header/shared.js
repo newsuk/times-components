28da9069696cc174543cccfd20c3cbb9d1aca6ca
James Miller <james@james-miller.co.uk>
2017-12-08 16:26:39 +0000

    Fix the broken tests and formatting

--- packages/article/styles/header/shared.js
+++ packages/article/styles/header/shared.js
@@ -4,7 +4,7 @@ const sharedStyles = {
     lineHeight: 32,
     color: "#1d1d1b",
     marginBottom: 7,
-    fontFamily: "TimesModern-Bold",
+    fontFamily: "TimesModern-Bold"
   },
   standFirst: {
     fontSize: 20,
