acadef3b8efb7c72ee58ea8721c298db03c26e8a
Craig Bilner <craig.bilner@gmail.com>
2017-12-11 16:41:53 +0000

    feat: refactor storybook package (#453)
    * chore: refactor storybook package
    chore: fix versions
    
    * chore: move storybook reporter to seperate import
    
    allows usage in test stories without causing storybook reporter to be bundled with production code

--- packages/tracking/tracking.js
+++ packages/tracking/tracking.js
@@ -1,4 +1,3 @@
 export { default as withTrackingContext } from "./tracking-context";
 export { default as withTrackEvents } from "./track-events";
 export { default as withTrackScrollDepth } from "./track-scroll-depth";
-export { default as tealiumReporter } from "./tealium/tealium-reporter";
