4da4a9cd1b8b4d4a22b69b39f6614d2b1cfa35d3
Daniele Conti <daniele.conti@novoda.com>
2017-12-18 16:19:10 +0000

    Reduce horizontal margin of bio on mobile viewport

--- packages/author-head/author-bio.js
+++ packages/author-head/author-bio.js
@@ -3,7 +3,7 @@ import { StyleSheet, View } from "react-native";
 import Bio from "./author-bio.base";
 
 const styles = StyleSheet.create({
-  bio: { maxWidth: "88%", paddingBottom: 32 }
+  bio: { paddingLeft: 10, paddingRight: 10, paddingBottom: 32 }
 });
 
 export default props => (
