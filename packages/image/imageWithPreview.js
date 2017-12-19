4e4dff876599640313f11d3fff7b674bdc7e549e
Daniele Conti <daniele.conti@novoda.com>
2017-12-19 15:19:42 +0000

    Image with preview feature

---
+++ packages/image/imageWithPreview.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import { ImageBackground, View } from "react-native"
+import { style } from "./style"
+import { defaultProps, propTypes } from "./image-prop-types";
+import Placeholder from "./placeholder";
+import { addMissingProtocol } from "./addMissingProtocol";
+
+export class ImageWithPreview extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoaded: false,
+            isHighResolutionLoaded: false
+        };
+    }
+
+    handleLoad = () => {
+        this.setState({
+            isLoaded: true,
+            isHighResolutionLoaded: true
+        });
+    }
+
+    handlePreviewLoad = () => {
+        this.setState({ isLoaded: true })
+    }
+
+    render() {
+        const { uri: dirtyUri, style, aspectRatio } = this.props;
+        const { isLoaded } = this.state;
+        // web handles missing protocols just fine, native doesn't. This evens out support.
+        const uri = addMissingProtocol(dirtyUri);
+        const previewUri = this.state.isHighResolutionLoaded ? null : uri + "&preview=true"; // TODO: Implement a separate uri for preview
+
+        const props = {
+            style: styles.imageBackground,
+            onLoad: this.handleLoad
+        };
+
+        if (uri) {
+            props.source = { uri };
+        }
+
+        const previewProps = { ...props, source: { uri: previewUri }, onLoad: this.handlePreviewLoad }
+
+        return (
+            <View aspectRatio={aspectRatio} style={style}>
+                <ImageBackground {...previewProps}>
+                    <ImageBackground {...props}>
+                        {isLoaded ? null : <Placeholder />}
+                    </ImageBackground>
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+
+ImageWithPreview.defaultProps = defaultProps;
+ImageWithPreview.propTypes = propTypes;
