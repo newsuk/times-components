6545006cca3adb44b6a1fcc7f0067d14780d051d
Daniele Conti <daniele.conti@novoda.com>
2017-12-19 15:18:10 +0000

    Component to activate preview image feature

---
+++ packages/image/activatePreviewImage.js
@@ -0,0 +1,23 @@
+class ActivatePreviewImage extends Component {
+    getChildContext() {
+        return {
+            previewImageActivated: true
+        }
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+export const activatePreviewImageContextTypes = {
+    previewImageActivated: PropTypes.bool
+}
+
+ActivatePreviewImage.childContextTypes = activatePreviewImageContextTypes
+
+export const activatePreviewImage = (WrappedComponent) => (props) => (
+    <ActivatePreviewImage>
+        <WrappedComponent {...props} />
+    </ActivatePreviewImage>
+)
