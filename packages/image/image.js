450ee326eeb8562cb0098fb8292d2a9ab354e368
Daniele Conti <daniele.conti@novoda.com>
2017-12-19 15:20:28 +0000

    TimesImage now switches between preview/non preview version depending on flag

--- packages/image/image.js
+++ packages/image/image.js
@@ -1,55 +1,20 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StyleSheet } from "react-native";
-import Placeholder from "./placeholder";
 import { defaultProps, propTypes } from "./image-prop-types";
-
-const styles = StyleSheet.create({
-  imageBackground: { width: "100%", height: "100%" }
-});
-
-const addMissingProtocol = uri => (uri.startsWith("//") ? `https:${uri}` : uri);
+import PropTypes from "prop-types";
+import { ImageWithPreview } from "./imageWithPreview";
+import { ImageWithoutPreview } from "./imageWithoutPreview";
+import { activatePreviewImageContextTypes } from "./activatePreviewImage";
 
 class TimesImage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoaded: false
-    };
-    this.handleLoad = this.handleLoad.bind(this);
-  }
-
-  handleLoad() {
-    this.setState({ isLoaded: true });
-  }
-
   render() {
-    const { uri: dirtyUri, style, aspectRatio } = this.props;
-    const { isLoaded } = this.state;
-    // web handles missing protocols just fine, native doesn't. This evens out support.
-    const uri = addMissingProtocol(dirtyUri);
-
-    const props = {
-      style: styles.imageBackground,
-      onLoad: this.handleLoad
-    };
-
-    if (uri) {
-      props.source = { uri };
-    }
-
-    return (
-      <View aspectRatio={aspectRatio} style={style}>
-        <ImageBackground {...props}>
-          {isLoaded ? null : <Placeholder />}
-        </ImageBackground>
-      </View>
-    );
+    return this.context.previewImageActivated
+      ? <ImageWithPreview {...this.props} />
+      : <ImageWithoutPreview {...this.props} />
   }
 }
 
+TimesImage.contextTypes = activatePreviewImageContextTypes
 TimesImage.defaultProps = defaultProps;
-
 TimesImage.propTypes = propTypes;
 
 export default TimesImage;
