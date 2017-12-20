9e91391703ceb8eb51b806525d592fdd8454a9af
Daniele Conti <daniele.conti@novoda.com>
2017-12-20 12:06:56 +0000

    Add snapshot tests for Image

---
+++ packages/image/__tests__/image.native.test.js
@@ -0,0 +1,26 @@
+/* eslint-env jest */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Image from "../../image/image";
+import { activatePreviewImage } from "../activatePreviewImage";
+
+export default () => {
+  it("renders ImageWithoutPreview without a context", () => {
+    const tree = renderer
+      .create(<Image uri="http://example.com/image.jpg" aspectRatio={3 / 2} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders ImageWithPreview if feature is activated", () => {
+    const ImageWithContext = activatePreviewImage(Image);
+    const tree = renderer
+      .create(
+        <ImageWithContext uri="http://example.com/image.jpg" aspectRatio={3 / 2}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+};
