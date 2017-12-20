441e599f7a72e75cd39141ae91856d15150e2001
Daniele Conti <daniele.conti@novoda.com>
2017-12-20 15:26:06 +0000

    Ignore coverage for web for image with and without preview
    Those are native specific classes and won't be used in web.

--- packages/image/__tests__/web/jest.config.js
+++ packages/image/__tests__/web/jest.config.js
@@ -4,6 +4,8 @@ module.exports = Object.assign(jestConfigurator("image", "web"), {
   collectCoverageFrom: [
     "**/packages/image/**/*.js",
     "!**/packages/image/**/image.js",
+    "!**/packages/image/**/image-with-preview.js",
+    "!**/packages/image/**/image-without-preview.js",
     "!**/packages/image/**/*stories*",
     "!**/packages/image/__tests__/**",
     "!**/packages/image/coverage/**"
