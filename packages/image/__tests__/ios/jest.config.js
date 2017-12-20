1d227a88475430b6e7b2ba865c7adef5e79632b9
tuncaulubilge <tunca.ulubilge@gmail.com>
2017-12-20 15:15:25 +0000

    Update coverage config to exclude stories in image

--- packages/image/__tests__/ios/jest.config.js
+++ packages/image/__tests__/ios/jest.config.js
@@ -4,7 +4,7 @@ module.exports = Object.assign(jestConfigurator("image", "ios"), {
   collectCoverageFrom: [
     "**/packages/image/**/*.js",
     "!**/packages/image/**/*.web.js",
-    "!**/packages/image/**/*stories.js",
+    "!**/packages/image/**/*stories*",
     "!**/packages/image/__tests__/**",
     "!**/packages/image/coverage/**"
   ]
