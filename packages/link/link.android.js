7a0c59133a018b829c47470763e66afe2b58d3ad
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-01-02 14:59:33 +0000

    Check can use native foreground

--- packages/link/link.android.js
+++ packages/link/link.android.js
@@ -3,13 +3,22 @@ import { View, TouchableNativeFeedback } from "react-native";
 import PropTypes from "prop-types";
 
 const Link = ({ onPress, children }) => (
-  <TouchableNativeFeedback onPress={onPress} useForeground delayPressIn={0}>
+  <TouchableNativeFeedback
+    onPress={onPress}
+    useForeground={TouchableNativeFeedback.canUseNativeForeground()}
+    delayPressIn={0}
+  >
     <View pointerEvents="box-only">{children}</View>
   </TouchableNativeFeedback>
 );
 
 Link.propTypes = {
-  onPress: PropTypes.func.isRequired
+  onPress: PropTypes.func.isRequired,
+  children: PropTypes.node
+};
+
+Link.defaultProps = {
+  children: null
 };
 
 export default Link;
