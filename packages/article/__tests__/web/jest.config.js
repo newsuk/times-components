0c3b5a30e5b03aa5c72040032a4de4b06275ad13
James Miller <james@james-miller.co.uk>
2018-01-03 10:46:36 +0000

    Fix: Coverage issue

--- packages/article/__tests__/web/jest.config.js
+++ packages/article/__tests__/web/jest.config.js
@@ -3,7 +3,6 @@ const jestConfigurator = require("@times-components/jest-configurator");
 module.exports = Object.assign(jestConfigurator("article", "web"), {
   collectCoverageFrom: [
     "**/packages/article/*.js",
-    "**/packages/article/styles/**/*.js",
     "!**/packages/article/article.js",
     "!**/packages/article/article-content.js",
     "!**/packages/article/*stories*",
