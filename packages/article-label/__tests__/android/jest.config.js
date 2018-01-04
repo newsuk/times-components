cd46cea268473c84c8da638e90bf29a2ae77ed73
James Miller <james@james-miller.co.uk>
2018-01-04 14:37:05 +0000

    Improve the coverage on Article Label

--- packages/article-label/__tests__/android/jest.config.js
+++ packages/article-label/__tests__/android/jest.config.js
@@ -1,5 +1,9 @@
 const jestConfigurator = require("@times-components/jest-configurator");
 
 module.exports = Object.assign(jestConfigurator("article-label", "android"), {
-  collectCoverageFrom: ["**/packages/article-label/article-label.js"]
+  collectCoverageFrom: [
+    "**/packages/article-label/article-label.js",
+    "**/packages/article-label/style/shared.js",
+    "**/packages/article-label/**/*.android.js"
+  ]
 });
