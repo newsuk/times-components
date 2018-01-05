9096f730f9ca0233759c4eeaa9da0aa8d424d663
killanaca <killanaca@gmail.com>
2018-01-05 09:10:45 +0100

    feat: gmt support
    tests: update tests

--- packages/date-publication/date-publication.js
+++ packages/date-publication/date-publication.js
@@ -1,13 +1,34 @@
 import format from "date-fns/format";
+import React from "react";
 import PropTypes from "prop-types";
 
 const publications = {
   SUNDAYTIMES: "The Sunday Times",
   TIMES: "The Times"
 };
-
-const DatePublication = ({ date, publication }) =>
-  `${format(date, "dddd MMMM DD YYYY")}, ${publications[publication]}`;
+class DatePublication extends React.Component {
+  constructor(props) {
+    super(props);
+    const offset = new Date().getTimezoneOffset();
+    this.state = {
+      gmt: offset !== 0 ? " GMT," : ","
+    };
+  }
+  render() {
+    const { date, publication } = this.props;
+    // use the date as an UTC/GMT date (due to date parse behaviour)
+    const UTCDate = new Date(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate(),
+      date.getUTCHours(),
+      date.getUTCMinutes()
+    );
+    return `${format(UTCDate, "dddd MMMM DD YYYY, hh:mma")}${this.state.gmt}${
+      publications[publication]
+    }`;
+  }
+}
 
 DatePublication.propTypes = {
   date: PropTypes.instanceOf(Date).isRequired,
