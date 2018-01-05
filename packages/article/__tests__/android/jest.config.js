5088bebeb9fd1696dca49b06095298ff2fe3e611
James Miller <james@james-miller.co.uk>
2018-01-05 11:23:07 +0000

    Bring article shared.js styles into coverage for Android

--- packages/article/__tests__/android/jest.config.js
+++ packages/article/__tests__/android/jest.config.js
@@ -4,7 +4,7 @@ module.exports = Object.assign(jestConfigurator("article", "android"), {
   collectCoverageFrom: [
     "**/packages/article/*.js",
     "**/packages/article/**/*.android.js",
-    "**/packages/article/styles/shared.js",
+    "**/packages/article/styles/**/shared.js",
     "!**/packages/article/*.web.js",
     "!**/packages/article/*stories*",
     "!**/packages/article/__tests__/**",
