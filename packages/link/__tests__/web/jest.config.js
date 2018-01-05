2873ed35dfdc21987275ec7710f8e075d18cd781
Craig Bilner <craig.bilner@news.co.uk>
2018-01-05 13:15:15 +0000

    chore: add coverage

--- packages/link/__tests__/web/jest.config.js
+++ packages/link/__tests__/web/jest.config.js
@@ -3,6 +3,7 @@ const jestConfigurator = require("@times-components/jest-configurator");
 module.exports = Object.assign(jestConfigurator("link", "web"), {
   collectCoverageFrom: [
     "**/packages/link/*.web.js",
+    "**/packages/link/text-link.js",
     "!**/packages/link/*.stories.js"
   ]
 });
