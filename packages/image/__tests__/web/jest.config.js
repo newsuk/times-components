c50e0c893a3e413a6f2a4311abf6a94cc0118aaf
Craig Bilner <craig.bilner@news.co.uk>
2018-01-08 16:40:15 +0000

    chore: fix imports

--- packages/image/__tests__/web/jest.config.js
+++ packages/image/__tests__/web/jest.config.js
@@ -1,4 +1,4 @@
-const jestConfigurator = require("@times-components/jest-configurator");
+const jestConfigurator = require("@times-components/jest-configurator").default;
 
 module.exports = jestConfigurator("image", "web", [
   "add-missing-protocol.js",
