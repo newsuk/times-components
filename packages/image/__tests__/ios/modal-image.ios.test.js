9bdb1d641c2fa180b4617e9928b21c4071d5cca7
Daniele Conti <daniele.conti@novoda.com>
2018-01-08 11:30:05 +0000

    Test native modal image

---
+++ packages/image/__tests__/ios/modal-image.ios.test.js
@@ -0,0 +1,16 @@
+/* eslint-env jest */
+
+import tests from "../modal-image.native.test";
+
+jest.mock("react-native", () => {
+  const reactNative = require.requireActual("react-native");
+  reactNative.Platform.OS = "ios";
+  jest
+    .spyOn(reactNative.Platform, "select")
+    .mockImplementation(obj => obj.ios || obj.default);
+  return reactNative;
+});
+
+describe("ModalImage test on ios", () => {
+  tests();
+});
