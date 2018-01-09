2dd07b3274c2504e9e6b836529c6ab0dd5dfe048
James Miller <jimhunty@gmail.com>
2018-01-09 10:18:22 +0000

    fix: Revert an accidentally deleted link.android.js file. (#504)
    Updated tests and coverage for the package

--- packages/link/__tests__/android/link.android.test.js
+++ packages/link/__tests__/android/link.android.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import { Text } from "react-native";
 import test from "../shared";
-import Link, { TextLink } from "../../link.js";
+import Link, { TextLink } from "../../link";
 
 jest.mock("react-native", () => {
   const reactNative = require.requireActual("react-native");
