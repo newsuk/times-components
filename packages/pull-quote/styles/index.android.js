aa35b726042a6d2264b2a843a4d2aab410b1447c
James Miller <jimhunty@gmail.com>
2018-01-09 14:08:57 +0000

    fix: Design review for pull quotes. Small changes for iOS. (#506)
    * fix: design review for pull quotes. Small changes for iOS.
    
    * fix: update markup snapshot tests for ios
    
    * fix: typo for Android styles on pull quote

--- packages/pull-quote/styles/index.android.js
+++ packages/pull-quote/styles/index.android.js
@@ -1,16 +1,13 @@
 import { StyleSheet } from "react-native";
 import sharedStyles from "./shared";
 
-const iosStyles = StyleSheet.create({
+const androidStyles = StyleSheet.create({
   ...sharedStyles,
   container: {
     ...sharedStyles.container,
-    paddingTop: 5,
     paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 25,
-    marginBottom: 0
+    paddingRight: 10
   }
 });
 
-export default iosStyles;
+export default androidStyles;
