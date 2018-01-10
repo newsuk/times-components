c2a8c479e11542e44a2e6e9f707c58286108893a
Aron Shamash <aronshamash@gmail.com>
2018-01-10 16:35:34 +0000

    fix: adjust padding bottom author bio

--- packages/author-head/author-head-container.base.js
+++ packages/author-head/author-head-container.base.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     flexDirection: "column",
-    paddingBottom: 30,
+    paddingBottom: 40,
     backgroundColor: "#F9F8F3"
   }
 });
