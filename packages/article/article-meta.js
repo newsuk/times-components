9ba2d67dbcfee58e764a28ae4f004e4337f6b95d
James Miller <james@james-miller.co.uk>
2018-01-10 15:08:53 +0000

    fix: Seperating responsive out of article meta

--- packages/article/article-meta.js
+++ packages/article/article-meta.js
@@ -1,51 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { View } from "react-native";
-import ArticleByline from "@times-components/article-byline";
-import DatePublication from "@times-components/date-publication";
 import styles from "./styles/meta";
-import { MetaTextElement, Meta } from "./styles/meta/responsive";
+import ArticleMetaBase from "./article-meta-base";
 
-const ArticleMetaRow = (textStyle, component) => (
-  <View style={styles.articleMetaElement}>
-    <MetaTextElement style={textStyle}>{component}</MetaTextElement>
+const ArticleMeta = props => (
+  <View style={[styles.articleMiddleContainer, styles.articleMeta]}>
+    <ArticleMetaBase {...props} RowWrapper={View} />
   </View>
 );
-
-const ArticleMeta = ({ byline, publishedTime, publicationName }) => (
-  <Meta style={[styles.articleMiddleContainer, styles.articleMeta]}>
-    {ArticleMetaRow(styles.byline, <ArticleByline ast={byline} />)}
-    {ArticleMetaRow(
-      styles.datePublication,
-      <DatePublication
-        date={new Date(publishedTime)}
-        publication={publicationName}
-      />
-    )}
-  </Meta>
-);
-
-const TextNode = PropTypes.shape({ text: PropTypes.string });
-
-const nodeShape = {
-  name: PropTypes.string.isRequired,
-  attributes: PropTypes.object.isRequired
-};
-
-nodeShape.children = PropTypes.arrayOf(
-  PropTypes.oneOfType([PropTypes.shape(nodeShape), TextNode])
-).isRequired;
-
-ArticleMeta.propTypes = {
-  byline: PropTypes.arrayOf(PropTypes.shape(nodeShape)),
-  publishedTime: PropTypes.string,
-  publicationName: PropTypes.string
-};
-
-ArticleMeta.defaultProps = {
-  byline: [],
-  publishedTime: null,
-  publicationName: null
-};
 
 export default ArticleMeta;
---
+++ packages/article/article-meta.web.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles/meta";
+import ArticleMetaBase from "./article-meta-base";
+import { MetaTextElement, Meta } from "./styles/meta/responsive";
+
+const ArticleMeta = props => (
+  <Meta style={[styles.articleMiddleContainer, styles.articleMeta]}>
+    <ArticleMetaBase {...props} RowWrapper={MetaTextElement} />
+  </Meta>
+);
+
+export default ArticleMeta;
