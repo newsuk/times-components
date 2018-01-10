873a16e421f039da788c287b69c1e09378123a72
James Miller <jimhunty@gmail.com>
2018-01-10 10:17:39 +0000

    fix: REPLAT-1166 Responsive Article Patterns (#500)
    * Change base level responsive styles to use the package
    
    *  Transfer article body responsive styles to the new responsive styles package
    
    * Convert article header responsive styles to the new responsive styles package
    
    * Converting Article Meta to use responsive styles package
    
    * Removal the temporary config variables used for comparison
    
    * Remove styled components packages where they are not being used.
    
    * Updating ios and android snapshots to fix tests
    
    * Fix failing article snapshot tests
    
    * Another attempt to fix tests, getting some strange results...
    
    * Changed a padding in meta as this was a mistake in the merge, and fixed the coverage flag issue for web tests
    
    * Since removing the hack, the ios and android tests need updating
    
    * fix: typo in the import name
    
    * fix: Code review updates to remove duplication in responsive body styles
    
    * fix: Update to the pull quotes tests
    
    * fix: update markup's tests to fix CI
    
    * fix: Adding jest-styled-components to produce snapshots with styling in them
    
    * fix: styling typo
    
    * chore: fix linting
    
    * chore: updating article snapshot tests

--- packages/article/styles/body/responsive.js
+++ packages/article/styles/body/responsive.js
@@ -1,109 +1,95 @@
 import { View } from "react-native";
-import styled from "styled-components";
+import withResponsiveStyles from "@times-components/responsive-styles";
 import config from "../responsive-config";
 
 /* --- Body --- */
 
-export const ParagraphContainer = styled.div`
-  ${config.articleContainerPadding} ${config.mediumBpPositioning} ${config.wideBpPositioning};
-`;
+export const ParagraphContainer = withResponsiveStyles("div", {
+  base: () => config.articleContainerPadding,
+  mediumUp: () => config.mediumBpPositioning,
+  wideUp: () => `width: ${config.wideBpWidth};`
+});
 
-export const Paragraph = styled.p`
-  color: "#333333";
-  font-family: "TimesDigitalW04-Regular";
-  line-height: 26px;
-  font-size: 17px;
-  margin-bottom: 25px;
-  margin-top: 0;
-
-  @media (min-width: ${config.mediumBreakpoint}) {
+export const Paragraph = withResponsiveStyles("p", {
+  base: () => `
+    color: "#333333";
+    font-family: "TimesDigitalW04-Regular";
+    line-height: 26px;
+    font-size: 17px;
+    margin-bottom: 25px;
+    margin-top: 0;
+  `,
+  mediumUp: () => `
     font-size: 18px;
     line-height: 30px;
-  }
-`;
+  `
+});
 
 /* --- Lead Asset Styles --- */
 
-export const LeadAsset = styled(View)`
-  margin-bottom: 10px;
-  @media (min-width: ${config.mediumBreakpoint}) {
-    margin-bottom: 20px;
-  }
-  @media (min-width: ${config.wideBreakpoint}) {
-    width: 100%;
-    margin: 0 auto;
-  }
-`;
+export const LeadAsset = withResponsiveStyles(View, {
+  base: () => "margin-bottom: 10px;",
+  mediumUp: () => "margin-bottom: 20px",
+  wideUp: () => "width: 100%; margin: 0 auto;"
+});
 
-export const LeadAssetMobile = styled(View)`
-  ${config.showHideToggle(true)};
-`;
+export const LeadAssetMobile = config.showHideToggle(View, true);
 
-export const LeadAssetDesktop = styled(View)`
-  ${config.showHideToggle(
-    false,
-    `
-    width: ${config.wideBpWidth};
-    margin: 0 auto;
-    // Temp padding for feature flag release
-    padding-bottom: 20px;
+export const LeadAssetDesktop = config.showHideToggle(
+  View,
+  false,
   `
-  )};
-`;
+  width: ${config.wideBpWidth};
+  margin: 0 auto;
+  // Temp padding for feature flag release
+  padding-bottom: 20px;
+`
+);
 
-export const MediaContainerMobile = styled(View)`
-  ${config.showHideToggle(true)};
-`;
+export const MediaContainerMobile = config.showHideToggle(View, true);
 
-export const MediaContainerDesktop = styled(View)`
-  ${config.showHideToggle()};
-`;
+export const MediaContainerDesktop = config.showHideToggle(View);
 
 /* --- Article Images --- */
 
-export const PrimaryImg = styled(View)`
-  width: 100%;
-  flex-direction: column;
-  padding-bottom: 25px;
-
-  @media (min-width: ${config.mediumBreakpoint}) {
+export const PrimaryImg = withResponsiveStyles(View, {
+  base: () => `
+    width: 100%;
+    flex-direction: column;
+    padding-bottom: 25px;
+  `,
+  mediumUp: () => `
     width: ${config.mediumBpWidth};
     margin: 0 auto;
-  }
-  ${config.wideBpPositioning};
-`;
+  `,
+  wideUp: () => `width: ${config.wideBpWidth};`
+});
 
-export const SecondaryImg = styled(View)`
-  width: 100%;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  padding-bottom: 25px;
-  padding-left: 10px;
-  padding-right: 10px;
+const ImageStyles = withResponsiveStyles(View, {
+  base: () => `
+    width: 100%;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    padding-bottom: 25px;
+    padding-left: 10px;
+    padding-right: 10px;
+  `,
+  mediumUp: () => config.mediumBpPositioning,
+  wideUp: () => `width: ${config.wideBpWidth};`
+});
 
-  ${config.mediumBpPositioning} ${config.wideBpPositioning};
-`;
-
-export const InlineImg = styled(View)`
-  width: 100%;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  padding-bottom: 25px;
-  padding-left: 10px;
-  padding-right: 10px;
-
-  ${config.mediumBpPositioning} ${config.wideBpPositioning};
-`;
+export const SecondaryImg = ImageStyles;
+export const InlineImg = ImageStyles;
 
 /* --- Pull Quotes --- */
 
-export const PullQuoteResp = styled(View)`
-
-  padding-left: 10px;
-  padding-right: 10px;
-  margin-bottom: 10px;
-
-  @media (min-width: ${config.mediumBreakpoint}) {
+export const PullQuoteResp = withResponsiveStyles(View, {
+  base: () => `
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-bottom: 10px;
+  `,
+  mediumUp: () => `
     width: 60%;
     float: left;
     margin-right: 20px;
@@ -111,10 +97,11 @@ export const PullQuoteResp = styled(View)`
     margin-top: 5px;
     padding-left: 0px;
     padding-right: 0px;
-  }
-`;
+  `
+});
 
-export const PullQuoteContainer = styled(View)`
-  display: block;
-  ${config.mediumBpPositioning} ${config.wideBpPositioning};
-`;
+export const PullQuoteContainer = withResponsiveStyles(View, {
+  base: () => `display: block;`,
+  mediumUp: () => config.mediumBpPositioning,
+  wideUp: () => `width: ${config.wideBpWidth};`
+});
--- packages/article/styles/responsive-config.js
+++ packages/article/styles/responsive-config.js
@@ -1,22 +1,13 @@
-const mediumBreakpoint = "768px";
-const wideBreakpoint = "1024px";
+import withResponsiveStyles from "@times-components/responsive-styles";
 
 const mediumBpWidth = "83.33333333%";
 const wideBpWidth = "58.33333%";
 
 const mediumBpPositioning = `
-  @media (min-width: ${mediumBreakpoint}) {
-    width: ${mediumBpWidth};
-    margin: 0 auto;
-    padding-left: 0;
-    padding-right: 0;
-  }
-`;
-
-const wideBpPositioning = `
-@media (min-width: ${wideBreakpoint}) {
-  width: ${wideBpWidth};
-}
+  width: ${mediumBpWidth};
+  margin: 0 auto;
+  padding-left: 0;
+  padding-right: 0;
 `;
 
 const articleContainerPadding = `
@@ -24,21 +15,21 @@ const articleContainerPadding = `
   padding-right: 10px;
 `;
 
-const showHideToggle = (show, styles) => `
-  display: ${show ? "flex" : "none"};
-  @media (min-width: ${wideBreakpoint}) {
-    display: ${show ? "none" : "flex"};
-    ${styles || ""}
-  }
-`;
+const showHideToggle = (component, show, styles = "") =>
+  withResponsiveStyles(component, {
+    base: () => `
+      display: ${show ? "flex" : "none"};
+    `,
+    wideUp: () => `
+      display: ${show ? "none" : "flex"};
+      ${styles}
+    `
+  });
 
 export default {
-  mediumBreakpoint,
-  wideBreakpoint,
   mediumBpWidth,
   wideBpWidth,
   mediumBpPositioning,
-  wideBpPositioning,
   articleContainerPadding,
   showHideToggle
 };
