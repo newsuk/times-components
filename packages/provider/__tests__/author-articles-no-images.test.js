b89d33ef4b3e9357a3bdf17bf3b19726c42c4239
Craig Bilner <craig.bilner@news.co.uk>
2018-01-10 18:10:35 +0000

    chore: refactor fixture generator chore: add tests

--- packages/provider/__tests__/author-articles-no-images.test.js
+++ packages/provider/__tests__/author-articles-no-images.test.js
@@ -3,17 +3,21 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { MockedProvider } from "@times-components/utils/graphql";
-import { AuthorProfileProvider } from "../provider";
+import { AuthorArticlesNoImagesProvider } from "../provider";
 import { makeArticleMocks } from "../fixtures/author-profile/fixture-generator";
 
 const renderComponent = child =>
   renderer.create(
-    <MockedProvider
-      mocks={makeArticleMocks({ withImages: true, pageSize: 5, delay: 0 })}
-    >
-      <AuthorProfileProvider slug="deborah-haynes">
+    <MockedProvider mocks={makeArticleMocks({ pageSize: 5, delay: 0 })}>
+      <AuthorArticlesNoImagesProvider
+        slug="deborah-haynes"
+        pageSise={5}
+        page={1}
+        shortSummaryLength={220}
+        longSummaryLength={360}
+      >
         {child}
-      </AuthorProfileProvider>
+      </AuthorArticlesNoImagesProvider>
     </MockedProvider>
   );
 
