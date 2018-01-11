642c681243dfa5b6df46bfe675b28e5491332441
Aron Shamash <aronshamash@gmail.com>
2018-01-11 15:57:54 +0000

    fix: reduce size of twitter link text and update snapshots

--- packages/author-head/author-head.js
+++ packages/author-head/author-head.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     })
   },
   twitterLink: {
-    fontSize: 18,
+    fontSize: 15,
     fontFamily: "GillSansMTStd-Medium",
     color: "#006699",
     textDecorationLine: "none",
