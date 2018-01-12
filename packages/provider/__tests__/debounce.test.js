b6c970bd1420a4e80118b15c258912b90c72523c
Bernie Sumption <bernie@berniecode.com>
2018-01-12 10:08:18 +0000

    test: added describe() block around Debounce tests

--- packages/provider/__tests__/debounce.test.js
+++ packages/provider/__tests__/debounce.test.js
@@ -31,112 +31,114 @@ class Inner extends React.Component {
   }
 }
 
-it("adds debounceProps to the props passed to the inner component", () => {
-  const Outer = withDebounce(Inner, 1000);
-  const component = shallow(<Outer foo="initialFoo" />);
-  expect(component.find("Inner").props()).toEqual({
-    foo: "initialFoo",
-    debouncedProps: { foo: "initialFoo" },
-    isDebouncing: false
-  });
-});
-
-const testUpdateInnerPropsAfterDelay = delay => {
-  const Outer = withDebounce(Inner, delay);
-  const component = shallow(<Outer foo="initialFoo" />);
-
-  component.setProps({ foo: "updatedFoo" });
-  expect(component.find("Inner").props()).toEqual({
-    foo: "updatedFoo",
-    debouncedProps: { foo: "initialFoo" },
-    isDebouncing: true
+describe("Debounce Tests", () => {
+  it("adds debounceProps to the props passed to the inner component", () => {
+    const Outer = withDebounce(Inner, 1000);
+    const component = shallow(<Outer foo="initialFoo" />);
+    expect(component.find("Inner").props()).toEqual({
+      foo: "initialFoo",
+      debouncedProps: { foo: "initialFoo" },
+      isDebouncing: false
+    });
   });
 
-  jest.advanceTimersByTime(0.99 * delay);
-  expect(
-    component
-      .update()
-      .find("Inner")
-      .props().debouncedProps
-  ).toEqual({ foo: "initialFoo" });
+  const testUpdateInnerPropsAfterDelay = delay => {
+    const Outer = withDebounce(Inner, delay);
+    const component = shallow(<Outer foo="initialFoo" />);
 
-  jest.advanceTimersByTime(0.02 * delay);
-  expect(
-    component
-      .update()
-      .find("Inner")
-      .props().debouncedProps
-  ).toEqual({ foo: "updatedFoo" });
-};
+    component.setProps({ foo: "updatedFoo" });
+    expect(component.find("Inner").props()).toEqual({
+      foo: "updatedFoo",
+      debouncedProps: { foo: "initialFoo" },
+      isDebouncing: true
+    });
 
-it("immediately updates props, but delays update of debouncedProps", () => {
-  testUpdateInnerPropsAfterDelay(1000);
-});
+    jest.advanceTimersByTime(0.99 * delay);
+    expect(
+      component
+        .update()
+        .find("Inner")
+        .props().debouncedProps
+    ).toEqual({ foo: "initialFoo" });
 
-it("handles different debounce delays", () => {
-  testUpdateInnerPropsAfterDelay(2500);
-});
+    jest.advanceTimersByTime(0.02 * delay);
+    expect(
+      component
+        .update()
+        .find("Inner")
+        .props().debouncedProps
+    ).toEqual({ foo: "updatedFoo" });
+  };
 
-it("updates debounceProps once for many closely spaced props updates", () => {
-  const Outer = withDebounce(Inner, 1000);
-  const component = mount(<Outer foo="initialFoo" />);
+  it("immediately updates props, but delays update of debouncedProps", () => {
+    testUpdateInnerPropsAfterDelay(1000);
+  });
 
-  const numberOfDebouncedPropsUpdates = () =>
-    component
-      .update()
-      .find("Inner")
-      .instance().numberOfDebouncedPropsUpdates;
+  it("handles different debounce delays", () => {
+    testUpdateInnerPropsAfterDelay(2500);
+  });
 
-  expect(numberOfDebouncedPropsUpdates()).toEqual(0);
+  it("updates debounceProps once for many closely spaced props updates", () => {
+    const Outer = withDebounce(Inner, 1000);
+    const component = mount(<Outer foo="initialFoo" />);
 
-  component.setProps({ foo: "updatedFoo1" });
-  jest.advanceTimersByTime(600);
-  component.setProps({ foo: "updatedFoo2" });
-  jest.advanceTimersByTime(600);
-  component.setProps({ foo: "updatedFoo3" });
-  jest.advanceTimersByTime(600);
-  component.setProps({ foo: "updatedFoo4" });
-  jest.advanceTimersByTime(1200);
+    const numberOfDebouncedPropsUpdates = () =>
+      component
+        .update()
+        .find("Inner")
+        .instance().numberOfDebouncedPropsUpdates;
 
-  expect(numberOfDebouncedPropsUpdates()).toEqual(1);
-});
+    expect(numberOfDebouncedPropsUpdates()).toEqual(0);
 
-it("does not atttempt to update props after unmount", () => {
-  const Outer = withDebounce(Inner, 1000);
-  const component = shallow(<Outer foo="initialFoo" />);
-  const setState = jest.spyOn(component.instance(), "handleDebounceTimer");
+    component.setProps({ foo: "updatedFoo1" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo2" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo3" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo4" });
+    jest.advanceTimersByTime(1200);
 
-  expect(setState.mock.calls.length).toEqual(0);
+    expect(numberOfDebouncedPropsUpdates()).toEqual(1);
+  });
 
-  component.setProps({ foo: "updatedFoo1" });
-  jest.advanceTimersByTime(1200);
-  expect(setState.mock.calls.length).toEqual(1);
+  it("does not atttempt to update props after unmount", () => {
+    const Outer = withDebounce(Inner, 1000);
+    const component = shallow(<Outer foo="initialFoo" />);
+    const setState = jest.spyOn(component.instance(), "handleDebounceTimer");
 
-  component.unmount();
-  component.setProps({ foo: "updatedFoo2" });
-  jest.advanceTimersByTime(1200);
-  expect(setState.mock.calls.length).toEqual(1);
-});
+    expect(setState.mock.calls.length).toEqual(0);
 
-it("has appropriate static members on the outer component", () => {
-  /* eslint react/no-multi-comp: 0 */
-  class InnerWithStatics extends React.Component {
-    static staticMember = "staticMemberValue";
-    render() {
-      return this.props.foo;
+    component.setProps({ foo: "updatedFoo1" });
+    jest.advanceTimersByTime(1200);
+    expect(setState.mock.calls.length).toEqual(1);
+
+    component.unmount();
+    component.setProps({ foo: "updatedFoo2" });
+    jest.advanceTimersByTime(1200);
+    expect(setState.mock.calls.length).toEqual(1);
+  });
+
+  it("has appropriate static members on the outer component", () => {
+    /* eslint react/no-multi-comp: 0 */
+    class InnerWithStatics extends React.Component {
+      static staticMember = "staticMemberValue";
+      render() {
+        return this.props.foo;
+      }
     }
-  }
-  InnerWithStatics.propTypes = {
-    foo: PropTypes.string
-  };
-  InnerWithStatics.defaultProps = {
-    foo: ""
-  };
-  InnerWithStatics.displayName = "InnerWithStatics";
+    InnerWithStatics.propTypes = {
+      foo: PropTypes.string
+    };
+    InnerWithStatics.defaultProps = {
+      foo: ""
+    };
+    InnerWithStatics.displayName = "InnerWithStatics";
 
-  const Outer = withDebounce(InnerWithStatics, 1000);
-  expect(Outer.displayName).toEqual("WithDebounce(InnerWithStatics)");
-  expect(Outer.staticMember).toEqual("staticMemberValue");
-  expect(Outer.propTypes).toEqual({ foo: PropTypes.string });
-  expect(Outer.defaultProps).toEqual(InnerWithStatics.defaultProps);
+    const Outer = withDebounce(InnerWithStatics, 1000);
+    expect(Outer.displayName).toEqual("WithDebounce(InnerWithStatics)");
+    expect(Outer.staticMember).toEqual("staticMemberValue");
+    expect(Outer.propTypes).toEqual({ foo: PropTypes.string });
+    expect(Outer.defaultProps).toEqual(InnerWithStatics.defaultProps);
+  });
 });
