95ab46cb9e048489174369f691f86803162f3677
James Miller <james@james-miller.co.uk>
2018-01-12 11:35:59 +0000

    chore: making icons look nice in storybook

--- packages/icons/icons.stories.js
+++ packages/icons/icons.stories.js
@@ -1,12 +1,45 @@
 import "react-native";
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import { IconDiamond, IconTwitter } from "./icons";
 
+const borderWidth = 1;
+const borderColor = "#d3d3d3";
+
+const styles =StyleSheet.create({
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    margin: 20,
+    borderLeftWidth: borderWidth,
+    borderLeftColor: borderColor
+  },
+  icon: {
+    padding: 20,
+    borderRightWidth: borderWidth,
+    borderRightColor: borderColor,
+    borderTopWidth: borderWidth,
+    borderTopColor: borderColor,
+    borderBottomWidth: borderWidth,
+    borderBottomColor: borderColor
+  },
+  label: {
+    paddingTop: 15,
+    fontFamily: "GillSansMTStd-Medium"
+  }
+})
+
 storiesOf("Icons", module).add("Icons", () => (
-  <View>
-    <IconDiamond width={50} height={50} fillColour="#FF0000" />
-    <IconTwitter width={50} height={50} />
+  <View style={styles.wrapper}>
+    <View style={styles.icon}>
+      <IconDiamond width={50} height={50} fillColour="#4D4D4D" />
+      <Text style={styles.label}>Diamond</Text>
+    </View>
+    <View style={styles.icon}>
+      <IconTwitter width={50} height={50} />
+      <Text style={styles.label}>Twitter</Text>
+    </View>
   </View>
 ));
