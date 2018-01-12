60199d59a89736dbeaef49ae00a44ee0b084e040
Craig Bilner <craig.bilner@gmail.com>
2018-01-12 12:57:59 +0000

    chore: increase tracking coverage (#521)
    * chore: fix coverage check
    
    chore: fix naming and coverage
    
    chore: correctly check for child
    
    * chore: lint

--- packages/tracking/__tests__/tracking.web.test.js
+++ packages/tracking/__tests__/tracking.web.test.js
@@ -2,9 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 import PropTypes from "prop-types";
 import renderer from "react-test-renderer";
+import Enzyme, { mount } from "enzyme";
+import React16Adapter from "enzyme-adapter-react-16";
 import { withTrackScrollDepth } from "../tracking";
 import withTestContext from "./test-tracking-context";
 import sharedTrackingTests from "./shared-tracking-tests";
+
+Enzyme.configure({ adapter: new React16Adapter() });
 
 class FakeIntersectionObserver {
   static clearObserving() {
@@ -18,6 +22,9 @@ class FakeIntersectionObserver {
         target: { id: element.id }
       }))
     );
+  }
+  static dispatchObservedAllAsUndefined() {
+    this.observationCallback();
   }
   constructor(callback) {
     this.constructor.observationCallback = callback;
@@ -57,6 +64,9 @@ class ListComponent extends React.Component {
   constructor(props) {
     super(props);
     props.receiveChildList(props.items);
+  }
+  componentDidUpdate() {
+    this.props.receiveChildList(this.props.items);
   }
   render() {
     return (
@@ -106,6 +116,34 @@ describe("WithTrackScrollDepth", () => {
         ]}
       />
     );
+
+    FakeIntersectionObserver.dispatchObservedAll();
+
+    expect(reporter.mock.calls).toMatchSnapshot();
+  });
+
+  it("tracks scroll depth after update", () => {
+    const reporter = jest.fn();
+    const ListWithChildTracking = withTestContext(
+      withTrackScrollDepth(ListComponent),
+      { trackingObject: "TestObject" }
+    );
+
+    const items = [
+      { someKey: "1", someValue: "one", elementId: 1 },
+      { someKey: "2", someValue: "two", elementId: 2 },
+      { someKey: "3", someValue: "three", elementId: 3 }
+    ];
+
+    const component = mount(
+      <ListWithChildTracking analyticsStream={reporter} items={items} />
+    );
+
+    FakeIntersectionObserver.dispatchObservedAll();
+
+    component.setProps({
+      items: [...items, { someKey: "4", someValue: "four", elementId: 4 }]
+    });
 
     FakeIntersectionObserver.dispatchObservedAll();
 
@@ -208,6 +246,111 @@ describe("WithTrackScrollDepth", () => {
       .unmount();
 
     expect(disconnectSpy).toHaveBeenCalled();
+  });
+
+  it("does not throw with no observer on unmount", () => {
+    const IO = global.window.IntersectionObserver;
+
+    delete global.window.IntersectionObserver;
+
+    const ListWithChildTracking = withTestContext(
+      withTrackScrollDepth(ListComponent)
+    );
+
+    const renderComponent = () =>
+      renderer
+        .create(<ListWithChildTracking analyticsStream={() => {}} items={[]} />)
+        .unmount();
+
+    expect(renderComponent).not.toThrow();
+
+    global.window.IntersectionObserver = IO;
+  });
+
+  it("does not throw if IntersectionObserver is not available", () => {
+    const ListWithChildTracking = withTestContext(
+      withTrackScrollDepth(ListComponent)
+    );
+
+    const IO = global.window.IntersectionObserver;
+
+    delete global.window.IntersectionObserver;
+
+    const renderComponent = () =>
+      renderer.create(
+        <ListWithChildTracking analyticsStream={() => {}} items={[]} />
+      );
+
+    expect(renderComponent).not.toThrow();
+
+    global.window.IntersectionObserver = IO;
+  });
+
+  it("does not throw if there is no context", () => {
+    const ListWithChildTracking = withTrackScrollDepth(ListComponent);
+
+    const renderComponent = () => {
+      renderer.create(
+        <ListWithChildTracking
+          analyticsStream={() => {}}
+          items={[
+            { someKey: "1", someValue: "one", elementId: 1 },
+            { someKey: "2", someValue: "two", elementId: 2 }
+          ]}
+        />
+      );
+
+      FakeIntersectionObserver.dispatchObservedAll();
+    };
+
+    expect(renderComponent).not.toThrow();
+  });
+
+  it("does not throw if there is nothing to observe", () => {
+    const ListWithChildTracking = withTestContext(
+      withTrackScrollDepth(ListComponent)
+    );
+
+    const renderComponent = () => {
+      renderer.create(
+        <ListWithChildTracking
+          analyticsStream={() => {}}
+          items={[
+            { someKey: "1", someValue: "one", elementId: 1 },
+            { someKey: "2", someValue: "two", elementId: 2 }
+          ]}
+        />
+      );
+
+      FakeIntersectionObserver.dispatchObservedAllAsUndefined();
+    };
+
+    expect(renderComponent).not.toThrow();
+  });
+
+  it("does not report missing elements", () => {
+    global.window.document.getElementById = () => null;
+
+    const reporter = jest.fn();
+    const ListWithChildTracking = withTestContext(
+      withTrackScrollDepth(ListComponent),
+      { trackingObject: "TestObject" }
+    );
+
+    renderer.create(
+      <ListWithChildTracking
+        analyticsStream={reporter}
+        items={[
+          { someKey: "1", someValue: "one", elementId: 1 },
+          { someKey: "2", someValue: "two", elementId: 2 },
+          { someKey: "3", someValue: "three", elementId: 3 }
+        ]}
+      />
+    );
+
+    FakeIntersectionObserver.dispatchObservedAll();
+
+    expect(reporter.mock.calls).toMatchSnapshot();
   });
 
   sharedTrackingTests(withTrackScrollDepth);
