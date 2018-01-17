fdfca709da8a80d47bf1574fd09ab19fd64cb468
James Miller <james@james-miller.co.uk>
2018-01-17 09:31:18 +0000

    chore: fixes to icon package

--- packages/icons/__tests__/android/jest.config.js
+++ packages/icons/__tests__/android/jest.config.js
@@ -1,3 +1,3 @@
-const jestConfigurator = require("@times-components/jest-configurator");
+const jestConfigurator = require("@times-components/jest-configurator").default;
 
-module.exports = jestConfigurator("icons", "android");
+module.exports = jestConfigurator("android", __dirname);
