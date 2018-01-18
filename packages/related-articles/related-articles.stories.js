38cad17bbe19df24ff63327ca3424b9f0c7cb142
Aron Shamash <aronshamash@gmail.com>
2018-01-18 12:32:48 +0000

    feat: Adding story for article with no images

--- packages/related-articles/related-articles.stories.js
+++ packages/related-articles/related-articles.stories.js
@@ -4,6 +4,7 @@ import { decorateAction } from "@storybook/addon-actions";
 import RelatedArticles from "./related-articles";
 
 const singleRelatedArticleFixture = require("./fixtures/related-article.json");
+const singleRelatedNoImageArticleFixture = require("./fixtures/related-article-no-image.json");
 
 const preventDefaultedAction = decorateAction([
   ([e, ...args]) => {
@@ -12,9 +13,17 @@ const preventDefaultedAction = decorateAction([
   }
 ]);
 
-storiesOf("Related Articles", module).add("Single article with images", () => {
+storiesOf("Related Articles", module)
+  .add("Single article with images", () => {
+    const props = {
+      ...singleRelatedArticleFixture.data.article,
+      onPress: preventDefaultedAction("onArticlePress")
+    };
+
+    return <RelatedArticles {...props} />;
+}).add("Single article with no lead image", () => {
   const props = {
-    ...singleRelatedArticleFixture.data.article,
+    ...singleRelatedNoImageArticleFixture.data.article,
     onPress: preventDefaultedAction("onArticlePress")
   };
 
