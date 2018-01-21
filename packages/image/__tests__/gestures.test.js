4c72055355a51cc6c32c5cc39f8573e30b03af66
Craig Bilner <craig.bilner@news.co.uk>
2018-01-21 12:41:52 +0000

    chore: refactor

--- packages/image/__tests__/gestures.test.js
+++ packages/image/__tests__/gestures.test.js
@@ -8,37 +8,145 @@ const mapTouches = ({ x, y }) => ({
   pageY: y
 });
 
-const makeTouchEvent = touches => ({
+const makeTouchEvent = (active, history = []) => ({
   touchHistory: {
-    numberActiveTouches: touches.length,
-    touchBank: touches
+    numberActiveTouches: history.length,
+    touchBank: history.map(mapTouches)
   },
   nativeEvent: {
-    touches: touches.map(mapTouches)
+    touches: active.map(mapTouches)
   }
 });
 
+const touchAndMove = (startTouchPositions, endTouchPositions) => {
+  const component = shallow(
+    <Gesture>
+      <Image uri="http://example.com/image.jpg" aspectRatio={3 / 2} />
+    </Gesture>
+  );
+
+  const {
+    panResponder: { panHandlers: { onResponderStart, onResponderMove } }
+  } = component.instance();
+
+  onResponderStart(makeTouchEvent(startTouchPositions));
+  onResponderMove(makeTouchEvent(endTouchPositions, startTouchPositions));
+
+  return component;
+};
+
 export default () => {
   describe("Gestures should", () => {
-    let component;
+    it("scale correctly when spreading fingers", () => {
+      const component = touchAndMove(
+        [
+          {
+            x: 50,
+            y: 50
+          },
+          {
+            x: 100,
+            y: 100
+          }
+        ],
+        [
+          {
+            x: 0,
+            y: 0
+          },
+          {
+            x: 150,
+            y: 150
+          }
+        ]
+      );
 
-    beforeEach(() => {
-       component = shallow( <Gesture>
-        <Image uri="http://example.com/image.jpg" aspectRatio={3 / 2} />
-      </Gesture>);
+      expect(component.find("AnimatedComponent")).toMatchSnapshot();
     });
 
-    it("handle a pinch", () => {
-      const {
-        panResponder: {
-          panHandlers: {
-            onResponderStart,
-            onResponderMove
+    it("rotate anti-clockwise when turning fingers", () => {
+      const component = touchAndMove(
+        [
+          {
+            x: 50,
+            y: 50
+          },
+          {
+            x: 50,
+            y: 100
           }
-        }
-      } = component.instance();
+        ],
+        [
+          {
+            x: 75,
+            y: 75
+          },
+          {
+            x: 25,
+            y: 75
+          }
+        ]
+      );
 
-      onResponderStart(makeTouchEvent([
+      expect(component.find("AnimatedComponent")).toMatchSnapshot();
+    });
+
+    it("rotate clockwise when turning fingers", () => {
+      const component = touchAndMove(
+        [
+          {
+            x: 50,
+            y: 50
+          },
+          {
+            x: 50,
+            y: 100
+          }
+        ],
+        [
+          {
+            x: 25,
+            y: 75
+          },
+          {
+            x: 75,
+            y: 75
+          }
+        ]
+      );
+
+      expect(component.find("AnimatedComponent")).toMatchSnapshot();
+    });
+
+    it("scale and rotate when spreading and rotating fingers", () => {
+      const component = touchAndMove(
+        [
+          {
+            x: 50,
+            y: 50
+          },
+          {
+            x: 100,
+            y: 100
+          }
+        ],
+        [
+          {
+            x: 30,
+            y: 40
+          },
+          {
+            x: 190,
+            y: 170
+          }
+        ]
+      );
+
+      expect(component.find("AnimatedComponent")).toMatchSnapshot();
+    });
+
+    it("do nothing if a finger is removed while interacting", () => {
+      const start = [
         {
           x: 50,
           y: 50
@@ -47,20 +155,21 @@ export default () => {
           x: 100,
           y: 100
         }
-      ]));
+      ];
 
-      onResponderMove(makeTouchEvent([
+      const end = [
         {
-          x: 0,
-          y: 0
-        },
-        {
-          x: 160,
-          y: 140
+          x: 190,
+          y: 170
         }
-      ]));
+      ];
 
-      expect(component.find('AnimatedComponent')).toMatchSnapshot();
+      const component = touchAndMove(
+        start,
+        end
+      );
+
+      expect(component.find("AnimatedComponent")).toMatchSnapshot();
     });
   });
 };
