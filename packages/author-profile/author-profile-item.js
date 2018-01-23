f9e5e6bfe5ad379f79639514e5ea7144795c405e
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-01-23 10:43:16 +0000

    Added tracking name to author profile items

--- packages/author-profile/author-profile-item.js
+++ packages/author-profile/author-profile-item.js
@@ -75,6 +75,7 @@ export default withTrackEvents(AuthorProfileItem, {
     {
       eventName: "onPress",
       actionName: "Pressed",
+      trackingName: "AuthorProfileItem",
       getAttrs: ({ headline, id }) => ({
         articleHeadline: headline,
         articleId: id
