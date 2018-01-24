6689e61c33b9b88f2856dcf124dae4c6d2838432
colincclark <col.c.clark@gmail.com>
2018-01-24 10:03:21 +0000

    chore: increase coverage for date, stub out Intl used for tz checks

--- packages/utils/__tests__/date.test.js
+++ packages/utils/__tests__/date.test.js
@@ -1,6 +1,5 @@
-/* eslint-env jest */
-
-import { isBST, getUTCTime } from "../date";
+/* global Intl */
+import { isBST, getUTCTime, isLondonTimezone } from "../date";
 
 describe("date helper", () => {
   it("2017-11-17 00:01:00 should be a GMT date", () => {
@@ -9,5 +8,21 @@ describe("date helper", () => {
 
   it("2017-05-17 00:01:00 should be a BST date", () => {
     expect(isBST(getUTCTime("2017-05-17T00:01:00.000Z"))).toEqual(true);
+  });
+});
+
+describe("timezone helper", () => {
+  it("should return true when London is the timezone", () => {
+    expect(isLondonTimezone()).toEqual(true);
+  });
+
+  it("should return false when London is NOT the timezone", () => {
+    Intl.DateTimeFormat = () => ({
+      resolvedOptions: () => ({
+        timeZone: "Somewhere/Else"
+      })
+    });
+
+    expect(isLondonTimezone()).toEqual(false);
   });
 });
---
+++ packages/utils/setup-jest.js
@@ -0,0 +1,7 @@
+global.Intl = {
+  DateTimeFormat: () => ({
+    resolvedOptions: () => ({
+      timeZone: "Europe/London"
+    })
+  })
+};
