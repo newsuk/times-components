28c3a94382f21353181c89d3d4bc6d0fa66b8d32
colincclark <col.c.clark@gmail.com>
2018-01-24 10:03:21 +0000

    chore: increase coverage for date, stub out Intl used for tz checks

--- packages/jest-configurator/setup-jest.js
+++ packages/jest-configurator/setup-jest.js
@@ -2,3 +2,11 @@ import Adapter from "enzyme-adapter-react-16";
 import Enzyme from "enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.Intl = {
+  DateTimeFormat: () => ({
+    resolvedOptions: () => ({
+      timeZone: "Europe/London"
+    })
+  })
+};
