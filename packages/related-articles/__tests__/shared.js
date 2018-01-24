4f30194fb73833708bda4a980d50c7e6ae3be7a3
colincclark <col.c.clark@gmail.com>
2018-01-24 13:20:53 +0000

    test: refactor tests to be more DRY

--- packages/related-articles/__tests__/shared.js
+++ packages/related-articles/__tests__/shared.js
@@ -8,52 +8,57 @@ const singleRelatedArticleNoImageFixture = require("../fixtures/related-article-
 const singleRelatedArticleNoLabelFixture = require("../fixtures/related-article-no-label.json");
 const singleRelatedArticleNoBylineFixture = require("../fixtures/related-article-no-byline.json");
 
+const createProps = fixtureData => ({
+  ...fixtureData.relatedArticles[0],
+  template: fixtureData.relatedArticlesTemplate,
+  onPress: () => {}
+});
+
 module.exports = () => {
   it("renders single default related article", () => {
-    const props = {
-      ...singleRelatedArticleFixture.data.relatedArticles[0],
-      template: singleRelatedArticleFixture.data.relatedArticlesTemplate,
-      onPress: () => {}
-    };
     const tree = renderer
-      .create(<RelatedArticles item={props} analyticsStream={() => {}} />)
+      .create(
+        <RelatedArticles
+          item={createProps(singleRelatedArticleFixture.data)}
+          analyticsStream={() => {}}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders single default related article with no lead image", () => {
-    const props = {
-      ...singleRelatedArticleNoImageFixture.data.relatedArticles[0],
-      template: singleRelatedArticleNoImageFixture.data.relatedArticlesTemplate,
-      onPress: () => {}
-    };
     const tree = renderer
-      .create(<RelatedArticles item={props} analyticsStream={() => {}} />)
+      .create(
+        <RelatedArticles
+          item={createProps(singleRelatedArticleNoImageFixture.data)}
+          analyticsStream={() => {}}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders single default related article with no label", () => {
-    const props = {
-      ...singleRelatedArticleNoLabelFixture.data.relatedArticles[0],
-      template: singleRelatedArticleNoLabelFixture.data.relatedArticlesTemplate,
-      onPress: () => {}
-    };
     const tree = renderer
-      .create(<RelatedArticles item={props} analyticsStream={() => {}} />)
+      .create(
+        <RelatedArticles
+          item={createProps(singleRelatedArticleNoLabelFixture.data)}
+          analyticsStream={() => {}}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders single default related article with no byline", () => {
-    const props = {
-      ...singleRelatedArticleNoBylineFixture.data.relatedArticles[0],
-      template:
-        singleRelatedArticleNoBylineFixture.data.relatedArticlesTemplate,
-      onPress: () => {}
-    };
     const tree = renderer
-      .create(<RelatedArticles item={props} analyticsStream={() => {}} />)
+      .create(
+        <RelatedArticles
+          item={createProps(singleRelatedArticleNoBylineFixture.data)}
+          analyticsStream={() => {}}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
