8b680b50b8e7d0b454867872087fc485b96b8694
NehaSrivastava <neha.srivastava84525@gmail.com>
2018-01-26 14:18:46 +0000

    chore: fixing bug

--- packages/ad/ad-composer.js
+++ packages/ad/ad-composer.js
@@ -46,33 +46,31 @@ class AdComposer extends Component {
   }
 
   setPageLevelConfig(adConfig) {
-    return [
-      {
-        edition_id: window.nuk ? nuk.ads.editionDate : null,
-        e_uuid: window.nuk ? nuk.ads.editionId : null,
-        search: "null",
-        share_token: "null",
-        shared: "0",
-        cont: adConfig.contentType,
-        aid: adConfig.id,
-        kw: `${adConfig.title} ${adConfig.label} ${
-          adConfig.commercialtags
-        }`.split(" "),
-        pw: "1",
-        teaser: window.nuk
-          ? !nuk.user.isLoggedIn || nuk.user.isMeteredExpired
-          : "0",
-        log: window.nuk ? this.isLoggedIn() : "0",
-        subscriber: window.nuk ? this.getSubscriber() : "0",
-        kuid: localStorage.get("kxkuid"),
-        ksg: localStorage.get("kxsegs"),
-        ppid: cookieHelper.getCpnId() || "null",
-        eid: cookieHelper.getCpnId() || "null",
-        om_v_id: cookieHelper.getVistorId() || "null",
-        cips: cookieHelper.getCips() || "null",
-        refresh: "false"
-      }
-    ];
+    return {
+      edition_id: window.nuk ? nuk.ads.editionDate : null,
+      e_uuid: window.nuk ? nuk.ads.editionId : null,
+      search: "null",
+      share_token: "null",
+      shared: "0",
+      cont: adConfig.contentType,
+      aid: adConfig.id,
+      kw: `${adConfig.title} ${adConfig.label} ${
+        adConfig.commercialtags
+      }`.split(" "),
+      pw: "1",
+      teaser: window.nuk
+        ? !nuk.user.isLoggedIn || nuk.user.isMeteredExpired
+        : "0",
+      log: window.nuk ? this.isLoggedIn() : "0",
+      subscriber: window.nuk ? this.getSubscriber() : "0",
+      kuid: localStorage.get("kxkuid"),
+      ksg: localStorage.get("kxsegs"),
+      ppid: cookieHelper.getCpnId() || "null",
+      eid: cookieHelper.getCpnId() || "null",
+      om_v_id: cookieHelper.getVistorId() || "null",
+      cips: cookieHelper.getCips() || "null",
+      refresh: "false"
+    };
   }
 
   render() {
