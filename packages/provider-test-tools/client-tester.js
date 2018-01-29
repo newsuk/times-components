e4fdacd3e5721c39b915c69c6bfc8439eb153228
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-01-29 17:32:28 +0000

    chore: improves readme of provider tester

--- packages/provider-test-tools/client-tester.js
+++ packages/provider-test-tools/client-tester.js
@@ -2,14 +2,6 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import TestLink from "./test-link";
 
-// @flow
-type Operation = {
-  queryName: string,
-  variables: any
-};
-
-type RequestHandler = Operation => Promise<any>;
-
 export default function clientTester(requestHandler: RequestHandler) {
   const link = new TestLink(requestHandler);
   const client = new ApolloClient({
