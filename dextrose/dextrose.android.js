35cf8e288994f31eab89db89d88867d6c5763c72
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-29 17:35:58 +0000

    removing unwanted dextrose config

--- dextrose/dextrose.android.js
+++ dextrose/dextrose.android.js
@@ -3,12 +3,6 @@ const path = require("path");
 const config = {
   snapPath: path.join(__dirname, "/snappy"),
   platformName: "Android",
-  platformVersion: "5.1",
-  deviceName: "Nexus_5X_API_22",
-  app: path.join(
-    __dirname,
-    "../android/app/build/outputs/apk/release/app-release.apk"
-  ),
   ignoredStories: [
     "Advertisement",
     "AuthorHeadTracking",
