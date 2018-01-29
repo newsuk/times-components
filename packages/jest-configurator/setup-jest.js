206c749b91fd5a387629aa8449beaef4a7904387
colincclark <col.c.clark@gmail.com>
2018-01-29 10:46:14 +0000

    fix: fix dextrose builds - new packages need the dextrose scripts

--- packages/jest-configurator/setup-jest.js
+++ packages/jest-configurator/setup-jest.js
@@ -2,11 +2,3 @@ import Adapter from "enzyme-adapter-react-16";
 import Enzyme from "enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
-
-global.Intl = {
-  DateTimeFormat: () => ({
-    resolvedOptions: () => ({
-      timeZone: "Europe/London"
-    })
-  })
-};
