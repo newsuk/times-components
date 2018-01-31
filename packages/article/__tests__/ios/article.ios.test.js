1df8ff01b09867442aad821966001e13d552fa30
colincclark <col.c.clark@gmail.com>
2018-01-31 21:52:09 +0000

    refactor: move related articles tests to their own files

--- packages/article/__tests__/ios/article.ios.test.js
+++ packages/article/__tests__/ios/article.ios.test.js
@@ -1,7 +1,5 @@
 import shared from "../shared";
-import relatedArticlesShared from "../related-articles-shared";
 
 describe("Article test on ios", () => {
   shared();
-  relatedArticlesShared();
 });
