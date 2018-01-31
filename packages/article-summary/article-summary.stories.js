4b58140637b459d8d2e33892f5fa8bf91f823f8b
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-01-31 17:04:55 +0000

    chore: adds remaining fixtures to article summary

--- packages/article-summary/article-summary.stories.js
+++ packages/article-summary/article-summary.stories.js
@@ -5,23 +5,20 @@ import { storiesOf } from "dextrose/storiesOfOverloader";
 import ArticleSummary from "./article-summary";
 
 import defaultFixture from "./fixtures/default.js";
-import articleMultiFixture from "./fixtures/article-multi.json";
-import noBylineFixture from "./fixtures/no-byline.json";
-import noLabelFixture from "./fixtures/no-label.json";
-import reviewFixture from "./fixtures/review.json";
+import articleMultiFixture from "./fixtures/article-multi.js";
+import noBylineFixture from "./fixtures/no-byline.js";
+import noLabelFixture from "./fixtures/no-label.js";
+import reviewFixture from "./fixtures/review.js";
 
 const story = m => <View style={{ padding: 20 }}>{m}</View>;
 
 storiesOf("ArticleSummary", module)
   .add("Default", () => story(<ArticleSummary {...defaultFixture} />))
-
-/*
+  .add("No byline", () => story(<ArticleSummary {...noBylineFixture} />))
   .add("Summary with multiple paragraphs", () =>
     story(<ArticleSummary {...articleMultiFixture} />)
   )
-  .add("No byline", () => story(<ArticleSummary {...noBylineFixture} />))
   .add("No label", () => story(<ArticleSummary {...noLabelFixture} />))
   .add("Review/Rating Summary", () =>
     story(<ArticleSummary {...reviewFixture} />)
   );
-*/
