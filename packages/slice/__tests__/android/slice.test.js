ffd47eff7a93d85fb547658a5b9c8966fdd0f310
colincclark <col.c.clark@gmail.com>
2018-01-31 21:15:41 +0000

    refactor: move related articles test from slice

--- packages/slice/__tests__/android/slice.test.js
+++ packages/slice/__tests__/android/slice.test.js
@@ -1,4 +1,4 @@
-import shared from "@times-components/article/__tests__/related-articles-shared.js";
+import shared from "@times-components/article/__tests__/related-articles-shared";
 
 describe("Slice test on android", () => {
   shared();
