d563b83592b53eaad1ded84b095e29a0a7b5c551
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2018-02-02 12:32:36 +0000

    feat: add provider testing package (#543)

---
+++ packages/provider-test-tools/provider-tester.js
@@ -0,0 +1,73 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PropTypes from "prop-types";
+import { ApolloProvider } from "react-apollo-temp";
+import clientTester from "./client-tester";
+
+export default function providerTester(
+  requestHandler,
+  Component,
+  defaultProps = {}
+) {
+  const { link, client } = clientTester(requestHandler);
+
+  let isMounted = false;
+  let setProps = () => Promise.resolve();
+  class Stateful extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = defaultProps;
+    }
+
+    componentDidMount() {
+      isMounted = true;
+      setProps = state => {
+        if (!isMounted) return Promise.resolve();
+
+        return new Promise(done =>
+          this.setState(() => {
+            done(state);
+            return state;
+          })
+        );
+      };
+    }
+
+    componentWillUnmount() {
+      isMounted = false;
+      setProps = () => Promise.resolve();
+    }
+
+    render() {
+      return this.props.children(this.state);
+    }
+  }
+
+  Stateful.propTypes = {
+    children: PropTypes.func.isRequired
+  };
+
+  const component = renderer.create(
+    <ApolloProvider client={client}>
+      {
+        <Stateful>
+          {state => (
+            <Component {...state}>
+              {props => {
+                link.pushEvent({ type: "render", props });
+                return null;
+              }}
+            </Component>
+          )}
+        </Stateful>
+      }
+    </ApolloProvider>
+  );
+
+  return {
+    client,
+    link,
+    setProps,
+    component
+  };
+}
---
+++ packages/provider-test-tools/test-link.js
@@ -0,0 +1,72 @@
+import { ApolloLink, Observable } from "apollo-link";
+import isEqual from "lodash.isequal";
+import createFuture from "./future";
+
+export default class TestLink extends ApolloLink {
+  constructor(onRequest) {
+    super();
+    this.onRequest = onRequest;
+    this.operations = [];
+    this.blocked = [];
+    this.events = [];
+  }
+
+  findByQuery(queryName, variables) {
+    return this.operations.find(
+      ({ operation }) =>
+        operation.operationName === queryName &&
+        (!variables || isEqual(operation.variables, variables))
+    );
+  }
+
+  filterByQuery(queryName, variables) {
+    return this.operations.filter(
+      ({ operation }) =>
+        operation.operationName === queryName &&
+        (!variables || isEqual(operation.variables, variables))
+    );
+  }
+
+  // push a custom event
+  pushEvent(data) {
+    this.events.push(data);
+  }
+
+  // get all requests
+  getRequests() {
+    return this.operations;
+  }
+
+  // find and resolve a request by a given filter
+
+  // get all events
+  getEvents() {
+    return this.events;
+  }
+
+  // used by apollo provider
+  request(operation) {
+    this.blocked.push(createFuture());
+    const { promise, resolve } = this.blocked[this.blocked.length - 1];
+    this.operations.push({ operation, resolve, promise });
+    this.events.push({ type: "request", operation });
+    return new Observable(observer => {
+      Promise.resolve(this.onRequest(operation))
+        .then(async data => {
+          this.events.push({ type: "resolving", operation });
+          await promise();
+          this.events.push({ type: "resolved", operation, data });
+          if (!observer.closed) {
+            observer.next(data);
+            observer.complete();
+          }
+        })
+        .catch(e => {
+          this.events.push({ type: "error", error: e });
+          if (!observer.closed) {
+            observer.error(e);
+          }
+        });
+    });
+  }
+}
