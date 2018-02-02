bfa675b0ba5572634eea0c40dc8c9d156bf10cc6
James Miller <jimhunty@gmail.com>
2018-02-02 11:57:36 +0000

    test: Updating Date Publication to use jest-configurator

--- packages/date-publication/__tests__/shared.js
+++ packages/date-publication/__tests__/shared.js
@@ -29,23 +29,21 @@ function gmtTests(userTimezone) {
 
   it("renders a DatePublication component with an appropriate GMT date", () => {
     const tree = renderer
-      .create(
-        <DatePublication date="2017-01-01T14:32:00.000Z" publication="TIMES" />
-      )
+      .create(<DatePublication date="2017-01-01T14:32:00.000Z" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders a DatePublication component with an appropriate BST date", () => {
     const tree = renderer
-      .create(
-        <DatePublication date="2017-07-01T14:32:00.000Z" publication="TIMES" />
-      )
+      .create(<DatePublication date="2017-07-01T14:32:00.000Z" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 }
 
+
+function tests() {
 describe("when the user has the same time zone as London time zone", () => {
   gmtTests("Europe/London");
 });
@@ -53,3 +51,6 @@ describe("when the user has the same time zone as London time zone", () => {
 describe("when the user has a different time zone than London time zone", () => {
   gmtTests("Europe/Kiev");
 });
+}
+
+export default tests;
