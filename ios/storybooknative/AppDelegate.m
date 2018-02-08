9fd019480963582e33f2e830255498bb68ac2223
kaliabadi <kouros.aliabadi@hotmail.com>
2018-02-08 14:46:36 +0000

    using a release vairant for dextrose to better manage errors

--- ios/storybooknative/AppDelegate.m
+++ ios/storybooknative/AppDelegate.m
@@ -18,13 +18,15 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"storybooknative"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  
+  NSLog(@"%@", rootView);
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
