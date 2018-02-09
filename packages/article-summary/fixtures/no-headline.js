e4c6d422d791afc432949d05b61fd82ef053f471
James Miller <jimhunty@gmail.com>
2018-02-09 10:45:21 +0000

    test: improve the test coverage on the article summary package

---
+++ packages/article-summary/fixtures/no-headline.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { ArticleSummaryContent } from "../";
+
+export default {
+  labelProps: {
+    title: "Camilla Long",
+    color: "#333333"
+  },
+  datePublicationProps: {
+    date: "2017-07-01T14:32:00.000Z",
+    publication: "SUNDAYTIMES"
+  },
+  content: () => (
+    <ArticleSummaryContent
+      ast={[
+        {
+          name: "paragraph",
+          attributes: {},
+          children: [
+            {
+              name: "text",
+              attributes: {
+                value:
+                  "The special forces dog fought on under fire, even after shrapnel from Taliban grenades tore into his belly and legs, blew out a front tooth and damaged his right ear."
+              },
+              children: []
+            }
+          ]
+        }
+      ]}
+    />
+  )
+};
