f109fe2dc11778051c740e853127465dcbdfac75
James Miller <jimhunty@gmail.com>
2018-02-09 09:53:46 +0000

    chore: implement the initial code review feedback

--- packages/article-summary/article-summary-content.js
+++ packages/article-summary/article-summary-content.js
@@ -5,11 +5,10 @@ import { treePropType } from "@times-components/markup";
 import { renderAst } from "./";
 import styles from "./styles";
 
-export default function ArticleSummaryContent({ ast }) {
-  return ast.length > 0 ? (
+const ArticleSummaryContent = ({ ast }) =>
+  ast.length > 0 ? (
     <Text style={styles.text}>{renderAst(ast)}</Text>
   ) : null;
-}
 
 ArticleSummaryContent.propTypes = {
   ast: PropTypes.arrayOf(treePropType)
@@ -18,3 +17,5 @@ ArticleSummaryContent.propTypes = {
 ArticleSummaryContent.defaultProps = {
   ast: []
 };
+
+export default ArticleSummaryContent;
