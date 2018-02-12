5dacb6fafed47ff506cf80ed52f4a8291955c7a3
kaliabadi <kouros.aliabadi@hotmail.com>
2018-02-12 11:08:42 +0000

    app delegate change

--- ios/storybooknative/AppDelegate.m
+++ ios/storybooknative/AppDelegate.m
@@ -18,7 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"storybooknative"
