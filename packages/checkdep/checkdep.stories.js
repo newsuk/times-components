90dcde98e6fb935ae607d4d4ac5d26bb12967b04
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-02-12 20:28:37 +0000

    feat: initial implementation for checkdep

---
+++ packages/checkdep/checkdep.stories.js
@@ -0,0 +1,8 @@
+import "react-native";
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import Checkdep from "./checkdep";
+
+storiesOf("Checkdep", module).add("Checkdep", () => (
+  <Checkdep />
+));
