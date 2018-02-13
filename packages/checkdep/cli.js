4e240485b3709fb84b600a17bcda0ad6c9e61b69
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-02-13 17:27:39 +0000

    feat: adds colours and options to depcheck

--- packages/checkdep/cli.js
+++ packages/checkdep/cli.js
@@ -1,32 +1,84 @@
 require("babel-register");
 require("babel-polyfill");
+const chalk = require("chalk");
+const yargs = require("optimist");
 const checkdep = require("./checkdep").default;
+const strategies = require("./strategies");
+const { writeJson } = require('fs-extra');
 
-const expr = process.argv[2] || "packages/*/package.json";
-const strategy = process.argv[3] || "majorityConservative";
-console.log({expr, strategy});
-checkdep(expr, strategy)
-  .then( ({divergent, wrong, fixupMap, suggestions, fixedPackages, all}) => {
-    console.log('----all direct dependencies-----');
-    console.log(Object.keys(all));
+const argv = yargs
+  .option("expr", "glob expression that finds package.json files")
+  .string("expr")
+  .alias("expr", "e")
+  .alias("strategy", "s")
+  .option("fix", "fixed dependencies with wrong versions")
+  .boolean("fix")
+  .option("show-rules", "prints rules that will be applied")
+  .boolean("show-rules")
+  .option("list", "prints all dependencies in use")
+  .boolean("list")
+  .option("bail", "returns 1 if packages need fixing")
+  .boolean("bail")
+  .option("hint", "print fix suggestions")
+  .boolean("hint")
+  .option("strategy", "how to resolve conflicts")
+  .boolean("string")
+  .default({
+    expr: "packages/*/package.json",
+    list: false,
+    fix: false,
+    bail: false,
+    info: true,
+    showRules: false,
+    strategy: undefined
+  }).argv;
 
-    console.log('----divergent dependencies-----');
-    console.log(divergent);
+checkdep(argv.expr, argv.strategy ? strategies[argv.strategy] : null)
+  .then( ({fixupMap, suggestions, fixedPackages, all}) => {
 
-    console.log('----out of sync packages-----');
-    console.log(wrong);
+    if (argv.list) {
+      Object.entries(all)
+        .map( ([name, versions]) => [name, [...versions]] )
+        .forEach( ([name, versions]) => {
+          const color = (versions.length>1 && !fixupMap[name])
+            ? chalk.red
+            : (!fixupMap[name])
+              ? chalk.green
+              : chalk.yellow;
 
-    console.log('----fixing rules-----')
-    console.log(fixupMap)
+          console.log(name, color(versions.join(' ')));
+      });
+    }
 
-    console.log('----fix suggestions---');
-    suggestions.forEach(
-      ([path, suggestions])=>
-        console.log(path, suggestions)
-    );
 
-    console.log('----fixed packages----');
-    console.log(fixedPackages);
+    if (argv.showRules) {
+      console.log(fixupMap);
+    }
+
+    if (argv.hint || argv.fix) {
+      suggestions.forEach(
+        ([path, suggestions]) => {
+
+        console.log(path)
+        console.log(
+          suggestions.map(
+            ([name, current, target]) =>
+              " "+ chalk.blue(name) + ": "+ chalk.red(current) + " -> "+ chalk.green(target)
+          ).join('\n'))
+      });
+    }
+
+    if (argv.bail && suggestions.length) {
+      process.exit(1);
+    }
+
+    if (argv.fix) {
+      return Promise.all(
+        fixedPackages.map(([path, json]) => writeJson(path, json))
+      );
+    }
+
+
   })
   .catch(e => {
     console.log(e.toString());
