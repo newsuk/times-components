45649c2e07a43bccd474db3c2370cc88fc2f2b28
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-02-13 12:14:42 +0000

    wip: rewrite in es6+

--- packages/checkdep/checkdep.stories.js
+++
@@ -1,8 +0,0 @@
-import "react-native";
-import React from "react";
-import { storiesOf } from "@storybook/react-native";
-import Checkdep from "./checkdep";
-
-storiesOf("Checkdep", module).add("Checkdep", () => (
-  <Checkdep />
-));
---
+++ packages/checkdep/get-packages.js
@@ -0,0 +1,13 @@
+import glob from "glob";
+import { readJson } from "fs-extra";
+
+export default function getPackages(expr) {
+  return new Promise((done, reject) => {
+    glob(expr, (error, paths) => {
+      if (error) return reject(error);
+      return done(
+        paths.map(p=>readJson(p))
+      );
+    });
+  }).then(files => Promise.all(files));
+}
