82ca9c380fe983994a347ba405d58fe34e92a99c
colincclark <col.c.clark@gmail.com>
2018-02-13 16:10:55 +0000

    feat: add semantic elements to related articles

--- packages/article/related-articles/related-articles.web.js
+++ packages/article/related-articles/related-articles.web.js
@@ -33,7 +33,7 @@ const RelatedArticles = ({ articles, onPress, template }) => {
         myArray.push(<StyledSeparator key={`separator-${article.id}`} />);
       }
       myArray.push(
-        <StyledRelatedArticleItemContainer key={article.id}>
+        <StyledRelatedArticleItemContainer accessibilityRole="article" key={article.id}>
           <RelatedArticleItem
             article={article}
             onPress={onPress}
