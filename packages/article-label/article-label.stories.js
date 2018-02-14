966553d45aeae7ea780ec7a80a292e451169e662
James Miller <jimhunty@gmail.com>
2018-02-14 14:55:26 +0000

    feat: Add knobs to Article Label

--- packages/article-label/article-label.stories.js
+++ packages/article-label/article-label.stories.js
@@ -1,8 +1,25 @@
 import "react-native";
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
+import { select } from '@storybook/addon-knobs/react';
 import ArticleLabel from "./article-label";
 
+ // This is fixed in a future version of Storybook; waiting on release
+ const swap = obj => {
+    let ret = {};
+    for(var key in obj){
+      ret[obj[key]] = key;
+    }
+    return ret;
+  }
+
+  // Waiting on styleguide approval
+  const options = {
+    thedish: "#db133b",
+    sport: "#008347",
+    comment: "#850029"
+  }
+
 storiesOf("Primitives/ArticleLabel", module).add("ArticleLabel", () => (
-  <ArticleLabel title="swimming" color="#008347" />
+  <ArticleLabel title="swimming" color={select("Section", swap(options), "sport")} />
 ));
