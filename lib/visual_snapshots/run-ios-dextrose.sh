d35ae4be6893e037d7cfc6acee869af767e22c64
kaliabadi <kouros.aliabadi@hotmail.com>
2018-02-15 14:50:02 +0000

    updating local script

--- lib/visual_snapshots/run-ios-dextrose.sh
+++ lib/visual_snapshots/run-ios-dextrose.sh
@@ -3,7 +3,7 @@ xcrun simctl boot 'iPhone 7'
 npx lerna run dextrose-clean
 npx lerna run dextrose-stories --since
 npm run fetch-fonts
-npx rnstl --searchDir ./packages --pattern '**/*/*.stories!(.ios).dextrose.tmp.js' --outputFile ./fructose/components.js
+npx rnstl --searchDir ./packages --pattern '**/*/*.stories!(.web).dextrose.tmp.js' --outputFile ./fructose/components.js
 export CWD=$(pwd) 
 export FRC=$CWD/fructose 
 npx react-native bundle --platform ios --dev false --reset-cache --entry-file fructose/index.js --bundle-output ios/main.jsbundle
