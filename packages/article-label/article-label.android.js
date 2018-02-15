c0c28da2b4b1889d756c9d94818c26970f3b0c24
James Miller <jimhunty@gmail.com>
2018-02-15 10:05:06 +0000

    feat: Small refactor to gain 100% coverage on Article Label

--- packages/article-label/article-label.android.js
+++ packages/article-label/article-label.android.js
@@ -3,8 +3,17 @@ import { Text } from "react-native";
 import PropTypes from "prop-types";
 import styles from "./style";
 
+// apply transformations to add uppercase and letter spacing.
+// letterSpacing CSS prop does not work on android:
+// https://github.com/facebook/react-native/pull/13199
+const beautifyTitle = title =>
+  title
+    .toUpperCase()
+    .split("")
+    .join("\u200A");
+
 const ArticleLabel = ({ title, color }) => (
-  <Text style={[styles.title, { color }]}>{title.toUpperCase()}</Text>
+  <Text style={[styles.title, { color }]}>{beautifyTitle(title)}</Text>
 );
 
 ArticleLabel.propTypes = {
