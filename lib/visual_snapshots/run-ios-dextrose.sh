90fe12af8e5cdf2225f3ad4af993c730e2abb4d2
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-15 10:58:07 +0000

    fix: Ios running locally

--- lib/visual_snapshots/run-ios-dextrose.sh
+++ lib/visual_snapshots/run-ios-dextrose.sh
@@ -3,11 +3,13 @@ xcrun simctl boot 'iPhone 7'
 npx lerna run dextrose-clean
 npx lerna run dextrose-stories --since
 npm run fetch-fonts
-npx rnstl --searchDir ./packages --pattern './*/*.dextrose.tmp.js' --outputFile ./fructose/components.js
-npx compile-tests -d fructose
+npx rnstl --searchDir ./packages --pattern '**/*/*.stories!(.ios).dextrose.tmp.js' --outputFile ./fructose/components.js
 export CWD=$(pwd) 
 export FRC=$CWD/fructose 
-npx react-native bundle --platform ios --dev false --entry-file fructose/index.js --bundle-output ios/main.jsbundle
+npx react-native bundle --platform ios --dev false --reset-cache --entry-file fructose/index.js --bundle-output ios/main.jsbundle
+mkdir -p ios/build/Build/Products/Release-iphonesimulator/storybooknative.app/
+cp ios/main.jsbundle.meta ios/build/Build/Products/Release-iphonesimulator/storybooknative.app/
+cp ios/main.jsbundle ios/build/Build/Products/Release-iphonesimulator/storybooknative.app/
 react-native run-ios --no-packager --configuration Release
 PACKAGER_PID=$!
 LOGLEVEL=verbose npx dextrose run --config ./dextrose/dextrose.ios.js --snapshotWait 2000
