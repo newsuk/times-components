4a1cedd959257ea3d5a7257e1cb6ca2e754614a2
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-15 17:45:23 +0000

    fix: recommiting slice.web.js

---
+++ packages/slice/slice.web.js
@@ -0,0 +1,27 @@
+import React from "react";  
+import { propTypes, defaultProps } from "./proptypes";  
+import getTemplateName from "./styles/templatemap";  
+import getStyledComponent from "./styles";  
+  
+const Slice = ({ children, template }) => {  
+  const templateName = getTemplateName(template);  
+  const StyledSliceContainer = getStyledComponent(  
+    templateName,  
+    "SliceContainer"  
+  );  
+  const StyledChildrenContainer = getStyledComponent(  
+    templateName,  
+    "ChildrenContainer"  
+  );  
+  
+  return (  
+    <StyledSliceContainer>  
+      <StyledChildrenContainer>{children}</StyledChildrenContainer>  
+    </StyledSliceContainer>  
+  );  
+};  
+  
+Slice.propTypes = propTypes;  
+Slice.defaultProps = defaultProps;  
+  
+export default Slice;
