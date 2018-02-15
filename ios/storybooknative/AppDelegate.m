ced6e6687b16684cf69a848a0de44a5a527bdf2b
killanaca <killanaca@gmail.com>
2018-02-15 18:23:43 +0000

    fix: storybook-native ios issue

--- ios/storybooknative/AppDelegate.m
+++ ios/storybooknative/AppDelegate.m
@@ -18,7 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"storybooknative"
