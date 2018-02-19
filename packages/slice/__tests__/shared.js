cb4235062f49c0dd66a45b248353d1c7e941b6e0
colincclark <col.c.clark@gmail.com>
2018-02-19 13:48:13 +0000

    chore: fix linting issues

--- packages/slice/__tests__/shared.js
+++ packages/slice/__tests__/shared.js
@@ -8,52 +8,34 @@ module.exports = () => {
   context("DEFAULT template", () => {
     const template = "DEFAULT";
 
-    const items = [
-      { key: 1 },
-      { key: 2 },
-      { key: 3 }
-    ];
+    const items = [{ key: 1 }, { key: 2 }, { key: 3 }];
 
     const createItems = noOfItems =>
       items
         .map(item => {
           const { key } = item;
           if (key > noOfItems) return false;
-          return (
-            <Text key={key} />
-          );
+          return <Text key={key} />;
         })
         .filter(item => item !== false);
 
     it("renders a single child element", () => {
       const tree = renderer
-        .create(
-          <Slice template={template}>
-            {createItems(1)}
-          </Slice>
-        )
+        .create(<Slice template={template}>{createItems(1)}</Slice>)
         .toJSON();
       expect(tree).toMatchSnapshot();
     });
 
     it("renders two child elements", () => {
       const tree = renderer
-        .create(
-          <Slice template={template}>
-            {createItems(2)}
-          </Slice>
-        )
+        .create(<Slice template={template}>{createItems(2)}</Slice>)
         .toJSON();
       expect(tree).toMatchSnapshot();
     });
 
     it("renders three child elements", () => {
       const tree = renderer
-        .create(
-          <Slice template={template}>
-            {createItems(3)}
-          </Slice>
-        )
+        .create(<Slice template={template}>{createItems(3)}</Slice>)
         .toJSON();
       expect(tree).toMatchSnapshot();
     });
