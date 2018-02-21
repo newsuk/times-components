88e6eacd052400cec93d9d66627733c5c790ea56
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-02-21 16:03:08 +0000

    feat: adds role names to related articles tracking

--- packages/article/related-articles/related-articles-tracking-context.js
+++ packages/article/related-articles/related-articles-tracking-context.js
@@ -1,5 +1,6 @@
 import get from "lodash.get";
 import { withTrackingContext } from "@times-components/tracking";
+import role from "@times-components/slice/styles/role-map";
 
 export default Component =>
   withTrackingContext(Component, {
@@ -7,11 +8,14 @@ export default Component =>
     isDataReady: ({ isLoading }) => !isLoading,
     getAttrs: ({ template, articles }) => ({
       template,
-      articles: articles.map(({ id, headline, publishedTime, byline }) => ({
-        id,
-        byline: get(byline, "[0].children[0].attributes.value", ""),
-        headline,
-        publishedTime
-      }))
+      articles: articles.map(
+        ({ id, headline, publishedTime, byline }, index) => ({
+          id,
+          byline: get(byline, "[0].children[0].attributes.value", ""),
+          headline,
+          publishedTime,
+          role: role(template, index)
+        })
+      )
     })
   });
