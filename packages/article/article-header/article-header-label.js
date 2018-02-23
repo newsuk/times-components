8c085264259e2ef5d4efcf8c59a67e64b4e227d8
Aron Shamash <aronshamash@gmail.com>
2018-02-23 13:01:05 +0000

    minor update

--- packages/article/article-header/article-header-label.js
+++ packages/article/article-header/article-header-label.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ArticleLabel from "@times-components/article-label";
+import { colours } from "@times-components/styleguide";
 import { View } from "react-native";
 import styles from "../styles/article-header";
+
 
 const HeaderLabel = ({ label }) => {
   if (!label) return null;
   return (
     <View accessibilityLabel="label" testID="label" style={styles.articleLabel}>
-      <ArticleLabel title={label} color="#13354E" />
+      <ArticleLabel title={label} color={colours.section.default} />
     </View>
   );
 };
