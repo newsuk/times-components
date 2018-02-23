f37bcca9eee8645e53ab560ec570b34a974a3ef9
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-02-23 11:35:07 +0000

    chore: seperate render test and analytics tests from related article tests

--- packages/article/__tests__/related-articles-shared.js
+++ packages/article/__tests__/related-articles-shared.js
@@ -14,7 +14,7 @@ import singleRelatedArticleNoBylineFixture from "../related-articles/fixtures/si
 import twoRelatedArticlesFixture from "../related-articles/fixtures/two-related-articles.json";
 import threeRelatedArticlesFixture from "../related-articles/fixtures/three-related-articles.json";
 
-const createRelatedArticlesProps = (fixtureData, action) => ({
+const createRelatedArticlesProps = (fixtureData, action = () => {}) => ({
   analyticsStream: action,
   articles: fixtureData.relatedArticles,
   template: fixtureData.relatedArticlesLayout.template,
@@ -38,7 +38,94 @@ export default () => {
     global.Intl = realIntl;
   });
 
-  describe("Related articles", () => {
+  describe("Related articles render", () => {
+    it("handles no related articles", () => {
+      const data = {
+        relatedArticles: [],
+        relatedArticlesLayout: {
+          template: "DEFAULT"
+        }
+      };
+      const tree = renderer
+        .create(<RelatedArticles {...createRelatedArticlesProps(data)} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders single related article", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(singleRelatedArticleFixture.data)}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders single related article with no lead image", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(
+              singleRelatedArticleNoImageFixture.data
+            )}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders single related article with no label", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(
+              singleRelatedArticleNoLabelFixture.data
+            )}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders single related article with no byline", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(
+              singleRelatedArticleNoBylineFixture.data
+            )}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders two related articles", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(twoRelatedArticlesFixture.data)}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders three related articles", () => {
+      const tree = renderer
+        .create(
+          <RelatedArticles
+            {...createRelatedArticlesProps(threeRelatedArticlesFixture.data)}
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("Related articles analytics", () => {
     it("handles no related articles", () => {
       const events = jest.fn();
       const data = {
@@ -47,108 +134,87 @@ export default () => {
           template: "DEFAULT"
         }
       };
-      const tree = renderer
-        .create(
-          <RelatedArticles {...createRelatedArticlesProps(data, events)} />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles {...createRelatedArticlesProps(data, events)} />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders single related article", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              singleRelatedArticleFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            singleRelatedArticleFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders single related article with no lead image", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              singleRelatedArticleNoImageFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            singleRelatedArticleNoImageFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders single related article with no label", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              singleRelatedArticleNoLabelFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            singleRelatedArticleNoLabelFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders single related article with no byline", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              singleRelatedArticleNoBylineFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            singleRelatedArticleNoBylineFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders two related articles", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              twoRelatedArticlesFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            twoRelatedArticlesFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
 
     it("renders three related articles", () => {
       const events = jest.fn();
-      const tree = renderer
-        .create(
-          <RelatedArticles
-            {...createRelatedArticlesProps(
-              threeRelatedArticlesFixture.data,
-              events
-            )}
-          />
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
+      const tree = renderer.create(
+        <RelatedArticles
+          {...createRelatedArticlesProps(
+            threeRelatedArticlesFixture.data,
+            events
+          )}
+        />
+      );
       expect(events.mock.calls).toMatchSnapshot();
     });
   });
