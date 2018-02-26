0b1c34af60f1045da5b9b743313394b7a5d31624
colincclark <col.c.clark@gmail.com>
2018-02-26 11:11:06 +0000

    refactor: maintain original API but use arrays to manage logic

--- packages/slice/templates/lead/lead.js
+++ packages/slice/templates/lead/lead.js
@@ -3,14 +3,14 @@ import { View } from "react-native";
 import { propTypes, defaultProps } from "./proptypes";
 import styles from "../styles";
 
-const Slice = ({ lead, sections }) => (
+const Slice = ({ lead, support1, support2 }) => (
   <View style={styles.container}>
     <View style={styles.childContainer}>
-      <View style={[styles.child, { paddingTop: 0 }]}>{lead()}</View>
+      <View style={[styles.child, { paddingTop: 0 }]}>{lead}</View>
     </View>
-    {sections.map(section => (
-      <View key={section.key} style={styles.childContainer}>
-        <View style={styles.child}>{section}</View>
+    {[support1, support2].filter(support => support !== null).map(support => (
+      <View key={support.key} style={styles.childContainer}>
+        <View style={styles.child}>{support}</View>
       </View>
     ))}
   </View>
