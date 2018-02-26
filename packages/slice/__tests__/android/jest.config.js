975a047a893bbb2221ca446ca24b75c5e181e540
colincclark <col.c.clark@gmail.com>
2018-02-26 13:12:42 +0000

    chore: format code

--- packages/slice/__tests__/android/jest.config.js
+++ packages/slice/__tests__/android/jest.config.js
@@ -1,4 +1,8 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
 
 // @TODO: remove "**/styles" and "slice.js" when related articles has been refactored and styles removed
-module.exports = jestConfigurator("android", __dirname, ["responsive.web.js", "**/styles", "slice.js"]);
+module.exports = jestConfigurator("android", __dirname, [
+  "responsive.web.js",
+  "**/styles",
+  "slice.js"
+]);
--- packages/slice/__tests__/ios/jest.config.js
+++ packages/slice/__tests__/ios/jest.config.js
@@ -1,4 +1,8 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
 
 // @TODO: remove "**/styles" when related articles has been refactored and styles removed
-module.exports = jestConfigurator("ios", __dirname, ["responsive.web.js", "**/styles", "slice.js"]);
+module.exports = jestConfigurator("ios", __dirname, [
+  "responsive.web.js",
+  "**/styles",
+  "slice.js"
+]);
