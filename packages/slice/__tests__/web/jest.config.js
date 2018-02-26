d51ea70da8fd02d68b0d3d70ceb3d651a7f81cfa
colincclark <col.c.clark@gmail.com>
2018-02-26 13:12:42 +0000

    chore: format code

--- packages/slice/__tests__/web/jest.config.js
+++ packages/slice/__tests__/web/jest.config.js
@@ -1,5 +1,5 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
-
+ 
 module.exports = jestConfigurator("web", __dirname, [
   "**/templates/**/index.js",
   "**/templates/styles.js",
