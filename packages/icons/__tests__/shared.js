99373c5cb604e621856cb9a3126cde3ef968263f
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-03-02 12:59:08 +0000

    chore: update tests

--- packages/icons/__tests__/shared.js
+++ packages/icons/__tests__/shared.js
@@ -13,6 +13,7 @@ module.exports = () => {
 
     expect(tree).toMatchSnapshot();
   });
+
   it("Twitter renders correctly", () => {
     const tree = renderer
       .create(<IconTwitter width={50} height={50} />)
@@ -20,6 +21,7 @@ module.exports = () => {
 
     expect(tree).toMatchSnapshot();
   });
+
   it("Twitter renders correctly with different fillColour", () => {
     const tree = renderer
       .create(<IconTwitter width={50} height={50} fillColour="#4D4D4D" />)
@@ -28,7 +30,15 @@ module.exports = () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it("Twitter renders correctly with different fillColour", () => {
+  it("Video renders correctly", () => {
+    const tree = renderer
+      .create(<IconVideo width={50} height={50} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Video renders correctly with different fillColour", () => {
     const tree = renderer
       .create(<IconVideo width={50} height={50} fillColour="#4D4D4D" />)
       .toJSON();
