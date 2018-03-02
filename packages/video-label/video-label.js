fdeb73ef0546f2f3178ed6d0c56cde7d13995157
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-03-02 15:34:53 +0000

    feat: adds video-icon to video-label

--- packages/video-label/video-label.js
+++ packages/video-label/video-label.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { VideoIcon } from "@times-components/icons";
 import PropTypes from "prop-types";
 import styles from "./style";
 
 const VideoLabel = ({ title, color }) => (
-  <Text style={[styles.title, { color }]}>{
-    `## Video ${title ? `| ${title}`: ""}`
-  }</Text>
+  <View>
+    <VideoIcon
+      width={styles.title.fontSize}
+      height={styles.title.fontSize}
+      fillColor={color}
+    />
+    <Text style={[styles.title, { color }]}>Video</Text>
+    {title && <Text style={[styles.title, { color }]}> | {Title}</Text>}
+  </View>
 );
 
 VideoLabel.propTypes = {
