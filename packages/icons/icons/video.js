55bcb12f379a7513ca246d6afde6c6338f54b175
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-03-02 11:48:31 +0000

    chore: make linter happy

--- packages/icons/icons/video.js
+++ packages/icons/icons/video.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Svg, { Path } from "svgs";
+import Svg, { Rect, Polygon } from "svgs";
 
 const viewBox = [
   0.15463916957378387,
@@ -11,18 +11,18 @@ const viewBox = [
 
 const IconVideo = ({ width, height, fillColour }) => (
   <Svg width={width} height={height} viewBox={viewBox}>
-    <rect
+    <Rect
       fill={fillColour}
       x="0.154639175"
       y="0.139754386"
       width="15.4550103"
       height="13.5721404"
     />
-    <polygon
+    <Polygon
       fill={fillColour}
       points="16.3405361 4.14989474 16.3405361 9.66442105 22.0216082 12.8146667 22.0216082 0.999894737"
     />
-    <polygon
+    <Polygon
       fill={fillColour}
       points="23.7538144 0.0496140351 22.7616495 0.643508772 22.7616495 13.1902105 23.7538144 13.7782105"
     />
