556775e62f6a7f8135ff241380301b30e7f8a55c
NehaSrivastava <neha.srivastava84525@gmail.com>
2018-03-03 14:46:14 +0000

    refactor: adding sponsored ad slot only, functionality is diff story

--- packages/article/article.web.js
+++ packages/article/article.web.js
@@ -67,6 +67,9 @@ class ArticlePage extends React.Component {
             <Ad pos="pixel" style={adStyle} />
             <Ad pos="pixelskin" style={adStyle} />
             <Ad pos="pixelteads" style={adStyle} />
+
+            // This should come after the related links, once we have it
+            <Ad pos="article-sponsored-ad" style={adStyle} />
           </View>
         </MainContainer>
       </Fragment>
