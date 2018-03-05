f8733e62f0505a8eacf2ed3f9036c5afffc01aa0
Bernie Sumption <bernie@berniecode.com>
2018-03-05 17:12:32 +0000

    Added stub tests

--- packages/ad/__tests__/ad-init-grapeshot.test.js
+++ packages/ad/__tests__/ad-init-grapeshot.test.js
@@ -13,6 +13,16 @@ describe("Ad init", () => {
     initOptions = adInitMocks.initOptions;
   });
 
-  it.skip("TODO: grapeshot tests", () => {
+  it.skip("Sets the page targeting value if the script loads", () => {
+    // check that window.gs_channels is copied into gs_cat page targeting
+    // by mocking gpt.scheduleSetPageTargetingValues
+  });
+
+  it.skip("Does not set page targeting value if the script errors", () => {
+
+  });
+
+  it.skip("Does not set page targeting value if the script times out", () => {
+
   });
 });
