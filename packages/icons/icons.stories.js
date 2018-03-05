928a0548cd901973471ff58c71b6c9428bdce5ac
Aron Shamash <aronshamash@gmail.com>
2018-03-05 10:22:14 +0000

    feat: icon colours from styleguide

--- packages/icons/icons.stories.js
+++ packages/icons/icons.stories.js
@@ -6,7 +6,7 @@ import { color } from "@storybook/addon-knobs";
 import { IconDiamond, IconTwitter, IconVideo } from "./icons";
 
 const borderWidth = 1;
-const borderColor = "#D3D3D3";
+const borderColor = colours.functional.alto;
 
 const styles = StyleSheet.create({
   wrapper: {
