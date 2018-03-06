1c61b6e74010b14ca8f2ded7e2bd0be26a79cc77
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-03-06 16:53:00 +0000

    chore: updates test names in video label

--- packages/video-label/__tests__/shared.js
+++ packages/video-label/__tests__/shared.js
@@ -3,11 +3,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import VideoLabel from "../video-label";
 
-<<<<<<< HEAD
 export default () => {
-=======
-module.exports = () => {
->>>>>>> feat: implements video-label
   it("renders VideoLabel", () => {
     const tree = renderer
       .create(<VideoLabel title="swimming" color="#008347" />)
--- packages/video-label/video-label.js
+++ packages/video-label/video-label.js
@@ -14,13 +14,6 @@ const VideoLabel = ({ title, color }) => (
         {title.toUpperCase()}
       </Text>
     ) : null}
-  <View style={{ flexDirection: "row", alignItems: "center" }}>
-    <IconVideo height={styles.title.fontSize} fillColor={color} />
-    <Text style={[styles.title, { color, marginLeft: 5 }]}>VIDEO</Text>
-    {title ? <Text style={[styles.title,{ marginLeft: 5 }]}>|</Text> : null}
-    {title ? (
-      <Text style={[styles.title, { color, paddingLeft: 5 }]}>{title}</Text>
-    ) : null}
   </View>
 );
 
