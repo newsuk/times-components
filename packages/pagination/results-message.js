7a29318bda39dca6f0547cc218867902cf3d712a
Aron Shamash <aronshamash@gmail.com>
2018-03-06 13:57:43 +0000

    feat: colours for pull-quotes, pagination and markup

--- packages/pagination/results-message.js
+++ packages/pagination/results-message.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Text, StyleSheet } from "react-native";
-import { fonts } from "@times-components/styleguide";
+import { colours, fonts } from "@times-components/styleguide";
 
 const styles = StyleSheet.create({
   message: {
-    color: "#696969",
+    color: colours.functional.secondary,
     fontFamily: fonts.supporting,
     fontSize: 15,
     paddingTop: 4
