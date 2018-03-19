728ce63c34c69c2fc9331cd8a0281eda8f9f54b3
NehaSrivastava <neha.srivastava84525@gmail.com>
2018-03-19 13:39:49 +0000

    test: fix tests

--- packages/ad/ad-init.js
+++ packages/ad/ad-init.js
@@ -120,9 +120,11 @@ const adInit = args => {
           slot.defineSizeMapping(gptMapping.build());
 
           const randomTestingGroup = Math.floor(Math.random() * 10).toString();
-          Object.keys(slotTargeting).forEach(key =>
-            slot.setTargeting(key, slotTargeting[key])
-          );
+          if (slotTargeting) {
+            Object.keys(slotTargeting).forEach(key =>
+              slot.setTargeting(key, slotTargeting[key])
+            );
+          }
           slot.setTargeting("timestestgroup", randomTestingGroup);
           slot.setTargeting("pos", containerID);
           window.googletag.display(containerID);
