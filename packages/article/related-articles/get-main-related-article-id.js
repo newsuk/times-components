fa262e9930be73b8c58c34b008b5cd07fcf2845e
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-03-29 17:21:57 +0100

    Added related articles lead and opinion until tpa is released

--- packages/article/related-articles/get-main-related-article-id.js
+++ packages/article/related-articles/get-main-related-article-id.js
@@ -1,6 +1,8 @@
 export default function getMainRelatedArticleId(relatedArticlesLayout) {
   return (
+    relatedArticlesLayout.lead ||
     relatedArticlesLayout.main ||
+    relatedArticlesLayout.opinion ||
     ""
   );
 }
