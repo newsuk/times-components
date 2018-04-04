3ca3d30b17594ae9d75c8b36d60923be5a42f629
L0wry <matthewlowry@hotmail.co.uk>
2018-04-04 10:54:55 +0100

    fix: make prettier happy

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import Fructose from './fructose/index';
 import Expo, { Font } from "expo";
-// eslint-disable-next-line import/no-unresolved
+import GillSansMTStd from './dist/public/fonts/GillSansMTStd-Medium.ttf';
+import TimesDigitalW04R from './dist/public/fonts/TimesDigitalW04-Regular.ttf';
+import TimesDigitalW04SC from './dist/public/fonts/TimesDigitalW04-RegularSC.ttf';
+import TimesDigitalW04 from './dist/public/fonts/TimesDigitalW04.ttf';
+import TimesDigitalW04B from './dist/public/fonts/TimesDigitalW04_bold.ttf';
+import TimesDigitalW04I from './dist/public/fonts/TimesDigitalW04_italic.ttf';
+import TimesModernB from  './dist/public/fonts/TimesModern-Bold.ttf';
+import TimesModernR from './dist/public/fonts/TimesModern-Regular.ttf';
 
 Expo.registerRootComponent(Fructose);
 
 Font.loadAsync({
-    "GillSansMTStd-Medium": require(`./dist/public/fonts/GillSansMTStd-Medium.ttf`),
-    "TimesDigitalW04-Regular": require(`./dist/public/fonts/TimesDigitalW04-Regular.ttf`),
-    "TimesDigitalW04-RegularSC": require(`./dist/public/fonts/TimesDigitalW04-RegularSC.ttf`),
-    "TimesDigitalW04": require(`./dist/public/fonts/TimesDigitalW04.ttf`),
-    "TimesDigitalW04_bold": require(`./dist/public/fonts/TimesDigitalW04_bold.ttf`),
-    "TimesDigitalW04_italic": require(`./dist/public/fonts/TimesDigitalW04_italic.ttf`),
-    "TimesModern-Bold": require(`./dist/public/fonts/TimesModern-Bold.ttf`),
-    "TimesModern-Regular": require(`./dist/public/fonts/TimesModern-Regular.ttf`),
+    "GillSansMTStd-Medium": GillSansMTStd,
+    "TimesDigitalW04-Regular": TimesDigitalW04R,
+    "TimesDigitalW04-RegularSC": TimesDigitalW04SC,
+    "TimesDigitalW04": TimesDigitalW04,
+    "TimesDigitalW04_bold": TimesDigitalW04B,
+    "TimesDigitalW04_italic": TimesDigitalW04I,
+    "TimesModern-Bold": TimesModernB,
+    "TimesModern-Regular": TimesModernR,
 });
