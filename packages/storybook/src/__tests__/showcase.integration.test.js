86067c991e949c173c3f7c02e47bbc11a9eb2aba
Craig Bilner <craig.bilner@news.co.uk>
2018-04-06 14:04:58 +0100

    chore: lint

--- packages/storybook/src/__tests__/showcase.integration.test.js
+++ packages/storybook/src/__tests__/showcase.integration.test.js
@@ -6,7 +6,7 @@ import showcaseToStoryBook from "../showcase-to-storybook";
 jest.mock("../showcase-to-storybook");
 
 test("The storybook should create the expected converter", () => {
-  require('../storybook');
+  require("../storybook");
 
   expect(showcaseToStoryBook).toHaveBeenCalledWith(storiesOf, knobs, actions);
 });
