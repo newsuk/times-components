e19a06630de9ae72b10dc916ffcec2b9aacd4f17
Craig Bilner <craig.bilner@gmail.com>
2018-04-12 10:36:30 +0100

    chore: lint (#853)

--- packages/utils/src/__tests__/add-missing-protocol.test.js
+++ packages/utils/src/__tests__/add-missing-protocol.test.js
@@ -2,10 +2,14 @@ import addMissingProtocol from "../add-missing-protocol";
 
 describe("addMissingProtocol should", () => {
   it("add https to the url if it's missing the protocol", () => {
-    expect(addMissingProtocol("//thetimes.co.uk")).toEqual("https://thetimes.co.uk");
+    expect(addMissingProtocol("//thetimes.co.uk")).toEqual(
+      "https://thetimes.co.uk"
+    );
   });
 
   it("return the given url if it has a protocol already", () => {
-    expect(addMissingProtocol("http://thetimes.co.uk")).toEqual("http://thetimes.co.uk");
+    expect(addMissingProtocol("http://thetimes.co.uk")).toEqual(
+      "http://thetimes.co.uk"
+    );
   });
 });
