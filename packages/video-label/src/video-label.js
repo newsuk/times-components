b5a51300f76189e061d276c85addfd378391c164
Bernie Sumption <bernie@berniecode.com>
2018-04-12 12:05:24 +0100

    feat: only set video label to "video" if no explicit label is provided

--- packages/video-label/src/video-label.js
+++ packages/video-label/src/video-label.js
@@ -1,28 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 import { IconVideo } from "@times-components/icons";
 import PropTypes from "prop-types";
 import styles from "./style";
 import beautifyTitle from "./beautify-title";
 
-const showTitle = title => title && title.toLowerCase() !== "video";
-
-const VideoLabel = ({ title, color }) => (
-  <View style={{ flexDirection: "row", alignItems: "center" }}>
-    <IconVideo height={styles.title.fontSize} fillColor={color} />
-    <Text style={[styles.title, { color, marginLeft: 5 }]}>
-      {beautifyTitle("video")}
-    </Text>
-    {showTitle(title) ? (
-      <Fragment>
-        <Text style={[styles.pipe, { color, marginLeft: 5 }]}>|</Text>
-        <Text style={[styles.title, { color, paddingLeft: 5 }]}>
-          {beautifyTitle(title)}
-        </Text>
-      </Fragment>
-    ) : null}
-  </View>
-);
+const VideoLabel = ({ title, color }) => {
+  const displayTitle = title || "VIDEO";
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <IconVideo height={styles.title.fontSize} fillColor={color} />
+      <Text style={[styles.title, { color, marginLeft: 5 }]}>
+        {beautifyTitle(displayTitle)}
+      </Text>
+    </View>
+  );
+};
 
 VideoLabel.propTypes = {
   title: PropTypes.string,
