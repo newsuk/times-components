91d836dd4d50f227014647b6c30c86de5779659a
colincclark <col.c.clark@gmail.com>
2018-04-13 11:36:33 +0100

    test: fix missing utils test export

--- packages/utils/src/screen.base.js
+++ packages/utils/src/screen.base.js
@@ -1,6 +1,6 @@
 import { Dimensions } from "react-native";
 
-const acceptedWidths = [320, 440, 660, 800, 1440];
+export const acceptedWidths = [320, 440, 660, 800, 1440];
 
 export const normaliseWidth = width => {
   const nWidth = acceptedWidths.find(w => width <= w);
