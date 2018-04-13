86d4841a54eaf578e82ee02a421ec29ef23cfe20
Bernie Sumption <bernie@berniecode.com>
2018-04-13 13:29:56 +0100

    chore: bump react to version 16.3.1 (#858)
    * Bumped react to version 16.3.1

---
+++ packages/storybook/src/knobs.js
@@ -0,0 +1,9 @@
+import * as nativeKnobs from "@storybook/addon-knobs/react";
+import select from "./select-shim";
+
+const knobs = {
+  nativeKnobs,
+  select
+};
+
+export default knobs;
--- packages/storybook/src/storybook.js
+++ packages/storybook/src/storybook.js
@@ -1,8 +1,8 @@
 import { storiesOf } from "@storybook/react-native";
-import * as knobs from "@storybook/addon-knobs/react";
 import * as actions from "@storybook/addon-actions";
 import * as decorators from "./decorators";
 import showcaseToStoryBook from "./showcase-to-storybook";
+import knobs from "./knobs";
 import select from "./select-shim";
 
 const {
