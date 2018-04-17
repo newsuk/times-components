07022c441f45a4e2a71e12c965cb5eb0ecb5e838
James Wright <james.wright@yld.io>
2018-04-17 12:43:26 +0100

    Tests for transform and formatting

--- packages/jest-configurator/__tests__/source-loader.test.js
+++ packages/jest-configurator/__tests__/source-loader.test.js
@@ -3,12 +3,15 @@ import * as fs from "fs";
 import { getCacheKey, process as transform } from "../src/source-loader";
 
 jest.mock("babel-jest");
+jest.mock("fs");
 
 const stubHrtime = (seconds, nanoseconds) => {
   const realHrtime = process.hrtime;
   process.hrtime = () => [seconds, nanoseconds];
 
-  return () => process.hrtime = realHrtime;
+  return () => {
+    process.hrtime = realHrtime;
+  };
 };
 
 describe("source loader", () => {
@@ -16,11 +19,14 @@ describe("source loader", () => {
   const nanoseconds = 50;
   let restoreHrtime;
 
-  beforeAll(() => restoreHrtime = stubHrtime(seconds, nanoseconds));
+  beforeAll(() => {
+    restoreHrtime = stubHrtime(seconds, nanoseconds);
+  });
+
   afterAll(() => restoreHrtime());
 
   describe("getCacheKey", () => {
-    it("should create a high-res time-based key if the file belongs a monorepo package", () => {
+    it("should create a high-res time-based key if the file belongs to a monorepo package", () => {
       const filename = "node_modules/@times-components/foo/bar.js";
       const expectedKey = `${filename}_${seconds}${nanoseconds}`;
       const actualKey = getCacheKey("", filename);
@@ -40,11 +46,64 @@ describe("source loader", () => {
       const actualKey = getCacheKey(src, filename, config, options);
 
       expect(actualKey).toEqual(expectedKey);
-      expect(babelJest.getCacheKey).toHaveBeenCalledWith(src, filename, config, options);
+      expect(babelJest.getCacheKey).toHaveBeenCalledWith(
+        src,
+        filename,
+        config,
+        options
+      );
     });
   });
 
   describe("transform", () => {
+    const inputSource = "source";
+    const expectedOutput = "output";
+    const config = {};
+    const options = {};
 
+    beforeEach(() => {
+      babelJest.process.mockImplementationOnce(() => expectedOutput);
+    });
+
+    afterEach(() => {
+      fs.readFileSync.mockReset();
+    });
+
+    it("should read the respective source file for a module if it belongs to a monorepo package", () => {
+      const srcFilename = "node_modules/@times-components/src/foo/bar.js";
+      const distFilename = "node_modules/@times-components/dist/foo/bar.js";
+      const rawSource = "raw source";
+
+      fs.readFileSync.mockImplementationOnce(() => rawSource);
+
+      const actualOutput = transform(
+        inputSource,
+        distFilename,
+        config,
+        options
+      );
+
+      expect(actualOutput).toEqual(expectedOutput);
+      expect(babelJest.process).toHaveBeenCalledWith(
+        rawSource,
+        srcFilename,
+        config,
+        options
+      );
+    });
+
+    it("should not manipulate the source and filename parameters if they don`t belong to a monorepo package", () => {
+      const filename = "node_modules/react-native/foo/bar.js";
+      const actualOutput = transform(inputSource, filename, config, options);
+
+      expect(actualOutput).toEqual(expectedOutput);
+      expect(babelJest.process).toHaveBeenCalledWith(
+        inputSource,
+        filename,
+        config,
+        options
+      );
+      expect(fs.readFileSync).not.toHaveBeenCalled();
+    });
   });
 });
