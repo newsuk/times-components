b183e59ba0c24c0b4eda7ed6d2581f9c27a0861e
James Wright <james.wright@yld.io>
2018-04-17 09:59:00 +0100

    Removes unnecessary hash generation for package source cache keys

--- packages/jest-configurator/src/source-loader.js
+++ packages/jest-configurator/src/source-loader.js
@@ -1,14 +1,15 @@
 const babelJest = require("babel-jest");
-const crypto = require("crypto");
 const fs = require("fs");
 
 const readSource = filename => fs.readFileSync(filename).toString();
 const isPackageFile = filename => filename.includes("@times-components");
-const getRandomKey = () => crypto.createHash('sha256').update(process.hrtime().join('')).digest();
+const getPackageCacheKey = filename => `${filename}_${process.hrtime().join('')}`;
 
 module.exports = {
   getCacheKey(src, filename, config, cacheOptions) {
-    return isPackageFile(filename) ? getRandomKey() : babelJest.getCacheKey(src, filename, config, cacheOptions);
+    return isPackageFile(filename)
+      ? getPackageCacheKey(filename)
+      : babelJest.getCacheKey(src, filename, config, cacheOptions);
   },
 
   process(src, targetFilename, config, processOptions) {
