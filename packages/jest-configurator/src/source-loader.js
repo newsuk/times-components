6e4e0111cb21800147509dd43841c5e7e27e2805
James Wright <james.wright@yld.io>
2018-04-17 10:08:50 +0100

    Migrates to ES module syntax

--- packages/jest-configurator/src/source-loader.js
+++ packages/jest-configurator/src/source-loader.js
@@ -1,26 +1,29 @@
-const babelJest = require("babel-jest");
-const fs = require("fs");
+import * as babelJest from "babel-jest";
+import * as fs from "fs";
 
 const readSource = filename => fs.readFileSync(filename).toString();
 const isPackageFile = filename => filename.includes("@times-components");
 const getPackageCacheKey = filename => `${filename}_${process.hrtime().join('')}`;
 
-module.exports = {
-  getCacheKey(src, filename, config, cacheOptions) {
-    return isPackageFile(filename)
-      ? getPackageCacheKey(filename)
-      : babelJest.getCacheKey(src, filename, config, cacheOptions);
-  },
+const getCacheKey = (src, filename, config, cacheOptions) => (
+  isPackageFile(filename)
+    ? getPackageCacheKey(filename)
+    : babelJest.getCacheKey(src, filename, config, cacheOptions)
+);
 
-  process(src, targetFilename, config, processOptions) {
-    let source = src;
-    let filename = targetFilename;
+const processFile = (src, targetFilename, config, processOptions) => {
+  let source = src;
+  let filename = targetFilename;
 
-    if (isPackageFile(filename)) {
-      filename = targetFilename.replace("dist", "src");
-      source = readSource(filename);
-    }
+  if (isPackageFile(filename)) {
+    filename = targetFilename.replace("dist", "src");
+    source = readSource(filename);
+  }
 
-    return babelJest.process(source, filename, config, processOptions);
-  },
+  return babelJest.process(source, filename, config, processOptions);
+};
+
+export {
+  getCacheKey,
+  processFile as process,
 };
