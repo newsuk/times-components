869293bebfaec696831a54e6b92a721a340350c0
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-04-19 14:18:22 +0100

    feat: adds star icon and updates snapshots

--- packages/icons/src/icons/star.js
+++ packages/icons/src/icons/star.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { colours } from "@times-components/styleguide";
 import { clean } from "@times-components/utils";
 import Svg, { Polygon } from "svgs";
 import iconPropTypes from "./proptypes";
@@ -7,13 +6,14 @@ import iconPropTypes from "./proptypes";
 const viewBox =
   "11.756953239440918 11.182208061218262 17.48609161376953 16.630264282226562";
 
-const IconStar = ({ title, width, height, fillColour, strokeColour }) => (
-  <Svg viewBox={viewBox} {...clean({ title, height, width: width || height })}>
+const ratio = 2 / 3;
+const IconStar = ({ title, width, height, fillColour }) => (
+  <Svg
+    viewBox={viewBox}
+    {...clean({ title, height, width: width || height * ratio })}
+  >
     <Polygon
-      {...clean({
-        stroke: strokeColour,
-        fill: fillColour
-      })}
+      fill={fillColour}
       points="20.5 24.9716827 15.0965 27.8124708 16.1284766 21.7955851 11.7569533 17.5344029 17.79825 16.6565511 20.5 11.1822077 23.20175 16.6565511 29.2430467 17.5344029 24.8715234 21.7955851 25.9035 27.8124708"
     />
   </Svg>
@@ -22,7 +22,7 @@ const IconStar = ({ title, width, height, fillColour, strokeColour }) => (
 IconStar.propTypes = iconPropTypes;
 
 IconStar.defaultProps = {
-  fillColour: colours.functional.brandColour
+  fillColour: "#000000"
 };
 
 export default IconStar;
