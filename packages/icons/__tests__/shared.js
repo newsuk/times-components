2b3c72d600c2498e41c547ebd513b94187e6fe5e
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-04-19 14:18:22 +0100

    feat: adds star icon and updates snapshots

--- packages/icons/__tests__/shared.js
+++ packages/icons/__tests__/shared.js
@@ -4,7 +4,13 @@ import "react-native";
 import React from "react";
 import renderer from "react-test-renderer";
 import { colours } from "@times-components/styleguide";
-import { IconDiamond, IconTwitter, IconVideo } from "../src/icons";
+import {
+  IconDiamond,
+  IconTwitter,
+  IconVideo,
+  IconEmail,
+  IconStar
+} from "../src/icons";
 
 module.exports = () => {
   it("Diamond renders correctly", () => {
@@ -52,6 +58,34 @@ module.exports = () => {
   it("Video renders correctly with different fillColour", () => {
     const tree = renderer
       .create(<IconVideo width={50} height={50} fillColour="#4D4D4D" />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Email renders correctly", () => {
+    const tree = renderer.create(<IconEmail width={50} height={50} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Email renders correctly with different fillColour", () => {
+    const tree = renderer
+      .create(<IconEmail width={50} height={50} fillColour="#4D4D4D" />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Star renders correctly", () => {
+    const tree = renderer.create(<IconStar width={50} height={50} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Star renders correctly with different fillColour", () => {
+    const tree = renderer
+      .create(<IconStar width={50} height={50} fillColour="#4D4D4D" />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
