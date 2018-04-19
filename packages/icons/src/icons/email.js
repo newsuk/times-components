dd836f94fa4b8ec2464453a7c6a6599451c11fdb
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-04-19 15:53:47 +0100

    chore: cleans up icon tests

--- packages/icons/src/icons/email.js
+++ packages/icons/src/icons/email.js
@@ -6,12 +6,12 @@ import iconPropTypes from "./proptypes";
 const viewBox = "0 0 22 16";
 
 const ratio = 22 / 16;
-const IconEmail = ({ title, width, height, fillColour }) => (
+const IconEmail = ({ title, width, height, fillColour, strokeColour }) => (
   <Svg
     viewBox={viewBox}
     {...clean({ title, height, width: width || height * ratio })}
   >
-    <G fill={fillColour} fillRule="nonzero">
+    <G fill={fillColour} fillRule="nonzero" stroke={strokeColour}>
       <Path d="M22,0 L22,16 L0,16 L0,0 L22,0 Z M13.5135,8 L20.2711667,2.23985068 L20.0346667,1.91880541 L11,9.20018665 L2.04233333,2.00093327 L1.80583333,2.32197853 L8.56533333,8.08026132 L1.80766667,13.8404106 L2.04233333,14.1595894 L9.0365,8.47970135 L11,10.1595894 L13.0423333,8.39944004 L20.0346667,14.079328 L20.2711667,13.7582828 L13.5135,8 Z" />
     </G>
   </Svg>
@@ -20,6 +20,7 @@ const IconEmail = ({ title, width, height, fillColour }) => (
 IconEmail.propTypes = iconPropTypes;
 
 IconEmail.defaultProps = {
+  strokeColour: "#ffffff",
   fillColour: "#000000"
 };
 
