ba68e77998648df26853cd1c1d06327b019a4df1
NehaSrivastava <neha.srivastava84525@gmail.com>
2018-04-27 16:39:31 +0100

    Rename the mocks to real components

--- packages/article/__tests__/web/article.web.test.js
+++ packages/article/__tests__/web/article.web.test.js
@@ -11,21 +11,21 @@ import sharedTracking from "../shared-tracking";
 
 import articleFixtureNoLeadAsset from "../../fixtures/no-lead-asset.json";
 
-jest.mock("@times-components/article-byline", () => "MockArticleByline");
+jest.mock("@times-components/article-byline", () => "ArticleByline");
 jest.mock("@times-components/article-flag", () => ({
-  ExclusiveArticleFlag: "MockExclusiveArticleFlag",
-  NewArticleFlag: "MockNewArticleFlag",
-  UpdatedArticleFlag: "MockUpdatedArticleFlag",
-  SponsoredArticleFlag: "MockSponsoredArticleFlag"
+  ExclusiveArticleFlag: "ExclusiveArticleFlag",
+  NewArticleFlag: "NewArticleFlag",
+  UpdatedArticleFlag: "UpdatedArticleFlag",
+  SponsoredArticleFlag: "SponsoredArticleFlag"
 }));
-jest.mock("@times-components/article-label", () => "MockArticleLabel");
-jest.mock("@times-components/video-label", () => "MockVideoLabel");
-jest.mock("@times-components/brightcove-video", () => "MockBrightcoveVideo");
-jest.mock("@times-components/article-image", () => "MockArticleImage");
-jest.mock("@times-components/pull-quote", () => "MockPullQuote");
-jest.mock("@times-components/related-articles", () => "MockRelatedArticles");
-jest.mock("@times-components/topics", () => "MockTopics");
-jest.mock("@times-components/watermark", () => "MockWatermark");
+jest.mock("@times-components/article-label", () => "ArticleLabel");
+jest.mock("@times-components/video-label", () => "VideoLabel");
+jest.mock("@times-components/brightcove-video", () => "BrightcoveVideo");
+jest.mock("@times-components/article-image", () => "ArticleImage");
+jest.mock("@times-components/pull-quote", () => "PullQuote");
+jest.mock("@times-components/related-articles", () => "RelatedArticles");
+jest.mock("@times-components/topics", () => "Topics");
+jest.mock("@times-components/watermark", () => "Watermark");
 jest.mock("@times-components/tracking", () => {
   const mockTracking = component => component;
   return {
