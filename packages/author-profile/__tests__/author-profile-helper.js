08d4322decef40e06830aee50603b8d5f86f37ae
colincclark <col.c.clark@gmail.com>
2018-05-03 10:16:01 +0100

    test: uncomment and fix tests

--- packages/author-profile/__tests__/author-profile-helper.js
+++ packages/author-profile/__tests__/author-profile-helper.js
@@ -345,54 +345,58 @@ export default AuthorProfileListContent => {
     });
   });
 
-  // it("calls refetch when retrying from author error", done => {
-  //   const wrapper = shallow(
-  //     <AuthorProfile
-  //       {...authorProfileProps}
-  //       author={null}
-  //       refetch={done}
-  //       error={new Error("It went wrong")}
-  //       isLoading={false}
-  //       slug="deborah-haynes"
-  //       page={1}
-  //       pageSize={3}
-  //     />
-  //   );
+  it("calls refetch when retrying from author error", done => {
+    const wrapper = shallow(
+      <AuthorProfile
+        {...authorProfileProps}
+        author={null}
+        refetch={done}
+        error={new Error("It went wrong")}
+        isLoading={false}
+        slug="deborah-haynes"
+        page={1}
+        pageSize={3}
+      />
+    );
 
-  //   const authProfileError = wrapper.dive().dive();
-  //   expect(authProfileError.type().name).toEqual("AuthorProfileError");
+    const authProfileListPageError = wrapper.dive().dive();
+    expect(authProfileListPageError.type().name).toEqual(
+      "AuthorProfileListPageError"
+    );
 
-  //   authProfileError
-  //     .dive()
-  //     .dive()
-  //     .find("TouchableOpacity")
-  //     .simulate("press");
-  // });
+    authProfileListPageError
+      .dive()
+      .dive()
+      .find("AuthorProfileListError")
+      .dive()
+      .find("TouchableOpacity")
+      .simulate("press");
+  });
 
-  // it("calls refetch when retrying from articles error", done => {
-  //   const wrapper = shallow(
-  //     <AuthorProfileListContent
-  //       count={0}
-  //       articles={[]}
-  //       author={fixtureGenerator.makeAuthor()}
-  //       slug="deborah-haynes"
-  //       page={1}
-  //       pageSize={3}
-  //       imageRatio={3 / 2}
-  //       error={new Error("Failed")}
-  //       refetch={done}
-  //       onTwitterLinkPress={() => {}}
-  //       onArticlePress={() => {}}
-  //       onViewed={() => {}}
-  //     />
-  //   );
+  it("calls refetch when retrying from articles error", done => {
+    const wrapper = shallow(
+      <AuthorProfileListContent
+        count={0}
+        articles={[]}
+        author={fixtureGenerator.makeAuthor()}
+        slug="deborah-haynes"
+        page={1}
+        pageSize={3}
+        imageRatio={3 / 2}
+        error={new Error("Failed")}
+        refetch={done}
+        onTwitterLinkPress={() => {}}
+        onArticlePress={() => {}}
+        onViewed={() => {}}
+      />
+    );
 
-  //   wrapper
-  //     .dive()
-  //     .dive()
-  //     .find("AuthorProfileListError")
-  //     .dive()
-  //     .find("TouchableOpacity")
-  //     .simulate("press");
-  // });
+    wrapper
+      .dive()
+      .dive()
+      .find("AuthorProfileListError")
+      .dive()
+      .find("TouchableOpacity")
+      .simulate("press");
+  });
 };
