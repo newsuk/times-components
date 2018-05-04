156f181ec745abb172b8248ac56b28d0c577c259
kaliabadi <kouros_aliabadi@hotmail.com>
2018-05-04 11:17:27 +0100

    chore: prettier for fructose index file

--- fructose/index.js
+++ fructose/index.js
@@ -3,6 +3,8 @@ import Fructose from "@times-components/fructose";
 // eslint-disable-next-line import/no-unresolved
 import { getStories } from "./components";
 
-AppRegistry.registerComponent("storybooknative", () => Fructose(getStories, { platform: "native" }));
+AppRegistry.registerComponent("storybooknative", () =>
+  Fructose(getStories, { platform: "native" })
+);
 
 export default Fructose(getStories);
--- fructose/index.web.js
+++ fructose/index.web.js
@@ -4,7 +4,9 @@ import fructose from "@times-components/fructose";
 // eslint-disable-next-line import/no-unresolved
 import { getStories } from "./components";
 
-AppRegistry.registerComponent("e2eTests", () => fructose(getStories, { platform: "web" }));
+AppRegistry.registerComponent("e2eTests", () =>
+  fructose(getStories, { platform: "web" })
+);
 AppRegistry.runApplication("e2eTests", {
   rootTag: document.getElementById("react-root")
 });
