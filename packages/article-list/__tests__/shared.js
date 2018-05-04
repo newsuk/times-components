8743a0b2c1dae444f0add42c9c75fcb15e70a057
colincclark <col.c.clark@gmail.com>
2018-05-04 11:02:12 +0100

    refactor: start tidying up article list tests

--- packages/article-list/__tests__/shared.js
+++ packages/article-list/__tests__/shared.js
@@ -1,12 +1,37 @@
 import "react-native";
 import React from "react";
-import renderer from "react-test-renderer";
-import ArticleList from "../src/article-list";
+import { shallow } from "enzyme";
+import ArticleListError from "../src/article-list-error";
+import ArticleListItemSeparator from "../src/article-list-item-separator";
+import ArticleListPageError from "../src/article-list-page-error";
 
 export default () => {
-  it("renders correctly", () => {
-    const tree = renderer.create(<ArticleList />).toJSON();
+  it("should render the error component correctly", () => {
+    const refetchMock = jest.fn();
+    const wrapper = shallow(<ArticleListError refetch={refetchMock} />);
 
-    expect(tree).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should handle the retry button in the error component correctly", () => {
+    const refetchMock = jest.fn();
+    const wrapper = shallow(<ArticleListError refetch={refetchMock} />);
+
+    wrapper.find("TouchableOpacity").simulate("press");
+
+    expect(refetchMock).toHaveBeenCalled();
+  });
+
+  it("should render the separator keyline correctly", () => {
+    const wrapper = shallow(<ArticleListItemSeparator />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render the article page error correctly", () => {
+    const refetchMock = jest.fn();
+    const wrapper = shallow(<ArticleListPageError refetch={refetchMock} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 };
