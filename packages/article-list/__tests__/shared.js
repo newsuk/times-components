c2ea854b483159b078b7b195a2b82160a399f404
colincclark <col.c.clark@gmail.com>
2018-05-04 11:22:07 +0100

    refactor: refactor article list pagination test

--- packages/article-list/__tests__/shared.js
+++ packages/article-list/__tests__/shared.js
@@ -1,19 +1,21 @@
 import "react-native";
 import React from "react";
+import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import ArticleListError from "../src/article-list-error";
 import ArticleListItemSeparator from "../src/article-list-item-separator";
 import ArticleListPageError from "../src/article-list-page-error";
+import ArticleListPagination from "../src/article-list-pagination";
 
 export default () => {
-  it("should render the error component correctly", () => {
+  it("should render an article list page error correctly", () => {
     const refetchMock = jest.fn();
     const wrapper = shallow(<ArticleListError refetch={refetchMock} />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should handle the retry button in the error component correctly", () => {
+  it("should handle the retry button in the article list page error correctly", () => {
     const refetchMock = jest.fn();
     const wrapper = shallow(<ArticleListError refetch={refetchMock} />);
 
@@ -22,16 +24,23 @@ export default () => {
     expect(refetchMock).toHaveBeenCalled();
   });
 
-  it("should render the separator keyline correctly", () => {
+  it("should render the article list item separator keyline correctly", () => {
     const wrapper = shallow(<ArticleListItemSeparator />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should render the article page error correctly", () => {
-    const refetchMock = jest.fn();
-    const wrapper = shallow(<ArticleListPageError refetch={refetchMock} />);
+  it("should render the article list page error correctly", () => {
+    const wrapper = shallow(<ArticleListPageError refetch={jest.fn()} />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render the article list pagination correctly", () => {
+    const tree = renderer.create(
+      <ArticleListPagination count={20} page={1} pageSize={10} />
+    );
+
+    expect(tree).toMatchSnapshot();
   });
 };
