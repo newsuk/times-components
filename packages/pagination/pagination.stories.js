704cd934f7b612241737bad4828e95a3cf3dc8e5
kaliabadi <kouros_aliabadi@hotmail.com>
2018-05-04 11:11:08 +0100

    fix: fixing storybook

--- packages/pagination/pagination.stories.js
+++ packages/pagination/pagination.stories.js
@@ -1,5 +1,5 @@
 import { showcaseConverter } from "@times-components/storybook";
-import { pagination, helper, icons } from "./pagination.showcase";
+import pagination, { helper, icons } from "./pagination.showcase";
 
 showcaseConverter(module, pagination);
 showcaseConverter(module, helper);
