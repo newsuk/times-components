831335f5b02e606a77b8e43d2019f154d5cfd589
milesillsley <milesillsley@hotmail.co.uk>
2018-05-14 14:28:14 +0100

    prettier uploadExpoQRCode.js

--- uploadExpoQRCode.js
+++ uploadExpoQRCode.js
@@ -1,31 +1,34 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
+const fs = require("fs");
+const AWS = require("aws-sdk");
 
 const args = process.argv.slice(2);
-const [imagePath,bucket, accessKeyId, secretAccessKey]  = args;
-AWS.config.update({ region: 'eu-west-1', accessKeyId, secretAccessKey });
+const [imagePath, bucket, accessKeyId, secretAccessKey] = args;
+AWS.config.update({
+  region: "eu-west-1",
+  accessKeyId,
+  secretAccessKey
+});
 const s3 = new AWS.S3();
 
-const file = fs.readFileSync(imagePath)
+const file = fs.readFileSync(imagePath);
 
-const uploadQRcode = (qrcode) => {
-    const uploadParams = {
-        Bucket: bucket,
-        Key: imagePath,
-        Body: qrcode,
-        ContentType: 'image/png',
-        ACL: 'public-read'
-    };
+const uploadQRcode = qrcode => {
+  const uploadParams = {
+    Bucket: bucket,
+    Key: imagePath,
+    Body: qrcode,
+    ContentType: "image/png",
+    ACL: "public-read"
+  };
 
-    s3.upload(uploadParams, (err, data) => {
-        if (err) {
-            console.log('upload qrcode error:', err); // eslint-disable-line no-console
-        }
-        if (data) {
-            console.log('upload qrcode success:', data.Key); // eslint-disable-line no-console
-        }
-    });
-
+  s3.upload(uploadParams, (err, data) => {
+    if (err) {
+      console.log("upload qrcode error:", err); // eslint-disable-line no-console
+    }
+    if (data) {
+      console.log("upload qrcode success:", data.Key); // eslint-disable-line no-console
+    }
+  });
 };
 
 uploadQRcode(file);
