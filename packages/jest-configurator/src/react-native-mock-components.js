eeba7acc5093c352d8c58981fc38306baab147fb
James Miller <james@james-miller.co.uk>
2018-05-14 17:30:54 +0100

    fix: Linting and formatting

--- packages/jest-configurator/src/react-native-mock-components.js
+++ packages/jest-configurator/src/react-native-mock-components.js
@@ -1,17 +1,16 @@
 import React from "react";
-import jest from "jest";
 
-  const mockReactNativeComponent = componentName => {
-      const RealComponent = require.requireActual(componentName);
-      const MockComponent = props =>
-        React.createElement(
-          componentName,
-          Object.assign({}, props, { style: null }),
-          props.children
-        );
+const mockReactNativeComponent = componentName => {
+  const RealComponent = require.requireActual(componentName);
+  const MockComponent = props =>
+    React.createElement(
+      componentName,
+      Object.assign({}, props, { style: null }),
+      props.children
+    );
 
-      MockComponent.propTypes = RealComponent.propTypes;
-      return MockComponent;
-    }
+  MockComponent.propTypes = RealComponent.propTypes;
+  return MockComponent;
+};
 
 export default mockReactNativeComponent;
