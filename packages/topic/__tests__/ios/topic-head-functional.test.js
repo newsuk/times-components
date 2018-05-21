432891d5d5680c35e9b26df88d3d68f197a49a0c
colincclark <col.c.clark@gmail.com>
2018-05-21 12:04:56 +0100

    refactor: redo tests

--- packages/topic/__tests__/ios/topic-head-functional.test.js
+++ packages/topic/__tests__/ios/topic-head-functional.test.js
@@ -2,8 +2,5 @@ import { mockReactNativeComponent } from "@times-components/jest-configurator";
 import sharedFunctional from "../topic-head-functional";
 
 describe("TopicHead functional tests on ios", () => {
-  jest.mock("Text", () => mockReactNativeComponent("Text"));
-  jest.mock("View", () => mockReactNativeComponent("View"));
-
   sharedFunctional();
 });
