8d1373ddd79408258722b6f78c7aa12575d9d026
colincclark <col.c.clark@gmail.com>
2018-05-25 14:02:41 +0100

    chore: destruct Component from React, not PropTypes, doh!

--- packages/gradient/src/gradient.js
+++ packages/gradient/src/gradient.js
@@ -1,5 +1,5 @@
-import PropTypes, { Component } from "prop-types";
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { StyleSheet, View, ViewPropTypes, ART } from "react-native";
 import { colours } from "@times-components/styleguide";
 
