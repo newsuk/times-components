6612f27b6ebcf41d7b0367c93060593d955c3af2
Colin Clark <col.c.clark@gmail.com>
2018-05-25 16:14:00 +0100

    chore: stop coverage collection of provider package showcase file (#969)
    * chore: remove jest code in provider package.json to ensure it uses jest-configurator package
    * refactor: add jest config to provider pacakge
    * chore: update test script in provider package for jest config
    * refactor: use jest configurator package, so refactored the debounce tests

---
+++ packages/provider/__tests__/web/debounce.web.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { mount } from "enzyme";
+import Inner from "../inner";
+import withDebounce from "../../src/debounce";
+
+jest.useFakeTimers();
+
+// Alias the confusingly misnamed runTimersToTime to advanceTimersByTime
+// Jest has done this in v22, so this can be removed if we upgrade
+jest.advanceTimersByTime = jest.runTimersToTime;
+
+describe("Debounce web test", () => {
+  it("updates debounceProps once for many closely spaced props updates", () => {
+    const Outer = withDebounce(Inner);
+    const component = mount(<Outer foo="initialFoo" debounceTimeMs={1000} />);
+
+    const numberOfDebouncedPropsUpdates = () =>
+      component
+        .update()
+        .find("Inner")
+        .instance().numberOfDebouncedPropsUpdates;
+
+    expect(numberOfDebouncedPropsUpdates()).toEqual(0);
+
+    component.setProps({ foo: "updatedFoo1" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo2" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo3" });
+    jest.advanceTimersByTime(600);
+    component.setProps({ foo: "updatedFoo4" });
+    jest.advanceTimersByTime(1200);
+
+    expect(numberOfDebouncedPropsUpdates()).toEqual(1);
+  });
+});
