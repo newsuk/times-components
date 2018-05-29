dbcfafa478c7390c599a5ca3aad79245aa25ecf2
Colin Clark <col.c.clark@gmail.com>
2018-05-29 10:47:45 +0100

    feat: removal of author head package (#971)

--- packages/author-head/src/author-photo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AuthorPhotoBase from "./author-photo.base";
-
-const AuthorPhoto = props => (
-  <AuthorPhotoBase {...props} style={{ width: 100, borderRadius: 50 }} />
-);
-
-export default AuthorPhoto;
