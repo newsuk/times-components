f85294454345f9f07aaa035c0b7dc3b1d982838b
colincclark <col.c.clark@gmail.com>
2018-05-30 09:59:18 +0100

    feat: new android button with drop shadow now working

--- packages/button-primary/src/button-primary.android.js
+++ packages/button-primary/src/button-primary.android.js
@@ -1,41 +1,49 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import cleanUpTitle from "../utils";
+import {
+  colours,
+  fonts,
+  fontSizes
+} from "@times-components/styleguide";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  button: {
     alignItems: 'center',
-    backgroundColor: '#FFFFFF'
-  },
-  textStyle: {
-    color: '#FFFFFF'
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    height: 36,
+    justifyContent: 'center',
+    width: "100%"
   },
   buttonContainer: {
-    backgroundColor: '#2E9298',
-    borderRadius: 10,
-    padding: 10,
+    alignItems: 'center',
+    backgroundColor: colours.functional.action,
+    borderRadius: 4,
+    elevation: 5,
+    height: 36,
+    justifyContent: 'center',
     shadowColor: '#000000',
     shadowOffset: {
       width: 0,
       height: 3
     },
-    shadowRadius: 5,
-    shadowOpacity: 1.0
+    shadowOpacity: 1.0,
+    shadowRadius: 5
+  },
+  textStyle: {
+    color: '#FFFFFF'
   }
 });
 
 const ButtonPrimary = ({ onPress, style, title }) => {
   const cleanTitle = cleanUpTitle(title);
   return (
-    <View style={styles.container}>
-      <View elevation={5} style={styles.buttonContainer}>
-        <TouchableOpacity accessible accessibilityLabel={cleanTitle} onPress={onPress}>
+    <TouchableOpacity accessible accessibilityLabel={cleanTitle} onPress={onPress} style={styles.button}>
+      <View style={[styles.buttonContainer, style]}>
           <Text style={styles.textStyle}>{cleanTitle}</Text>
-        </TouchableOpacity>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
