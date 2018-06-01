14f5e20218198a594b9263d1f76d3c7098763cfd
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2018-06-01 17:19:07 +0100

    fix: not call end reached before load (#987)

--- packages/article-list/src/article-list.js
+++ packages/article-list/src/article-list.js
@@ -122,7 +122,10 @@ class ArticleList extends Component {
         onViewableItemsChanged={this.onViewableItemsChanged}
         pageSize={pageSize}
         onEndReachedThreshold={2}
-        onEndReached={fetchMoreOnEndReached}
+        onEndReached={() =>
+          // Workaround for iOS Flatlist bug (https://github.com/facebook/react-native/issues/16067)
+          data.length > 0 ? fetchMoreOnEndReached() : null
+        }
         renderItem={({ item, index }) => (
           <ErrorView>
             {({ hasError }) =>
