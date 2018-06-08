b2b4f75a74b2f9c9447dcd22814e7a6ba55474ca
Aron Shamash <aronshamash@gmail.com>
2018-06-08 14:15:18 +0100

    fix: lint error

--- packages/article-byline/article-byline-with-links.showcase.js
+++ packages/article-byline/article-byline-with-links.showcase.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 
 import React from "react";
-import { Text } from "react-native";
 import { colours } from "@times-components/styleguide";
 import { ArticleBylineWithLinks } from "./src/article-byline";
 
