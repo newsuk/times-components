a36dae3540e0274985e2313be96141c0d938a346
Daniele Conti <daniele.conti@novoda.com>
2018-06-12 14:54:51 +0100

    Accept ART Path instances in d prop

--- packages/svgs/src/path.js
+++ packages/svgs/src/path.js
@@ -2,36 +2,39 @@ import React from "react";
 import { ART } from "react-native";
 import PropTypes from "prop-types";
 
-const { Shape } = ART;
+const { Shape, Path: ARTPath } = ART;
 
 const Path = ({ d, fill, stroke, strokeWidth, opacity }) => {
-  const artStroke = stroke === "none" ? undefined : stroke;
-  const artFill = fill === "none" ? undefined : fill;
+    const artStroke = stroke === "none" ? undefined : stroke;
+    const artFill = fill === "none" ? undefined : fill;
 
-  return (
-    <Shape
-      fill={artFill}
-      stroke={artStroke}
-      strokeWidth={strokeWidth}
-      d={d}
-      opacity={opacity}
-    />
-  );
+    return (
+        <Shape
+            fill={artFill}
+            stroke={artStroke}
+            strokeWidth={strokeWidth}
+            d={d}
+            opacity={opacity}
+        />
+    );
 };
 
 Path.propTypes = {
-  d: PropTypes.string.isRequired,
-  fill: PropTypes.string,
-  stroke: PropTypes.string,
-  strokeWidth: PropTypes.string,
-  opacity: PropTypes.string
+    d: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.instanceOf(ARTPath)
+    ]).isRequired,
+    fill: PropTypes.string,
+    stroke: PropTypes.string,
+    strokeWidth: PropTypes.string,
+    opacity: PropTypes.string
 };
 
 Path.defaultProps = {
-  fill: undefined,
-  stroke: undefined,
-  strokeWidth: undefined,
-  opacity: undefined
+    fill: undefined,
+    stroke: undefined,
+    strokeWidth: undefined,
+    opacity: undefined
 };
 
 export default Path;
