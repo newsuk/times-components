33828dc6e029d1531b1b3c6065392e6e5004f765
Daniele Conti <daniele.conti@novoda.com>
2018-06-12 15:21:25 +0100

    Reformat file

--- packages/svgs/src/path.js
+++ packages/svgs/src/path.js
@@ -5,36 +5,34 @@ import PropTypes from "prop-types";
 const { Shape, Path: ARTPath } = ART;
 
 const Path = ({ d, fill, stroke, strokeWidth, opacity }) => {
-    const artStroke = stroke === "none" ? undefined : stroke;
-    const artFill = fill === "none" ? undefined : fill;
+  const artStroke = stroke === "none" ? undefined : stroke;
+  const artFill = fill === "none" ? undefined : fill;
 
-    return (
-        <Shape
-            fill={artFill}
-            stroke={artStroke}
-            strokeWidth={strokeWidth}
-            d={d}
-            opacity={opacity}
-        />
-    );
+  return (
+    <Shape
+      fill={artFill}
+      stroke={artStroke}
+      strokeWidth={strokeWidth}
+      d={d}
+      opacity={opacity}
+    />
+  );
 };
 
 Path.propTypes = {
-    d: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.instanceOf(ARTPath)
-    ]).isRequired,
-    fill: PropTypes.string,
-    stroke: PropTypes.string,
-    strokeWidth: PropTypes.string,
-    opacity: PropTypes.string
+  d: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(ARTPath)])
+    .isRequired,
+  fill: PropTypes.string,
+  stroke: PropTypes.string,
+  strokeWidth: PropTypes.string,
+  opacity: PropTypes.string
 };
 
 Path.defaultProps = {
-    fill: undefined,
-    stroke: undefined,
-    strokeWidth: undefined,
-    opacity: undefined
+  fill: undefined,
+  stroke: undefined,
+  strokeWidth: undefined,
+  opacity: undefined
 };
 
 export default Path;
