81deb3378c073ea407ec4a95f54b5b55c03e3ff9
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-06-15 13:59:53 +0100

    Fix ios style

--- packages/video-label/src/style/index.js
+++ packages/video-label/src/style/index.js
@@ -1,3 +1,16 @@
-import styles from "./shared";
+import sharedStyles from "./shared";
+
+const styles = {
+  title: {
+    ...sharedStyles.title
+  },
+  iconContainer: {
+    ...sharedStyles.iconContainer,
+    paddingBottom: 2
+  },
+  icon: {
+    ...sharedStyles.icon
+  }
+};
 
 export default styles;
