d4b22a2ffaf21afbb8a0b4c806ae289e5e8432f1
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-06-18 16:42:17 +0100

    Use button for linking to homepage

--- packages/article-list/src/article-list-empty-state.web.js
+++ packages/article-list/src/article-list-empty-state.web.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { TextLink } from "@times-components/link";
+import Button from "@times-components/button";
+import Link from "@times-components/link";
 import Watermark from "@times-components/watermark";
 import styles from "./styles";
 
@@ -9,14 +10,11 @@ const ArticleListEmptyState = () => (
     <Text style={styles.listEmptyMessage}>
       Unfortunately, there are no articles relating to this topic
     </Text>
-    <TextLink
-      onPress={() => {}}
-      url="https://thetimes.co.uk"
-      style={styles.listEmptyBackButton}
-      title="Back to home"
-    >
-      Back to home
-    </TextLink>
+    <View style={styles.listEmptyBackButton}>
+      <Link onPress={() => {}} url="https://thetimes.co.uk">
+        <Button onPress={() => {}} title="Back to home" />
+      </Link>
+    </View>
     <View style={styles.listEmptyWatermark}>
       <Watermark width={500} height={350} viewBox="0 0 350 200" />
     </View>
