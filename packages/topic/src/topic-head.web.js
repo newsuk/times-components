d853651ca71960e966cfb3b1990fabda5ee824db
James Miller <james@james-miller.co.uk>
2018-06-26 17:32:12 +0100

    chore: Formatting

--- packages/topic/src/topic-head.web.js
+++ packages/topic/src/topic-head.web.js
@@ -28,10 +28,7 @@ const TopicHead = ({ name, description, isLoading }) => {
   ) : (
     <View style={styles.wrapper}>
       <HeadContainer>
-        <ResponsiveName
-          testID="topic-name"
-          accessibilityRole="heading"
-        >
+        <ResponsiveName testID="topic-name" accessibilityRole="heading">
           {name}
         </ResponsiveName>
         {showDescription()}
