620bccd14c2cbcb1ed00ab14014c4f0fab60cd41
colincclark <col.c.clark@gmail.com>
2018-06-27 12:24:20 +0100

    refactor: rename viewbox

--- packages/ad/__tests__/ad-placeholder.shared.js
+++ packages/ad/__tests__/ad-placeholder.shared.js
@@ -12,35 +12,35 @@ export default () => {
     expect(testInstance).toMatchSnapshot("1. Advert placeholder");
   });
 
-  it("should return a small viewbox config", () => {
+  it("should return a small ViewBox config", () => {
     const height = 90;
     const width = 728;
     expect(calculateViewBox({ height, width })).toMatchSnapshot(
-      "2. Small viewbox config"
+      "2. Small ViewBox config"
     );
   });
 
-  it("should return a mpu viewbox config", () => {
+  it("should return a mpu ViewBox config", () => {
     const height = 250;
     const width = 300;
     expect(calculateViewBox({ height, width })).toMatchSnapshot(
-      "3. MPU viewbox config"
+      "3. MPU ViewBox config"
     );
   });
 
-  it("should return a billboard viewbox config", () => {
+  it("should return a billboard ViewBox config", () => {
     const height = 250;
     const width = 970;
     expect(calculateViewBox({ height, width })).toMatchSnapshot(
-      "3. Billboard viewbox config"
+      "3. Billboard ViewBox config"
     );
   });
 
-  it("should return a default viewbox config", () => {
+  it("should return a default ViewBox config", () => {
     const height = 50;
     const width = 700;
     expect(calculateViewBox({ height, width })).toMatchSnapshot(
-      "4. Default viewbox config"
+      "4. Default ViewBox config"
     );
   });
 };
