62e044fb3780bb44a122cd3370f29b65b916701d
colincclark <col.c.clark@gmail.com>
2018-07-11 10:11:29 +0100

    test: re-add more props in tests

--- packages/watermark/__tests__/shared.js
+++ packages/watermark/__tests__/shared.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { createHash } from "crypto";
 import {
   addSerializers,
   compose,
   enzymeTreeSerializer,
   minimaliseTransform,
   minimalNativeTransform,
-  print
+  print,
+  replacePropTransform
 } from "@times-components/jest-serializer";
 import Watermark from "../src/watermark";
 
 export default () => {
+  const hash = v =>
+    createHash("md5")
+      .update(v)
+      .digest("hex");
+
   addSerializers(
     expect,
     enzymeTreeSerializer(),
     compose(
       print,
       minimaliseTransform(
-        (value, key) => key === "d" || key === "id" || key === "source"
+        (value, key) => key === "source"
       ),
-      minimalNativeTransform
+      minimalNativeTransform,
+      replacePropTransform((value, key) => (key === "d" ? hash(value) : value))
     )
   );
 
