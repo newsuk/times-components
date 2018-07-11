9b77bd570679aff39f48b26b872c65ae1b0b84e5
colincclark <col.c.clark@gmail.com>
2018-07-11 21:47:20 +0100

    refactor: improve snaps

--- packages/video/__tests__/web/video-error.web.test.js
+++ packages/video/__tests__/web/video-error.web.test.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import {
   addSerializers,
   compose,
-  enzymeTreeSerializer,
+  enzymeRenderedSerializer,
   minimaliseTransform,
-  print
+  minimalWebTransform,
+  print,
+  rnwTransform
 } from "@times-components/jest-serializer";
 import VideoError from "../../src/video-error";
 
 addSerializers(
   expect,
-  enzymeTreeSerializer(),
-  compose(print, minimaliseTransform((value, key) => key === "style"))
+  enzymeRenderedSerializer(),
+  compose(
+    print,
+    minimalWebTransform,
+    minimaliseTransform((value, key) => key === "style" || key === "className"),
+    rnwTransform()
+  )
 );
 
 it("1. video with error", () => {
-  const wrapper = shallow(<VideoError height={100} width={100} />);
+  const wrapper = mount(<VideoError height={100} width={100} />);
   expect(wrapper).toMatchSnapshot();
 });
