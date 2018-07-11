93c435948f80045a1acc528132836e53332bf83c
colincclark <col.c.clark@gmail.com>
2018-07-11 14:04:19 +0100

    refactor: ignore touchable native code in web on video

--- packages/video/__tests__/web/jest.config.js
+++ packages/video/__tests__/web/jest.config.js
@@ -1,3 +1,5 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
 
-module.exports = jestConfigurator("web", __dirname);
+module.exports = jestConfigurator("web", __dirname, {
+  coverageIgnoreGlobs: ["touchable.js"]
+});
