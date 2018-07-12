6da3c1a17c3710d309f386abc0d1fbf0339ad803
colincclark <col.c.clark@gmail.com>
2018-07-12 12:02:31 +0100

    feat: add ast handling to key facts and relevant fixrture data

--- packages/key-facts/src/key-facts.js
+++ packages/key-facts/src/key-facts.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { TextLink } from "@times-components/link";
+import { renderTree } from "@times-components/markup";
 import styles from "./styles";
 
 const KeyFacts = ({ data }) => {
   const { data: keyFactsData } = data;
-  const renderKeyFact = (keyFact, index) => (
-    <View key={index} style={styles.container}>
-      <View style={styles.bullet} />
-      <Text style={styles.text}>
-        dskjhf sdjfh sdkjfskfhdskfsdkfdskjfdskj fsd fsdkf sdjf sdk hsk
-      </Text>
-    </View>
-  );
+  const renderKeyFact = (keyFact, index) => {
+    const { children } = keyFact;
+    return (
+      <View key={index} style={styles.container}>
+        <View style={styles.bullet} />
+        <Text style={styles.text}>
+          {children.map(ast =>
+            renderTree(ast, {
+              link(key, attributes, children) {
+                const url = attributes.href;
+                return (
+                  <TextLink
+                    key={index}
+                    onPress={e =>
+                      onLinkPress(e, {
+                        url: attributes.href,
+                        type: attributes.type,
+                        canonicalId: attributes.canonicalId
+                      })
+                    }
+                    url={url}
+                  >
+                    {children}
+                  </TextLink>
+                );
+              }
+            })
+          )}
+        </Text>
+      </View>
+    );
+  };
 
   return keyFactsData.map((keyFact, index) => renderKeyFact(keyFact, index));
 };
