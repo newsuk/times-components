e24ffbde441cb25838912d2ee996535cb9d3e6ff
colincclark <col.c.clark@gmail.com>
2018-07-12 11:29:47 +0100

    refactor: amend spacing below items

--- packages/key-facts/src/styles/index.android.js
+++ packages/key-facts/src/styles/index.android.js
@@ -2,11 +2,7 @@ import { StyleSheet } from "react-native";
 import sharedStyles from "./shared";
 
 const styles = StyleSheet.create({
-  ...sharedStyles,
-  KeyFactsBody: {
-    ...sharedStyles.KeyFactsBody,
-    color: "green"
-  }
+  ...sharedStyles
 });
 
 export default styles;
