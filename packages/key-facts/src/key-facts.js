5c55a93358e9fea519314eb2f7d15d90a5492de3
colincclark <col.c.clark@gmail.com>
2018-07-12 13:41:32 +0100

    refactor: latest story

--- packages/key-facts/src/key-facts.js
+++ packages/key-facts/src/key-facts.js
@@ -6,38 +6,35 @@ import styles from "./styles";
 
 const KeyFacts = ({ data }) => {
   const { data: keyFactsData } = data;
-  const renderKeyFact = (keyFact, index) => {
-    const { children } = keyFact;
-    return (
-      <View key={index} style={styles.container}>
-        <View style={styles.bullet} />
-        <Text style={styles.text}>
-          {children.map(ast =>
-            renderTree(ast, {
-              link(key, attributes, children) {
-                const url = attributes.href;
-                return (
-                  <TextLink
-                    key={index}
-                    onPress={e =>
-                      onLinkPress(e, {
-                        url: attributes.href,
-                        type: attributes.type,
-                        canonicalId: attributes.canonicalId
-                      })
-                    }
-                    url={url}
-                  >
-                    {children}
-                  </TextLink>
-                );
-              }
-            })
-          )}
-        </Text>
-      </View>
-    );
-  };
+  const renderKeyFact = (keyFact, index) => (
+    <View key={index} style={styles.container}>
+      <View style={styles.bullet} />
+      <Text style={styles.text}>
+        {keyFact.children.map(ast =>
+          renderTree(ast, {
+            link(key, attributes, children) {
+              const url = attributes.href;
+              return (
+                <TextLink
+                  key={key}
+                  onPress={e =>
+                    onLinkPress(e, {
+                      url: attributes.href,
+                      type: attributes.type,
+                      canonicalId: attributes.canonicalId
+                    })
+                  }
+                  url={url}
+                >
+                  {children}
+                </TextLink>
+              );
+            }
+          })
+        )}
+      </Text>
+    </View>
+  );
 
   return keyFactsData.map((keyFact, index) => renderKeyFact(keyFact, index));
 };
