1b2e4a52c85b48f3892f19f0ae77899a7ba26d75
colincclark <col.c.clark@gmail.com>
2018-07-12 11:14:31 +0100

    feat: add key facts styles for ios and data fixture

--- packages/key-facts/src/key-facts.js
+++ packages/key-facts/src/key-facts.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import styles from "./styles";
 
-const KeyFacts = () => <Text style={styles.KeyFactsBody}>KeyFacts</Text>;
+const KeyFacts = ({ data }) => {
+  const { data: keyFactsData } = data;
+  const renderKeyFact = (keyFact, index) => (
+    <View key={index} style={styles.container}>
+      <View style={styles.bullet} />
+      <Text style={styles.text}>
+        dskjhf sdjfh sdkjfskfhdskfsdkfdskjfdskj fsd fsdkf sdjf sdk hsk
+      </Text>
+    </View>
+  );
+
+  return keyFactsData.map((keyFact, index) => renderKeyFact(keyFact, index));
+};
 
 export default KeyFacts;
--- packages/key-facts/src/styles/shared.js
+++ packages/key-facts/src/styles/shared.js
@@ -1,6 +1,22 @@
 const sharedStyle = {
-  KeyFactsBody: {
-    padding: 20
+  bullet: {
+    backgroundColor: "#103546",
+    height: 8,
+    top: 6,
+    transform: [{ rotate: "45deg" }],
+    width: 8
+  },
+  container: {
+    flexDirection: "row",
+    marginBottom: 15
+  },
+  text: {
+    color: "#333333",
+    fontFamily: "TimesDigitalW04",
+    fontSize: 16,
+    lineHeight: 30,
+    marginTop: -5,
+    paddingLeft: 15
   }
 };
 
