a4d9c6431f48e80314fb4aef2beef8f170c68903
colincclark <col.c.clark@gmail.com>
2018-07-12 11:14:31 +0100

    feat: add key facts styles for ios and data fixture

--- packages/key-facts/key-facts.showcase.js
+++ packages/key-facts/key-facts.showcase.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { LateralSpacingDecorator } from "@times-components/storybook";
 import KeyFacts from "./src/key-facts";
+import data from "./fixtures/key-facts.json";
 
 export default {
   name: "KeyFacts",
   children: [
     {
+      type: "decorator",
+      decorator: LateralSpacingDecorator
+    },
+    {
       type: "story",
       name: "KeyFacts",
-      component: () => <KeyFacts />
+      component: () => <KeyFacts data={data} />
     }
   ]
 };
