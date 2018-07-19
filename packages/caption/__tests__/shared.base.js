11aae56c4b0fe799b959c307e032d3521f693b19
colincclark <col.c.clark@gmail.com>
2018-07-19 19:33:37 +0100

    test: add iteraator to caption

--- packages/caption/__tests__/shared.base.js
+++ packages/caption/__tests__/shared.base.js
@@ -1,41 +1,55 @@
 import React from "react";
 import { Text } from "react-native";
 import TestRenderer from "react-test-renderer";
+import { iterator } from "@times-components/test-utils";
 import Caption from "../src/caption";
 
 const captionText = "Some caption text goes in here";
 const credits = "Just credits";
 
 export default () => {
-  it("renders without credits", () => {
-    const testInstance = TestRenderer.create(<Caption text={captionText} />);
+  const tests = [
+    {
+      name: "caption without credits",
+      test: () => {
+        const testInstance = TestRenderer.create(
+          <Caption text={captionText} />
+        );
 
-    expect(testInstance.toJSON()).toMatchSnapshot("1. renders without credits");
-  });
+        expect(testInstance.toJSON()).toMatchSnapshot();
+      }
+    },
+    {
+      name: "caption with credits",
+      test: () => {
+        const testInstance = TestRenderer.create(
+          <Caption credits={credits} text={captionText} />
+        );
 
-  it("renders with credits", () => {
-    const testInstance = TestRenderer.create(
-      <Caption credits={credits} text={captionText} />
-    );
+        expect(testInstance.toJSON()).toMatchSnapshot();
+      }
+    },
+    {
+      name: "caption with credits only",
+      test: () => {
+        const testInstance = TestRenderer.create(<Caption credits={credits} />);
 
-    expect(testInstance.toJSON()).toMatchSnapshot("2. renders with credits");
-  });
+        expect(testInstance.toJSON()).toMatchSnapshot();
+      }
+    },
+    {
+      name: "caption with child",
+      test: () => {
+        const testInstance = TestRenderer.create(
+          <Caption credits={credits} text={captionText}>
+            <Text>Hello world!</Text>
+          </Caption>
+        );
 
-  it("renders with credits only", () => {
-    const testInstance = TestRenderer.create(<Caption credits={credits} />);
+        expect(testInstance.toJSON()).toMatchSnapshot();
+      }
+    }
+  ];
 
-    expect(testInstance.toJSON()).toMatchSnapshot(
-      "3. renders with credits only"
-    );
-  });
-
-  it("renders a child", () => {
-    const testInstance = TestRenderer.create(
-      <Caption credits={credits} text={captionText}>
-        <Text>Hello world!</Text>
-      </Caption>
-    );
-
-    expect(testInstance.toJSON()).toMatchSnapshot("4. renders a child");
-  });
+  iterator(tests);
 };
