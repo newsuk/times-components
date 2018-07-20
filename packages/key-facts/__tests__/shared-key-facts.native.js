624a2d8d2b7cfc3721f385b0ffa78e02077c9221
colincclark <col.c.clark@gmail.com>
2018-07-20 13:33:20 +0100

    test: improve native test with rtr

--- packages/key-facts/__tests__/shared-key-facts.native.js
+++ packages/key-facts/__tests__/shared-key-facts.native.js
@@ -1,29 +1,22 @@
-import { shallow } from "enzyme";
+import TestRenderer from "react-test-renderer";
 import {
   addSerializers,
   compose,
-  enzymeRenderedSerializer,
   minimaliseTransform,
   minimalNativeTransform,
-  print,
-  propsNoChildren,
-  replaceTransform
+  print
 } from "@times-components/jest-serializer";
 import renderKeyFacts from "./shared-render-key-facts";
 
 export default () => {
   addSerializers(
     expect,
-    enzymeRenderedSerializer(),
     compose(
       print,
       minimalNativeTransform,
-      minimaliseTransform((value, key) => key === "style"),
-      replaceTransform({
-        TextLink: propsNoChildren
-      })
+      minimaliseTransform((value, key) => key === "style")
     )
   );
 
-  renderKeyFacts(shallow);
+  renderKeyFacts(TestRenderer.create);
 };
