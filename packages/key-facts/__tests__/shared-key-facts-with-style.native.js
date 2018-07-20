a83c10e57e9a62ab4faf0d5600cb89ab8c47a1a5
colincclark <col.c.clark@gmail.com>
2018-07-20 13:26:27 +0100

    test: add missing classNames to snapshot

--- packages/key-facts/__tests__/shared-key-facts-with-style.native.js
+++ packages/key-facts/__tests__/shared-key-facts-with-style.native.js
@@ -2,16 +2,22 @@ import TestRenderer from "react-test-renderer";
 import {
   addSerializers,
   compose,
+  flattenStyleTransform,
   hoistStyleTransform,
   minimalNativeTransform,
-  stylePrinter
+  print
 } from "@times-components/jest-serializer";
 import renderKeyFacts from "./shared-render-key-facts";
 
 export default () => {
   addSerializers(
     expect,
-    compose(stylePrinter, hoistStyleTransform, minimalNativeTransform)
+    compose(
+      print,
+      minimalNativeTransform,
+      flattenStyleTransform,
+      hoistStyleTransform
+    )
   );
 
   renderKeyFacts(TestRenderer.create);
