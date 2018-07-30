a1ce5ab4fc379bd6f90b85689973e509e78104d6
Craig Bilner <craig.bilner@news.co.uk>
2018-07-30 13:20:53 +0100

    refactor: tests

--- packages/jest-configurator/__tests__/source-loader.test.js
+++ packages/jest-configurator/__tests__/source-loader.test.js
@@ -1,16 +1,20 @@
+import { iterator } from "@times-components/test-utils";
 import { readFileSync } from "fs";
 import { getCacheKey, process as transform } from "../src/source-loader";
 
-describe("source loader", () => {
-  describe("getCacheKey", () => {
-    it("should create a hash based upon the source contents if the file belongs to a monorepo package", () => {
+const tests = [
+  {
+    name: "monorepo package hash",
+    test() {
       const filePath = "./fixtures/times-components/src/test.js";
       const fileContents = readFileSync(filePath).toString();
 
       expect(getCacheKey(fileContents, filePath, "", {})).toMatchSnapshot();
-    });
-
-    it("should delegate to babel-jest if it is not a monorepo package file", () => {
+    }
+  },
+  {
+    name: "non-monorepo package hash",
+    test() {
       const filePath = "./fixtures/test.js";
       const fileContents = readFileSync(filePath).toString();
 
@@ -19,11 +23,11 @@ describe("source loader", () => {
           rootDir: ""
         })
       ).toMatchSnapshot();
-    });
-  });
-
-  describe("transform", () => {
-    it("should return a file that is not able to be babeled", () => {
+    }
+  },
+  {
+    name: "untransformed unbabelable file",
+    test() {
       const fileContents = `fn main(){"println!("Hello World!");}`;
 
       const actual = transform(fileContents, "./fixtures/test.rs", {
@@ -32,9 +36,11 @@ describe("source loader", () => {
       });
 
       expect(actual).toEqual(fileContents);
-    });
-
-    it("should transform the given file", () => {
+    }
+  },
+  {
+    name: "transformed non-monorepo file",
+    test() {
       const fileContents = readFileSync("./fixtures/test.js").toString();
 
       const { code } = transform(fileContents, "./fixtures/test.js", {
@@ -43,9 +49,11 @@ describe("source loader", () => {
       });
 
       expect(code).toMatchSnapshot();
-    });
-
-    it("should transform the given src version of the file", () => {
+    }
+  },
+  {
+    name: "transformed monorepo src file",
+    test() {
       const fileContents = readFileSync(
         "./fixtures/times-components/dist/test.js"
       ).toString();
@@ -60,6 +68,8 @@ describe("source loader", () => {
       );
 
       expect(code).toMatchSnapshot();
-    });
-  });
-});
+    }
+  }
+];
+
+iterator(tests);
