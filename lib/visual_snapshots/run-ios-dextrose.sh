00cc8c9c5fa51eb5cab2f644f2bb70ab411eda9a
Matt Lowry <matthewlowry@hotmail.co.uk>
2018-08-01 20:41:19 +0100

    fix: use dev packager for dextrose (#1156)

--- lib/visual_snapshots/run-ios-dextrose.sh
+++ lib/visual_snapshots/run-ios-dextrose.sh
@@ -13,14 +13,10 @@ node fructose/changedComponents.js
 ## create components file of all showcase file following pattern
 npx rnscl --pattern '*.showcase!(.web|.styles).js' --outputFile ./fructose/components.js --config ./fructose/rnscl.config
 
-##create bundle
-./node_modules/.bin/react-native bundle --platform ios --dev false --reset-cache --entry-file ./fructose/index.js --bundle-output ios/main.jsbundle
-PACKAGER_PID=$!
-
-##build app
-./node_modules/.bin/react-native run-ios --configuration Release --no-packager
+./node_modules/.bin/react-native start --root $(pwd)/fructose --projectRoots $(pwd)/fructose,$(pwd) --reset-cache &
+./node_modules/.bin/react-native run-ios --no-packager
 
 ##run dextrose
 ./node_modules/.bin/dextrose run --config ./dextrose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
 kill -9 $PACKAGER_PID
-xcrun simctl shutdown booted
+
