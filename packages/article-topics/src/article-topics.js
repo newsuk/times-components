d9737276cf097943ba81804a5c842d56c43e1ed3
colincclark <col.c.clark@gmail.com>
2018-08-03 09:07:55 +0100

    docs: add readme, and fix test to make coverage 100%

--- packages/article-topics/src/article-topics.js
+++ packages/article-topics/src/article-topics.js
@@ -3,6 +3,7 @@ import { View, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 import ArticleTopic from "./article-topic";
 import styles from "./styles";
+import propTypes from "./article-topic-prop-types";
 
 const { style: ViewPropTypesStyle } = ViewPropTypes;
 
@@ -15,14 +16,14 @@ const ArticleTopics = ({ onPress, style, topics }) => (
 );
 
 ArticleTopics.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  style: ViewPropTypesStyle,
   topics: PropTypes.arrayOf(
     PropTypes.shape({
-      name: ArticleTopic.propTypes.name,
-      slug: ArticleTopic.propTypes.slug
+      name: propTypes.name.isRequired,
+      slug: propTypes.slug.isRequired
     }).isRequired
-  ).isRequired,
-  onPress: ArticleTopic.propTypes.onPress,
-  style: ViewPropTypesStyle
+  ).isRequired
 };
 
 ArticleTopics.defaultProps = {
