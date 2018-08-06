8ac778f40966e69e6f350be16dbbf527f435994d
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-06 11:14:58 +0100

    Add test for single comment case

--- packages/article/__tests__/comments.native.js
+++ packages/article/__tests__/comments.native.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   addSerializers,
   compose,
@@ -5,8 +6,12 @@ import {
   minimaliseTransform,
   minimalNativeTransform
 } from "@times-components/jest-serializer";
+import TestRenderer from "react-test-renderer";
 import "./mocks.native";
 import shared from "./comments.base";
+import Article from "../src/article";
+import articleFixture, { testFixture } from "../fixtures/full-article";
+import { adConfig } from "./ad-mock";
 
 const omitKeys = new Set([
   "data",
@@ -41,4 +46,40 @@ export default () => {
   });
 
   shared();
+
+  test("single comment", () => {
+    const article = articleFixture({
+      ...testFixture,
+      commentCount: 1
+    });
+
+    const testInstance = TestRenderer.create(
+      <Article
+        adConfig={adConfig}
+        analyticsStream={() => {}}
+        article={article}
+        onAuthorPress={() => {}}
+        onCommentGuidelinesPress={() => {}}
+        onCommentsPress={() => {}}
+        onLinkPress={() => {}}
+        onRelatedArticlePress={() => {}}
+        onTopicPress={() => {}}
+        onVideoPress={() => {}}
+      />
+    );
+
+    const comments = testInstance.root.find(node => {
+      if (
+        typeof node.type === "string" &&
+        node.type.includes("Text") &&
+        node.children.length > 0 &&
+        node.children[0] === "1 comment"
+      ) {
+        return true;
+      }
+      return false;
+    });
+
+    expect(comments).toBeDefined();
+  });
 };
