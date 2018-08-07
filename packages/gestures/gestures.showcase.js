18d7ba1964100bae79b7ea8e5de0d3afa44dd257
colincclark <col.c.clark@gmail.com>
2018-08-07 11:35:37 +0100

    refactor: remove unsafe lifecycle event and amend showcase names

--- packages/gestures/gestures.showcase.js
+++ packages/gestures/gestures.showcase.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { CenteredDecorator } from "@times-components/storybook";
 import { fontSizes } from "@times-components/styleguide";
 import Gestures from "./src/gestures";
