b57fc722d312ecf955b188d39a0ad209d99102e4
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-10 11:22:08 +0100

    Provide theme in android-app

--- android-app/pages/article.js
+++ android-app/pages/article.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NativeModules } from "react-native";
 import { Article } from "@times-components/pages";
+import Context, { defaults } from "@times-components/context";
+import { colours } from "@times-components/styleguide";
 
 const config = NativeModules.ReactConfig;
 const { fetch } = NativeModules.NativeFetch;
@@ -33,27 +35,34 @@ const platformAdConfig = {
   platform: "mobile"
 };
 
-const ArticleView = ({ articleId, sectionName }) => {
+const ArticleView = ({ articleId, scale, sectionName }) => {
   const adConfig = { ...platformAdConfig, sectionName };
+  const theme = {
+    scale: scale || defaults.theme.scale,
+    sectionColour: colours.section[sectionName]
+  };
 
   return (
-    <ArticlePageView
-      articleId={articleId}
-      analyticsStream={track}
-      onArticlePress={onArticlePress}
-      onAuthorPress={onAuthorPress}
-      onCommentsPress={onCommentsPress}
-      onCommentGuidelinesPress={onCommentGuidelinesPress}
-      onLinkPress={onLinkPress}
-      onVideoPress={onVideoPress}
-      onTopicPress={onTopicPress}
-      platformAdConfig={adConfig}
-    />
+    <Context.Provider values={{ theme }}>
+      <ArticlePageView
+        articleId={articleId}
+        analyticsStream={track}
+        onArticlePress={onArticlePress}
+        onAuthorPress={onAuthorPress}
+        onCommentsPress={onCommentsPress}
+        onCommentGuidelinesPress={onCommentGuidelinesPress}
+        onLinkPress={onLinkPress}
+        onVideoPress={onVideoPress}
+        onTopicPress={onTopicPress}
+        platformAdConfig={adConfig}
+      />
+    </Context.Provider>
   );
 };
 
 ArticleView.propTypes = {
   articleId: PropTypes.string.isRequired,
+  scale: PropTypes.string.isRequired,
   sectionName: PropTypes.string.isRequired
 };
 
