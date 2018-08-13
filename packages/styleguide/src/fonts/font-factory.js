b12e09c1b84bba1bdc92e2bc90446d45e9559667
James Miller <james@james-miller.co.uk>
2018-08-13 17:03:52 +0100

    chore: Computed font factory

--- packages/styleguide/src/fonts/font-factory.js
+++ packages/styleguide/src/fonts/font-factory.js
@@ -1,54 +1,10 @@
 import timesStyleguide from "../styleguide";
 
-const fontMapping = styleguide => ({
-  body: {
-    bodyMobile: {
-      fontFamily: styleguide.fonts.body,
-      fontSize: styleguide.fontSizes.bodyMobile,
-      lineHeight: styleguide.lineHeight({
-        font: "body",
-        fontSize: "bodyMobile"
-      })
-    },
-    secondary: {
-      fontFamily: styleguide.fonts.body,
-      fontSize: styleguide.fontSizes.secondary,
-      lineHeight: styleguide.lineHeight({ font: "body", fontSize: "secondary" })
-    },
-    tertiary: {
-      fontFamily: styleguide.fonts.body,
-      fontSize: styleguide.fontSizes.tertiary,
-      lineHeight: styleguide.lineHeight({ font: "body", fontSize: "tertiary" })
-    }
-  },
-  headline: {
-    pageComponentHeadline: {
-      fontFamily: styleguide.fonts.headline,
-      fontSize: styleguide.fontSizes.pageComponentHeadline,
-      lineHeight: styleguide.lineHeight({ font: "headline", fontSize: "pageComponentHeadline" })
-    },
-    pageHeadline: {
-      fontFamily: styleguide.fonts.headline,
-      fontSize: styleguide.fontSizes.pageHeadline,
-      lineHeight: styleguide.lineHeight({ font: "headline", fontSize: "pageHeadline" })
-    },
-    smallHeadline: {
-      fontFamily: styleguide.fonts.headline,
-      fontSize: styleguide.fontSizes.smallHeadline,
-      lineHeight: styleguide.lineHeight({ font: "headline", fontSize: "smallHeadline" })
-    }
-  },
-  supporting: {
-    cardMetaMobile: {
-      fontFamily: styleguide.fonts.supporting,
-      fontSize: styleguide.fontSizes.cardMetaMobile,
-      lineHeight: styleguide.lineHeight({ font: "supporting", fontSize: "cardMetaMobile" })
-    }
-  }
-});
-
 export default scale => ({ font, fontSize }) => {
   const styleguide = timesStyleguide({ scale });
-  const mapping = fontMapping(styleguide);
-  return mapping[font][fontSize];
+  return {
+    fontFamily: styleguide.fonts[font],
+    fontSize: styleguide.fontSizes[fontSize],
+    lineHeight: styleguide.lineHeight({ font, fontSize })
+  };
 };
