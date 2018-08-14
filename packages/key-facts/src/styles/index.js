58d19a7d844391fa3c5b7fcb0a1cab3415b2f143
James Miller <james@james-miller.co.uk>
2018-08-14 10:41:45 +0100

    chore: Key facts tidy

--- packages/key-facts/src/styles/index.js
+++ packages/key-facts/src/styles/index.js
@@ -1,38 +1,48 @@
 import { StyleSheet } from "react-native";
 import { colours, fonts, spacing } from "@times-components/styleguide";
 
-export default StyleSheet.create({
-  container: {
-    marginBottom: spacing(1),
-    marginLeft: spacing(2),
-    marginRight: spacing(2),
-    marginTop: spacing(1)
-  },
-  bullet: {
-    height: 8,
-    top: 2,
-    transform: [{ rotate: "45deg" }],
-    width: 8
-  },
-  bulletContainer: {
-    flexDirection: "row",
-    marginBottom: spacing(4),
-    paddingLeft: 1,
-    width: "100%"
-  },
-  link: {
-    color: colours.functional.action
-  },
-  text: {
-    color: colours.functional.primary,
-    marginTop: -8,
-    paddingLeft: spacing(3),
-    width: "95%"
-  },
-  title: {
-    fontFamily: fonts.supporting,
-    fontWeight: "400",
-    letterSpacing: 1.2,
-    marginBottom: spacing(4)
-  }
-});
+const styling = scale => {
+  const { fontFactory, fontSizes } = styleguide({ scale });
+  return StyleSheet.create({
+    container: {
+      marginBottom: spacing(1),
+      marginLeft: spacing(2),
+      marginRight: spacing(2),
+      marginTop: spacing(1)
+    },
+    bullet: {
+      height: 8,
+      top: 2,
+      transform: [{ rotate: "45deg" }],
+      width: 8
+    },
+    bulletContainer: {
+      flexDirection: "row",
+      marginBottom: spacing(4),
+      paddingLeft: 1,
+      width: "100%"
+    },
+    link: {
+      color: colours.functional.action
+    },
+    text: {
+      color: colours.functional.primary,
+      ...fontFactory({
+        font: "body",
+        fontSize: "secondary"
+      }),
+      marginTop: -8,
+      paddingLeft: spacing(3),
+      width: "95%"
+    },
+    title: {
+      fontFamily: fonts.supporting,
+      fontSize: fontSizes.cardMetaMobile,
+      fontWeight: "400",
+      letterSpacing: 1.2,
+      marginBottom: spacing(4)
+    }
+  });
+};
+
+export default styling;
