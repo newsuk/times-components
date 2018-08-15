82a67bd87a632f8a417727bac7b1ff324887f9d8
Craig Bilner <craig.bilner@news.co.uk>
2018-08-15 17:07:41 +0100

    chore: lint

--- packages/pull-quote/src/pull-quote.js
+++ packages/pull-quote/src/pull-quote.js
@@ -2,7 +2,9 @@ import React from "react";
 import PullQuoteBase from "./pull-quote.base";
 import { propTypes, defaultProps } from "./pull-quote-prop-types";
 
-const PullQuote = ({ caption, ...props }) => <PullQuoteBase {...props}>{caption}</PullQuoteBase>;
+const PullQuote = ({ caption, ...props }) => (
+  <PullQuoteBase {...props}>{caption}</PullQuoteBase>
+);
 
 PullQuote.propTypes = propTypes;
 PullQuote.defaultProps = defaultProps;
