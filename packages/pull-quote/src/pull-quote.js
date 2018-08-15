acacd2049168859a9f97f2b1178cc7abff97e309
Craig Bilner <craig.bilner@news.co.uk>
2018-08-15 15:01:32 +0100

    fix: name

--- packages/pull-quote/src/pull-quote.js
+++ packages/pull-quote/src/pull-quote.js
@@ -1,8 +1,8 @@
 import React from "react";
-import PQ from "./pull-quote.base";
+import PullQuoteBase from "./pull-quote.base";
 import { propTypes, defaultProps } from "./pull-quote-prop-types";
 
-const PullQuote = ({ caption, ...props }) => <PQ {...props}>{caption}</PQ>;
+const PullQuote = ({ caption, ...props }) => <PullQuoteBase {...props}>{caption}</PullQuoteBase>;
 
 PullQuote.propTypes = propTypes;
 PullQuote.defaultProps = defaultProps;
