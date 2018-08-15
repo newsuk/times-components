a54ff9470dba946b2af3c4a47b09188f25c241e7
Aron Shamash <aronshamash@gmail.com>
2018-08-15 10:58:27 +0100

    feat: update fixture data to include italic and bold text

--- packages/topic/__tests__/fixtures.js
+++ packages/topic/__tests__/fixtures.js
@@ -4,7 +4,40 @@ export default {
     {
       name: "text",
       attributes: {
-        value: "Test Description"
+        value: "Test "
+      },
+      children: []
+    },
+    {
+      name: "italic",
+      attributes: {},
+      children: [
+        {
+          name: "text",
+          attributes: {
+            value: "italic "
+          },
+          children: []
+        }
+      ]
+    },
+    {
+      name: "bold",
+      attributes: {},
+      children: [
+        {
+          name: "text",
+          attributes: {
+            value: "bold"
+          },
+          children: []
+        }
+      ]
+    },
+    {
+      name: "text",
+      attributes: {
+        value: " text."
       },
       children: []
     }
