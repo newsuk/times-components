5e9b07c0ab8de69a68e281da076cf503d82978c0
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-20 13:12:35 +0100

    Style minor refactor

--- packages/article/src/styles/article-error/index.js
+++ packages/article/src/styles/article-error/index.js
@@ -10,32 +10,28 @@ export default StyleSheet.create({
   errorContainer: {
     height: "100%",
     justifyContent: "space-between",
-    marginLeft: spacing(2),
-    marginRight: spacing(2),
+    marginHorizontal: spacing(2),
     paddingTop: spacing(2),
     paddingBottom: spacing(14)
   },
   errorImageContainer: {
     alignSelf: "center",
-    marginBottom: spacing(4),
-    marginTop: spacing(4)
+    marginVertical: spacing(4)
   },
   errorHeading: {
     alignSelf: "center",
     color: colours.functional.brandColour,
     fontFamily: fonts.headline,
     fontSize: fontSizes.leadHeadline,
-    marginBottom: 10,
-    marginLeft: spacing(8),
-    marginRight: spacing(8),
+    marginBottom: spacing(2),
+    marginHorizontal: spacing(8),
     textAlign: "center"
   },
   errorMessage: {
     color: colours.functional.secondary,
     fontFamily: fonts.bodyRegular,
     fontSize: fontSizes.infoSubText,
-    marginLeft: spacing(8),
-    marginRight: spacing(8),
+    marginHorizontal: spacing(8),
     textAlign: "center"
   }
 });
