c0688dc8f26f079566f83c5866da403166ccb286
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-21 16:57:02 +0100

    Remove duplicate view in loading

--- packages/card/src/card-loading.js
+++ packages/card/src/card-loading.js
@@ -1,25 +1,12 @@
-import React from "react";
-import { View } from "react-native";
+import React, { Fragment } from "react";
 import Gradient from "@times-components/gradient";
-import { sharedPropTypes, sharedDefaultProps } from "./card-shared-prop-types";
 import styles from "./styles";
 
-const Loading = ({ contentContainerClass, isReversed }) => (
-  <View
-    className={contentContainerClass}
-    style={[
-      styles.contentContainer,
-      isReversed ? styles.reversedContentContainer : ""
-    ]}
-  >
+export default () => (
+  <Fragment>
     <Gradient degrees={264} style={[styles.headerContainer]} />
     <Gradient degrees={267} style={[styles.textContainer]} />
     <Gradient degrees={267} style={[styles.textContainer]} />
     <Gradient degrees={267} style={[styles.textContainer, styles.lastBar]} />
-  </View>
+  </Fragment>
 );
-
-Loading.propTypes = sharedPropTypes;
-Loading.defaultProps = sharedDefaultProps;
-
-export default Loading;
