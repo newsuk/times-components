2b24da26e8a6711d487041898a8a65b54a669951
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-22 15:09:53 +0100

    Add opacity to rect

--- packages/svgs/src/rect.js
+++ packages/svgs/src/rect.js
@@ -5,7 +5,7 @@ import SVGPath from "./path";
 
 const { Path } = ART;
 
-const Rect = ({ stroke, fill, x, y, width, height }) => {
+const Rect = ({ stroke, fill, x, y, width, height, fillOpacity }) => {
   const d = new Path()
     .moveTo(x, y)
     .line(width, 0)
@@ -13,7 +13,7 @@ const Rect = ({ stroke, fill, x, y, width, height }) => {
     .line(-width, 0)
     .line(0, -height);
 
-  return <SVGPath d={d} fill={fill} stroke={stroke} />;
+  return <SVGPath d={d} fill={fill} opacity={fillOpacity} stroke={stroke} />;
 };
 
 Rect.propTypes = {
@@ -22,12 +22,14 @@ Rect.propTypes = {
   x: PropTypes.string.isRequired,
   y: PropTypes.string.isRequired,
   width: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired
+  height: PropTypes.string.isRequired,
+  fillOpacity: PropTypes.string
 };
 
 Rect.defaultProps = {
   stroke: null,
-  fill: null
+  fill: null,
+  fillOpacity: null
 };
 
 export default Rect;
