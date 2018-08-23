a48b60a59bdb0f2cd4f0741d60f4c8ccf070d5fa
James Miller <james@james-miller.co.uk>
2018-08-23 11:31:00 +0100

    chore: Tweaks, tests and linting

--- packages/article-byline/shared-showcase.js
+++ packages/article-byline/shared-showcase.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import React from "react";
+import { View } from "react-native";
 import { colours } from "@times-components/styleguide";
 
 const authorsAST = require("./fixtures/authors.json");
@@ -20,6 +21,10 @@ const styles = {
   }
 };
 
+const ComponentWrapper = ({ children }) => (
+  <View style={{ flexDirection: "row", flexWrap: "wrap" }}>{children}</View>
+);
+
 const getProps = (selectV2, decorateAction) => ({
   color: selectV2("Section colours: ", colours.section),
   onAuthorPress: preventDefaultedAction(decorateAction)("onAuthorPress")
@@ -32,61 +37,85 @@ export default (Component, name) => ({
       type: "story",
       name: "Article Byline with a single author",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.singleAuthor}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.singleAuthor}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     },
     {
       type: "story",
       name: "Article Byline with a text only element",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.singleInlineElement}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.singleInlineElement}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     },
     {
       type: "story",
       name: "Article Byline with multiple authors",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.multipleAuthorsCommaSeparated}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.multipleAuthorsCommaSeparated}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     },
     {
       type: "story",
       name: "Article Byline with author in the beginning",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.authorInTheBeginning}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.authorInTheBeginning}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     },
     {
       type: "story",
       name: "Article Byline with author at the end",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.authorAtTheEnd}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.authorAtTheEnd}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     },
     {
       type: "story",
       name: "Article Byline with styles",
       component: ({ selectV2 }, { decorateAction }) => (
-        <Component
-          ast={authorsAST.multipleAuthorsCommaSeparated}
-          style={styles}
-          {...getProps(selectV2, decorateAction)}
-        />
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.multipleAuthorsCommaSeparated}
+            style={styles}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
+      )
+    },
+    {
+      type: "story",
+      name: "Article Byline that is impossibly long",
+      component: ({ selectV2 }, { decorateAction }) => (
+        <ComponentWrapper>
+          <Component
+            ast={authorsAST.veryLongByline}
+            {...getProps(selectV2, decorateAction)}
+          />
+        </ComponentWrapper>
       )
     }
   ]
