30501ef00a107cdb6a45659dee32671bd88fbe0f
Craig Bilner <craig.bilner@news.co.uk>
2018-08-28 21:10:29 +0100

    fix: ssr styles

--- packages/article-summary/src/styles/index.js
+++ packages/article-summary/src/styles/index.js
@@ -1,11 +1,12 @@
+import { StyleSheet } from "react-native";
 import sharedStyles from "./shared";
 
-const styles = {
+const styles = StyleSheet.create({
   ...sharedStyles,
   headlineWrapper: {
     ...sharedStyles.headlineWrapper,
     lineHeight: 25
   }
-};
+});
 
 export default styles;
