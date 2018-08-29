a2a3603a996727954963a3f9e127391179cc1072
L0wry <matthewlowry@hotmail.co.uk>
2018-08-29 12:48:08 +0100

    chore: bumping fructose and dextrose

--- lib/visual_snapshots/run-web-dextrose.sh
+++ lib/visual_snapshots/run-web-dextrose.sh
@@ -6,7 +6,7 @@ npx selenium-standalone start 2>> selenium.log &
 npm run fetch-fonts
 
 ##construct list of changed packages
-npx lerna ls --json --since > fructose/changedPackages.json
+npx lerna ls --json > fructose/changedPackages.json
 
 ##get array of changed components∏
 node fructose/changedComponents.js
@@ -25,5 +25,5 @@ npx fructose-web -d ./fructose >> webpack.log &
 FRUCTOSE_WEB_PID=$!
 
 ##run dextrose
-npx dextrose run --config ./dextrose/dextrose.web.js 
+npx dextrose run --config ./dextrose/dextrose.web.js --snapshotWait 2000
 kill -9 $FRUCTOSE_WEB_PID # close the web app
