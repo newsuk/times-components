f851eb5a660d02c8409e5a08812e3ed815ed2f59
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-08-29 15:01:46 +0100

    Article provider with a native bridge

---
+++ packages/provider/src/article-from-native.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+class NativeArticleProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+    this.fetchArticle = this.fetchArticle.bind(this);
+  }
+
+  componentDidMount() {
+    if (!this.fetched) {
+      this.fetched = true;
+      this.fetchArticle();
+    }
+  }
+
+  fetchArticle() {
+    const { articleId, fetch } = this.props;
+
+    this.setState({ isLoading: true }, () => {
+      fetch(articleId)
+        .then(data => {
+          this.setState({ isLoading: false, data });
+        })
+        .catch(error => {
+          this.setState({ isLoading: false, error });
+        });
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { error, isLoading, data } = this.state;
+    return children({ error, isLoading, data, refetch: this.fetchArticle });
+  }
+}
+
+NativeArticleProvider.propTypes = {
+  articleId: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  fetch: PropTypes.func.isRequired
+};
+
+export default NativeArticleProvider;
--- packages/provider/src/provider.js
+++ packages/provider/src/provider.js
@@ -1,18 +1,20 @@
 import connect from "./connect";
+import ArticleProvider from "./article";
 import AuthorProfileProvider from "./author-profile";
 import AuthorArticlesNoImagesProvider from "./author-articles-no-images";
 import AuthorArticlesWithImagesProvider from "./author-articles-with-images";
-import ArticleProvider from "./article";
+import NativeArticleProvider from "./article-from-native";
 import TopicProvider from "./topic";
 import TopicArticlesProvider from "./topic-articles";
 
 export default connect;
 
 export {
+  ArticleProvider,
   AuthorProfileProvider,
   AuthorArticlesNoImagesProvider,
   AuthorArticlesWithImagesProvider,
-  ArticleProvider,
+  NativeArticleProvider,
   TopicProvider,
   TopicArticlesProvider
 };
