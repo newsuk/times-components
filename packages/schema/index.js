92d9bb59f8b49f8530563c742f8e142520d7de0a
Craig Bilner <craig.bilner@news.co.uk>
2018-08-30 18:30:00 +0100

    fix: provide helpful warnings

--- packages/schema/index.js
+++ packages/schema/index.js
@@ -1,5 +1,13 @@
+/* eslint-disable no-console */
+
 const fetch = require("node-fetch");
+const path = require("path");
+const { promisify } = require("util");
+const fs = require("fs");
+const chalk = require("chalk");
 const fetchGql = require("./fetch-gql-schema");
+
+const access = promisify(fs.access);
 
 fetchGql(
   process.cwd(),
@@ -7,8 +15,17 @@ fetchGql(
   process.env.GRAPHQL_ENDPOINT || "http://localhost:4000/graphql"
 )
   .then(() => process.exit())
-  .catch(e => {
-    // eslint-disable-next-line no-console
-    console.log(e);
-    process.exit(1);
+  .catch(async e => {
+    console.log(chalk.yellow(e));
+
+    try {
+      await access(path.join(process.cwd(), "schema.json"));
+      console.log(chalk.yellow("Your schema may be out of date for linting"));
+    } catch (err) {
+      console.log(
+        chalk.yellow("Without a schema you cannot perform gql linting")
+      );
+    }
+
+    process.exit();
   });
