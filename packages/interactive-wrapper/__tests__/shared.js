a6bd9059322f2cb1dd0633480cbb61e71ad976cf
James Miller <james@james-miller.co.uk>
2018-08-30 11:59:21 +0100

    chore: Update shared test

--- packages/interactive-wrapper/__tests__/shared.js
+++ packages/interactive-wrapper/__tests__/shared.js
@@ -1,10 +1,22 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
-import Interactive from "../src/interactive";
+import InteractiveWrapper from "../src/interactive-wrapper";
 
 export default () => {
   it("renders correctly", () => {
-    const testInstance = TestRenderer.create(<Interactive />);
+    const props = {
+      id: "a0534eee-682e-4955-8e1e-84b428ef1e79",
+      attributes: {
+        chaptercounter: "Chapter%20one",
+        heading: "Xxxx%20xxxxxx%20xxxx%20xxxxx%20",
+        standfirst:
+          "Xxxx%20xxxxxx%20xxxx%20xxxxx%20xxxxxxxx%20xxxxxx%20xxxx%20xx%20xxxxxxxx"
+      },
+      element: "chapter-header",
+      source:
+        "//components.timesdev.tools/lib2/times-chapter-header-1.0.0/chapter-header.html"
+    };
+    const testInstance = TestRenderer.create(<InteractiveWrapper {...props} />);
 
     expect(testInstance.toJSON()).toMatchSnapshot();
   });
