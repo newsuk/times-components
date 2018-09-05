74efce2d6274eb3565ff088d13c54a134b2825df
L0wry <matthewlowry@hotmail.co.uk>
2018-09-05 10:06:44 +0100

    chore: bumping fructose

--- lib/visual_snapshots/run-web-dextrose.sh
+++ lib/visual_snapshots/run-web-dextrose.sh
@@ -6,24 +6,21 @@ npx selenium-standalone start 2>> selenium.log &
 npm run fetch-fonts
 
 ##construct list of changed packages
-npx lerna ls --json --since > fructose/changedPackages.json
+# npx lerna ls --json --since > fructose/changedPackages.json
 
-##get array of changed components∏
-node fructose/changedComponents.js
+# ##get array of changed components∏
+# node fructose/changedComponents.js
 
-## create components file of all showcase file following pattern
-npx rnscl --pattern '*.showcase!(.styles).js' --outputFile ./fructose/components.js --config ./fructose/rnscl.config
+# ## create components file of all showcase file following pattern
+# npx rnscl --pattern '*.showcase!(.styles).js' --outputFile ./fructose/components.js --config ./fructose/rnscl.config
 
 ## build vendor package
-npx webpack --config fructose/vendor.webpack.config.js
-
-##start bundler
-npx react-native start --reset-cache &
+# npx webpack --config fructose/vendor.webpack.config.js
 
 ##run fructose app
-npx fructose-web -d ./fructose >> webpack.log &
+npx fructose-web -d ./fructose &
 FRUCTOSE_WEB_PID=$!
 
 ##run dextrose
 npx dextrose run --config ./dextrose/dextrose.web.js --snapshotWait 2000
-kill -9 $FRUCTOSE_WEB_PID # close the web app
+# kill -9 $FRUCTOSE_WEB_PID # close the web app
