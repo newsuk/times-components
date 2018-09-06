75fe7abb239b6b9b2f819425427fb23627467725
Craig Bilner <craig.bilner@news.co.uk>
2018-09-06 16:49:50 +0100

    test: improve coverage

--- packages/image/__tests__/android/jest.config.js
+++ packages/image/__tests__/android/jest.config.js
@@ -1,5 +1,5 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
 
 module.exports = jestConfigurator("android", __dirname, {
-  coverageIgnoreGlobs: ["index.js"]
+  coverageIgnoreGlobs: ["index.js", "utils.js"]
 });
--- packages/image/__tests__/shared.web.js
+++ packages/image/__tests__/shared.web.js
@@ -1,3 +1,6 @@
+/* eslint-env browser */
+
+import React from "react";
 import TestRenderer from "react-test-renderer";
 import {
   addSerializers,
@@ -8,6 +11,7 @@ import {
   replacePropTransform
 } from "@times-components/jest-serializer";
 import { hash } from "@times-components/test-utils";
+import Image from "../src";
 import "./mocks";
 import shared from "./shared.base";
 
@@ -27,5 +31,48 @@ export default () => {
   // eslint-disable-next-line global-require
   require("jest-styled-components");
 
-  shared(TestRenderer.create);
+  const tests = [
+    {
+      name: "an invalid uri",
+      test: () => {
+        const testRenderer = TestRenderer.create(
+          <Image aspectRatio={3 / 2} highResSize={1400} uri="not-valid" />
+        );
+
+        expect(testRenderer).toMatchSnapshot();
+      }
+    },
+    {
+      name: "no URL in environment",
+      test: () => {
+        const origURL = window.URL;
+
+        delete window.URL;
+
+        const testInstance = TestRenderer.create(
+          <Image aspectRatio={3 / 2} highResSize={1400} uri="not-valid" />
+        );
+
+        expect(testInstance).toMatchSnapshot();
+
+        window.URL = origURL;
+      }
+    },
+    {
+      name: "with existing URL params",
+      test: () => {
+        const testRenderer = TestRenderer.create(
+          <Image
+            aspectRatio={3 / 2}
+            highResSize={1400}
+            uri="https://image.io"
+          />
+        );
+
+        expect(testRenderer).toMatchSnapshot();
+      }
+    }
+  ];
+
+  shared(TestRenderer.create, tests);
 };
