ac284292f56b74121341974829d42080dee588b0
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2018-09-14 17:30:56 +0100

    fix: use whatwg-url for creating urls in android (#1326)

--- android-app/index.android.js
+++ android-app/index.android.js
@@ -1,7 +1,12 @@
 import { AppRegistry } from "react-native";
+import { URL, URLSearchParams } from "whatwg-url";
 import AuthorProfileView from "./pages/author-profile";
 import ArticleView from "./pages/article";
 import TopicView from "./pages/topic";
+
+// see https://github.com/facebook/react-native/issues/16434
+global.URL = URL;
+global.URLSearchParams = URLSearchParams;
 
 AppRegistry.registerComponent("AuthorProfile", () => AuthorProfileView);
 AppRegistry.registerComponent("Article", () => ArticleView);
