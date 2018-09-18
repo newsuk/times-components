4210a277ddda5430f82eceba6a85b5c9ac145bf0
Paul Calver <thecalver@gmail.com>
2018-09-18 11:25:22 +0100

    updates for polyfill

--- ios-app/index.ios.js
+++ ios-app/index.ios.js
@@ -1,7 +1,12 @@
 import { AppRegistry } from "react-native";
+import { URL, URLSearchParams } from "url-polyfill";
 import AuthorProfileView from "./pages/author-profile";
 import ArticleView from "./pages/article";
 import TopicView from "./pages/topic";
+
+// see https://github.com/facebook/react-native/issues/16434
+global.URL = URL;
+global.URLSearchParams = URLSearchParams;
 
 AppRegistry.registerComponent("AuthorProfilePage", () => AuthorProfileView);
 AppRegistry.registerComponent("ArticlePage", () => ArticleView);
