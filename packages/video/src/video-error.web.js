76e63bcde4cc4a15abab142b1c10d6db2ff0639d
Aron Shamash <aronshamash@gmail.com>
2018-09-18 12:20:42 +0100

    feat: make poster not required

--- packages/video/src/video-error.web.js
+++ packages/video/src/video-error.web.js
@@ -6,37 +6,48 @@ import Button from "@times-components/button";
 import Image from "@times-components/image";
 import styles, { retryButtonStyles } from "./styles";
 
-const VideoError = ({ height, width, poster }) => (
-  <View style={[styles.background, { width, height }]}>
-    <View style={styles.posterContainer}>
+const VideoError = ({ height, width, poster }) => {
+  const renderPosterImage = () => {
+    if (!poster) return null;
+    return (
       <Image
         aspectRatio={width / height}
         style={styles.posterImage}
         uri={poster.uri}
       />
+    );
+  };
+
+  return (
+    <View style={[styles.background, { width, height }]}>
+      <View style={styles.posterContainer}>{renderPosterImage()}</View>
+      <View style={styles.textContainer}>
+        <Text style={styles.heading}>Something&apos;s gone wrong</Text>
+        <Text style={styles.body}>
+          Please check your network connection and retry to try again
+        </Text>
+        <Button
+          onPress={() => {
+            if (typeof window !== "undefined") {
+              window.location.reload();
+            }
+          }}
+          style={retryButtonStyles}
+          title="Retry"
+        />
+      </View>
     </View>
-    <View style={styles.textContainer}>
-      <Text style={styles.heading}>Something&apos;s gone wrong</Text>
-      <Text style={styles.body}>
-        Please check your network connection and retry to try again
-      </Text>
-      <Button
-        onPress={() => {
-          if (typeof window !== "undefined") {
-            window.location.reload();
-          }
-        }}
-        style={retryButtonStyles}
-        title="Retry"
-      />
-    </View>
-  </View>
-);
+  );
+};
 
 VideoError.propTypes = {
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  poster: PropTypes.shape({ uri: PropTypes.string.isRequired }).isRequired,
+  poster: PropTypes.shape({ uri: PropTypes.string.isRequired }),
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
+};
+
+VideoError.defaultProps = {
+  poster: ""
 };
 
 export default VideoError;
