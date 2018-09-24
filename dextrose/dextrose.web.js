fa83edb0d97f55a9892d160a7f7c509595cd3d7a
Matt Lowry <matthewlowry@hotmail.co.uk>
2018-09-24 18:11:50 +0100

    chore: add large breakpoint for dextrose web (#1347)

--- dextrose/dextrose.web.js
+++ dextrose/dextrose.web.js
@@ -6,7 +6,7 @@ const config = {
   snapPath: path.join(__dirname, "/snappy"),
   platformName: "web",
   ignoredShowcases,
-  breakpoints: [520]
+  breakpoints: [520, 1100]
 };
 
 module.exports = config;
