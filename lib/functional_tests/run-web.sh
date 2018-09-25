8af78b4920fab4fd4f6e187e2d52812533dbb592
colincclark <col.c.clark@gmail.com>
2018-09-25 15:57:54 +0100

    chore: remove unused functional tests

--- lib/functional_tests/run-web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm run fetch-fonts  # fetch times fonts
-rnstl --searchDir ./ --pattern './packages/*/*.fructose.web.js' --outputFile ./fructose/components.js # create single file with all the components
-npx compile-tests -d fructose -t components.test.js -a components.js # single file for all the tests
-npx webpack --config fructose/vendor.webpack.config.js # build vendor package
-npx fructose-web -d fructose & # start the fructose web app
-FRUCTOSE_WEB_PID=$!
-jest fructose/components.test.js --setupTestFrameworkScriptFile ./fructose/setup.web.js --verbose --forceExit --no-cache # run the tests
-kill -9 $FRUCTOSE_WEB_PID # close the web app
