ab53e5a736adb3cedf27bd43d788a77ed8979434
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-09-27 10:38:36 +0100

    Remove the span if no publication

--- packages/date-publication/__tests__/ios/date-publication-bst-non-london.ios.test.js
+++ packages/date-publication/__tests__/ios/date-publication-bst-non-london.ios.test.js
@@ -3,4 +3,3 @@ import shared from "../shared-non-london";
 const dateBST = "2017-01-01T14:32:00.000Z";
 
 shared(dateBST);
-
--- packages/date-publication/__tests__/ios/date-publication-gmt-non-london.ios.test.js
+++ packages/date-publication/__tests__/ios/date-publication-gmt-non-london.ios.test.js
@@ -3,4 +3,3 @@ import shared from "../shared-non-london";
 const dateGMT = "2017-07-01T14:32:00.000Z";
 
 shared(dateGMT);
-
