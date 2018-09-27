9ee196ce980a99d42d60d4bfb9cc040b135bd810
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-09-27 16:23:51 +0100

    Lint test uitls

--- packages/test-utils/src/index.js
+++ packages/test-utils/src/index.js
@@ -1,6 +1,6 @@
 import iterator from "./iterator";
 import hash from "./hash";
-import mockNativeModules from "@times-components/test-utils/src/mock-native-modules";
+import mockNativeModules from "./mock-native-modules";
 
 export { iterator, hash, mockNativeModules };
 export * from "./faketime";
