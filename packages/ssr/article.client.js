017c0b110251da038a55f0e014942d716ee4c13c
Craig Bilner <craig.bilner@news.co.uk>
2018-09-28 15:02:41 +0100

    chore: lint

--- packages/ssr/article.client.js
+++ packages/ssr/article.client.js
@@ -4,6 +4,4 @@ const article = require("./article");
 const apolloClient = require("./make-client-ac");
 const runClient = require("./run-client");
 
-runClient(
-  article(apolloClient(), window.nuk.id, window.nuk.adConfig)
-);
+runClient(article(apolloClient(), window.nuk.id, window.nuk.adConfig));
