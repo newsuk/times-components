00e87cf49639b18f9f1bb1a52f7b578806eb84cf
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-10-08 16:51:10 +0100

    Add more showcases for dropcaps

--- packages/drop-cap/drop-cap.showcase.android.js
+++ packages/drop-cap/drop-cap.showcase.android.js
@@ -6,11 +6,31 @@ export default {
   children: [
     {
       type: "story",
-      name: "DropCap",
+      name: "Simple dropCap",
       component: () => (
         <DropCap
           dropCap="I"
           text="n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. If, in some respects, they may have left too deep an impression on my mind, the gain was greater than the loss.” The gain was a lifelong conviction that the central aim of domestic policy must be to avoid the horror of mass unemployment. Forget ideological posturing; the job of a responsible Conservative government was to keep people in work."
+        />
+      )
+    },
+    {
+      type: "story",
+      name: "Multi character dropCap",
+      component: () => (
+        <DropCap
+          dropCap="&quot;I"
+          text="n 1924&quot; Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. If, in some respects, they may have left too deep an impression on my mind, the gain was greater than the loss.” The gain was a lifelong conviction that the central aim of domestic policy must be to avoid the horror of mass unemployment. Forget ideological posturing; the job of a responsible Conservative government was to keep people in work."
+        />
+      )
+    },
+    {
+      type: "story",
+      name: "DropCap with short text",
+      component: () => (
+        <DropCap
+          dropCap="I"
+          text="n 1924&quot; Harold Macmillan became MP for Stockton-on-Tees."
         />
       )
     }
