b2e8606d0d2df997d84af628597a3e45fd04b41d
colincclark <col.c.clark@gmail.com>
2018-10-09 14:22:29 +0100

    chore: reorder keys in pull-quote

--- packages/pull-quote/src/styles/index.js
+++ packages/pull-quote/src/styles/index.js
@@ -4,6 +4,11 @@ import sharedStyles from "./shared";
 
 const styles = StyleSheet.create({
   ...sharedStyles,
+  // iOS Font hack
+  caption: {
+    ...sharedStyles.caption,
+    paddingTop: 4
+  },
   container: {
     ...sharedStyles.container,
     marginBottom: 0,
@@ -11,11 +16,6 @@ const styles = StyleSheet.create({
     paddingLeft: spacing(2),
     paddingRight: spacing(2),
     paddingTop: spacing(1)
-  },
-  // iOS Font hack
-  caption: {
-    ...sharedStyles.caption,
-    paddingTop: 4
   },
   link: {
     ...sharedStyles.link,
