f0e3d9ef254ad2fc35012cc8513985fe7ecabe14
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-10-10 10:27:53 +0100

    Remove async from tests

--- packages/drop-cap/__tests__/shared.js
+++ packages/drop-cap/__tests__/shared.js
@@ -4,8 +4,8 @@ import "./mock-text-measure-module";
 import DropCap from "../src/drop-cap";
 
 export default () => {
-  it("paragraph with single drop cap", async () => {
-    const testInstance = await TestRenderer.create(
+  it("paragraph with single drop cap", () => {
+    const testInstance = TestRenderer.create(
       <DropCap
         dropCap="I"
         text="n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. If, in some respects, they may have left too deep an impression on my mind, the gain was greater than the loss.” The gain was a lifelong conviction that the central aim of domestic policy must be to avoid the horror of mass unemployment. Forget ideological posturing; the job of a responsible Conservative government was to keep people in work."
