25ae4acd56b02aaf011d1e902848a27039259b5a
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-10-10 11:50:16 +0100

    Shorten the text in fixture

--- packages/drop-cap/__tests__/shared.js
+++ packages/drop-cap/__tests__/shared.js
@@ -8,10 +8,10 @@ export default () => {
     const testInstance = TestRenderer.create(
       <DropCapWithContext
         dropCap="I"
-        text="n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. If, in some respects, they may have left too deep an impression on my mind, the gain was greater than the loss.” The gain was a lifelong conviction that the central aim of domestic policy must be to avoid the horror of mass unemployment. Forget ideological posturing; the job of a responsible Conservative government was to keep people in work."
+        text="n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. "
       />
     );
 
-    expect(testInstance.toJSON()).toMatchSnapshot();
+    expect(testInstance).toMatchSnapshot();
   });
 };
