7bc1e3305dad0915ed9499ca94d8fd54a9f9ae89
milesillsley <milesillsley@hotmail.co.uk>
2018-10-10 11:00:43 +0100

    pass creds to config constructor

--- uploadExpoQRCode.js
+++ uploadExpoQRCode.js
@@ -5,14 +5,13 @@ const args = process.argv.slice(2);
 
 const [imagePath, bucket, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY] = args;
 console.log("before AWS config update");
-config = new AWS.Config();
-config.update({
-  region: "eu-west-1",
-  aws_access_key_id: AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
-  aws_secret_access_key: AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY
+const config = new AWS.Config({
+  accessKeyId: AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
+  region: "eu-west-1"
 });
 console.log("after AWS config update");
-
+console.log(config)
 const s3 = new AWS.S3();
 
 const file = fs.readFileSync(imagePath);
