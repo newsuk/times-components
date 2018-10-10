af4b809ea096aa15284e7ace37c81b1f5fcfa1c2
milesillsley <milesillsley@hotmail.co.uk>
2018-10-10 10:18:34 +0100

    fix: allowUnknownKeys is true for aws config update

--- uploadExpoQRCode.js
+++ uploadExpoQRCode.js
@@ -2,13 +2,16 @@ const fs = require("fs");
 const AWS = require("aws-sdk");
 
 const args = process.argv.slice(2);
-console.log(args)
+
 const [imagePath, bucket, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY] = args;
+console.log("before AWS config update");
 AWS.config.update({
   region: "eu-west-1",
   aws_access_key_id: AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
   aws_secret_access_key: AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY
-});
+}, allowUnknownKeys = true );
+console.log("after AWS config update");
+
 const s3 = new AWS.S3();
 
 const file = fs.readFileSync(imagePath);
