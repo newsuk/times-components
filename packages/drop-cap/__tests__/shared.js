f8e12dc3a190051d28180179c1d8a2e52514d0b3
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-10-11 16:13:06 +0100

    Add test for scale changes

--- packages/drop-cap/__tests__/shared.js
+++ packages/drop-cap/__tests__/shared.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "react-native";
 import TestRenderer from "react-test-renderer";
 import {
   addSerializers,
@@ -8,8 +9,14 @@ import {
   minimalNativeTransform,
   print
 } from "@times-components/jest-serializer";
+import { scales } from "@times-components/styleguide";
 import "./mock-text-measure-module";
 import DropCapWithContext from "../src/drop-cap-with-context";
+import DropCap from "../src/drop-cap";
+
+const mockDropCap = "I";
+const mockText =
+  "n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. ";
 
 export default () => {
   addSerializers(
@@ -20,12 +27,26 @@ export default () => {
 
   it("paragraph with single drop cap", () => {
     const testInstance = TestRenderer.create(
-      <DropCapWithContext
-        dropCap="I"
-        text="n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. "
-      />
+      <DropCapWithContext dropCap={mockDropCap} text={mockText} />
     );
 
     expect(testInstance).toMatchSnapshot();
+  });
+
+  it("re-measures when scale changes", () => {
+    const testInstance = TestRenderer.create(
+      <DropCap dropCap={mockDropCap} scale={scales.large} text={mockText} />
+    );
+
+    expect(
+      testInstance.root.findAllByType(Text)[0].props.style[0].fontSize
+    ).toBe(115);
+    testInstance.update(
+      <DropCap dropCap={mockDropCap} scale={scales.xlarge} text={mockText} />
+    );
+
+    expect(
+      testInstance.root.findAllByType(Text)[0].props.style[0].fontSize
+    ).toBe(124);
   });
 };
