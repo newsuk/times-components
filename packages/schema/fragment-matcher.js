b7ccf07f9e9aa137f97cd0954ffe85f83d3fada1
colincclark <col.c.clark@gmail.com>
2018-10-12 10:55:15 +0100

    chore: ignore fragment matcher when linting cos it is dynamically generated

--- packages/schema/fragment-matcher.js
+++ packages/schema/fragment-matcher.js
@@ -4,76 +4,51 @@ const introspectionQueryResultData = {
   __schema: {
     types: [
       {
+        kind: "UNION",
+        name: "Media",
         description: "",
-        enumValues: null,
         fields: null,
         inputFields: null,
         interfaces: null,
-        kind: "UNION",
-        name: "Media",
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "Image",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "Video",
-            ofType: null
-          }
+          { kind: "OBJECT", name: "Image", ofType: null },
+          { kind: "OBJECT", name: "Video", ofType: null }
         ]
       },
       {
+        kind: "INTERFACE",
+        name: "Layout",
         description:
           "A selection of template types that have opinions over how they should be presented. Usually used within the context of an associated list of articles",
-        enumValues: null,
         fields: [
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "template",
-            type: {
-              kind: "ENUM",
-              name: "Template",
-              ofType: null
-            }
+            description: "",
+            args: [],
+            type: { kind: "ENUM", name: "Template", ofType: null },
+            isDeprecated: false,
+            deprecationReason: null
           }
         ],
         inputFields: null,
         interfaces: null,
-        kind: "INTERFACE",
-        name: "Layout",
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "LeadAndTwo",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "OpinionAndTwo",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "Default",
-            ofType: null
-          }
+          { kind: "OBJECT", name: "LeadAndTwo", ofType: null },
+          { kind: "OBJECT", name: "OpinionAndTwo", ofType: null },
+          { kind: "OBJECT", name: "Default", ofType: null }
         ]
       },
       {
+        kind: "INTERFACE",
+        name: "ArticleSlice",
         description: "",
-        enumValues: null,
         fields: [
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "items",
+            description: "",
+            args: [],
             type: {
               kind: "NON_NULL",
               name: null,
@@ -83,121 +58,46 @@ const introspectionQueryResultData = {
                 ofType: {
                   kind: "NON_NULL",
                   name: null,
-                  ofType: {
-                    kind: "OBJECT",
-                    name: "Tile",
-                    ofType: null
-                  }
+                  ofType: { kind: "OBJECT", name: "Tile", ofType: null }
                 }
               }
-            }
+            },
+            isDeprecated: false,
+            deprecationReason: null
           }
         ],
         inputFields: null,
         interfaces: null,
-        kind: "INTERFACE",
-        name: "ArticleSlice",
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "StandardSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "FocusSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadersSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneFullWidthSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "StandardSlice", ofType: null },
+          { kind: "OBJECT", name: "FocusSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadersSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndOneSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneFullWidthSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "LeadOneNoPicAndOneAndPortraitSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "LeadTwoNoPicAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryTwoAndTwoSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "LeadTwoNoPicAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryTwoAndTwoSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "SecondaryTwoNoPicAndTwoSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "TwoPicAndSixNoPicSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "CommentLeadAndCartoonSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LetterThundererPodcastSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "CommentTwoAndNotebookSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "ObituariesLeadAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "OpinionOneAndTwoSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "TwoPicAndSixNoPicSlice", ofType: null },
+          { kind: "OBJECT", name: "CommentLeadAndCartoonSlice", ofType: null },
+          { kind: "OBJECT", name: "LetterThundererPodcastSlice", ofType: null },
+          { kind: "OBJECT", name: "CommentTwoAndNotebookSlice", ofType: null },
+          { kind: "OBJECT", name: "ObituariesLeadAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "OpinionOneAndTwoSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "SecondaryOneAndColumnistSlice",
@@ -206,344 +106,176 @@ const introspectionQueryResultData = {
         ]
       },
       {
+        kind: "INTERFACE",
+        name: "Section",
         description: "",
-        enumValues: null,
         fields: [
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "id",
+            description: "",
+            args: [],
             type: {
               kind: "NON_NULL",
               name: null,
-              ofType: {
-                kind: "SCALAR",
-                name: "UUID",
-                ofType: null
-              }
-            }
+              ofType: { kind: "SCALAR", name: "UUID", ofType: null }
+            },
+            isDeprecated: false,
+            deprecationReason: null
           },
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "title",
+            description: "",
+            args: [],
             type: {
               kind: "NON_NULL",
               name: null,
-              ofType: {
-                kind: "SCALAR",
-                name: "String",
-                ofType: null
-              }
-            }
+              ofType: { kind: "SCALAR", name: "String", ofType: null }
+            },
+            isDeprecated: false,
+            deprecationReason: null
           },
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "slug",
+            description: "",
+            args: [],
             type: {
               kind: "NON_NULL",
               name: null,
-              ofType: {
-                kind: "SCALAR",
-                name: "Slug",
-                ofType: null
-              }
-            }
+              ofType: { kind: "SCALAR", name: "Slug", ofType: null }
+            },
+            isDeprecated: false,
+            deprecationReason: null
           },
           {
-            args: [],
-            deprecationReason: null,
-            description: "",
-            isDeprecated: false,
             name: "colour",
+            description: "",
+            args: [],
             type: {
               kind: "NON_NULL",
               name: null,
-              ofType: {
-                kind: "OBJECT",
-                name: "Colour",
-                ofType: null
-              }
-            }
+              ofType: { kind: "OBJECT", name: "Colour", ofType: null }
+            },
+            isDeprecated: false,
+            deprecationReason: null
           }
         ],
         inputFields: null,
         interfaces: null,
-        kind: "INTERFACE",
-        name: "Section",
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "StandardSection",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "PuzzleSection",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "MagazineSection",
-            ofType: null
-          }
+          { kind: "OBJECT", name: "StandardSection", ofType: null },
+          { kind: "OBJECT", name: "PuzzleSection", ofType: null },
+          { kind: "OBJECT", name: "MagazineSection", ofType: null }
         ]
       },
       {
-        description: "",
-        enumValues: null,
-        fields: null,
-        inputFields: null,
-        interfaces: null,
         kind: "UNION",
         name: "PuffMainLink",
-        possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "Tile",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "NamedLink",
-            ofType: null
-          }
-        ]
-      },
-      {
         description: "",
-        enumValues: null,
         fields: null,
         inputFields: null,
         interfaces: null,
+        enumValues: null,
+        possibleTypes: [
+          { kind: "OBJECT", name: "Tile", ofType: null },
+          { kind: "OBJECT", name: "NamedLink", ofType: null }
+        ]
+      },
+      {
         kind: "UNION",
         name: "PuffTopicLink",
-        possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "StandardSection",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "PuzzleSection",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "NamedLink",
-            ofType: null
-          }
-        ]
-      },
-      {
         description: "",
-        enumValues: null,
         fields: null,
         inputFields: null,
         interfaces: null,
+        enumValues: null,
+        possibleTypes: [
+          { kind: "OBJECT", name: "StandardSection", ofType: null },
+          { kind: "OBJECT", name: "PuzzleSection", ofType: null },
+          { kind: "OBJECT", name: "NamedLink", ofType: null }
+        ]
+      },
+      {
         kind: "UNION",
         name: "StandardSectionItem",
+        description: "",
+        fields: null,
+        inputFields: null,
+        interfaces: null,
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "StandardSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "PuffSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "FocusSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadersSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneFullWidthSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "StandardSlice", ofType: null },
+          { kind: "OBJECT", name: "PuffSlice", ofType: null },
+          { kind: "OBJECT", name: "FocusSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadersSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndOneSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneFullWidthSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "LeadOneNoPicAndOneAndPortraitSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "LeadTwoNoPicAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryTwoAndTwoSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "LeadTwoNoPicAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryTwoAndTwoSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "SecondaryTwoNoPicAndTwoSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "TwoPicAndSixNoPicSlice",
-            ofType: null
-          }
+          { kind: "OBJECT", name: "TwoPicAndSixNoPicSlice", ofType: null }
         ]
       },
       {
-        description: "",
-        enumValues: null,
-        fields: null,
-        inputFields: null,
-        interfaces: null,
         kind: "UNION",
         name: "PuzzleSectionItem",
-        possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "Puzzle",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "PuffSlice",
-            ofType: null
-          }
-        ]
-      },
-      {
         description: "",
-        enumValues: null,
         fields: null,
         inputFields: null,
         interfaces: null,
+        enumValues: null,
+        possibleTypes: [
+          { kind: "OBJECT", name: "Puzzle", ofType: null },
+          { kind: "OBJECT", name: "PuffSlice", ofType: null }
+        ]
+      },
+      {
         kind: "UNION",
         name: "MagazineSectionItem",
+        description: "",
+        fields: null,
+        inputFields: null,
+        interfaces: null,
+        enumValues: null,
         possibleTypes: [
-          {
-            kind: "OBJECT",
-            name: "StandardSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "PuffSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "FocusSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadersSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "LeadOneFullWidthSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "StandardSlice", ofType: null },
+          { kind: "OBJECT", name: "PuffSlice", ofType: null },
+          { kind: "OBJECT", name: "FocusSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadersSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndOneSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "LeadOneFullWidthSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "LeadOneNoPicAndOneAndPortraitSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "LeadTwoNoPicAndTwoSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryOneAndFourSlice",
-            ofType: null
-          },
-          {
-            kind: "OBJECT",
-            name: "SecondaryTwoAndTwoSlice",
-            ofType: null
-          },
+          { kind: "OBJECT", name: "LeadTwoNoPicAndTwoSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryOneAndFourSlice", ofType: null },
+          { kind: "OBJECT", name: "SecondaryTwoAndTwoSlice", ofType: null },
           {
             kind: "OBJECT",
             name: "SecondaryTwoNoPicAndTwoSlice",
             ofType: null
           },
-          {
-            kind: "OBJECT",
-            name: "TwoPicAndSixNoPicSlice",
-            ofType: null
-          }
+          { kind: "OBJECT", name: "TwoPicAndSixNoPicSlice", ofType: null }
         ]
       }
     ]
