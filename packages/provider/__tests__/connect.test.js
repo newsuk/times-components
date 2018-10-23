a018df46f6ded7d9418dff20528d45a40d5c53fe
Nate Higgins <nat@nath.is>
2018-10-23 15:54:19 +0100

    feat: use apollo query

--- packages/provider/__tests__/connect.test.js
+++ packages/provider/__tests__/connect.test.js
@@ -1,25 +1,32 @@
-import { makeGraphqlOptions } from "../src/connect";
+import { makeVariableFactory } from "../src/connect";
 
 describe("connect.js", () => {
-  it("picks only required variables from the supplied props", () => {
-    const options = makeGraphqlOptions(["a", "b"]);
-    const props = { a: 1, b: 2, c: 3 };
+  describe("makeVariableFactory", () => {
+    it("picks only required variables from the supplied props", () => {
+      const options = makeVariableFactory(["a", "b"]);
+      const props = { a: 1, b: 2, c: 3 };
 
-    expect(options(props)).toEqual({ variables: { a: 1, b: 2 } });
-  });
+      expect(options(props)).toEqual({ a: 1, b: 2 });
+    });
 
-  it("prefers the debouncedProps object, if it exists", () => {
-    const options = makeGraphqlOptions(["a", "b"]);
-    const props = { a: 1, b: 2, c: 3, debouncedProps: { a: 10, b: 20, c: 30 } };
+    it("prefers the debouncedProps object, if it exists", () => {
+      const options = makeVariableFactory(["a", "b"]);
+      const props = {
+        a: 1,
+        b: 2,
+        c: 3,
+        debouncedProps: { a: 10, b: 20, c: 30 }
+      };
 
-    expect(options(props)).toEqual({ variables: { a: 10, b: 20 } });
-  });
+      expect(options(props)).toEqual({ a: 10, b: 20 });
+    });
 
-  it("transforms props with the propsToVariables function", () => {
-    const propsToVariables = ({ a, b }) => ({ A: a + 10, B: b + 10 });
-    const options = makeGraphqlOptions(["A", "B"], propsToVariables);
-    const props = { a: 1, b: 2, c: 3 };
+    it("transforms props with the propsToVariables function", () => {
+      const propsToVariables = ({ a, b }) => ({ A: a + 10, B: b + 10 });
+      const options = makeVariableFactory(["A", "B"], propsToVariables);
+      const props = { a: 1, b: 2, c: 3 };
 
-    expect(options(props)).toEqual({ variables: { A: 11, B: 12 } });
+      expect(options(props)).toEqual({ A: 11, B: 12 });
+    });
   });
 });
