a9fbb03fcc19aeadecdc0f7578192a5fd9480828
NehaSrivastava <neha.srivastava84525@gmail.com>
2018-10-24 16:03:45 +0100

    feat: exporting client side as a separate endpoint to client side

--- packages/ssr/src/client/index.js
+++
@@ -1,9 +0,0 @@
-const articleClient = require("./article");
-const authorProfileClient = require("./author-profile");
-const topicClient = require("./topic");
-
-module.exports = {
-  article: articleClient,
-  authorProfile: authorProfileClient,
-  topic: topicClient
-};
