877066b40261179317057500aa61738a3fa9ded9
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-10-26 16:44:22 +0100

    Comment error state

--- packages/article-comments/article-comments.showcase.js
+++ packages/article-comments/article-comments.showcase.js
@@ -1,13 +1,35 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import ArticleComments from "./src/article-comments";
+import CommentError from "./src/comment-error";
 
+const renderComments = ({ enabled }) => (
+  <ArticleComments
+    articleId="dummy-article-id"
+    commentCount={123}
+    commentsEnabled={enabled}
+    onCommentGuidelinesPress={() => { }}
+    onCommentsPress={() => { }}
+    url="dummy-article-url"
+  />
+)
 export default {
   children: [
     {
-      component: () => <ArticleComments />,
-      name: "ArticleComments",
+      component: () => renderComments({ enabled: true }),
+      name: "Enabled",
+      type: "story"
+    },
+    {
+      component: () => renderComments({ enabled: false }),
+      name: "Disabled",
+      type: "story"
+    },
+    {
+      component: () => (<CommentError refetch={() => { }} />),
+      name: "Error",
       type: "story"
     }
   ],
-  name: "ArticleComments"
+  name: "Primitives/ArticleComments"
 };
