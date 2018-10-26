88d1126c562c254841fb02a1f5d15d97de998c94
colincclark <col.c.clark@gmail.com>
2018-10-26 15:45:33 +0100

    test: split out shared tests into web and native

--- packages/article/__tests__/images.native.js
+++ packages/article/__tests__/images.native.js
@@ -6,7 +6,7 @@ import {
   minimalNativeTransform
 } from "@times-components/jest-serializer";
 import "./mocks.native";
-import shared from "./images.base";
+import shared from "./images.base.native";
 
 jest.mock("../src/article-comments/article-comments", () => "ArticleComments");
 
--- packages/article/__tests__/shared-with-style.native.js
+++ packages/article/__tests__/shared-with-style.native.js
@@ -9,7 +9,7 @@ import {
 } from "@times-components/jest-serializer";
 import { iterator } from "@times-components/test-utils";
 import "./mocks.native";
-import { snapshotTests } from "./shared.base";
+import { snapshotTests } from "./shared.base.native";
 
 jest.mock("../src/article-comments/article-comments", () => "ArticleComments");
 
