86b2b62feb718e990cc86333b16c2fea8f77740e
L0wry <matthewlowry@hotmail.co.uk>
2018-11-07 14:35:31 +0000

    chore: cleaning mock tpa package

--- packages/ssr/mock-server/index.js
+++ packages/ssr/mock-server/index.js
@@ -1,54 +1,7 @@
-/* eslint-disable no-console */
+const mockTpa = require("../../mock-tpa-server/dist/index");
 
-const { ApolloServer } = require("apollo-server");
-const { article, authorProfile, makeMocks, authorProfileNew } = require("@times-components/provider-test-tools");
-
-function combineDefaults(...bits) {
-  let defaults = {};
-
-  bits.forEach(bit => {
-    Object.assign(defaults, ...bit.map(d => ({
-      types: d.defaults.types,
-      values: d.defaults.values
-    })));
-  });
-
-  return defaults;
-}
-
-//TODO: Desired way of doing it. (uses all exports - overrides each other atm)
-// const defaults = combineDefaults(
-//   authorProfile({slug: 'baked-beanzz', pageSize: 1}),
-//   article()
-// );
-
-// const [{defaults: articleDefaults}] = article();
-// const [{defaults: authorDefaults}] = authorProfile({slug: 'deborah-haynes', pageSize: 10, articleCount: 22});
-
-// const defaults = {
-//   types: Object.assign({}, articleDefaults.types, authorDefaults.types),
-//   values: Object.assign({}, articleDefaults.values, authorDefaults.values)
-// };
-
-const schema = makeMocks(authorProfileNew());
-
-const server = new ApolloServer({ schema,
-  formatError: error => {
-    console.log(error);
-    return new Error('Internal server error');
-  },
-  formatResponse: response => {
-    console.log(response);
-    return response;
-  }
-});
-
-const App = server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+mockTpa.start();
 
 process.on("SIGTERM", () => {
-  App.close(() => {
-    process.exit(0);
-  });
+  mockTpa.stop();
 });
