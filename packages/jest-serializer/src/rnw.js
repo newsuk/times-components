06f211f38c452b1104d66e57ba6acf87b6948272
colincclark <col.c.clark@gmail.com>
2018-11-12 09:52:50 +0000

    refactor: add back legit code

--- packages/jest-serializer/src/rnw.js
+++ packages/jest-serializer/src/rnw.js
@@ -36,7 +36,7 @@ const getStyleSheet = AppRegistry => {
   const { getStyleElement } = AppRegistry.getApplication("App");
   const ssString = ReactDOMServer.renderToStaticMarkup(getStyleElement());
 
-  const [, match] = ssString.match(/<style[\s\S]*>([\s\S]*)<\/style>/);
+  const [, match] = ssString.match(/<style.*>(.*)<\/style>/s);
 
   if (!match) {
     return {};
