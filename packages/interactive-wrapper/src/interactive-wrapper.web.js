6653643168cae56572432e9dee5568ef1c254242
Nate Higgins <nat@nath.is>
2018-11-14 14:05:54 +0000

    fix: potential solution for fixing interactives

--- packages/interactive-wrapper/src/interactive-wrapper.web.js
+++ packages/interactive-wrapper/src/interactive-wrapper.web.js
@@ -1,16 +1,36 @@
 /* eslint-disable react/forbid-prop-types */
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export default function InteractiveWrapper(props) {
-  const Element = props.element;
-  return (
-    <Fragment>
-      <link href={props.source} rel="import" />
-      <Element {...props.attributes} />
-    </Fragment>
-  );
+export default class InteractiveWrapper extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.div = React.createRef(null);
+  }
+
+  componentDidMount() {
+    const div = this.div.current;
+    const parent = div.parentNode;
+    const element = document.createElement(this.props.element);
+    const link = document.createElement('link');
+
+    link.setAttribute('href', this.props.source);
+    link.setAttribute('rel', 'import');
+
+    Object.entries(this.props.attributes).forEach(
+      ([key, value]) => element.setAttribute(key, value)
+    );
+
+    parent.appendChild(element);
+    parent.appendChild(link);
+    parent.removeChild(div);
+  }
+
+  render() {
+    return <div ref={this.div} />;
+  }
 }
+
 InteractiveWrapper.propTypes = {
   attributes: PropTypes.object,
   element: PropTypes.string.isRequired,
