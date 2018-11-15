1c3eb81e76c1aee1b01edea08bb4eeb80c7b2954
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2018-11-15 10:54:25 +0000

    feat: Add referral url to article analytics (#1487)
    * Add referral url to article analytics
    
    * Fix formatting
    
    * Update snaps

--- packages/article/src/article-tracking-context.js
+++ packages/article/src/article-tracking-context.js
@@ -3,13 +3,14 @@ import { withTrackingContext } from "@times-components/tracking";
 
 export default Component =>
   withTrackingContext(Component, {
-    getAttrs: ({ data, pageSection }) => ({
+    getAttrs: ({ data, pageSection, referralUrl = "" }) => ({
       articleId: get(data, "id", ""),
       byline: get(data, "byline[0].children[0].attributes.value", ""),
       headline: get(data, "headline", ""),
       label: get(data, "label", ""),
       pageName: `${get(data, "slug", "")}-${get(data, "shortIdentifier", "")}`,
       publishedTime: get(data, "publishedTime", ""),
+      referralUrl,
       section: pageSection || get(data, "section", ""),
       template: get(data, "template", "Default"),
       topics: get(data, "topics", [])
