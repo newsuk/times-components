acc7cf8cc9d75be4e2b7da7ce12369126b79ea34
Nate Higgins <nat@nath.is>
2018-11-16 10:48:12 +0000

    chore: mock InteractiveWrapper in article main standard tests

--- packages/article-main-standard/__tests__/mocks.web.js
+++ packages/article-main-standard/__tests__/mocks.web.js
@@ -14,7 +14,12 @@ jest.mock("@times-components/article-topics", () => "ArticleTopics");
 jest.mock("@times-components/caption", () => "Caption");
 jest.mock("@times-components/date-publication", () => "DatePublication");
 jest.mock("@times-components/image", () => "Image");
-jest.mock("@times-components/link", () => "Link");
+jest.mock("@times-components/interactive-wrapper", () => "InteractiveWrapper");
+jest.mock("@times-components/link", () => ({
+  __esModule: true,
+  default: "Link",
+  TextLink: "TextLink"
+}));
 jest.mock("@times-components/pull-quote", () => "PullQuote");
 jest.mock("@times-components/related-articles", () => "RelatedArticles");
 jest.mock("@times-components/video-label", () => "VideoLabel");
