270811d492ae213ff9c3deff377604886450baf5
Aron Shamash <aronshamash@gmail.com>
2018-11-20 12:11:20 +0000

    refactor: DRY up the ad config

--- packages/ssr/src/lib/ads/make-topic-ad-config.js
+++ packages/ssr/src/lib/ads/make-topic-ad-config.js
@@ -1,31 +1,10 @@
 const { defaultClient, defaultServer } = require("./base-ad-config");
+const listPageTargeting = require("./list-page-targeting");
 
 module.exports = {
   defaultClient: () => ({
     ...defaultClient,
-    pageTargeting: {
-      aid: "null",
-      breakpoint: "medium",
-      cips: null,
-      cont: "listing",
-      e_uuid: "null",
-      edition_id: "null",
-      eid: "JEK5925426746",
-      ksg: "rzxmnyauv",
-      kuid: "L3uO9V7m",
-      kw: "null",
-      log: "1",
-      om_v_id: "0159ff285be100121d968d18a23605078001607000c48",
-      ppid: "JEK5925426746",
-      pw: "1",
-      ra: "1",
-      refresh: "false",
-      search: "null",
-      share_token: "null",
-      shared: "0",
-      teaser: false,
-      testmode: "null"
-    },
+    ...listPageTargeting,
     slotTargeting: {
       path: "topic/chelsea",
       section: "topic/chelsea",
