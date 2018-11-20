25fdb13626507eeea44544d16a06728c5caceda5
James Miller <jimhunty@gmail.com>
2018-11-20 15:10:21 +0000

    test: getHeadline test added to utils package (#1501)
    * test: getHeadline test added to utils package

---
+++ packages/utils/__tests__/get-headline.test.js
@@ -0,0 +1,18 @@
+import { getHeadline } from "../src";
+
+const headline = "A really long headline";
+const shortHeadline = "Short Headline";
+
+describe("getHeadline", () => {
+  it("should give the headline over the short headline if provided", () => {
+    expect(getHeadline(headline, shortHeadline)).toEqual(headline);
+  });
+
+  it("should give the short headline if not headline is provided", () => {
+    expect(getHeadline(null, shortHeadline)).toEqual(shortHeadline);
+  });
+
+  it("should give the headline over the short headline if the short headline is not provided", () => {
+    expect(getHeadline(headline, null)).toEqual(headline);
+  });
+});
