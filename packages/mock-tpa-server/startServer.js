68f65c617ba2e26d5ed0e5d6ba9b0e01225be29d
L0wry <matthewlowry@hotmail.co.uk>
2018-11-23 11:23:28 +0000

    fix: removing start server

--- packages/mock-tpa-server/startServer.js
+++
@@ -1,8 +0,0 @@
-const { MockArticle } = require("@times-components/fixture-generator")
-const mockTpa = require("./dist/index")
-
-mockTpa.startWithMockData({Article:new MockArticle()
-    .sundayTimes()
-    .withRelatedArticles(3)
-    .create()
-    })
