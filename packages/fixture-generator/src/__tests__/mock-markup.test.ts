cea0fe721ca1ea152c7f81ab9e5cf4e97975b148
L0wry <matthewlowry@hotmail.co.uk>
2018-11-28 16:59:56 +0000

    test: switch to snapshot

--- packages/fixture-generator/src/__tests__/mock-markup.test.ts
+++ packages/fixture-generator/src/__tests__/mock-markup.test.ts
@@ -1,32 +1,141 @@
-import MockMarkup from "../mock-markup"
+import MockMarkup from "../mock-markup";
 
 describe("get markup", () => {
-
   it("returns an array", () => {
-    expect(new MockMarkup().get()).toEqual([])
-  })
+    expect(new MockMarkup().get()).toEqual([]);
+  });
 
-  it("should be able to generate paragraphs of markup", () => {
-    const mockMarkup = new MockMarkup().withParagraph().get()
-    for (const markup of mockMarkup) {
-      expect(markup.name).toBe('paragraph')
-    }
-  })
+  it("should be able to generate paragraph of markup", () => {
+    expect(new MockMarkup().withParagraph().get()).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+]
+`);
+  });
 
-  it("should be able to generate ads of markup", () => {
-    const mockMarkup = new MockMarkup().withAd().get()
-    for (const markup of mockMarkup) {
-      expect(markup.name).toBe('ad')
-    }
-  })
+  it("should be able to generate markup with ads", () => {
+    expect(new MockMarkup().withAd().get()).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "attributes": Object {},
+    "children": Array [],
+    "name": "ad",
+  },
+]
+`);
+  });
 
   it("should be able to generate inline of markup", () => {
-    const mockMarkup = new MockMarkup().withInline().withInline().get()
-    for (const markup of mockMarkup) {
-      expect(markup.name).toBe('inline')
-    }
-  })
+    expect(new MockMarkup().withInline().get()).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "inline markup",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "inline",
+  },
+]
+`);
+  });
 
-  it("should generate large markup shapes", () => 
-    expect(new MockMarkup().withParagraphs(5).withAd().get()).toMatchSnapshot())
-})
+  it("should generate large markup shapes", () => {
+    expect(
+      new MockMarkup()
+        .withParagraphs(5)
+        .withAd()
+        .get()
+    ).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+  Object {
+    "attributes": Object {},
+    "children": Array [
+      Object {
+        "attributes": Object {
+          "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        },
+        "children": Array [],
+        "name": "text",
+      },
+    ],
+    "name": "paragraph",
+  },
+  Object {
+    "attributes": Object {},
+    "children": Array [],
+    "name": "ad",
+  },
+]
+`);
+  });
+});
