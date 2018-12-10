fffabadd68b6067b888bc2e3bd2850e80cf493ad
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-12-10 12:11:09 +0000

    Add a test case for nested markups in first paragraph for drop cap tests

---
+++ packages/article-skeleton/fixtures/bold-article-content.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    children: [
+      {
+        children: [
+          {
+            attributes: {
+              value:
+                "Q Some years ago I made the decision to take as a friend a woman who is very opinionated."
+            },
+            children: [],
+            name: "text"
+          }
+        ],
+        name: "bold"
+      }
+    ],
+    name: "paragraph"
+  },
+  {
+    children: [
+      {
+        children: [
+          {
+            attributes: {
+              value: "When she does come to social events, she dominates."
+            },
+            children: [],
+            name: "text"
+          }
+        ],
+        name: "bold"
+      }
+    ],
+    name: "paragraph"
+  }
+];
