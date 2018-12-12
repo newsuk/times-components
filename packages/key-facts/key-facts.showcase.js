3e09b2a4fee5bcc1bf44c18cfefe0b7357db7c79
Colin Clark <col.c.clark@gmail.com>
2018-12-12 10:18:53 +0000

    fix: key facts stories (#1566)
    * fix: split web and native stories
    * fix: remove scaling from the web story of key facts
    * refactor: DRY up showcases with a helper

--- packages/key-facts/key-facts.showcase.js
+++ packages/key-facts/key-facts.showcase.js
@@ -1,27 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import invert from "lodash.invert";
 import { ScrollView } from "react-native";
-import Context from "@times-components/context";
 import { LateralSpacingDecorator } from "@times-components/storybook";
-import { colours, scales } from "@times-components/styleguide";
-import KeyFacts from "./src/key-facts";
 import data from "./fixtures/key-facts-showcase.json";
 import dataNoTitle from "./fixtures/key-facts-no-title-showcase.json";
-
-const selectSection = select =>
-  select("Section", invert(colours.section), colours.section.default);
-const selectScales = select => select("Scale", scales, scales.medium);
-
-const renderKeyFacts = (ast, select) => {
-  const scale = selectScales(select);
-  const sectionColour = selectSection(select);
-  return (
-    <Context.Provider value={{ theme: { scale, sectionColour } }}>
-      <KeyFacts ast={ast} onLinkPress={() => {}} />
-    </Context.Provider>
-  );
-};
+import renderKeyFacts from "./showcase-helper";
 
 export default {
   children: [
@@ -32,33 +15,21 @@ export default {
     {
       component: ({ select }) => (
         <ScrollView style={{ width: "100%" }}>
-          {renderKeyFacts(data, select)}
+          {renderKeyFacts({ ast: data, hasScaling: true, select })}
         </ScrollView>
       ),
       name: "with title",
       platform: "native",
-      type: "story"
-    },
-    {
-      component: ({ select }) => renderKeyFacts(data, select),
-      name: "with title",
-      platform: "web",
       type: "story"
     },
     {
       component: ({ select }) => (
         <ScrollView style={{ width: "100%" }}>
-          {renderKeyFacts(dataNoTitle, select)}
+          {renderKeyFacts({ ast: dataNoTitle, hasScaling: true, select })}
         </ScrollView>
       ),
       name: "without title",
       platform: "native",
-      type: "story"
-    },
-    {
-      component: ({ select }) => renderKeyFacts(dataNoTitle, select),
-      name: "without title",
-      platform: "web",
       type: "story"
     }
   ],
