207543c870fd40608521e9c8740d0cd5f8e4688f
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-12-13 15:20:13 +0000

    Add magazine specific fonts to theme factory

--- packages/styleguide/src/theme/theme-factory.js
+++ packages/styleguide/src/theme/theme-factory.js
@@ -1,4 +1,5 @@
 import sectionColours, { secondarySectionColours } from "../colours/section";
+import fonts from "../fonts/fonts";
 
 const sectionColourPicker = (
   section = "default",
@@ -28,6 +29,41 @@ const sectionColourPicker = (
   return config[template][section];
 };
 
+const magazineFontPicker = (
+  defaultFont,
+  section = "default",
+  template = "mainstandard"
+) => {
+  const config = {
+    indepth: {
+      culture: fonts.cultureMagazine,
+      style: fonts.styleMagazine,
+      thesundaytimesmagazine: fonts.stMagazine
+    },
+    magazinecomment: {
+      culture: fonts.cultureMagazine,
+      style: fonts.styleMagazine,
+      thesundaytimesmagazine: fonts.stMagazine
+    },
+    magazinestandard: {
+      culture: fonts.cultureMagazine,
+      style: fonts.styleMagazine,
+      thesundaytimesmagazine: fonts.stMagazine
+    },
+    maincomment: {},
+    mainstandard: {
+      culture: fonts.cultureMagazine,
+      style: fonts.styleMagazine,
+      thesundaytimesmagazine: fonts.stMagazine
+    }
+  };
+
+  return config[template][section] || defaultFont;
+};
+
 export default (section, template) => ({
+  dropCapFont: magazineFontPicker(fonts.dropCap, section, template),
+  headlineFont: magazineFontPicker(fonts.headline, section, template),
+  pullQuoteFont: magazineFontPicker(fonts.headlineRegular, section, template),
   sectionColour: sectionColourPicker(section, template)
 });
