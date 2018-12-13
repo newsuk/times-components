650df9f1acac9e1b3f09c5e1b0c80f4ed13f8f08
Colin Clark <col.c.clark@gmail.com>
2018-12-13 10:19:24 +0000

    fix: remove web scaling in article storybook (#1569)

--- packages/article/showcase-helper.js
+++ packages/article/showcase-helper.js
@@ -1,12 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies, no-bitwise, operator-assignment, react/prop-types */
-
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import articleAdConfig from "@times-components/ad/fixtures/article-ad-config.json";
+import Context, { defaults } from "@times-components/context";
+import { ArticleProvider } from "@times-components/provider";
 import {
   article as makeParams,
   fixtures,
   MockedProvider,
   schemaToMocks
 } from "@times-components/provider-test-tools";
+import { sections } from "@times-components/storybook";
+import { scales, themeFactory } from "@times-components/styleguide";
+import storybookReporter from "@times-components/tealium-utils";
+import Article, { templates } from "./src/article";
+
+const makeArticleUrl = ({ slug, shortIdentifier }) =>
+  slug && shortIdentifier
+    ? `https://www.thetimes.co.uk/article/${slug}-${shortIdentifier}`
+    : "";
+
+const preventDefaultedAction = decorateAction =>
+  decorateAction([
+    ([e, ...args]) => {
+      e.preventDefault();
+      return ["[SyntheticEvent (storybook prevented default)]", ...args];
+    }
+  ]);
 
 const FLAGS = 1;
 const HEADLINE = 2;
@@ -92,7 +111,7 @@ const makeArticle = configuration => article => {
   return configuredArticle;
 };
 
-export class ArticleConfigurator extends Component {
+class ArticleConfigurator extends Component {
   constructor(props) {
     super(props);
 
@@ -145,3 +164,136 @@ export class ArticleConfigurator extends Component {
     return <MockedProvider mocks={mocks}>{children}</MockedProvider>;
   }
 }
+
+const renderArticle = ({
+  adConfig = articleAdConfig,
+  analyticsStream,
+  decorateAction,
+  id,
+  scale,
+  section,
+  template
+}) => (
+  <ArticleProvider debounceTimeMs={0} id={id}>
+    {({ article, error, refetch }) => {
+      if (!article) {
+        return null;
+      }
+
+      const data = {
+        ...article,
+        author: {
+          image:
+            "https://feeds.thetimes.co.uk/web/imageserver/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F0694e84e-04ff-11e7-976a-0b4b9a1a67a3.jpg?crop=854,854,214,0&resize=400"
+        },
+        template
+      };
+      return (
+        <Context.Provider
+          value={{
+            makeArticleUrl,
+            theme: {
+              ...themeFactory(section, template),
+              scale: scale || defaults.theme.scale
+            }
+          }}
+        >
+          <Article
+            adConfig={adConfig}
+            analyticsStream={analyticsStream}
+            article={data}
+            error={error}
+            isLoading={false}
+            onAuthorPress={preventDefaultedAction(decorateAction)(
+              "onAuthorPress"
+            )}
+            onCommentGuidelinesPress={preventDefaultedAction(decorateAction)(
+              "onCommentGuidelinesPress"
+            )}
+            onCommentsPress={preventDefaultedAction(decorateAction)(
+              "onCommentsPress"
+            )}
+            onLinkPress={preventDefaultedAction(decorateAction)("onLinkPress")}
+            onRelatedArticlePress={preventDefaultedAction(decorateAction)(
+              "onRelatedArticlePress"
+            )}
+            onTopicPress={preventDefaultedAction(decorateAction)(
+              "onTopicPress"
+            )}
+            onTwitterLinkPress={preventDefaultedAction(decorateAction)(
+              "onTwitterLinkPress"
+            )}
+            onVideoPress={preventDefaultedAction(decorateAction)(
+              "onVideoPress"
+            )}
+            refetch={refetch}
+          />
+        </Context.Provider>
+      );
+    }}
+  </ArticleProvider>
+);
+
+const templateNames = Object.keys(templates).reduce(
+  (t, key) => ({ ...t, [key]: key }),
+  {}
+);
+
+const selectScales = select => select("Scale", scales, scales.medium);
+const selectSection = select =>
+  sections[select("Section", sections, sections.default)];
+const selectTemplate = select =>
+  select("Template", templateNames, templateNames.mainstandard);
+
+const renderArticleConfig = ({
+  boolean,
+  decorateAction,
+  hasScaling,
+  link = null,
+  select
+}) => {
+  const id = "263b03a1-2ce6-4b94-b053-0d35316548c5";
+  const withFlags = boolean("Flags", true);
+  const withHeadline = boolean("Headline", true);
+  const withLabel = boolean("Label", true);
+  const withLeadAsset = boolean("Lead Asset", true);
+  const withLinkedByline = boolean("Linked Byline", true);
+  const withStandfirst = boolean("Standfirst", true);
+  const withVideo = boolean("Video", true);
+
+  const scale = hasScaling ? selectScales(select) : null;
+  const section = selectSection(select);
+  const template = selectTemplate(select);
+
+  return (
+    <Fragment>
+      {link}
+      {
+        <ArticleConfigurator
+          configuration={makeArticleConfiguration({
+            withFlags,
+            withHeadline,
+            withLabel,
+            withLeadAsset,
+            withLinkedByline,
+            withStandfirst,
+            withVideo
+          })}
+          id={id}
+        >
+          {renderArticle({
+            adConfig: articleAdConfig,
+            analyticsStream: storybookReporter,
+            decorateAction,
+            id,
+            scale,
+            section,
+            template
+          })}
+        </ArticleConfigurator>
+      }
+    </Fragment>
+  );
+};
+
+export default renderArticleConfig;
