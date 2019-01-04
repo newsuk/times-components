55b394847d44a4790d12b7c469eb6e90b2868675
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-01-04 13:30:38 +0000

    Author profile and topics max width on description

--- packages/author-profile/src/styles/index.js
+++ packages/author-profile/src/styles/index.js
@@ -1,9 +1,14 @@
 import { StyleSheet } from "react-native";
 import { spacing } from "@times-components/styleguide";
+import { tabletWidth } from "@times-components/utils";
 import sharedStyles from "./shared";
 
 const styles = StyleSheet.create({
   ...sharedStyles,
+  biography: {
+    ...sharedStyles.biography,
+    maxWidth: tabletWidth
+  },
   twitter: {
     ...sharedStyles.twitter,
     marginTop: spacing(1)
