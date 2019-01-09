ef552749484ed337605cc5de352544db6609cd25
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-01-09 13:11:42 +0000

    Add an orientation change test in responsive package

--- packages/responsive/__tests__/shared.js
+++ packages/responsive/__tests__/shared.js
@@ -2,6 +2,24 @@ import React from "react";
 import TestRenderer from "react-test-renderer";
 import Responsive, { ResponsiveContext } from "../src/responsive";
 
+let dimensionChangeHandler;
+const dimensionUpdate = newWidth => {
+  if (dimensionChangeHandler) {
+    dimensionChangeHandler({
+      screen: { height: 500, width: newWidth },
+      window: { height: 500, width: newWidth }
+    });
+  }
+};
+jest.mock("Dimensions", () => ({
+  addEventListener: (event, handler) => {
+    if (event === "change") {
+      dimensionChangeHandler = handler;
+    }
+  },
+  get: () => ({ height: 500, width: 750 })
+}));
+
 export default () => {
   it("with default values", () => {
     const testInstance = TestRenderer.create(
@@ -13,5 +31,8 @@ export default () => {
     );
 
     expect(testInstance).toMatchSnapshot();
+
+    dimensionUpdate(1000);
+    expect(testInstance).toMatchSnapshot("after width update");
   });
 };
