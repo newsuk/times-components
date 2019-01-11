2802a16c1043ffa5b45367c6ca43986fc95af14d
Nate Higgins <nat@nath.is>
2019-01-11 13:43:27 +0000

    fix: pull quotes and drop caps should be aligned correctly at different scales (#1600)

--- packages/article-paragraph/src/styles/drop-cap-sizes.js
+++ packages/article-paragraph/src/styles/drop-cap-sizes.js
@@ -1,9 +1,9 @@
 const fontSize = (font, scale) => {
   const config = {
     cultureMagazine: {
-      large: 115,
+      large: 110,
       medium: 90,
-      xlarge: 124
+      xlarge: 122
     },
     dropCap: {
       large: 115,
@@ -11,14 +11,14 @@ const fontSize = (font, scale) => {
       xlarge: 124
     },
     stMagazine: {
-      large: 115,
+      large: 113,
       medium: 90,
-      xlarge: 124
+      xlarge: 120
     },
     styleMagazine: {
-      large: 115,
+      large: 112,
       medium: 90,
-      xlarge: 124
+      xlarge: 119
     }
   };
 
@@ -28,9 +28,9 @@ const fontSize = (font, scale) => {
 const margins = (font, scale) => {
   const config = {
     cultureMagazine: {
-      large: { bottom: -26, top: -14 },
+      large: { bottom: -20, top: -23 },
       medium: { bottom: -30, top: -17 },
-      xlarge: { bottom: -22, top: -16 }
+      xlarge: { bottom: -22, top: -26 }
     },
     dropCap: {
       large: { bottom: -26, top: -14 },
@@ -38,14 +38,14 @@ const margins = (font, scale) => {
       xlarge: { bottom: -22, top: -16 }
     },
     stMagazine: {
-      large: { bottom: -26, top: -14 },
+      large: { bottom: -26, top: -22 },
       medium: { bottom: -30, top: -17 },
-      xlarge: { bottom: -22, top: -16 }
+      xlarge: { bottom: -22, top: -24 }
     },
     styleMagazine: {
-      large: { bottom: -26, top: -14 },
+      large: { bottom: -26, top: -25 },
       medium: { bottom: -30, top: -17 },
-      xlarge: { bottom: -22, top: -16 }
+      xlarge: { bottom: -22, top: -26 }
     }
   };
 
