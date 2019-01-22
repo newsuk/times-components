008e4fb6cbce946c000022a532708993be7c229e
James Miller <james@james-miller.co.uk>
2019-01-22 15:36:01 +0000

    test: adding Article Paragraph responsive tests

--- packages/article-paragraph/__tests__/shared-with-style.native.js
+++ packages/article-paragraph/__tests__/shared-with-style.native.js
@@ -1,3 +1,4 @@
+import "./mocks.native";
 import TestRenderer from "react-test-renderer";
 import {
   addSerializers,
@@ -7,9 +8,11 @@ import {
   flattenStyleTransform,
   print
 } from "@times-components/jest-serializer";
+import { mockSetIsTablet as setIsTablet } from "@times-components/responsive";
 import { delay, iterator } from "@times-components/test-utils";
 import renderParagraph from "./renderer";
 import dropCapData from "./fixtures/drop-cap-showcase.json";
+import paragraphData from "./fixtures/paragraph-showcase.json"
 
 export default () => {
   addSerializers(
@@ -26,6 +29,24 @@ export default () => {
     {
       name: "paragraph with a drop cap",
       test: async () => {
+        const testInstance = TestRenderer.create(renderParagraph(dropCapData));
+        await delay(0);
+        expect(testInstance).toMatchSnapshot();
+      }
+    },
+    {
+      name: "responsive tablet paragraph",
+      test: async () => {
+        setIsTablet(true);
+        const testInstance = TestRenderer.create(renderParagraph(paragraphData));
+        await delay(0);
+        expect(testInstance).toMatchSnapshot();
+      }
+    },
+    {
+      name: "responsive tablet paragraph with a drop cap",
+      test: async () => {
+        setIsTablet(true);
         const testInstance = TestRenderer.create(renderParagraph(dropCapData));
         await delay(0);
         expect(testInstance).toMatchSnapshot();
