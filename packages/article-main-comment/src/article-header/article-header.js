6ba6dc1157ae45fefd6f4f94a8665ae6c8e46edb
Nate Higgins <nat@nath.is>
2019-01-24 15:32:10 +0000

    feat: main comment tablet header design

--- packages/article-main-comment/src/article-header/article-header.js
+++ packages/article-main-comment/src/article-header/article-header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
+import { ResponsiveContext } from "@times-components/responsive";
 import Label from "../article-label/article-label";
 import Flags from "../article-flags/article-flags";
 import Meta from "../article-meta/article-meta";
@@ -24,19 +25,25 @@ const ArticleHeader = ({
   publishedTime,
   standfirst
 }) => (
-  <View style={styles.container}>
-    <Image aspectRatio={1} style={styles.authorImage} uri={authorImage} />
-    <Label isVideo={hasVideo} label={label} />
-    <Text style={styles.articleHeadline}>{headline}</Text>
-    <Flags flags={flags} />
-    <Standfirst standfirst={standfirst} />
-    <Meta
-      byline={byline}
-      onAuthorPress={onAuthorPress}
-      publicationName={publicationName}
-      publishedTime={publishedTime}
-    />
-  </View>
+  <ResponsiveContext.Consumer>
+    {({ isTablet }) => (
+      <View style={styles.header}>
+        <View style={[styles.container, isTablet && styles.containerTablet]}>
+          <Image aspectRatio={1} style={styles.authorImage} uri={authorImage} />
+          <Label isVideo={hasVideo} label={label} />
+          <Text style={styles.articleHeadline}>{headline}</Text>
+          <Flags flags={flags} />
+          <Standfirst standfirst={standfirst} />
+          <Meta
+            byline={byline}
+            onAuthorPress={onAuthorPress}
+            publicationName={publicationName}
+            publishedTime={publishedTime}
+          />
+        </View>
+      </View>
+    )}
+  </ResponsiveContext.Consumer>
 );
 
 ArticleHeader.propTypes = {
