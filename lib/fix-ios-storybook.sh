e72686091b3c187d23c8fc1b5fc25fdd47c05d86
Daniel Devesa <daniel.devesa@desfici.com>
2019-01-29 12:10:25 +0000

    fix: update fix script to install third party dependencies before configuring glog and copying the libfishhook.a library in the right location

--- lib/fix-ios-storybook.sh
+++ lib/fix-ios-storybook.sh
@@ -3,5 +3,8 @@
 # Fix for the missing 'config.h' file in the iOS Storybook app
 # This is needed on the version 0.55.4 of React Native and can be removed once upgraded
 
-cd node_modules/react-native/third-party/glog-0.3.4
-./configure
+./node_modules/react-native/scripts/ios-install-third-party.sh
+pushd node_modules/react-native/third-party/glog-*
+../../scripts/ios-configure-glog.sh
+popd
+cp ios/build/Build/Products/Debug-iphonesimulator/libfishhook.a node_modules/react-native/Libraries/WebSocket/;
