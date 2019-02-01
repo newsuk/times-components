97caf578b1c6493e8ae735c78e0b6187bdad7bfc
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-02-01 11:32:46 +0000

    Update the script

--- lib/fix-ios-storybook.sh
+++ lib/fix-ios-storybook.sh
@@ -4,8 +4,10 @@
 # The libfishhook.a library is not properly linked either
 # This is needed on the version 0.55.4 of React Native and can be removed once upgraded
 
-./node_modules/react-native/scripts/ios-install-third-party.sh
+pushd node_modules/react-native/
+./scripts/ios-install-third-party.sh
+popd
 pushd node_modules/react-native/third-party/glog-*
 ../../scripts/ios-configure-glog.sh
 popd
-cp ios/build/Build/Products/Debug-iphonesimulator/libfishhook.a node_modules/react-native/Libraries/WebSocket/;
+cp ios/build/Build/Products/Debug-iphonesimulator/libfishhook.a node_modules/react-native/Libraries/WebSocket/ | true
