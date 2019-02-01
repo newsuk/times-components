459d400291e47e67441e15281c346e05708bcb14
Daniel Devesa <daniel.devesa@desfici.com>
2019-02-01 09:53:36 +0000

    chore: update comment

--- lib/fix-ios-storybook.sh
+++ lib/fix-ios-storybook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Fix for the missing 'config.h' file in the iOS Storybook app
+# The libfishhook.a library is not properly linked either
 # This is needed on the version 0.55.4 of React Native and can be removed once upgraded
 
 ./node_modules/react-native/scripts/ios-install-third-party.sh
