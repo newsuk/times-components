94ddf869f3ba1c43c0c775a0f4753087193fc66b
Nate Higgins <nat@nath.is>
2019-02-01 11:24:21 +0000

    feat: fullwidth image display type (#1653)

--- packages/article-image/article-image.showcase.js
+++ packages/article-image/article-image.showcase.js
@@ -1,20 +1,27 @@
 import React from "react";
+import Responsive from "@times-components/responsive";
 import ArticleImage from "./src/article-image";
 import primaryImageFixture from "./fixtures/primary-image";
 import secondaryImageFixture from "./fixtures/secondary-image";
+import fullwidthImageFixture from "./fixtures/fullwidth-image";
 import landscapeInlineImageFixture from "./fixtures/landscape-inline-image";
 import portraitInlineImageFixture from "./fixtures/portrait-inline-image";
 
 const primaryImage = primaryImageFixture();
 const secondaryImage = secondaryImageFixture();
+const fullwidthImage = fullwidthImageFixture();
 const landscapeInlineImage = landscapeInlineImageFixture();
 const portraitInlineImage = portraitInlineImageFixture();
+
+const withResponsive = render => (...args) => (
+  <Responsive>{render(...args)}</Responsive>
+);
 
 export default {
   children: [
     {
       // eslint-disable-next-line react/prop-types
-      component: ({ boolean }) => {
+      component: withResponsive(({ boolean }) => {
         const withHighRes = boolean("As high resolution");
 
         const imageOptions = {
@@ -33,37 +40,47 @@ export default {
             imageOptions={imageOptions}
           />
         );
-      },
+      }),
       name: "Primary",
       type: "story"
     },
     {
-      component: () => (
+      component: withResponsive(() => (
         <ArticleImage
           captionOptions={secondaryImage.captionOptions}
           imageOptions={secondaryImage.imageOptions}
         />
-      ),
+      )),
       name: "Secondary",
       type: "story"
     },
     {
-      component: () => (
+      component: withResponsive(() => (
+        <ArticleImage
+          captionOptions={fullwidthImage.captionOptions}
+          imageOptions={fullwidthImage.imageOptions}
+        />
+      )),
+      name: "Full Width",
+      type: "story"
+    },
+    {
+      component: withResponsive(() => (
         <ArticleImage
           captionOptions={portraitInlineImage.captionOptions}
           imageOptions={portraitInlineImage.imageOptions}
         />
-      ),
+      )),
       name: "Inline (portrait)",
       type: "story"
     },
     {
-      component: () => (
+      component: withResponsive(() => (
         <ArticleImage
           captionOptions={landscapeInlineImage.captionOptions}
           imageOptions={landscapeInlineImage.imageOptions}
         />
-      ),
+      )),
       name: "Inline (landscape)",
       type: "story"
     }
