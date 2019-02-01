a18fda66b015af445bc023deee71ac7e4baa0cf8
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-02-01 13:29:13 +0000

    Add borders and elevation to slice layout

---
+++ packages/slice-layout/src/templates/secondaryfour/styles.android.js
@@ -0,0 +1,22 @@
+const styles = {
+  container: {
+    backgroundColor: "#FBF9F9",
+    flex: 1
+  },
+  item: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 2,
+    borderStyle: "solid",
+    elevation: 2,
+    width: "48%",
+    margin: 3,
+    padding: 5
+  },
+  itemContainer: {
+    flexDirection: "row"
+  },
+  itemSeparator: {
+  }
+};
+
+export default styles;
---
+++ packages/slice-layout/src/templates/styles/index.android.js
@@ -0,0 +1,20 @@
+import { colours } from "@times-components/styleguide";
+
+const styles = {
+  item: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 2,
+    borderStyle: "solid",
+    elevation: 2,
+    margin: 5,
+    width: "97%"
+  },
+  itemContainer: {
+    width: "100%"
+  },
+  itemContainerWithoutBorders: {
+    width: "100%"
+  }
+};
+
+export default styles;
