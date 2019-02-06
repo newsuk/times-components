a54dad926866450369cb482d65666552682eaca3
Aron Shamash <aronshamash@gmail.com>
2019-02-06 10:06:26 +0000

    feat: add tile link to tile g

--- packages/edition-slices/src/tiles/tile-g/index.js
+++ packages/edition-slices/src/tiles/tile-g/index.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { View } from "react-native";
 import PropTypes from "prop-types";
-import { TileImage, TileSummary } from "../shared";
+import { TileImage, TileLink, TileSummary } from "../shared";
 import styles from "./styles";
 
-const TileG = ({ tile }) => (
-  <View style={styles.container}>
-    <TileImage
-      aspectRatio={1}
-      style={styles.imageContainer}
-      uri={tile.article.leadAsset.crop11.url}
-    />
-    <TileSummary
-      headlineStyle={styles.headline}
-      style={styles.summaryContainer}
-      tile={tile}
-    />
-  </View>
+const TileG = ({ onPress, tile }) => (
+  <TileLink onPress={onPress} style={styles.container} tile={tile}>
+    <View style={styles.container}>
+      <TileImage
+        aspectRatio={1}
+        style={styles.imageContainer}
+        uri={tile.article.leadAsset.crop11.url}
+      />
+      <TileSummary
+        headlineStyle={styles.headline}
+        style={styles.summaryContainer}
+        tile={tile}
+      />
+    </View>
+  </TileLink>
 );
 
 TileG.propTypes = {
+  onPress: PropTypes.func.isRequired,
   tile: PropTypes.shape({}).isRequired
 };
 
