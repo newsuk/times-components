f64ae96e6a113e1d1a1f029ebd849ca2998d8daf
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-02-08 11:55:58 +0000

    fix: adding slice layout

---
+++ packages/slice-layout/src/templates/leaders/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {View, Text} from "react-native";
+import styles from "./styles";
+import propTyes from "./proptypes";
+
+const keySeperator = ()=> (<View style={styles.keySeperator} />)
+
+const Leaders = ({renderLeader1, renderLeader2, renderLeader3}) => {
+    return (
+        <View style={styles.container}>
+            <Text> IMG </Text>
+            <View style={styles.leadText}>
+                <Text> Leading Articles </Text>
+            </View>
+            {renderLeader1()}
+            {keySeperator()}
+            {renderLeader2()}
+            {keySeperator()}
+            {renderLeader3()}
+        </View>
+    );
+};
+
+Leaders.propTyes = propTyes;
+
+export default Leaders;
---
+++ packages/slice-layout/src/templates/leaders/styles.js
@@ -0,0 +1,15 @@
+import {spacing } from "@times-components/styleguide";
+
+const styles = {
+    container: {
+        paddingBottom: spacing(1),
+        paddingTop: spacing(4),
+        textAlign: 'center'
+    },
+    leadText: {
+        paddingTop: spacing(2),
+        paddingBottom: spacing(1)
+    }
+};
+
+export default styles;
