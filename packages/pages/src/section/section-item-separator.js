496705b5018193569fc739d0864105b45d6c4ce0
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-02-08 09:58:17 +0000

    Section page

---
+++ packages/pages/src/section/section-item-separator.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./styles";
+
+const SectionItemSeparator = () => <View style={styles.listItemSeparator} />;
+
+export default SectionItemSeparator;
