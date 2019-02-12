6c1b27a1bdb11567d2627d6e2fa25fa11244f2d5
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2019-02-12 12:04:07 +0000

    fix: Secondary two and two slice export (#1681)
    * Export the right slice on index
    
    * Jestlint
    
    * Fix tests for sd2and2
    
    * Update snaps with keyline changes
    
    * Jestlint update

--- packages/edition-slices/src/slices/index.js
+++ packages/edition-slices/src/slices/index.js
@@ -5,7 +5,7 @@ export { default as ListTwoAndSixNoPicSlice } from "./listtwoandsixnopic";
 export { default as LeadTwoNoPicAndTwoSlice } from "./leadtwonopicandtwo";
 export { default as SecondaryOneSlice } from "./secondaryone";
 export { default as SecondaryFourSlice } from "./secondaryfour";
-export { default as SecondaryTwoAndTwoSlice } from "./secondarytwonopicandtwo";
+export { default as SecondaryTwoAndTwoSlice } from "./secondarytwoandtwo";
 export {
   default as SecondaryTwoNoPicAndTwoSlice
 } from "./secondarytwonopicandtwo";
--- packages/slice-layout/__tests__/sd2and2-with-style.native.js
+++ packages/slice-layout/__tests__/sd2and2-with-style.native.js
@@ -1,5 +1,5 @@
 import TestRenderer from "react-test-renderer";
 import "./serializers-with-style.native";
-import shared from "./sd2np2.base";
+import shared from "./sd2and2.base";
 
 export default () => shared(TestRenderer.create);
