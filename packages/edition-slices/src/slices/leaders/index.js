979febf1380be0750d4a331ed45407bbcf23b7d4
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-02-13 15:07:22 +0000

    chore: refactor masthead

--- packages/edition-slices/src/slices/leaders/index.js
+++ packages/edition-slices/src/slices/leaders/index.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View } from "react-native";
 import { Leaders } from "@times-components/slice-layout";
 import PropTypes from "prop-types";
 import { TileM } from "../../tiles";
 import styles from "./styles";
-
-const renderHeader = () => (
-  <View>
-    <Image
-      resizeMode="contain"
-      // eslint-disable-next-line global-require
-      source={require("../../../assets/leaders-masthead.png")}
-      style={styles.mastheadStyle}
-    />
-    <View style={styles.leadTextContainer}>
-      <Text style={[styles.leadText, styles.text]}> Leading Articles </Text>
-    </View>
-  </View>
-);
+import MastHead from "./masthead";
 
 const LeadersSlice = ({ leader1, leader2, leader3, onPress }) => (
   <View style={styles.container}>
-    {renderHeader()}
+    <MastHead />
     <Leaders
       renderLeader1={() => <TileM onPress={onPress} tile={leader1} />}
       renderLeader2={() => <TileM onPress={onPress} tile={leader2} />}
