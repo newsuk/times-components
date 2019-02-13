6f14c42b3bda28b05ceaa9b64fc975dfd1cc0b00
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-02-13 12:19:24 +0000

    Lint

--- ios-app/index.ios.js
+++ ios-app/index.ios.js
@@ -1,6 +1,11 @@
 import { AppRegistry } from "react-native";
 import { URL, URLSearchParams } from "url-polyfill";
-import { Article, AuthorProfile, Section, Topic } from "@times-components/pages";
+import {
+  Article,
+  AuthorProfile,
+  Section,
+  Topic
+} from "@times-components/pages";
 
 // see https://github.com/facebook/react-native/issues/16434
 global.URL = URL;
