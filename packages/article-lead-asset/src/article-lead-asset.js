c5959b3aecd3b84d733f063cb7b6d7bc2040ab96
Nate Higgins <nat@nath.is>
2019-02-14 19:46:03 +0000

    fix: video lead assets should fill container

--- packages/article-lead-asset/src/article-lead-asset.js
+++ packages/article-lead-asset/src/article-lead-asset.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View } from "react-native";
 import { ModalImage } from "@times-components/image";
-import { ResponsiveContext } from "@times-components/responsive";
 import ArticleLeadAssetVideo from "./article-lead-asset-video";
 import {
   nativePropTypes,
@@ -43,22 +42,17 @@ const ArticleLeadAsset = ({
   };
 
   return (
-    <ResponsiveContext.Consumer>
-      {({ isTablet }) => (
-        <View style={style}>
-          <LeadAsset
-            aspectRatio={getRatio(crop.ratio)}
-            caption={renderModalCaption({ caption })}
-            isTablet={isTablet}
-            leadAsset={leadAsset}
-            onVideoPress={onVideoPress}
-            uri={crop.url}
-            width={width}
-          />
-          {renderCaption({ caption })}
-        </View>
-      )}
-    </ResponsiveContext.Consumer>
+    <View style={style}>
+      <LeadAsset
+        aspectRatio={getRatio(crop.ratio)}
+        caption={renderModalCaption({ caption })}
+        leadAsset={leadAsset}
+        onVideoPress={onVideoPress}
+        uri={crop.url}
+        width={width}
+      />
+      {renderCaption({ caption })}
+    </View>
   );
 };
 
