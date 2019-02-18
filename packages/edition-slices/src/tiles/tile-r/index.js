610a878a0bddac7676d6055d1818d3b2a92a7bc2
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2019-02-18 17:20:42 +0000

    fix: Use getcrop in tile-r (#1718)
    * Use getcrop in tile r
    
    * Fix versions

--- packages/edition-slices/src/tiles/tile-r/index.js
+++ packages/edition-slices/src/tiles/tile-r/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 import PropTypes from "prop-types";
-import { TileImage, TileLink, TileSummary } from "../shared";
+import { getCrop, TileImage, TileLink, TileSummary } from "../shared";
 import styles from "./styles";
 
 const TileR = ({ onPress, tile }) => (
@@ -10,7 +10,7 @@ const TileR = ({ onPress, tile }) => (
       <TileSummary headlineStyle={styles.headline} tile={tile} />
       <TileImage
         aspectRatio={16 / 9}
-        uri={tile.article.leadAsset.crop169.url}
+        uri={getCrop(tile.article.leadAsset, "crop169")}
       />
     </View>
   </TileLink>
