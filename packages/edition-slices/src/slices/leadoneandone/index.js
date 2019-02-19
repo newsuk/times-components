6723ec9f01c5c62af7d08a5561efff5f40929626
Aron Shamash <aronshamash@gmail.com>
2019-02-19 09:47:50 +0000

    feat: lead one and one tablet layout

--- packages/edition-slices/src/slices/leadoneandone/index.js
+++ packages/edition-slices/src/slices/leadoneandone/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { LeadOneAndOneSlice } from "@times-components/slice-layout";
-import { TileA, TileB } from "../../tiles";
+import { TileA, TileB, TileC, TileU } from "../../tiles";
+import { ResponsiveSlice } from "../shared";
 
 const LeadOneAndOne = ({ onPress, slice: { lead, support } }) => (
-  <LeadOneAndOneSlice
-    renderLead={() => <TileA onPress={onPress} tile={lead} />}
-    renderSupport={() => <TileB onPress={onPress} tile={support} />}
+  <ResponsiveSlice
+    renderMedium={editionBreakpoint => (
+      <LeadOneAndOneSlice
+        editionBreakpoint={editionBreakpoint}
+        renderLead={() => <TileU onPress={onPress} tile={lead} />}
+        renderSupport={() => <TileC onPress={onPress} tile={support} />}
+      />
+    )}
+    renderSmall={editionBreakpoint => (
+      <LeadOneAndOneSlice
+        editionBreakpoint={editionBreakpoint}
+        renderLead={() => <TileA onPress={onPress} tile={lead} />}
+        renderSupport={() => <TileB onPress={onPress} tile={support} />}
+      />
+    )}
   />
 );
 
--- packages/slice-layout/src/templates/leadoneandone/index.js
+++ packages/slice-layout/src/templates/leadoneandone/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { View } from "react-native";
-import propTypes from "./proptypes";
+import { editionBreakpoints } from "@times-components/styleguide";
+import { defaultProps, propTypes } from "./proptypes";
+import { ItemColSeparator } from "../shared";
+import styles from "./styles";
 
-const leadOneAndOneSlice = ({ renderLead, renderSupport }) => (
-  <View>
-    {renderLead()}
-    {renderSupport()}
-  </View>
-);
+const leadOneAndOneSlice = ({ editionBreakpoint, renderLead, renderSupport }) =>
+  editionBreakpoint === editionBreakpoints.medium ? (
+    <View style={styles.container}>
+      <View style={styles.leadItem}>{renderLead()}</View>
+      <ItemColSeparator />
+      <View style={styles.supportItem}>{renderSupport()}</View>
+    </View>
+  ) : (
+    <View>
+      {renderLead()}
+      {renderSupport()}
+    </View>
+  );
 
 leadOneAndOneSlice.propTypes = propTypes;
+leadOneAndOneSlice.defaultProps = defaultProps;
 
 export default leadOneAndOneSlice;
