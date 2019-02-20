7cda47e7fd91c257421ae81f43b8b6919ccc6b8c
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-02-20 14:45:17 +0000

    Move out section pages from pages

--- packages/pages/src/section/section-item-separator.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-import styles from "./styles";
-
-const SectionItemSeparator = () => <View style={styles.listItemSeparator} />;
-
-export default SectionItemSeparator;
