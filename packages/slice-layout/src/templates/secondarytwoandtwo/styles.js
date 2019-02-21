d78ffa130d7fc1416dd94f8312df9345debc9f51
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-02-21 17:38:24 +0000

    chore: linting

--- packages/slice-layout/src/templates/secondarytwoandtwo/styles.js
+++ packages/slice-layout/src/templates/secondarytwoandtwo/styles.js
@@ -1,17 +1,18 @@
 import { editionBreakpoints, spacing } from "@times-components/styleguide";
 
 const smallBreakpointStyles = {
-  itemHalfWidth: {
-    width: "50%"
-  },
   itemContainer: {
     flex: 1,
     flexDirection: "row"
+  },
+  itemHalfWidth: {
+    width: "50%"
   }
 };
 
 const mediumBreakpointStyles = {
   container: {
+    flex: 1,
     marginHorizontal: spacing(2)
   },
   item: {
