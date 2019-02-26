c4ff6c40dd53119a4591db37ef6648731b6ecb1b
James Miller <james@james-miller.co.uk>
2019-02-26 15:45:24 +0000

    feat: Portrait Tablet (768px) for Lead Two No Pic And Two slice

--- packages/edition-slices/src/slices/leadtwonopicandtwo/index.js
+++ packages/edition-slices/src/slices/leadtwonopicandtwo/index.js
@@ -1,24 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { LeadTwoNoPicAndTwoSlice } from "@times-components/slice-layout";
-import { TileB, TileD, TileE, TileF } from "../../tiles";
+import { TileB, TileD, TileE, TileF, TileX, TileY, TileZ } from "../../tiles";
+import { ResponsiveSlice } from "../shared";
 
 const LeadTwoNoPicAndTwo = ({
   onPress,
   slice: { lead1, lead2, support1, support2 }
 }) => (
-  <LeadTwoNoPicAndTwoSlice
-    renderLead1={() => (
-      <TileF onPress={onPress} tile={lead1} tileName="lead1" />
+  <ResponsiveSlice
+    renderMedium={editionBreakpoint => (
+      <LeadTwoNoPicAndTwoSlice
+        breakpoint={editionBreakpoint}
+        renderLead1={() => (
+          <TileX onPress={onPress} tile={lead1} tileName="lead1" />
+        )}
+        renderLead2={() => (
+          <TileY onPress={onPress} tile={lead2} tileName="lead2" />
+        )}
+        renderSupport1={() => (
+          <TileD onPress={onPress} tile={support1} tileName="support1" />
+        )}
+        renderSupport2={() => (
+          <TileZ onPress={onPress} tile={support2} tileName="support2" />
+        )}
+      />
     )}
-    renderLead2={() => (
-      <TileB onPress={onPress} tile={lead2} tileName="lead2" />
-    )}
-    renderSupport1={() => (
-      <TileD onPress={onPress} tile={support1} tileName="support1" />
-    )}
-    renderSupport2={() => (
-      <TileE onPress={onPress} tile={support2} tileName="support2" />
+    renderSmall={editionBreakpoint => (
+      <LeadTwoNoPicAndTwoSlice
+        breakpoint={editionBreakpoint}
+        renderLead1={() => (
+          <TileF onPress={onPress} tile={lead1} tileName="lead1" />
+        )}
+        renderLead2={() => (
+          <TileB onPress={onPress} tile={lead2} tileName="lead2" />
+        )}
+        renderSupport1={() => (
+          <TileD onPress={onPress} tile={support1} tileName="support1" />
+        )}
+        renderSupport2={() => (
+          <TileE onPress={onPress} tile={support2} tileName="support2" />
+        )}
+      />
     )}
   />
 );
--- packages/slice-layout/src/templates/leadtwonopicandtwo/proptypes.js
+++ packages/slice-layout/src/templates/leadtwonopicandtwo/proptypes.js
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
+import { editionBreakpoints } from "@times-components/styleguide";
 
 const propTypes = {
+  breakpoint: PropTypes.string,
   renderLead1: PropTypes.func.isRequired,
   renderLead2: PropTypes.func.isRequired,
   renderSupport1: PropTypes.func.isRequired,
   renderSupport2: PropTypes.func.isRequired
 };
 
-export default propTypes;
+const defaultProps = {
+  breakpoint: editionBreakpoints.small
+};
+
+export { propTypes, defaultProps };
