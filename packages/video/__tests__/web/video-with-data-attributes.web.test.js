f53814e3268d50380b6bf7d70e2fd77c8135ef30
Aron Shamash <aronshamash@gmail.com>
2019-03-04 16:37:41 +0000

    chore: fmt

--- packages/video/__tests__/web/video-with-data-attributes.web.test.js
+++ packages/video/__tests__/web/video-with-data-attributes.web.test.js
@@ -16,7 +16,7 @@ import Video from "../../src/video";
 import defaultVideoProps from "../default-video-props";
 
 jest.mock("@times-components/image", () => "Image");
-jest.mock("@times-components/icons", () => "Icons")
+jest.mock("@times-components/icons", () => "Icons");
 
 const omitProps = new Set(["className", "controls", "id", "style"]);
 
