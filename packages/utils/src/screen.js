4caa2e025c6abf74eeb754bf66885aa498ebfb34
Nate Higgins <nat@nath.is>
2019-03-04 14:27:04 +0000

    chore: clean up 3

--- packages/utils/src/screen.js
+++ packages/utils/src/screen.js
@@ -8,7 +8,8 @@ import {
 
 export { acceptedWidths, normaliseWidthForAssetRequestCache, screenWidth };
 
-export const convertToPixels = points => PixelRatio.getPixelSizeForLayoutSize(points);
+export const convertToPixels = points =>
+  PixelRatio.getPixelSizeForLayoutSize(points);
 
 export const screenWidthInPixels = isTablet =>
   convertToPixels(screenWidth(isTablet));
