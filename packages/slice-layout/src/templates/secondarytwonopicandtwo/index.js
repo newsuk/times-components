95e0510e3436d98b3eeba51d58ecb1ce79b751c5
James Miller <james@james-miller.co.uk>
2019-03-06 09:56:07 +0000

    chore: Formatting

--- packages/slice-layout/src/templates/secondarytwonopicandtwo/index.js
+++ packages/slice-layout/src/templates/secondarytwonopicandtwo/index.js
@@ -16,37 +16,40 @@ const SecondaryTwoNoPicAndTwoSlice = ({
   if (breakpoint === editionBreakpoints.small) {
     return (
       <Column
-        tiles={[renderSecondary1, renderSecondary2, renderSupport1, renderSupport2]}
+        tiles={[
+          renderSecondary1,
+          renderSecondary2,
+          renderSupport1,
+          renderSupport2
+        ]}
       />
-
-    )
+    );
   }
 
   return (
     <Fragment>
       <View style={styles.itemContainer}>
-          <View key={renderSecondary1().props.id} style={styles.item}>
-            {renderSecondary1()}
-          </View>
-          <ItemColSeparator />
-          <View key={renderSecondary2().props.id} style={styles.item}>
-            {renderSecondary2()}
-          </View>
+        <View key={renderSecondary1().props.id} style={styles.item}>
+          {renderSecondary1()}
         </View>
-        <ItemRowSeparator />
-        <View style={styles.itemContainer}>
-          <View key={renderSupport1().props.id} style={styles.item}>
-            {renderSupport1()}
-          </View>
-          <ItemColSeparator />
-          <View key={renderSupport2().props.id} style={styles.item}>
-            {renderSupport2()}
-          </View>
+        <ItemColSeparator />
+        <View key={renderSecondary2().props.id} style={styles.item}>
+          {renderSecondary2()}
         </View>
-        </Fragment>
-
-);
-  }
+      </View>
+      <ItemRowSeparator />
+      <View style={styles.itemContainer}>
+        <View key={renderSupport1().props.id} style={styles.item}>
+          {renderSupport1()}
+        </View>
+        <ItemColSeparator />
+        <View key={renderSupport2().props.id} style={styles.item}>
+          {renderSupport2()}
+        </View>
+      </View>
+    </Fragment>
+  );
+};
 
 SecondaryTwoNoPicAndTwoSlice.propTypes = propTypes;
 
