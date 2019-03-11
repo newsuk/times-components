cf950431726584cd113614317b87d95b85ae6589
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-03-11 16:29:47 +0000

    Move style to link and flex on TileY

--- packages/edition-slices/src/tiles/tile-y/index.js
+++ packages/edition-slices/src/tiles/tile-y/index.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { View } from "react-native";
 import PropTypes from "prop-types";
 import { TileLink, TileSummary, withTileTracking } from "../shared";
 import styles from "./styles";
 
 const TileY = ({ onPress, tile }) => (
-  <TileLink onPress={onPress} tile={tile}>
-    <View style={styles.container}>
-      <TileSummary
-        headlineStyle={styles.headline}
-        summary={tile.article.summary300}
-        tile={tile}
-      />
-    </View>
+  <TileLink onPress={onPress} style={styles.container} tile={tile}>
+    <TileSummary
+      headlineStyle={styles.headline}
+      summary={tile.article.summary300}
+      tile={tile}
+    />
   </TileLink>
 );
 
