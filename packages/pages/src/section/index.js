f3fca5e9fae7acf8c698f9a99282866ec7adc4b1
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-03-13 10:36:34 +0000

    Add new params to section clicks

--- packages/pages/src/section/index.js
+++ packages/pages/src/section/index.js
@@ -29,8 +29,8 @@ const SectionPage = ({ editionId, publicationName, section, sectionTitle }) => {
     section ? (
       <Section
         analyticsStream={analyticsStream}
-        onArticlePress={(_, { url }) => onArticlePress(url)}
-        onPuzzlePress={(_, { url }) => onPuzzlePress(url)}
+        onArticlePress={({ id, url }) => onArticlePress(url, id)}
+        onPuzzlePress={({ id, title, url }) => onPuzzlePress(url, title, id)}
         publicationName={publicationName}
         section={JSON.parse(section)}
       />
