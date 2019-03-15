9fa963b789bfe0aac181b7ff1d9604f9f6dde0a4
Tunca Ulubilge <tunca.ulubilge@gmail.com>
2019-03-15 11:23:32 +0000

    feat: Remove showInteractives flag and delete logic to remove flag (#1810)

--- packages/pages/__tests__/shared.base.js
+++ packages/pages/__tests__/shared.base.js
@@ -3,7 +3,6 @@ import { scales } from "@times-components/styleguide";
 import "./mocks";
 import { Article, Section } from "../src/pages";
 import getAdTargetingConfig from "../src/article/ad-targeting-config";
-import filterInteractives from "../src/article/filter-interactives";
 
 export default makeTest => {
   it("article page", () => {
@@ -38,32 +37,5 @@ export default makeTest => {
     });
 
     expect(adTargetingConfig).toMatchSnapshot();
-  });
-
-  it("filter interactives from article ast as expected", () => {
-    const article = {
-      content: [
-        { name: "paragraph", value: "dummy value" },
-        { name: "paragraph", value: "dummy value" },
-        { name: "ad", value: "dummy value" },
-        { name: "interactive", value: "dummy value" },
-        { name: "paragraph", value: "dummy value" }
-      ],
-      headline: "This is a headline",
-      id: "this-is-a-id"
-    };
-
-    const filteredArticle = {
-      content: [
-        { name: "paragraph", value: "dummy value" },
-        { name: "paragraph", value: "dummy value" },
-        { name: "ad", value: "dummy value" },
-        { name: "paragraph", value: "dummy value" }
-      ],
-      headline: "This is a headline",
-      id: "this-is-a-id"
-    };
-
-    expect(filterInteractives(article)).toEqual(filteredArticle);
   });
 };
--- packages/pages/src/article/filter-interactives.js
+++
@@ -1,7 +0,0 @@
-export default article =>
-  article
-    ? {
-        ...article,
-        content: article.content.filter(node => node.name !== "interactive")
-      }
-    : article;
