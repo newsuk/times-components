26a39cd0844d5bb444f626f2ce9f3495b2c1bd2e
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-03-20 13:36:44 +0000

    Add offline support to ios

--- packages/image/src/lazy-loading-image/lazy-loading-image.js
+++ packages/image/src/lazy-loading-image/lazy-loading-image.js
@@ -1,3 +1,44 @@
+import React, { Component, Fragment } from "react";
 import { Image } from "react-native";
+import appendToUrl from "../utils/appendToURL";
 
-export default Image;
+class LazyLoadingImage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null
+    };
+  }
+
+  render() {
+    const { source } = this.props;
+    const { error } = this.state;
+    return (
+      <Fragment>
+        {error &&
+        source &&
+        source.uri &&
+        !source.uri.includes("data:image/") ? (
+          <Image
+            {...this.props}
+            source={
+              source
+                ? {
+                    uri: appendToUrl(source.uri, "offline", true)
+                  }
+                : null
+            }
+          />
+        ) : null}
+        <Image
+          {...this.props}
+          onError={({ nativeEvent: { error: imageError } }) => {
+            this.setState({ error: imageError });
+          }}
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default LazyLoadingImage;
