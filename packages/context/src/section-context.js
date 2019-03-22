fe1ce6c4bb9319b9f82521bbbbf96359be8967b3
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-03-22 14:46:36 +0000

    Remove unused context defaults

--- packages/context/src/section-context.js
+++ packages/context/src/section-context.js
@@ -1,9 +1,6 @@
 import { createContext } from "react";
 
 export default createContext({
-  // TODO: Clean up
   publicationName: "TIMES",
-  readArticleStore: {},
-  recentlyOpenedPuzzles: 0,
-  savedArticleStore: {},
+  recentlyOpenedPuzzles: 0
 });
