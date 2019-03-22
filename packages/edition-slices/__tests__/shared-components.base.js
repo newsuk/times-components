5c4f063be30211342d0f1be0d5f6cfd69a4f9888
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-03-22 09:53:20 +0000

    chore: correcting a typo

--- packages/edition-slices/__tests__/shared-components.base.js
+++ packages/edition-slices/__tests__/shared-components.base.js
@@ -24,7 +24,7 @@ export default () => {
           ...tile,
           article: {
             ...tile.article,
-            shortHeadline: "This is shortheadlin"
+            shortHeadline: "This is shortheadline"
           },
           headline: ""
         };
