827bccf150febb3da056dbaf84940eaca53c317a
Dimova <pdimova@MC111547.local>
2019-03-29 14:38:15 +0200

    feat: add wide breakpoint for secondary 2 and 2 slice

--- packages/edition-slices/src/slices/secondarytwoandtwo/index.js
+++ packages/edition-slices/src/slices/secondarytwoandtwo/index.js
@@ -1,50 +1,67 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { SecondaryTwoAndTwoSlice } from "@times-components/slice-layout";
-import { TileC, TileG, TileV } from "../../tiles";
+import { TileC, TileG, TileV, TileAM, TileAN } from "../../tiles";
 import { ResponsiveSlice } from "../shared";
 
 const SecondaryTwoAndTwo = ({
   onPress,
   slice: { secondary1, secondary2, support1, support2 }
 }) => (
-  <ResponsiveSlice
-    renderMedium={breakpoint => (
-      <SecondaryTwoAndTwoSlice
-        breakpoint={breakpoint}
-        renderSecondary1={() => (
-          <TileV onPress={onPress} tile={secondary1} tileName="secondary1" />
-        )}
-        renderSecondary2={() => (
-          <TileV onPress={onPress} tile={secondary2} tileName="secondary2" />
-        )}
-        renderSupport1={() => (
-          <TileG onPress={onPress} tile={support1} tileName="support1" />
-        )}
-        renderSupport2={() => (
-          <TileG onPress={onPress} tile={support2} tileName="support2" />
-        )}
-      />
-    )}
-    renderSmall={breakpoint => (
-      <SecondaryTwoAndTwoSlice
-        breakpoint={breakpoint}
-        renderSecondary1={() => (
-          <TileC onPress={onPress} tile={secondary1} tileName="secondary1" />
-        )}
-        renderSecondary2={() => (
-          <TileC onPress={onPress} tile={secondary2} tileName="secondary2" />
-        )}
-        renderSupport1={() => (
-          <TileG onPress={onPress} tile={support1} tileName="support1" />
-        )}
-        renderSupport2={() => (
-          <TileG onPress={onPress} tile={support2} tileName="support2" />
-        )}
-      />
-    )}
-  />
-);
+    <ResponsiveSlice
+      renderWide={breakpoint => (
+        <SecondaryTwoAndTwoSlice
+          breakpoint={breakpoint}
+          renderSecondary1={() => (
+            <TileAM onPress={onPress} tile={secondary1} tileName="secondary1" />
+          )}
+          renderSecondary2={() => (
+            <TileAM onPress={onPress} tile={secondary2} tileName="secondary2" />
+          )}
+          renderSupport1={() => (
+            <TileAN onPress={onPress} tile={support1} tileName="support1" />
+          )}
+          renderSupport2={() => (
+            <TileAN onPress={onPress} tile={support2} tileName="support2" />
+          )}
+        />
+      )}
+      renderMedium={breakpoint => (
+        <SecondaryTwoAndTwoSlice
+          breakpoint={breakpoint}
+          renderSecondary1={() => (
+            <TileV onPress={onPress} tile={secondary1} tileName="secondary1" />
+          )}
+          renderSecondary2={() => (
+            <TileV onPress={onPress} tile={secondary2} tileName="secondary2" />
+          )}
+          renderSupport1={() => (
+            <TileG onPress={onPress} tile={support1} tileName="support1" />
+          )}
+          renderSupport2={() => (
+            <TileG onPress={onPress} tile={support2} tileName="support2" />
+          )}
+        />
+      )}
+      renderSmall={breakpoint => (
+        <SecondaryTwoAndTwoSlice
+          breakpoint={breakpoint}
+          renderSecondary1={() => (
+            <TileC onPress={onPress} tile={secondary1} tileName="secondary1" />
+          )}
+          renderSecondary2={() => (
+            <TileC onPress={onPress} tile={secondary2} tileName="secondary2" />
+          )}
+          renderSupport1={() => (
+            <TileG onPress={onPress} tile={support1} tileName="support1" />
+          )}
+          renderSupport2={() => (
+            <TileG onPress={onPress} tile={support2} tileName="support2" />
+          )}
+        />
+      )}
+    />
+  );
 
 SecondaryTwoAndTwo.propTypes = {
   onPress: PropTypes.func.isRequired,
