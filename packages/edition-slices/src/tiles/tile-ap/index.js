8a59c1240eef1cb10b0938265db701c0453899cd
Paola Dimova <paola.dimova@news.co.uk>
2019-04-03 17:16:22 +0300

    feat: add tileAP

---
+++ packages/edition-slices/src/tiles/tile-ap/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View } from "react-native";
+import PropTypes from "prop-types";
+import {
+  getCrop,
+  TileImage,
+  TileLink,
+  TileSummary,
+  withTileTracking
+} from "../shared";
+import styles from "./styles";
+
+const TileAP = ({ onPress, tile }) => (
+  <TileLink onPress={onPress} tile={tile}>
+    <View style={styles.container}>
+      <TileImage
+        aspectRatio={1}
+        style={styles.imageContainer}
+        uri={getCrop(
+          tile.leadAsset || tile.article.listingAsset || tile.article.leadAsset,
+          "crop11"
+        )}
+      />
+      <TileSummary
+        headlineStyle={styles.headline}
+        style={styles.summaryContainer}
+        tile={tile}
+      />
+    </View>
+  </TileLink>
+);
+
+TileAP.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  tile: PropTypes.shape({}).isRequired
+};
+
+export default withTileTracking(TileAP);
