06e22e44db2400443a2e2f2c56ff53f81f37a35c
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-04-08 23:43:07 +0100

    chore: linting

--- packages/provider-queries/src/section-fragment.js
+++ packages/provider-queries/src/section-fragment.js
@@ -264,9 +264,9 @@ export default gql`
             }
             listingAsset {
               ...listingAsset32
-            },
+            }
             summary125: summary(maxCharCount: 125)
-          },
+          }
           teaser125: teaser(maxCharCount: 125)
         }
         support2 {
