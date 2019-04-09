18784889f0d896b11d27abdc58c1a7edddae8dd5
Jaye Marshall <marshall.jaye@gmail.com>
2019-04-09 15:32:02 +0000

    rest of unit tests

--- packages/text-flow/__tests__/layout.shared.test.js
+++ packages/text-flow/__tests__/layout.shared.test.js
@@ -10,11 +10,41 @@ export default () => {
     ])
   })
 
-  it('does layout for a Text Flow', () => {
+  it('indents lines next to an inline', () => {
       const flow = new Layout.TextFlow({
+          width: 660,
           flow: [
-            
+            new Layout.Block({
+              height: 100,
+              width: 660,
+              getComponent() {
+
+              }
+            }),
+            new Layout.InlineBlock({
+              width: 660 * 0.35,
+              height: 30,
+              getComponent() {
+
+              }
+            }),
+            new Text.Text({
+              width: 660,
+              size: 18,
+              lineHeight: 30,
+              font: 'TimesDigitalW04-Regular',
+              markup: [
+                new Markup.MarkupString("example text")
+              ]
+            })
           ]
       })
+
+      const textFlows = flow.block.children
+      const inline = textFlows[1]
+      const text = inline.children[0]
+      const lines = text.block.children
+
+      expect(lines[0].x).toBeGreaterThan(230)
   })
 };
