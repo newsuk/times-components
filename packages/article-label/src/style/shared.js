df3e836cfe98027c2edff920b89c97ae2cfc2a7d
Aron Shamash <aronshamash@gmail.com>
2019-04-09 08:15:56 +0100

    fix: Video labels and Article label bugs (#1863)
    * fix: video labels android
    
    * fix: deps

---
+++ packages/article-label/src/style/shared.js
@@ -0,0 +1,19 @@
+import styleguide from "@times-components/styleguide";
+
+const { fontFactory } = styleguide();
+const styles = {
+  title: {
+    ...fontFactory({
+      font: "supporting",
+      fontSize: "cardMetaMobile"
+    }),
+    fontWeight: "400",
+    letterSpacing: 0.6,
+    lineHeight: 11,
+    marginBottom: 0,
+    marginTop: -1,
+    paddingTop: 1
+  }
+};
+
+export default styles;
