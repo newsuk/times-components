69176353f964607225797f19c605c964fc1e7155
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-04-11 15:03:05 +0100

    Format

--- packages/provider-queries/src/article-extras.js
+++ packages/provider-queries/src/article-extras.js
@@ -4,76 +4,50 @@ import articleProps from "./article-props";
 
 export default addTypenameToDocument(
   gql`
-  query ArticleExtrasQuery($id: ID!) {
-    article(id: $id) {
-      id
-      commentCount
-      commentsEnabled
-
-      relatedArticleSlice {
-        sliceName: __typename
-        ... on StandardSlice {
-          items {
-            ...relatedProps
-          }
-        }
-        ... on LeadOneAndTwoSlice {
-          lead {
-            ...relatedProps
-          }
-          support1 {
-            ...relatedProps
-          }
-          support2 {
-            ...relatedProps
-          }
-        }
-        ... on OpinionOneAndTwoSlice {
-          opinion {
-            ...relatedProps
-          }
-          support1 {
-            ...relatedProps
-          }
-          support2 {
-            ...relatedProps
-          }
-        }
-      }
-      topics(maxCount: 5) {
-        name
-        slug
-      }
-    }
-  }
-
-  fragment relatedProps on Tile {
-    leadAsset {
-      __typename
-      ... on Image {
-        crop169: crop(ratio: "16:9") {
-          url
-        }
-        crop32: crop(ratio: "3:2") {
-          url
-        }
+    query ArticleExtrasQuery($id: ID!) {
+      article(id: $id) {
         id
-        title
-      }
-      ... on Video {
-        posterImage {
-          crop169: crop(ratio: "16:9") {
-            url
+        commentCount
+        commentsEnabled
+
+        relatedArticleSlice {
+          sliceName: __typename
+          ... on StandardSlice {
+            items {
+              ...relatedProps
+            }
           }
-          crop32: crop(ratio: "3:2") {
-            url
+          ... on LeadOneAndTwoSlice {
+            lead {
+              ...relatedProps
+            }
+            support1 {
+              ...relatedProps
+            }
+            support2 {
+              ...relatedProps
+            }
           }
-          id
-          title
+          ... on OpinionOneAndTwoSlice {
+            opinion {
+              ...relatedProps
+            }
+            support1 {
+              ...relatedProps
+            }
+            support2 {
+              ...relatedProps
+            }
+          }
+        }
+        topics(maxCount: 5) {
+          name
+          slug
         }
       }
     }
-    article {
+
+    fragment relatedProps on Tile {
       leadAsset {
         __typename
         ... on Image {
@@ -99,14 +73,41 @@ export default addTypenameToDocument(
           }
         }
       }
-      ...articleProps
-      ...summaries
+      article {
+        leadAsset {
+          __typename
+          ... on Image {
+            crop169: crop(ratio: "16:9") {
+              url
+            }
+            crop32: crop(ratio: "3:2") {
+              url
+            }
+            id
+            title
+          }
+          ... on Video {
+            posterImage {
+              crop169: crop(ratio: "16:9") {
+                url
+              }
+              crop32: crop(ratio: "3:2") {
+                url
+              }
+              id
+              title
+            }
+          }
+        }
+        ...articleProps
+        ...summaries
+      }
     }
-  }
 
-  fragment summaries on Article {
-    summary125: summary(maxCharCount: 125)
-  }
+    fragment summaries on Article {
+      summary125: summary(maxCharCount: 125)
+    }
 
-  ${articleProps}
-`);
+    ${articleProps}
+  `
+);
