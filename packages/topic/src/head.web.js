670fc8a12fdddecfb460f636f3f4368f3ad6a6b7
Nate Higgins <nat@nath.is>
2019-04-18 16:57:47 +0100

    feat: move existing topic render tags over to times-components

--- packages/topic/src/head.web.js
+++ packages/topic/src/head.web.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet-async";
+import Context from "@times-components/context";
 
-function Head({ page, pageSize, name }) {
+function Head({ name, slug }) {
   return (
-    <Helmet>
-      <title>
-        {name} | Page {`${page}/${pageSize}`} | The Times &amp; The Sunday Times
-      </title>
-    </Helmet>
+    <Context.Consumer>
+      {({ makeTopicUrl }) => (
+        <Helmet>
+          <title>{name} | The Times &amp; The Sunday Times</title>
+          <meta
+            content={`Discover expert ${name} articles from The Times and The Sunday Times.`}
+            name="description"
+          />
+          <link href={makeTopicUrl({ slug })} rel="canonical" />
+        </Helmet>
+      )}
+    </Context.Consumer>
   );
 }
 
 Head.propTypes = {
   name: PropTypes.string.isRequired,
-  page: PropTypes.number.isRequired,
-  pageSize: PropTypes.number.isRequired
+  slug: PropTypes.string.isRequired
 };
 
 export default Head;
