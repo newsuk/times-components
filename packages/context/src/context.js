176016e510be2993af166614d66f60ca327d4dd0
Nate Higgins <nat@nath.is>
2019-04-18 12:20:16 +0100

    fix: ContextProviderWithDefaults

--- packages/context/src/context.js
+++ packages/context/src/context.js
@@ -5,7 +5,7 @@ import SectionContext from "./section-context";
 const Context = createContext(defaults);
 
 function ContextProviderWithDefaults({ value, ...props }) {
-  return <Context.Provider {...props} value={{ ...defaults, value }} />;
+  return <Context.Provider {...props} value={{ ...defaults, ...value }} />;
 }
 
 export default Context;
