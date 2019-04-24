897ad8ed417279c43b5f104e5b3d048c2bbe1ee1
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-04-24 16:12:34 +0100

    Update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,35 +1,35 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  devtool: "eval-source-map",
-  resolve: {
-    // Maps the 'react-native' import to 'react-native-web'.
+module.exports = async (baseConfig, env, defaultConfig) => {
+  baseConfig.devtool = "eval-source-map"
+  baseConfig.resolve = {
+    ...baseConfig.resolve,
     alias: {
+      ...baseConfig.resolve.alias,
       "react-native": "react-native-web",
       "@storybook/react-native": "@storybook/react"
     },
-    // If you're working on a multi-platform React Native app, web-specific
-    // module implementations should be written in files using the extension
-    // `.web.js`.
-    extensions: [".web.js", ".js", ".ios.js", ".android.js"],
-    mainFields: ["devModule", "dev", "module", "main"],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        loader: 'react-native-web-image-loader?name=[hash].[ext]',
-      }
-    ]
-  },
-
-  plugins: [
-    // Use the DLL in development.
+    extensions: [".web.js", ".js", ".ios.js", ".android.js", ".mjs"],
+    mainFields: ["devModule", "dev", "module", "main"]
+  };
+  baseConfig.plugins.push(
     new webpack.DllReferencePlugin({
       context: __dirname,
       manifest: path.resolve("./dist/public/vendor-manifest.json")
     })
-  ]
+  );
+  baseConfig.module.rules.push(
+    {
+      test: /\.(png|jpe?g|gif)$/,
+      loader: 'react-native-web-image-loader?name=[hash].[ext]',
+    },
+    {
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    }
+  );
+
+  return baseConfig;
 };
