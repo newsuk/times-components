db6e7cd09381f90f09770249e16dc434ae4063f8
parkjh <joonhop@me.com>
2019-04-29 14:49:53 +0100

    add additional padding for tablet

--- packages/edition-slices/src/slices/shared/gutter.js
+++ packages/edition-slices/src/slices/shared/gutter.js
@@ -1,14 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
-import { editionMaxWidth } from "@times-components/styleguide";
+import { editionMaxWidth, spacing } from "@times-components/styleguide";
 
 const Gutter = ({ children }) => (
   <View
     style={{
       alignSelf: "center",
       maxWidth: "100%",
-      width: editionMaxWidth
+      width: editionMaxWidth,
+      paddingHorizontal: spacing(2)
     }}
   >
     {children}
