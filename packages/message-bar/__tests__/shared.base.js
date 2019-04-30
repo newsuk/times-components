68feee414a2e8fe1526b0928f39693d771839869
Jaye Marshall <marshall.jaye@gmail.com>
2019-04-30 13:11:32 +0100

    missed renames

--- packages/message-bar/__tests__/shared.base.js
+++ packages/message-bar/__tests__/shared.base.js
@@ -1,9 +1,9 @@
 import { iterator } from "@times-components/test-utils";
 import MessageBarTests from "./message-bar.base";
-import MessageQueueTests from "./message-queue.base";
+import MessageManagerTests from "./message-manager.base";
 
 export default animate => {
-  const tests = [...MessageBarTests(animate), ...MessageQueueTests(animate)];
+  const tests = [...MessageBarTests(animate), ...MessageManagerTests(animate)];
 
   iterator(tests);
 };
