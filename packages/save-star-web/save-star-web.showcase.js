f79b05dc74771dc1c65bc54939cc009c0d1b2f52
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-05-01 19:44:14 +0100

    chore: fix style on star web showcase

--- packages/save-star-web/save-star-web.showcase.js
+++ packages/save-star-web/save-star-web.showcase.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { View } from "react-native";
+import { spacing } from "@times-components/styleguide";
 import SaveStarWeb from "./src/save-star-web";
 import mockSaveApi from "./mock-save-api-showcase";
+
+const styles = {
+  star: {
+    alignItems: "center",
+    flexDirection: "row",
+    padding: spacing(4)
+  }
+};
 
 export default {
   children: [
     {
       component: () => (
-        <SaveStarWeb
-          articleId="5504b5a8-b1c0-11e8-a553-a0ee9be48bc6"
-          saveApi={mockSaveApi}
-        />
+        <View style={styles.star}>
+          <SaveStarWeb
+            articleId="5504b5a8-b1c0-11e8-a553-a0ee9be48bc6"
+            saveApi={mockSaveApi}
+          />
+        </View>
       ),
       name: "SaveStarWeb",
       type: "story"
