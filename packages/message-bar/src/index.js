90c0f498c8d970cbaad9068732015b17ccc20a50
Jaye Marshall <jaye.marshall@news.co.uk>
2019-05-10 15:06:23 +0100

    feat: user notification bar (REPLAT-6144) (#1896)
    feat: user notification system

---
+++ packages/message-bar/src/index.js
@@ -0,0 +1,3 @@
+export { default as MessageManager } from "./message-manager";
+export { default as MessageBar } from "./message-bar";
+export { default as MessageContext } from "./message-context";
