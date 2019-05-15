adbdcdc00b2a01cea726e3e39a1e52b0a2264a90
Nate Higgins <nathaniel.higgins@news.co.uk>
2019-05-15 17:10:33 +0100

    feat: full width images on web (#1954)

--- packages/article-skeleton/__tests__/images.base.js
+++ packages/article-skeleton/__tests__/images.base.js
@@ -22,6 +22,100 @@ const emptyArticle = {
 export default () =>
   iterator([
     {
+      name: "a primary image",
+      test() {
+        const output = TestRenderer.create(
+          <Context.Provider
+            value={{
+              user: { isLoggedIn: true }
+            }}
+          >
+            <ArticleSkeleton
+              {...articleSkeletonProps}
+              adConfig={adConfig}
+              analyticsStream={() => {}}
+              data={articleFixture({
+                ...testFixture,
+                ...emptyArticle,
+                content: [
+                  {
+                    attributes: {
+                      caption: "A Caption",
+                      credits: "Some Credits",
+                      display: "primary",
+                      imageIndex: 1,
+                      ratio: "3:2",
+                      url: "https://image-2.io"
+                    },
+                    children: [],
+                    name: "image"
+                  }
+                ]
+              })}
+              onAuthorPress={() => {}}
+              onCommentGuidelinesPress={() => {}}
+              onCommentsPress={() => {}}
+              onLinkPress={() => {}}
+              onRelatedArticlePress={() => {}}
+              onTopicPress={() => {}}
+              onTwitterLinkPress={() => {}}
+              onVideoPress={() => {}}
+              spotAccountId=""
+            />
+          </Context.Provider>
+        );
+
+        expect(output).toMatchSnapshot();
+      }
+    },
+    {
+      name: "a fullwidth image",
+      test() {
+        const output = TestRenderer.create(
+          <Context.Provider
+            value={{
+              user: { isLoggedIn: true }
+            }}
+          >
+            <ArticleSkeleton
+              {...articleSkeletonProps}
+              adConfig={adConfig}
+              analyticsStream={() => {}}
+              data={articleFixture({
+                ...testFixture,
+                ...emptyArticle,
+                content: [
+                  {
+                    attributes: {
+                      caption: "A Caption",
+                      credits: "Some Credits",
+                      display: "fullwidth",
+                      imageIndex: 1,
+                      ratio: "3:2",
+                      url: "https://image-2.io"
+                    },
+                    children: [],
+                    name: "image"
+                  }
+                ]
+              })}
+              onAuthorPress={() => {}}
+              onCommentGuidelinesPress={() => {}}
+              onCommentsPress={() => {}}
+              onLinkPress={() => {}}
+              onRelatedArticlePress={() => {}}
+              onTopicPress={() => {}}
+              onTwitterLinkPress={() => {}}
+              onVideoPress={() => {}}
+              spotAccountId=""
+            />
+          </Context.Provider>
+        );
+
+        expect(output).toMatchSnapshot();
+      }
+    },
+    {
       name: "a secondary image",
       test() {
         const output = TestRenderer.create(
