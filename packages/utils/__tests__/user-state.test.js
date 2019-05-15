d48c14b136951aa339a558fadb1eee4ba94538a2
Dimitrov <gabriel.dimitrov@news.co.uk>
2019-05-15 17:02:57 +0300

    Fix lint issues

--- packages/utils/__tests__/user-state.test.js
+++ packages/utils/__tests__/user-state.test.js
@@ -1,38 +1,38 @@
-import { isLoggedIn, isMeteredExpired, isShared, isMetered } from '../src';
+import { isLoggedIn, isMeteredExpired, isShared, isMetered } from "../src";
 
 const defaultUserState = {
-    isLoggedIn: true,
-    isMetered: false,
-    isMeteredExpired: false,
-    isShared: false
+  isLoggedIn: true,
+  isMetered: false,
+  isMeteredExpired: false,
+  isShared: false
 };
 
-describe('user state should', () => {
-    it('return true if userState isLoggedIn is true', () => {
-        expect(isLoggedIn(defaultUserState)).toBe(true);
-    });
+describe("user state should", () => {
+  it("return true if userState isLoggedIn is true", () => {
+    expect(isLoggedIn(defaultUserState)).toBe(true);
+  });
 
-    it('return true if user.isLoggedIn equals true and user.isMeteredExpired equals true', () => {
-        const userState = {...defaultUserState, isMeteredExpired: true};
-        expect(isMeteredExpired(userState)).toBe(true);
-    });
+  it("return true if user.isLoggedIn equals true and user.isMeteredExpired equals true", () => {
+    const userState = { ...defaultUserState, isMeteredExpired: true };
+    expect(isMeteredExpired(userState)).toBe(true);
+  });
 
-    it('return false if user.isLoggedIn equals true and iuser.isMeteredExpired equals false', () => {
-        expect(isMeteredExpired(defaultUserState)).toBe(false);
-    });
+  it("return false if user.isLoggedIn equals true and iuser.isMeteredExpired equals false", () => {
+    expect(isMeteredExpired(defaultUserState)).toBe(false);
+  });
 
-    it('return true if userState.isShared equals true', () => {
-        const userState = {...defaultUserState, isShared: true}
-        expect(isShared(userState)).toBe(true);
-    });
+  it("return true if userState.isShared equals true", () => {
+    const userState = { ...defaultUserState, isShared: true };
+    expect(isShared(userState)).toBe(true);
+  });
 
-    it('return true if user.isLoggedIn equals true and user.isMetered equals true', () => {
-        const userState = {...defaultUserState, isMetered: true};
-        expect(isMetered(userState)).toBe(true);
-    });
+  it("return true if user.isLoggedIn equals true and user.isMetered equals true", () => {
+    const userState = { ...defaultUserState, isMetered: true };
+    expect(isMetered(userState)).toBe(true);
+  });
 
-    it('return true if user.isLoggedIn equals true and user.isMetered equals true', () => {
-        const userState = {...defaultUserState, isShared: true};
-        expect(isMetered(userState)).toBe(false);
-    });
+  it("return true if user.isLoggedIn equals true and user.isMetered equals true", () => {
+    const userState = { ...defaultUserState, isShared: true };
+    expect(isMetered(userState)).toBe(false);
+  });
 });
--- packages/utils/src/user-state.js
+++ packages/utils/src/user-state.js
@@ -1,7 +1,9 @@
 export const isLoggedIn = userState => userState.isLoggedIn;
 
-export const isMeteredExpired = userState => isLoggedIn(userState) && userState.isMeteredExpired;
+export const isMeteredExpired = userState =>
+  isLoggedIn(userState) && userState.isMeteredExpired;
 
 export const isShared = userState => userState.isShared;
 
-export const isMetered = userState => isLoggedIn(userState) && userState.isMetered;
+export const isMetered = userState =>
+  isLoggedIn(userState) && userState.isMetered;
