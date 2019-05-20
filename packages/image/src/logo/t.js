3b47bee7590ad5017e084c4653e667227818cad1
Nate Higgins <nat@nath.is>
2019-05-20 15:23:53 +0100

    chore: don't require logo path on every render

--- packages/image/src/logo/t.js
+++ packages/image/src/logo/t.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Image } from "react-native";
 
+// eslint-disable-next-line global-require
+const logoPath = require("../../assets/t.png");
+
 function T() {
   return (
     <Image
       fadeDuration={0}
       resizeMode="contain"
-      // eslint-disable-next-line global-require
-      source={require("../../assets/t.png")}
+      source={logoPath}
       style={{ width: "25%" }}
     />
   );
