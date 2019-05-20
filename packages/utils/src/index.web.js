722ba1d870a36356c1fac7ee41fbe822453b04e6
tuncaulubilge <tunca.ulubilge@gmail.com>
2019-05-20 11:45:40 +0100

    Move HoverIcon to web only exports

--- packages/utils/src/index.web.js
+++ packages/utils/src/index.web.js
@@ -11,3 +11,4 @@ export { default as getStandardTemplateCrop } from "./crop-config";
 export { default as getHeadline } from "./get-headline";
 export { default as gqlRgbaToHex } from "./gql-rgba-to-hex";
 export { default as gqlRgbaToStyle } from "./gql-rgba-to-style";
+export { default as HoverIcon } from "./hover-icon";
