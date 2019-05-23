4dfc29e5e161699d3c3920a61e262b24221f45f8
--global <--global>
2019-05-23 18:49:17 +0100

    chore: refactor image

--- packages/image/src/lazy-loading-image/lazy-loading-image.android.js
+++ packages/image/src/lazy-loading-image/lazy-loading-image.android.js
@@ -1,54 +1,42 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { Image } from "react-native";
 import { appendParamsToQuery } from "../utils";
 
-class LazyLoadingImage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      error: null
-    };
-  }
+const LazyLoadingImage = props => {
+  const {
+    source,
+    relativeWidth,
+    relativeHeight,
+    relativeHorizontalOffset,
+    relativeVerticalOffset
+  } = props;
 
-  render() {
-    const {
-      source,
-      relativeWidth,
-      relativeHeight,
-      relativeHorizontalOffset,
-      relativeVerticalOffset
-    } = this.props;
-    const { error } = this.state;
+  const queryArray = [
+    ["offline", true],
+    ["rel_height", relativeHeight],
+    ["rel_width", relativeWidth],
+    ["rel_horizontal_offset", relativeHorizontalOffset],
+    ["rel_vertical_offset", relativeVerticalOffset]
+  ];
+  const queryParamMap = new Map(queryArray);
 
-    const queryArray = [
-      ["offline", true],
-      ["rel_height", relativeHeight],
-      ["rel_width", relativeWidth],
-      ["rel_horizontal_offset", relativeHorizontalOffset],
-      ["rel_vertical_offset", relativeVerticalOffset]
-    ];
-    const queryParamMap = new Map(queryArray);
-
-    return (
-      <Fragment>
-        {error &&
-        source &&
-        source.uri &&
-        !source.uri.includes("data:image/") ? (
-          <Image
-            {...this.props}
-            source={
-              source
-                ? {
-                    uri: appendParamsToQuery(source.uri, queryParamMap)
-                  }
-                : null
-            }
-          />
-        ) : null}
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      {source && source.uri && !source.uri.includes("data:image/") ? (
+        <Image
+          {...props}
+          source={
+            source
+              ? {
+                  uri: appendParamsToQuery(source.uri, queryParamMap)
+                }
+              : null
+          }
+        />
+      ) : null}
+      <Image {...props} />
+    </Fragment>
+  );
+};
 
 export default LazyLoadingImage;
