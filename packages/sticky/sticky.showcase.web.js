b2fabf98301c688f4ba1332486a4cc1b4ad5ad3c
Nate Higgins <nat@nath.is>
2019-05-30 17:36:42 +0100

    chore: add simple showcase

--- packages/sticky/sticky.showcase.web.js
+++ packages/sticky/sticky.showcase.web.js
@@ -8,18 +8,100 @@ export default {
     {
       component: () => (
         <>
-          <div className="bar fixed" />
-          <StickyProvider className="container">
+          <div className="container">
             <h2>Scroll down…</h2>
+            <Sticky className="bar" />
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
               assumenda autem dolorem dolores eligendi error ipsam itaque
               laborum odit officia perspiciatis praesentium provident quaerat
               qui reiciendis, sunt vel vitae?
             </p>
-            <Sticky>
-              <div className="bar" />
-            </Sticky>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
+              assumenda autem dolorem dolores eligendi error ipsam itaque
+              laborum odit officia perspiciatis praesentium provident quaerat
+              qui reiciendis, sunt vel vitae?
+            </p>
+          </div>
+        </>
+      ),
+      name: "Sticky without fixed sibling",
+      type: "story"
+    },
+    {
+      component: () => (
+        <>
+          <div className="bar fixed" />
+          <StickyProvider className="container">
+            <h2>Scroll down…</h2>
+            <Sticky className="bar" />
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet
               assumenda autem dolorem dolores eligendi error ipsam itaque
