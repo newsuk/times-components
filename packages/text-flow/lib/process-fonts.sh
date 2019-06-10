38d2fe8e102676f86c491a2b0aa33ee3d27658ca
Mike Field-May <mfieldmay@gmail.com>
2019-06-10 11:48:19 +0100

    Update readme and install script for fonts (#2022)

--- packages/text-flow/lib/process-fonts.sh
+++ packages/text-flow/lib/process-fonts.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-pip install bs4 lxml
+pip install --user bs4 lxml
 fontforge -script ./lib/font_export/convert.pe ../../dist/public/fonts/*.ttf
 python ./lib/font_export/main.py
