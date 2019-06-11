1bef713076b971e049ee7d2078f3777c63934e7c
Nate Higgins <nat@nath.is>
2019-06-11 15:02:33 +0100

    chore: only show user state knobs if user state is being used

--- packages/storybook/src/user-state.js
+++ packages/storybook/src/user-state.js
@@ -1,13 +1,24 @@
+/* eslint-disable import/prefer-default-export */
+
 /* eslint-env browser */
-export function addUserStateKnobs(knobs, defaults = { isLoggedIn: true }) {
-  const { boolean } = knobs;
-
-  const groupId = "User State";
-  const isLoggedIn = boolean("Is Logged In", defaults.isLoggedIn || false, groupId);
-  const isMetered = boolean("Is Metered", defaults.isMetered || false, groupId);
-  const isMeteredExpired = boolean("Is Metered Expired", defaults.isMeteredExpired || false, groupId);
-  const isShared = boolean("Is Shared", defaults.isShared || false, groupId);
-
+export function addUserStateKnobs({ boolean }, defaults = { isLoggedIn: true }) {
   window.nuk = window.nuk || {};
-  window.nuk.user = { isLoggedIn, isMetered, isMeteredExpired, isShared };
+
+  Object.defineProperty(
+    window.nuk,
+    "user",
+    {
+      enumerable: true,
+      configurable: true,
+      get() {
+        const groupId = "User State";
+        const isLoggedIn = boolean("Is Logged In", defaults.isLoggedIn || false, groupId);
+        const isMetered = boolean("Is Metered", defaults.isMetered || false, groupId);
+        const isMeteredExpired = boolean("Is Metered Expired", defaults.isMeteredExpired || false, groupId);
+        const isShared = boolean("Is Shared", defaults.isShared || false, groupId);
+
+        return { isLoggedIn, isMetered, isMeteredExpired, isShared };
+      }
+    }
+  );
 }
