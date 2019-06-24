97baae842e8fd36fbf47f95b475b7b86d3bda745
Aron Shamash <aronshamash@gmail.com>
2019-06-24 14:55:06 +0100

    refactor: article link styles

--- packages/article-skeleton/src/article-body/article-link.web.js
+++ packages/article-skeleton/src/article-body/article-link.web.js
@@ -1,42 +1,16 @@
 import React from "react";
 import Link from "@times-components/link";
-import { colours, fonts, fontSizes } from "@times-components/styleguide";
 import PropTypes from "prop-types";
+import {
+  linkStyles,
+  dropCapLinkStyles
+} from "../styles/article-body/article-link";
 import withTrackEvents from "./article-link-tracking-events";
-
-// SHOULD BE IN STYLES
-const responsiveLinkStyles = {
-  base: `
-      color: ${colours.functional.action};
-      font-family: "${fonts.bodyRegular}";
-      line-height: 26px;
-      font-size: ${fontSizes.bodyMobile}px;
-      margin-bottom: 25px;
-      margin-top: 0;
-  `,
-  medium: `
-      font-size: ${fontSizes.body}px;
-      line-height: 30px;
-  `
-};
-
-const dropCapLinkStyles = {
-  base: `
-    font-size: inherit;
-    text-decoration: none;
-    color: ${colours.functional.action};
-  `,
-  medium: `
-    font-size: inherit;
-    text-decoration: none;
-    color: ${colours.functional.action};
-  `
-};
 
 const ArticleLink = ({ children, target, url, onPress, dropCap }) => (
   <Link
     underlined={!dropCap}
-    responsiveLinkStyles={dropCap ? dropCapLinkStyles : responsiveLinkStyles}
+    responsiveLinkStyles={dropCap ? dropCapLinkStyles : linkStyles}
     target={target}
     url={url}
     onPress={onPress}
---
+++ packages/article-skeleton/src/styles/article-body/article-link.web.js
@@ -0,0 +1,29 @@
+import {
+  colours,
+  fonts,
+  fontSizes,
+  spacing
+} from "@times-components/styleguide";
+
+export const linkStyles = {
+  base: `
+      color: ${colours.functional.action};
+      font-family: "${fonts.bodyRegular}";
+      line-height: 26px;
+      font-size: ${fontSizes.bodyMobile}px;
+      margin-bottom: ${spacing(5)}px;
+      margin-top: 0;
+  `,
+  medium: `
+      font-size: ${fontSizes.body}px;
+      line-height: 30px;
+  `
+};
+
+export const dropCapLinkStyles = {
+  base: `
+    font-size: inherit;
+    text-decoration: none;
+    color: ${colours.functional.action};
+  `
+};
