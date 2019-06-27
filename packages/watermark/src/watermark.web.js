896f54d145eaa5245a5250fd1cf4c8a1f78823f9
Todor Radkov <todor.radkov@news.co.uk>
2019-06-27 16:49:51 +0300

    refactoring and removing unnecessary constants

--- packages/watermark/src/watermark.web.js
+++ packages/watermark/src/watermark.web.js
@@ -2,20 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { colours } from "@times-components/styleguide";
 
-const Watermark = ({ height, viewBox, width }) => {
-  const viewBoxWithComma = viewBox.split(' ').join(',');
-  const svgView = '#svgView(viewBox(' + viewBoxWithComma + '))';
-
-  const fill= colours.functional.brandColour;
-
-  return (
-    <span className="watermark-svg-wrapper" style={{color: colours.functional.brandColour}}>
-      <svg viewBox={viewBox}  width={width} height={height}>
-        <use href="watermark.svg#watermark"></use>
-      </svg>
-    </span>
-  )
-};
+const Watermark = ({ height, viewBox, width }) => (
+  <span className="watermark-svg-wrapper" style={{color: colours.functional.brandColour}}>
+    <svg viewBox={viewBox}  width={width} height={height}>
+      <use href="watermark.svg#watermark"></use>
+    </svg>
+  </span>
+);
 
 Watermark.propTypes = {
   height: PropTypes.number.isRequired,
