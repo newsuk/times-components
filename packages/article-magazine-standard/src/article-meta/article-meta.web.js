4e2bc7750320eff0588c2fcfab21c2c414896c4a
Aron Shamash <aronshamash@gmail.com>
2019-06-27 11:10:04 +0100

    fix: article meta layout when bylines are empty (#2083)

--- packages/article-magazine-standard/src/article-meta/article-meta.web.js
+++ packages/article-magazine-standard/src/article-meta/article-meta.web.js
@@ -15,7 +15,7 @@ import styles from "../styles";
 
 const ArticleMeta = ({ bylines, publicationName, publishedTime }) => (
   <MetaContainer>
-    {bylines && (
+    {bylines.length && (
       <Fragment>
         <Meta style={styles.meta}>
           <Context.Consumer>
