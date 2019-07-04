276f4d0da4404918a1233f76e86718123027eb4c
Paola Dimova <paola.dimova@news.co.uk>
2019-07-04 16:46:56 +0300

    feat: implement huge and wide breakpoints

--- packages/edition-slices/src/tiles/tile-u/index.js
+++ packages/edition-slices/src/tiles/tile-u/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
+import { editionBreakpoints } from "@times-components/styleguide";
 import {
   getTileImage,
+  getTileSummary,
   TileLink,
   TileSummary,
   withTileTracking
 } from "../shared";
-import styles from "./styles";
+import styleFactory from "./styles";
 
-const TileU = ({ onPress, tile }) => {
+const TileU = ({ onPress, tile, breakpoint }) => {
+  const styles = styleFactory(breakpoint);
   const crop = getTileImage(tile, "crop32");
+
+  const teaserLength = {
+    [editionBreakpoints.wide]: 125,
+    [editionBreakpoints.huge]: 300
+  };
+  const summary =
+    breakpoint !== editionBreakpoints.medium
+      ? getTileSummary(tile, teaserLength[breakpoint])
+      : null;
 
   return (
     <TileLink
@@ -22,6 +34,7 @@ const TileU = ({ onPress, tile }) => {
       <TileSummary
         headlineStyle={styles.headline}
         style={styles.summaryContainer}
+        summary={summary}
         tile={tile}
         withStar
       />
@@ -39,8 +52,13 @@ const TileU = ({ onPress, tile }) => {
 };
 
 TileU.propTypes = {
+  breakpoint: PropTypes.string,
   onPress: PropTypes.func.isRequired,
   tile: PropTypes.shape({}).isRequired
+};
+
+TileU.defaultProps = {
+  breakpoint: editionBreakpoints.small
 };
 
 export default withTileTracking(TileU);
