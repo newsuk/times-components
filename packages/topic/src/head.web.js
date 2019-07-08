e27318c6b869b88e363ac95d2c4bea13b6dfaa73
Field-May <mfield-may@MC111580.local>
2019-07-08 15:06:54 +0100

    fix formtatting

--- packages/topic/src/head.web.js
+++ packages/topic/src/head.web.js
@@ -2,17 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet-async";
 import Context from "@times-components/context";
-import { getMetaContent } from '@times-components/utils';
+import { getMetaContent } from "@times-components/utils";
+
+import { propTypes as topicHeadPropTypes } from "./topic-head-prop-types";
 
 const Head = ({ description, name, slug }) => {
-  const content = description ? getMetaContent(description) : `Discover expert ${name} articles from The Times and The Sunday Times.`
+  const content = description
+    ? getMetaContent(description)
+    : `Discover expert ${name} articles from The Times and The Sunday Times.`;
   return (
     <Context.Consumer>
       {({ makeTopicUrl }) => (
         <Helmet>
           <title>{name} | The Times &amp; The Sunday Times</title>
           <meta
-            content={content || `Discover expert ${name} articles from The Times and The Sunday Times.`}
+            content={
+              content ||
+              `Discover expert ${name} articles from The Times and The Sunday Times.`
+            }
             name="description"
           />
           <link href={makeTopicUrl({ slug })} rel="canonical" />
@@ -20,10 +27,10 @@ const Head = ({ description, name, slug }) => {
       )}
     </Context.Consumer>
   );
-}
+};
 
 Head.propTypes = {
-  description: PropTypes.array,
+  description: topicHeadPropTypes.description.isRequired,
   name: PropTypes.string.isRequired,
   slug: PropTypes.string.isRequired
 };
