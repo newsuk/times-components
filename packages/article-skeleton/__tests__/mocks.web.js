e664cf265f3cdad05890e32c79b582b063da243c
Nate Higgins <nat@nath.is>
2019-07-16 16:42:37 +0100

    chore: snaps

--- packages/article-skeleton/__tests__/mocks.web.js
+++ packages/article-skeleton/__tests__/mocks.web.js
@@ -25,7 +25,11 @@ jest.mock("@times-components/date-publication", () => "DatePublication");
 jest.mock("@times-components/image", () => "Image");
 jest.mock("@times-components/interactive-wrapper", () => "InteractiveWrapper");
 jest.mock("@times-components/key-facts", () => "KeyFacts");
-jest.mock("@times-components/link", () => "Link");
+jest.mock("@times-components/link", () => ({
+  __esModule: true,
+  default: "Link",
+  TextLink: "TextLink"
+}));
 jest.mock("@times-components/pull-quote", () => "PullQuote");
 jest.mock("@times-components/related-articles", () => "RelatedArticles");
 jest.mock("@times-components/video-label", () => "VideoLabel");
