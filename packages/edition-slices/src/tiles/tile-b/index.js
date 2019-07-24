009c030eae3b816541ac6836d6df9c500728ad13
Paola Dimova <paola.dimova@news.co.uk>
2019-07-24 11:59:16 +0300

    feat: update TileB

--- packages/edition-slices/src/tiles/tile-b/index.js
+++ packages/edition-slices/src/tiles/tile-b/index.js
@@ -1,26 +1,33 @@
+/* eslint-disable react/require-default-props */
 import React from "react";
 import PropTypes from "prop-types";
+import { editionBreakpoints } from "@times-components/styleguide";
 import {
   getTileSummary,
   TileLink,
   TileSummary,
   withTileTracking
 } from "../shared";
-import styles from "./styles";
+import stylesFactory from "./styles";
 
-const TileB = ({ onPress, tile }) => (
-  <TileLink onPress={onPress} style={styles.container} tile={tile}>
-    <TileSummary
-      headlineStyle={styles.headline}
-      summary={getTileSummary(tile, 125)}
-      tile={tile}
-    />
-  </TileLink>
-);
+const TileB = ({ onPress, tile, breakpoint = editionBreakpoints.small }) => {
+  const styles = stylesFactory(breakpoint);
+
+  return (
+    <TileLink onPress={onPress} style={styles.container} tile={tile}>
+      <TileSummary
+        headlineStyle={styles.headline}
+        summary={getTileSummary(tile, 125)}
+        tile={tile}
+      />
+    </TileLink>
+  );
+};
 
 TileB.propTypes = {
   onPress: PropTypes.func.isRequired,
-  tile: PropTypes.shape({}).isRequired
+  tile: PropTypes.shape({}).isRequired,
+  breakpoint: PropTypes.string
 };
 
 export default withTileTracking(TileB);
