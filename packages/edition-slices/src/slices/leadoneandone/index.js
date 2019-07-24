999c3db6690731970e43d97d2535c13f86692f45
Paola Dimova <paola.dimova@news.co.uk>
2019-07-24 15:02:28 +0300

    fix: keep TileC for wide breakpoint

--- packages/edition-slices/src/slices/leadoneandone/index.js
+++ packages/edition-slices/src/slices/leadoneandone/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { LeadOneAndOneSlice } from "@times-components/slice-layout";
-import { TileA, TileB, TileU, TileAQ } from "../../tiles";
+import { TileA, TileB, TileC, TileU, TileAQ } from "../../tiles";
 import { ResponsiveSlice } from "../shared";
 
 class LeadOneAndOne extends Component {
@@ -9,6 +9,7 @@ class LeadOneAndOne extends Component {
     super(props);
     this.renderSmall = this.renderSmall.bind(this);
     this.renderMedium = this.renderMedium.bind(this);
+    this.renderWide = this.renderWide.bind(this);
     this.renderHuge = this.renderHuge.bind(this);
   }
 
@@ -54,6 +55,27 @@ class LeadOneAndOne extends Component {
     );
   }
 
+  renderWide(breakpoint) {
+    const {
+      onPress,
+      slice: { lead, support }
+    } = this.props;
+    return (
+      <LeadOneAndOneSlice
+        breakpoint={breakpoint}
+        lead={
+          <TileU
+            breakpoint={breakpoint}
+            onPress={onPress}
+            tile={lead}
+            tileName="lead"
+          />
+        }
+        support={<TileC onPress={onPress} tile={support} tileName="support" />}
+      />
+    );
+  }
+
   renderHuge(breakpoint) {
     const {
       onPress,
@@ -80,7 +102,7 @@ class LeadOneAndOne extends Component {
       <ResponsiveSlice
         renderSmall={this.renderSmall}
         renderMedium={this.renderMedium}
-        renderWide={this.renderMedium}
+        renderWide={this.renderWide}
         renderHuge={this.renderHuge}
       />
     );
