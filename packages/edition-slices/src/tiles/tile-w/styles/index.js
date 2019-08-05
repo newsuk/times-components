4407525e2b619324d8287f4dee93f473644ed3ea
Apostol Nikov <apostolnikov@users.noreply.github.com>
2019-08-05 16:13:55 +0300

    feat: implement secondary one slice 768px (#2151)
    * feat: implement secondary one slice 768px
    
    * fix: recacl width and margin
    
    * fix: more styles
    
    * chore: update tests and snaps

--- packages/edition-slices/src/tiles/tile-w/styles/index.js
+++ packages/edition-slices/src/tiles/tile-w/styles/index.js
@@ -1,20 +1,16 @@
-import {
-  fontFactory,
-  spacing,
-  editionBreakpoints
-} from "@times-components/styleguide";
+import { fontFactory, spacing, fonts } from "@times-components/styleguide";
 
-const styles = fontSize => ({
+const main = {
   container: {
     flexDirection: "row",
     paddingHorizontal: spacing(4),
-    paddingTop: spacing(2),
+    paddingTop: spacing(3),
     paddingBottom: spacing(3)
   },
   headline: {
     ...fontFactory({
       font: "headline",
-      fontSize
+      fontSize: "tileLeadHeadline"
     })
   },
   imageContainer: {
@@ -24,10 +20,81 @@ const styles = fontSize => ({
     paddingBottom: spacing(1),
     paddingRight: spacing(4),
     width: "33.33%"
+  },
+  star: {
+    alignItems: "flex-start",
+    flex: 1,
+    marginTop: spacing(-1),
+    marginLeft: spacing(5),
+    width: "75%"
   }
-});
+};
 
-export default breakpoint =>
-  editionBreakpoints.huge === breakpoint
-    ? styles("articleHeadline")
-    : styles("tileLeadHeadline");
+const mediumStyles = {
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    marginHorizontal: spacing(4),
+    paddingHorizontal: spacing(2),
+    paddingVertical: spacing(3)
+  },
+  headline: {
+    fontFamily: fonts.headline,
+    fontSize: 30,
+    lineHeight: 30,
+    marginBottom: spacing(2)
+  },
+  summaryContainer: {
+    width: "51.5%",
+    paddingRight: spacing(4)
+  },
+  imageContainer: {
+    width: "48.5%"
+  },
+  star: {
+    starButton: {
+      position: "absolute",
+      right: -spacing(1),
+      bottom: -spacing(1)
+    }
+  }
+};
+
+const wideStyles = {
+  star: {
+    alignItems: "flex-start",
+    flex: 1,
+    marginTop: spacing(-1),
+    width: "72%"
+  },
+  headline: {
+    fontFamily: fonts.headline,
+    fontSize: 35,
+    lineHeight: 35
+  }
+};
+
+const hugeStyles = {
+  headline: {
+    fontFamily: fonts.headline,
+    fontSize: 35,
+    lineHeight: 35
+  },
+  star: {
+    alignItems: "flex-start",
+    flex: 1,
+    marginTop: spacing(-1),
+    width: "72%"
+  }
+};
+
+const stylesResolver = {
+  medium: mediumStyles,
+  wide: wideStyles,
+  huge: hugeStyles
+};
+
+export default breakpoint => ({
+  ...main,
+  ...(stylesResolver[breakpoint] || {})
+});
