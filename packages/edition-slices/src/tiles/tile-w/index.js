7795c4b017dbfefc190f0d4dbf004410d8df6490
Paola Dimova <paola.dimova@news.co.uk>
2019-08-05 14:00:29 +0300

    chore: update tests and snaps

--- packages/edition-slices/src/tiles/tile-w/index.js
+++ packages/edition-slices/src/tiles/tile-w/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
-import { editionBreakpoints } from "@times-components/styleguide";
 import {
   getTileImage,
   getTileSummary,
