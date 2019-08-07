3a329cf4a508038abba8a146331c4001b84b8cbe
Jaye Marshall <jaye.marshall@news.co.uk>
2019-08-07 03:41:10 -1000

    feat: (REPLAT-6797) add viewport tracking for unruly ads (#2111)

--- packages/jest-configurator/setup-jest.js
+++ packages/jest-configurator/setup-jest.js
@@ -7,3 +7,14 @@ Enzyme.configure({ adapter: new Adapter() });
 
 if (typeof window !== "undefined")
   window.HTMLCanvasElement.prototype.getContext = () => {};
+
+jest.mock("react-native-device-info", () => {
+  return {
+    getApplicationName: jest.fn(),
+    getBuildNumber: jest.fn(),
+    getBundleId: jest.fn(),
+    getDeviceId: jest.fn(),
+    getReadableVersion: jest.fn(),
+    getVersion: jest.fn()
+  };
+});
