b7a7d58635f00d7971cf33e2f0474e1e2f4a6585
parkjh <joonhop@me.com>
2019-08-09 09:02:28 +0100

    remove eslint disable

--- packages/edition-slices/src/tiles/tile-g/index.js
+++ packages/edition-slices/src/tiles/tile-g/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/require-default-props */
+
 import React from "react";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
--- packages/edition-slices/src/tiles/tile-x/index.js
+++ packages/edition-slices/src/tiles/tile-x/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/require-default-props */
+
 import React from "react";
 import PropTypes from "prop-types";
 import editionBreakpoints from "@times-components/styleguide";
