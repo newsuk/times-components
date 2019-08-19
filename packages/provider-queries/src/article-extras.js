fabde40f1c66077b2ba6c7300edd82a10a02130f
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2019-08-19 14:02:05 +0300

    refactor: cleanup unnecessary code

--- packages/provider-queries/src/article-extras.js
+++ packages/provider-queries/src/article-extras.js
@@ -1,5 +1,4 @@
 import { addTypenameToDocument } from "apollo-utilities";
-import gql from "graphql-tag";
 import articleExtras from './article_extras.graphql';
 
 export default addTypenameToDocument(articleExtras);
--- packages/provider-queries/src/article-fragment.js
+++ packages/provider-queries/src/article-fragment.js
@@ -1,4 +1,3 @@
-import gql from "graphql-tag";
 import articlePageProps from "./article_page_props.graphql";
 
 export default articlePageProps;
