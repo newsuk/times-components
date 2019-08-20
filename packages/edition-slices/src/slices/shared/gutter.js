3c8c839a93c837bd77cb955479da383d94320360
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-08-20 14:56:50 +0300

    feat: gutter styling moved to styles

--- packages/edition-slices/src/slices/shared/gutter.js
+++ packages/edition-slices/src/slices/shared/gutter.js
@@ -2,35 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
 import { ResponsiveContext } from "@times-components/responsive";
-import {
-  editionMaxWidth,
-  spacing,
-  editionBreakpoints
-} from "@times-components/styleguide";
+import styleFactory from "./styles";
 
 const Gutter = ({ children }) => (
   <ResponsiveContext.Consumer>
     {({ isTablet, editionBreakpoint }) => {
-      const tabletPaddingHorizontalResolver = {
-        [editionBreakpoints.medium]: spacing(2),
-        [editionBreakpoints.wide]: spacing(6),
-        [editionBreakpoints.huge]: spacing(2)
-      };
+      const styles = styleFactory(isTablet, editionBreakpoint);
 
-      return (
-        <View
-          style={{
-            alignSelf: "center",
-            maxWidth: "100%",
-            paddingHorizontal: isTablet
-              ? tabletPaddingHorizontalResolver[editionBreakpoint]
-              : 0,
-            width: editionMaxWidth
-          }}
-        >
-          {children}
-        </View>
-      );
+      return <View style={styles.gutterStyles}>{children}</View>;
     }}
   </ResponsiveContext.Consumer>
 );
--- packages/edition-slices/src/slices/shared/styles.js
+++ packages/edition-slices/src/slices/shared/styles.js
@@ -1,11 +1,29 @@
-import { sliceContentMaxWidth } from "@times-components/styleguide";
+import {
+  editionMaxWidth,
+  sliceContentMaxWidth,
+  spacing,
+  editionBreakpoints
+} from "@times-components/styleguide";
 
-const styles = {
+const tabletPaddingHorizontalResolver = {
+  [editionBreakpoints.medium]: spacing(2),
+  [editionBreakpoints.wide]: spacing(6),
+  [editionBreakpoints.huge]: spacing(2)
+};
+
+export default (isTablet, breakpoint) => ({
   contentWrapperStyles: {
     flex: 1,
     alignSelf: "center",
     width: sliceContentMaxWidth
-  }
-};
+  },
 
-export default styles;
+  gutterStyles: {
+    alignSelf: "center",
+    maxWidth: "100%",
+    paddingHorizontal: isTablet
+      ? tabletPaddingHorizontalResolver[breakpoint]
+      : 0,
+    width: editionMaxWidth
+  }
+});
