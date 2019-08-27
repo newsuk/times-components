208d63b2f21a15ef8e6fbe1652eaef9d21b7f0e6
Nikola Ivanov <nikola.ivanov@news.co.uk>
2019-08-27 11:41:37 +0300

    Add summary and teaser for leadoneandfour fragment

--- packages/provider-queries/src/section-fragment.js
+++ packages/provider-queries/src/section-fragment.js
@@ -133,7 +133,9 @@ export default gql`
             listingAsset {
               ...listingAsset169
             }
+            summary125: summary(maxCharCount: 125)
           }
+          teaser125: teaser(maxCharCount: 125)
         }
         support1 {
           headline
@@ -801,7 +803,9 @@ export default gql`
             listingAsset {
               ...listingAsset169
             }
+            summary125: summary(maxCharCount: 125)
           }
+          teaser125: teaser(maxCharCount: 125)
         }
         support1 {
           headline
