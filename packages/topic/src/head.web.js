09af5845544046690469c841d110e7a44d311672
Tobenna <tobennandu@gmail.com>
2019-08-27 16:00:42 +0100

    :art:

--- packages/topic/src/head.web.js
+++ packages/topic/src/head.web.js
@@ -16,7 +16,11 @@ function Head({ description, name, slug }) {
     <Context.Consumer>
       {({ makeTopicUrl }) => (
         <Helmet>
-          <link rel="preload" href="/assets/optimizely/16079460199.js" as="script" />
+          <link
+            as="script"
+            href="/assets/optimizely/16079460199.js"
+            rel="preload"
+          />
           <script src="/assets/optimizely/16079460199.js" />
           <title>{name} | The Times &amp; The Sunday Times</title>
           <meta content={content} name="description" />
