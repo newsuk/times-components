65b8ea325d4f51a45c075b0f3809ef78ad7ae542
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-08-27 10:47:30 +0300

    fix: lead one will width 768 spacing updates

--- packages/edition-slices/edition-tiles.showcase.js
+++ packages/edition-slices/edition-tiles.showcase.js
@@ -159,7 +159,7 @@ const tileStories = [
     Tile: TileQ
   },
   {
-    name: "Tile R - Bottom image, 45pt headline, no teaser",
+    name: "Tile R - Bottom image, 40pt headline, no teaser",
     Tile: TileR
   },
   {
