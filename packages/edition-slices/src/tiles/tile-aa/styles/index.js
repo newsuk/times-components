3d2ef9aeda8e749b552374fcb6fb019b01554d28
Valentin Georgiev <valentin.georgiev@news.co.uk>
2019-08-29 17:52:32 +0300

    fix: tile aa style

--- packages/edition-slices/src/tiles/tile-aa/styles/index.js
+++ packages/edition-slices/src/tiles/tile-aa/styles/index.js
@@ -1,6 +1,9 @@
-import { fonts, spacing } from "@times-components/styleguide";
-
-const styles = {
+import {
+  fonts,
+  spacing,
+  editionBreakpoints
+} from "@times-components/styleguide";
+const mediumBreakpointStyles = {
   container: {
     flex: 1,
     paddingVertical: spacing(3),
@@ -12,5 +15,16 @@ const styles = {
     lineHeight: 20
   }
 };
-
-export default styles;
+const wideBreakpointStyles = {
+  ...mediumBreakpointStyles,
+  headline: {
+    fontFamily: fonts.headline,
+    fontSize: 28,
+    lineHeight: 28,
+    marginBottom: spacing(2)
+  }
+};
+export default breakpoint =>
+  breakpoint === editionBreakpoints.medium
+    ? mediumBreakpointStyles
+    : wideBreakpointStyles;
