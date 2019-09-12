d24ec5732a35297d4c9346a04cf49e5cdb829f46
Aron Shamash <aronshamash@gmail.com>
2019-09-12 17:08:35 +0100

    feat: test extractNames function

--- packages/ad/__tests__/utils/ad-init-admantx.js
+++ packages/ad/__tests__/utils/ad-init-admantx.js
@@ -8,7 +8,7 @@ export default () => {
     ({ initOptions } = makeAdInitMocks());
   });
 
-  it("ADmantx is initialised", () => {
+  it("initialises admantx", () => {
     const init = adInit(
       merge(initOptions, { data: { bidInitialiser: false } })
     );
@@ -16,5 +16,26 @@ export default () => {
     jest.spyOn(init.admantx, "init");
     init.init();
     expect(init.admantx.init).toHaveBeenCalledTimes(1);
+  });
+
+  it("extracts names correctly", () => {
+    const init = adInit(initOptions);
+
+    const input = [
+      { score: 6, origin: "NORMAL", name: "foo", type: "MAINLEMMAS" },
+      { score: 1, origin: "NORMAL", name: "Bar", type: "PLACES" },
+      { score: 2, origin: "NORMAL", name: "Foo Bar    ", type: "MAINLEMMAS" },
+      { score: 2, origin: "NORMAL", name: "Foo Bar    ", type: "PLACES" },
+      {
+        score: 1,
+        origin: "NORMAL",
+        name: "Foo Foo Bar['=!+#*~;^()<>[],& ",
+        type: "PLACES"
+      }
+    ];
+
+    const output = "foo,bar,foo_bar,foo_foo_bar";
+
+    expect(init.admantx.extractNames(input)).toEqual(output);
   });
 };
