0f089b68b2665d0aacb4c62ee13d69c6c48d32f7
Apostol Nikov <apostolnikov@users.noreply.github.com>
2019-09-25 13:23:16 +0300

    feat: lead2-no-pic-and2 for 1366px (#2310)
    * feat: lead2-no-pic-and2 for 1366px
    
    * chore: add default breakpoint TileAL
    
    * fix: wrong import

--- packages/edition-slices/src/tiles/tile-al/styles/index.js
+++ packages/edition-slices/src/tiles/tile-al/styles/index.js
@@ -1,6 +1,15 @@
-import { fonts, spacing } from "@times-components/styleguide";
+import {
+  fonts,
+  spacing,
+  editionBreakpoints
+} from "@times-components/styleguide";
 
-const styles = {
+const fontSizeResolver = {
+  [editionBreakpoints.wide]: 20,
+  [editionBreakpoints.huge]: 22
+};
+
+export default breakpoint => ({
   container: {
     flex: 1,
     paddingHorizontal: spacing(2),
@@ -8,13 +17,11 @@ const styles = {
   },
   headline: {
     fontFamily: fonts.headline,
-    fontSize: 20,
-    lineHeight: 20
+    fontSize: fontSizeResolver[breakpoint],
+    lineHeight: fontSizeResolver[breakpoint]
   },
   imageContainer: {
     width: "100%",
     marginBottom: spacing(2)
   }
-};
-
-export default styles;
+});
