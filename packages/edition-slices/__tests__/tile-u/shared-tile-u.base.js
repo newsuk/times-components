905de2f371ee284c59fcf21bac7fb58973dffac4
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-10-02 15:24:29 +0300

    feat: REPLAT-9112 lead 1 and 1 tile U updated

--- packages/edition-slices/__tests__/tile-u/shared-tile-u.base.js
+++ packages/edition-slices/__tests__/tile-u/shared-tile-u.base.js
@@ -1,23 +1,10 @@
 import "../mocks-tiles";
-import { editionBreakpoints } from "@times-components/styleguide";
 import { testTile } from "../shared-tile-utils";
 import { TileU } from "../../src/tiles";
 
 export default () => {
   describe("tile u", () => {
-    it("medium", () => {
-      testTile(TileU, editionBreakpoints.medium);
-    });
-
-    it("wide", () => {
-      testTile(TileU, editionBreakpoints.wide);
-    });
-
-    it("huge", () => {
-      testTile(TileU, editionBreakpoints.huge);
-    });
-
-    it("without breakpoint should be like medium", () => {
+    it("without breakpoint", () => {
       testTile(TileU);
     });
   });
--- packages/edition-slices/src/tiles/tile-u/styles/index.js
+++ packages/edition-slices/src/tiles/tile-u/styles/index.js
@@ -1,39 +1,21 @@
-import {
-  fonts,
-  spacing,
-  editionBreakpoints
-} from "@times-components/styleguide";
+import { fonts, spacing } from "@times-components/styleguide";
 
-const mediumBreakpointStyles = {
+const styles = {
   container: {
     flex: 1,
     paddingVertical: spacing(3),
     paddingHorizontal: spacing(2)
   },
   imageContainer: {
-    width: "100%"
-  },
-  summaryContainer: {
     width: "100%",
-    paddingBottom: spacing(2)
+    marginBottom: spacing(2)
   },
   headline: {
     fontFamily: fonts.headline,
     fontSize: 40,
     lineHeight: 40,
-    marginBottom: spacing(2)
-  }
-};
-
-const wideBreakpointStyles = {
-  ...mediumBreakpointStyles,
-  headline: {
-    ...mediumBreakpointStyles.headline,
     marginBottom: 0
   }
 };
 
-export default breakpoint =>
-  breakpoint === editionBreakpoints.medium
-    ? mediumBreakpointStyles
-    : wideBreakpointStyles;
+export default styles;
