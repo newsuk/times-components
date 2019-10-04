d685fe69fee2a6b84ef579a8917a376c9399c8f3
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-10-04 07:52:59 +0100

    feat: REPLAT-9112 lead 1 and 1 tile U updated (#2322)

--- packages/edition-slices/src/tiles/tile-u/index.js
+++ packages/edition-slices/src/tiles/tile-u/index.js
@@ -1,44 +1,21 @@
 /* eslint-disable react/require-default-props */
 import React from "react";
-import { View } from "react-native";
 import PropTypes from "prop-types";
 import Image from "@times-components/image";
-import { editionBreakpoints } from "@times-components/styleguide";
 import {
   getTileImage,
-  getTileSummary,
   TileLink,
   TileSummary,
   withTileTracking
 } from "../shared";
-import styleFactory from "./styles";
-import WithoutWhiteSpace from "../shared/without-white-space";
-import PositionedTileStar from "../shared/positioned-tile-star";
+import styles from "./styles";
 
-const TileU = ({ onPress, tile, breakpoint = editionBreakpoints.medium }) => {
-  const styles = styleFactory(breakpoint);
-  const crop = getTileImage(tile, "crop32");
-
+const TileU = ({ onPress, tile }) => {
+  const crop = getTileImage(tile, "crop169");
   return (
     <TileLink onPress={onPress} style={styles.container} tile={tile}>
-      <View style={styles.summaryContainer}>
-        <WithoutWhiteSpace
-          render={whiteSpaceHeight => (
-            <TileSummary
-              headlineStyle={styles.headline}
-              summary={getTileSummary(tile, 800)}
-              tile={tile}
-              whiteSpaceHeight={whiteSpaceHeight}
-              linesOfTeaserToRender={3}
-              withStar={false}
-            />
-          )}
-        />
-        <PositionedTileStar articleId={tile.article.id} />
-      </View>
-
       <Image
-        aspectRatio={3 / 2}
+        aspectRatio={16 / 9}
         relativeWidth={crop.relativeWidth}
         relativeHeight={crop.relativeHeight}
         relativeHorizontalOffset={crop.relativeHorizontalOffset}
@@ -47,12 +24,12 @@ const TileU = ({ onPress, tile, breakpoint = editionBreakpoints.medium }) => {
         uri={crop.url}
         fill
       />
+      <TileSummary headlineStyle={styles.headline} tile={tile} />
     </TileLink>
   );
 };
 
 TileU.propTypes = {
-  breakpoint: PropTypes.string,
   onPress: PropTypes.func.isRequired,
   tile: PropTypes.shape({}).isRequired
 };
