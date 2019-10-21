b956c801d3f9ee0adc76931da96621dc8580e863
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-10-21 14:12:32 +0300

    feat: REPLAT-9113 global spacing styles added

--- packages/styleguide/src/spacing/index.js
+++ packages/styleguide/src/spacing/index.js
@@ -1,3 +1,12 @@
 import spacing from "./base";
 
+export const globalSpacingStyles = {
+    tabletHeadline: {
+        marginBottom: 0
+    },
+    tabletTeaser: {
+        marginTop: spacing(2)
+    }
+};
+
 export default spacing;
