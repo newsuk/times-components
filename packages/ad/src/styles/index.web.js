103ecee644fa1a44b767849a72a14e7652b34a07
Jaye Marshall <marshall.jaye@gmail.com>
2019-10-21 16:41:00 +0100

    fix a changed web snapshot

---
+++ packages/ad/src/styles/index.web.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+import baseStyles from "./index.shared";
+
+export default StyleSheet.create(baseStyles);
+
+export { calculateViewBox, calculateViewportVisible } from "./index.shared";
