dac9ade8a1030f1ab93c7e90bc79aaa60ecd1da8
Georgi Mirchev <georgi.mirchev@news.co.uk>
2019-10-22 13:30:55 +0300

    feat: REPLAT-9113 tileZ and tileAF styling

--- packages/edition-slices/__tests__/tile-af/shared-tile-af.base.js
+++ packages/edition-slices/__tests__/tile-af/shared-tile-af.base.js
@@ -1,10 +1,19 @@
 import "../mocks-tiles";
+import { editionBreakpoints } from "@times-components/styleguide";
 import { testTile } from "../shared-tile-utils";
 import { TileAF } from "../../src/tiles";
 
 export default () => {
   describe("tile af", () => {
-    it("without breakpoint", () => {
+    it("wide", () => {
+      testTile(TileAF, editionBreakpoints.wide);
+    });
+
+    it("huge", () => {
+      testTile(TileAF, editionBreakpoints.huge);
+    });
+
+    it("without breakpoint should be like wide", () => {
       testTile(TileAF);
     });
   });
