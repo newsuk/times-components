34aef8f1c2940283e0e9e185772546c399e724fe
Jaye Marshall <jaye@Jayes-iMac.local>
2019-11-05 17:28:44 +0000

    adjust padding on ios

--- packages/article-main-standard/src/styles/article-meta/index.js
+++ packages/article-main-standard/src/styles/article-meta/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import styleguide from "@times-components/styleguide";
 import sharedStyles from "./shared";
 
@@ -10,8 +10,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     marginLeft: spacing(2),
     marginRight: spacing(2),
-    paddingBottom: spacing(2),
-    marginBottom: spacing(4)
+    marginBottom: spacing(4),
+    ...Platform.select({
+      ios: {
+        paddingBottom: spacing(1)
+      },
+      android: {
+        paddingBottom: spacing(2)
+      }
+    })
   },
   articleMetaElement: {
     ...sharedStyles.articleMetaElement,
