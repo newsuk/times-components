2268559b32a0949ef0daa6cb9916df114a42190f
Jaye Marshall <marshall.jaye@gmail.com>
2019-12-09 19:02:44 -0800

    fix: more snaps

--- packages/typeset/src/index.ts
+++ packages/typeset/src/index.ts
@@ -6,4 +6,3 @@ export { default as PositionedItem } from './PositionedItem';
 export * from './AttributedString';
 export * from './BoxExclusion';
 export { default as FontStorage } from './FontStorage';
-export * from './Utils';
