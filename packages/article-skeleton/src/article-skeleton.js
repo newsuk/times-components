f14e38786aa84485d49731674b28bbe6641a0f44
Jaye Marshall <marshall.jaye@gmail.com>
2019-12-12 12:50:34 -0800

    fix: scroll to top on content load

--- packages/article-skeleton/src/article-skeleton.js
+++ packages/article-skeleton/src/article-skeleton.js
@@ -1,10 +1,9 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import { View, FlatList, ActivityIndicator, Platform } from "react-native";
 import PropTypes from "prop-types";
 import { screenWidth } from "@times-components/utils";
 import { withTrackScrollDepth } from "@times-components/tracking";
 import { Viewport } from "@skele/components";
-import { AdComposer } from "@times-components/ad";
 import { render } from "@times-components/markup-forest";
 import ArticleExtras from "@times-components/article-extras";
 import {
@@ -32,9 +31,8 @@ const isDropcapsDisabled = ({ template, dropcapsDisabled }) => {
   return !templateWithDropCaps.includes(template);
 };
 
-const ArticleSkeleton = props => {
+const ArticleWithContent = props => {
   const {
-    adConfig,
     Header,
     data,
     analyticsStream,
@@ -45,6 +43,8 @@ const ArticleSkeleton = props => {
     isTablet,
     onViewed
   } = props;
+
+  const { id, url, content } = data;
 
   const onViewableItemsChanged = useCallback(
     info => {
@@ -72,88 +72,132 @@ const ArticleSkeleton = props => {
     setLoading(false);
   }, []);
 
-  if (!data) {
-    return null;
-  }
-
-  const { id, url, content } = data;
-
-  if (!content) {
-    return null;
-  }
-
-  const header = (
-    <Gutter>
-      <Header width={Math.min(maxWidth, screenWidth())} />
-    </Gutter>
+  const header = useMemo(
+    () => (
+      <Gutter>
+        <Header width={Math.min(maxWidth, screenWidth())} />
+      </Gutter>
+    ),
+    []
   );
 
-  const footer = (
-    <Gutter>
-      <ArticleExtras
-        analyticsStream={analyticsStream}
-        articleId={id}
-        articleUrl={url}
-        onCommentGuidelinesPress={onCommentGuidelinesPress}
-        onCommentsPress={onCommentsPress}
-        onRelatedArticlePress={onRelatedArticlePress}
-        onTopicPress={onTopicPress}
-      />
-    </Gutter>
+  const footer = useMemo(
+    () => (
+      <Gutter>
+        <ArticleExtras
+          analyticsStream={analyticsStream}
+          articleId={id}
+          articleUrl={url}
+          onCommentGuidelinesPress={onCommentGuidelinesPress}
+          onCommentsPress={onCommentsPress}
+          onRelatedArticlePress={onRelatedArticlePress}
+          onTopicPress={onTopicPress}
+        />
+      </Gutter>
+    ),
+    [
+      analyticsStream,
+      id,
+      onCommentGuidelinesPress,
+      onCommentsPress,
+      onRelatedArticlePress,
+      onTopicPress,
+      url
+    ]
   );
 
   const dropcapsDisabled = isDropcapsDisabled(data);
   const renderChild = render(renderers({ dropcapsDisabled, ...props }));
   // eslint-disable-next-line react/prop-types
-  const Child = ({ item, index }) => (
-    <Gutter style={{ overflow: "hidden" }}>
-      <ErrorBoundary>
-        {item.name === "footer"
-          ? footer
-          : renderChild(item, index.toString(), index)}
-      </ErrorBoundary>
-    </Gutter>
+  const Child = useCallback(
+    ({ item, index }) => (
+      <Gutter style={{ overflow: "hidden" }}>
+        <ErrorBoundary>
+          {item.name === "footer"
+            ? footer
+            : renderChild(item, index.toString(), index)}
+        </ErrorBoundary>
+      </Gutter>
+    ),
+    [footer, renderChild]
   );
 
-  const fixedContent = [...fixup(isTablet, content), { name: "footer" }];
+  const fixedContent = useMemo(
+    () => [...fixup(isTablet, content), { name: "footer" }],
+    [content, isTablet]
+  );
 
   // FIXME: remove this when ios memory leaks are resolved
-  const Scroller = scrollprops =>
-    Platform.select({
-      ios: (
-        <FlatList
-          {...scrollprops}
-          data={scrollprops.data.map((item, index) => Child({ item, index }))}
-          renderItem={({ item }) => item}
-        />
-      ),
-      android: <FlatList {...scrollprops} />
-    });
+  const Scroller = React.useCallback(
+    scrollprops =>
+      Platform.select({
+        ios: (
+          <FlatList
+            {...scrollprops}
+            data={scrollprops.data.map((item, index) => Child({ item, index }))}
+            renderItem={({ item }) => item}
+          />
+        ),
+        android: <FlatList {...scrollprops} />
+      }),
+    [Child]
+  );
 
   return (
-    <AdComposer adConfig={adConfig}>
-      <View style={styles.articleContainer}>
-        <Viewport.Tracker>
-          <Scroller
-            data={fixedContent}
-            ListEmptyComponent={Loading}
-            ListHeaderComponent={header}
-            ListFooterComponent={Loading}
-            onEndReached={onEndReached}
-            showsVerticalScrollIndicator={false}
-            renderItem={Child}
-            onViewableItemsChanged={onViewableItemsChanged}
-            removeClippedSubviews
-            keyExtractor={(item, index) => index.toString()}
-            initialNumToRender={2}
-            windowSize={3}
-            nestedScrollEnabled
-            testID="flat-list-article"
-          />
-        </Viewport.Tracker>
-      </View>
-    </AdComposer>
+    <View style={styles.articleContainer}>
+      <Viewport.Tracker>
+        <Scroller
+          data={fixedContent}
+          extraData={loading}
+          ListEmptyComponent={Loading}
+          ListHeaderComponent={header}
+          ListFooterComponent={Loading}
+          onEndReached={onEndReached}
+          showsVerticalScrollIndicator={false}
+          renderItem={Child}
+          onViewableItemsChanged={onViewableItemsChanged}
+          removeClippedSubviews
+          keyExtractor={(item, index) => index.toString()}
+          initialNumToRender={2}
+          windowSize={3}
+          nestedScrollEnabled
+          testID="flat-list-article"
+        />
+      </Viewport.Tracker>
+    </View>
   );
+};
+
+ArticleWithContent.propTypes = {
+  ...articleSkeletonPropTypes,
+  interactiveConfig: PropTypes.shape({}),
+  onCommentGuidelinesPress: PropTypes.func.isRequired,
+  onCommentsPress: PropTypes.func.isRequired,
+  onLinkPress: PropTypes.func.isRequired,
+  onRelatedArticlePress: PropTypes.func.isRequired,
+  onTwitterLinkPress: PropTypes.func.isRequired,
+  onVideoPress: PropTypes.func.isRequired,
+  onImagePress: PropTypes.func.isRequired
+};
+ArticleWithContent.defaultProps = {
+  ...articleSkeletonDefaultProps,
+  interactiveConfig: {}
+};
+
+const ArticleSkeleton = props => {
+  const { data } = props;
+
+  if (!data) {
+    return null;
+  }
+
+  const { content } = data;
+
+  if (!content) {
+    return null;
+  }
+
+  return <ArticleWithContent {...props} />;
 };
 
 ArticleSkeleton.displayName = "ArticleSkeleton";
