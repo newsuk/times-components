51af92783e1c9d8b1bc7c41a9a29966acd8c9bdd
Georgiev, Daniel <Daniel.Georgiev@news.co.uk>
2019-12-20 10:20:54 +0200

    :chore: removed brightcove dependencies, updated snapshots

--- packages/article-skeleton/__tests__/mocks.native.js
+++ packages/article-skeleton/__tests__/mocks.native.js
@@ -17,7 +17,6 @@ jest.mock("@times-components/article-image", () => "ArticleImage");
 jest.mock("@times-components/article-label", () => "ArticleLabel");
 
 jest.mock("@times-components/article-topics", () => "ArticleTopics");
-jest.mock("@times-components/brightcove-video", () => "BrightcoveVideo");
 jest.mock("@times-components/button", () => "Button");
 jest.mock("@times-components/image", () => ({
   ModalImage: "ModalImage"
