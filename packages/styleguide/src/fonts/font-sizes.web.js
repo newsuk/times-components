74ee7b1bf6b9526576c55308399285eaf66a16d7
Georgi Mirchev <georgi.mirchev@news.co.uk>
2020-01-23 16:55:35 +0200

    feat: REPLAT-11948 article H-tags web implemented (#2493)
    Co-authored-by: Valentin Georgiev <valentin.al.georgiev@gmail.com>

--- packages/styleguide/src/fonts/font-sizes.web.js
+++ packages/styleguide/src/fonts/font-sizes.web.js
@@ -2,7 +2,15 @@ import sharedFontSizes from "./font-sizes-base";
 
 const fontSizes = () => ({
   ...sharedFontSizes,
-  infoSubText: 18
+  infoSubText: 18,
+  heading2: 28,
+  heading2Mobile: 25,
+  heading3: 24,
+  heading3Mobile: 22,
+  heading4: 20,
+  heading4Mobile: 19,
+  heading5: 19,
+  heading5Mobile: 17
 });
 
 export default fontSizes;
