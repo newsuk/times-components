addbf25ccb2315dfd912c0f517eb809b5eeda737
Apostol Nikov <apostolnikov@users.noreply.github.com>
2020-01-30 16:09:43 +0200

    feat: lead asset image optimization (#2506)
    * feat: lead asset image optimization
    
    * fix: snaps
    
    * fix: lint
    
    * chore: apply changes from review
    
    Co-authored-by: Valentin Georgiev <valentin.al.georgiev@gmail.com>

--- packages/utils/src/index.web.js
+++ packages/utils/src/index.web.js
@@ -13,3 +13,4 @@ export { default as gqlRgbaToHex } from "./gql-rgba-to-hex";
 export { default as gqlRgbaToStyle } from "./gql-rgba-to-style";
 export { default as HoverIcon } from "./hover-icon";
 export { default as ServerClientRender } from "./server-client-render";
+export { default as appendToImageURL } from "./append-to-image-url";
