36b7429bacb5943de7eccb22873d5c955e16d504
Apostol Nikov <apostol.nikov@news.co.uk>
2020-02-07 12:30:18 +0200

    fix: missing import in web index

--- packages/utils/src/index.web.js
+++ packages/utils/src/index.web.js
@@ -15,3 +15,4 @@ export { default as gqlRgbaToStyle } from "./gql-rgba-to-style";
 export { default as HoverIcon } from "./hover-icon";
 export { default as ServerClientRender } from "./server-client-render";
 export { default as appendToImageURL } from "./append-to-image-url";
+export { default as getSectionFromTiles } from "./get-section-from-tiles";
