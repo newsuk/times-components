f1b67a5d1c78520129055d78da278b38fab33d0c
Themis Chatzis <46359871+themischatzis@users.noreply.github.com>
2020-03-10 18:40:35 +0200

    feat: Update styles with variables

--- packages/article-skeleton/src/styles/inline-newsletter-puff.js
+++ packages/article-skeleton/src/styles/inline-newsletter-puff.js
@@ -6,7 +6,7 @@ import {
     fontSizes,
     spacing
   } from "@times-components/styleguide";
-  import {View, Text} from 'react-native';
+import {View, Text} from 'react-native';
 
 export const InpContainer = styled(View)`
     background-color: ${colours.functional.newsletterPuffBackground};
@@ -18,7 +18,7 @@ export const InpContainer = styled(View)`
 
    @media (min-width: ${breakpoints.medium}px) {
     flex-direction: row;
-    margin: 0 auto;
+    margin: 0 auto ${spacing(4)};
     width: 80.8%;
   }
   @media (min-width: ${breakpoints.wide}px) {
@@ -30,50 +30,51 @@ export const InpImageContainer = styled(View)`
     width: 45%;
   } 
 `
+
 export const InpTextEditor = styled(View)`
-justify-content: center;
-    padding: ${spacing(4)};
+    justify-content: center;
+    padding-top: ${spacing(4)};
+    padding-right: ${spacing(4)};
+    padding-bottom: ${spacing(4)};
+    padding-left: ${spacing(4)};
    @media (min-width: ${breakpoints.small}px) {
       padding-right: ${spacing(0)};
       padding-left: ${spacing(0)};
       flex: 1;
   } 
 `
+
 export const InpLabel = styled(Text)`
     font-family: ${fonts.supporting};
-    font-size: 12px;
+    font-size: ${fontSizes.newsletterPuffLabel}px;
     letter-spacing: 1px;
     color: ${colours.functional.brandColour};
     text-align: center;
     text-transform: uppercase;
-    margin-bottom: 3px;
-    @media (min-width: ${breakpoints.medium}px) {
-      margin-bottom: 6px;
-  } 
+    margin-bottom: ${spacing(1)};
 `
+
 export const InpHeadline = styled(Text)`
     color: ${colours.functional.brandColour};
     font-family: ${fonts.headline};
     text-align: center;
-    font-size: 28px;
+    font-size: ${fontSizes.newsletterPuffHeadline}px;
     text-decoration: none;
     margin-bottom: ${spacing(1)};
-    @media (max-width: ${breakpoints.medium}px) {
-  } 
 `
-export const InpDetails = styled(Text)`
+
+export const InpCopy = styled(Text)`
     font-family: ${fonts.body};
-    font-size: 15px;
+    font-size: ${fontSizes.newsletterPuffCopy}px;
     text-align: center;
     color: ${colours.functional.primary};
     margin-bottom: ${spacing(3)};
-    @media (max-width: ${breakpoints.medium}px) {
-  } 
 `
+
 export const SignupContainer = styled(View)`
    @media (min-width: ${breakpoints.medium}px) {
     width: 220px;
-    margin: ${spacing(0)} auto;
+    margin: 0px auto;
   } 
 `
 
@@ -88,9 +89,8 @@ justify-content: center;
 align-items: center;
 `
 
-
 export const SignupText = styled(Text)`
-font-size: 15px;
+font-size: ${fontSizes.newsletterPuffCopy}px;
 font-family: ${fonts.supporting};
 `
 
@@ -101,10 +101,9 @@ justify-content: center;
 
 export const PreferencesText = styled(Text)`
 color: ${colours.functional.action};
-cursor: pointer;
 `
 
 export const IconContainer = styled(View)`
 padding-left: 5px;
-padding-top: 4px;
+padding-top: 6px;
 `
