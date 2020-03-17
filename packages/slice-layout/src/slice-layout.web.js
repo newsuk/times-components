3a6fcb1df4a2ab726e4ccfd5e552db035bdba92a
Georgi Mirchev <georgi.mirchev@news.co.uk>
2020-03-17 18:19:59 +0200

    chore: delete slice-layout

--- packages/slice-layout/src/slice-layout.web.js
+++
@@ -1,5 +0,0 @@
-import StandardSlice from "./templates/standard";
-import LeadOneAndTwoSlice from "./templates/leadoneandtwo";
-import OpinionOneAndTwoSlice from "./templates/opiniononeandtwo";
-
-export { StandardSlice, LeadOneAndTwoSlice, OpinionOneAndTwoSlice };
