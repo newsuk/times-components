02050e56753db0bbdeb53eb9d6dddb736f3745ff
Georgi Mirchev <georgi.mirchev@news.co.uk>
2020-03-18 14:52:13 +0200

    feat: remove not called web test functions

--- packages/slice-layout/__tests__/list2and6nopic.web.js
+++
@@ -1,5 +0,0 @@
-import { mount } from "enzyme";
-import "./serializers.web";
-import shared from "./list2and6nopic.base";
-
-export default () => shared(mount);
--- packages/slice-layout/__tests__/sd4-with-style.web.js
+++
@@ -1,5 +0,0 @@
-import { mount } from "enzyme";
-import "./serializers-with-style.web";
-import shared from "./sd4.base";
-
-export default () => shared(mount);
