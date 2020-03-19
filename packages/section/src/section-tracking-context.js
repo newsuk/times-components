17f476ac74b0e84708ec4b8aacecaadafb927dfd
Valentin Georgiev <valentin.georgiev@news.co.uk>
2020-03-19 11:12:50 +0200

    fix: publication name in section pages (#2571)

--- packages/section/src/section-tracking-context.js
+++ packages/section/src/section-tracking-context.js
@@ -4,7 +4,7 @@ import { withTrackingContext } from "@times-components/tracking";
 
 export default Component =>
   withTrackingContext(Component, {
-    getAttrs: ({ section }) => {
+    getAttrs: ({ section, publicationName }) => {
       const { slices } = section;
       const firstSlice = slices[0];
       const nonName = Object.keys(firstSlice).filter(n => n !== "name")[0];
@@ -21,7 +21,7 @@ export default Component =>
         edition_type: editionType,
         past_edition_date:
           editionType === "past 6 days" ? get(data, "publishedTime", "") : null,
-        parent_site: get(data, "publicationName", "")
+        parent_site: publicationName || ""
       };
     },
     trackingObjectName: "Section"
