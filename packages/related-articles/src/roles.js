a670ff3380a51520ec109373969d31700d82fc1a
Georgi Mirchev <georgi.mirchev@news.co.uk>
2020-04-27 18:11:56 +0300

    feat: remove unused cases from related articles

--- packages/related-articles/src/roles.js
+++ packages/related-articles/src/roles.js
@@ -1,15 +1,3 @@
-const leadOneAndTwoRoles = [
-  "lead-one-and-two",
-  "lead-one-and-two-supporting-1",
-  "lead-one-and-two-supporting-2"
-];
-
 const standardRoles = ["standard-1", "standard-2", "standard-3"];
 
-const opinionOneAndTwoRoles = [
-  "opinion-one-and-two",
-  "opinion-one-and-two-supporting-1",
-  "opinion-one-and-two-supporting-2"
-];
-
-export { leadOneAndTwoRoles, opinionOneAndTwoRoles, standardRoles };
+export default standardRoles;
