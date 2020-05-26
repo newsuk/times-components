e70eab54f0cd2585a5b513d11397d8e0b2e84f4d
Georgi Mirchev <georgi.mirchev@news.co.uk>
2020-05-26 16:35:31 +0300

    fix: TNLT-1826 support bold/italic links for native (#2604)

---
+++ packages/article-skeleton/fixtures/boldItalic-text-article-content.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    name: "paragraph",
+    children: [
+      {
+        name: "bold",
+        children: [
+          {
+            name: "italic",
+            children: [
+              {
+                name: "text",
+                children: [],
+                attributes: {
+                  value: "Conversations with Friends"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
