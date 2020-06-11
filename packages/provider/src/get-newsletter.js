6aba662678052345bc3b05fa2c612410b5f7472d
Themis Chatzis <46359871+themischatzis@users.noreply.github.com>
2020-06-11 17:50:27 +0100

    feat: Add GetNewsletter provider

---
+++ packages/provider/src/get-newsletter.js
@@ -0,0 +1,4 @@
+import { getNewsletter } from "@times-components/provider-queries";
+import connectGraphql from "./connect";
+
+export default connectGraphql(getNewsletter);
--- packages/provider/src/provider.js
+++ packages/provider/src/provider.js
@@ -4,6 +4,7 @@ export AuthorArticlesNoImagesProvider from "./author-articles-no-images";
 export AuthorArticlesWithImagesProvider from "./author-articles-with-images";
 export ArticleBookmarked from "./article-bookmarked";
 export ArticleProvider from "./article";
+export GetNewsletter from "./get-newsletter";
 export ArticleExtrasProvider from "./article-extras";
 export EditionProvider from "./edition";
 export TopicProvider from "./topic";
