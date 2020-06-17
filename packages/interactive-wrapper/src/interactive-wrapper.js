7542fbc9f97d4504b4dcd4c367aadc208e443349
Apostol Nikov <apostolnikov@users.noreply.github.com>
2020-06-17 18:29:17 +0300

    Revert "fix: error boundaries interactives error (#2615)"
    This reverts commit 9299e83a5579e91ae0df983f0abd8229d8099285.

--- packages/interactive-wrapper/src/interactive-wrapper.js
+++ packages/interactive-wrapper/src/interactive-wrapper.js
@@ -52,9 +52,7 @@ class InteractiveWrapper extends Component {
   }
 
   onLoadEnd() {
-    if (this.webview) {
-      this.webview.postMessage("thetimes.co.uk", "*");
-    }
+    this.webview.postMessage("thetimes.co.uk", "*");
   }
 
   // eslint-disable-next-line class-methods-use-this
