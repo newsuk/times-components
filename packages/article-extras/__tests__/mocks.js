e9678ed21c4ccb09d94581c18b760d53ad2571dd
Emmanouil Konstantinidis <hello@manos.im>
2020-06-25 13:18:31 +0100

    Remove 'deleted' internal (web-related) dependencies

--- packages/article-extras/__tests__/mocks.js
+++ packages/article-extras/__tests__/mocks.js
@@ -1,9 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
-import { mockUserState } from "@times-components-native/user-state";
-
-export const UserState = mockUserState();
-
 jest.mock("@times-components-native/article-comments", () => "ArticleComments");
 jest.mock("@times-components-native/article-topics", () => "ArticleTopics");
 jest.mock("@times-components-native/related-articles", () => "RelatedArticles");
--- packages/storybook/src/showcase-to-storybook.js
+++ packages/storybook/src/showcase-to-storybook.js
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import PropTypes from "prop-types";
 import { Platform } from "react-native";
-import { addUserStateKnobs } from "@times-components-native/user-state";
 
 React.Fragment = ({ children }) => children;
 React.Fragment.propTypes = {
@@ -32,10 +31,6 @@ const addStories = (
     const args = [knobs, actions];
 
     builder.add(child.name, () => {
-      if (Platform.OS === "web") {
-        addUserStateKnobs(child.defaultUserState);
-      }
-
       return strictMode ? (
         <StrictWrapper>{child.component(...args)}</StrictWrapper>
       ) : (
