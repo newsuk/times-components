2acd0e99da0553f0954d7b9fcca60cc9ff980b66
Chris Jordan <chris.jordan@news.co.uk>
2020-07-01 11:01:20 +0100

    fix: register fonts for styorybook native

--- storybook-native/index.js
+++ storybook-native/index.js
@@ -1,6 +1,15 @@
 import "core-js";
 import "regenerator-runtime/runtime";
 import { AppRegistry } from "react-native";
+import { FontStorage } from "@times-components/typeset";
 import StorybookUIRoot from "./storybook";
+import ttf from "../fonts";
+
+Object.keys(ttf).forEach(fontName => {
+  FontStorage.registerFont(fontName, ttf[fontName]);
+});
+
+// Suppress all warnings (WIP solution until issues are correctly addressed)
+// YellowBox.ignoreWarnings([""]);
 
 AppRegistry.registerComponent("storybooknative", () => StorybookUIRoot);
