c468d11aff6701407673894762242f665d65c896
nikkibyrne <62644474+nikkibyrne@users.noreply.github.com>
2020-11-02 16:56:27 +0000

    feat(scb-525): implement native ad slots replacing nativo on article pages (#2650)
    * SCB-525: add native ad div for article pages replacing to replace nativo ads
    
    * update code to be on the 7th paywall paragraph
    
    * update PR comments
    
    * SCB-525 - updated PR comments
    
    * fix: add check back in
    
    * SCB-525 - fix additional tests and add hidden class
    
    * SCB-525 - tweak styling for the ad styling for only mobile
    
    * fix lint errors
    
    * take unused variable out for lint erroring
    
    * SCB-525 - update logic for paragraph count
    
    * SCB-5225 - fix conflicts
    
    * feat: update snapshots
    
    * SCB-525 - fix redix error
    
    * fix lint error
    
    Co-authored-by: Mike Field-May <michael.field-may@news.co.uk>

--- packages/article-skeleton/src/native-ad.web.js
+++ packages/article-skeleton/src/native-ad.web.js
@@ -1,16 +1,20 @@
 const insertNativeAd = children => {
   const clonedChildren = [...children];
   const child = clonedChildren.find(item => item.name === "paywall");
+  const paragraph = clonedChildren.filter(x => x.name === "paragraph");
 
   if (!child) {
     return clonedChildren;
   }
 
+  const paragraphCount = Number(9 - paragraph.length);
   const paywallChildren = child.children;
   const paragraphItems = paywallChildren
     .map((item, index) => ({ ...item, index }))
     .filter(item => item.name === "paragraph");
-  const indexToAdd = paragraphItems[6] ? paragraphItems[6].index : null;
+  const indexToAdd = paragraphItems[paragraphCount]
+    ? paragraphItems[paragraphCount].index
+    : null;
   // checks if nativeAd only renders once
   const nativeAd = paywallChildren.find(item => item.name === "nativeAd");
 
@@ -19,7 +23,7 @@ const insertNativeAd = children => {
   }
 
   if (indexToAdd && indexToAdd !== null) {
-    paywallChildren.splice(indexToAdd + 1, 0, {
+    paywallChildren.splice(indexToAdd, 0, {
       name: "nativeAd",
       children: []
     });
