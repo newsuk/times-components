2e0f94c8b140e4a5517245501ae97fb1ff1e3d0f
Kufour <akufour@mc105621.mynet>
2020-11-06 13:15:50 +0000

    pushing tracking and test changes

--- packages/article-comments/src/comments.web.js
+++ packages/article-comments/src/comments.web.js
@@ -2,13 +2,9 @@
 
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { TextLink } from "@times-components/link";
 import {
   CommentContainer,
-  CommentEnabledGuidelines
 } from "./styles/responsive";
-import executeSSOtransaction from "./comment-login";
-import styles from "./styles";
 import withTrackEvents from "./tracking/with-track-events";
 
 class Comments extends Component {
@@ -31,10 +27,19 @@ class Comments extends Component {
   initialiseComments() {
     const {
       articleId,
-      isReadOnly,
       spotAccountId,
       onCommentStart,
-      onCommentPost
+      onCommentPost,
+      onCommentNotification,
+      onCommentFilterNewest,
+      onCommentFilterMostRecommended,
+      onCommentFilterOldest,
+      onCommentReplyClick,
+      onCommentSettingsClick,
+      onCommentShareLink,
+      onCommentShareTwitter,
+      onCommentShareEmail,
+      onCommentShareFacebook
     } = this.props;
 
     if (!this.container || !articleId || !spotAccountId) {
@@ -68,24 +73,50 @@ class Comments extends Component {
       "spot-im-current-user-sent-message",
       onCommentPost
     );
-
-    if (!isReadOnly) {
-      if (window.SPOTIM && window.SPOTIM.startSSO) {
-        executeSSOtransaction(() => {
-          this.setState({
-            showLabel: true
-          });
-        });
-      } else {
-        document.addEventListener("spot-im-api-ready", () =>
-          executeSSOtransaction(() => {
-            this.setState({
-              showLabel: true
-            });
-          })
-        );
+    document.addEventListener(
+      "spot-im-notification-drop-down-link",
+      onCommentNotification
+    );
+    document.addEventListener(
+      "spot-im-sort-by-select",
+      function(event) {
+        switch (event.detail.sortedBy) {
+          case 'best':
+            onCommentFilterMostRecommended;
+          case 'oldest':
+            onCommentFilterOldest;
+          case 'newest':
+            onCommentFilterNewest
+          default:
+            break;
+        }
       }
-    }
+    );
+    document.addEventListener(
+      "spot-im-user-clicked-reply",
+      onCommentReplyClick
+    );
+    document.addEventListener(
+      "spot-im-clicked-settings",
+      onCommentSettingsClick
+    );
+    document.addEventListener(
+      "spot-im-share-type",
+      function(event) {
+        switch(event.detail.type) {
+          case 'link':
+            onCommentShareLink;
+          case 'email':
+            onCommentShareEmail;
+          case 'twitter':
+            onCommentShareTwitter;
+          case 'facebook':
+            onCommentShareFacebook;
+          default:
+            break;
+        }
+      }
+    );
   }
 
   disposeComments() {
@@ -95,34 +126,44 @@ class Comments extends Component {
   }
 
   render() {
-    const { showLabel } = this.state;
-    const { onCommentStart, onCommentPost } = this.props;
+    const { 
+      onCommentStart, 
+      onCommentPost, 
+      onCommentNotification,
+      onCommentFilterNewest,
+      onCommentFilterMostRecommended,
+      onCommentFilterOldest,
+      onCommentReplyClick,
+      onCommentSettingsClick,
+      onCommentShareLink,
+      onCommentShareTwitter,
+      onCommentShareEmail,
+      onCommentShareFacebook
+    } = this.props;
 
     return (
       <CommentContainer
         id="comments-container"
         onCommentStart={onCommentStart}
         onCommentPost={onCommentPost}
+        onCommentNotification={onCommentNotification}
+        onCommentNotification={onCommentNotification}
+        onCommentReplyClick={onCommentReplyClick}
+        onCommentSettingsClick={onCommentSettingsClick}
+        onCommentShare={onCommentShare}
+        onCommentFilterNewest={onCommentFilterNewest}
+        onCommentFilterMostRecommended={onCommentFilterMostRecommended}
+        onCommentFilterOldest={onCommentFilterOldest}
+        onCommentShareLink={onCommentShareLink}
+        onCommentShareTwitter={onCommentShareTwitter}
+        onCommentShareEmail={onCommentShareEmail}
+        onCommentShareFacebook={onCommentShareFacebook}
       >
-        {showLabel ? (
-          <CommentEnabledGuidelines>
-            Comments are subject to our community guidelines, which can be
-            viewed{" "}
-            <TextLink
-              style={styles.link}
-              url="https://www.thetimes.co.uk/article/f4024fbe-d989-11e6-9063-500e6740fc32"
-            >
-              here
-            </TextLink>
-            .
-          </CommentEnabledGuidelines>
-        ) : null}
-
         <div
           ref={el => {
             this.container = el;
           }}
-        />
+        /> 
       </CommentContainer>
     );
   }
@@ -133,13 +174,35 @@ Comments.propTypes = {
   isReadOnly: PropTypes.bool.isRequired,
   spotAccountId: PropTypes.string.isRequired,
   onCommentStart: PropTypes.func,
-  onCommentPost: PropTypes.func
+  onCommentPost: PropTypes.func,
+  onCommentNotification: PropTypes.func,
+  onCommentFilterNewest: PropTypes.func,
+  onCommentFilterMostRecommended: PropTypes.func,
+  onCommentFilterOldest: PropTypes.func,
+  onCommentReplyClick: PropTypes.func,
+  onCommentSettingsClick: PropTypes.func,
+  onCommentShare: PropTypes.func,
+  onCommentShareLink: PropTypes.func,
+  onCommentShareTwitter: PropTypes.func,
+  onCommentShareEmail: PropTypes.func,
+  onCommentShareFacebook: PropTypes.func
 };
 
 // onCommentStart and onCommentPost are added as props in order to allow this events to be tracked by analytics.
 Comments.defaultProps = {
   onCommentStart: () => {},
-  onCommentPost: () => {}
+  onCommentPost: () => {},
+  onCommentNotification: () => {},
+  onCommentReplyClick: () => {},
+  onCommentSettingsClick: () => {},
+  onCommentShare: () => {},
+  onCommentFilterNewest: () => {},
+  onCommentFilterMostRecommended: () => {},
+  onCommentFilterOldest: () => {},
+  onCommentShareLink: () => {},
+  onCommentShareTwitter: () => {},
+  onCommentShareEmail: () => {},
+  onCommentShareFacebook: () => {}
 };
 
 export default withTrackEvents(Comments);
