53bfe4acc3f7176245b933db4ecde9957a96ac98
Mike Field-May <michael.field-may@news.co.uk>
2021-03-23 17:33:42 +0000

    fix(tnlt-000): slight tweaks to ad styles (#2670)
    * fix: slight tweaks to ad styles
    
    * fix: spelling error
    
    * feat: update snapshots

--- packages/article-skeleton/src/styles/responsive.web.js
+++ packages/article-skeleton/src/styles/responsive.web.js
@@ -8,6 +8,7 @@ export const getHeaderAdStyles = template => {
 
   return styled(View)`
     box-sizing: content-box;
+    justify-content: center;
     min-height: 50px;
     display: flex;
     border-top-color: ${colours.functional.keyline};
