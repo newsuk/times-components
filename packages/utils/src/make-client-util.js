5fa62c8f78dbcb03261dfee6024015c940e87187
Jack Stevens <jack@applyr.com>
2021-04-15 15:04:44 +0100

    feat: make persisted queries optional for "utils" package

--- packages/utils/src/make-client-util.js
+++ packages/utils/src/make-client-util.js
@@ -9,11 +9,13 @@ import { ApolloClient } from "apollo-client";
 
 const makeClient = () => {
   let graphqlapi = null;
+  let usePersistedQueries = null;
   let acsTnlCookie = null;
   let sacsTnlCookie = null;
 
   if (typeof window !== "undefined" && window.nuk) {
     graphqlapi = window.nuk.graphqlapi.url;
+    usePersistedQueries = !!window.nuk.graphqlapi.usePersistedQueries;
     acsTnlCookie = window.nuk.getCookieValue("acs_tnl");
     sacsTnlCookie = window.nuk.getCookieValue("sacs_tnl");
   }
@@ -24,9 +26,9 @@ const makeClient = () => {
   networkInterfaceOptions.headers.Authorization = `Cookie acs_tnl=${acsTnlCookie};sacs_tnl=${sacsTnlCookie}`;
 
   const link = ApolloLink.from([
-    createPersistedQueryLink({ useGETForHashedQueries: true }),
+    usePersistedQueries && createPersistedQueryLink({ useGETForHashedQueries: true }),
     createHttpLink(networkInterfaceOptions)
-  ]);
+  ].filter(Boolean));
 
   return new ApolloClient({
     cache: new InMemoryCache({ fragmentMatcher }),
