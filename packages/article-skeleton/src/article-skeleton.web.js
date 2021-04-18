b82508477a899837dfe4d2afc11ee18149ea06e4
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2021-04-18 15:01:08 +0100

    fix: remove auto newsletter puff

--- packages/article-skeleton/src/article-skeleton.web.js
+++ packages/article-skeleton/src/article-skeleton.web.js
@@ -16,7 +16,7 @@ import {
 import articleTrackingContext from "./tracking/article-tracking-context";
 import insertDropcapIntoAST from "./dropcap-util";
 import insertNativeAd from "./native-ad.web";
-import insertNewsletterPuff from "./newsletter-puff.web";
+// import insertNewsletterPuff from "./newsletter-puff.web";
 import tagLastParagraph from "./tracking/article-tracking-last-paragraph";
 
 import {
@@ -60,15 +60,15 @@ const ArticleSkeleton = ({
     sharingEnabled
   } = article;
 
-  const insertNewsletterPuffCurry = newContent =>
-    insertNewsletterPuff(section, newContent, isPreview);
+  /* const insertNewsletterPuffCurry = newContent =>
+    insertNewsletterPuff(section, newContent, isPreview); */
 
   const insertDropcapIntoASTCurry = newContent =>
     insertDropcapIntoAST(newContent, template, dropcapsDisabled);
 
   const contentReducers = [
     insertDropcapIntoASTCurry,
-    insertNewsletterPuffCurry,
+    // insertNewsletterPuffCurry,
     insertNativeAd,
     tagLastParagraph
   ];
