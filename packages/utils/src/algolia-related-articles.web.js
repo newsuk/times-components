229eb62aba24fea19662c2071270e3afaabe4dc2
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2021-04-23 17:54:22 +0100

    remove console log

--- packages/utils/src/algolia-related-articles.web.js
+++ packages/utils/src/algolia-related-articles.web.js
@@ -59,11 +59,10 @@ const formatQuery = article => {
 const searchRelatedArticles = async (index, article) => {
   if (index) {
     const query = formatQuery(article);
-    console.log("AlgoliaSearch query", query); // eslint-disable-line no-console
-
     const search = await index.search(query, { hitsPerPage: 5 });
 
     return {
+      query,
       sliceName: "StandardSlice",
       items: search.hits
         .filter(({ id }) => id !== article.id)
