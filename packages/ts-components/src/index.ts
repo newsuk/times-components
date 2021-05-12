e87e5a641a5a7d1402fd0d7f7678410c808c1406
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2021-05-12 13:11:41 +0100

    feat(TE-43): code structure updates

--- packages/ts-components/src/index.ts
+++ packages/ts-components/src/index.ts
@@ -1,4 +1,5 @@
 export { InArticlePuff } from './components/in-article-puff/InArticlePuff';
+
 export {
   ViewCountWrapper
-} from './components/view-count-wrapper/ViewCountWrapper';
+} from './helpers/view-count-wrapper/ViewCountWrapper';
