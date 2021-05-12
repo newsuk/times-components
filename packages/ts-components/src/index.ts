ff66f672a13b2ff6c3014bbc880e78e025e1358c
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2021-05-12 13:09:04 +0100

    feat(TE-43): InArticlePuff data

--- packages/ts-components/src/index.ts
+++ packages/ts-components/src/index.ts
@@ -1,4 +1,4 @@
 export { InArticlePuff } from './components/in-article-puff/InArticlePuff';
 export {
   ViewCountWrapper
-} from './components/view-count-wrapper/ViewCountWrapper';
+} from './helpers/view-count-wrapper/ViewCountWrapper';
