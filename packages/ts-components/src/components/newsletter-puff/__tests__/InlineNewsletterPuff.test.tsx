767e92a65bcd5e5583abbabc5fb57b5e8afddd21
Vinh Lam <vinh.lam@gmail.com>
2021-05-25 17:45:17 +0100

    added analytics tracking tests

--- packages/ts-components/src/components/newsletter-puff/__tests__/InlineNewsletterPuff.test.tsx
+++ packages/ts-components/src/components/newsletter-puff/__tests__/InlineNewsletterPuff.test.tsx
@@ -12,10 +12,13 @@ import {
   subscribeNewsletter
 } from '@times-components/provider-queries';
 import { InlineNewsletterPuff } from '../InlineNewsletterPuff';
+import { TrackingContextProvider } from '../../../helpers/tracking/TrackingContextProvider';
 
-const mockAnalyticsStream = jest.fn();
+import FakeIntersectionObserver from '../../../test-utils/FakeIntersectionObserver';
+
 const renderComponent = (
-  mocks = [
+  analyticsStream?: () => void,
+  mocks: any[] = [
     {
       request: {
         query: getNewsletter,
@@ -33,25 +36,44 @@ const renderComponent = (
           }
         }
       }
+    },
+    {
+      delay: 50,
+      request: {
+        query: subscribeNewsletter,
+        variables: {
+          code: 'TNL-119'
+        }
+      },
+      result: {
+        data: {
+          subscribeNewsletter: {
+            id: 'a2l6E000000CdHzQAK',
+            isSubscribed: true,
+            title: 'RED BOX',
+            __typename: 'Newsletter'
+          }
+        }
+      }
     }
-  ] as any,
-  addtionalProps: any = {}
+  ]
 ) =>
   render(
-    <MockedProvider mocks={mocks} {...addtionalProps}>
-      <InlineNewsletterPuff
-        {...{
-          analyticsStream: mockAnalyticsStream,
-          code: 'TNL-119',
+    <MockedProvider mocks={mocks}>
+      <TrackingContextProvider analyticsStream={analyticsStream}>
+        <InlineNewsletterPuff
+          {...{
+            code: 'TNL-119',
 
-          label: 'STRAIGHT IN YOUR INBOX',
-          headline: 'Politics. Explained.',
-          copy:
-            'Sign up to receive our brilliant Red Box newsletter, Matt Chorley`s poke at politics delivered every weekday morning at 8am.',
-          imageUri:
-            'https://nuk-tnl-deck-prod-static.s3-eu-west-1.amazonaws.com/uploads/2aa9050e6c3d4de682f11a4802ebba96.jpg'
-        }}
-      />
+            label: 'STRAIGHT IN YOUR INBOX',
+            headline: 'Politics. Explained.',
+            copy:
+              'Sign up to receive our brilliant Red Box newsletter, Matt Chorley`s poke at politics delivered every weekday morning at 8am.',
+            imageUri:
+              'https://nuk-tnl-deck-prod-static.s3-eu-west-1.amazonaws.com/uploads/2aa9050e6c3d4de682f11a4802ebba96.jpg'
+          }}
+        />
+      </TrackingContextProvider>
     </MockedProvider>
   );
 
@@ -70,7 +92,7 @@ describe('Inline Newsletter Puff', () => {
   });
 
   it('renders null when is already subscribed', async () => {
-    const component = renderComponent([
+    const component = renderComponent(jest.fn(), [
       {
         request: {
           query: getNewsletter,
@@ -103,106 +125,118 @@ describe('Inline Newsletter Puff', () => {
     expect(component.baseElement).toMatchSnapshot();
   });
 
-  it("renders 'saving' when the button is clicked", async () => {
-    const component = renderComponent([
-      {
-        request: {
-          query: getNewsletter,
-          variables: {
-            code: 'TNL-119'
-          }
-        },
-        result: {
-          data: {
-            newsletter: {
-              id: 'a2l6E000000CdHzQAK',
-              isSubscribed: false,
-              title: 'RED BOX',
-              __typename: 'Newsletter'
-            }
-          }
-        }
-      },
-      {
-        delay: 1000,
-        request: {
-          query: subscribeNewsletter,
-          variables: {
-            code: 'TNL-119'
-          }
-        },
-        result: {
-          data: {
-            subscribeNewsletter: {
-              id: 'a2l6E000000CdHzQAK',
-              isSubscribed: true,
-              title: 'RED BOX',
-              __typename: 'Newsletter'
-            }
-          }
-        }
-      }
-    ]);
+  describe('Clicking Subscribe', () => {
+    it("renders 'saving' when the button is clicked", async () => {
+      const component = renderComponent();
 
-    await delay(0);
+      const signupButton = await component.findByText('Sign up now');
+      fireEvent.click(signupButton);
 
-    fireEvent.click(component.baseElement.querySelector('button')!);
+      expect(await component.findByText('Saving…')).toBeTruthy();
+      expect(component.baseElement).toMatchSnapshot();
+    });
 
-    expect(await component.findByText('Saving…')).toBeTruthy();
-    expect(component.baseElement).toMatchSnapshot();
+    it('triggers analytics when subscribed', async () => {
+      const analyticsStream = jest.fn();
+      const component = renderComponent(analyticsStream);
+
+      const signupButton = await component.findByText('Sign up now');
+      fireEvent.click(signupButton);
+
+      await component.findByText('Saving…');
+
+      expect(analyticsStream).toHaveBeenCalledWith({
+        article_parent_name: 'RED BOX',
+        event_navigation_action: 'navigation',
+        event_navigation_browsing_method: 'click',
+        event_navigation_name: 'widget : puff : sign up now'
+      });
+    });
   });
 
-  it('renders the success view after subscribing ', async () => {
-    const component = renderComponent([
-      {
-        request: {
-          query: getNewsletter,
-          variables: {
-            code: 'TNL-119'
-          }
-        },
-        result: {
-          data: {
-            newsletter: {
-              id: 'a2l6E000000CdHzQAK',
-              isSubscribed: false,
-              title: 'RED BOX',
-              __typename: 'Newsletter'
-            }
-          }
-        }
-      },
-      {
-        delay: 50,
-        request: {
-          query: subscribeNewsletter,
-          variables: {
-            code: 'TNL-119'
-          }
-        },
-        result: {
-          data: {
-            subscribeNewsletter: {
-              id: 'a2l6E000000CdHzQAK',
-              isSubscribed: true,
-              title: 'RED BOX',
-              __typename: 'Newsletter'
-            }
-          }
-        }
-      }
-    ]);
+  describe('Manage preferences ', () => {
+    it('renders the success view after subscribing ', async () => {
+      const component = renderComponent();
 
-    await delay(0);
+      const signupButton = await component.findByText('Sign up now');
+      fireEvent.click(signupButton);
 
-    fireEvent.click(await component.queryByRole('button')!);
+      expect(
+        await component.findByText('Manage preferences here')
+      ).toBeTruthy();
+      expect(component.baseElement).toMatchSnapshot();
+    });
 
-    // await waitFor(() => {
-    //   expect(
-    //     component.queryByText('Manage preferences here')
-    //   ).toBeInTheDocument();
-    // });
-    expect(await component.findByText('Manage preferences here')).toBeTruthy();
-    expect(component.baseElement).toMatchSnapshot();
+    it('triggers analytics event when manage preferences clicked', async () => {
+      const analyticsStream = jest.fn();
+      const component = renderComponent(analyticsStream);
+
+      const signupButton = await component.findByText('Sign up now');
+      fireEvent.click(signupButton);
+
+      const link = await component.findByText('Manage preferences here');
+
+      analyticsStream.mockClear();
+
+      fireEvent.click(link);
+      expect(analyticsStream).toHaveBeenCalledWith({
+        article_parent_name: 'RED BOX',
+        event_navigation_action: 'navigation',
+        event_navigation_browsing_method: 'click',
+        event_navigation_name: 'widget : puff : manage preferences here'
+      });
+    });
+  });
+
+  describe('intersectionObserverTests', () => {
+    let oldIntersectionObserver: typeof IntersectionObserver;
+    beforeEach(() => {
+      oldIntersectionObserver = window.IntersectionObserver;
+      // @ts-ignore
+      window.IntersectionObserver = FakeIntersectionObserver;
+    });
+
+    afterEach(() => {
+      window.IntersectionObserver = oldIntersectionObserver;
+    });
+
+    it('sign up now : displayed', async () => {
+      const analyticsStream = jest.fn();
+      renderComponent(analyticsStream);
+
+      await delay(10);
+
+      FakeIntersectionObserver.intersect();
+
+      expect(analyticsStream).toHaveBeenCalledWith({
+        article_parent_name: 'RED BOX',
+        event_navigation_action: 'navigation',
+        event_navigation_browsing_method: 'automated',
+        event_navigation_name: 'widget : puff : sign up now : displayed'
+      });
+    });
+    it('manage preferences here : displayed', async () => {
+      const analyticsStream = jest.fn();
+      const component = renderComponent(analyticsStream);
+
+      const signupButton = await component.findByText('Sign up now');
+      fireEvent.click(signupButton);
+
+      await component.findByText('Manage preferences here');
+
+      analyticsStream.mockClear();
+
+      FakeIntersectionObserver.intersect();
+
+      await delay(10);
+
+      expect(analyticsStream).toHaveBeenCalledWith({
+        article_parent_name: 'RED BOX',
+        event_navigation_action: 'navigation',
+        event_navigation_browsing_method: 'automated',
+        event_navigation_name:
+          'widget : puff : manage preferences here : displayed'
+      });
+    });
   });
 });
