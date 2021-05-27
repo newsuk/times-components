99cbec1b0ceb71439cd22b755b1feebfc58587b0
Bibi Collins <bibivictoria.collins@news.co.uk>
2021-05-27 13:14:04 +0100

    Add sanitiseHtml prop to InArticlePuff

--- packages/ts-components/src/components/in-article-puff/InArticlePuff.tsx
+++ packages/ts-components/src/components/in-article-puff/InArticlePuff.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import sanitizeHtml from 'sanitize-html';
 
 import { Placeholder } from '@times-components/image';
 import { IconForwardChevron } from '@times-components/icons';
@@ -43,7 +44,8 @@ const clickEvent = (buttonLabel: string) => ({
 export const InArticlePuff: React.FC<{
   sectionColour: string;
   forceImageAspectRatio?: AspectRatios;
-}> = ({ sectionColour, forceImageAspectRatio }) => {
+  sanitiseHtml: boolean;
+}> = ({ sectionColour, forceImageAspectRatio, sanitiseHtml }) => {
   const [colour, setColour] = useState('#bf0000');
 
   const handleClick = (
@@ -75,6 +77,11 @@ export const InArticlePuff: React.FC<{
     link,
     linkText
   } = data.body.data[0].data;
+
+  const sanitisedCopy = sanitizeHtml(copy, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
 
   const hasImage = Boolean(image);
 
@@ -116,7 +123,7 @@ export const InArticlePuff: React.FC<{
               >
                 <Headline hasImage={hasImage}>{headline}</Headline>
               </a>
-              {copy && <Copy>{copy}</Copy>}
+              {copy && <Copy>{sanitiseHtml ? sanitisedCopy : copy}</Copy>}
             </div>
 
             <Link
