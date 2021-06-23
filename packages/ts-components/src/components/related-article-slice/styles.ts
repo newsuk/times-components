2823cc0b44562949b1e17e08d6e5f5a61983e9d0
Vin L <vinh.lam@news.co.uk>
2021-06-23 10:05:14 +0100

    feat:(TE-120) - related article slice - minor styling update (#2727)
    * updated heading styling and responsiveness

--- packages/ts-components/src/components/related-article-slice/styles.ts
+++ packages/ts-components/src/components/related-article-slice/styles.ts
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
-import { colours, fonts } from '@times-components/styleguide';
+import { breakpoints, colours, fonts } from '@times-components/styleguide';
 
 export const Container = styled.div`
   display: none;
   .heading {
     text-align: center;
     font-family: ${fonts.headline};
-    font-size: 40px;
-    line-height: 40px;
-    padding: 24px;
-    margin: 40px 0;
+    font-size: 25px;
+    line-height: 25px;
+    padding: 16px;
+    margin: 12px 0;
     border-top: 1px solid ${colours.functional.keyline};
     border-bottom: 1px solid ${colours.functional.keyline};
+
+    @media (min-width: ${breakpoints.medium}px) {
+      font-size: 28px;
+      line-height: 28px;
+    }
+
+    @media (min-width: ${breakpoints.wide}px) {
+      padding: 20px;
+      font-size: 36px;
+      line-height: 36px;
+    }
   }
 `;
