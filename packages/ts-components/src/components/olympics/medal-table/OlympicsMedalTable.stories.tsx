3312570acb22868d60bcc6d04557a1e78b58399d
Vin L <vinh.lam@news.co.uk>
2021-07-20 12:49:47 +0100

    feat:Olympics Medal Table - changed games code and allow it to be updated via knobs (#2739)

--- packages/ts-components/src/components/olympics/medal-table/OlympicsMedalTable.stories.tsx
+++ packages/ts-components/src/components/olympics/medal-table/OlympicsMedalTable.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { OlympicsMedalTable } from './OlympicsMedalTable';
 import { HelmetProvider } from 'react-helmet-async';
 import { ArticleHarness } from '../../../fixtures/article-harness/ArticleHarness';
+import { text } from '@storybook/addon-knobs';
 
 storiesOf('Typescript Component/Olympics', module)
   .addDecorator((storyFn: () => React.ReactNode) => (
@@ -13,5 +14,8 @@ storiesOf('Typescript Component/Olympics', module)
   ))
 
   .add('Medal Table', () => {
-    return <OlympicsMedalTable />;
+    const authToken = text('Auth Token', '6i3DuEwbVhr2Fht6');
+    const gamesCode = text('Games Code', 'OG2020-TR2');
+
+    return <OlympicsMedalTable authToken={authToken} gamesCode={gamesCode} />;
   });
