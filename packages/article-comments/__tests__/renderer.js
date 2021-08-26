4264ae75c919a48c86cfddcd00d30db123dd1a6c
Vinh Lam <vinh.lam@gmail.com>
2021-08-26 14:15:30 +0100

    feat(TE-178): Real Name Commenting - updated unit tests

--- packages/article-comments/__tests__/renderer.js
+++ packages/article-comments/__tests__/renderer.js
@@ -1,17 +1,30 @@
 import React from "react";
-import TestRenderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 import ArticleComments from "../src/article-comments";
 
-export default ({ count, enabled }) =>
-  TestRenderer.create(
+export default ({
+  count,
+  enabled,
+  publishedTime = "2021-08-10T16:00:00.000Z"
+}) =>
+  render(
     <ArticleComments
       articleId="dummy-article-id"
+      publishedTime={publishedTime}
       commentCount={count}
       commentsEnabled={enabled}
       isEnabled={enabled}
       onCommentGuidelinesPress={() => {}}
       onCommentsPress={() => {}}
-      commentingConfig={{ account: { current: "dummiy-spotim-id" } }}
+      commentingConfig={{
+        account: {
+          current: "CurrentSpotID",
+          readonly: "ReadOnlySpotID"
+        },
+        switchOver: "2020-08-10T16:00:00.000Z"
+      }}
       url="dummy-article-url"
     />
   );
