229e0de720e02a53ebd9cc33203cc48e9c42ed0b
nikkibyrne <62644474+nikkibyrne@users.noreply.github.com>
2021-08-27 15:09:32 +0100

    feat(scb-1193): add aditional in article ad slots, restructure code to stop rendering twice (#2781)
    * feat(SCB-1193): add extra inline ad slots and styling to hide
    
    * SCB-1193 - fix linting errors
    
    * SCB-1193 - fix linting errors
    
    * SCB-1193 - fix linting errors
    
    * feat(SCB-1193): fix lint errors
    
    * feat(scb-1193): add event listener to callapse ad if not rendered
    
    * feat(scb-1193): add code for adwrapper with height
    
    * feat(SCB-1193): revert last commit with updates
    
    * feat(SCB-1193): add hidden styling for initial ad load
    
    * feat(SCB-272): fix test failing
    
    * feat(scb-1193): update snapshot
    
    * feat(scb-1193): update snapshot
    
    * feat(scb-1193): update snapshot
    
    * feat(scb-1193): remove unecessary condition checking inline ad existing
    
    * feat(scb-1193): add inrememting to the index for ads
    
    * feat(scb-1193): restructure code to stop it rendering twice
    
    * feat(sub-1193): modify code to stop rendering twice
    
    * feat(scb-1193): lint fix
    
    * feat(scb-1193): add test to check if ad exists

--- packages/article-skeleton/src/contentModifiers/inline-ad.js
+++ packages/article-skeleton/src/contentModifiers/inline-ad.js
@@ -13,19 +13,20 @@ const insertInlineAd = children => {
     .filter(item => item.name === "paragraph");
   const paraPostition = [13, 20, 27];
 
-  paraPostition.map((item, i) => {
-    const inlineAdPos = paywallParagraphs[item - paragraph.length]
-      ? paywallChildren.splice(
-          paywallParagraphs[item - paragraph.length]?.index + i,
-          0,
-          {
-            name: `inlineAd${i + 1}`,
-            children: []
-          }
-        )
-      : null;
+  paraPostition.forEach((item, i) => {
+    const inlineAd = paywallChildren.find(ad => ad.name === `inlineAd${i + 1}`);
+    if (!inlineAd) {
+      const indexPos = paywallParagraphs[item - paragraph.length]
+        ? paywallParagraphs[item - paragraph.length].index
+        : null;
 
-    return inlineAdPos;
+      if (indexPos && indexPos !== null) {
+        paywallChildren.splice(indexPos + i, 0, {
+          name: `inlineAd${i + 1}`,
+          children: []
+        });
+      }
+    }
   });
 
   return clonedChildren;
