eece13c1638965f763ed73f58f96bea3513c59a2
Srinivasan-Ramasamy <87307441+Srinivasan-Ramasamy@users.noreply.github.com>
2021-09-14 15:04:02 +0530

    Feat/TDP-121 Inarticle Bignumbers Analytics (#2790)
    * UI Changes
    * Code Review Fix

--- packages/ts-components/src/components/in-article-big-numbers/BigNumbers.stories.tsx
+++ packages/ts-components/src/components/in-article-big-numbers/BigNumbers.stories.tsx
@@ -3,10 +3,23 @@ import { storiesOf } from '@storybook/react';
 import { ArticleHarness } from '../../fixtures/article-harness/ArticleHarness';
 import { FetchProvider } from '../../helpers/fetch/FetchProvider';
 import { BigNumbers } from './BigNumbers';
+import { TrackingContextProvider } from '../../helpers/tracking/TrackingContextProvider';
+import analyticsStream from '../../fixtures/analytics-actions/analytics-actions';
 
 storiesOf('Typescript Component/In Article/Big Numbers', module)
   .addDecorator((storyFn: () => React.ReactNode) => (
-    <ArticleHarness>{storyFn()}</ArticleHarness>
+    <TrackingContextProvider
+      context={{
+        component: 'ArticleSkeleton',
+        attrs: {
+          article_name: 'articleHeadline',
+          section_details: 'section'
+        }
+      }}
+      analyticsStream={analyticsStream}
+    >
+      <ArticleHarness>{storyFn()}</ArticleHarness>
+    </TrackingContextProvider>
   ))
   .add('Standard without a headline', () => (
     <FetchProvider url="https://gobble.timesdev.tools/deck/api/deck-post-action/44335">
--- packages/ts-components/src/components/in-article-big-numbers/BigNumbers.tsx
+++ packages/ts-components/src/components/in-article-big-numbers/BigNumbers.tsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Placeholder } from '@times-components/image';
 import { useFetch } from '../../helpers/fetch/FetchProvider';
+import {
+  TrackingContext,
+  TrackingContextProvider
+} from '../../helpers/tracking/TrackingContextProvider';
 import { sanitiseCopy } from '../../helpers/text-formatting/SanitiseCopy';
 import {
   Container,
@@ -28,6 +32,21 @@ type BigNumbersData = {
 
 type BigNumbersDeckData = DeckData<never, BigNumbersData>;
 
+const scrollEvent = {
+  attrs: {
+    event_navigation_name: 'in-article component displayed : big numbers',
+    event_navigation_browsing_method: 'scroll'
+  }
+};
+
+const clickEvent = (buttonLabel: string) => ({
+  action: 'Clicked',
+  attrs: {
+    event_navigation_name: `button : ${buttonLabel}`,
+    event_navigation_browsing_method: 'click'
+  }
+});
+
 export const BigNumbers: React.FC<{
   sectionColour: string;
 }> = ({ sectionColour }) => {
@@ -48,52 +67,83 @@ export const BigNumbers: React.FC<{
   const { headline, label, size } = data.fields;
   const bigNumbersData = data.body.data;
   const [showAll, setShowAll] = useState(false);
-  const handleShowAll = () => {
+  const handleShowAll = (
+    fireAnalyticsEvent: (evt: TrackingContext) => void,
+    buttonLabel: string
+  ) => {
+    fireAnalyticsEvent && fireAnalyticsEvent(clickEvent(buttonLabel));
     setShowAll(!showAll);
   };
 
   const showAllRef = useRef<HTMLDivElement>(null);
-  const [displayShowAll, setShowShowAll] = useState(false);
+  const [displayShowAll, setDisplayShowAll] = useState(false);
   const maxHeight = isWide(size) ? 250 : 350;
   useEffect(() => {
     const listContainer = showAllRef.current;
     if (listContainer) {
-      setShowShowAll(listContainer.clientHeight > maxHeight);
+      setDisplayShowAll(listContainer.clientHeight > maxHeight);
     }
   }, []);
 
   return (
-    <Container sectionColour={sectionColour} isWide={isWide(size)}>
-      <ContentContainer>
-        <Label sectionColour={sectionColour}>{label}</Label>
-        {headline && <Headline>{headline}</Headline>}
-        <ListContainer
-          ref={showAllRef}
-          showAll={showAll}
-          maxHeight={maxHeight}
-          displayShowAll={displayShowAll}
+    <TrackingContextProvider
+      context={{
+        object: 'InArticleBigNumbers',
+        attrs: {
+          component_type:
+            'in-article component : big numbers: ' +
+            (displayShowAll ? 'interactive' : 'static'),
+          event_navigation_action: 'navigation',
+          component_name: `${headline}`
+        }
+      }}
+      scrolledEvent={scrollEvent}
+    >
+      {({ fireAnalyticsEvent, intersectObserverRef }) => (
+        <Container
+          ref={intersectObserverRef}
+          sectionColour={sectionColour}
+          isWide={isWide(size)}
         >
-          <List>
-            {bigNumbersData.map((row: BigNumbersData, index: number) => (
-              <ListItem key={index} isStandard={isStandard(size)}>
-                <NumberContainer sectionColour={sectionColour}>
-                  {row.data.number}
-                </NumberContainer>
-                <Copy
-                  dangerouslySetInnerHTML={{
-                    __html: sanitiseCopy(row.data.copy, ['br', 'b', 'i'])
-                  }}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </ListContainer>
-      </ContentContainer>
-      <ShowAllContainer showAll={showAll} displayShowAll={displayShowAll}>
-        <ShowAllButton onClick={handleShowAll}>
-          {showAll ? 'Collapse' : 'Show all'}
-        </ShowAllButton>
-      </ShowAllContainer>
-    </Container>
+          <ContentContainer>
+            <Label sectionColour={sectionColour}>{label}</Label>
+            {headline && <Headline>{headline}</Headline>}
+            <ListContainer
+              ref={showAllRef}
+              showAll={showAll}
+              maxHeight={maxHeight}
+              displayShowAll={displayShowAll}
+            >
+              <List>
+                {bigNumbersData.map((row: BigNumbersData, index: number) => (
+                  <ListItem key={index} isStandard={isStandard(size)}>
+                    <NumberContainer sectionColour={sectionColour}>
+                      {row.data.number}
+                    </NumberContainer>
+                    <Copy
+                      dangerouslySetInnerHTML={{
+                        __html: sanitiseCopy(row.data.copy, ['br', 'b', 'i'])
+                      }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </ListContainer>
+          </ContentContainer>
+          <ShowAllContainer showAll={showAll} displayShowAll={displayShowAll}>
+            <ShowAllButton
+              onClick={() =>
+                handleShowAll(
+                  fireAnalyticsEvent,
+                  showAll ? 'Collapse' : 'Show all'
+                )
+              }
+            >
+              {showAll ? 'Collapse' : 'Show all'}
+            </ShowAllButton>
+          </ShowAllContainer>
+        </Container>
+      )}
+    </TrackingContextProvider>
   );
 };
