5e94536ab474215893215e13deebc0b86afd5d27
budala-naveen <budala.naveen@news.co.uk>
2021-09-17 11:25:05 +0530

    commiting Inarticle timelines analytical changes

--- packages/ts-components/src/components/in-article-timelines/Timelines.tsx
+++ packages/ts-components/src/components/in-article-timelines/Timelines.tsx
@@ -19,6 +19,10 @@ import {
 } from './styles';
 import { PlaceholderContainer, Label } from '../common-styles';
 import { DeckData } from '../../helpers/fetch/types';
+import {
+  TrackingContext,
+  TrackingContextProvider
+} from '../../helpers/tracking/TrackingContextProvider';
 
 type TimelinesData = {
   type: string;
@@ -28,6 +32,13 @@ type TimelinesData = {
     image: string;
     copy: string;
   };
+};
+
+const scrollEvent = {
+  attrs: {
+    event_navigation_name: 'in-article component displayed : timelines',
+    event_navigation_browsing_method: 'scroll'
+  }
 };
 
 type TimelinesDeckData = DeckData<never, TimelinesData>;
@@ -52,9 +63,22 @@ export const Timelines: React.FC<{
   const { headline, label } = data.fields;
   const timelinesData = data.body.data;
   const [showAll, setShowAll] = useState(false);
-  const handleShowAll = () => {
+
+  const handleShowAll = (
+    fireAnalyticsEvent: (evt: TrackingContext) => void,
+    buttonLabel: string
+  ) => {
+    fireAnalyticsEvent && fireAnalyticsEvent(clickEvent(buttonLabel));
     setShowAll(!showAll);
   };
+
+  const clickEvent = (buttonLabel: string) => ({
+    action: 'Clicked',
+    attrs: {
+      event_navigation_name: `button : ${buttonLabel}`,
+      event_navigation_browsing_method: 'click'
+    }
+  });
 
   const showAllRef = useRef<HTMLDivElement>(null);
   const [displayShowAll, setShowShowAll] = useState(false);
@@ -67,44 +91,67 @@ export const Timelines: React.FC<{
   }, []);
 
   return (
-    <Container sectionColour={sectionColour}>
-      <ContentContainer>
-        <Label sectionColour={sectionColour}>{label}</Label>
-        {headline && <Headline>{headline}</Headline>}
-        <ListContainer
-          ref={showAllRef}
-          showAll={showAll}
-          maxHeight={maxHeight}
-          displayShowAll={displayShowAll}
-        >
-          <List>
-            {timelinesData.map((row: TimelinesData, index: number) => (
-              <ListItem key={index}>
-                <LeftPanel
-                  sectionColour={sectionColour}
-                  circularImage={row.data.image}
-                >
-                  {row.data.image && <img src={row.data.image} />}
-                </LeftPanel>
-                <RightPanel>
-                  <Date sectionColour={sectionColour}>{row.data.date}</Date>
-                  <SubHeading>{row.data.eventHeading}</SubHeading>
-                  <Copy
-                    dangerouslySetInnerHTML={{
-                      __html: sanitiseCopy(row.data.copy, ['br', 'b', 'i'])
-                    }}
-                  />
-                </RightPanel>
-              </ListItem>
-            ))}
-          </List>
-        </ListContainer>
-      </ContentContainer>
-      <ShowAllContainer showAll={showAll} displayShowAll={displayShowAll}>
-        <ShowAllButton onClick={handleShowAll}>
-          {showAll ? 'Collapse' : 'Show all'}
-        </ShowAllButton>
-      </ShowAllContainer>
-    </Container>
+    <TrackingContextProvider
+      context={{
+        object: 'InArticleTimelines',
+        attrs: {
+          component_type:
+            'in-article component : timelines: ' +
+            (displayShowAll ? 'interactive' : 'static'),
+          event_navigation_action: 'navigation',
+          component_name: `${headline}`
+        }
+      }}
+      scrolledEvent={scrollEvent}
+    >
+      {({ fireAnalyticsEvent, intersectObserverRef }) => (
+        <Container sectionColour={sectionColour}  ref={intersectObserverRef}>
+        <ContentContainer>
+          <Label sectionColour={sectionColour}>{label}</Label>
+          {headline && <Headline>{headline}</Headline>}
+          <ListContainer
+            ref={showAllRef}
+            showAll={showAll}
+            maxHeight={maxHeight}
+            displayShowAll={displayShowAll}
+          >
+            <List>
+              {timelinesData.map((row: TimelinesData, index: number) => (
+                <ListItem key={index}>
+                  <LeftPanel
+                    sectionColour={sectionColour}
+                    circularImage={row.data.image}
+                  >
+                    {row.data.image && <img src={row.data.image} />}
+                  </LeftPanel>
+                  <RightPanel>
+                    <Date sectionColour={sectionColour}>{row.data.date}</Date>
+                    <SubHeading>{row.data.eventHeading}</SubHeading>
+                    <Copy
+                      dangerouslySetInnerHTML={{
+                        __html: sanitiseCopy(row.data.copy, ['br', 'b', 'i'])
+                      }}
+                    />
+                  </RightPanel>
+                </ListItem>
+              ))}
+            </List>
+          </ListContainer>
+        </ContentContainer>
+        <ShowAllContainer showAll={showAll} displayShowAll={displayShowAll}>
+        <ShowAllButton
+              onClick={() =>
+                handleShowAll(
+                  fireAnalyticsEvent,
+                  showAll ? 'Collapse' : 'Show all'
+                )
+              }
+            >
+              {showAll ? 'Collapse' : 'Show all'}
+            </ShowAllButton>
+        </ShowAllContainer>
+      </Container>
+      )}
+    </TrackingContextProvider>
   );
 };
