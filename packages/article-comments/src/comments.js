60621b9f988593825910e3c38292d1a02971abb5
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2021-09-27 10:59:49 +0100

    add commenting login listener

--- packages/article-comments/src/comments.js
+++ packages/article-comments/src/comments.js
@@ -74,6 +74,7 @@ class Comments extends Component {
           return null;
       }
     };
+
     let spotAccountId = commentingConfig.account.readOnly;
     if (commentingConfig && commentingConfig.switchOver) {
       const switchOverDate = commentingConfig.switchOver;
@@ -81,22 +82,6 @@ class Comments extends Component {
         spotAccountId = commentingConfig.account.current;
       }
     }
-
-    const launcherScript = document.createElement("script");
-    launcherScript.setAttribute("async", "async");
-    launcherScript.setAttribute(
-      "src",
-      `https://launcher.spot.im/spot/${spotAccountId}`
-    );
-    launcherScript.setAttribute("data-spotim-module", "spotim-launcher");
-    launcherScript.setAttribute("data-post-id", articleId);
-    launcherScript.setAttribute(
-      "data-post-url",
-      `https://www.thetimes.co.uk/article/${articleId}`
-    );
-    launcherScript.setAttribute("data-seo-enabled", true);
-    launcherScript.setAttribute("data-livefyre-url", articleId);
-    this.container.appendChild(launcherScript);
 
     document.addEventListener(
       "spot-im-current-user-typing-start",
@@ -137,11 +122,30 @@ class Comments extends Component {
       getShareEvent(event)
     );
 
+    const launcherScript = document.createElement("script");
+    launcherScript.setAttribute("async", "async");
+    launcherScript.setAttribute(
+      "src",
+      `https://launcher.spot.im/spot/${spotAccountId}`
+    );
+    launcherScript.setAttribute("data-spotim-module", "spotim-launcher");
+    launcherScript.setAttribute("data-post-id", articleId);
+    launcherScript.setAttribute(
+      "data-post-url",
+      `https://www.thetimes.co.uk/article/${articleId}`
+    );
+    launcherScript.setAttribute("data-seo-enabled", true);
+    launcherScript.setAttribute("data-livefyre-url", articleId);
+    this.container.appendChild(launcherScript);
+
     if (!isReadOnly) {
       if (window.SPOTIM && window.SPOTIM.startSSO) {
         executeSSOtransaction(() => {});
       } else {
         document.addEventListener("spot-im-api-ready", () =>
+          executeSSOtransaction(() => {})
+        );
+        document.addEventListener("spot-im-login-start", () =>
           executeSSOtransaction(() => {})
         );
       }
