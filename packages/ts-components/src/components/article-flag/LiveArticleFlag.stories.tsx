e0393f0d36cd7ac597c85f3fa50e526a379b1dee
Olivia Thetgyi <othetgyi@gmail.com>
2021-11-10 15:00:28 +0000

    feat(TDP-478): add component skeleton, storybook

---
+++ packages/ts-components/src/components/article-flag/LiveArticleFlag.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { LiveArticleFlag } from './LiveArticleFlag';
+
+storiesOf('Typescript Component/LiveArticleFlag', module).add('default', () => {
+    return (
+        <LiveArticleFlag />
+    );
+});
---
+++ packages/ts-components/src/components/article-flag/LiveArticleFlag.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { colours } from '@times-components/styleguide';
+import { gqlRgbaToStyle } from '@times-components/utils';
+import Styles from './styles';
+
+export const LiveArticleFlag: React.FC = () => {
+    return (
+        <div>
+            <div>
+                <li>
+                    LIVE
+                </li>
+            </div>
+        </div>
+    );
+
+};
+
