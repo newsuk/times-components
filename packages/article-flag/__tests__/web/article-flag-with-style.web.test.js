f123bc4f4863ab692ee4a91ee5bd981cee45da6d
Vinh Lam <vinh.lam@gmail.com>
2021-11-15 12:53:58 +0000

    feat(TDP-478): Fixed article-flags test

--- packages/article-flag/__tests__/web/article-flag-with-style.web.test.js
+++ packages/article-flag/__tests__/web/article-flag-with-style.web.test.js
@@ -1,3 +1,9 @@
 import shared from "../shared-with-style.web";
 
+jest.mock("@times-components/ts-components", () => ({
+  __esModule: true,
+  ...jest.requireActual("@times-components/ts-components"),
+  LiveArticleFlag: "LiveArticleFlag"
+}));
+
 shared();
