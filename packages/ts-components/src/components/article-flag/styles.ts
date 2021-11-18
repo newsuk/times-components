eaca37646cdbd29cc08132428984d26c8759914c
Olivia Thetgyi <othetgyi@gmail.com>
2021-11-18 16:14:05 +0000

    feat(TDP-478): center bullet in flag

--- packages/ts-components/src/components/article-flag/styles.ts
+++ packages/ts-components/src/components/article-flag/styles.ts
@@ -10,6 +10,7 @@ export const Container = styled.div`
 export const BulletContainer = styled.div`
   margin-right: 4px;
   color: #ffffff;
+  align-self: center;
 `;
 
 export const ContentContainer = styled.div`
