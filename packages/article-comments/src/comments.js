4ae794b3f5e54cf56a261a917dbcd7f5c5d9b44e
Vinh Lam <vinh.lam@gmail.com>
2021-12-16 15:57:30 +0000

    TDP-1024 - reverted changes as bug resolved by openweb.

--- packages/article-comments/src/comments.js
+++ packages/article-comments/src/comments.js
@@ -92,7 +92,7 @@ class Comments extends Component {
       { once: true }
     );
     document.addEventListener(
-      "spot-im-current-user-sent-message-succeed",
+      "spot-im-current-user-sent-message",
       onCommentPost
     );
     document.addEventListener(
