7c63cedc35660cf90d2dabe3e088260690a0b91c
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2021-12-21 09:57:56 +0000

    feat(TDP-601): Make live article flag separate to sponsored flag (#2826)
    * feat(TDP-601): add live article flag separate to sponsored flag
    
    * feat(TDP-601): update snapshot
    
    * feat(TDP-601): update snapshots in provider package

--- packages/article-flag/src/article-flag.js
+++ packages/article-flag/src/article-flag.js
@@ -80,7 +80,7 @@ const flagsMapping = color =>
     ["LIVE", <LiveArticleFlag color="white" />],
     ["UPDATED", <UpdatedArticleFlag color={color} />],
     ["EXCLUSIVE", <ExclusiveArticleFlag color={color} />],
-    ["SPONSORED", <LiveArticleFlag color="white" />],
+    ["SPONSORED", <SponsoredArticleFlag color={color} />],
     ["LONGREAD", <LongReadArticleFlag color={color} />]
   ]);
 
