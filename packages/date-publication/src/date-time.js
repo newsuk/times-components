9b623ec87ddb904e5544ee4699eadb354ec5b09e
Adam Osborne <me@adamosborne.com>
2021-12-22 15:32:23 +0000

    feat(TDP-588): temporarily removed the update to date-publication
    as it was breaking tests for package/article just by having date-fns "2.+ in the date-time module

--- packages/date-publication/src/date-time.js
+++ packages/date-publication/src/date-time.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
 import format from "date-fns/format";
-import addMinutes from "date-fns/addMinutes";
+import addMinutes from "date-fns/add_minutes"; // addMinutes
 import { getUTCTime, isBST, isLondonTimezone } from "./date";
 
 class DatePublication extends Component {
@@ -33,9 +33,9 @@ class DatePublication extends Component {
     const isDateBST = isBST(datetimeUTC);
     const offset = isDateBST ? 60 : 0;
     const datetimeLondonTimezone = addMinutes(datetimeUTC, offset);
-    const baseFormatString = "MMMM dd yyyy, h.mmaaa";
+    const baseFormatString = "MMMM DD YYYY, h.mma"; // "MMMM dd yyyy, h.mmaaa";
     const formatString = showDay
-      ? `EEEE ${baseFormatString}`
+      ? `dddd ${baseFormatString}` // EEEE
       : baseFormatString;
 
     return children(
