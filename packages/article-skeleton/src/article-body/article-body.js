a45332682acd76e72b7613de26c6878d8ed9834b
Bibi Collins <bibivictoria.collins@news.co.uk>
2022-01-07 14:29:40 +0000

    Update snapshots

--- packages/article-skeleton/src/article-body/article-body.js
+++ packages/article-skeleton/src/article-body/article-body.js
@@ -365,12 +365,12 @@ const renderers = ({
     return (
       <Context.Consumer key={key}>
         {({ theme }) => (
-          <div style={{display: 'none'}} className="InArticleRelatedArticles">
-          <InArticleRelatedArticles
-            heading="Related Articles"
-            relatedArticles={element.attributes.relatedArticles}
-            sectionColour={theme.sectionColour}
-          />
+          <div style={{ display: "none" }} className="inlineRelatedArticles">
+            <InArticleRelatedArticles
+              heading="Related Articles"
+              relatedArticles={element.attributes.relatedArticles}
+              sectionColour={theme.sectionColour}
+            />
           </div>
         )}
       </Context.Consumer>
