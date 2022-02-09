410008dbdd485c8c066e9129fcd0eaeaa65bca62
Olivia Thetgyi <othetgyi@gmail.com>
2022-02-09 15:54:55 +0000

    feat(TDP-760): add basic UI styling

--- packages/ts-components/src/components/article-header/ArticleHeader.tsx
+++ packages/ts-components/src/components/article-header/ArticleHeader.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Container, ContentContainer } from './styles';
+import { Container, Divider, Headline, TimeSincePublishing, UpdatedDate, UpdatedTime, UpdatedTimeItems, UpdatesContainer } from './styles';
 
 export const ArticleHeader: React.FC = () => {
     return (
         <Container>
-            <ContentContainer>
-                I am content
-            </ContentContainer>
+            <UpdatesContainer>
+                <UpdatedTimeItems>
+                    <TimeSincePublishing>
+                        10 minutes ago
+                    </TimeSincePublishing>
+                    <Divider>
+                        |
+                    </Divider>
+                    <UpdatedTime>
+                        11.25am
+                    </UpdatedTime>
+                </UpdatedTimeItems>
+                    <UpdatedDate>
+                        February 9 2022
+                    </UpdatedDate>
+            </UpdatesContainer>
+            <Headline>
+                    This is a headline that resizes at a breakpoint of 768px
+            </Headline>
         </Container>
     
         )
--- packages/ts-components/src/components/article-header/styles.ts
+++ packages/ts-components/src/components/article-header/styles.ts
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
-import  { breakpoints } from '@times-components/styleguide';
+import styled, { css } from 'styled-components';
+import  { breakpoints, colours, fonts } from '@times-components/styleguide';
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    margin: 0 auto 20px auto;
-    padding: 20px;
+    margin: 48px auto 20px auto;
+    padding-top: 8px;
     border-top: 2px solid #9F0000;
 
     @media (min-width: ${breakpoints.medium}px) {
         flex-direction: row;
         width: 80.8%;
+        margin-top: 64px;
     }
 
     @media (min-width: ${breakpoints.wide}px) {
@@ -18,8 +19,58 @@ export const Container = styled.div`
     }
 `;
 
-export const ContentContainer = styled.div`
+export const UpdatesContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: start;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const TimeSincePublishing = styled.div`
+    color: ${colours.functional.brandColour};
+    font-family: ${fonts.supporting};
+    font-size: 14px;
+    line-height: 18px;
+    padding: 0 12px 0 4px;
+`;
+const updatedStyle = css`
+    color: ${colours.functional.secondary};
+    font-family: ${fonts.supporting};
+    font-size: 14px;
+    line-height: 18px;
+`;
+
+export const UpdatedTimeItems = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+`;
+
+export const UpdatedTime = styled.div`
+    ${updatedStyle}
+    padding: 0 0 0 8px;
+`;
+
+export const UpdatedDate = styled.div`
+    ${updatedStyle}
+    justify-content: end;
+    padding: 0 4px 0 0;
+`;
+
+export const Divider = styled.div`
+    color: ${colours.functional.greyLabel};
+    font-size: 14px;
+    line-height: 18px;
+    
+`;
+
+export const Headline = styled.h2`
+    color: ${colours.functional.brandColour};
+    font-family: ${fonts.headline};
+    font-size: 28px;
+    line-height: 28px;
+
+    @media (min-width: ${breakpoints.medium}px) {
+        font-size: 36px;
+        line-height: 36px;
+    }
 `;
