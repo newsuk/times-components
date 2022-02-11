3919d6c539b377b5285bb78a0f4ce0f3eeb47245
Olivia Thetgyi <othetgyi@gmail.com>
2022-02-11 12:30:27 +0000

    feat(TDP-760): add logic to display time since publishing

--- packages/ts-components/src/components/article-header/ArticleHeader.tsx
+++ packages/ts-components/src/components/article-header/ArticleHeader.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { format } from 'date-fns';
+import differenceInSeconds from 'date-fns/difference_in_seconds';
+import differenceInHours from 'date-fns/difference_in_hours'
+import distanceInWordsStrict from 'date-fns/distance_in_words_strict';
 
 import { BreakingArticleFlag } from '../article-flag/LiveArticleFlag';
 import { Container, Divider, Headline, TimeSincePublishing, UpdatedDate, UpdatedTime, UpdatedTimeItems, UpdatesContainer } from './styles';
 
 export const ArticleHeader: React.FC<{ updated: string; breaking: boolean, headline: string}> = ( { updated, breaking, headline }) => {
+    const currentDateTime = new Date();
+    const timeSincePublishing = distanceInWordsStrict(updated, currentDateTime, {partialMethod: 'floor'}) + ' ago';
+    const secondsDifference = differenceInSeconds(currentDateTime, updated);
+    const hoursDifference = differenceInHours(currentDateTime,updated);
 
     return (
         <Container>
@@ -12,7 +19,8 @@ export const ArticleHeader: React.FC<{ updated: string; breaking: boolean, headl
                 <UpdatedTimeItems>
                     {breaking ? <BreakingArticleFlag/>: null}
                     <TimeSincePublishing>
-                        Time
+                        {secondsDifference > 59 && hoursDifference < 13
+                        ? timeSincePublishing : null}
                     </TimeSincePublishing>
                     <Divider>
                         |
