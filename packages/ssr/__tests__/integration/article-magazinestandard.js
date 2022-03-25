576e67db358e05df7e7bb68ddba817cde0b5b689
Yavor Peev <yavor.peev@news.co.uk>
2022-03-25 23:04:55 +0200

    skipping match snapshots in regards with https://nidigitalsolutions.jira.com/browse/TDP-1249

--- packages/ssr/__tests__/integration/article-magazinestandard.js
+++ packages/ssr/__tests__/integration/article-magazinestandard.js
@@ -3,6 +3,7 @@ import articleTests from "../helpers/article-helper";
 articleTests("magazinestandard", {
   stickyElements: ["#nav"],
   attachFlags: true,
+  skipSnapshotTest: true,
   blackoutElements: [
     "[data-testid=save-and-share-bar]",
     '[data-tc-sticky-container="true"]',
