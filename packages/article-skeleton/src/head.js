9d7f2af53ca14f1f3e946bf2c670fc6e7116d192
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-03-28 19:32:08 +0100

    add check for empty paragraph value

--- packages/article-skeleton/src/head.js
+++ packages/article-skeleton/src/head.js
@@ -181,14 +181,8 @@ const getLiveBlogUpdates = (article, publisher, author) => {
           }
         } else if (contentObj[i].name === "paragraph") {
           if (update !== undefined) {
-<<<<<<< HEAD
-            const updateText = `${get(
-              contentObj[i],
-              "children[0].attributes.value"
-            )} `;
-=======
-            const updateText = `${get(contentObj[i], "children[0].attributes.value")} `;
->>>>>>> 1f49535c06f53a9407a9c63a1172a67d91094101
+            const text = get(contentObj[i], "children[0].attributes.value");
+            const updateText = text ? `${get(text)} ` : "";
             if (update.articleBody) {
               update.articleBody += updateText;
             } else {
