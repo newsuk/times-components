92ac99a2024d45eee68b53ea876103691b8c716e
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-04-01 12:42:48 +0100

    fix(TDP-1577): key facts anchor scroll

--- packages/ssr/src/client/article.js
+++ packages/ssr/src/client/article.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-useless-escape */
 
+const smoothscroll = require('smoothscroll-polyfill');
+
 const article = require("../component/article");
 const runClient = require("../lib/run-client");
 
@@ -50,6 +52,8 @@ if (window.nuk && window.nuk.ssr && window.nuk.article) {
       "x-new-topic-data-source": true
     }
   };
+
+  smoothscroll.polyfill();
 
   runClient(article, clientOptions, data);
 }
