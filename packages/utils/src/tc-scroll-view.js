12dcd07e7fdef2f06e41d44a74a34cb39b93556d
Adele Kufour <adele.kufour@news.co.uk>
2022-04-08 14:03:11 +0100

    adding new tc scroll view component and starting to fix styleguide storybook

---
+++ packages/utils/src/tc-scroll-view.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+// not sure how else to put the default styles, this the default style for a React Native <Text/> component
+
+const TcScrollView = styled.div`
+  height: 500px;
+  overflow-y: scroll;
+  white-space: nowrap;
+`;
+
+export default TcScrollView;
--- packages/utils/src/tc-text.js
+++ packages/utils/src/tc-text.js
@@ -1,10 +1,13 @@
-import styled from "styled-components";
+import React from "react";
+import styled,  { Interpolation } from "styled-components";
 // not sure how else to put the default styles, this the default style for a React Native <Text/> component
+
+
 
 const TcText = styled.div`
   border: 0px solid black;
   box-sizing: border-box;
-  color: rgb(0, 0, 0);
+  color: ${props => props.style && props.style.color ? props.style.color : 'rgb(0, 0, 0)'};
   display: inline;
   font: 14px system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Ubuntu, "Helvetica Neue", sans-serif;
@@ -12,6 +15,16 @@ const TcText = styled.div`
   padding: 0px;
   white-space: pre-wrap;
   overflow-wrap: break-word;
+  font-size: ${props => props.style && props.style.fontSize ? props.style.fontSize : '14px'};
+  font-weight: ${props => props.style && props.style.fontWeight && props.style.fontWeight};
+  margin-bottom: ${props => props.style && props.style.marginBottom ? props.style.marginBottom : '0px'};
+  margin-left: ${props => props.style && props.style.marginLeft ? props.style.marginLeft : '0px'};
+  margin-top: ${props => props.style && props.style.marginTop ? props.style.marginTop : '0px'};
+  font-family: ${props => props.style && props.style.fontFamily ? props.style.fontFamily : 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif'};
 `;
 
 export default TcText;
+// ${({ style }) => style.color ? style.color : 'rgb(0, 0, 0)'};
+//${props => props.style.color || 'rgb(0, 0, 0)'};`
+
+
