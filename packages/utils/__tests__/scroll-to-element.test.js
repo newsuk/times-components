994c75a42e7731e10e0e1b3363c6671dc692d454
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-04-11 16:24:19 +0100

    feat(TDP-1579): inline image and interactive anchor links

--- packages/utils/__tests__/scroll-to-element.test.js
+++ packages/utils/__tests__/scroll-to-element.test.js
@@ -1,17 +1,81 @@
 import { handleOnClickScrollTo, handleHrefScrollTo } from "../src/index";
 
+const scrollTo = jest.fn();
+
+global.window = { pageYOffset: 150, scrollTo };
+global.document = {
+  getElementById: () => ({ getBoundingClientRect: () => ({ top: 100 }) }),
+  querySelector: () => ({ offsetHeight: 0 })
+};
+
+const event = { preventDefault: () => {} };
+
+const id = "12345";
+const hash = `#${id}`;
+const link = "https://www.thetimes.co.uk";
+
 describe("handleOnClickScrollTo", () => {
-  it("should return undefined when no url is supplied", () => {
-    expect(handleOnClickScrollTo()).toBeUndefined();
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should return undefined - when no url is supplied", () => {
+    expect(handleOnClickScrollTo(event)).toBeUndefined();
+  });
+
+  it("should return undefined - when url does not starts with a hash", () => {
+    expect(handleOnClickScrollTo(event, link)).toBeUndefined();
+  });
+
+  describe("small breakpoint", () => {
+    it("should call scrollTo - with correct values - for small breakpoint", () => {
+      window.innerWidth = 370;
+      handleOnClickScrollTo(event, hash);
+      expect(scrollTo).toHaveBeenCalledWith({ top: 140, behavior: "smooth" });
+    });
+  });
+
+  describe("medium breakpoint", () => {
+    it("should call scrollTo - with correct values - for medium breakpoint", () => {
+      window.innerWidth = 1024;
+      handleOnClickScrollTo(event, hash);
+      expect(scrollTo).toHaveBeenCalledWith({ top: 150, behavior: "smooth" });
+    });
+
+    it("should call scrollTo - with correct values - for medium breakpoint with section bar", () => {
+      window.innerWidth = 1024;
+      document.querySelector = () => ({ offsetHeight: 20 });
+      handleOnClickScrollTo(event, hash);
+      expect(scrollTo).toHaveBeenCalledWith({ top: 130, behavior: "smooth" });
+    });
+  });
+
+  describe("large breakpoint", () => {
+    it("should call scrollTo - with correct values - for large breakpoint", () => {
+      window.innerWidth = 1420;
+      handleOnClickScrollTo(event, hash);
+      expect(scrollTo).toHaveBeenCalledWith({ top: 180, behavior: "smooth" });
+    });
+
+    it("should call scrollTo - with correct values - for large breakpoint with section bar", () => {
+      window.innerWidth = 1420;
+      document.querySelector = () => ({ offsetHeight: 40 });
+      handleOnClickScrollTo(event, hash);
+      expect(scrollTo).toHaveBeenCalledWith({ top: 160, behavior: "smooth" });
+    });
   });
 });
 
 describe("handleHrefScrollTo", () => {
-  it("should return undefined when no url is supplied", () => {
+  it("should return undefined - when no url is supplied", () => {
     expect(handleHrefScrollTo()).toBeUndefined();
   });
 
-  it("should return null when url starts with a hash", () => {
-    expect(handleHrefScrollTo("#12345")).toBeNull();
+  it("should return null - when url starts with a hash", () => {
+    expect(handleHrefScrollTo(hash)).toBeNull();
+  });
+
+  it("should return the url - when url does not starts with a hash", () => {
+    expect(handleHrefScrollTo(link)).toEqual(link);
   });
 });
