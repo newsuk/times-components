33ffed91f43d93926c24d6436aa910c8a0fdc580
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-04-20 22:09:36 +0100

    fix(TDP-000): article header data and time

--- packages/ts-components/src/components/article-header/ArticleHeader.stories.tsx
+++ packages/ts-components/src/components/article-header/ArticleHeader.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { date, select, text } from '@storybook/addon-knobs';
+import { select, text } from '@storybook/addon-knobs';
 
 import { ArticleHarness } from '../../fixtures/article-harness/ArticleHarness';
 import ArticleHeader from './ArticleHeader';
@@ -10,40 +10,39 @@ storiesOf('Typescript Component/Article Header', module)
     <ArticleHarness>{storyFn()}</ArticleHarness>
   ))
   .add('Article Header with headline', () => {
-    const label = 'Updated Date/Time';
-    const defaultValue = new Date();
     const groupId = 'Options';
-    const value = date(label, defaultValue, groupId);
-    const breakingOptions = {
-      True: 'true',
-      False: undefined
-    };
-    const updated = new Date(value).toISOString();
 
+    // const label = 'Updated Date/Time';
+    // const defaultValue = new Date();
+    // const value = date(label, defaultValue, groupId);
+    // const updated = new Date(value).toISOString();
+
+    const breakingOptions = { True: 'true', False: undefined };
     const headline = text('Headline', 'This is the headline', groupId);
 
     return (
       <ArticleHeader
-        updated={updated}
+        date={'20/04/2022'}
+        time={'18:30'}
         breaking={select('Breaking', breakingOptions, undefined, groupId)}
         headline={encodeURIComponent(headline)}
       />
     );
   })
   .add('Article Header without headline', () => {
-    const label = 'Updated Date/Time';
-    const defaultValue = new Date();
     const groupId = 'Options';
-    const value = date(label, defaultValue, groupId);
-    const breakingOptions = {
-      True: 'true',
-      False: undefined
-    };
-    const updated = new Date(value).toISOString();
+
+    // const label = 'Updated Date/Time';
+    // const defaultValue = new Date();
+    // const value = date(label, defaultValue, groupId);
+    // const updated = new Date(value).toISOString();
+
+    const breakingOptions = { True: 'true', False: undefined };
 
     return (
       <ArticleHeader
-        updated={updated}
+        date={'31/12/2021'}
+        time={'21:30'}
         breaking={select('Breaking', breakingOptions, undefined, groupId)}
       />
     );
