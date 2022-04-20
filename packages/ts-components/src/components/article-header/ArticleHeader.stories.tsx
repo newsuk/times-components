f279757785be6f5a4630dab2d157bf307b3e49cb
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-04-20 22:50:52 +0100

    fix(TDP-000): article header data and time

--- packages/ts-components/src/components/article-header/ArticleHeader.stories.tsx
+++ packages/ts-components/src/components/article-header/ArticleHeader.stories.tsx
@@ -1,49 +1,48 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select, text } from '@storybook/addon-knobs';
+import { format } from 'date-fns';
 
 import { ArticleHarness } from '../../fixtures/article-harness/ArticleHarness';
 import ArticleHeader from './ArticleHeader';
+
+const getAttributes = () => {
+  const id = 'Options';
+
+  const now = new Date();
+  const date = text('Date', format(now, 'dd/MM/yyyy'), id);
+  const time = text('Time', format(now, 'HH:mm'), id);
+
+  const options = { True: 'true', False: undefined };
+  const breaking = select('Breaking', options, 'true', id);
+
+  const headline = text('Headline', 'This is the headline', id);
+
+  return { date, time, breaking, headline };
+};
 
 storiesOf('Typescript Component/Article Header', module)
   .addDecorator((storyFn: () => React.ReactNode) => (
     <ArticleHarness>{storyFn()}</ArticleHarness>
   ))
   .add('Article Header with headline', () => {
-    const groupId = 'Options';
-
-    // const label = 'Updated Date/Time';
-    // const defaultValue = new Date();
-    // const value = date(label, defaultValue, groupId);
-    // const updated = new Date(value).toISOString();
-
-    const breakingOptions = { True: 'true', False: undefined };
-    const headline = text('Headline', 'This is the headline', groupId);
-
+    const props = getAttributes();
     return (
       <ArticleHeader
-        date={'20/04/2022'}
-        time={'18:30'}
-        breaking={select('Breaking', breakingOptions, undefined, groupId)}
-        headline={encodeURIComponent(headline)}
+        date={props.date}
+        time={props.time}
+        breaking={props.breaking}
+        headline={encodeURIComponent(props.headline)}
       />
     );
   })
   .add('Article Header without headline', () => {
-    const groupId = 'Options';
-
-    // const label = 'Updated Date/Time';
-    // const defaultValue = new Date();
-    // const value = date(label, defaultValue, groupId);
-    // const updated = new Date(value).toISOString();
-
-    const breakingOptions = { True: 'true', False: undefined };
-
+    const props = getAttributes();
     return (
       <ArticleHeader
-        date={'31/12/2021'}
-        time={'21:30'}
-        breaking={select('Breaking', breakingOptions, undefined, groupId)}
+        date={props.date}
+        time={props.time}
+        breaking={props.breaking}
       />
     );
   });
