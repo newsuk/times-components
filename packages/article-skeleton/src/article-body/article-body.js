7453ab2498cc12a6e6ea8ba1df9ee71bb4798628
Callum Christie <callumchristie@hotmail.com>
2022-04-21 17:54:04 +0100

    fix(TDP-000): ArticleHeader date and time update (#2924)
    * fix(TDP-000): article header date and time
    
    * fix(TDP-000): article header date and time

--- packages/article-skeleton/src/article-body/article-body.js
+++ packages/article-skeleton/src/article-body/article-body.js
@@ -410,8 +410,9 @@ const renderers = ({
         return (
           <div id={id}>
             <ArticleHeader
-              date={attributes.date}
-              time={attributes.time}
+              updated={attributes.updated}
+              // date={attributes.date}
+              // time={attributes.time}
               breaking={attributes.breaking}
               headline={attributes.headline}
             />
