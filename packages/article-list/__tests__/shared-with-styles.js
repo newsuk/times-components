3179d86a68dba6411299abf726fa3bf24ed2f445
Adam Osborne <me@adamosborne.com>
2022-04-26 10:59:14 +0100

    feat(TDP-1693): removed react native from article list

--- packages/article-list/__tests__/shared-with-styles.js
+++ packages/article-list/__tests__/shared-with-styles.js
@@ -1,4 +1,3 @@
-import { AppRegistry } from "react-native-web";
 import {
   addSerializers,
   compose,
@@ -6,45 +5,12 @@ import {
   hoistStyleTransform,
   minimaliseTransform,
   minimalWebTransform,
-  rnwTransform,
   stylePrinter
 } from "@times-components/jest-serializer";
 import "./mocks";
 import shared from "./shared.base";
 
 const keepProps = new Set(["class", "className", "style"]);
-
-const styles = [
-  "alignItems",
-  "alignSelf",
-  "backgroundColor",
-  "borderBottomWidth",
-  "borderColor",
-  "borderTopWidth",
-  "bottom",
-  "color",
-  "flex",
-  "flexDirection",
-  "fontFamily",
-  "fontSize",
-  "height",
-  "justifyContent",
-  "margin",
-  "marginBottom",
-  "marginLeft",
-  "marginRight",
-  "marginTop",
-  "maxWidth",
-  "minHeight",
-  "paddingBottom",
-  "paddingTop",
-  "paddingVertical",
-  "position",
-  "right",
-  "textAlign",
-  "viewBox",
-  "width"
-];
 
 export default () => {
   addSerializers(
@@ -55,7 +21,6 @@ export default () => {
       minimaliseTransform(
         (value, key) => !keepProps.has(key) && !key.includes("Class")
       ),
-      rnwTransform(AppRegistry, styles),
       flattenStyleTransform,
       hoistStyleTransform
     )
