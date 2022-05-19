ae5513fd4f8051fb2573099f04080d8ae1c7c683
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-05-19 11:56:10 +0100

    TDP-1942 Sorted out StoryBook entries

---
+++ packages/ts-components/src/components/recommended-articles/RecommendedArticles.stories.tsx
@@ -0,0 +1,135 @@
+import { showcaseConverter } from '@times-components/storybook';
+
+import React from 'react';
+import { MockedProvider } from '@times-components/provider-test-tools';
+import { recommendations } from '@times-components/provider-queries';
+import { RecommendedArticles } from './RecommendedArticles';
+import { TrackingContextProvider } from '../../helpers/tracking/TrackingContextProvider';
+import analyticsStream from '../../fixtures/analytics-actions/analytics-actions';
+
+const mocks = [
+  {
+    delay: 1000,
+    request: {
+      query: recommendations,
+      variables: {
+        "publisher": "TIMES",
+        "recomArgs": {
+          "userId": "1234",
+          "articleId": "94a01926-719a-11ec-aacf-0736e08b15cd"
+        }
+      }
+    },
+    result: {
+      data: {
+        recommendations: {
+          "__typename": "Recommendations",
+          "articles": [
+            {
+              "__typename": "UniversalArticle",
+              "bylines": [
+                {
+                  "__typename": "TextByline"
+                }
+              ],
+              "flags": [
+                "NEW"
+              ],
+              "headline": "Whole world is against us, says top Russian strategist",
+              "id": "a9ffb7cc-d5d1-11ec-bb99-1bcd45646516",
+              "label": "war in ukraine",
+              "media": {
+                "__typename": "Image"
+              },
+              "publishedDateTime": "2022-05-17T12:10:00.000Z",
+              "publisher": "TIMES",
+              "slug": "were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel",
+              "summary": {
+                "__typename": "ArticleParagraph",
+                "children": [
+                  {
+                    "__typename": "ArticleText"
+                  },
+                  {
+                    "__typename": "ArticleText"
+                  }
+                ]
+              },
+              "template": "mainstandard",
+              "updatedDateTime": "2022-05-18T14:02:47.000Z",
+              "url": "https://www.staging-thetimes.co.uk/article/were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel-lhnvsfj33"
+            },
+            {
+              "__typename": "UniversalArticle",
+              "bylines": [
+                {
+                  "__typename": "TextByline"
+                }
+              ],
+              "flags": [],
+              "headline": "Vardys leave court with swipe at Wayne Rooney",
+              "id": "f3d730a0-d5c2-11ec-8585-951ab3afb4d2",
+              "label": "UK NEWS",
+              "media": {
+                "__typename": "Image"
+              },
+              "publishedDateTime": "2022-05-17T23:00:00.000Z",
+              "publisher": "TIMES",
+              "slug": "wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time",
+              "summary": {
+                "__typename": "ArticleParagraph",
+                "children": [
+                  {
+                    "__typename": "ArticleText"
+                  },
+                  {
+                    "__typename": "ArticleHyperlink"
+                  },
+                  {
+                    "__typename": "ArticleText"
+                  },
+                  {
+                    "__typename": "ArticleText"
+                  }
+                ]
+              },
+              "template": "mainstandard",
+              "updatedDateTime": "2022-05-17T23:34:37.000Z",
+              "url": "https://www.staging-thetimes.co.uk/article/wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time-wlzvxklc6"
+            }
+          ]
+        }
+      }
+    }
+  }
+];
+
+const recarticles = {
+  children: [
+    {
+      // eslint-disable-next-line react/prop-types
+      component: () => (
+        <MockedProvider mocks={mocks}>
+          <TrackingContextProvider
+            analyticsStream={analyticsStream}
+            context={{ component: 'ArticleSkeleton' }}
+          >
+            <RecommendedArticles
+              recomArgs={{
+                userId: '1234',
+                articleId: '94a01926-719a-11ec-aacf-0736e08b15cd'
+              }}
+            />
+          </TrackingContextProvider>
+        </MockedProvider>
+      ),
+
+      name: 'Recommended Articles',
+      platform: 'web',
+      type: 'story'
+    }
+  ],
+  name: 'Typescript Component/Recommended Articles/Recommended Articles'
+};
+
+showcaseConverter(module, recarticles);
