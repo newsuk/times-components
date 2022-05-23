80cee0e2f1f7fafaf19340adc443763b31f428e8
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-05-23 14:27:13 +0100

    TDP-1942 Cleaned up GQL in Sotrybook

--- packages/ts-components/src/components/recommended-articles/RecommendedArticles.stories.tsx
+++ packages/ts-components/src/components/recommended-articles/RecommendedArticles.stories.tsx
@@ -28,74 +28,22 @@ const mocks = [
           articles: [
             {
               __typename: 'UniversalArticle',
-              bylines: [
-                {
-                  __typename: 'TextByline'
-                }
-              ],
-              flags: [
-                'NEW'
-              ],
               headline: 'Whole world is against us, says top Russian strategist',
               id: 'a9ffb7cc-d5d1-11ec-bb99-1bcd45646516',
-              label: 'war in ukraine',
               media: {
                 __typename: 'Image'
               },
-              publishedDateTime: '2022-05-17T12:10:00.000Z',
-              publisher: 'TIMES',
               slug: 'were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel',
-              summary: {
-                __typename: 'ArticleParagraph',
-                children: [
-                  {
-                    __typename: 'ArticleText'
-                  },
-                  {
-                    __typename: 'ArticleText'
-                  }
-                ]
-              },
-              template: 'mainstandard',
-              updatedDateTime: '2022-05-18T14:02:47.000Z',
               url: 'https://www.staging-thetimes.co.uk/article/were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel-lhnvsfj33'
             },
             {
               __typename: 'UniversalArticle',
-              bylines: [
-                {
-                  __typename: 'TextByline'
-                }
-              ],
-              flags: [],
               headline: 'Vardys leave court with swipe at Wayne Rooney',
               id: 'f3d730a0-d5c2-11ec-8585-951ab3afb4d2',
-              label: 'UK NEWS',
               media: {
                 __typename: 'Image'
               },
-              publishedDateTime: '2022-05-17T23:00:00.000Z',
-              publisher: 'TIMES',
               slug: 'wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time',
-              summary: {
-                __typename: 'ArticleParagraph',
-                children: [
-                  {
-                    __typename: 'ArticleText'
-                  },
-                  {
-                    __typename: 'ArticleHyperlink'
-                  },
-                  {
-                    __typename: 'ArticleText'
-                  },
-                  {
-                    __typename: 'ArticleText'
-                  }
-                ]
-              },
-              template: 'mainstandard',
-              updatedDateTime: '2022-05-17T23:34:37.000Z',
               url: 'https://www.staging-thetimes.co.uk/article/wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time-wlzvxklc6'
             }
           ]
