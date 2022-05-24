1d8b7940ba06ee75c9613d0a5beda508a0266d7b
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-05-24 15:27:36 +0100

    TDP-1942 Cleared up linting issues

--- packages/ts-components/src/components/recommended-articles/__tests__/RecommendedArticles.test.tsx
+++ packages/ts-components/src/components/recommended-articles/__tests__/RecommendedArticles.test.tsx
@@ -9,86 +9,83 @@ import { recommendations } from '@times-components/provider-queries';
 
 const mocks = [
 {
-    delay: 1000,
-    request: {
-    query: recommendations,
-    variables: {
-        publisher: 'TIMES',
-        recomArgs: {
-        userId: '1234',
-        articleId: '94a01926-719a-11ec-aacf-0736e08b15cd'
-        }
-    }
-    },
-    result: {
-    data: {
-        recommendations: {
-        __typename: 'Recommendations',
-        leadAsset: 'null',
-        articles: [
-            {
-            __typename: 'UniversalArticle',
-            headline:
-                'Whole world is against us, says top Russian strategist',
-            id: 'a9ffb7cc-d5d1-11ec-bb99-1bcd45646516',
-            media: {
-                __typename: 'Image'
-            },
-            slug:
-                'were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel',
-            url:
-                'https://www.staging-thetimes.co.uk/article/were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel-lhnvsfj33'
-            },
-            {
-            __typename: 'UniversalArticle',
-            headline: 'Vardys leave court with swipe at Wayne Rooney',
-            id: 'f3d730a0-d5c2-11ec-8585-951ab3afb4d2',
-            media: {
-                __typename: 'Image'
-            },
-            slug:
-                'wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time',
-            url:
-                'https://www.staging-thetimes.co.uk/article/wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time-wlzvxklc6'
-            }
-        ]
-        }
-    }
-    }
+	delay: 1000,
+	request: {
+	query: recommendations,
+	variables: {
+		publisher: 'TIMES',
+		recomArgs: {
+		userId: '1234',
+		articleId: '94a01926-719a-11ec-aacf-0736e08b15cd'
+		}
+	}},
+	result: {
+	data: {
+		recommendations: {
+		__typename: 'Recommendations',
+		leadAsset: 'null',
+		articles: [
+			{
+			__typename: 'UniversalArticle',
+			headline:
+				'Whole world is against us, says top Russian strategist',
+			id: 'a9ffb7cc-d5d1-11ec-bb99-1bcd45646516',
+			media: {
+				__typename: 'Image'
+			},
+			slug:
+				'were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel',
+			url:
+				'https://www.staging-thetimes.co.uk/article/were-no-match-for-ukrainian-grit-and-firepower-says-retired-russian-colonel-lhnvsfj33'
+			},
+			{
+			__typename: 'UniversalArticle',
+			headline: 'Vardys leave court with swipe at Wayne Rooney',
+			id: 'f3d730a0-d5c2-11ec-8585-951ab3afb4d2',
+			media: {
+				__typename: 'Image'
+			},
+			slug:
+				'wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time',
+			url:
+				'https://www.staging-thetimes.co.uk/article/wayne-rooney-to-give-evidence-in-wagatha-christie-trial-as-jamie-vardy-attends-court-for-first-time-wlzvxklc6'
+			}
+		]}
+	}}
 }
 ];
 
 describe('Recommended Articles', () => {
   afterEach(() => {
-    jest.clearAllMocks();
-    cleanup();
+	jest.clearAllMocks();
+	cleanup();
   });
 
   it('it renders', () => {
-    const { asFragment } = render(
-        <MockedProvider mocks={mocks}>
-            <RecommendedArticles
-            articleId="94a01926-719a-11ec-aacf-0736e08b15cd"
-            section="News"
-            analyticsStream={analyticsStream}
-            />
-        </MockedProvider>
-    );
-    expect(asFragment()).toMatchSnapshot();
+	const { asFragment } = render(
+		<MockedProvider mocks={mocks}>
+			<RecommendedArticles
+			articleId="94a01926-719a-11ec-aacf-0736e08b15cd"
+			section="News"
+			analyticsStream={analyticsStream}
+			/>
+		</MockedProvider>
+	);
+	expect(asFragment()).toMatchSnapshot();
   });
 
   describe('error checks', () => {
-    it('empty slice', () => {
-      const { asFragment } = render(
-        <MockedProvider mocks={mocks}>
-            <RecommendedArticles
-            articleId=""
-            section="News"
-            analyticsStream={analyticsStream}
-            />
-        </MockedProvider>
-      );
-      expect(asFragment()).toMatchSnapshot();
-    });
+	it('empty slice', () => {
+	  const { asFragment } = render(
+		<MockedProvider mocks={mocks}>
+			<RecommendedArticles
+			articleId=""
+			section="News"
+			analyticsStream={analyticsStream}
+			/>
+		</MockedProvider>
+	  );
+	  expect(asFragment()).toMatchSnapshot();
+	});
   });
 });
