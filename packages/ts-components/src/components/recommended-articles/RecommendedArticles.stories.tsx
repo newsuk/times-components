c347eb7385cc15d711dfe494a7060974c281a444
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-05-26 14:35:13 +0100

    fix(TDP-1942): Recommended articles - hidden

--- packages/ts-components/src/components/recommended-articles/RecommendedArticles.stories.tsx
+++ packages/ts-components/src/components/recommended-articles/RecommendedArticles.stories.tsx
@@ -66,6 +66,7 @@ const recarticles = {
           <RecommendedArticles
             articleId="94a01926-719a-11ec-aacf-0736e08b15cd"
             section="News"
+            isVisible
             analyticsStream={analyticsStream}
           />
         </MockedProvider>
--- packages/ts-components/src/components/recommended-articles/RecommendedArticles.tsx
+++ packages/ts-components/src/components/recommended-articles/RecommendedArticles.tsx
@@ -7,15 +7,19 @@ import { RelatedArticleSliceType } from '../../types/related-article-slice';
 
 import { Placeholder } from '@times-components/image';
 
+import { RecommendedArticlesContainer } from './styles';
+
 type RecommendedArticlesProps = {
   articleId: string;
   section: string;
+  isVisible?: boolean;
   analyticsStream?: (evt: any) => void;
 };
 
 export const RecommendedArticles = ({
   articleId,
   section,
+  isVisible,
   analyticsStream
 }: RecommendedArticlesProps) => {
   const [isReady, setIsReady] = useState<boolean>(false);
@@ -29,41 +33,43 @@ export const RecommendedArticles = ({
   }
 
   return (
-    <GetRecommendedArticles
-      publisher={'TIMES'}
-      recomArgs={{ userId: '1234', articleId }}
-      ssr={false}
-      debounceTimeMs={0}
-    >
-      {({ isLoading, error, recommendations }: any) => {
-        if (error) {
-          return null;
-        }
+    <RecommendedArticlesContainer isVisible={isVisible}>
+      <GetRecommendedArticles
+        publisher={'TIMES'}
+        recomArgs={{ userId: '1234', articleId }}
+        ssr={false}
+        debounceTimeMs={0}
+      >
+        {({ isLoading, error, recommendations }: any) => {
+          if (error) {
+            return null;
+          }
 
-        if (isLoading || !recommendations) {
+          if (isLoading || !recommendations) {
+            return (
+              <div className="placeholder">
+                <Placeholder />
+              </div>
+            );
+          }
+
+          const slice: RelatedArticleSliceType = {
+            sliceName: 'StandardSlice',
+            items: recommendations
+              ? recommendations.articles.map((article: any) => ({ article }))
+              : []
+          };
+
           return (
-            <div className="placeholder">
-              <Placeholder />
-            </div>
+            <RelatedArticles
+              heading={`Today's ${section}`}
+              slice={slice}
+              isVisible
+              analyticsStream={analyticsStream}
+            />
           );
-        }
-
-        const slice: RelatedArticleSliceType = {
-          sliceName: 'StandardSlice',
-          items: recommendations
-            ? recommendations.articles.map((article: any) => ({ article }))
-            : []
-        };
-
-        return (
-          <RelatedArticles
-            heading={`Today's ${section}`}
-            slice={slice}
-            isVisible
-            analyticsStream={analyticsStream}
-          />
-        );
-      }}
-    </GetRecommendedArticles>
+        }}
+      </GetRecommendedArticles>
+    </RecommendedArticlesContainer>
   );
 };
