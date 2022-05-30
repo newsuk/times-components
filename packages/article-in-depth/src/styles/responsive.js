970ae591acb3ce0d66228ee505fd36daad5b4cb5
Adam Osborne <me@adamosborne.com>
2022-05-30 11:42:51 +0100

    feat(TDP-1690): updated styling of a couple of components to have default values as not using TcText

--- packages/article-in-depth/src/styles/responsive.js
+++ packages/article-in-depth/src/styles/responsive.js
@@ -43,6 +43,14 @@ export const HeaderContainer = styled(TcView)`
 `;
 
 export const HeadlineContainer = styled.h1`
+  border: 0px solid black;
+  box-sizing: border-box;
+  display: inline;
+  font: 14px system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+  margin: 0px;
+  padding: 0px;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
   ${props => props.styles && props.styles}
   @media (min-width: ${breakpoints.medium}px) {
     font-size: ${fontSizes.articleHeadline}px;
@@ -87,6 +95,14 @@ export const Separator = styled(TcView)`
 `;
 
 export const StandfirstContainer = styled.h2`
+  border: 0px solid black;
+  box-sizing: border-box;
+  display: inline;
+  font: 14px system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+  margin: 0px;
+  padding: 0px;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
   ${props => props.styles && props.styles}
   @media (min-width: ${breakpoints.medium}px) {
     font-size: ${fontSizes.infoTitle}px;
