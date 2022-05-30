cf174cc37bb63b2cb30eb9668add339ac47b7dd4
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-05-30 09:36:42 +0100

    TDP-1935 Retrieving Customer ID (CPN)

--- packages/ts-components/src/components/recommended-articles/RecommendedArticles.tsx
+++ packages/ts-components/src/components/recommended-articles/RecommendedArticles.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+// import gql from "graphql-tag";
 
 import RelatedArticles from '@times-components/related-articles';
 import { GetRecommendedArticles } from '@times-components/provider';
