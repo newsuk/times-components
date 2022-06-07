b7a2196af8304e49b46e29f9e785a6621468ee76
Callum Christie <Callum.Christie@Sunday-Times.co.uk>
2022-06-07 13:16:14 +0100

    fix(TDP-1825): update tests

--- packages/ts-components/src/components/article-header/__tests__/ArticleHeader.test.tsx
+++ packages/ts-components/src/components/article-header/__tests__/ArticleHeader.test.tsx
@@ -9,50 +9,50 @@ describe('ArticleHeader', () => {
   describe('Same calendar day during GMT', () => {
     afterEach(() => MockDate.reset());
 
-    const updated = '2021-12-31T06:30:00Z';
+    const updated = '2021-12-31T06:30:00+00:00';
 
     it('Within a minute of updating', () => {
-      MockDate.set('2021-12-31T06:30:10Z');
+      MockDate.set('2021-12-31T06:30:10+00:00');
 
       const { getByText, queryByTestId } = render(
         <ArticleHeader updated={updated} />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(queryByTestId('TimeSincePublishing')).toBeFalsy();
     });
 
     it('Within an hour of updating', () => {
-      MockDate.set('2021-12-31T07:00:00Z');
+      MockDate.set('2021-12-31T07:00:00+00:00');
 
       const { getByText } = render(<ArticleHeader updated={updated} />);
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('30 minutes ago')).toBeVisible();
     });
 
     it('Between 1 and 12 hours after updating', () => {
-      MockDate.set('2021-12-31T08:30:00Z');
+      MockDate.set('2021-12-31T08:30:00+00:00');
 
       const { getByText } = render(<ArticleHeader updated={updated} />);
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('2 hours ago')).toBeVisible();
     });
 
     it('After 12 hours but same calendar day', () => {
-      MockDate.set('2021-12-20T20:30:00Z');
+      MockDate.set('2021-12-20T20:30:00+00:00');
 
       const { getByText, queryByTestId } = render(
         <ArticleHeader updated={updated} />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(queryByTestId('TimeSincePublishing')).toBeFalsy();
     });
 
     it('With breaking flag and headline', () => {
-      MockDate.set('2021-12-31T07:00:00Z');
+      MockDate.set('2021-12-31T07:00:00+00:00');
 
       const { baseElement, getByText } = render(
         <ArticleHeader
@@ -62,7 +62,7 @@ describe('ArticleHeader', () => {
         />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('30 minutes ago')).toBeVisible();
       expect(getByText('BREAKING')).toBeVisible();
       expect(getByText('This is the headline')).toBeVisible();
@@ -71,13 +71,13 @@ describe('ArticleHeader', () => {
     });
 
     it('With breaking flag expired', () => {
-      MockDate.set('2021-12-31T08:30:00Z');
+      MockDate.set('2021-12-31T08:30:00+00:00');
 
       const { getByText, queryByText } = render(
         <ArticleHeader updated={updated} breaking="true" />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('2 hours ago')).toBeVisible();
       expect(queryByText('BREAKING')).toBeFalsy();
     });
@@ -95,7 +95,7 @@ describe('ArticleHeader', () => {
         <ArticleHeader updated={updated} />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(queryByTestId('TimeSincePublishing')).toBeFalsy();
     });
 
@@ -104,7 +104,7 @@ describe('ArticleHeader', () => {
 
       const { getByText } = render(<ArticleHeader updated={updated} />);
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('30 minutes ago')).toBeVisible();
     });
 
@@ -113,7 +113,7 @@ describe('ArticleHeader', () => {
 
       const { getByText } = render(<ArticleHeader updated={updated} />);
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(getByText('2 hours ago')).toBeVisible();
     });
 
@@ -124,7 +124,7 @@ describe('ArticleHeader', () => {
         <ArticleHeader updated={updated} />
       );
 
-      expect(getByText(/6.30am/i)).toBeVisible();
+      expect(getByText('6.30am')).toBeVisible();
       expect(queryByTestId('TimeSincePublishing')).toBeFalsy();
     });
   });
@@ -132,14 +132,14 @@ describe('ArticleHeader', () => {
   describe('Different calendar days', () => {
     afterEach(() => MockDate.reset());
 
-    const updated = '2021-12-31T22:30:00Z';
+    const updated = '2021-12-31T22:30:00+00:00';
 
     it('Between 1 and 12 hours after updating', () => {
-      MockDate.set('2022-01-01T02:30:00Z');
+      MockDate.set('2022-01-01T02:30:00+00:00');
 
       const { getByText } = render(<ArticleHeader updated={updated} />);
 
-      expect(getByText(/10.30pm/i)).toBeVisible();
+      expect(getByText('10.30pm')).toBeVisible();
       expect(getByText('4 hours ago')).toBeVisible();
       expect(getByText('December 31')).toBeVisible();
     });
