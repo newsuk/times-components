d1e73c19c986b13c3b228efd50aaba0b60a9f964
Olivia Thetgyi <othetgyi@gmail.com>
2022-06-09 14:36:42 +0100

    feat(TDP-2101): remove navigationMode, update snapshots

--- packages/article-skeleton/showcase-helper.js
+++ packages/article-skeleton/showcase-helper.js
@@ -157,12 +157,6 @@ const renderArticleSkeleton = ({
           )}
           onVideoPress={preventDefaultedAction(decorateAction)("onVideoPress")}
           onViewableItemsChanged={() => null}
-          navigationMode={{
-            isCurrentEdition: false,
-            isPastSixDays: false,
-            isMyArticles: false,
-            isStateless: true
-          }}
         />
       </ContextProviderWithDefaults>
     </MockBookmarksProvider>
