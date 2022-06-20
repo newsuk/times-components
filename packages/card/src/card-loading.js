5518b84c3fc73f15b5b003325d452792f9c4e193
Adele Kufour <adele.kufour@news.co.uk>
2022-06-20 15:55:12 +0100

    fixing ui issue, updating snapshots

--- packages/card/src/card-loading.js
+++ packages/card/src/card-loading.js
@@ -4,9 +4,9 @@ import styles from "./styles";
 
 export default () => (
   <Fragment>
-    <Gradient degrees={264} style={{...styles.headerContainer}} />
-    <Gradient degrees={267} style={{...styles.textContainer}} />
-    <Gradient degrees={267} style={{...styles.textContainer}} />
+    <Gradient degrees={264} style={styles.headerContainer} />
+    <Gradient degrees={267} style={styles.textContainer} />
+    <Gradient degrees={267} style={styles.textContainer} />
     <Gradient degrees={267} style={{...styles.textContainer, ...styles.lastBar}} />
   </Fragment>
 );
