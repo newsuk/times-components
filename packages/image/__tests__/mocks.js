5b774c9d90da3b44b880ceea4ddea01b9112af94
Adam Osborne <me@adamosborne.com>
2022-07-05 16:01:45 +0100

    feat(TDP-1422): remove react native from svgs
    which ultimately means the removal of the svgs package as was only there to produce RN svgs

--- packages/image/__tests__/mocks.js
+++ packages/image/__tests__/mocks.js
@@ -1,6 +1,3 @@
 jest.mock("@times-components/gradient", () => ({
   OverlayGradient: "OverlayGradient"
 }));
-
-// eslint-disable-next-line global-require
-jest.mock("@times-components/svgs", () => require("./mock-svg"));
