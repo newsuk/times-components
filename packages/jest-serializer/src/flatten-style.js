78b8eabfcd97a379e1b86188550ae6ab8c1b6e16
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-07 10:25:21 +0100

    TDP-1704 Removed react-native from jest-serializer

--- packages/jest-serializer/src/flatten-style.js
+++ packages/jest-serializer/src/flatten-style.js
@@ -1,19 +1,15 @@
-import { StyleSheet } from "react-native-web";
 import traverse from "./traverse";
 import print from "./printers";
 
 export const flattenStyleTransform = (accum, node, props, children) => {
   const { style: styles, ...other } = props;
-  const flattened = StyleSheet.flatten(styles);
-  const style = Object.keys(flattened || {}).length ? { style: flattened } : {};
 
   return {
     accum,
     children,
     node,
     props: {
-      ...other,
-      ...style
+      ...other
     }
   };
 };
