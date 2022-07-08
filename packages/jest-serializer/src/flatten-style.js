4aaa85d096e343578e7ece2952ea8a6791d7f02e
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-08 13:11:46 +0100

    TDP-1704 Removed flatten completely and updated test

--- packages/jest-serializer/src/flatten-style.js
+++ packages/jest-serializer/src/flatten-style.js
@@ -1,11 +1,9 @@
 import traverse from "./traverse";
 import print from "./printers";
-import flatten from "lodash.flatten";
 
 export const flattenStyleTransform = (accum, node, props, children) => {
   const { style: styles, ...other } = props;
-  const flattened = flatten(styles);
-  const style = Object.keys(flattened || {}).length ? { style: flattened } : {};
+  const style = props.style;
 
   return {
     accum,
