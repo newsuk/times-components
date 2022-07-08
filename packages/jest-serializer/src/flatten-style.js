0f175e49b42b30a89fdfcc3519b3a82c548f8096
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-08 11:45:27 +0100

    TDP-1704 Replaced stylesheet.flatten with lodash.flatten

--- packages/jest-serializer/src/flatten-style.js
+++ packages/jest-serializer/src/flatten-style.js
@@ -1,8 +1,11 @@
 import traverse from "./traverse";
 import print from "./printers";
+import flatten from "lodash.flatten";
 
 export const flattenStyleTransform = (accum, node, props, children) => {
   const { style: styles, ...other } = props;
+  const flattened = flatten(styles);
+  const style = Object.keys(flattened || {}).length ? { style: flattened } : {};
 
   return {
     accum,
