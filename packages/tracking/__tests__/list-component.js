52064de1e2ad2d56b1b67a0bff0bd8ce060a000e
Adele Kufour <adele.kufour@news.co.uk>
2022-07-11 15:14:29 +0100

    formatting

--- packages/tracking/__tests__/list-component.js
+++ packages/tracking/__tests__/list-component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text} from "react-native";
+import { Text } from "react-native";
 import { TcFlatList } from "@times-components/utils";
 import PropTypes from "prop-types";
 
