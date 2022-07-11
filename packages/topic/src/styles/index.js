85cd12eaf132bf9f410d154a2e0400cea87a20b1
Adele MA² <adele.kufour@news.co.uk>
2022-07-11 17:12:52 +0100

    feat(TDP-1715): remove react native from topics (#3023)
    * removingtopic
    
    * updating tests & linting
    
    * minor
    
    * removingtopic
    
    * updating tests & linting
    
    * minor
    
    * updating storybook and ts-styleguide

--- packages/topic/src/styles/index.js
+++ packages/topic/src/styles/index.js
@@ -1,13 +1,12 @@
-import { StyleSheet } from "react-native";
 import { spacing } from "@times-components/ts-styleguide";
 import sharedStyles from "./shared";
 
-const styles = StyleSheet.create({
+const styles = {
   ...sharedStyles,
   description: {
     ...sharedStyles.description,
     paddingBottom: spacing(3)
   }
-});
+};
 
 export default styles;
