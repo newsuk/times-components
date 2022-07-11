aa9cafc9bf5b444f3ce6a8e39de71126d672e570
Adele Kufour <adele.kufour@news.co.uk>
2022-07-11 15:43:04 +0100

    adding prop types

--- packages/utils/src/tc-flat-list.js
+++ packages/utils/src/tc-flat-list.js
@@ -1,41 +1,27 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const TcFlatList = ({
-  data,
-  initialNumToRender,
-  RenderItem,
-  ListHeaderComponent,
-  ItemSeparatorComponent,
-  misc,
-  style
-}) => {
-
+const TcFlatList = ({ data, initialNumToRender, RenderItem }) => {
   if (Array.isArray(data)) {
     const render = data.map((v, i) => {
       if (i < initialNumToRender) {
-      return (
-      
-      <div role="listitem">
-      
-     { RenderItem({ data: v, key: i })}
-{data.length != data[i + 1] && ItemSeparatorComponent({leadingItem: v},misc.editionBreakpoint ) }
-      </div>
-      )
+        return <div role="listitem">{RenderItem({ data: v, key: i })}</div>;
       }
+      return null;
     });
 
     if (render.length) {
-      return (
-        <div role="list" style={style ? style : {}}>
-          {ListHeaderComponent || null}
-          {render}
-          </div>
-        );
-      }
-  
+      return <div role="list">{render}</div>;
     }
+  }
 
-    return null;
-  };
+  return null;
+};
+
+TcFlatList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.element).isRequired,
+  initialNumToRender: PropTypes.number.isRequired,
+  RenderItem: PropTypes.element.isRequired
+};
 
 export default TcFlatList;
