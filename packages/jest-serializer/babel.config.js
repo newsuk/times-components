c79cdfe2875d01d48c638fd256f0dced8d5c3dfb
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-13 17:00:18 +0100

    TDP-1704 Reverted updated babel config

--- packages/jest-serializer/babel.config.js
+++ packages/jest-serializer/babel.config.js
@@ -1,9 +1,7 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: ["@babel/plugin-proposal-object-rest-spread",
-              "@babel/plugin-transform-react-display-name",
-              "@babel/plugin-transform-flow-strip-types"],
-    presets: ["@babel/preset-react"]
+    plugins: ["@babel/plugin-proposal-object-rest-spread"],
+    presets: ["module:metro-react-native-babel-preset"]
   };
 };
