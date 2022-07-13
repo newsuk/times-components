8bab5d8d75ac3444a4e107cc7dbe245352beec0b
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-13 16:39:41 +0100

    TDP-1704 Removed remaining react-native references and updated babel config

--- packages/jest-serializer/babel.config.js
+++ packages/jest-serializer/babel.config.js
@@ -1,7 +1,9 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: ["@babel/plugin-proposal-object-rest-spread"],
-    presets: ["module:metro-react-native-babel-preset"]
+    plugins: ["@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-transform-react-display-name",
+              "@babel/plugin-transform-flow-strip-types"],
+    presets: ["@babel/preset-react"]
   };
 };
