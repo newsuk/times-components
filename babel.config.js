657c760d67fe59de18d87e78fd1ed341bdd44113
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-18 14:50:56 +0100

    TDP-1712 Fixed small package.json issue

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
 module.exports = api => {
   api.cache(true);
   return {
-    presets: ["module:metro-react-native-babel-preset"],
     plugins: [
       "babel-plugin-styled-components",
-      "@babel/plugin-transform-react-display-name",
       "@babel/plugin-transform-runtime",
       "@babel/plugin-proposal-export-default-from"
-    ]
+    ],
+    presets:["@babel/preset-flow", "@babel/preset-react"]
   };
 };
