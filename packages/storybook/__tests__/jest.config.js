47b7bc2f1f6ea14963f99f057a7adf27ce4b441f
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-19 23:07:44 +0100

    TDP-1712 Updated storybook

--- packages/storybook/__tests__/jest.config.js
+++ packages/storybook/__tests__/jest.config.js
@@ -1,4 +1,4 @@
-const jestConfigurator = require("@times-components/jest-configurator").default;
+const jestConfigurator = require("@times-components/jest-configurator-web").default;
 
 module.exports = jestConfigurator(null, __dirname, [
   "decorators.js",
