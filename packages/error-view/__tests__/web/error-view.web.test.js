0ee7157512775f3cfd2ccdaaf6c07d12073bd623
Bibi Collins <bibivictoria.collins@news.co.uk>
2022-07-19 13:14:11 +0100

    Update jest-configurator-web version in packages, remove react-native from error-view package

--- packages/error-view/__tests__/web/error-view.web.test.js
+++ packages/error-view/__tests__/web/error-view.web.test.js
@@ -1,7 +1,3 @@
-import { AppRegistry } from "react-native-web";
-import { addSerializers, minimalRnw } from "@times-components/jest-serializer";
 import shared from "../shared";
-
-addSerializers(expect, minimalRnw(AppRegistry));
 
 shared();
