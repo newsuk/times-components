b32d2182ce30aaf1323cf735d68cf1c5decffc05
Adam Osborne <me@adamosborne.com>
2022-07-21 16:52:19 +0100

    feat(TDP-1712): updated jest config in articleExtras

--- packages/article-extras/__tests__/web/jest.config.js
+++ packages/article-extras/__tests__/web/jest.config.js
@@ -1,3 +1,12 @@
-const jestConfigurator = require("@times-components/jest-configurator").default;
+const jestConfigurator = require("@times-components/jest-configurator-web")
+  .default;
 
-module.exports = jestConfigurator("web", __dirname);
+const jc = jestConfigurator(__dirname);
+jc.preset = "ts-jest";
+jc.globals = {
+  "ts-jest": {
+    tsConfigFile: "./tsconfig.jest.json"
+  }
+};
+
+module.exports = jc;
