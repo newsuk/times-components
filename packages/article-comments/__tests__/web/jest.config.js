2b4cebb0f9451281bf8f1e73ab3a451123d4439f
Adam Osborne <me@adamosborne.com>
2022-07-21 16:44:22 +0100

    feat(TDP-1712): updated jest config for articleComments

--- packages/article-comments/__tests__/web/jest.config.js
+++ packages/article-comments/__tests__/web/jest.config.js
@@ -1,3 +1,13 @@
-const jestConfigurator = require("@times-components/jest-configurator").default;
+const jestConfigurator = require("@times-components/jest-configurator-web")
+  .default;
 
-module.exports = jestConfigurator("web", __dirname);
+  module.exports = jestConfigurator("web", __dirname);
+  const jc = jestConfigurator(__dirname);
+  jc.preset = "ts-jest";
+  jc.globals = {
+    "ts-jest": {
+      tsConfigFile: "./tsconfig.jest.json"
+    }
+  };
+  
+  module.exports = jc;
