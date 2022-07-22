82604dcaf473c911f05a1fb7e66d60b3970f3bb4
Bibi Collins <bibivictoria.collins@news.co.uk>
2022-07-22 11:20:09 +0100

    Revert related articles jest-configurator change

--- packages/related-articles/babel.config.js
+++ packages/related-articles/babel.config.js
@@ -2,18 +2,9 @@ module.exports = api => {
   api.cache(true);
   return {
     plugins: [
-      "babel-plugin-styled-components"
+      "babel-plugin-styled-components",
+      "@babel/plugin-transform-react-display-name"
     ],
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            esmodules: true,
-          },
-        },
-      ],
-      "@babel/preset-react"
-    ], 
-};
+    presets: ["module:metro-react-native-babel-preset"]
+  };
 };
