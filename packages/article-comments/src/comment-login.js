032df92fb01d4e18fa0c0c41dda46862e3055c20
Adele Kufour <adele.kufour@news.co.uk>
2022-07-25 13:37:26 +0100

    fixing tests

--- packages/article-comments/src/comment-login.js
+++ packages/article-comments/src/comment-login.js
@@ -13,31 +13,28 @@ const loginRequest = (url, completeSSOCallback) => {
   xhr.send();
 };
 
-const isFeatureFlagEnabled = window.location.search.includes('enableRealNameCommenting');
-const loginRequestUrl = isFeatureFlagEnabled ? '/api/comments/loginv2' : '/api/comments/login'
-
-
-const ssoCallbackReadOnly = (codeA, completeSSOCallback) =>
-  loginRequest(
-    `/api/comments/loginv2?codeA=${encodeURIComponent(codeA)}&readOnly=true`,
-    completeSSOCallback
+const ssoCallback = (codeA, completeSSOCallback) => {
+  const isFeatureFlagEnabled = window.location.search.includes(
+    "enableRealNameCommenting"
   );
 
-const ssoCallback = (codeA, completeSSOCallback) =>  loginRequest(
-    `${loginRequestUrl}/loginv2?codeA=${encodeURIComponent(codeA)}`,
+  const loginRequestUrl = isFeatureFlagEnabled
+    ? "/api/comments/loginv2"
+    : "/api/comments/login";
+
+  return loginRequest(
+    `${loginRequestUrl}?codeA=${encodeURIComponent(codeA)}`,
     completeSSOCallback
-  )
+  );
+};
 
 const executeSSOtransaction = (isReadOnly, callback) => {
   if (window.SPOTIM && window.SPOTIM.startSSO) {
-    if (isReadOnly) {
-      window.SPOTIM.startSSO(ssoCallbackReadOnly);
-    } else {
-      window.SPOTIM.startSSO(ssoCallback);
-   }
+    window.SPOTIM.startSSO(ssoCallback);
 
     callback();
   }
 };
 
+export { ssoCallback };
 export default executeSSOtransaction;
