5f78eae7760263ad278e29e9ef7072d6dacfc3dc
Bibi Collins <44647540+bibicollins1@users.noreply.github.com>
2022-07-25 10:50:14 +0100

    feat(TDP-1712): Update packages to use new jest configurator and remove react-native (#3037)
    * Use new jest-configurator-web in date-publication package
    
    * Remove react-native from interactive-wrapper, markup, provider and responsive packages
    
    * Fix linting
    
    * Update date-publication to use latest jest-configurator-web
    
    * Remove react-native presets from depend package
    
    * Add target to babel config in packages with regenerator runtime error
    
    * Update yarn.lock
    
    * Update snapshots
    
    * Add new jest configurator to jest config
    
    * Revert "Update snapshots"
    
    This reverts commit 08a2e6431ad4658230e39f5cf8e46d654617df15.
    
    * Add new jest configurator to interactive-wrapper
    
    * Revert change to interactive-wrapper
    
    * Revert related articles jest-configurator change
    
    * Update jest-configurator-web version
    
    * Fix linting

---
+++ packages/utils/__tests__/web/media-aspect-ratio.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import {
+  addSerializers,
+  compose,
+  minimaliseTransform,
+  minimalNativeTransform,
+  print
+} from "@times-components/jest-serializer";
+import TcText from "../../src/tc-text";
+import AspectRatioContainer from "../../src/media-aspect-ratio";
+
+addSerializers(
+  expect,
+  compose(
+    print,
+    minimalNativeTransform,
+    minimaliseTransform((value, key) => key !== "style")
+  )
+);
+
+describe("AspectRatioContainer should", () => {
+  it("provide an appropriate container with childen based on a given aspect ratio", () => {
+    const testInstance = TestRenderer.create(
+      <AspectRatioContainer aspectRatio="16:9">
+        <TcText>Here are some children</TcText>
+      </AspectRatioContainer>
+    );
+    expect(testInstance).toMatchSnapshot();
+  });
+});
