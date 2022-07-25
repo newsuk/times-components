86c715798ddb468d31a75a68fad8b431bbe19803
Bibi Collins <bibivictoria.collins@news.co.uk>
2022-07-25 16:45:46 +0100

    Create setupTests enzyme adapter file. remove jest-configurator-web from article-image

--- packages/article-byline/__tests__/web/article-byline-with-links.web.test.js
+++ packages/article-byline/__tests__/web/article-byline-with-links.web.test.js
@@ -1,4 +1,5 @@
 import { ArticleBylineWithLinks } from "../../src/article-byline";
 import shared from "../shared";
+import '../../../../setupTests';
 
 shared(ArticleBylineWithLinks);
--- packages/article-image/__tests__/web/article-image.web.test.js
+++ packages/article-image/__tests__/web/article-image.web.test.js
@@ -1,7 +1,4 @@
 import shared from "../shared";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
+import '../../../../setupTests';
 
 shared();
