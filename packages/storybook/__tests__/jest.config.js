230009d833f7fbbe6d622e84f12668a3b8dc5dfa
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-25 12:41:57 +0100

    TDP-1712 Added missing updates

--- packages/storybook/__tests__/jest.config.js
+++ packages/storybook/__tests__/jest.config.js
@@ -1,10 +1,7 @@
 const jestConfigurator = require("@times-components/jest-configurator-web")
   .default;
 
-const jc = jestConfigurator(__dirname, [
-  "decorators.js",
-  "storybook.js"
-]);
+const jc = jestConfigurator(__dirname, ["decorators.js", "storybook.js"]);
 jc.preset = "ts-jest";
 jc.globals = {
   "ts-jest": {
