b9ecfd074c31113d8b1c593d13b3385f679c15e9
Adam Osborne <me@adamosborne.com>
2022-07-26 13:40:00 +0100

    feat(TDP-1712): updated jest config

--- packages/jest-configurator-web/src/jest-configurator.js
+++ packages/jest-configurator-web/src/jest-configurator.js
@@ -2,7 +2,7 @@ import findNodeModules from "find-node-modules";
 import path from "path";
 
 export default (cwd, options = {}) => {
-  const { coverageIgnoreGlobs = [], setupFilesAfterEnv } = options;
+  const { setupFilesAfterEnv } = options;
   const [local, global] = findNodeModules(cwd);
   const module = path.resolve(cwd, local.replace("node_modules", ""));
   const rootDir = path.resolve(
@@ -10,38 +10,23 @@ export default (cwd, options = {}) => {
     (global || local).replace("node_modules", "")
   );
 
-  const platformPath = "web";
-
   const config = {
     moduleFileExtensions: ["ts", "tsx", "graphql", "js", "json"],
     moduleNameMapper: {
       "\\.(png)$": "identity-obj-proxy"
     },
     testEnvironment: "jsdom",
-    collectCoverageFrom: [path.join(rootDir, module)],
-    coverageDirectory: path.join(module, "coverage", platformPath),
-    coveragePathIgnorePatterns: coverageIgnoreGlobs,
-    modulePathIgnorePatterns: [
-      "node_modules/redbox-react/node_modules/react/",
-      "node_modules/@storybook/"
-    ],
+    modulePathIgnorePatterns: ["node_modules/@storybook/"],
     rootDir,
-    setupFiles: [
-      path.resolve(__dirname, "../setup-jest.js"),
-      "jest-plugin-context/setup"
-    ],
+    setupFiles: [path.resolve(__dirname, "../setup-jest.js")],
     setupFilesAfterEnv: setupFilesAfterEnv ? [setupFilesAfterEnv] : [],
-    testMatch: [`${module}/__tests__/${platformPath}/*.test.js`],
-    testPathIgnorePatterns: [
-      path.join(module, "__tests__", platformPath, "jest.config.js")
-    ],
+    testMatch: [`${module}/__tests__/**/*.test.js`],
+    testPathIgnorePatterns: [path.join(module, "__tests__", "jest.config.js")],
     testURL: "http://localhost",
     transform: {
-      "^.+\\.js$": path.resolve(__dirname, "source-loader.js"),
-      // used to make jest understand graphql files once they're loaded
       "\\.(gql|graphql)$": "jest-transform-graphql",
       "^.+\\.graphql": "babel-jest",
-      ".+\\.tsx?$": "ts-jest"
+      "^.+\\.[jt]sx?$": "babel-jest"
     },
     transformIgnorePatterns: [
       "node_modules/(?!(@times-components|@storybook/react)/)"
