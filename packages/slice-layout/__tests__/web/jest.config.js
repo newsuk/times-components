6a1215891326200c1dd5385e6e1a8cc004a8e75b
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-26 23:05:53 +0100

    TDP-1712 Updated slice-layout tests

--- packages/slice-layout/__tests__/web/jest.config.js
+++
@@ -1,5 +0,0 @@
-const jestConfigurator = require("@times-components/jest-configurator").default;
-
-module.exports = jestConfigurator("web", __dirname, {
-  coverageIgnoreGlobs: ["index.js", "slice-layout.js", "styles.js"]
-});
