b9ea049f013a31df12b692b2fa72681da7f0d301
Bibi Collins <44647540+bibicollins1@users.noreply.github.com>
2022-07-27 12:27:51 +0100

    feat(TDP-1712): update packages to use new jest config web (#3056)
    * Update date-publication to use latest jest-configurator-web
    
    * Remove react-native from error-view and use latest jest-configurator-web
    
    * Use latest jest-configurator-web in gradient
    
    * Use latest jest-configurator-web in icons
    
    * Use latest jest-configurator-web package in image
    
    * Update key-facts to use latest jest-configurator-web
    
    * Update lazy-load to use latest jest-configurator-web
    
    * Update link package to use latest jest-configurator-web
    
    * Update markup to use new jest-configurator-web
    
    * Update markup-forest babel config to not use react-native
    
    * Fix markup-forest linting
    
    * Update message-bar to use latest jest-configurator-web
    
    * Update babel config in mock-tpa-server to not use react native
    
    * Update pagination to use latest jest-configurator-web
    
    * Update pull-quote with latest jest-configurator-web
    
    * Update related-articles to use latest jest-configurator-web
    
    * Update responsive package to use jest-configurator-web
    
    * Update storybook for error-view

--- packages/error-view/__tests__/web/error-view.web.test.js
+++ packages/error-view/__tests__/web/error-view.web.test.js
@@ -1,7 +1,6 @@
-import { AppRegistry } from "react-native-web";
-import { addSerializers, minimalRnw } from "@times-components/jest-serializer";
+import { addSerializers } from "@times-components/jest-serializer";
 import shared from "../shared";
 
-addSerializers(expect, minimalRnw(AppRegistry));
+addSerializers(expect);
 
 shared();
