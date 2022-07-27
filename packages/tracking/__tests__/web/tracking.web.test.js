86ba38a6dc6ecc901496fe1070d6be6f39e472f3
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-27 11:27:25 +0100

    TDP-1712 Updated tracking package

--- packages/tracking/__tests__/web/tracking.web.test.js
+++ packages/tracking/__tests__/web/tracking.web.test.js
@@ -1,7 +1,7 @@
-import tracking from "../tracking.test";
-import trackEvents from "../track-events.test";
-import trackingContext from "../tracking-context.test";
-import resolveAttrs from "../resolve-attrs.test";
+import tracking from "../tracking-test";
+import trackEvents from "../track-events-test";
+import trackingContext from "../tracking-context-test";
+import resolveAttrs from "../resolve-attrs-test";
 
 describe("Tracking tests on web", () => {
   tracking();
