0a4663005cd4c0109761b47f01b8bd2b1d8c5d57
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-27 11:52:32 +0100

    TDP-1712 Updated ts-* packages

--- packages/ts-styleguide/babel.config.js
+++ packages/ts-styleguide/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: [
-      'babel-plugin-styled-components',
-      '@babel/plugin-transform-react-display-name'
-    ],
-    presets: ['@babel/preset-react']
+    plugins: ["babel-plugin-styled-components"],
+    presets: ["@babel/preset-env", "@babel/preset-react"]
   };
 };
