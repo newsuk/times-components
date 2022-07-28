a3cf04c29b7cf163c3e65b20ad61cd370f1dfb8b
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-28 13:05:31 +0100

    TDP-1712 Resolved jest.config.js location issues

---
+++ packages/article-list/__tests__/jest.config.js
@@ -0,0 +1,5 @@
+const jestConfigurator = require("@times-components/jest-configurator").default;
+
+module.exports = jestConfigurator("web", __dirname, {
+  coverageIgnoreGlobs: ["article-list-item-prop-types.js"]
+});
