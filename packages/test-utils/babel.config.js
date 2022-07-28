a25538717fc03dc689cef82905933122f39e2a4b
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-07-28 10:05:06 +0100

    TDP-1712 Refactored tes-utils

--- packages/test-utils/babel.config.js
+++ packages/test-utils/babel.config.js
@@ -1,7 +1,15 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: ["babel-plugin-styled-components"],
-    presets: ["@babel/preset-env", "@babel/preset-react"]
+    plugins: ["babel-plugin-styled-components", '@babel/plugin-transform-runtime'],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          "targets": {
+            "node": "current"
+          }
+        }
+      ], "@babel/preset-react"]
   };
 };
