707ce80e5210b42a0927b3ff0eb6ec0421a7a5d0
adamosborne-tnl <89213300+adamosborne-tnl@users.noreply.github.com>
2022-07-28 11:00:50 +0100

    feat(TPD-1712): updated jest config  (#3057)
    * feat(TDP-1712): updated article-byline to version 0.7.0 of jest-config-web
    
    * updated article to jcw. 0.7
    
    * updated article-comments for jcw to 0.7.0
    
    * updated article-extras to use jcw 0.7.0
    
    * updated article-image to use jest config web 0.7.0
    
    * updated article-lead-asset to use jest-conf-web 0.7.0
    
    * updated article-main-comment to use jcw 0.7.0
    
    * updated article-main-standard to JCW 0.7.0
    
    * updated article-main-standard to JCW 0.7.0
    
    * updated article-topics to use jest-config-web 0.7.0
    
    * updated author-profile to use jest-config-web 0.7.0
    
    * updated Button to use JCW 0.7.0
    
    * updated Caption to use jest-config-web 0.7.0
    
    * removed unneccesary if as style doesn't exist
    
    * updated card to use new jest-config-web 0.7.0
    
    * updated context to use jest-config-web 0.7.0
    
    * updated article-in-depth to use JCW 0.7.0
    
    * updated article-magazine-comment to use jest config 0.7.0
    
    * updated article-magazine-standard to use jest-config-web 0.7.0
    
    * updated article-paragraph to use jest-config-web 0.7.0 and some native removal
    
    * updated article-summary to use JCW 0.7.0
    
    * updated article-skeleton to use jest-config-web 0.7.0 and some react-native removal
    
    * update for tsconfig to use es2020 to be able to test for optional chaining
    
    * bumping node to match cps-content-render version
    
    * changed docker image node to 14.5.0 as 16 wouldn't run fontforge
    
    * another node version to try as last one wasn't available on host
    
    * had to simplify this config as circleCi wasn't allow us to use node 14+ so that optional chaining was ok, and because the eslint ignore was being ignored
    
    * updated snapshots
    
    * updated snapshots
    
    * update snapshot
    
    * re-added NativeAd and updated snapshots
    
    * updated snapshot for cypress

--- packages/article-paragraph/__tests__/mock-text-measure-module.js
+++
@@ -1,7 +0,0 @@
-jest.mock("react-native", () => {
-  const rn = require.requireActual("react-native");
-  rn.NativeModules.RNTextSize = {
-    measure: () => Promise.resolve({ lineEnd: 24, width: 100 })
-  };
-  return rn;
-});
