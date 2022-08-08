4921472c8e778b7ba88dc2db93a4cfa8da7b8df1
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-08-08 11:30:27 +0100

    TDP-2343 Resolved linting issue

--- packages/provider-test-tools/babel.config.js
+++ packages/provider-test-tools/babel.config.js
@@ -5,6 +5,16 @@ module.exports = api => {
       "@babel/plugin-proposal-class-properties",
       "@babel/plugin-transform-runtime"
     ],
-    presets: ["@babel/preset-env", "@babel/preset-react"]
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            esmodules: true
+          }
+        }
+      ],
+      "@babel/preset-react"
+    ]
   };
 };
