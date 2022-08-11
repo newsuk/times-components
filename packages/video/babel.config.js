28bfb0a567a010865613ccaf0f143227c979080b
Bibi Collins <bibivictoria.collins@news.co.uk>
2022-08-11 13:57:57 +0100

    Add typescript preset to video

--- packages/video/babel.config.js
+++ packages/video/babel.config.js
@@ -5,6 +5,10 @@ module.exports = api => {
       "babel-plugin-styled-components",
       "@babel/plugin-proposal-class-properties"
     ],
-    presets: ["@babel/preset-env", "@babel/preset-react"]
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript"
+    ]
   };
 };
