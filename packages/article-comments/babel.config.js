b190a33bbf88df35d29008482a87a8e0a9a594e6
Darren Mackintosh <darren.mackintosh@news.co.uk>
2022-08-11 13:03:37 +0100

    TDP-2379 Removed unnecessary dependencies from article-comment package

--- packages/article-comments/babel.config.js
+++ packages/article-comments/babel.config.js
@@ -1,7 +1,9 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: ["babel-plugin-styled-components"],
+    plugins: [
+      "babel-plugin-styled-components",
+      "@babel/plugin-proposal-export-default-from"],
     presets: [
       [
         "@babel/preset-env",
@@ -11,7 +13,8 @@ module.exports = api => {
           }
         }
       ],
-      "@babel/preset-react"
+      "@babel/preset-react",
+      "@babel/preset-typescript"
     ]
   };
 };
