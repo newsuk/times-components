6a0c3fd4cbc97566787d95311754b5749367c536
Adele Kufour <adele.kufour@news.co.uk>
2022-08-15 15:26:31 +0100

    linting

--- packages/article-comments/__tests__/web/utils.test.js
+++ packages/article-comments/__tests__/web/utils.test.js
@@ -27,58 +27,58 @@ describe("utils", () => {
     jest.clearAllMocks();
   });
 
-describe("getDisplayNameFromLocalStorage()", () => {
-  it("should return false if user it not signed in", () => {
-    expect(getDisplayNameFromLocalStorage()).toEqual(false);
+  describe("getDisplayNameFromLocalStorage()", () => {
+    it("should return false if user it not signed in", () => {
+      expect(getDisplayNameFromLocalStorage()).toEqual(false);
+    });
+
+    it("should return false if there is no display name", () => {
+      window.localStorage.setItem(
+        "SPOTIM_CURRENT_USER",
+        '{"data":{"id":"u_sgCrqrs7KNLv","imageId":"#Grey-Cactus","username":"JohnSmith750","isRegistered":true}}'
+      );
+      expect(getDisplayNameFromLocalStorage()).toEqual(false);
+    });
+    it("should return the display name ", () => {
+      window.localStorage.setItem(
+        "SPOTIM_CURRENT_USER",
+        '{"data":{"id":"u_sgCrqrs7KNLv","displayName":"John Smith","imageId":"#Grey-Cactus","username":"JohnSmith750","isRegistered":true}}'
+      );
+      expect(getDisplayNameFromLocalStorage()).toEqual("John Smith");
+    });
   });
 
-  it("should return false if there is no display name", () => {
-    window.localStorage.setItem(
-      "SPOTIM_CURRENT_USER",
-      '{"data":{"id":"u_sgCrqrs7KNLv","imageId":"#Grey-Cactus","username":"JohnSmith750","isRegistered":true}}'
-    );
-    expect(getDisplayNameFromLocalStorage()).toEqual(false);
+  describe("userShouldUpdateName()", () => {
+    beforeAll(() => {
+      global.fetch = () =>
+        Promise.resolve({
+          json: () => Promise.resolve(mockFetchResponse)
+        });
+    });
+
+    afterAll(() => {
+      global.fetch = unmockedFetch;
+    });
+    it("it should return false if no username", async () => {
+      const result = await userShouldUpdateName();
+
+      expect(result).toEqual(false);
+    });
+
+    it("it should return false if the username is valid", async () => {
+      mockFetchResponse = { isPseudonym: false };
+
+      const result = await userShouldUpdateName("john");
+
+      expect(result).toEqual(false);
+    });
+
+    it("should set local storage values if they do not already exist and the user is on the banned list", async () => {
+      mockFetchResponse = { isPseudonym: true };
+
+      const result = await userShouldUpdateName("MockBannedName");
+
+      expect(result).toEqual(true);
+    });
   });
-  it("should return the display name ", () => {
-    window.localStorage.setItem(
-      "SPOTIM_CURRENT_USER",
-      '{"data":{"id":"u_sgCrqrs7KNLv","displayName":"John Smith","imageId":"#Grey-Cactus","username":"JohnSmith750","isRegistered":true}}'
-    );
-    expect(getDisplayNameFromLocalStorage()).toEqual("John Smith");
-  });
-});
-
-describe("userShouldUpdateName()", () => {
-  beforeAll(() => {
-    global.fetch = () =>
-      Promise.resolve({
-        json: () => Promise.resolve(mockFetchResponse)
-      });
-  });
-
-  afterAll(() => {
-    global.fetch = unmockedFetch;
-  });
-  it("it should return false if no username", async () => {
-    const result = await userShouldUpdateName();
-
-    expect(result).toEqual(false);
-  });
-
-  it("it should return false if the username is valid", async () => {
-    mockFetchResponse = { isPseudonym: false };
-
-    const result = await userShouldUpdateName("john");
-
-    expect(result).toEqual(false);
-  });
-
-  it("should set local storage values if they do not already exist and the user is on the banned list", async () => {
-    mockFetchResponse = { isPseudonym: true };
-
-    const result = await userShouldUpdateName("MockBannedName");
-
-    expect(result).toEqual(true);
-  });
-});
 });
