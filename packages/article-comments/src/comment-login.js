7e9c2db20297fd512a0b9cd870112f2030c57853
Callum Christie <callumchristie@hotmail.com>
2022-09-08 15:59:24 +0100

    feat(TDP-000): Commenting re-auth testing (#3148)
    * faet(TDP-000): Commenting re-auth testing
    
    * faet(TDP-000): Commenting re-auth testing

--- packages/article-comments/src/comment-login.js
+++ packages/article-comments/src/comment-login.js
@@ -1,23 +1,34 @@
 /* eslint-env browser */
 
 const loginRequest = (url, completeSSOCallback) => {
+  console.log("loginRequest", 1);
   const xhr = new XMLHttpRequest();
+  console.log("loginRequest", 2);
   xhr.addEventListener("load", () => {
+    console.log("loginRequest", 6);
     const success = xhr.status === 200;
+    console.log("loginRequest", 7);
     const { spotimCodeB } = success ? JSON.parse(xhr.response) : {};
+    console.log("loginRequest", 8);
     if (spotimCodeB) {
+      console.log("loginRequest", 9);
       completeSSOCallback(spotimCodeB);
     }
   });
+  console.log("loginRequest", 3);
   xhr.open("GET", url);
+  console.log("loginRequest", 4);
   xhr.send();
+  console.log("loginRequest", 5);
 };
 
-const ssoCallback = (codeA, completeSSOCallback) =>
+const ssoCallback = (codeA, completeSSOCallback) => {
+  console.log("ssoCallback");
   loginRequest(
     `/api/comments/loginv2?codeA=${encodeURIComponent(codeA)}`,
     completeSSOCallback
   );
+};
 
 const setNewUserToken = () => {
   window.localStorage.setItem("isUsingRealNameCommenting", true);
