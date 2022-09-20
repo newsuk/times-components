ed2ff6958c5206be9364342e90e4175273dd5699
Adele Kufour <adele.kufour@news.co.uk>
2022-09-20 12:07:00 +0100

    fixing build and test errors

--- packages/caption/babel.config.js
+++ packages/caption/babel.config.js
@@ -1,9 +1,17 @@
 module.exports = api => {
   api.cache(true);
   return {
-    plugins: ["babel-plugin-styled-components"],
+    plugins: [
+      "babel-plugin-styled-components",   
+      "@babel/plugin-proposal-export-default-from"
+    ],
     presets: [
-      "@babel/preset-env",
+     [ "@babel/preset-env",
+     {
+       targets: {
+         esmodules: true
+       }
+     }],
       "@babel/preset-react",
       "@babel/preset-typescript"
     ]
