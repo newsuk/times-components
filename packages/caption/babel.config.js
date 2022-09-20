980413e7905d8a6047600b706a3bd7b26aeaccaf
Adele Kufour <adele.kufour@news.co.uk>
2022-09-20 12:52:09 +0100

    snapshots, undoing card

--- packages/caption/babel.config.js
+++ packages/caption/babel.config.js
@@ -2,16 +2,18 @@ module.exports = api => {
   api.cache(true);
   return {
     plugins: [
-      "babel-plugin-styled-components",   
+      "babel-plugin-styled-components",
       "@babel/plugin-proposal-export-default-from"
     ],
     presets: [
-     [ "@babel/preset-env",
-     {
-       targets: {
-         esmodules: true
-       }
-     }],
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            esmodules: true
+          }
+        }
+      ],
       "@babel/preset-react",
       "@babel/preset-typescript"
     ]
