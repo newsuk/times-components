84d198882c3b995b3b5d14eeb6744c5e76cd1488
Adele Kufour <adele.kufour@news.co.uk>
2022-10-13 10:22:15 +0100

    style ammendments

--- packages/ts-components/src/helpers/text-formatting/CapitaliseFirstCharacter.ts
+++
@@ -1,9 +0,0 @@
-export const capitaliseFirstCharacter = (stringToCapitalise?: string) => {
-  if (!stringToCapitalise) {
-    return;
-  }
-
-  const firstCharacter = stringToCapitalise.charAt(0);
-  const restOfString = stringToCapitalise.split(firstCharacter);
-  return `${firstCharacter.toUpperCase()}${restOfString[1]}`;
-};
