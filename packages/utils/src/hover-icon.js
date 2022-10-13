61c3679c5be82a2fd94ea55cfd90f3df7c36d430
Adele Kufour <adele.kufour@news.co.uk>
2022-10-13 10:59:27 +0100

    linting

--- packages/utils/src/hover-icon.js
+++ packages/utils/src/hover-icon.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const HoverIcon = styled.div`
   color: ${props => props.colour};
-  text-decoration-line: ${props => props.underline && 'underline'};
+  text-decoration-line: ${props => props.underline && "underline"};
   &:hover {
     color: ${props => props.hoverColour || props.colour};
-    text-decoration-line: ${props => props.underline && 'none'};
+    text-decoration-line: ${props => props.underline && "none"};
   }
 `;
 
