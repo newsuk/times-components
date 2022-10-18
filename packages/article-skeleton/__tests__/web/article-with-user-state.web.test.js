d2c240d82304500aef026571f0186303f73888e6
Callum <callumchristie@hotmail.com>
2022-10-18 15:36:52 +0100

    fix(TDP-2612): User state refactor

--- packages/article-skeleton/__tests__/web/article-with-user-state.web.test.js
+++ packages/article-skeleton/__tests__/web/article-with-user-state.web.test.js
@@ -315,10 +315,8 @@ const renderArticle = () => (
 
 describe("Article with user state", () => {
   it("Render full article when user has access to full article", () => {
-    UserState.mockStates = [
-      UserState.showSaveAndShareBar,
-      UserState.showArticleExtras
-    ];
+    UserState.mockStates = [UserState.showArticleExtras];
+
     const output = TestRenderer.create(renderArticle());
 
     expect(output).toMatchSnapshot();
