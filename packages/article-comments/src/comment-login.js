47374806474ef32c68eb45c8f477541ead3067c8
Callum Christie <callumchristie@hotmail.com>
2022-12-09 13:25:14 +0000

    feat(TDP-000): commenting reauth - ready for switch over to v2 (#3155)
    Co-authored-by: Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>

--- packages/article-comments/src/comment-login.js
+++ packages/article-comments/src/comment-login.js
@@ -28,17 +28,7 @@ const executeSSOtransaction = () => {
       window.nuk.getCookieValue &&
       window.nuk.getCookieValue("acs_tnl");
 
-    let cpn = getCpnId(acsTnlCookie);
-
-    if (window.location.search.includes("enableRealNameReauthentication")) {
-      cpn = `${cpn}_v2`;
-
-      if (!window.localStorage.getItem("isUsingRealNameCommentingV2")) {
-        window.localStorage.removeItem("SPOTIM_DEVICE_V2");
-        window.localStorage.removeItem("SPOTIM_ACCESS_TOKEN");
-        window.localStorage.setItem("isUsingRealNameCommentingV2", true);
-      }
-    }
+    const cpn = `${getCpnId(acsTnlCookie)}_v2`;
 
     window.SPOTIM.startSSO({ callback: ssoCallback, userId: cpn });
   }
