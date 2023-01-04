87b33f50e50aa06e17902fd67f30a150e0070e7f
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-01-04 09:51:35 +0000

    TDP-2934 Updated copy in banner

--- packages/ts-components/src/components/inline-message/InlineMessage.stories.tsx
+++ packages/ts-components/src/components/inline-message/InlineMessage.stories.tsx
@@ -7,7 +7,7 @@ storiesOf('Typescript Component/InlineMessage', module).add('default', () => {
   const type = select('Level', ['info', 'warning', 'error'], 'info');
   return (
     <InlineMessage title="Real-name comments" type={type}>
-      We&apos;ve changed our policy - if you need to,{' '}
+      Our policy is now for commenters to use real names. You may{' '}
       <a href="https://home.thetimes.co.uk/">edit your screen name here</a>.
     </InlineMessage>
   );
