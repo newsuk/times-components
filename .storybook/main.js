1ac3bddd1e40178b6494fb5f3aa694352dfcfe75
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-01-20 21:01:17 +0000

    feat: add accessibility and docs addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,12 @@
 module.exports = {
-  addons: ['@storybook/addon-options',"@storybook/addon-actions","@storybook/addon-knobs"],
+  addons: [
+    "@storybook/addon-options",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-info",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y"
+  ],
   framework: '@storybook/react',
 };
 
