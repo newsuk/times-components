b7e6699ddc4b3d674d1ef5d259b4fd4ea8dc6fe6
Darren Mackintosh <94699701+DMackintosh-TNL@users.noreply.github.com>
2023-02-07 09:30:42 +0000

    feat(TDP-3135): Remove Blue RNC Banner (#3231)
    * TDP-3135 Removed Blue RNC Banner
    
    * TDP-3135 Updated Snapshots
    
    * TDP-3135 Resolved Snapshot issue
    
    * TDP-3135 Resolved Snapshot issue
    
    * TDP-3135 Resolved Snapshot issue
    
    * TDP-3135 Resolved linting issue

--- packages/article-comments/src/article-comments.js
+++ packages/article-comments/src/article-comments.js
@@ -3,12 +3,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import UserState from "@times-components/user-state";
-import { InlineMessage } from "@times-components/ts-components";
-
 import Comments from "./comments";
-
 import DisabledComments from "./disabled-comments";
-import { CommentContainer } from "./styles/responsive";
 import JoinTheConversationDialog from "./join-the-conversation-dialog";
 
 const ArticleComments = ({
@@ -23,15 +19,6 @@ const ArticleComments = ({
         <JoinTheConversationDialog />
       </UserState>
       <UserState state={UserState.showCommentingModule}>
-        <CommentContainer>
-          <InlineMessage title="Real-name comments" type="info">
-            Our policy is now for commenters to use real names. You may{" "}
-            <a href="https://home.thetimes.co.uk/">
-              edit your screen name here
-            </a>
-            .
-          </InlineMessage>
-        </CommentContainer>
         <Comments
           articleId={articleId}
           isReadOnly={isReadOnly}
