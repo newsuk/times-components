49dee3a88b31c21f24319beaa5a53f5c761605cd
Michael Douglas <michael.douglas1@news.co.uk>
2023-02-07 17:04:22 +0000

    feat(TDP-3009): WIP Top Nav

--- packages/ts-newskit/src/theme/times-web-light/style-presets/index.ts
+++ packages/ts-newskit/src/theme/times-web-light/style-presets/index.ts
@@ -1,9 +1,11 @@
 import { colorStylePresets } from './color';
+import { navigationStylePresets } from './navigation';
 import { pageStylePresets } from './page';
 import { textStylePresets } from './text';
 
 export const stylePresets = {
   ...colorStylePresets,
+  ...navigationStylePresets,
   ...pageStylePresets,
   ...textStylePresets
 };
---
+++ packages/ts-newskit/src/theme/times-web-light/style-presets/navigation.ts
@@ -0,0 +1,37 @@
+export const navigationStylePresets = {
+  topNavItem: {
+    base: {
+      backgroundColor: '{{colors.interface060}}',
+      color: '{{colors.inkInverse}}',
+      height: '60px',
+      fontSize: '{{fontSize.fontSize020}}',
+      iconColor: '{{colors.inkInverse}}',
+    },
+    hover: {
+      backgroundColor: '{{colors.interface020}}',
+      color: '{{colors.inkContrast}}',
+      iconColor: '{{colors.inkContrast}}'
+    }
+  },
+  topNavDivider: {
+    base: {
+    }
+  },
+
+  articleHeading: {
+    base: {
+      color: '{{colors.inkContrast}}'
+    }
+  },
+  articleParagraph: {
+    base: {
+      marginBottom: '25px',
+      color: '{{colors.inkBase}}'
+    }
+  },
+  cardTeaserParagraph: {
+    base: {
+      color: '{{colors.inkSubtle}}'
+    }
+  }
+};
