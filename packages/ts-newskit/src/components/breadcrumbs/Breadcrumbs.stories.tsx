bf29e5a7827b0c5847a733348404e6192ae6f2a9
Bibi Collins <bibivictoria.collins@news.co.uk>
2023-02-08 15:30:36 +0000

    Remove breadcrumbs

--- packages/ts-newskit/src/components/breadcrumbs/Breadcrumbs.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import NewBreadcrumbs from './Breadcrumbs';
-
-storiesOf('Newskit/Breadcrumbs', module).add('Breadcrumbs', () => <NewBreadcrumbs />);
