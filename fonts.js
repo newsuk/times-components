56ed98b77c7385c8519a91d32979addd21133d3e
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-02-10 10:29:12 +0000

    feat(TDP-3186): add roboto font

--- fonts.js
+++ fonts.js
@@ -13,7 +13,11 @@ const fonts = {
   TimesDigitalW04: () =>
     require("./dist/public/fonts/TimesDigitalW04.js").default,
   "TimesModern-Regular": () =>
-    require("./dist/public/fonts/TimesModern-Regular.js").default
+    require("./dist/public/fonts/TimesModern-Regular.js").default,
+  "Roboto-Medium": () =>
+    require("./dist/public/fonts/Roboto-Medium.js").default,
+  "Roboto-Regular": () =>
+    require("./dist/public/fonts/Roboto-Regular.js").default
 };
 
 const ttf = {};
