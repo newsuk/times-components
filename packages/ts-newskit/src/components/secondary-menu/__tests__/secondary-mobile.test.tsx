945a663c5191be33f08527cf1a026c4db725dbbd
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-02-15 06:11:52 +0000

    Fix linting errors

--- packages/ts-newskit/src/components/secondary-menu/__tests__/secondary-mobile.test.tsx
+++ packages/ts-newskit/src/components/secondary-menu/__tests__/secondary-mobile.test.tsx
@@ -13,54 +13,86 @@ describe('Secondary Menu Mobile', () => {
 
   it('should render the dropdown', () => {
     const { asFragment } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={true} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={true}
+      />
     );
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not render the dropdown', () => {
     const { asFragment } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={false} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={false}
+      />
     );
     expect(asFragment()).toMatchSnapshot();
   });
   it('should render the textblock', () => {
     const { getByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={false} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={false}
+      />
     );
     const title = getByText('Top Stories');
     expect(title).toBeInTheDocument();
   });
   it('should render the dropdown text close', () => {
     const { getByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={true} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={true}
+      />
     );
     const label = getByText('Close');
     expect(label).toBeInTheDocument();
   });
   it('should render the dropdown text See all', () => {
     const { getByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={false} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={false}
+      />
     );
     const label = getByText('See all');
     expect(label).toBeInTheDocument();
   });
   it('should render the dropdown text Home', () => {
     const { getByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={true} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={true}
+      />
     );
     const label = getByText('Home');
     expect(label).toBeInTheDocument();
   });
   it('should not render the dropdown text Home', () => {
     const { queryByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={false} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={false}
+      />
     );
     const label = queryByText('Home');
     expect(label).not.toBeInTheDocument();
   });
   it('should render the dropdown', () => {
     const { queryByText, getByText } = customRender(
-      <SecondaryNavMobile data={mainMenuItems} title="Top Stories" isActive={false} />
+      <SecondaryNavMobile
+        data={mainMenuItems}
+        title="Top Stories"
+        isActive={false}
+      />
     );
     const closeButton = getByText('See all');
     fireEvent.click(closeButton);
