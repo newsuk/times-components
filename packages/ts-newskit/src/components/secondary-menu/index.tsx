61b143849565ae5cc266388139d1b2d3997b5185
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-02-15 05:21:32 +0000

    feat: add breakpoint

--- packages/ts-newskit/src/components/secondary-menu/index.tsx
+++ packages/ts-newskit/src/components/secondary-menu/index.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { SecondaryNavDesktop } from './desktop';
 import { SecondaryNavMobile } from './mobile';
 import { mainMenuItems } from './fixtures/menu-items.json';
+import { Visible } from 'newskit';
+import { SecondaryMenuItem } from './types';
 
-export const SecondaryNav: React.FC<{}> = () => {
+export const SecondaryNav: React.FC<{
+  title: string;
+  isActive: boolean;
+  data: SecondaryMenuItem[];
+}> = ({ title }) => {
   return (
     <>
-      <SecondaryNavDesktop data={mainMenuItems} />
-      <SecondaryNavMobile
-        data={mainMenuItems}
-        title={'Top Stories'}
-        isActive={false}
-      />
+      <Visible sm xs>
+        <SecondaryNavMobile
+          data={mainMenuItems}
+          title={title}
+          isActive={false}
+        />
+      </Visible>
+      <Visible md lg xl>
+        <SecondaryNavDesktop data={mainMenuItems} />
+      </Visible>
     </>
   );
 };
