87c70c3cd0324f0367f338aa573f6d2412daa59e
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-22 11:44:05 +0000

    TDP-3011 - Actioned part of PR feedback

--- packages/ts-newskit/src/components/section-header/SectionHeader.tsx
+++ packages/ts-newskit/src/components/section-header/SectionHeader.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { ThemeProvider } from 'newskit'
-
 import { SectionHeaderText } from './style'
-import { TimesWebLightTheme } from '../../theme';
 
 export const SectionHeaderName: React.FC<{ title: string }> = ( {title} ) => {
   return (
-    <ThemeProvider theme={TimesWebLightTheme}>
-      <SectionHeaderText>{title}</SectionHeaderText>
-    </ThemeProvider>
+    <SectionHeaderText>{title}</SectionHeaderText>
   );
 };
--- packages/ts-newskit/src/components/section-header/index.tsx
+++ packages/ts-newskit/src/components/section-header/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from 'newskit'
-import { SectionHeaderName } from './SectionHeader';
+import { SectionHeaderText } from './style'
 import { TimesWebLightTheme } from '../../theme';
 
 export const SectionHeader: React.FC<{ title: string }> = ( {title} ) => {
   return (
     <ThemeProvider theme={TimesWebLightTheme}>
-      <SectionHeaderName title={title} />
+      <SectionHeaderText>{title}</SectionHeaderText>
     </ThemeProvider>
   );
 };
