57b333f4ab47e25f9491715e2f1cbccec6bd2e62
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-22 13:35:18 +0000

    TDP-3011 - Cleaned up files

--- packages/ts-newskit/src/components/masthead/index.tsx
+++ packages/ts-newskit/src/components/masthead/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { ThemeProvider } from 'newskit'
+import { ThemeProvider, DateTime, LinkInline, ScreenReaderOnly } from 'newskit'
 import { TimesWebLightTheme } from '../../theme';
-import { MainMasthead } from './Masthead';
+import { MainHeader, Masthead } from './style';
+import { SundayTimesMasthead, TimesMasthead } from './assets';
 
 export const EditionMasthead: React.FC<{ isSunday: boolean }> = ( {isSunday} ) => {
+	const todaysDate = (new Date()).toISOString();
 	return (
 		<ThemeProvider theme={TimesWebLightTheme}>
-			<MainMasthead isSunday={isSunday} />
+			<MainHeader>
+				<Masthead>
+					<ScreenReaderOnly id="sr-only"><h1>The Times &amp; The Sunday Times Homepage</h1></ScreenReaderOnly>
+					<DateTime
+						date={todaysDate}
+						dateFormat='EEEE MMMM d yyyy'
+						prefix=''
+						suffix=''
+						overrides={{
+							stylePreset: 'mastheadTime'
+						}}
+					/>
+					<LinkInline href='/' overrides={{stylePreset: 'mastheadLogo' }}>
+						{isSunday ? <SundayTimesMasthead /> : <TimesMasthead />}
+					</LinkInline>
+				</Masthead>
+        	</MainHeader>
 		</ThemeProvider>
 	);
 };
--- packages/ts-newskit/src/components/section-header/SectionHeader.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { SectionHeaderText } from './style'
-
-export const SectionHeaderName: React.FC<{ title: string }> = ( {title} ) => {
-  return (
-    <SectionHeaderText>{title}</SectionHeaderText>
-  );
-};
