c04a24c88453fb7389f37bea392157042e58a35f
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-22 10:02:20 +0000

    TDP-3011 - Got stroybook working

--- packages/ts-newskit/src/components/masthead/Masthead.tsx
+++ packages/ts-newskit/src/components/masthead/Masthead.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
-import { ThemeProvider, DateTime, LinkInline } from 'newskit';
+import { DateTime, LinkInline } from 'newskit';
 import { MainHeader, Masthead, MastheadTitle } from './style';
 
-import { TimesWebLightTheme } from '../../theme';
-import { TimesMasthead } from './assets';
+import { SundayTimesMasthead, TimesMasthead } from './assets';
 
 const todaysDate = (new Date()).toISOString();
 
-export const MainMasthead: React.FC<{}> = () => {
+export const MainMasthead: React.FC<{ isSunday: boolean }> = ( {isSunday} ) => {
+	const ShowMasthead = (isSunday: boolean) => isSunday ? <>{SundayTimesMasthead}</> : <>{TimesMasthead}</>;
 	return (
-        <ThemeProvider theme={TimesWebLightTheme}>
-            <MainHeader>
-                <Masthead>
-                    <MastheadTitle>The Times & The Sunday Times Homepage</MastheadTitle>
-                    <DateTime
-                        date={todaysDate}
-                        dateFormat='EEEE MMMM d yyyy'
-                        prefix=''
-                        suffix=''
-                        overrides={{
-                            stylePreset: 'mastheadTime'
-                        }}
-                    />
-                    <LinkInline href='/' overrides={{stylePreset: 'mastheadLogo' }}>
-                        <TimesMasthead />
-                    </LinkInline>
-                </Masthead>
-            </MainHeader>
-        </ThemeProvider>
+        <MainHeader>
+            <Masthead>
+                <MastheadTitle>The Times & The Sunday Times Homepage</MastheadTitle>
+                <DateTime
+                    date={todaysDate}
+                    dateFormat='EEEE MMMM d yyyy'
+                    prefix=''
+                    suffix=''
+                    overrides={{
+                        stylePreset: 'mastheadTime'
+                    }}
+                />
+                <LinkInline href='/' overrides={{stylePreset: 'mastheadLogo' }}>
+                    {ShowMasthead(isSunday)}
+                </LinkInline>
+            </Masthead>
+        </MainHeader>
 	);
 };
--- packages/ts-newskit/src/components/masthead/index.tsx
+++ packages/ts-newskit/src/components/masthead/index.tsx
@@ -10,3 +10,5 @@ export const EditionMasthead: React.FC<{}> = () => {
 		</NewsKitProvider>
 	);
 };
+
+export default EditionMasthead
