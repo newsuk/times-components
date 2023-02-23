84a2c396058c001ca97d1aefa00a1b1d678a829a
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-23 13:45:32 +0000

    TDP-3011 Resolved Linting issue

--- packages/ts-newskit/src/components/masthead/index.tsx
+++ packages/ts-newskit/src/components/masthead/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { ThemeProvider, DateTime, LinkInline, ScreenReaderOnly, Headline, Visible } from 'newskit';
+import {
+  ThemeProvider,
+  DateTime,
+  LinkInline,
+  ScreenReaderOnly,
+  Headline,
+  Visible
+} from 'newskit';
 import { TimesWebLightTheme } from '../../theme';
 import { MainHeader, Masthead } from './styles';
 import { NewsKitTimesMasthead, NewsKitSundayTimesMasthead } from '../../assets';
@@ -13,28 +20,30 @@ export const EditionMasthead: React.FC<{
   return (
     <ThemeProvider theme={TimesWebLightTheme}>
       <MainHeader>
-		<Visible md lg xl>
-        <Masthead>
-          <ScreenReaderOnly id="sr-only">
-		  	<Headline headingAs="h1">The Times &amp; The Sunday Times Homepage</Headline>
-          </ScreenReaderOnly>
-          <DateTime
-            date={todaysDate}
-            dateFormat="EEEE MMMM d yyyy"
-            overrides={{
-              stylePreset: 'mastheadTime',
-              typographyPreset: 'mastheadTime'
-            }}
-          />
-          <LinkInline href="/" overrides={{ stylePreset: 'mastheadLogo' }}>
-            {isSunday ? (
-              <NewsKitSundayTimesMasthead />
-            ) : (
-              <NewsKitTimesMasthead />
-            )}
-          </LinkInline>
-        </Masthead>
-		</Visible>
+        <Visible md lg xl>
+          <Masthead>
+            <ScreenReaderOnly id="sr-only">
+              <Headline headingAs="h1">
+                The Times &amp; The Sunday Times Homepage
+              </Headline>
+            </ScreenReaderOnly>
+            <DateTime
+              date={todaysDate}
+              dateFormat="EEEE MMMM d yyyy"
+              overrides={{
+                stylePreset: 'mastheadTime',
+                typographyPreset: 'mastheadTime'
+              }}
+            />
+            <LinkInline href="/" overrides={{ stylePreset: 'mastheadLogo' }}>
+              {isSunday ? (
+                <NewsKitSundayTimesMasthead />
+              ) : (
+                <NewsKitTimesMasthead />
+              )}
+            </LinkInline>
+          </Masthead>
+        </Visible>
       </MainHeader>
     </ThemeProvider>
   );
