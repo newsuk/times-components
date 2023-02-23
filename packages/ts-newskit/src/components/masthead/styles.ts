4524c5b1f3caccc1055471583c41a42fde5f7c50
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-23 13:35:24 +0000

    TDP-3011 Resolved PR comments

--- packages/ts-newskit/src/components/masthead/styles.ts
+++ packages/ts-newskit/src/components/masthead/styles.ts
@@ -1,18 +1,12 @@
 import { styled, getMediaQueryFromTheme } from 'newskit';
 
 export const MainHeader = styled.header`
-  left: 0;
+  width: 100%;
   ${getMediaQueryFromTheme('md')} {
-    margin-top: 0;
-    width: 100%;
-    position: relative;
-    z-index: 3;
-    max-width: 720px;
     margin-left: auto;
     margin-right: auto;
   }
   ${getMediaQueryFromTheme('lg')} {
-    max-width: 984px;
     margin-left: auto;
     margin-right: auto;
   }
