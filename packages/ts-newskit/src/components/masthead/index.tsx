754934fa8bf34253f17be6c79f4ef50c447a3123
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-24 14:59:35 +0000

    TDP-3011 Removed unnecessary code

--- packages/ts-newskit/src/components/masthead/index.tsx
+++ packages/ts-newskit/src/components/masthead/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {
-  ThemeProvider,
   DateTime,
   LinkInline,
   ScreenReaderOnly,
   Headline,
   Visible
 } from 'newskit';
-import { TimesWebLightTheme } from '../../theme';
 import { MainHeader, Masthead } from './styles';
 import { NewsKitTimesMasthead, NewsKitSundayTimesMasthead } from '../../assets';
 
@@ -18,35 +16,33 @@ export const EditionMasthead: React.FC<{
   todaysDate?: string;
 }> = ({ isSunday, todaysDate = dateNow }) => {
   return (
-    <ThemeProvider theme={TimesWebLightTheme}>
-      <MainHeader>
-        <Visible md lg xl>
-          <Masthead>
-            <ScreenReaderOnly id="sr-only">
-              <Headline headingAs="h1">
-                The Times &amp; The Sunday Times Homepage
-              </Headline>
-            </ScreenReaderOnly>
-            <DateTime
-              date={todaysDate}
-              dateFormat="EEEE MMMM d yyyy"
-              overrides={{
-                stylePreset: 'mastheadTime',
-                typographyPreset: 'mastheadTime'
-              }}
-              data-testid="date-time"
-            />
-            <LinkInline href="/" overrides={{ stylePreset: 'mastheadLogo' }}>
-              {isSunday ? (
-                <NewsKitSundayTimesMasthead />
-              ) : (
-                <NewsKitTimesMasthead />
-              )}
-            </LinkInline>
-          </Masthead>
-        </Visible>
-      </MainHeader>
-    </ThemeProvider>
+    <MainHeader>
+      <Visible md lg xl>
+        <Masthead>
+          <ScreenReaderOnly id="sr-only">
+            <Headline headingAs="h1">
+              The Times &amp; The Sunday Times Homepage
+            </Headline>
+          </ScreenReaderOnly>
+          <DateTime
+            date={todaysDate}
+            dateFormat="EEEE MMMM d yyyy"
+            overrides={{
+              stylePreset: 'mastheadTime',
+              typographyPreset: 'mastheadTime'
+            }}
+            data-testid="date-time"
+          />
+          <LinkInline href="/" overrides={{ stylePreset: 'mastheadLogo' }}>
+            {isSunday ? (
+              <NewsKitSundayTimesMasthead />
+            ) : (
+              <NewsKitTimesMasthead />
+            )}
+          </LinkInline>
+        </Masthead>
+      </Visible>
+    </MainHeader>
   );
 };
 
--- packages/ts-newskit/src/components/section-header/index.tsx
+++ packages/ts-newskit/src/components/section-header/index.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { ThemeProvider, Headline } from 'newskit';
-import { TimesWebLightTheme } from '../../theme';
+import { Headline } from 'newskit';
 
 export const SectionHeader: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <ThemeProvider theme={TimesWebLightTheme}>
-      <Headline
-        headingAs="h1"
-        overrides={{
-          typographyPreset: 'sectionHeader010',
-          heading: {
-            stylePreset: 'sectionheaderText'
-          }
-        }}
-      >
-        {title}
-      </Headline>
-    </ThemeProvider>
+    <Headline
+      headingAs="h1"
+      overrides={{
+        typographyPreset: 'sectionHeader010',
+        heading: {
+          stylePreset: 'sectionheaderText'
+        }
+      }}
+    >
+      {title}
+    </Headline>
   );
 };
