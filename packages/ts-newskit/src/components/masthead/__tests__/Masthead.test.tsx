f0b66c0f12c363d73234b1405b660e1d2c279051
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-02-24 09:41:18 +0000

    TDP-3011 Fixed final unit test

--- packages/ts-newskit/src/components/masthead/__tests__/Masthead.test.tsx
+++ packages/ts-newskit/src/components/masthead/__tests__/Masthead.test.tsx
@@ -1,35 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import { EditionMasthead } from '../index';
+import EditionMasthead from '../index';
+import { useBreakpointKey } from 'newskit';
+jest.mock('newskit', () => ({
+  ...jest.requireActual('newskit'),
+  useBreakpointKey: jest.fn().mockReturnValue('xl')
+}));
 
 describe('EditionMasthead', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    cleanup();
+  });
+
   it('should render The Times masthead', () => {
+    (useBreakpointKey as any).mockReturnValue('md');
     const { asFragment } = render(
       <EditionMasthead
         isSunday={false}
+        todaysDate="Wed Feb 22 2023 00:01:00 GMT+0000 (Greenwich Mean Time)"
       />
     );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should render The Sunday Times masthead', () => {
+    (useBreakpointKey as any).mockReturnValue('md');
     const { asFragment } = render(
       <EditionMasthead
         isSunday={true}
+        todaysDate="Sun Feb 26 2023 00:01:00 GMT+0000 (Greenwich Mean Time)"
       />
     );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should render date correctly', () => {
+    (useBreakpointKey as any).mockReturnValue('md');
     const { getByText } = render(
       <EditionMasthead
         isSunday={false}
-        todaysDate="Wed Feb 22 2023 00:00:00 GMT+0000 (Greenwich Mean Time)"
+        todaysDate="Wed Feb 22 2023 00:01:00 GMT+0000 (Greenwich Mean Time)"
       />
     );
-    expect(getByText('Wednesday February 22 2023')).toBeVisible();
+    const date = getByText('Wednesday February 22 2023');
+    expect(date).toBeInTheDocument();
   });
 });
