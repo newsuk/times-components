63406e48f9ebd09e00ce6784752430aced8ef419
Michael Douglas <michael.douglas1@news.co.uk>
2023-02-28 16:18:03 +0000

    feat(TDP-3290): update test and linting

--- packages/ts-newskit/src/components/utils/TCThemeProvider.tsx
+++ packages/ts-newskit/src/components/utils/TCThemeProvider.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { NewsKitProvider, Theme } from 'newskit'
-import { TimesWebLightTheme } from '../../theme'
+import React from 'react';
+import { NewsKitProvider, Theme } from 'newskit';
+import { TimesWebLightTheme } from '../../theme';
 
 type TCThemeProviderProps = {
-  theme?: Theme
-}
+  theme?: Theme;
+};
 
-export const TCThemeProvider: React.FC<TCThemeProviderProps> = ({ theme = TimesWebLightTheme, children }) => 
-  <NewsKitProvider theme={theme}>
-    {children}
-  </NewsKitProvider>
+export const TCThemeProvider: React.FC<TCThemeProviderProps> = ({
+  theme = TimesWebLightTheme,
+  children
+}) => <NewsKitProvider theme={theme}>{children}</NewsKitProvider>;
--- packages/ts-newskit/src/components/utils/__tests__/TCThemeProvider.test.tsx
+++ packages/ts-newskit/src/components/utils/__tests__/TCThemeProvider.test.tsx
@@ -2,29 +2,30 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { SectionHeader } from '../../section-header';
-import { TCThemeProvider } from "../TCThemeProvider"
+import { TCThemeProvider } from '../TCThemeProvider';
 
-const renderComponent = () => render(
-  <TCThemeProvider>
-    <SectionHeader title="test" />
-  </TCThemeProvider>
-);
+const renderComponent = () =>
+  render(
+    <TCThemeProvider>
+      <SectionHeader title="test" />
+    </TCThemeProvider>
+  );
 
 describe('<TCThemeProvider>', () => {
   it('should match snapshot', () => {
-    const wrapper = renderComponent();
+    const { asFragment } = renderComponent();
 
-    expect(wrapper).toMatchSnapshot();
-  })
+    expect(asFragment()).toMatchSnapshot();
+  });
   it('should wrap child components with theme and pass styling', () => {
     renderComponent();
 
-    const title = screen.getByText("test");
+    const title = screen.getByText('test');
 
     expect(title).toHaveStyle({
-      font: "inherit",
-      display: "inline-block",
-      paddingBottom: "0.8rem"
+      font: 'inherit',
+      display: 'inline-block',
+      paddingBottom: '0.8rem'
     });
-  })
-})
+  });
+});
