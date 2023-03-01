d1ac376bc9fc4cb17b81dcc6c42d36e44f47f30e
Michael Douglas <michael.douglas1@news.co.uk>
2023-03-01 12:15:23 +0000

    feat(TDP-2766): update styling presets

--- packages/ts-newskit/src/components/breadcrumbs/index.tsx
+++ packages/ts-newskit/src/components/breadcrumbs/index.tsx
@@ -4,9 +4,7 @@ import { BreadcrumbsItem } from './types';
 
 export const Breadcrumb: React.FC<{ data: BreadcrumbsItem[]; }> = ({ data }) => {
     return (
-        <Breadcrumbs
-            size="small"
-        >
+        <Breadcrumbs size="small">
             {data.map((breadcrumbItem, breadcrumbIndex, breadcrumbArr) => (
                 <BreadcrumbItem 
                     href={breadcrumbItem.url}
