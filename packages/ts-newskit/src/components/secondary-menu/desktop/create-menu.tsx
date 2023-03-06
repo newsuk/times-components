2163159de5d0e7e1bf51ab81fe36f5b999a04739
Michael Douglas <michael.douglas1@news.co.uk>
2023-03-06 17:29:34 +0000

    responsive updates

--- packages/ts-newskit/src/components/secondary-menu/desktop/create-menu.tsx
+++ packages/ts-newskit/src/components/secondary-menu/desktop/create-menu.tsx
@@ -10,48 +10,85 @@ export const CreateMenu: React.FC<{
   options: SecondaryMenuOptions;
   data: SecondaryMenuItem[];
 }> = ({ options, data }) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const navListContainerRef = useRef<HTMLDivElement>(null);
   const contanierRef = useRef<HTMLDivElement>(null);
   const { isExpanded, setIsExpanded } = options;
   const { moreMenuLength, menuItems, breakpointKey } = getBreakpoint(data);
   const [moreMenuItemsLength, setMoreMenuItemsLength] = useState<number>(0);
   const [hasMenuItem, setHasMenuItem] = useState<number>(data.length);
 
-  useEffect(
-    () => {
-      if (ref.current) {
-        if (
-          ref.current.offsetWidth < 900 &&
-          breakpointKey === 'lg' &&
-          data.length < 10
-        ) {
-          setMoreMenuItemsLength(moreMenuLength - 1);
-          setHasMenuItem(menuItems + 1);
-        } else if (ref.current.offsetWidth > 532 && breakpointKey === 'lg') {
-          setMoreMenuItemsLength(moreMenuLength + 1);
-          setHasMenuItem(menuItems - 1);
-        } else if (
-          ref.current.offsetWidth > 540 &&
-          ref.current.offsetWidth < 645 &&
-          breakpointKey === 'md'
-        ) {
-          setMoreMenuItemsLength(moreMenuLength - 1);
-          setHasMenuItem(menuItems + 1);
-        } else if (
-          ref.current.offsetWidth > 645 &&
-          breakpointKey === 'md' &&
-          data.length < 7
-        ) {
-          setMoreMenuItemsLength(moreMenuItemsLength + 1);
-          setHasMenuItem(hasMenuItem - 1);
-        } else if (ref.current.offsetWidth > 645 && breakpointKey === 'md') {
-          setMoreMenuItemsLength(moreMenuLength + 1);
-          setHasMenuItem(menuItems - 1);
-        } else {
-          setMoreMenuItemsLength(moreMenuLength);
-          setHasMenuItem(menuItems);
+    const getWidth = (el: any) => el.clientWidth
+
+    useEffect(() => {
+    setMoreMenuItemsLength(0);
+    setHasMenuItem(data.length);
+
+
+
+      const updateNav = (navAdjustCount = 1) => {
+        const navListContainerWidth = getWidth(contanierRef.current);
+        const navListWidth = getWidth(navListContainerRef.current);
+
+        if(navListWidth && navListContainerWidth) {
+          console.log("contanierRef: ", navListContainerWidth);
+          console.log("navListContainer: ", navListWidth);
+          console.log("called");
+
+          if(navListWidth > navListContainerWidth - 150) {
+            console.log("updated", navAdjustCount);
+            console.log("moreMenuLength", moreMenuLength);
+            console.log("menuItems", menuItems);
+
+            setMoreMenuItemsLength(moreMenuLength + navAdjustCount);
+            setHasMenuItem(menuItems - navAdjustCount);
+
+            setTimeout(() => updateNav(navAdjustCount + 1), 1000);
+          } 
         }
+        // else {
+        //   console.log("updated", navAdjustCount);
+        //   setMoreMenuItemsLength(moreMenuLength - 2);
+        //   setHasMenuItem(menuItems + 2);
+        //   // updateNav(navAdjustCount + 1);
+        // }
       }
+  
+      updateNav();
+      // setTimeout(() => updateNav(), 1000);
+
+      // if (ref.current) {
+      //   if (
+      //     ref.current.offsetWidth < 900 &&
+      //     breakpointKey === 'lg' &&
+      //     data.length < 10
+      //   ) {
+      //     setMoreMenuItemsLength(moreMenuLength - 1);
+      //     setHasMenuItem(menuItems + 1);
+      //   } else if (ref.current.offsetWidth > 532 && breakpointKey === 'lg') {
+      //     setMoreMenuItemsLength(moreMenuLength + 1);
+      //     setHasMenuItem(menuItems - 1);
+      //   } else if (
+      //     ref.current.offsetWidth > 540 &&
+      //     ref.current.offsetWidth < 645 &&
+      //     breakpointKey === 'md'
+      //   ) {
+      //     setMoreMenuItemsLength(moreMenuLength - 1);
+      //     setHasMenuItem(menuItems + 1);
+      //   } else if (
+      //     ref.current.offsetWidth > 645 &&
+      //     breakpointKey === 'md' &&
+      //     data.length < 7
+      //   ) {
+      //     setMoreMenuItemsLength(moreMenuItemsLength + 1);
+      //     setHasMenuItem(hasMenuItem - 1);
+      //   } else if (ref.current.offsetWidth > 645 && breakpointKey === 'md') {
+      //     setMoreMenuItemsLength(moreMenuLength + 1);
+      //     setHasMenuItem(menuItems - 1);
+      //   } else {
+      //     setMoreMenuItemsLength(moreMenuLength);
+      //     setHasMenuItem(menuItems);
+      //   }
+      // }
     },
     [breakpointKey]
   );
@@ -84,7 +121,7 @@ export const CreateMenu: React.FC<{
       }}
     >
       <Container ref={contanierRef} moreMenuItemsLength={moreMenuItemsLength}>
-        <Wrapper ref={ref} data-testid="navitems-test-id">
+        <Wrapper ref={navListContainerRef} data-testid="navitems-test-id">
           <NavItems data={data} options={options} hasMenuItem={hasMenuItem} />
         </Wrapper>
         {moreMenuItemsLength > 0 && (
