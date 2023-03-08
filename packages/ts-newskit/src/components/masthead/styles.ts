509040efde3e49b59998f44823f44316904e286b
Michael Douglas <michael.douglas1@news.co.uk>
2023-03-08 15:02:16 +0000

    feat(TDP-3291): correct masthead styles

--- packages/ts-newskit/src/components/masthead/styles.ts
+++ packages/ts-newskit/src/components/masthead/styles.ts
@@ -6,7 +6,7 @@ export const Masthead = styled(Block)`
 `;
 
 export const MastheadDate = styled(DateTime)`
-  texttransform: 'capitalize';
+  text-transform: 'capitalize';
   & > span {
     ${getColorCssFromTheme('color', 'inkBase')};
   }
