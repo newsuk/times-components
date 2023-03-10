2c49df66dc0345c421fc1d027264274b57079894
Bibi Collins <bibivictoria.collins@news.co.uk>
2023-03-10 09:46:26 +0000

    Export ThemeProvider wrapped hamburger and global navigation

--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -1,5 +1,13 @@
 export { TCThemeProvider } from './components/utils/TCThemeProvider';
 export { Breadcrumb } from './components/breadcrumb';
 export { GlobalNav } from './components/global-nav';
+export { TopNav } from './components/global-nav/topnav/topnav';
+export {
+  HamburgerMenu
+} from './components/global-nav/hamburger-menu/HamburgerMenu';
 export { EditionMasthead } from './components/masthead';
 export { SectionHeader } from './components/section-header/';
+export {
+  WrappedHamburger,
+  WrappedGlobalNav
+} from './components/global-nav/wrapped-components/WrappedComponents';
