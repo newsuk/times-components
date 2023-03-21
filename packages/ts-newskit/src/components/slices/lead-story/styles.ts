6b7c19e8eda17956d497dd5f286275f63166ac33
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-03-21 16:52:48 +0000

    feat(TDP-3412): develop lead story component

---
+++ packages/ts-newskit/src/components/slices/lead-story/styles.ts
@@ -0,0 +1,16 @@
+import { styled, TextBlock, Stack } from 'newskit';
+
+export const StyledTextBlock = styled(TextBlock)`
+  writing-mode: vertical-lr;
+  text-orientation: sideways;
+  transform: scale(-1, -1);
+  margin-bottom: 20px;
+`;
+
+export const StyledTextStack = styled(Stack)`
+  border-top: 1px dotted black;
+`;
+
+export const Styledinterviewee = styled(TextBlock)`
+  color: ${({ color }) => color};
+`;
