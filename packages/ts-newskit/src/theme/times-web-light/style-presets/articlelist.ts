3e1fab333e5455cab68c5853c5acd75a9109fb10
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-03-23 09:23:39 +0000

    TDP-3414 - Cleaned up hover code

--- packages/ts-newskit/src/theme/times-web-light/style-presets/articlelist.ts
+++ packages/ts-newskit/src/theme/times-web-light/style-presets/articlelist.ts
@@ -5,27 +5,5 @@ export const articleListStylePresets = {
             margin: '10px 0',
             textDecoration: 'none'
         }
-    },
-    articleTestLink: {
-        base: {
-            color: '{{colors.inkSubtle}}',
-            textDecoration: 'none'
-        },
-        hover: {
-            color: '{{colors.inkSubtle}}',
-            textDecoration: 'none'
-        },
-        ':hover:not([disabled])': {
-            color: '{{colors.inkSubtle}}',
-            textDecoration: 'none'
-        },
-        selected: {
-            color: '{{colors.inkSubtle}}',
-            textDecoration: 'none'
-        },
-        'selected:hover': {
-            color: '{{colors.inkSubtle}}',
-            textDecoration: 'none'
-        }
     }
 }
