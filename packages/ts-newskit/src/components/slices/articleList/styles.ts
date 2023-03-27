330e082b6ede42e8e47fac0ccf18a5c2721dbd76
Darren, Mackintosh <darren.mackintosh@news.co.uk>
2023-03-27 10:23:23 +0100

    TDP-3414 - Resolved linting issue

--- packages/ts-newskit/src/components/slices/articleList/styles.ts
+++ packages/ts-newskit/src/components/slices/articleList/styles.ts
@@ -1,7 +1,6 @@
 import { styled, TextBlock, getSizingCssFromTheme, Block } from 'newskit';
 export const ArticleListType = styled(TextBlock)<{ $color: string }>`
   color: ${({ $color }) => ($color ? $color : '#666')};
-
 `;
 export const ContainerInline = styled(Block)`
   display: inline-block;
