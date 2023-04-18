c5bbcf571f47ff95ab24d3fab89e42d67cfa6f1a
jasminaj0 <jasmina.jakonic@news.co.uk>
2023-04-18 14:09:43 +0200

    package updates

--- packages/ts-newskit/src/components/puzzles/archive/index.tsx
+++ packages/ts-newskit/src/components/puzzles/archive/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Accordion, GridLayout, Scroll, getSSRId, Stack, toNewsKitIcon, TextBlock } from 'newskit';
+import {
+  Accordion,
+  GridLayout,
+  Scroll,
+  getSSRId,
+  Stack,
+  toNewsKitIcon,
+  TextBlock
+} from 'newskit';
 import { CardContainer, SeeMoreBox, StyledAccordionGroup } from './styles';
-import { East } from '@emotion-icons/material'
+import { East } from '@emotion-icons/material';
 import { Puzzles } from './types';
 
 interface ArchiveProps {
   data: Puzzles;
 }
 
-const IconEast = toNewsKitIcon(East)
+const IconEast = toNewsKitIcon(East);
 
 export const Archive = ({ data }: ArchiveProps) => {
-
   const [expandedMultiple, setExpandedMultiple] = React.useState([0]);
   const puzzleTitleSet = new Set<string>();
   data.list.forEach(puzzle => puzzleTitleSet.add(puzzle.title));
@@ -23,39 +30,42 @@ export const Archive = ({ data }: ArchiveProps) => {
       onChange={setExpandedMultiple}
     >
       {puzzleTitles.map(title => (
-        <Accordion header={title.toLocaleUpperCase()} id={getSSRId()} 
+        <Accordion
+          header={title.toLocaleUpperCase()}
+          id={getSSRId()}
           overrides={{
             header: {
-              typographyPreset: 'utilityButton010', 
-              paddingInline: 'space000',
+              typographyPreset: 'utilityButton010',
+              paddingInline: 'space000'
             },
             panel: {
               paddingInline: 'space000',
               paddingBlockStart: 'space000',
               paddingBlockEnd: 'space050'
             }
-          }}>
-          <Scroll
-            overrides={{ overlays: { stylePreset: '__delete' } }}
-          >
-            <GridLayout autoFlow="column" 
-              autoColumns={{xs: '110px', lg: '111.25px', xl: '148.75px'}} columnGap='space030'            
+          }}
+        >
+          <Scroll overrides={{ overlays: { stylePreset: '__delete' } }}>
+            <GridLayout
+              autoFlow="column"
+              autoColumns={{ xs: '110px', lg: '111.25px', xl: '148.75px' }}
+              columnGap="space030"
             >
-              {data.list.filter(puzzle => puzzle.title === title).map(() => 
-                <CardContainer id={getSSRId()}>
-                </CardContainer>
-              )}
-              <SeeMoreBox> 
-                <Stack flow='vertical-center' stackDistribution='center'>
-                  <IconEast     
+              {data.list.filter(puzzle => puzzle.title === title).map(() => (
+                <CardContainer id={getSSRId()} />
+              ))}
+              <SeeMoreBox>
+                <Stack flow="vertical-center" stackDistribution="center">
+                  <IconEast
                     overrides={{
-                    size: 'iconSize030',
-                    stylePreset: 'inkBase',
-                  }}/>
-                  <TextBlock     
-                    typographyPreset='utilityButton020'
-                    stylePreset="inkContrast" 
-                    marginBlockStart='space030'
+                      size: 'iconSize030',
+                      stylePreset: 'inkBase'
+                    }}
+                  />
+                  <TextBlock
+                    typographyPreset="utilityButton020"
+                    stylePreset="inkContrast"
+                    marginBlockStart="space030"
                   >
                     See more
                   </TextBlock>
