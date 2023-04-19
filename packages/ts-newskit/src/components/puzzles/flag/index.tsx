7ae1df991390fe10d2138b447a4afcd57fa4c600
jasminaj0 <jasmina.jakonic@news.co.uk>
2023-04-19 11:19:02 +0200

    flag component

---
+++ packages/ts-newskit/src/components/puzzles/flag/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Flag } from 'newskit';
+
+interface FlagProps {
+  status: 'COMPLETE' | 'IN PROGRESS';
+}
+
+const FlagStatuses = new Map([
+  ['COMPLETE', { stylePreset: 'flagComplete' }],
+  ['IN PROGRESS', { stylePreset: 'flagProgress' }]
+]);
+
+export const PuzzlesFlag: FC<FlagProps> = ({ status }) => {
+  return (
+    <Flag
+      size="small"
+      overrides={{
+        minWidth: '92px',
+        stylePreset: `${FlagStatuses.get(status)!.stylePreset}`
+      }}
+    >
+      {status}
+    </Flag>
+  );
+};
