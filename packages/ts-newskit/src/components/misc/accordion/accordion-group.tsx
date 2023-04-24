2399358184f4cf08ebcdf67ab6099961c9496fbb
Callum <callumchristie@hotmail.com>
2023-04-24 13:37:44 +0100

    feat(TDP-000): Accordion

--- packages/ts-newskit/src/components/misc/accordion/accordion-group.tsx
+++ packages/ts-newskit/src/components/misc/accordion/accordion-group.tsx
@@ -4,8 +4,26 @@ import {
   AccordionGroup as NewskitAccordionGroup,
   TextBlock
 } from 'newskit';
+import xss from 'xss';
 
-import { sanitiseCopy } from '@times-components/ts-components';
+const sanitiseCopy = (copy: string = '', allowedTags: {} = {}) => {
+  const decodeEntities = (inputString: string) => {
+    let decodedString = document.createElement('textarea');
+    decodedString.innerHTML = inputString;
+    return decodedString.value;
+  };
+
+  let options = {
+    whiteList: allowedTags,
+    stripIgnoreTag: true,
+    stripIgnoreTagBody: ['script']
+  };
+
+  let decodedCopy = decodeEntities(copy);
+
+  return xss(decodedCopy, options);
+};
+
 
 type AccordionProps = {
   header: string;
