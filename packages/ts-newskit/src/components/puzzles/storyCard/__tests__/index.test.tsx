84399511d1dd65099edfaa6add212194b712ed69
Josip Vukovic <josip.vukovic@news.co.uk>
2023-04-28 09:59:03 +0200

    feat(TMST-623): fixed test import

--- packages/ts-newskit/src/components/puzzles/storyCard/__tests__/index.test.tsx
+++ packages/ts-newskit/src/components/puzzles/storyCard/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '../../../utils/test-utils';
+import { render, screen } from '../../../../utils/test-utils';
 import { StoryCard, StoryCardProps } from '../index';
 
 const renderComponent = (props: StoryCardProps) =>
