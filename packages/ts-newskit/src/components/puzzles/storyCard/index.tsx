e6d17dcd97dc1917b9cf68146b007ce3d7364d7c
josipVuko <129039019+josipVuko@users.noreply.github.com>
2023-05-05 11:22:24 +0200

    Feat/tmst 620 create recent stories component (#3343)
    * feat(TMST-632): built flag component
    
    * feat(TMST-623): created article card
    
    * feat(TMST-623): updated article card
    
    * feat(TMST-623): added placeholder img and tests
    
    * feat(TMST-623): checked all test. Updated documentation
    
    * feat(TMST-623): updated story book
    
    * feat(TMST-623): removed stylepreset
    
    * feat(TMST-623): fixed test import
    
    * feat(TMST-623): fixed circleCI error
    
    * feat(TMST-623): fixed circle ci error
    
    * feat(TMST-623): added new theme and changed styleprests
    
    * feat(TMST-623): updated storycard snapshot
    
    * feat(TMST-620): added additional proprs to story card. Built story card listing. Wrote tests
    
    ---------
    
    Co-authored-by: Josip Vukovic <josip.vukovic@q.agency>

--- packages/ts-newskit/src/components/puzzles/storyCard/index.tsx
+++ packages/ts-newskit/src/components/puzzles/storyCard/index.tsx
@@ -9,17 +9,20 @@ import {
   CardLink,
   Headline,
   CardMedia,
-  GridLayout
+  GridLayout,
+  Divider
 } from 'newskit';
 
 export interface StoryCardProps {
-  image: string;
-  altText: string;
+  image?: string;
+  altText?: string;
   title: string;
   url: string;
-  category: string;
-  timeToRead: string;
-  imgHiddenMobile: boolean;
+  category?: string;
+  timeToRead?: string;
+  imgHiddenMobile?: boolean;
+  hiddenMobile?: boolean;
+  mobileDivider?: boolean;
 }
 
 export const StoryCard: FC<StoryCardProps> = ({
@@ -29,73 +32,86 @@ export const StoryCard: FC<StoryCardProps> = ({
   url,
   category,
   timeToRead,
-  imgHiddenMobile
+  imgHiddenMobile,
+  hiddenMobile,
+  mobileDivider
 }) => {
   return (
-    <CardComposable>
-      <Visible xs={!imgHiddenMobile} sm={!imgHiddenMobile} md lg xl>
-        {image ? (
-          <CardMedia
-            media={{
-              loadingAspectRatio: '3:2',
-              alt: altText ? altText : title,
-              src: image,
-              placeholderIcon: true
-            }}
-          />
-        ) : (
-          <NewsKitArticlePlaceholder data-testid="storyCard-placeholder" />
-        )}
+    <Visible xs={!hiddenMobile} sm={!hiddenMobile} md lg xl>
+      <Visible xs={mobileDivider} sm={mobileDivider}>
+        <Divider
+          overrides={{ marginBlock: 'space040', stylePreset: 'dashedDivider' }}
+        />
       </Visible>
-      <CardContent>
-        <CardLink
-          expand
-          href={url}
-          data-testid="storyCard-link"
-          overrides={{
-            externalIcon: {
-              size: '0'
-            },
-            stylePreset: 'inkContrast'
-          }}
-        >
-          <Headline
-            headingAs="h3"
+      <CardComposable>
+        <Visible xs={!imgHiddenMobile} sm={!imgHiddenMobile} md lg xl>
+          {image ? (
+            <CardMedia
+              media={{
+                loadingAspectRatio: '3:2',
+                alt: altText || title,
+                src: image,
+                placeholderIcon: true
+              }}
+            />
+          ) : (
+            <NewsKitArticlePlaceholder data-testid="storyCard-placeholder" />
+          )}
+        </Visible>
+        <CardContent>
+          <CardLink
+            expand
+            href={url}
+            data-testid="storyCard-link"
             overrides={{
-              marginBlock: 'space040',
-              typographyPreset: 'editorialHeadline020'
+              externalIcon: {
+                size: '0'
+              },
+              stylePreset: 'inkContrast'
             }}
           >
-            {title}
-          </Headline>
-        </CardLink>
-      </CardContent>
-      <CardActions>
-        <GridLayout columns="repeat(2, auto)" columnGap="space020">
-          <Tag
-            overrides={{
-              stylePreset: 'inkContrast',
-              typographyPreset: 'utilityLabel010',
-              minHeight: '16px',
-              paddingBlock: '0',
-              paddingInline: '0'
-            }}
-          >
-            {category}
-          </Tag>
-          <Tag
-            overrides={{
-              stylePreset: 'inkSubtle',
-              typographyPreset: 'utilityLabel010',
-              minHeight: '16px',
-              paddingBlock: '0',
-              paddingInline: '0'
-            }}
-          >
-            {timeToRead}
-          </Tag>
-        </GridLayout>
-      </CardActions>
-    </CardComposable>
+            <Headline
+              headingAs="h3"
+              overrides={{
+                marginBlockEnd: 'space040',
+                marginBlockStart: {
+                  xs: imgHiddenMobile ? '' : 'space040',
+                  md: 'space040'
+                },
+                typographyPreset: 'editorialHeadline020'
+              }}
+            >
+              {title}
+            </Headline>
+          </CardLink>
+        </CardContent>
+        <CardActions>
+          <GridLayout columns="repeat(2, auto)" columnGap="space020">
+            <Tag
+              overrides={{
+                stylePreset: 'inkContrast',
+                typographyPreset: 'utilityLabel010',
+                minHeight: '16px',
+                paddingBlock: '0',
+                paddingInline: '0'
+              }}
+            >
+              {category}
+            </Tag>
+            <Tag
+              overrides={{
+                stylePreset: 'inkSubtle',
+                typographyPreset: 'utilityLabel010',
+                minHeight: '16px',
+                paddingBlock: '0',
+                paddingInline: '0'
+              }}
+            >
+              {timeToRead}
+            </Tag>
+          </GridLayout>
+        </CardActions>
+      </CardComposable>
+    </Visible>
   );
 };
