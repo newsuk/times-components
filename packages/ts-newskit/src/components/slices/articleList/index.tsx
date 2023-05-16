fea87f72a552cc7df4b0f21644a8b0a9d20b8641
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-05-16 10:09:55 +0100

    feat(TMRX-1226): update articleList component to accommodate slice

--- packages/ts-newskit/src/components/slices/articleList/index.tsx
+++ packages/ts-newskit/src/components/slices/articleList/index.tsx
@@ -6,7 +6,8 @@ import {
   TextBlock,
   CardComposable,
   CardMedia,
-  GridLayoutItem
+  GridLayoutItem,
+  useBreakpointKey
 } from 'newskit';
 import {
   CardHeadlineLink,
@@ -29,7 +30,12 @@ export interface ArticleListItemProps {
   hasTopBorder?: boolean;
   hideImage?: boolean;
   isLeadImage?: boolean;
+  isBucket1?: boolean;
 }
+
+type LayoutProps = {
+  isBucket1True: boolean;
+};
 
 export const ArticleListItem = ({
   image,
@@ -39,8 +45,10 @@ export const ArticleListItem = ({
   timeToRead,
   hasTopBorder,
   hideImage,
-  isLeadImage
+  isLeadImage,
+  isBucket1
 }: ArticleListItemProps) => {
+  const breakpointKey = useBreakpointKey();
   const cardImage = !hideImage &&
     image && {
       media: {
@@ -50,16 +58,27 @@ export const ArticleListItem = ({
       }
     };
 
+  const isBucket1True = isBucket1 && breakpointKey === 'xl';
+
   const CardMediaComponent = isLeadImage ? FullWidthCardMediaMob : CardMedia;
+
+  const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return isBucket1True ? <Block>{children}</Block> : <>{children}</>;
+  };
 
   return (
     <CardComposable
       alignContent="start"
-      areas={`
-        border
-        media
-        content
-      `}
+      areas={
+        isBucket1True
+          ? `content media`
+          : `border
+         media
+         content
+        `
+      }
+      columns={{ xl: isBucket1True ? '1fr 1fr' : '1fr' }}
+      columnGap="space040"
     >
       {hasTopBorder && (
         <GridLayoutItem area="border">
@@ -73,9 +92,9 @@ export const ArticleListItem = ({
       )}
 
       {image && !hideImage && <CardMediaComponent {...cardImage} />}
-
       <CardContent>
         {image &&
+          !isBucket1True &&
           image.credit &&
           !hideImage && (
             <TextBlock
@@ -86,50 +105,48 @@ export const ArticleListItem = ({
               {image.credit}
             </TextBlock>
           )}
-
-        <CardHeadlineLink
-          href={url}
-          role="link"
-          overrides={{
-            typographyPreset: 'editorialHeadline020',
-            marginBlockStart: 'space040'
-          }}
-        >
-          {title}
-        </CardHeadlineLink>
-
-        {(articleType || timeToRead) && (
-          <Block>
-            <TextBlock
-              typographyPreset="articleListArticleType"
-              as="span"
-              marginBlockStart="space030"
-              tabIndex={0}
-            >
-              {articleType}
-            </TextBlock>
-            {articleType &&
-              timeToRead && (
-                <ContainerInline>
-                  <Divider
-                    vertical
-                    overrides={{
-                      marginInline: 'space020'
-                    }}
-                  />
-                </ContainerInline>
-              )}
-            <TextBlock
-              typographyPreset="articleListTimeToRead"
-              stylePreset="articleListTimeToRead"
-              as="span"
-              marginBlockStart="space030"
-              tabIndex={0}
-            >
-              {timeToRead}
-            </TextBlock>
-          </Block>
-        )}
+        <Layout isBucket1True={isBucket1True || false}>
+          <CardHeadlineLink
+            href={url}
+            role="link"
+            overrides={{
+              typographyPreset: 'editorialHeadline020',
+              paddingBlockStart: isBucket1True ? 'space000' : 'space040'
+            }}
+          >
+            {title}
+          </CardHeadlineLink>
+          {(articleType || timeToRead) && (
+            <Block>
+              <TextBlock
+                typographyPreset="articleListArticleType"
+                as="span"
+                marginBlockStart="space030"
+              >
+                {articleType}
+              </TextBlock>
+              {articleType &&
+                timeToRead && (
+                  <ContainerInline>
+                    <Divider
+                      vertical
+                      overrides={{
+                        marginInline: 'space020'
+                      }}
+                    />
+                  </ContainerInline>
+                )}
+              <TextBlock
+                typographyPreset="articleListTimeToRead"
+                stylePreset="articleListTimeToRead"
+                as="span"
+                marginBlockStart="space030"
+              >
+                {timeToRead}
+              </TextBlock>
+            </Block>
+          )}
+        </Layout>
       </CardContent>
     </CardComposable>
   );
