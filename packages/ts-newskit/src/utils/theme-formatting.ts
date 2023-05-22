17db53396ddb2eddd254bb1a2acf703f638794af
Michael Douglas <michael.douglas1@news.co.uk>
2023-05-22 16:42:52 +0100

    feat(TMRX-1272): update font based on weight

--- packages/ts-newskit/src/utils/theme-formatting.ts
+++ packages/ts-newskit/src/utils/theme-formatting.ts
@@ -7,19 +7,29 @@ const modifyBaseRemValue = (multiplier: number, valueToUpdate: string) => {
   return `${updatedVal}rem`
 }
 
+const modifyFontFamilyValue = (family: string, weight?: number) => {
+  let updatedFont = family;
+  switch (family) {
+    case "Roboto":
+      updatedFont = weight && weight > 500 ? "Roboto-Medium" : "Roboto-Regular"
+      break;
+    case "Times Modern":
+      updatedFont = weight && weight > 500 ? "TimesModern-Bold" : "TimesModern-Regular"
+      break;
+    case "Times Digital W04 Regular":
+      updatedFont = "TimesDigitalW04-Regular"
+      break;
+  
+    default:
+      family
+      break;
+  }
+
+  return updatedFont;
+}
+
   
 export const updateThemeTypography = (themeObj: Theme) => {
-  // Update Fonts
-  themeObj.fonts = Object.fromEntries(
-    Object.entries(themeObj.fonts).map(font => {
-      const updatedVal = typeof(font[1]) === 'string' && font[1].includes("rem")
-      ? modifyBaseRemValue(1.6, font[1])
-      : font[1]
-      
-      return [font[0], updatedVal]
-    })
-  );
-    
   // Update Borders
   themeObj.borders = Object.fromEntries(
     Object.entries(themeObj.borders).map(border => {
@@ -31,11 +41,34 @@ export const updateThemeTypography = (themeObj: Theme) => {
     })
   );
 
+  // Update Font Sizes
+  themeObj.fonts = Object.fromEntries(
+    Object.entries(themeObj.fonts).map(font => {
+      const updatedVal = typeof(font[1]) === 'string' && font[1].includes("rem")
+      ? modifyBaseRemValue(1.6, font[1])
+      : font[1]
+      
+      return [font[0], updatedVal]
+    })
+  );
+
+  // Update Font Families
+  Object.values(themeObj.fonts).map(font => {
+    if(font.fontFamily) {
+      font.fontFamily = font.fontFamily
+        ? modifyFontFamilyValue(font.fontFamily)
+        : null      
+    }
+  });  
+
   // Update Typography Presets
   Object.values(themeObj.typographyPresets).map(preset => {
+    preset.fontFamily = modifyFontFamilyValue(String(preset.fontFamily), Number(preset.fontWeight));
+    preset.fontWeight = 400;
+
     preset.fontSize = typeof(preset.fontSize) === 'string' && preset.fontSize.includes("rem")
-    ? modifyBaseRemValue(1.6, preset.fontSize)
-    : preset.fontSize    
+      ? modifyBaseRemValue(1.6, preset.fontSize)
+      : preset.fontSize;
   });  
   
   return themeObj;
@@ -43,9 +76,11 @@ export const updateThemeTypography = (themeObj: Theme) => {
   
 export const formatThemeOverrides = (themeObj: Theme, stylePresets = {}, typographyPresets = {}) => {
   const updatedThemeTypography = updateThemeTypography(themeObj);
+  console.log("updatedThemeTypography: ", updatedThemeTypography)
 
   return {
     overrides: {
+      ...updatedThemeTypography,
       breakpoints: {
         xs: 0,
         sm: 520,
@@ -53,7 +88,6 @@ export const formatThemeOverrides = (themeObj: Theme, stylePresets = {}, typogra
         lg: 1024,
         xl: 1320
       },
-      ...updatedThemeTypography,
       typographyPresets: {
         ...updatedThemeTypography.typographyPresets,
         ...typographyPresets
