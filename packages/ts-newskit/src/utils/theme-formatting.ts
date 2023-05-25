53a51ee81ff5ae0899a8ebb16885e12bb2ebec03
Michael Douglas <michael.douglas1@news.co.uk>
2023-05-25 12:29:39 +0100

    fix(TMRX-1272): reeveert font family mapping

--- packages/ts-newskit/src/utils/theme-formatting.ts
+++ packages/ts-newskit/src/utils/theme-formatting.ts
@@ -7,34 +7,6 @@ const modifyBaseRemValue = (multiplier: number, valueToUpdate: string) => {
   return `${updatedVal}rem`;
 };
 
-export const modifyFontFamilyValue = (family: string, weight?: number) => {
-  let updatedFont = family;
-  switch (family) {
-    case 'Roboto':
-      updatedFont =
-        weight && weight >= 500
-          ? "'Roboto-Medium', 'Roboto-Medium-fallback', sans-serif"
-          : "'Roboto-Regular', 'Roboto-Regular-fallback', sans-serif";
-      break;
-    case 'Times Modern':
-      updatedFont =
-        weight && weight > 500
-          ? "'TimesModern-Bold', 'TimesModern-Bold-fallback', serif"
-          : "'TimesModern-Regular', 'TimesModern-Regular-fallback', serif";
-      break;
-    case 'Times Digital W04 Regular':
-      updatedFont =
-        "'TimesDigitalW04-Regular', 'TimesDigitalW04-Regular-fallback', serif";
-      break;
-
-    default:
-      family;
-      break;
-  }
-
-  return updatedFont;
-};
-
 export const updateThemeTypography = (themeObj: Theme) => {
   if (!themeObj.fonts.updated) {
     themeObj.fonts.updated = 1;
@@ -63,21 +35,8 @@ export const updateThemeTypography = (themeObj: Theme) => {
       })
     );
 
-    // Update Font Families
-    Object.values(themeObj.fonts).map(font => {
-      if (font.fontFamily) {
-        font.fontFamily = modifyFontFamilyValue(font.fontFamily);
-      }
-    });
-
     // Update Typography Presets
     Object.values(themeObj.typographyPresets).map(preset => {
-      preset.fontFamily = modifyFontFamilyValue(
-        String(preset.fontFamily),
-        Number(preset.fontWeight)
-      );
-      preset.fontWeight = 400;
-
       preset.fontSize =
         typeof preset.fontSize === 'string' && preset.fontSize.includes('rem')
           ? modifyBaseRemValue(1.6, preset.fontSize)
