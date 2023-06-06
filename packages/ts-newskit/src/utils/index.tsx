b7af561800e02cc109dfe3d7ff9ab116feb9fddc
Josip Vukovic <129039019+josipVuko@users.noreply.github.com>
2023-06-06 09:44:17 +0200

    feat(TMST-683): updated puzzle card date formating (#3385)
    * feat(TMST-683): updated puzzle card date formating
    
    * feat(TMST-683): moved date formater to new file
    
    * feat(TMST-683): refactored date formater
    
    * feat(TMST-683): exported dateforamter in index.tsx

--- packages/ts-newskit/src/utils/index.tsx
+++ packages/ts-newskit/src/utils/index.tsx
@@ -2,3 +2,4 @@ export { debounce } from './debounced';
 export { getWidth } from './getWidth';
 export { TCThemeProvider, themes } from './TCThemeProvider';
 export { GridOverlay } from './gridOverlay';
+export { convertDateToWeekday } from './date-formatting';
