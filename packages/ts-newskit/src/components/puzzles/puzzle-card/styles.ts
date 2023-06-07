c1f2cec263f9c6cd75a6f7643e4820b37e0d88d5
Josip Vukovic <129039019+josipVuko@users.noreply.github.com>
2023-06-07 16:57:42 +0200

    Feat/tmst 686 puzzle card fix card height issue (#3392)
    * feat(TMST-683): updated puzzle card date formating
    
    * feat(TMST-683): moved date formater to new file
    
    * feat(TMST-683): refactored date formater
    
    * feat(TMST-683): exported dateforamter in index.tsx
    
    * feat(TMST-686): fixed puzzle card height issue
    
    * feat(TMST-686): created new style component for puzzles

--- packages/ts-newskit/src/components/puzzles/puzzle-card/styles.ts
+++ packages/ts-newskit/src/components/puzzles/puzzle-card/styles.ts
@@ -1,12 +1,23 @@
-import { styled, Block, getColorCssFromTheme } from 'newskit';
+import { styled, Block, getColorCssFromTheme, CardComposable } from 'newskit';
 import { NewsKitPuzzlePlaceholder } from './assets';
 
 export const Wrap = styled(Block)`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 `;
+
+export const PuzzleCardComposable = styled(CardComposable)`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PuzzleCardImgWrapper = styled(Block)`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: flex-start;
+`;
+
 export const StyledNewsKitPuzzlePlaceholder = styled(NewsKitPuzzlePlaceholder)`
   ${getColorCssFromTheme('color', 'puzzles010')};
 `;
