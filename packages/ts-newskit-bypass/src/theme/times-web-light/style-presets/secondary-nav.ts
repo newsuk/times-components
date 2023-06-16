4f3bca4c8f33c38bb8a5d8b23523e5c630ce0fbd
Adam Osborne <me@adamosborne.com>
2023-06-16 16:33:57 +0100

    updated theme and tests

---
+++ packages/ts-newskit-bypass/src/theme/times-web-light/style-presets/secondary-nav.ts
@@ -0,0 +1,65 @@
+export const secondaryNavigationStylePresets = {
+  subMenu: {
+    base: {
+      borderRadius: '{{sizing.sizing010}}',
+      backgroundColor: '{{colors.white}}'
+    }
+  },
+  menuItemDesktop: {
+    base: {
+      color: '{{colors.neutral090}}',
+      backgroundColor: '{{colors.white}}',
+      borderBottom: '4px solid transparent'
+    },
+    hover: {
+      backgroundColor: '{{colors.neutral010}}'
+    },
+    selected: {
+      borderBottom: '4px solid {{colors.neutral100}}'
+    }
+  },
+
+  secondaryMenuItem: {
+    base: {
+      color: '{{colors.neutral090}}',
+      borderBottom: '1px solid {{colors.neutral030}}'
+    },
+    hover: {
+      backgroundColor: '{{colors.neutral010}}',
+      borderBottom: '1px solid #F5F5F5'
+    }
+  },
+  icon: {
+    base: {
+      position: 'absolute',
+      right: '25px',
+      height: '{{sizing.sizing040}}',
+      width: '{{sizing.sizing040}}'
+    }
+  },
+
+  secondaryNavMenuBlock: {
+    base: {
+      backgroundColor: '{{colors.neutral010}}'
+    }
+  },
+
+  menuItemMore: {
+    base: {
+      color: '{{colors.neutral090}}'
+    }
+  },
+  subMenuPreset1: {
+    base: {
+      backgroundColor: '{{colors.white}}'
+    },
+    hover: {
+      backgroundColor: '{{colors.neutral010}}'
+    }
+  },
+  subMenuPreset2: {
+    base: {
+      backgroundColor: '{{colors.neutral010}}'
+    }
+  }
+};
