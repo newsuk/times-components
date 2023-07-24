cec5eaf59b6b7e577c81c9917c657bf77a6a1a37
Callum <callumchristie@hotmail.com>
2023-07-24 11:53:42 +0100

    move session logic to the welcome banner

--- packages/ts-components/src/components/welcome-banner/WelcomeBanner.stories.tsx
+++ packages/ts-components/src/components/welcome-banner/WelcomeBanner.stories.tsx
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react';
 
 import { WelcomeBanner } from './WelcomeBanner';
 
-storiesOf('Typescript Component/Welcome Banner', module).add('Welcome Banner', () => {
-  return (
-    <WelcomeBanner />
-  );
-});
+storiesOf('Typescript Component/Welcome Banner', module).add(
+  'Welcome Banner',
+  () => {
+    window.sessionStorage.setItem('showWelcomeBanner', 'true');
+    return <WelcomeBanner />;
+  }
+);
--- packages/ts-components/src/components/welcome-banner/WelcomeBanner.tsx
+++ packages/ts-components/src/components/welcome-banner/WelcomeBanner.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container, Text, Title } from './styles';
 
 export const WelcomeBanner: React.FC = () => {
-  return (
+  const [isEnabled, setIsEnabled] = useState<Boolean>(false);
+
+  useEffect(() => {
+    if (window.sessionStorage.getItem('showWelcomeBanner')) {
+      window.sessionStorage.removeItem('showWelcomeBanner');
+      setIsEnabled(true);
+    }
+  }, []);
+
+  return isEnabled ? (
     <Container>
       <Title>Welcome to The Times and The Sunday Times</Title>
       <Text>We hope you enjoy your free article</Text>
     </Container>
-  )
-}
+  ) : null;
+};
