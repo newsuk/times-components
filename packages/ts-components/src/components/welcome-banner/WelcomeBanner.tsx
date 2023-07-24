1b7fbdc42f96ca96d3c2c13cb1a9af16da4d8835
Olivia Thetgyi <othetgyi@gmail.com>
2023-07-24 17:45:08 +0100

    feat(TMG-773): add tests

--- packages/ts-components/src/components/welcome-banner/WelcomeBanner.tsx
+++ packages/ts-components/src/components/welcome-banner/WelcomeBanner.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Container, Text, Title } from './styles';
 
 export const WelcomeBanner: React.FC = () => {
-  const [isEnabled, setIsEnabled] = useState<Boolean>(false);
+  const [isEnabled, setIsEnabled] = useState<boolean>(false);
 
   useEffect(() => {
     if (window.sessionStorage.getItem('showWelcomeBanner')) {
@@ -14,8 +14,8 @@ export const WelcomeBanner: React.FC = () => {
 
   return isEnabled ? (
     <Container>
-      <Title>Welcome to The Times and The Sunday Times</Title>
-      <Text>We hope you enjoy your free article</Text>
+      <Title data-testId='title'>Welcome to The Times and The Sunday Times</Title>
+      <Text data-testId='text'>We hope you enjoy your free article</Text>
     </Container>
   ) : null;
 };
