44baa16fc42c487e57747523a74b6c25083f3e70
Callum Christie <callumchristie@hotmail.com>
2023-07-26 11:22:51 +0100

    Feat/welcome banner styles (#3455)
    * feat(TM-000): Welcome banner style update
    
    * feat(TM-000): Welcome banner style update

--- packages/ts-components/src/components/welcome-banner/styles.ts
+++ packages/ts-components/src/components/welcome-banner/styles.ts
@@ -6,21 +6,22 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100%;
-  padding: 38px 33px 34px;
+  margin-bottom: 28px;
+  padding: 28px 12px;
   border: 1px solid ${colours.functional.primary};
 `;
 
 export const Title = styled.div`
+  color: ${colours.functional.greyText};
   font-family: ${fonts.headline};
   font-size: 28px;
-  color: ${colours.functional.greyText};
   text-align: center;
 `;
 
 export const Text = styled.div`
+  padding-top: 8px;
+  color: ${colours.functional.primary};
   font-family: ${fonts.supporting};
   font-size: 18px;
-  color: ${colours.functional.primary};
-  padding-top: 9px;
   text-align: center;
 `;
