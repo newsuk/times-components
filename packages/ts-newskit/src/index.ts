9a50a8a24f635133d8b26971a1f15aed2ab7a901
adminrl <rianne.lewis@news.co.uk>
2023-07-27 15:17:06 +0100

    chore(TMP-320): Add verdaccio to test lerna publish workflow.

--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -62,3 +62,5 @@ export { JobTitle } from './components/misc/job-title/job-title';
 export {
   UpdateButtonWithDelay
 } from './components/misc/update-button/update-button-with-delay';
+
+// TODO Remove after tests - borisovy
