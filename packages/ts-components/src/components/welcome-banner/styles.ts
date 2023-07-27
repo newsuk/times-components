4cb7e2626d8ff4cc55cbbceabc46356df37714ec
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2023-07-27 17:07:31 +0100

    feat(TMG-773): Fix Welcome Banner styling (#3461)
    * feat(TMG-773): fix styling
    
    * feat(TMG-773): fix padding, line heights
    
    * feat(TMG-773): fix spacing

--- packages/ts-components/src/components/welcome-banner/styles.ts
+++ packages/ts-components/src/components/welcome-banner/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 import { colours, fonts } from '@times-components/ts-styleguide';
 
-export const Container = styled.div`
+export const WelcomeBannerContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   width: 100%;
   margin-bottom: 28px;
-  padding: 28px 12px;
+  padding: 16px 34px;
   border: 1px solid ${colours.functional.primary};
 `;
 
@@ -15,13 +15,15 @@ export const Title = styled.div`
   color: ${colours.functional.greyText};
   font-family: ${fonts.headline};
   font-size: 28px;
+  line-height: 31px;
   text-align: center;
 `;
 
 export const Text = styled.div`
-  padding-top: 8px;
+  margin-top: 6px;
   color: ${colours.functional.primary};
   font-family: ${fonts.supporting};
   font-size: 18px;
   text-align: center;
+  line-height: 27px;
 `;
