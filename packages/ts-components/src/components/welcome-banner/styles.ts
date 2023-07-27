98df2e7e4d37a774a162656eb6d9f291aa3f613c
Olivia Thetgyi <othetgyi@gmail.com>
2023-07-27 10:07:10 +0100

    feat(TMG-773): fix padding, line heights

--- packages/ts-components/src/components/welcome-banner/styles.ts
+++ packages/ts-components/src/components/welcome-banner/styles.ts
@@ -7,7 +7,7 @@ export const WelcomeBannerContainer = styled.div`
   justify-content: center;
   width: 100%;
   margin-bottom: 28px;
-  padding: 21px 34px 20px;
+  padding: 23px 34px;
   border: 1px solid ${colours.functional.primary};
 `;
 
@@ -15,14 +15,16 @@ export const Title = styled.div`
   color: ${colours.functional.greyText};
   font-family: ${fonts.headline};
   font-size: 28px;
-  line-height: 31px;
+  line-height: 26px;
+  height: 10px;
   text-align: center;
 `;
 
 export const Text = styled.div`
-  padding-top: 8px;
+  padding-top: 35px;
   color: ${colours.functional.primary};
   font-family: ${fonts.supporting};
   font-size: 18px;
   text-align: center;
+  line-height: 12px;
 `;
