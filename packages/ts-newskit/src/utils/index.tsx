dfc6f01ba1811787d5e15f8e65658d8a87b0e611
suneethanagella <105289286+suneethanagella@users.noreply.github.com>
2023-08-01 18:01:40 +0530

    feat(TMPZ-44): Allow the reader to click through to L2 pages from L1 puzzle sections (#3469)
    * Link for titles
    
    * Added common styles
    
    * Test case for contentwrapper
    
    * test case to cover date formats

--- packages/ts-newskit/src/utils/index.tsx
+++ packages/ts-newskit/src/utils/index.tsx
@@ -4,3 +4,4 @@ export { TCThemeProvider, themes } from './TCThemeProvider';
 export { GridOverlay } from './gridOverlay';
 export { convertDateToWeekday } from './date-formatting';
 export { updateThemeTypography } from './theme-formatting';
+export { ContentWrapper } from './contentWrapper';
