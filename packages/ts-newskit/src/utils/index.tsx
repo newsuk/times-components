3fefdaae373924ea8eab435d3773be14d7af6efe
Adam Osborne <me@adamosborne.com>
2023-08-02 16:34:58 +0100

    feat(TMRX-1152): corrected export from TC

--- packages/ts-newskit/src/utils/index.tsx
+++ packages/ts-newskit/src/utils/index.tsx
@@ -3,5 +3,5 @@ export { getWidth } from './getWidth';
 export { TCThemeProvider, themes } from './TCThemeProvider';
 export { GridOverlay } from './gridOverlay';
 export { convertDateToWeekday } from './date-formatting';
-export { updateThemeTypography } from './theme-formatting';
+export { formatThemeOverrides } from './theme-formatting';
 export { ContentWrapper } from './contentWrapper';
