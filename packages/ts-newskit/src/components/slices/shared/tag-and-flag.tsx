ff9f2e3442a5f9cd8c049c84520a07973059012b
Bibi Collins <bibivictoria.collins@news.co.uk>
2023-08-04 15:04:27 +0100

    Add tracking call to tag

--- packages/ts-newskit/src/components/slices/shared/tag-and-flag.tsx
+++ packages/ts-newskit/src/components/slices/shared/tag-and-flag.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Block, TextBlock, Divider } from 'newskit';
 import { ContainerInline, TextLink } from '../shared-styles';
+import { TrackingContextProvider, TrackingContext } from '@times-components/ts-components';
 
 export interface TagAndFlagProps {
   flag?: string;
@@ -23,7 +24,23 @@ export const TagAndFlag = ({
     return null;
   }
 
+  const clickEvent = () => ({
+    action: 'Clicked',
+    attrs: {
+      event_navigation_action: 'navigation',
+      event_navigation_name: 'article category selected',
+      event_navigation_browsing_method: 'click',
+      article_parent_name: hasTag && tag.label
+    }
+  });
+
+  const handleClick = (fireAnalyticsEvent: (evt: TrackingContext) => void) => {
+    fireAnalyticsEvent && fireAnalyticsEvent(clickEvent());
+  };
+
   return (
+    <TrackingContextProvider>
+    {({ fireAnalyticsEvent }) => (
     <Block marginBlockStart={marginBlockStart} data-testid="tag-and-flag">
       {tag && (
         <TextLink
@@ -32,6 +49,7 @@ export const TagAndFlag = ({
             stylePreset: 'inkBrand010'
           }}
           href={tag.href}
+          onClick={() => handleClick(fireAnalyticsEvent)}
         >
           {tag.label}
         </TextLink>
@@ -61,5 +79,7 @@ export const TagAndFlag = ({
         </TextBlock>
       )}
     </Block>
+        )}
+    </TrackingContextProvider>
   );
 };
