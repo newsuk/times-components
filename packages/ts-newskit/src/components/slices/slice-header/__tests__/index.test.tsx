787a05fc9b74a68be1d16bb8d708b61081048168
Bibi Collins <bibivictoria.collins@news.co.uk>
2023-08-11 15:01:03 +0100

    TEMP: Remove slice header tracking as currently used as block title which has its own tracking in render

--- packages/ts-newskit/src/components/slices/slice-header/__tests__/index.test.tsx
+++ packages/ts-newskit/src/components/slices/slice-header/__tests__/index.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '../../../../utils/test-utils';
+import { render } from '../../../../utils/test-utils';
 import { SliceHeader } from '../index';
-import { TrackingContextProvider } from '../../../../utils/TrackingContextProvider';
-import mockDate from 'mockdate';
 
 describe('Render Header', () => {
   it('should render a snapshot', () => {
@@ -27,27 +25,6 @@ describe('Render Header', () => {
     );
     const text = getByText('Rugby Union');
     expect(text).toHaveStyle('color: #01000d');
-  });
-  it('should trigger tracking', () => {
-    mockDate.set(1620000000000);
-    const analyticsStream = jest.fn();
-    const { getByRole } = render(
-      <TrackingContextProvider analyticsStream={analyticsStream}>
-        <SliceHeader title="Rugby Union" href="https://www.thetimes.co.uk/" />
-      </TrackingContextProvider>
-    );
-    fireEvent.click(getByRole('link'));
-    expect(analyticsStream).toHaveBeenCalledWith({
-      action: 'Clicked',
-      object: 'SliceHeader',
-      attrs: {
-        article_parent_name: 'Rugby Union',
-        eventTime: '2021-05-03T00:00:00.000Z',
-        event_navigation_action: 'navigation',
-        event_navigation_browsing_method: 'click',
-        event_navigation_name: 'title block link'
-      }
-    });
   });
   it('does not render the icon button if no href is supplied', () => {
     const { queryByRole } = render(<SliceHeader title="Rugby Union" />);
--- packages/ts-newskit/src/components/slices/slice-header/index.tsx
+++ packages/ts-newskit/src/components/slices/slice-header/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { NewsKitChevronRightIcon } from '../../../assets';
 import { Block, FlagSize, IconButton, Stack, TitleBar } from 'newskit';
-import {
-  TrackingContextProvider,
-  TrackingContext
-} from '../../../utils/TrackingContextProvider';
 
 export interface SliceHeaderProps {
   title: string;
@@ -22,27 +18,7 @@ export const SliceHeader = ({
   iconArrowSize = 'iconSize020',
   iconSize = 'medium',
   padding = 'space030'
-}: SliceHeaderProps) => {
-  const clickEvent = () => ({
-    action: 'Clicked',
-    attrs: {
-      event_navigation_action: 'navigation',
-      event_navigation_name: 'title block link',
-      event_navigation_browsing_method: 'click',
-      article_parent_name: title
-    }
-  });
-
-  const handleClick = (fireAnalyticsEvent: (evt: TrackingContext) => void) => {
-    fireAnalyticsEvent && fireAnalyticsEvent(clickEvent());
-  };
-  return (
-    <TrackingContextProvider
-      context={{
-        object: 'SliceHeader'
-      }}
-    >
-      {({ fireAnalyticsEvent }) => (
+}: SliceHeaderProps) => (
         <Block stylePreset="sliceHeaderPreset">
           <Stack
             flow="horizontal-center"
@@ -70,14 +46,10 @@ export const SliceHeader = ({
                 }}
                 role="link"
                 href={href}
-                onClick={() => handleClick(fireAnalyticsEvent)}
               >
                 <NewsKitChevronRightIcon />
               </IconButton>
             )}
           </Stack>
         </Block>
-      )}
-    </TrackingContextProvider>
   );
-};
