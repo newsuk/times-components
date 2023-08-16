0fbccab35613151adb369e3bb35a778e65c8223e
yukiinasawa <yuki.inasawa@news.co.uk>
2023-08-16 12:49:32 +0100

    feat(tmrx-1388): fix lint

--- packages/ts-newskit/src/components/navigation/secondary-menu/mobile/navItems.tsx
+++ packages/ts-newskit/src/components/navigation/secondary-menu/mobile/navItems.tsx
@@ -4,32 +4,31 @@ import { NavItemsContainer } from '../styles';
 import { SecondaryMenuOptions, SecondaryMenuItem } from '../types';
 
 export const NavItems: React.FC<{
-    options: SecondaryMenuOptions;
-    data: SecondaryMenuItem[];
+  options: SecondaryMenuOptions;
+  data: SecondaryMenuItem[];
 }> = ({ options, data }) => {
-    const { handleSelect, isSelected } = options;
+  const { handleSelect , isSelected } = options;
 
-    return (
-        <NavItemsContainer>
-            {data.map(({ url, slug, title }) => (
-                <MenuItem
-                    overrides={{
-                        paddingInlineStart: 'space060',
-                        paddingInlineEnd: 'space060',
-                        marginBlockStart: '-2px',
-                        marginInlineEnd: '-2px',
-                        stylePreset: 'secondaryMenuItem',
-                        typographyPreset:
-                            isSelected === title ? 'robotoBold' : 'newPreset020'
-                    }}
-                    href={url}
-                    id={`vertical-${slug}`}
-                    onClick={() => handleSelect(slug)}
-                    key={slug}
-                >
-                    {title}
-                </MenuItem>
-            ))}
-        </NavItemsContainer>
-    );
+  return (
+    <NavItemsContainer>
+      {data.map(({ url, slug, title }) => (
+        <MenuItem
+          overrides={{
+            paddingInlineStart: 'space060',
+            paddingInlineEnd: 'space060',
+            marginBlockStart: '-2px',
+            marginInlineEnd: '-2px',
+            stylePreset: 'secondaryMenuItem',
+            typographyPreset: isSelected === title ? 'robotoBold' : 'newPreset020'
+          }}
+          href={url}
+          id={`vertical-${slug}`}
+          onClick={() => handleSelect(slug)}
+          key={slug}
+        >
+         {title}
+        </MenuItem>
+      ))}
+    </NavItemsContainer>
+  );
 };
