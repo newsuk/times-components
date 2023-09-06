f7a294bfe1b1b324002a6d3b39147d2545a70811
Michael Douglas <michael.douglas1@news.co.uk>
2023-09-06 13:16:53 +0100

    feat(TMRX-1441): add spacing on slices

--- packages/ts-newskit/src/slices/section-bucket/index.tsx
+++ packages/ts-newskit/src/slices/section-bucket/index.tsx
@@ -86,7 +86,7 @@ export const SectionBucket = ({
 
   return isMobile ? (
     <Scroll
-      overrides={{ overlays: { stylePreset: 'transparentBackground' } }}
+      overrides={{ overlays: { stylePreset: 'transparentBackground' }, marginBlock: 'space040' }}
       tabIndex={undefined}
     >
       {ArticleStackBlocks}
@@ -99,6 +99,7 @@ export const SectionBucket = ({
         lg: '976px',
         xl: '1276px'
       }}
+      marginBlock="space040"
     >
       {ArticleStackBlocks}
     </BlockItem>
