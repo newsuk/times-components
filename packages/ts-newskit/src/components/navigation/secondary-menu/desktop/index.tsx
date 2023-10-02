8080ece98edb4a9e8712afba5682dda97eb1ae2f
yukiinasawa <yuki.inasawa@news.co.uk>
2023-10-02 23:48:32 +0900

    feat(tmrx-1490): updated divider width

--- packages/ts-newskit/src/components/navigation/secondary-menu/desktop/index.tsx
+++ packages/ts-newskit/src/components/navigation/secondary-menu/desktop/index.tsx
@@ -2,18 +2,18 @@ import React, { Fragment } from 'react';
 import { MenuDivider } from '../styles';
 import { SecondaryMenuOptions, SecondaryMenuItem } from '../types';
 import { CreateMenu } from './create-menu';
-import { useBreakpointKey } from 'newskit';
+// import { useBreakpointKey } from 'newskit';
 
 export const SecondaryNavDesktop: React.FC<{
   options: SecondaryMenuOptions;
   data: SecondaryMenuItem[];
 }> = ({ options, data }) => {
-  const breakpointKey = useBreakpointKey();
+  // const breakpointKey = useBreakpointKey();
 
   return (
     <Fragment>
       <CreateMenu data={data} options={options} />
-      {/* <MenuDivider breakpointKey={breakpointKey} /> */}
+      <MenuDivider />
     </Fragment>
   );
 };
