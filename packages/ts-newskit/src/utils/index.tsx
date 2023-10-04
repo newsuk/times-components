733bf03a10cad60cee4c05fcba0de16be5c79f48
Michael Douglas <michael.douglas1@news.co.uk>
2023-10-04 14:44:24 +0100

    chore(TMRX-0000): export themes as object

--- packages/ts-newskit/src/utils/index.tsx
+++ packages/ts-newskit/src/utils/index.tsx
@@ -1,6 +1,6 @@
 export { debounce } from './debounced';
 export { getWidth } from './getWidth';
-export { TCThemeProvider, themes } from './TCThemeProvider';
+export { TCThemeProvider, TimesWebThemes } from './TCThemeProvider';
 export { GridOverlay } from './gridOverlay';
 export { convertDateToWeekday } from './date-formatting';
 export { formatThemeOverrides } from './theme-formatting';
