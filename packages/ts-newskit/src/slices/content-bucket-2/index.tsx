064de48cb645d712d5154776488f7d3cbe06477f
Michael Douglas <michael.douglas1@news.co.uk>
2023-10-12 14:07:41 +0100

    feat(TMRX-1556): refactor content bucket 2

--- packages/ts-newskit/src/slices/content-bucket-2/index.tsx
+++ packages/ts-newskit/src/slices/content-bucket-2/index.tsx
@@ -1,5 +1,5 @@
-import { GridLayout, useBreakpointKey, BreakpointKeys } from 'newskit';
-import React, { useState, useEffect } from 'react';
+import { GridLayout, Visible, Hidden, Block } from 'newskit';
+import React from 'react';
 import { Article, ArticleProps } from '../../components/slices/article';
 import { StackItem, StyledDivider } from '../shared-styles';
 import { CustomStackLayout } from '../shared';
@@ -14,65 +14,65 @@ export interface ContentBucket2Props {
 export const ContentBucket2 = ({
   articles,
   clickHandler
-}: ContentBucket2Props) => {
-  const [currentBreakpoint, setBreakpoint] = useState<BreakpointKeys>('xl');
-  const breakpointKey = useBreakpointKey();
-  useEffect(
-    () => {
-      setBreakpoint(breakpointKey);
-    },
-    [breakpointKey]
-  );
-
-  const isMob = currentBreakpoint === 'xs' || currentBreakpoint === 'sm';
-
-  return (
-    <CustomStackLayout>
-      <StackItem
-        $width={{
-          xs: '100%',
-          md: '720px',
-          lg: '976px',
-          xl: '1276px'
+}: ContentBucket2Props) => (
+  <CustomStackLayout>
+    <StackItem
+      $width={{
+        xs: '100%',
+        md: '720px',
+        lg: '976px',
+        xl: '1276px'
+      }}
+    >
+      <GridLayout
+        columns={{
+          xs: '1fr',
+          md: '1fr 1px 1fr 1px 1fr 1px 1fr'
         }}
+        columnGap="space040"
+        rowGap="space000"
+        data-testid="article-container"
       >
-        <GridLayout
-          columns={{
-            xs: '1fr',
-            md: '1fr 1px 1fr 1px 1fr 1px 1fr'
-          }}
-          columnGap="space040"
-          rowGap="space040"
-          data-testid="article-container"
-        >
-          {articles.map((article: ArticleProps, articleIndex, articleArr) => {
-            const articleBorder = articleIndex < articleArr.length - 1 &&
-              !isMob && (
-                <StyledDivider
-                  overrides={{ stylePreset: 'lightDivider' }}
-                  vertical
-                />
-              );
+        {articles.map((article: ArticleProps, articleIndex, articleArr) => {
+          const articleBorder = articleIndex < articleArr.length - 1 && (
+            <StyledDivider
+              overrides={{ stylePreset: 'lightDivider' }}
+              vertical
+            />
+          );
 
-            const isAfterFirstArticle = isMob && articleIndex > 0;
+          return (
+            <React.Fragment key={article.id}>
+              <Block marginBlockEnd={{xs: 'space040', md: 'space000'}}>
+                <Visible xs sm>
+                  <Article
+                    article={{
+                      ...clearCreditsAndCaption(article),
+                      hideImage: articleIndex > 0,
+                      isLeadImage: articleIndex === 0,
+                      hasTopBorder: articleIndex > 0
+                    }}
+                    clickHandler={clickHandler}
+                  />
+                </Visible>
+                <Hidden xs sm>
+                  <Article
+                    article={{
+                      ...clearCreditsAndCaption(article),
+                      hideImage: false,
+                      isLeadImage: false,
+                      hasTopBorder: false
+                    }}
+                    clickHandler={clickHandler}
+                  />
+                </Hidden>
+              </Block>
+              {articleBorder}
+            </React.Fragment>
+          );
+        })}
+      </GridLayout>
+    </StackItem>
+  </CustomStackLayout>
+);
 
-            return (
-              <React.Fragment key={article.headline}>
-                <Article
-                  article={{
-                    ...clearCreditsAndCaption(article),
-                    hideImage: isAfterFirstArticle,
-                    isLeadImage: isMob && articleIndex === 0,
-                    hasTopBorder: isMob && articleIndex > 0
-                  }}
-                  clickHandler={clickHandler}
-                />
-                {articleBorder}
-              </React.Fragment>
-            );
-          })}
-        </GridLayout>
-      </StackItem>
-    </CustomStackLayout>
-  );
-};
