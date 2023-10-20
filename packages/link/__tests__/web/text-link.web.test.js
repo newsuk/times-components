666ed528663969f7742032b7778036b06549b7d2
Hrvoje Maracic <hrvoje.maracic@q.agency>
2023-10-20 17:34:03 +0200

    feat(TMP-464): extracted urls for link

--- packages/link/__tests__/web/text-link.web.test.js
+++ packages/link/__tests__/web/text-link.web.test.js
@@ -2,24 +2,25 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TextLink from "../../src/text-link";
+import { BASE_DOMAIN } from "../../constants";
 
 describe("Test Link", () => {
   it("renders the link", () => {
     const { baseElement, getByText, getByRole } = render(
-      <TextLink onPress={() => {}} url="http://thetimes.co.uk">
+      <TextLink onPress={() => {}} url={`http://${BASE_DOMAIN}`}>
         The Times
       </TextLink>
     );
     expect(baseElement).toMatchSnapshot();
     expect(getByText("The Times")).toBeVisible();
-    expect(getByRole("link")).toHaveAttribute("href", "http://thetimes.co.uk");
+    expect(getByRole("link")).toHaveAttribute("href", `http://${BASE_DOMAIN}`);
   });
   it("renders the link with the styles passed down", () => {
     const { baseElement, getByText, getByRole } = render(
       <TextLink
         onPress={() => {}}
         style={{ color: "#006699", backgroundColor: "#006699" }}
-        url="http://thetimes.co.uk"
+        url={`http://${BASE_DOMAIN}`}
       >
         The Times
       </TextLink>
@@ -37,7 +38,7 @@ describe("Test Link", () => {
       <TextLink
         onPress={mockOnPress}
         style={{ color: "#006699", backgroundColor: "#006699" }}
-        url="http://thetimes.co.uk"
+        url={`http://${BASE_DOMAIN}`}
       >
         The Times
       </TextLink>
@@ -48,23 +49,23 @@ describe("Test Link", () => {
   });
   it("renders the link with children", () => {
     const { baseElement, getByText, getByRole } = render(
-      <TextLink onPress={() => {}} url="http://thetimes.co.uk">
+      <TextLink onPress={() => {}} url={`http://${BASE_DOMAIN}`}>
         <p>Children</p>
       </TextLink>
     );
     expect(baseElement).toMatchSnapshot();
     expect(getByText("Children")).toBeVisible();
-    expect(getByRole("link")).toHaveAttribute("href", "http://thetimes.co.uk");
+    expect(getByRole("link")).toHaveAttribute("href", `http://${BASE_DOMAIN}`);
   });
   it("renders the text link with target passed to it", () => {
     const { baseElement, getByText, getByRole } = render(
-      <TextLink onPress={() => {}} url="http://thetimes.co.uk" target="_blank">
+      <TextLink onPress={() => {}} url={`http://${BASE_DOMAIN}`} target="_blank">
         <p>Children</p>
       </TextLink>
     );
     expect(baseElement).toMatchSnapshot();
     expect(getByText("Children")).toBeVisible();
-    expect(getByRole("link")).toHaveAttribute("href", "http://thetimes.co.uk");
+    expect(getByRole("link")).toHaveAttribute("href", `http://${BASE_DOMAIN}`);
     expect(getByRole("link")).toHaveAttribute("target", "_blank");
   });
 });
---
+++ packages/link/constants.js
@@ -0,0 +1,1 @@
+export const BASE_DOMAIN = "thetimes.co.uk";
