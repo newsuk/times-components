9266833b9ef9b488423767875e597d0849116d4f
Hrvoje Maracic <hrvoje.maracic@news.co.uk>
2023-10-23 18:30:52 +0200

    feat(TMP-464): extracted urls for ts-slices

--- packages/ts-slices/src/fixtures/getArticle.ts
+++ packages/ts-slices/src/fixtures/getArticle.ts
@@ -1,6 +1,7 @@
 import shuffle from 'lodash.shuffle';
 
 import { SliceArticle } from '../types/slice';
+import { BASE_URL, IMAGE_BASE_URL } from '../constants';
 
 const sportArticles: SliceArticle[] = [
   {
@@ -11,13 +12,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fa505cdb4-e191-11ea-9a3a-c45f6db42dc9.jpg?crop=4203%2C2364%2C32%2C196'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fa505cdb4-e191-11ea-9a3a-c45f6db42dc9.jpg?crop=4203%2C2364%2C32%2C196`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fa505cdb4-e191-11ea-9a3a-c45f6db42dc9.jpg?crop=4203%2C2802%2C32%2C21'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fa505cdb4-e191-11ea-9a3a-c45f6db42dc9.jpg?crop=4203%2C2802%2C32%2C21`
         }
       ]
     },
@@ -27,8 +26,7 @@ const sportArticles: SliceArticle[] = [
       'Antonio Conte’s misfits show Premier League what it’s been missing',
     datePublished: '2021-05-22T23:01:00.000Z',
     dateUpdated: '2021-05-23T01:50:23.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/antonio-contes-misfits-show-premier-league-what-its-been-missing-h77bz7n73'
+    url: `${BASE_URL}/article/antonio-contes-misfits-show-premier-league-what-its-been-missing-h77bz7n73`
   },
   {
     id: '225c4dce-e21e-11ea-980d-ea124c352a60',
@@ -38,13 +36,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fae513a54-e22f-11ea-980d-ea124c352a60.jpg?crop=4384%2C2466%2C137%2C155'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fae513a54-e22f-11ea-980d-ea124c352a60.jpg?crop=4384%2C2466%2C137%2C155`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fae513a54-e22f-11ea-980d-ea124c352a60.jpg?crop=4603%2C3069%2C0%2C0'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fae513a54-e22f-11ea-980d-ea124c352a60.jpg?crop=4603%2C3069%2C0%2C0`
         }
       ]
     },
@@ -54,8 +50,7 @@ const sportArticles: SliceArticle[] = [
       'From a teenage dream to battling the biggest clubs in Europe, the rise of Glasgow City',
     datePublished: '2021-05-14T16:00:00.000Z',
     dateUpdated: '2021-05-14T17:25:08.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/from-a-teenage-dream-to-battling-the-biggest-clubs-in-europe-the-rise-of-glasgow-city-c72gvbhgj'
+    url: `${BASE_URL}/article/from-a-teenage-dream-to-battling-the-biggest-clubs-in-europe-the-rise-of-glasgow-city-c72gvbhgj`
   },
   {
     id: '44bb39b0-df13-11ea-a18f-15f41f6d2fa7',
@@ -64,13 +59,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2Fa17d0694-df16-11ea-a18f-15f41f6d2fa7.jpg?crop=3254%2C1830%2C43%2C361'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2Fa17d0694-df16-11ea-a18f-15f41f6d2fa7.jpg?crop=3254%2C1830%2C43%2C361`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2Fa17d0694-df16-11ea-a18f-15f41f6d2fa7.jpg?crop=3352%2C2235%2C0%2C0'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2Fa17d0694-df16-11ea-a18f-15f41f6d2fa7.jpg?crop=3352%2C2235%2C0%2C0`
         }
       ]
     },
@@ -79,8 +72,7 @@ const sportArticles: SliceArticle[] = [
     headline: 'Ben Spencer shines on his debut as Bath dominate London Irish',
     datePublished: '2020-08-15T23:01:00.000Z',
     dateUpdated: '2020-08-15T20:39:04.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/ben-spencer-shines-on-his-debut-as-bath-dominate-london-irish-9kq9gj3pk'
+    url: `${BASE_URL}/article/ben-spencer-shines-on-his-debut-as-bath-dominate-london-irish-9kq9gj3pk`
   },
   {
     id: '199af56e-dca6-11ea-be0a-064b00f2181c',
@@ -89,13 +81,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Feb034180-dcae-11ea-be0a-064b00f2181c.jpg?crop=2923%2C1644%2C32%2C231'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Feb034180-dcae-11ea-be0a-064b00f2181c.jpg?crop=2923%2C1644%2C32%2C231`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fceab2304-dcae-11ea-be0a-064b00f2181c.jpg?crop=2967%2C1978%2C0%2C0'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fceab2304-dcae-11ea-be0a-064b00f2181c.jpg?crop=2967%2C1978%2C0%2C0`
         }
       ]
     },
@@ -104,8 +94,7 @@ const sportArticles: SliceArticle[] = [
     headline: 'Even at 40, Venus Williams can still serve up something new',
     datePublished: '2020-08-12T23:01:00.000Z',
     dateUpdated: '2020-08-13T02:41:59.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/even-at-40-venus-williams-can-still-serve-up-something-new-kzxlxnxtg'
+    url: `${BASE_URL}/article/even-at-40-venus-williams-can-still-serve-up-something-new-kzxlxnxtg`
   },
   {
     id: '975fffa6-d8d4-11ea-a099-b011877a86c1',
@@ -115,13 +104,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F6394f40a-d8d5-11ea-a099-b011877a86c1.jpg?crop=2804%2C1577%2C14%2C155'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F6394f40a-d8d5-11ea-a099-b011877a86c1.jpg?crop=2804%2C1577%2C14%2C155`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F6394f40a-d8d5-11ea-a099-b011877a86c1.jpg?crop=2804%2C1869%2C14%2C9'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F6394f40a-d8d5-11ea-a099-b011877a86c1.jpg?crop=2804%2C1869%2C14%2C9`
         }
       ]
     },
@@ -130,8 +117,7 @@ const sportArticles: SliceArticle[] = [
     headline: 'Danny Tudhope can upset form book to complete double at Ascot',
     datePublished: '2020-08-07T23:01:00.000Z',
     dateUpdated: '2020-08-07T22:18:31.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/danny-tudhope-can-upset-form-book-to-complete-double-at-ascot-zmshctw73'
+    url: `${BASE_URL}/article/danny-tudhope-can-upset-form-book-to-complete-double-at-ascot-zmshctw73`
   },
   {
     id: '296c4da8-dada-11ea-8d9c-6a97b944a97b',
@@ -140,13 +126,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F687165ce-dada-11ea-8d9c-6a97b944a97b.jpg?crop=3037%2C1708%2C295%2C66'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F687165ce-dada-11ea-8d9c-6a97b944a97b.jpg?crop=3037%2C1708%2C295%2C66`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F687165ce-dada-11ea-8d9c-6a97b944a97b.jpg?crop=3037%2C2024%2C263%2C66'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F687165ce-dada-11ea-8d9c-6a97b944a97b.jpg?crop=3037%2C2024%2C263%2C66`
         }
       ]
     },
@@ -156,8 +140,7 @@ const sportArticles: SliceArticle[] = [
       '‘Maybe I’m not as good as I was’ – Rory McIlroy ponders major drought',
     datePublished: '2020-08-10T08:00:00.000Z',
     dateUpdated: '2020-08-10T09:35:26.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/maybe-im-not-as-good-as-i-was-rory-mcilroy-ponders-major-drought-gdtvb3gr9'
+    url: `${BASE_URL}/article/maybe-im-not-as-good-as-i-was-rory-mcilroy-ponders-major-drought-gdtvb3gr9`
   },
   {
     id: 'bf8c09e2-d65f-11ea-ae2c-1f6a5007a0a3',
@@ -167,13 +150,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F7fe41d12-da50-11ea-8bfe-166b156f6daa.jpg?crop=4646%2C2613%2C116%2C502'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F7fe41d12-da50-11ea-8bfe-166b156f6daa.jpg?crop=4646%2C2613%2C116%2C502`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F7fe41d12-da50-11ea-8bfe-166b156f6daa.jpg?crop=4878%2C3252%2C0%2C0'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F7fe41d12-da50-11ea-8bfe-166b156f6daa.jpg?crop=4878%2C3252%2C0%2C0`
         }
       ]
     },
@@ -183,8 +164,7 @@ const sportArticles: SliceArticle[] = [
       'Lando Norris, the Formula One boy wonder racing in today’s grand prix',
     datePublished: '2020-08-08T23:01:00.000Z',
     dateUpdated: '2020-08-09T14:57:04.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/lando-norris-formula-one-mclaren-silverstone-grand-prix-zrzb9cv9q'
+    url: `${BASE_URL}/article/lando-norris-formula-one-mclaren-silverstone-grand-prix-zrzb9cv9q`
   },
   {
     id: '862a106a-dffe-11ea-9a3a-c45f6db42dc9',
@@ -194,13 +174,11 @@ const sportArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '16:9',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb64edc3c-e001-11ea-9a3a-c45f6db42dc9.jpg?crop=2758%2C1551%2C633%2C278'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb64edc3c-e001-11ea-9a3a-c45f6db42dc9.jpg?crop=2758%2C1551%2C633%2C278`
         },
         {
           ratio: '3:2',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb64edc3c-e001-11ea-9a3a-c45f6db42dc9.jpg?crop=2626%2C1751%2C782%2C277'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb64edc3c-e001-11ea-9a3a-c45f6db42dc9.jpg?crop=2626%2C1751%2C782%2C277`
         }
       ]
     },
@@ -209,8 +187,7 @@ const sportArticles: SliceArticle[] = [
     headline: 'Nike may drop 36 British athletes',
     datePublished: '2020-08-16T23:01:00.000Z',
     dateUpdated: '2020-08-16T22:51:54.000Z',
-    url:
-      'https://www.thetimes.co.uk/article/nike-may-drop-36-british-athletes-x5x9c3dg2'
+    url: `${BASE_URL}/article/nike-may-drop-36-british-athletes-x5x9c3dg2`
   }
 ];
 
@@ -221,8 +198,7 @@ const sportCommentArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '1:1',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F5e3d6642-e0d0-11ea-9a3a-c45f6db42dc9.png?crop=420%2C420%2C2327%2C1042'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F5e3d6642-e0d0-11ea-9a3a-c45f6db42dc9.png?crop=420%2C420%2C2327%2C1042`
         }
       ]
     },
@@ -231,8 +207,7 @@ const sportCommentArticles: SliceArticle[] = [
     datePublished: '2020-08-18T16:00:00.000Z',
     dateUpdated: '2020-08-17T22:58:03.000Z',
     template: 'maincomment',
-    url:
-      'https://www.thetimes.co.uk/article/why-manchester-united-win-so-many-penalties-nzgzkdtj8'
+    url: `${BASE_URL}/article/why-manchester-united-win-so-many-penalties-nzgzkdtj8`
   },
   {
     id: '6416fad8-e22e-11ea-9a3a-c45f6db42dc9',
@@ -241,8 +216,7 @@ const sportCommentArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '1:1',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F0ec9e850-e234-11ea-980d-ea124c352a60.jpg?crop=1586%2C1586%2C272%2C180'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F0ec9e850-e234-11ea-980d-ea124c352a60.jpg?crop=1586%2C1586%2C272%2C180`
         }
       ]
     },
@@ -252,8 +226,7 @@ const sportCommentArticles: SliceArticle[] = [
     datePublished: '2021-05-14T16:00:00.000Z',
     dateUpdated: '2021-05-14T17:25:07.000Z',
     template: 'maincomment',
-    url:
-      'https://www.thetimes.co.uk/article/welcome-fawad-alam-to-the-ministry-of-silly-stances-50zvt5b92'
+    url: `${BASE_URL}/article/welcome-fawad-alam-to-the-ministry-of-silly-stances-50zvt5b92`
   },
   {
     id: '7eaa3842-dfdb-11ea-9a3a-c45f6db42dc9',
@@ -262,8 +235,7 @@ const sportCommentArticles: SliceArticle[] = [
       crops: [
         {
           ratio: '1:1',
-          url:
-            'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb6874c8e-dfde-11ea-9a3a-c45f6db42dc9.jpg?crop=1460%2C1460%2C1337%2C338'
+          url: `${IMAGE_BASE_URL}/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fb6874c8e-dfde-11ea-9a3a-c45f6db42dc9.jpg?crop=1460%2C1460%2C1337%2C338`
         }
       ]
     },
@@ -273,8 +245,7 @@ const sportCommentArticles: SliceArticle[] = [
     datePublished: '2020-08-16T23:01:00.000Z',
     dateUpdated: '2020-08-16T22:33:56.000Z',
     template: 'magazinecomment',
-    url:
-      'https://www.thetimes.co.uk/article/marcus-smith-is-a-class-act-now-but-do-not-ignore-ben-spencer-d3v320mm6'
+    url: `${BASE_URL}/article/marcus-smith-is-a-class-act-now-but-do-not-ignore-ben-spencer-d3v320mm6`
   }
 ];
 
