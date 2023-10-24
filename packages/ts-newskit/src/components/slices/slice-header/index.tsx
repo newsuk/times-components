75712d027000919073ecaae3f7b64f72f867b645
milanharia-tm <milan.haria@news.co.uk>
2023-10-24 12:34:05 +0100

    feat(TMRX-1562): update slice header

--- packages/ts-newskit/src/components/slices/slice-header/index.tsx
+++ packages/ts-newskit/src/components/slices/slice-header/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NewsKitChevronRightIcon } from '../../../assets';
-import { Block, FlagSize, IconButton, TitleBar } from 'newskit';
-import { SliceHeaderContainer } from './styles';
+import { FlagSize, IconButton, TitleBar, Visible, MQ } from 'newskit';
+import {
+  SliceHeaderWrapper,
+  SliceHeaderLink,
+  SliceHeaderContainer
+} from './styles';
 
 export interface SliceHeaderProps {
   title: string;
   href?: string;
-  titleTypographyPreset?: string;
-  iconArrowSize?: string;
-  iconSize?: FlagSize;
+  titleTypographyPreset?: MQ<string>;
+  iconArrowSize?: MQ<string>;
+  iconSize?: MQ<FlagSize>;
   padding?: string;
   sliceHeaderClickHandler: (title: string) => void;
 }
@@ -16,46 +20,72 @@ export interface SliceHeaderProps {
 export const SliceHeader = ({
   title,
   href,
-  titleTypographyPreset = 'editorialDisplay004',
-  iconArrowSize = 'iconSize020',
-  iconSize = 'medium',
+  titleTypographyPreset = {
+    xs: 'editorialDisplay003',
+    md: 'editorialDisplay004'
+  },
+  iconArrowSize = { xs: 'iconSize010', md: 'iconSize020' },
+  iconSize = { xs: 'small', md: 'medium' },
   padding = 'space030',
   sliceHeaderClickHandler
 }: SliceHeaderProps) => {
   return (
-    <Block stylePreset="sliceHeaderPreset">
-      <SliceHeaderContainer
-        flow="horizontal-center"
-        stackDistribution="space-between"
-        paddingBlock={padding}
+    <SliceHeaderWrapper
+      stylePreset={{
+        xs: 'sliceHeaderPresetMobile',
+        md: 'sliceHeaderPresetDesktop'
+      }}
+    >
+      <SliceHeaderLink
+        href={href}
+        onClick={() => sliceHeaderClickHandler(title)}
       >
-        <TitleBar
-          overrides={{
-            heading: {
-              typographyPreset: titleTypographyPreset,
-              stylePreset: 'inkBrand010'
-            },
-            paddingInline: 'space000',
-            paddingBlock: 'space000'
-          }}
+        <SliceHeaderContainer
+          flow="horizontal-center"
+          stackDistribution="space-between"
+          paddingBlock={padding}
+          paddingInline={{ xs: 'space045', md: 'space000' }}
         >
-          {title}
-        </TitleBar>
-        {href && (
-          <IconButton
-            size={iconSize}
+          <TitleBar
             overrides={{
-              stylePreset: 'sliceIconPreset',
-              iconSize: iconArrowSize
+              heading: {
+                typographyPreset: titleTypographyPreset,
+                stylePreset: 'inkBrand010'
+              },
+              paddingInline: 'space000',
+              paddingBlock: 'space000'
             }}
-            role="link"
-            href={href}
-            onClick={() => sliceHeaderClickHandler(title)}
           >
-            <NewsKitChevronRightIcon />
-          </IconButton>
-        )}
-      </SliceHeaderContainer>
-    </Block>
+            {title}
+          </TitleBar>
+          {href && (
+            <>
+              <Visible xs sm>
+                <IconButton
+                  size={typeof iconSize === 'object' ? iconSize.xs : iconSize}
+                  overrides={{
+                    stylePreset: 'sliceIconPreset',
+                    iconSize: iconArrowSize
+                  }}
+                >
+                  <NewsKitChevronRightIcon />
+                </IconButton>
+              </Visible>
+              <Visible md lg xl>
+                <IconButton
+                  size={typeof iconSize === 'object' ? iconSize.md : iconSize}
+                  overrides={{
+                    stylePreset: 'sliceIconPreset',
+                    iconSize: iconArrowSize
+                  }}
+                >
+                  <NewsKitChevronRightIcon />
+                </IconButton>
+              </Visible>
+            </>
+          )}
+        </SliceHeaderContainer>
+      </SliceHeaderLink>
+    </SliceHeaderWrapper>
   );
 };
--- packages/ts-newskit/src/theme/times-web-light/style-presets/slice-header.ts
+++ packages/ts-newskit/src/theme/times-web-light/style-presets/slice-header.ts
@@ -1,5 +1,13 @@
 export const sliceHeaderStylePresets = {
-  sliceHeaderPreset: {
+  sliceHeaderPresetMobile: {
+    base: {
+      borderTop: 'solid',
+      borderWidth: '{{borders.borderWidth030}}',
+      borderColor: '{{colors.sectionBrand060}}',
+      borderRadius: '{{borders.borderRadiusSharp}}'
+    }
+  },
+  sliceHeaderPresetDesktop: {
     base: {
       borderTop: 'dashed',
       borderWidth: '{{borders.borderWidth010}}',
@@ -12,12 +20,6 @@ export const sliceHeaderStylePresets = {
       backgroundColor: '{{colors.interactiveSecondary020}}',
       color: '{{colors.inkBrand010}}',
       borderRadius: '{{borders.borderRadiusCircle}}'
-    },
-    hover: {
-      backgroundColor: '{{colors.interface040}}'
-    },
-    active: {
-      backgroundColor: '{{colors.interface050}}'
     }
   }
 };
