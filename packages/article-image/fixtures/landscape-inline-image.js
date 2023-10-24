292c622708229214a8653812800be96dd3acfa45
Hrvoje Maracic <hrvoje.maracic@q.agency>
2023-10-24 17:23:51 +0200

    feat(TMP-464): extracted urls for article-image

--- packages/article-image/fixtures/landscape-inline-image.js
+++ packages/article-image/fixtures/landscape-inline-image.js
@@ -1,5 +1,7 @@
+import { IMAGE_BASE_URL } from "../constants";
+
 const defaultImageSrc =
-  "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F3ac674bc-beb2-11e7-8bb9-94e1372175c0.jpg?crop=5576%2C3717%2C0%2C0";
+  `${IMAGE_BASE_URL}/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F3ac674bc-beb2-11e7-8bb9-94e1372175c0.jpg?crop=5576%2C3717%2C0%2C0`;
 const defaultCaption = "All the latest stories in culture and books.";
 const defaultCredits = "Times photographer Jack Hill";
 
--- packages/article-image/fixtures/secondary-image.js
+++ packages/article-image/fixtures/secondary-image.js
@@ -1,5 +1,7 @@
+import { IMAGE_BASE_URL } from "../constants";
+
 const defaultImageSrc =
-  "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F4476fabc-be54-11e7-b58a-4186f6049f2e.jpg?crop=5760%2C3840%2C0%2C0";
+  `${IMAGE_BASE_URL}/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F4476fabc-be54-11e7-b58a-4186f6049f2e.jpg?crop=5760%2C3840%2C0%2C0`;
 const defaultCaption =
   "Sebastian Schwarz, Glyndbourne’s general director, took issue with the BBC";
 const defaultCredits = "Times photographer Richard Pohle";
