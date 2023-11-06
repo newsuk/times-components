95b64f4c9168cfde842e1712cbeb1450d2adcd2f
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-11-06 11:15:52 +0000

    remove space

--- packages/related-articles/src/related-articles-heading.js
+++ packages/related-articles/src/related-articles-heading.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { TcView, checkStylesForUnits } from "@times-components/utils";
-
 import styles from "./styles";
 
 export const HeadingForRelatedArticles = styled.h3`
