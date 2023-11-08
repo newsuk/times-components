e9573b0eabae0a4edd7a0f5255248d0f612093fe
Adam Osborne <me@adamosborne.com>
2023-11-08 14:14:56 +0000

    feat(TMPZ-199): updated puzzles category for improved CWV

--- packages/ts-newskit/src/components/puzzles/cards-container/ScrollControls.tsx
+++ packages/ts-newskit/src/components/puzzles/cards-container/ScrollControls.tsx
@@ -52,6 +52,7 @@ export const ScrollControls = ({
         }}
         onClick={() => controlsHandler('left')}
         data-testid="scroll-left"
+        aria-aria-label='scroll left'
       >
         <NewskitIconBack
           overrides={{
@@ -68,6 +69,7 @@ export const ScrollControls = ({
         }}
         onClick={() => controlsHandler('right')}
         data-testid="scroll-right"
+        aria-label='scroll right'
       >
         <NewskitIconForward
           overrides={{
