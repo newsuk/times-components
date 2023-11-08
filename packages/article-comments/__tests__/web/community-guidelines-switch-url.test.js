17d189c75df7a8a529173bff608322ae6d918c13
Olivia Thetgyi <othetgyi@gmail.com>
2023-11-08 14:32:18 +0000

    feat(TMG-1049): add function to replace thetimes with uat or staging version

--- packages/article-comments/__tests__/web/community-guidelines-switch-url.test.js
+++ packages/article-comments/__tests__/web/community-guidelines-switch-url.test.js
@@ -1,4 +1,4 @@
-import { getEnvironmentName } from "../../src/community-guidelines-url-switch";
+import { getEnvironmentName, changeCommunityGuidelinesUrl } from "../../src/community-guidelines-switch-url";
 
 describe("communityGuidelinesUrlSwitcher", () => {
   it("should find the environment from the window object", () => {
@@ -8,10 +8,17 @@ describe("communityGuidelinesUrlSwitcher", () => {
     expect(getEnvironmentName()).toEqual("uat");
   });
 
-  // it("should put uat in the url if the environment is UAT", () => {
+  it("should add uat- before thetimes in the URL if the environment is UAT", () => {
+    window.__TIMES_CONFIG__ = {
+      environmentName: "uat"
+    }
+    expect(changeCommunityGuidelinesUrl()).toEqual("https://www.uat-thetimes.co.uk/article/f4024fbe-d989-11e6-9063-500e6740fc32");
+  });
 
-  // });
-  // it("should should add staging to the url if the environment is staging", () => {
-
-  // });
+  it("should should add staging- before thetimes in the url if the environment is staging", () => {
+    window.__TIMES_CONFIG__ = {
+      environmentName: "staging"
+    }
+    expect(changeCommunityGuidelinesUrl()).toEqual("https://www.staging-thetimes.co.uk/article/f4024fbe-d989-11e6-9063-500e6740fc32");
+  });
 })
