3e3602e22cd0c0226a7d339c8335652e811c1c36
yukiinasawa <yuki.inasawa@news.co.uk>
2023-11-13 15:00:20 +0000

    feat(tmrx-1648): temp jest commented out

--- packages/ts-newskit/src/components/navigation/secondary-menu/__tests__/secondary-navigation.test.tsx
+++ packages/ts-newskit/src/components/navigation/secondary-menu/__tests__/secondary-navigation.test.tsx
@@ -4,15 +4,15 @@ import '@testing-library/jest-dom';
 import { mainMenuItems } from '../fixtures/menu-items.json';
 import { SecondaryNavigation } from '../index';
 import {
-  cleanup
+  cleanup,
   // fireEvent,
-  // getAllByTestId,
+  getAllByTestId
   // waitFor
 } from '@testing-library/react';
 
 jest.mock('newskit', () => ({
   ...jest.requireActual('newskit'),
-  useBreakpointKey: jest.fn().mockReturnValue('sm')
+  useBreakpointKey: jest.fn().mockReturnValue('md')
 }));
 
 const mockClickHandler = jest.fn();
@@ -39,56 +39,56 @@ describe('Secondary Menu', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  // it('should not highlight any titles when default index is not provided and slug is not found', () => {
-  //   const { container } = render(
-  //     <SecondaryNavigation
-  //       data={[mainMenuItems[0], mainMenuItems[1]]}
-  //       pageSlug=""
-  //       title=""
-  //       stickyTop={0}
-  //       onClick={() => {
-  //         // noop
-  //       }}
-  //       clickHandler={mockClickHandler}
-  //     />
-  //   );
+  it('should not highlight any titles when default index is not provided and slug is not found', () => {
+    const { container } = render(
+      <SecondaryNavigation
+        data={[mainMenuItems[0], mainMenuItems[1]]}
+        pageSlug=""
+        title=""
+        stickyTop={0}
+        onClick={() => {
+          // noop
+        }}
+        clickHandler={mockClickHandler}
+      />
+    );
 
-  //   const firstMenuItem = getAllByTestId(container, 'buttonLink')[0];
-  //   const secondMenuItem = getAllByTestId(container, 'buttonLink')[1];
+    const firstMenuItem = getAllByTestId(container, 'buttonLink')[0];
+    const secondMenuItem = getAllByTestId(container, 'buttonLink')[1];
 
-  //   expect(firstMenuItem).toHaveStyle({
-  //     'border-bottom': '4px solid transparent'
-  //   });
-  //   expect(secondMenuItem).toHaveStyle({
-  //     'border-bottom': '4px solid transparent'
-  //   });
-  // });
+    expect(firstMenuItem).toHaveStyle({
+      'border-bottom': '4px solid transparent'
+    });
+    expect(secondMenuItem).toHaveStyle({
+      'border-bottom': '4px solid transparent'
+    });
+  });
 
-  // it('should highlight the correct title when default index is provided', () => {
-  //   const { container } = render(
-  //     <SecondaryNavigation
-  //       data={[mainMenuItems[0], mainMenuItems[1]]}
-  //       pageSlug=""
-  //       title=""
-  //       stickyTop={0}
-  //       defaultSelectedIndex={0}
-  //       onClick={() => {
-  //         // noop
-  //       }}
-  //       clickHandler={mockClickHandler}
-  //     />
-  //   );
+  it('should highlight the correct title when default index is provided', () => {
+    const { container } = render(
+      <SecondaryNavigation
+        data={[mainMenuItems[0], mainMenuItems[1]]}
+        pageSlug=""
+        title=""
+        stickyTop={0}
+        defaultSelectedIndex={0}
+        onClick={() => {
+          // noop
+        }}
+        clickHandler={mockClickHandler}
+      />
+    );
 
-  //   const firstMenuItem = getAllByTestId(container, 'buttonLink')[0];
-  //   const secondMenuItem = getAllByTestId(container, 'buttonLink')[1];
+    const firstMenuItem = getAllByTestId(container, 'buttonLink')[0];
+    const secondMenuItem = getAllByTestId(container, 'buttonLink')[1];
 
-  //   expect(firstMenuItem).toHaveStyle({
-  //     'border-bottom': '4px solid #01000d'
-  //   });
-  //   expect(secondMenuItem).toHaveStyle({
-  //     'border-bottom': '4px solid transparent'
-  //   });
-  // });
+    expect(firstMenuItem).toHaveStyle({
+      'border-bottom': '4px solid #01000d'
+    });
+    expect(secondMenuItem).toHaveStyle({
+      'border-bottom': '4px solid transparent'
+    });
+  });
 
   // it('should close the dropdown ', () => {
   //   const { getByTestId, queryByText, getAllByText } = render(
