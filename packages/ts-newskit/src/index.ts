2eb01fe3c2171adaad879051289a3db1ccb2725c
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-11-16 16:06:30 +0000

    feat: export lead story 4

--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -61,6 +61,7 @@ export { ContentBucket3 } from './slices/content-bucket-3';
 export { LeadStory1 } from './slices/lead-story-1';
 export { LeadStory2 } from './slices/lead-story-2';
 export { LeadStory3 } from './slices/lead-story-3';
+export { LeadStory4 } from './slices/lead-story-4';
 export { SectionBucket } from './slices/section-bucket';
 export { StackModule1 } from './slices/stacked-module-1';
 export { CommentBucket1 } from './slices/comment-bucket-1';
