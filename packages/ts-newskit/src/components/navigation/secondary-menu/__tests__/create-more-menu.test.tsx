5fe0eb7acb8d3046f8b0b5d3dea47a349320ec00
yukiinasawa <yuki.inasawa@news.co.uk>
2023-11-17 09:15:10 +0000

    feat(tmrx-1648): tests updated

--- packages/ts-newskit/src/components/navigation/secondary-menu/__tests__/create-more-menu.test.tsx
+++ packages/ts-newskit/src/components/navigation/secondary-menu/__tests__/create-more-menu.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '../../../../utils/test-utils';
 import '@testing-library/jest-dom';
 import { mainMenuItems } from '../fixtures/menu-items.json';
 import { cleanup, fireEvent } from '@testing-library/react';
 import { CreateMoreMenu } from '../desktop/create-more-menu';
+import { BreakpointKeys } from 'newskit';
+import { renderComponent } from '../../../../utils';
 
 const options = {
   handleSelect: jest.fn(),
@@ -14,86 +15,57 @@ const options = {
 
 const mockClickHandler = jest.fn();
 
+const renderMoreMenu = (size: BreakpointKeys = 'xl') =>
+  renderComponent(
+    <CreateMoreMenu
+      data={mainMenuItems}
+      options={options}
+      clickHandler={mockClickHandler}
+    />,
+    size
+  );
+
 describe('Create More Menu', () => {
-  // beforeEach(() => {
-  //   Object.defineProperty(window, 'innerWidth', {
-  //     writable: true,
-  //     configurable: true,
-  //     value: 1000
-  //   });
-  // });
   afterEach(() => {
     jest.clearAllMocks();
     cleanup();
   });
 
   it('should render snapshot', () => {
-    const { asFragment } = render(
-      <CreateMoreMenu
-        data={mainMenuItems}
-        options={options}
-        clickHandler={mockClickHandler}
-      />
-    );
+    const { asFragment } = renderMoreMenu();
     expect(asFragment()).toMatchSnapshot();
   });
   it('should render the menu item', () => {
-    const { getByText } = render(
-      <CreateMoreMenu
-        data={mainMenuItems}
-        options={options}
-        clickHandler={mockClickHandler}
-      />
-    );
+    const { getByText } = renderMoreMenu();
     const title = getByText('Puzzles');
     expect(title).toBeInTheDocument();
   });
-  // it('items should have ancher with href', () => {
-  //   const { getAllByTestId } = render(
-  //     <CreateMoreMenu
-  //       data={mainMenuItems}
-  //       options={options}
-  //       clickHandler={mockClickHandler}
-  //     />
-  //   );
-  //   const title = getAllByTestId('buttonLink')[0];
-  //   expect(title).toHaveAttribute('href', '/puzzles');
-  // });
-
   it('items should have ancher with href', () => {
-    const { getByText } = render(
-      <CreateMoreMenu
-        data={mainMenuItems}
-        options={options}
-        clickHandler={mockClickHandler}
-      />
-    );
+    const { getAllByTestId } = renderMoreMenu();
+    const title = getAllByTestId('buttonLink')[0];
+    expect(title).toHaveAttribute('href', '/home');
+  });
+  it('items should have ancher with href', () => {
+    const { getByText } = renderMoreMenu();
     const title = getByText('Puzzles');
     fireEvent.click(title);
     expect(options.handleSelect).toHaveBeenCalled();
     expect(options.setIsExpanded).toHaveBeenCalled();
   });
-
-  // it('items should have length of 2', () => {
-  //   const { getAllByRole } = render(
-  //     <CreateMoreMenu
-  //       data={mainMenuItems}
-  //       options={options}
-  //       clickHandler={mockClickHandler}
-  //     />
-  //   );
+  // investigate : mock screen width
+  // it('items should have length of 9', () => {
+  //   const { getAllByRole } = renderMoreMenu();
   //   const list = getAllByRole('listitem');
-  //   expect(list.length).toEqual(2);
+  //   expect(list.length).toEqual(9);
   // });
-  // it('items should have length of 8', () => {
-  //   const { getAllByRole } = render(
-  //     <CreateMoreMenu
-  //       data={mainMenuItems}
-  //       options={options}
-  //       clickHandler={mockClickHandler}
-  //     />
-  //   );
+  // it('items should have length of 9', () => {
+  //   const { getAllByRole } = renderMoreMenu('lg');
   //   const list = getAllByRole('listitem');
-  //   expect(list.length).toEqual(8);
+  //   expect(list.length).toEqual(9);
+  // });
+  // it('items should have length of 9', () => {
+  //   const { getAllByRole } = renderMoreMenu('md');
+  //   const list = getAllByRole('listitem');
+  //   expect(list.length).toEqual(9);
   // });
 });
