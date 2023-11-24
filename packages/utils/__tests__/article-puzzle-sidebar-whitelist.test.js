7221dba9304ca82176241951d97677e196535a7a
Adam Osborne <me@adamosborne.com>
2023-11-24 12:57:48 +0000

    linting

--- packages/utils/__tests__/article-puzzle-sidebar-whitelist.test.js
+++ packages/utils/__tests__/article-puzzle-sidebar-whitelist.test.js
@@ -2,9 +2,9 @@ import { CanShowPuzzleSidebar } from "../src/index";
 
 describe("CanShowPuzzleSidebar should", () => {
   it("not allow the sidebar to show on the news articles", () => {
-    expect(CanShowPuzzleSidebar('News')).toBeFalsy();
+    expect(CanShowPuzzleSidebar("News")).toBeFalsy();
   });
   it("allow the sidebar to show on Times2", () => {
-    expect(CanShowPuzzleSidebar('Times2')).toBeTruthy();
+    expect(CanShowPuzzleSidebar("Times2")).toBeTruthy();
   });
 });
--- packages/utils/src/article-puzzle-sidebar-whitelist.js
+++ packages/utils/src/article-puzzle-sidebar-whitelist.js
@@ -1,6 +1,6 @@
-const CanShowPuzzleSidebar = (currentsection) => {
-  const allowedSections = ['times2'];
-  if (allowedSections.includes(currentsection.toLowerCase())) {
+const CanShowPuzzleSidebar = currentsection => {
+  const allowedSections = ["times2"];
+  if(currentsection && allowedSections.includes(currentsection.toLowerCase())) {
     return true;
   }
   return false;
