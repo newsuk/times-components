7706450ae022d9eabc3ea5d174033002ad5ae7e0
ibrahim-kurhan <ibrahim.kurhan@news.co.uk>
2023-11-28 16:32:36 +0000

    fix: solve failed setup

--- packages/ts-newskit/src/components/navigation/global-nav/wrapped-components/WrappedComponents.tsx
+++ packages/ts-newskit/src/components/navigation/global-nav/wrapped-components/WrappedComponents.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { GlobalNav } from '..';
 import { HamburgerMenu } from '../hamburger-menu/HamburgerMenu';
 import { NavigationData } from '../types';
+import { HAMBURGER_MENU, GLOBAL_NAVIGATION } from '../../constants';
 
 export const WrappedGlobalNav: React.FC<{
   data: NavigationData;
-  clickHandler: (title: string) => void;
-}> = ({ data, clickHandler }) => (
-  <GlobalNav data={data} clickHandler={clickHandler} />
-);
+  clickHandler: (title: string, section: string) => void;
+}> = ({ data, clickHandler }) => {
+  const hamburgerClickHandler = (title: string) => {
+    clickHandler(title, GLOBAL_NAVIGATION);
+  };
+  return <GlobalNav data={data} clickHandler={hamburgerClickHandler} />;
+};
 
 export const WrappedHamburger: React.FC<{
   data: NavigationData;
   isLoggedIn: boolean;
-  clickHandler: (title: string) => void;
-}> = ({ data, isLoggedIn, clickHandler }) => (
-  <HamburgerMenu
-    isLoggedIn={isLoggedIn}
-    data={data}
-    clickHandler={clickHandler}
-  />
-);
+  clickHandler: (title: string, section: string) => void;
+}> = ({ data, isLoggedIn, clickHandler }) => {
+  const hamburgerClickHandler = (title: string) => {
+    clickHandler(title, HAMBURGER_MENU);
+  };
+  return (
+    <HamburgerMenu
+      isLoggedIn={isLoggedIn}
+      data={data}
+      hamburgerClickHandler={hamburgerClickHandler}
+    />
+  );
+};
