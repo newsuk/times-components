a1eb38c34b30c8f334223488dfd26f8034f335cb
ibrahim-kurhan <116718171+ibrahim-kurhan@users.noreply.github.com>
2023-12-05 10:42:22 +0000

    feat(TMRX-1582): add tracking to global navigation (#3672)
    * feat(TMRX-1582): add tracking to global navigation
    
    * fix: resolve failed test cases
    
    * add test cases
    
    * fix: change test cases
    
    * fix: add test cases
    
    * fix: add test cases
    
    * fix: add test cases
    
    * fix: add test cases
    
    * fix: add test cases
    
    * fix: add test cases
    
    * fix: change the way function passed
    
    * fix: solve failed setup
    
    * add space
    
    * remove unused nav component
    
    * move ga event to global nav
    
    * add type
    
    * add provider
    
    * remove provider
    
    * add click event to masthead
    
    * chore: remove dublicate function

--- packages/ts-newskit/src/components/navigation/global-nav/hamburger-menu/NavButtons.tsx
+++ packages/ts-newskit/src/components/navigation/global-nav/hamburger-menu/NavButtons.tsx
@@ -5,17 +5,20 @@ import { HamburgerStyledMenu } from '../styles';
 const NavButtonSection: React.FC<{
   setSelected: (title: string) => void;
   selected: string;
-}> = ({ setSelected, selected }) => (
+  clickHandler: (title: string) => void;
+}> = ({ setSelected, selected, clickHandler }) => (
   <HamburgerStyledMenu role="region" aria-label="Navigation Menu">
     <LoggedInMenuItem
       title="Sections"
       setSelected={setSelected}
       selected={selected}
+      clickHandler={clickHandler}
     />
     <LoggedInMenuItem
       title="My account"
       setSelected={setSelected}
       selected={selected}
+      clickHandler={clickHandler}
     />
   </HamburgerStyledMenu>
 );
