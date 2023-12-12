c6365c0999c39e5f7f12b9be16d9c61aaa25efd7
Bibi Collins <44647540+bibicollins1@users.noreply.github.com>
2023-12-12 12:15:37 +0000

    fix(TMRX-1745): Fix ads on list view slice (#3688)
    * Update id
    
    * Fix tests

--- packages/ad/src/ad-container.js
+++ packages/ad/src/ad-container.js
@@ -8,7 +8,7 @@ const AdContainer = ({ slotName, style }) => {
   const adMap = {
     header: "ad-header",
     "inline-ad": "ad-article-inline",
-    articleListAd: "inline-ad",
+    articleListAd: "ad-article-inline",
     pixel: "ad-pixel",
     pixelteads: "ad-pixelteads",
     pixelskin: "ad-pixelskin",
