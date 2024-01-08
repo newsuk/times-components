301f4bde69f919f618382bc1c5b8746bf1bd6582
Peace <149379909+peacewilliams@users.noreply.github.com>
2024-01-08 18:21:30 +0000

    creating theme for business channel (#3706)

--- packages/ts-newskit/src/theme/index.ts
+++ packages/ts-newskit/src/theme/index.ts
@@ -3,3 +3,4 @@ export { TimesWebLightTheme } from './times-web-light';
 export { TimesWebLightSportTheme } from './times-web-light-sport';
 export { TimesWebLightTravelTheme } from './times-web-light-travel';
 export { PuzzlesWebLightTheme } from './puzzles-web-light';
+export { TimesWebLightBusinessTheme } from './times-web-light-business';
