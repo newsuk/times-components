51008efca24e0cbc4e22341ac2987cc7f3b48bd8
Adam Osborne <me@adamosborne.com>
2024-01-11 16:05:08 +0000

    feat(TMPZ-280): reversed display order of mini-quiz hints

--- packages/ts-newskit/src/components/puzzles/daily-quiz/hints/styles.ts
+++ packages/ts-newskit/src/components/puzzles/daily-quiz/hints/styles.ts
@@ -1,5 +1,14 @@
-import { styled, TextBlock } from 'newskit';
+import { Block, styled, TextBlock } from 'newskit';
 
 export const StyledTextBlock = styled(TextBlock)`
   text-align: center;
+`;
+
+export const HintContainerBlock = styled(Block)`
+  display: flex;
+  flex-wrap: wrap-reverse;
+
+  &> div {
+    width: 100%;
+  }
 `;
