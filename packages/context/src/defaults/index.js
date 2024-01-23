484e58a393bbe1b60e682cfeef620721c213b9c7
flashcheeks <callumchristie@hotmail.com>
2024-01-23 08:33:35 +0000

    feat(TMRX-000): Update article path

--- packages/context/src/defaults/index.js
+++ packages/context/src/defaults/index.js
@@ -3,7 +3,8 @@ import { scales } from "@times-components/ts-styleguide";
 export default {
   makeArticleUrl: ({ path, slug, shortIdentifier }) =>
     slug && shortIdentifier
-      ? `https://www.thetimes.co.uk/${path || 'article'}/${slug}-${shortIdentifier}`
+      ? `https://www.thetimes.co.uk/${path ||
+          "article"}/${slug}-${shortIdentifier}`
       : "",
   makeTopicUrl: ({ slug }) => `/topic/${slug}`,
   theme: {
