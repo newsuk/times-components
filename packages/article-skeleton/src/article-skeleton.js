6c931fd4c7ce5a68053acfdfb20a45302ea277aa
Bibi Collins <bibivictoria.collins@news.co.uk>
2024-01-23 15:33:16 +0000

    Try fix error

--- packages/article-skeleton/src/article-skeleton.js
+++ packages/article-skeleton/src/article-skeleton.js
@@ -232,7 +232,7 @@ const ArticleSkeleton = ({
                 )}
               </HeaderContainer>
               <BodyContainer>
-                {categoryConnection?.nodes[0]?.title && <div>{categoryConnection?.nodes[0]?.title}</div>}
+                {!!categoryConnection?.nodes[0]?.title && <div>{categoryConnection?.nodes[0]?.title}</div>}
                 {!!zephrDivs && (
                   <StaticContent
                     html={
