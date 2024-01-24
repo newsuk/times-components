5af207fc031700e870dd55205459a15740b527ca
Michael Douglas <michael.douglas1@news.co.uk>
2024-01-24 17:06:21 +0000

    feat(TMRX-1850): add tagline to sliceheader

--- packages/ts-newskit/src/components/slices/slice-header/styles.tsx
+++ packages/ts-newskit/src/components/slices/slice-header/styles.tsx
@@ -1,4 +1,14 @@
-import { Block, Stack, getColorCssFromTheme, styled } from 'newskit';
+import {
+  Block,
+  Stack,
+  getColorCssFromTheme,
+  styled,
+  getMediaQueryFromTheme,
+  TextBlock,
+  Divider,
+  TitleBar,
+  IconButton
+} from 'newskit';
 
 export const SliceHeaderWrapper = styled(Block)`
   & a:hover {
@@ -29,4 +39,35 @@ export const SliceHeaderLink = styled.a`
 
 export const SliceHeaderContainer = styled(Stack)`
   min-height: 72px;
+`;
+
+export const TitleBarContainer = styled(Stack)`
+  max-width: 760px;
+`;
+
+export const StyledTitleBar = styled(TitleBar)`
+  ${getMediaQueryFromTheme('sm')} {
+    white-space: nowrap;
+  }
+`;
+export const StyledDivider = styled(Divider)`
+  display: none;
+  ${getMediaQueryFromTheme('sm')} {
+    display: block;
+    height: 28px;
+  }
+`;
+export const StyledIconButton = styled(IconButton)`
+  flex-shrink: 0;
+`;
+export const Tagline = styled(TextBlock)`
+  display: none;
+  ${getMediaQueryFromTheme('sm')} {
+    display: block;
+  }
+`;
+export const TaglineMob = styled(TextBlock)`
+  ${getMediaQueryFromTheme('sm')} {
+    display: none;
+  }
 `;
