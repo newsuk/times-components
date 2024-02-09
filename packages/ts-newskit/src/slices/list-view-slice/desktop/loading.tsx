545fd0f0b6ddf7479d2468c00e98b3e48e2a599d
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2024-02-09 16:31:10 +0000

    chore(TMRX-1911): remove slices and all related files

--- packages/ts-newskit/src/slices/list-view-slice/desktop/loading.tsx
+++ packages/ts-newskit/src/slices/list-view-slice/desktop/loading.tsx
@@ -1,3 +1,8 @@
+// IMPORTANT: This component is in the process of being moved to the main repository.
+// Please avoid making any changes to this file for the time being.
+// For updates or modifications, refer to the main repository once the move is complete.
+// In case of emergencies, please reach out to reader-experience team for further assistance.
+
 import React, { Fragment } from 'react';
 import { Divider, Block, Stack } from 'newskit';
 import { StyledMainDivider, LoadingBlock } from '../styles';
