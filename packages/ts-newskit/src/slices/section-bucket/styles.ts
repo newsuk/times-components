cf08f3430200fd5bf85630293e6ca36fb3a52b76
ibrahim-kurhan <116718171+ibrahim-kurhan@users.noreply.github.com>
2024-02-12 12:31:45 +0000

    chore(TMRX-1911): remove slices and all related files (#3764)

--- packages/ts-newskit/src/slices/section-bucket/styles.ts
+++ packages/ts-newskit/src/slices/section-bucket/styles.ts
@@ -1,3 +1,8 @@
+// IMPORTANT: This component is in the process of being moved to the main repository.
+// Please avoid making any changes to this file for the time being.
+// For updates or modifications, refer to the main repository once the move is complete.
+// In case of emergencies, please reach out to reader-experience team for further assistance.
+
 import { Block, styled, getMediaQueryFromTheme } from 'newskit';
 
 export const StyledBlock = styled(Block)`
