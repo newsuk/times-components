52ece53c2feed7aca6e02ffe14527b5f3a2e6c18
amerkadicE <amer@q.agency>
2024-02-15 10:11:43 +0100

    feat(TMRX-1852): Apply Culture theme to Culture channel page

---
+++ packages/ts-newskit/src/theme/times-web-light-culture/index.tsx
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { createTheme } from 'newskit';
+import TheTimesLightCulture from '@newskit-themes/the-times/TheTimes-light-culture.json.json';
+import { stylePresets } from '../times-web-light/style-presets';
+import { typographyPresets } from '../times-web-light/styles/typography-presets';
+import { formatThemeOverrides } from '../../utils/theme-formatting';
+
+const themeOverrides = {
+  stylePresets,
+  typographyPresets
+};
+
+const updatedTheme = formatThemeOverrides(
+  TheTimesLightCulture,
+  themeOverrides
+);
+
+export const TimesWebLightCultureTheme = createTheme({
+  name: 'times-web-light-culture',
+  ...updatedTheme
+});
