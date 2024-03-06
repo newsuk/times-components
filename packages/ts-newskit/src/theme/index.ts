e29dcd654be93db5db73095c1921fc2fdfcf7870
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2024-03-06 13:12:57 +0000

    feat(TMRX-1975): implement world channel design theme

--- packages/ts-newskit/src/theme/index.ts
+++ packages/ts-newskit/src/theme/index.ts
@@ -5,3 +5,6 @@ export { TimesWebLightTravelTheme } from './times-web-light-travel';
 export { PuzzlesWebLightTheme } from './puzzles-web-light';
 export { TimesWebLightBusinessTheme } from './times-web-light-business';
 export { TimesWebLightCultureTheme } from './times-web-light-culture';
+export { TimesWebLightWorldTheme } from './times-web-light-world';
+
+
