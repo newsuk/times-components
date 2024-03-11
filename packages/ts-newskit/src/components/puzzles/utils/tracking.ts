9691075ea4579fb4251fd276cc3407020b6ea9f7
adamosborne-tnl <89213300+adamosborne-tnl@users.noreply.github.com>
2024-03-11 10:21:07 +0000

    feat(TMPZ-19): adding pass through click tracking (#3779)
    * feat(TMPZ-19): adding pass through click tracking
    
    * moved tracking
    
    * fix: fix storybook issues
    
    * fix: remove href, add any
    
    * feat: add href="" and preventDefault
    
    * fix: yarn fix
    
    * added extra tests
    
    * linting
    
    * minor update to update canary release
    
    ---------
    
    Co-authored-by: Ivan Mariic <ivan.mariic@q.agency>

---
+++ packages/ts-newskit/src/components/puzzles/utils/tracking.ts
@@ -0,0 +1,17 @@
+import {
+  PuzzleScrollClickHandlerType,
+  MouseEventType
+} from '../cards-container/types';
+
+type TrackingPuzzleCategoryProps = {
+  nextPrev: string;
+  puzzleType: string;
+};
+
+export const puzzleCategoryClickTracking = (
+  event: MouseEventType,
+  puzzleScroll: TrackingPuzzleCategoryProps,
+  clickHandler: PuzzleScrollClickHandlerType
+) => {
+  puzzleScroll && clickHandler(event, puzzleScroll);
+};
