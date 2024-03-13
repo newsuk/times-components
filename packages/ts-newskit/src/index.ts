8054ae6da4e7472dfb11ea5d6c1ca809dda1351e
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2024-03-13 16:10:56 +0000

    fix: remove exported puzzle components

--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -11,30 +11,6 @@ export { TimesWebLightTravelTheme } from './theme/times-web-light-travel';
 export { Breadcrumb } from './components/navigation/breadcrumb';
 
 // PUZZLES
-export { Archive } from './components/puzzles/archive';
-export { DateCard } from './components/puzzles/date-card';
-export { PuzzlesFlag } from './components/puzzles/flag';
-export { AccordionGroup } from './components/misc/accordion/accordion-group';
-export { BreakoutSection } from './components/puzzles/breakout-section';
-export { HeroBanner } from './components/puzzles/hero-banner';
-export {
-  TeaserCardsContainer
-} from './components/puzzles/teaserCards-container';
-export { HintButton } from './components/puzzles/daily-quiz/hint-button';
-export { HintDisplay } from './components/puzzles/daily-quiz/hints';
-export { HintContainer } from './components/puzzles/daily-quiz/hint-container';
-export { HowToPlay } from './components/puzzles/daily-quiz/how-to-play';
-export { FinalScreen } from './components/puzzles/daily-quiz/final-screen';
-export { CardsContainer } from './components/puzzles/cards-container';
-export { ContactUs } from './components/puzzles/contact-us';
-export { Faq } from './components/puzzles/faq';
-export { OffersBanner } from './components/puzzles/offers-banner';
-export { FreeTrial } from './components/puzzles/free-trial';
-export { PersonalStats } from './components/puzzles/personal-stats';
-export { PuzzleCard } from './components/puzzles/puzzle-card';
-export { StoryCardListing } from './components/puzzles/story-card-listing';
-export { StoryCard } from './components/puzzles/storyCard';
-export { SubscribeBanner } from './components/puzzles/subscribe-banner';
 export { ArticleSidebar } from './components/puzzles/article-sidebar';
 export { PuzzlesWebLightTheme } from './theme/puzzles-web-light/index';
 
