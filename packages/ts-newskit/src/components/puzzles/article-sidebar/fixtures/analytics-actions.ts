4f9a507df851fa98c6eadf73afa77eaa86c203af
suneethanagella <suneetha.nagella@news.co.uk>
2024-03-14 17:06:38 +0530

    changes

--- packages/ts-newskit/src/components/puzzles/article-sidebar/fixtures/analytics-actions.ts
+++ packages/ts-newskit/src/components/puzzles/article-sidebar/fixtures/analytics-actions.ts
@@ -2,6 +2,6 @@ import { action } from '@storybook/addon-actions';
 
 export default (event: any | any[]) => {
   // tslint:disable-next-line:no-console
-  console.log('analytics-action', event);
-  action('analytics-action')(event);
+  console.log('analytics-event', event);
+  action('analytics-event')(event);
 };
