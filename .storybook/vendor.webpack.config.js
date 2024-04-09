d69bf0f753aee4b37e00784aaf7e589031c4553e
flashcheeks <callumchristie@hotmail.com>
2024-04-09 13:37:11 +0100

    chore(TMW-000): Node 18 upgrade

--- .storybook/vendor.webpack.config.js
+++ .storybook/vendor.webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const webpack = require("webpack");
+const crypto = require("crypto");
+
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
 module.exports = {
   context: process.cwd(),
