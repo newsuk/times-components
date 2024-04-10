bfafaf52f3d4985608ad35d1722202b1c52e15b9
Saurav Arora <saurav.arora@news.co.uk>
2024-04-10 13:36:28 +0100

    fixed lint errors

--- packages/ts-newskit/src/components/misc/banner/index.tsx
+++ packages/ts-newskit/src/components/misc/banner/index.tsx
@@ -8,8 +8,7 @@ import {
   Wrapper,
   BannerWrapper
 } from './styles';
-import EmailIcon from '../../../assets/EmailIcon';
-import CloseIcon from '../../../assets/CloseIcon2';
+import { NewsKitCloseIcon2, NewsKitEmailIcon } from '../../../assets';
 
 type Props = {
   onClose: () => void;
@@ -23,13 +22,11 @@ export const Banner: React.FC<Props> = ({ title, body, onClose }) => {
       <NewsKitBanner aria-label="Email verification banner">
         <Wrapper>
           <TitleWrapper>
-            <EmailIcon />
+            <NewsKitEmailIcon />
             <Title>{title}</Title>
           </TitleWrapper>
-
-          {/* can be refactored */}
           <CloseIconWrapper onClick={onClose}>
-            <CloseIcon width={14} height={14} />
+            <NewsKitCloseIcon2 width={14} height={14} />
           </CloseIconWrapper>
         </Wrapper>
         <Body>{body}</Body>
