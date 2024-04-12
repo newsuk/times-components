f6f568bd7841bfca55f397a1c5876a90202ec14f
Bibi Collins <bibivictoria.collins@news.co.uk>
2024-04-12 11:56:23 +0100

    Changes without test updates'

---
+++ packages/ts-newskit/src/components/misc/banner/__tests__/banner.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '../../../../utils/test-utils';
+import { Banner } from '../banner';
+
+describe('Banner', () => {
+  it('renders the banner', () => {
+    const { asFragment } = render(
+      <Banner title="Title" body="Body" onClose={jest.fn} />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
