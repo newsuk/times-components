fc071808662bbd63dc9dd7f67fb10976f15a2aa8
seldinpQ <seldin.puce@news.co.uk>
2024-04-15 18:53:09 +0200

    chore(tmp-993): Run lint

--- packages/ssr/src/lib/graphql-error-link.js
+++ packages/ssr/src/lib/graphql-error-link.js
@@ -5,12 +5,16 @@ const errorLink = logger =>
     let msg = "";
     if (graphQLErrors) {
       graphQLErrors.forEach(({ message }) => {
-        msg = `[GraphQL error]: Message: ${message}, Query: ${operation.query}, Variables: ${JSON.stringify(operation.variables)}`;
+        msg = `[GraphQL error]: Message: ${message}, Query: ${
+          operation.query
+        }, Variables: ${JSON.stringify(operation.variables)}`;
         logger.error(msg);
       });
     }
     if (networkError) {
-      msg = `[GraphQL Network error]: ${networkError}, Query: ${operation.query}, Variables: ${JSON.stringify(operation.variables)}`;
+      msg = `[GraphQL Network error]: ${networkError}, Query: ${
+        operation.query
+      }, Variables: ${JSON.stringify(operation.variables)}`;
       logger.error(msg);
     }
   });
