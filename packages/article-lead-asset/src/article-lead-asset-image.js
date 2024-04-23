31cd2e44651cafc7754c64a74791177a4b6de72e
Luka Kerečeni <luka.kereceni@q.agency>
2024-04-23 11:50:23 +0200

    Remove high fetchpriority

--- packages/article-lead-asset/src/article-lead-asset-image.js
+++ packages/article-lead-asset/src/article-lead-asset-image.js
@@ -23,7 +23,6 @@ const LeadAssetImage = ({ aspectRatio, alt, uri }) => {
         style={styles.img}
         src={appendToImageURL(url, "resize", sizes[0])}
         srcSet={srcSet.join(",")}
-        fetchpriority="high"
       />
     </div>
   );
