565507a0141efd188a1cb516928e233aa0b9841d
Ivan Mariic <ivan.mariic@q.agency>
2024-05-09 12:17:36 +0200

    fix: add export, fix lint, update snapshots

--- packages/ts-components/src/components/article-sidebar/ArticleSidebar.stories.tsx
+++ packages/ts-components/src/components/article-sidebar/ArticleSidebar.stories.tsx
@@ -3,17 +3,30 @@ import { storiesOf } from '@storybook/react';
 import { ArticleSidebar } from './ArticleSidebar';
 
 const getAttributes = () => {
-
   const sectionTitle = 'Puzzles';
   const data = [
-    {title: 'Crossword', url: 'https://www.thetimes.co.uk/puzzles/crossword', imgUrl: 'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/crossword-c7ae8934ef.png'}, 
-    {title: 'Polygon', url: 'https://www.thetimes.co.uk/puzzles/word-puzzles', imgUrl: 'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/polygon-875ea55487.png'}, 
-    {title: 'Sudoku', url: 'https://www.thetimes.co.uk/puzzles/sudoku', imgUrl: 'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/sudoku-ee2aea0209.png'}
-  ]
+    {
+      title: 'Crossword',
+      url: 'https://www.thetimes.co.uk/puzzles/crossword',
+      imgUrl:
+        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/crossword-c7ae8934ef.png'
+    },
+    {
+      title: 'Polygon',
+      url: 'https://www.thetimes.co.uk/puzzles/word-puzzles',
+      imgUrl:
+        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/polygon-875ea55487.png'
+    },
+    {
+      title: 'Sudoku',
+      url: 'https://www.thetimes.co.uk/puzzles/sudoku',
+      imgUrl:
+        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/sudoku-ee2aea0209.png'
+    }
+  ];
   const pageLink = 'https://www.thetimes.co.uk/puzzles';
 
-
-  return { sectionTitle, data, pageLink};
+  return { sectionTitle, data, pageLink };
 };
 
 storiesOf('Typescript Component/Article Sidebar', module).add(
@@ -22,10 +35,8 @@ storiesOf('Typescript Component/Article Sidebar', module).add(
     const props = getAttributes();
 
     return (
-        <div style={{maxWidth: "204px"}}>
-      <ArticleSidebar
-        {...props}
-      />
+      <div style={{ maxWidth: '204px' }}>
+        <ArticleSidebar {...props} />
       </div>
     );
   }
