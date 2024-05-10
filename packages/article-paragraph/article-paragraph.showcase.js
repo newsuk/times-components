eb891c5e980f511144d9a68cc3cd6a74641b5ee4
Milan Haria <milan.haria@news.co.uk>
2024-05-10 15:36:04 +0100

    fix: circular dependency

--- packages/article-paragraph/article-paragraph.showcase.js
+++ packages/article-paragraph/article-paragraph.showcase.js
@@ -3,7 +3,6 @@ import React, { Fragment } from "react";
 import coreRenderers from "@times-components/markup";
 import { renderTree } from "@times-components/markup-forest";
 import { colours, themeFactory } from "@times-components/ts-styleguide";
-import { ArticleLink } from "@times-components/article-skeleton";
 import paragraphData from "./fixtures/paragraph-showcase.json";
 import dropCapData from "./fixtures/drop-cap-showcase.json";
 import dropCapShortTextData from "./fixtures/drop-cap-short-text-showcase.json";
@@ -43,13 +42,9 @@ const renderParagraph = ({ select, boolean }, ast) => {
       );
     },
     link(key, attributes, children) {
-      const { href, target, dropCap } = attributes;
+      const { href } = attributes;
 
-      return (
-        <ArticleLink dropCap={dropCap} key={key} target={target} url={href}>
-          {children}
-        </ArticleLink>
-      );
+      return <a href={href}>{children}</a>;
     }
   });
 };
