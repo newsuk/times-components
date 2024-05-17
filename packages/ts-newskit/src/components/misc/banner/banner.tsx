1bd5da6ecfd38af005553d39ed7e0965553f70e0
Bibi Collins <bibivictoria.collins@news.co.uk>
2024-05-17 11:56:22 +0100

    Fix linting

--- packages/ts-newskit/src/components/misc/banner/banner.tsx
+++ packages/ts-newskit/src/components/misc/banner/banner.tsx
@@ -21,18 +21,18 @@ export const Banner: React.FC<Props> = ({ title, body, onClose }) => {
   return (
     <BannerWrapper>
       <NewsKitBanner aria-label="Email verification banner">
-      <BannerContentWrapper role="region">
-        <Wrapper>
-          <TitleWrapper>
-            <EmailIcon />
-            <Title>{title}</Title>
-          </TitleWrapper>
-          <CloseIconWrapper onClick={onClose}>
-            <CloseIconBlack width={14} height={14} />
-          </CloseIconWrapper>
-        </Wrapper>
-        <Body>{body}</Body>
-      </BannerContentWrapper>
+        <BannerContentWrapper role="region">
+          <Wrapper>
+            <TitleWrapper>
+              <EmailIcon />
+              <Title>{title}</Title>
+            </TitleWrapper>
+            <CloseIconWrapper onClick={onClose}>
+              <CloseIconBlack width={14} height={14} />
+            </CloseIconWrapper>
+          </Wrapper>
+          <Body>{body}</Body>
+        </BannerContentWrapper>
       </NewsKitBanner>
     </BannerWrapper>
   );
