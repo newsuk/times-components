28014698a925197c222b11f96bab2b8bbc235e3d
Bibi Collins <44647540+bibicollins1@users.noreply.github.com>
2024-05-21 09:40:53 +0100

    feat(TMD-334): Remove newskit from breadcrumb component (#3850)
    * Replace BreadCrumb Item from newskit with styled component
    
    * Remove Breadcrumbs component and create custom
    
    * Update padding
    
    * Refactor
    
    * Add object name to tracking for object/component mapping
    
    * Update tests and move styles
    
    * Add aria-label, fix lint
    
    * Fix linting
    
    * Update snaps
    
    * chore(TMD-337): Remove toast
    
    * Export SVG
    
    * Removed unused icon
    
    * Fix linting

--- packages/ts-newskit/src/assets/index.tsx
+++ packages/ts-newskit/src/assets/index.tsx
@@ -34,3 +34,4 @@ export const NewsKitTooltipIcon = customToNewsKitIcon(
 export { default as CloseIconBlack } from '../assets/CloseIconBlack';
 export { default as EmailIcon } from '../assets/EmailIcon';
 export { default as FilledArrowIcon } from './FilledArrowIcon';
+export { default as BreadcrumbIcon } from './BreadcrumbIcon';
