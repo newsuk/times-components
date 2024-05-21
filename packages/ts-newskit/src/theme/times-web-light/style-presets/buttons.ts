5bf329428b83e20d1e4784c10b74796e718d6606
Bibi Collins <bibivictoria.collins@news.co.uk>
2024-05-21 12:38:26 +0100

    Remove unused style presets

--- packages/ts-newskit/src/theme/times-web-light/style-presets/buttons.ts
+++ packages/ts-newskit/src/theme/times-web-light/style-presets/buttons.ts
@@ -8,22 +8,4 @@ export const buttonStylePresets = {
         '{{borders.borderWidth020}} {{borders.borderWidth000}} {{borders.borderWidth000}} {{borders.borderWidth000}}'
     }
   },
-  updateButton: {
-    base: {
-      backgroundColor: '{{colors.interactiveNegative040}}',
-      borderRadius: '{{borders.borderRadiusSharp}}',
-      color: '{{colors.inkInverse}}',
-      shadow: '{{shadows.shadow040}}'
-    },
-    hover: {
-      backgroundColor: '{{colors.interactiveNegative050}}'
-    },
-    active: {
-      backgroundColor: '{{colors.interactiveNegative050}}'
-    },
-    loading: {
-      backgroundColor: '{{colors.interface040}}',
-      borderRadius: '{{borders.borderRadiusCircle}}'
-    }
-  }
 };
