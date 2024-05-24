1db4d5f44f5e73d63ad9c41d29f0aacdc0a59390
douglasmik <michael@viaverso.co.uk>
2024-05-24 09:42:35 +0100

    feat(TMRS-170): remove log

--- packages/ts-components/src/components/opta/utils/__tests__/replaceFlags.test.tsx
+++ packages/ts-components/src/components/opta/utils/__tests__/replaceFlags.test.tsx
@@ -37,37 +37,3 @@ describe('replaceFlags', () => {
     expect(transformedElements[1]).toHaveTextContent('Spain');
   });
 });
-
-// beforeAll(() => {
-//   spy = jest.spyOn(document, 'getElementById');
-// });
-
-// describe('activeListItem', () => {
-//   describe('with found element', () => {
-//     let mockElement;
-//     beforeAll(() => {
-//       // Here you create the element that the document.createElement will return
-//       // It might be even without an id
-//       mockElement = document.createElement(....);
-//       spy.mockReturnValue(mockElement);
-//     });
-
-//     // And then you could expect it to have the background
-//     it('should have the background applied', () => {
-//       expect(mockElement.style.backgroundColor).toBe('#ededed');
-//     });
-//   });
-
-//   describe('without found element', () => {
-//     // And here you can create a scenario
-//     // When document.createElement returns null
-//     beforeAll(() => {
-//       spy.mockReturnValue(null);
-//     });
-
-//     // And expect you function not to throw an error
-//     it('should not throw an error', () => {
-//       expect(() => activeListItem({id:'123'})).not.toThrow();
-//     });
-//   });
-// });
