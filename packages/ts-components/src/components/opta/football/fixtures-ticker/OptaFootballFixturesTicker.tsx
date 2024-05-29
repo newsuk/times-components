4d011af71c847784e0b8b67d79ccd327bce6be0b
douglasmik <48333391+douglasmik@users.noreply.github.com>
2024-05-29 10:11:54 +0100

    feat(TMRS-209): add filters for displayed fixtures (#3862)

--- packages/ts-components/src/components/opta/football/fixtures-ticker/OptaFootballFixturesTicker.tsx
+++ packages/ts-components/src/components/opta/football/fixtures-ticker/OptaFootballFixturesTicker.tsx
@@ -12,77 +12,94 @@ import {
 
 import { PlaceholderContainer } from '../shared-styles';
 import { WidgetContainer } from './styles';
-import { replaceFlags } from '../../utils/replaceFlags';
+import { replaceNationalTeamDetails } from '../../utils/replaceNationalTeamDetails';
 
 export const OptaFootballFixturesTicker: React.FC<{
   season: string;
   competition: string;
   date_from?: string;
   date_to?: string;
-}> = React.memo(({ season, competition, date_from, date_to }) => {
-  const ref = React.createRef<HTMLDivElement>();
+  days_ahead?: number;
+  days_before?: number;
+  round?: string;
+}> = React.memo(
+  ({
+    season,
+    competition,
+    date_from,
+    date_to,
+    days_ahead,
+    days_before,
+    round
+  }) => {
+    const ref = React.createRef<HTMLDivElement>();
 
-  const [isReady, setIsReady] = useState<boolean>(false);
-  const [optaImages, setOptaImages] = useState<
-    HTMLCollectionOf<Element> | undefined
-  >();
-  const nationalCompetitions = ['3', '5', '6', '235', '941', '1125'];
-  const isNationalCompetition = nationalCompetitions.includes(competition);
+    const [isReady, setIsReady] = useState<boolean>(false);
+    const [optaImages, setOptaImages] = useState<
+      HTMLCollectionOf<Element> | undefined
+    >();
+    const nationalCompetitions = ['3', '5', '6', '235', '941', '1125'];
+    const isNationalCompetition = nationalCompetitions.includes(competition);
 
-  useEffect(
-    () => {
-      const sport = 'football';
+    useEffect(
+      () => {
+        const sport = 'football';
 
-      initSettings();
-      initStyleSheet(sport);
+        initSettings();
+        initStyleSheet(sport);
 
-      initScript().then(() => {
-        if (ref.current) {
-          ref.current.innerHTML = initElement('opta-widget', {
-            sport,
-            widget: 'fixtures',
-            season,
-            competition,
-            date_from,
-            date_to,
-            live: true,
-            start_on_current: true,
-            template: 'strip',
-            team_naming: 'brief',
-            match_status: 'all',
-            order_by: 'date_ascending',
-            show_grouping: true,
-            show_crests: !isNationalCompetition,
-            show_date: true,
-            date_format: 'ddd Do MMM'
-          }).outerHTML;
+        initScript().then(() => {
+          if (ref.current) {
+            ref.current.innerHTML = initElement('opta-widget', {
+              sport,
+              widget: 'fixtures',
+              season,
+              competition,
+              date_from,
+              date_to,
+              days_ahead,
+              days_before,
+              round,
+              live: true,
+              start_on_current: true,
+              template: 'strip',
+              team_naming: 'brief',
+              match_status: 'all',
+              order_by: 'date_ascending',
+              show_grouping: true,
+              show_crests: !isNationalCompetition,
+              show_date: true,
+              show_live: true,
+              date_format: 'ddd Do MMM'
+            }).outerHTML;
 
-          initComponent();
-          setIsReady(true);
+            initComponent();
+            setIsReady(true);
 
-          if (isNationalCompetition) {
-            const TeamNameContainers =
-              ref.current &&
-              ref.current.getElementsByClassName('Opta-TeamName');
-            TeamNameContainers && setOptaImages(TeamNameContainers);
+            if (isNationalCompetition) {
+              const TeamNameContainers =
+                ref.current &&
+                ref.current.getElementsByClassName('Opta-TeamName');
+              TeamNameContainers && setOptaImages(TeamNameContainers);
+            }
           }
-        }
-      });
-    },
-    [ref]
-  );
+        });
+      },
+      [ref]
+    );
 
-  isNationalCompetition && replaceFlags(optaImages);
+    isNationalCompetition && replaceNationalTeamDetails(optaImages);
 
-  return (
-    <>
-      <WidgetContainer ref={ref} />
+    return (
+      <>
+        <WidgetContainer ref={ref} />
 
-      {!isReady && (
-        <PlaceholderContainer height={100}>
-          <Placeholder />
-        </PlaceholderContainer>
-      )}
-    </>
-  );
-});
+        {!isReady && (
+          <PlaceholderContainer height={100}>
+            <Placeholder />
+          </PlaceholderContainer>
+        )}
+      </>
+    );
+  }
+);
