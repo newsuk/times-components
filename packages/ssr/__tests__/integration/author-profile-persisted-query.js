cbd708d3853e886680e53438b214ff68f164785b
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2024-07-03 15:02:50 +0100

    remove tests

--- packages/ssr/__tests__/integration/author-profile-persisted-query.js
+++
@@ -1,8 +0,0 @@
-import authorProfileTests from "../helpers/author-profile-helper";
-
-authorProfileTests({
-  qs: "?pq=1",
-  variant: "Persisted Query",
-  stickyElements: ["#nav"],
-  skipSnapshotTest: true
-});
