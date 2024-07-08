1c65eebedb182c3430bad5257c8b4efbedbbb9c1
Haris Hodzic <haris_hodzicnt@hotmail.com>
2024-07-08 15:44:36 +0200

    bug(TMRS-6): Typo

--- packages/article-comments/src/comment-login.js
+++ packages/article-comments/src/comment-login.js
@@ -2,12 +2,12 @@
 import getCpnId from "./utils";
 
 const loginRequest = (url, completeSSOCallback) => {
-      // eslint-disable-next-line no-consoleß
+      // eslint-disable-next-line no-console
   console.log("doso je pajdo");
   const xhr = new XMLHttpRequest();
   xhr.addEventListener("load", () => {
     const success = xhr.status === 200;
-        // eslint-disable-next-line no-consoleß
+        // eslint-disable-next-line no-console
     console.log(success, "sucess wtf");
     const { spotimCodeB } = success ? JSON.parse(xhr.response) : {};
     if (spotimCodeB) {
@@ -19,7 +19,7 @@ const loginRequest = (url, completeSSOCallback) => {
 };
 
 const ssoCallback = (codeA, completeSSOCallback) =>{
-      // eslint-disable-next-line no-consoleß
+      // eslint-disable-next-line no-console
   console.log('SSO FIRED')
 return loginRequest(
   `/api/comments/loginv2?codeA=${encodeURIComponent(codeA)}`,
@@ -37,7 +37,7 @@ const executeSSOtransaction = () => {
       window.nuk.getCookieValue("acs_tnl");
 
     const cpn = `${getCpnId(acsTnlCookie)}_v2`;
-        // eslint-disable-next-line no-consoleß
+        // eslint-disable-next-line no-console
     console.log(cpn, "CPN");
     window.SPOTIM.startSSO({ callback: ssoCallback, userId: cpn });
   }
