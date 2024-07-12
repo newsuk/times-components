9610e2569b679d6441f3129ddd905cbe0dd4d496
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2024-07-12 11:58:44 +0100

    Prettier

--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -6,6 +6,9 @@ export {
   UpdateButtonWithDelay
 } from './components/misc/update-button/update-button-with-delay';
 export { Banner } from './components/misc/banner/banner';
-export { TrackingContext, TrackingContextProvider } from './utils/TrackingContextProvider'
+export {
+  TrackingContext,
+  TrackingContextProvider
+} from './utils/TrackingContextProvider';
 
 // TODO Remove after tests - borisovy
