5793ce23ad042288167329a44de614861edeb272
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2024-07-12 11:56:23 +0100

    Empty

--- packages/ts-components/src/assets/index.tsx
+++ packages/ts-components/src/assets/index.tsx
@@ -1,7 +1,3 @@
-export {
-  default as CloseIconBlack
-} from '../../../ts-components/src/assets/CloseIconBlack';
-export {
-  default as EmailIcon
-} from '../../../ts-components/src/assets/EmailIcon';
+export { default as CloseIconBlack } from './CloseIconBlack';
+export { default as EmailIcon } from './EmailIcon';
 export { default as FilledArrowIcon } from './FilledArrowIcon';
