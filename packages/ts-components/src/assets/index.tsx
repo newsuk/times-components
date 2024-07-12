a6ed338c5c5e14f1b69b5d0a80fb1664ac941e66
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2024-07-12 11:35:39 +0100

    Moved icons over to ts-components

---
+++ packages/ts-components/src/assets/index.tsx
@@ -0,0 +1,1 @@
+export { default as TooltipIcon } from '../assets/TooltipIcon';
--- packages/ts-newskit/src/index.ts
+++ packages/ts-newskit/src/index.ts
@@ -6,6 +6,5 @@ export {
   UpdateButtonWithDelay
 } from './components/misc/update-button/update-button-with-delay';
 export { Banner } from './components/misc/banner/banner';
-export { TooltipIcon } from './assets';
 
 // TODO Remove after tests - borisovy
