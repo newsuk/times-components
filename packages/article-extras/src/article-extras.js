8d5df4a517311fd24645a6013e31d1ca514a555d
Luka Kerečeni <luka.kereceni@q.agency>
2024-07-25 19:48:07 +0200

    Fix lint issue

--- packages/article-extras/src/article-extras.js
+++ packages/article-extras/src/article-extras.js
@@ -80,20 +80,18 @@ const ArticleExtras = ({
     </>
   );
 
-  const PromotedContentSection = () => {
-    return (
-      <PromotedContentContainer>
-        <PromotedContentTitle>PROMOTED CONTENT</PromotedContentTitle>
-        <PromotedContentGrid>
-          <PromotedContentAd id="advert-inarticle-native-1" />
-          <PromotedContentAd id="advert-inarticle-native-2" />
-          <PromotedContentSectionDivider />
-          <PromotedContentAd id="advert-inarticle-native-3" />
-          <PromotedContentAd id="advert-inarticle-native-4" />
-        </PromotedContentGrid>
-      </PromotedContentContainer>
-    );
-  }
+  const PromotedContentSection = () => (
+    <PromotedContentContainer>
+      <PromotedContentTitle>PROMOTED CONTENT</PromotedContentTitle>
+      <PromotedContentGrid>
+        <PromotedContentAd id="advert-inarticle-native-1" />
+        <PromotedContentAd id="advert-inarticle-native-2" />
+        <PromotedContentSectionDivider />
+        <PromotedContentAd id="advert-inarticle-native-3" />
+        <PromotedContentAd id="advert-inarticle-native-4" />
+      </PromotedContentGrid>
+    </PromotedContentContainer>
+  );
 
   return (
     <UserState
