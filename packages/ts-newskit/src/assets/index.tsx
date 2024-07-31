d4390153a2c80e984e9e7a80f227cf95fe491d13
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2024-07-31 13:46:44 +0100

    empty

--- packages/ts-newskit/src/assets/index.tsx
+++ packages/ts-newskit/src/assets/index.tsx
@@ -1,5 +1,3 @@
 export { default as TooltipIcon } from '../assets/TooltipIcon';
-export { default as CloseIconBlack } from '../assets/CloseIconBlack';
-export { default as EmailIcon } from '../assets/EmailIcon';
 export { default as FilledArrowIcon } from './FilledArrowIcon';
 export { default as BreadcrumbIcon } from './BreadcrumbIcon';
