1da4e606744d2acaa242052f53e419e70f9dd2a3
ivazlopasa2 <iva.zlopasa@news.co.uk>
2024-09-19 12:58:52 +0200

    fixing lint errors

--- packages/article-skeleton/src/contentModifiers/setExternalLinkTargets.js
+++ packages/article-skeleton/src/contentModifiers/setExternalLinkTargets.js
@@ -9,7 +9,7 @@ const contentChildren = [
         },
         children: [],
       },
-       {
+      {
         name: 'link',
         attributes: {
           href: 'https://www.thetimes.com',
@@ -20,39 +20,48 @@ const contentChildren = [
   },
 ];
 
-export const setExternalLinkTargets = (children) => {
+const setExternalLinkTargets = (children) => {
   const clonedChildren = [...children];
-  const checkAndSetLinkTarget = (elements) => {
-    elements.forEach((el) => {
-      // Check if element is a link or an interactive element with a URL
+
+  const checkAndSetLinkTarget = (elements) =>
+    elements.map((el) => {
+      let newElement = { ...el }; 
+
       if (
-        (el.name === 'link' || (el.name === 'interactive' && el.attributes?.element?.value === 'times-travel-cta'))
+        newElement.name === 'link' || 
+        (newElement.name === 'interactive' && newElement.attributes?.element?.value === 'times-travel-cta')
       ) {
-        const href = el.name === 'interactive'
-          ? el.attributes?.element?.attributes?.url ?? ''
-          : el.attributes?.href ?? '';
+        const href = newElement.name === 'interactive'
+          ? newElement.attributes?.element?.attributes?.url ?? ''
+          : newElement.attributes?.href ?? '';
 
         // If the link is external, set target to _blank
-        if (href && !href.startsWith('https://www.thetimes.co.uk') && !href.startsWith('https://www.thetimes.com')) {
-          el.attributes = {
-            ...el.attributes,
-            target: '_blank',
+        if (
+          href &&
+          !href.startsWith('https://www.thetimes.co.uk') &&
+          !href.startsWith('https://www.thetimes.com')
+        ) {
+          newElement = {
+            ...newElement,
+            attributes: {
+              ...newElement.attributes,
+              target: '_blank',
+            },
           };
         }
       }
 
-      // Recursively check for nested children
-      if (el.children && el.children.length) {
-        checkAndSetLinkTarget(el.children);
+      if (newElement.children && newElement.children.length) {
+        newElement.children = checkAndSetLinkTarget(newElement.children);
       }
-    });
-  };
 
-  checkAndSetLinkTarget(clonedChildren);
-  return clonedChildren;
+      return newElement;
+    });
+
+  return checkAndSetLinkTarget(clonedChildren);
 };
 
 const updatedChildren = setExternalLinkTargets(contentChildren);
-console.log('logiram updatedChildren,', updatedChildren);
+console.log('logiram updatedChildren,', updatedChildren); 
 
 export default setExternalLinkTargets;
