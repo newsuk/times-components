f9772bb146885c59e08f5775610d1a1e91051f59
simun-ivanac <simun.ivanac@news.co.uk>
2024-09-23 11:57:10 +0200

    format code, fix tests

--- packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
+++ packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
@@ -1,55 +1,61 @@
 const newDisclaimerText = {
-  name: 'paragraph',
+  name: "paragraph",
   children: [
     {
-        name: 'italic',
-        children: [
-          {
-            name: 'text',
-            children: [],
-            attributes: {
-              value: 'All recommendations within this article are informed by expert editorial opinion. If you click on a link in this story we may earn affiliate revenue.'
-            }
+      name: "italic",
+      children: [
+        {
+          name: "text",
+          children: [],
+          attributes: {
+            value:
+              "All recommendations within this article are informed by expert editorial opinion. If you click on a link in this story we may earn affiliate revenue."
           }
-        ]
+        }
+      ]
     }
   ]
-}
+};
 
 const oldDisclaimerTexts = [
-  'This article contains affiliate links that can earn us revenue',
-  'This article contains affiliate links that may earn us revenue',
-  'This article contains affiliate links which may earn us revenue',
-  'This article contains affiliate links, which can earn us revenue',
-  'This article contains affiliate links, which may earn us revenue',
-  'This article contains affiliate links, which may earn�us revenue',
-  'This article contains affiliate links, which�may earn us revenue',
-  'This article contains�affiliate links, which may earn us revenue',
-  'This article contains affiliate links',
-  'This article contains links that can earn us revenue',
-  'This article contains links which may earn us revenue',
-  'This article contains links, which can earn us revenue',
-  'This article contains links, which may earn us revenu',
-  'This article contains links from which we may earn revenue',
+  "This article contains affiliate links that can earn us revenue",
+  "This article contains affiliate links that may earn us revenue",
+  "This article contains affiliate links which may earn us revenue",
+  "This article contains affiliate links, which can earn us revenue",
+  "This article contains affiliate links, which may earn us revenue",
+  "This article contains affiliate links, which may earn�us revenue",
+  "This article contains affiliate links, which�may earn us revenue",
+  "This article contains�affiliate links, which may earn us revenue",
+  "This article contains affiliate links",
+  "This article contains links that can earn us revenue",
+  "This article contains links which may earn us revenue",
+  "This article contains links, which can earn us revenue",
+  "This article contains links, which may earn us revenu",
+  "This article contains links from which we may earn revenue"
 ];
 
-
-const shouldIncludeDisclaimer = (children) => {
+const shouldIncludeDisclaimer = children => {
   const clonedChildren = [...children];
   let affiliateLinkExist = false;
   let affiliateDisclaimerExist = false;
 
-  const checkForAffiliate = (elements) => {
-    elements.some((el) => {
+  const checkForAffiliate = elements => {
+    elements.some(el => {
       // Check if disclaimer text already exists.
       if (
         !affiliateDisclaimerExist &&
-        (el.name === 'text' ||
-        (el.name === 'interactive' && el.attributes?.element?.value === 'times-text-collapse'))
+        (el.name === "text" ||
+          (el.name === "interactive" &&
+            el.attributes?.element?.value === "times-text-collapse"))
       ) {
-        const text = (el.name === 'interactive') ? (el.attributes?.element?.attributes?.disclaimer_text ?? '') : (el.attributes?.value ?? '');
+        const text =
+          el.name === "interactive"
+            ? el.attributes?.element?.attributes?.disclaimer_text ?? ""
+            : el.attributes?.value ?? "";
 
-        if (oldDisclaimerTexts.some((contentText) => text.includes(contentText))) {
+        if (
+          oldDisclaimerTexts.some(contentText => text.includes(contentText))
+        ) {
           affiliateDisclaimerExist = true;
         }
       }
@@ -60,8 +66,8 @@ const shouldIncludeDisclaimer = (children) => {
 
       // Move to next iteration if element is not link, and doesn't have children.
       if (
-        el.name !== 'interactive' &&
-        el.name !== 'link' &&
+        el.name !== "interactive" &&
+        el.name !== "link" &&
         (el.children === undefined || !el.children.length)
       ) {
         return false;
@@ -70,15 +76,19 @@ const shouldIncludeDisclaimer = (children) => {
       // Check for affiliate links.
       if (
         !affiliateLinkExist &&
-        ((el.name === 'interactive' && el.attributes?.element?.value === 'times-travel-cta') ||
-        el.name === 'link')
+        ((el.name === "interactive" &&
+          el.attributes?.element?.value === "times-travel-cta") ||
+          el.name === "link")
       ) {
-        const href = (el.name === 'interactive') ? (el.attributes?.element?.attributes?.url ?? '') : (el.attributes?.href ?? '');
+        const href =
+          el.name === "interactive"
+            ? el.attributes?.element?.attributes?.url ?? ""
+            : el.attributes?.href ?? "";
 
         if (
           !href ||
-          href.startsWith('https://www.thetimes.co.uk') ||
-          href.startsWith('https://www.thetimes.com')
+          href.startsWith("https://www.thetimes.co.uk") ||
+          href.startsWith("https://www.thetimes.com")
         ) {
           return false;
         }
@@ -98,17 +108,24 @@ const shouldIncludeDisclaimer = (children) => {
       affiliateLinkExist,
       affiliateDisclaimerExist
     };
-  }
+  };
 
   const affiliatesCheck = checkForAffiliate(clonedChildren);
 
   // Add disclaimer after first paragraph (in case disclaimer doesn't already exist).
-  if (!affiliatesCheck.affiliateDisclaimerExist && affiliatesCheck.affiliateLinkExist) {
-    const firstParagraph = clonedChildren.find((el) => el.name === 'paragraph');
-    clonedChildren.splice(clonedChildren.indexOf(firstParagraph) + 1, 0, newDisclaimerText);
+  if (
+    !affiliatesCheck.affiliateDisclaimerExist &&
+    affiliatesCheck.affiliateLinkExist
+  ) {
+    const firstParagraph = clonedChildren.find(el => el.name === "paragraph");
+    clonedChildren.splice(
+      clonedChildren.indexOf(firstParagraph) + 1,
+      0,
+      newDisclaimerText
+    );
   }
 
   return clonedChildren;
-}
+};
 
 export default shouldIncludeDisclaimer;
