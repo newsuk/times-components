546e59cde91f3bcd46b6cdd0ab0daf395a852c0b
Avi247h <avinash.narayan@news.co.uk>
2024-09-24 18:30:42 +0530

    fix fail code

--- packages/ssr/__tests__/integration/author-profile-default.js
+++ packages/ssr/__tests__/integration/author-profile-default.js
@@ -1,14 +1,4 @@
-//import authorProfileTests from "../helpers/author-profile-helper";
+// import authorProfileTests from "../helpers/author-profile-helper";
 
 // default
 authorProfileTests({ stickyElements: ["nav"], skipSnapshotTest: true });
-const rerun = process.env.RERUN; // Access the environment variable
-
-console.log(`RERUN: ${rerun}`); // Log the value of RERUN
-
-if (!!rerun) { // Check if RERUN is truthy
-  throw new Error('Error'); // Throw an error if RERUN is set
-}
-
-// Continue with other tests if no error is thrown
-expect(true).toBe(true); 
