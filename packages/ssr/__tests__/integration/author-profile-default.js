da98c32b171092085d0a2b076d9e030fed0b8f27
Avi247h <avinash.narayan@news.co.uk>
2024-09-24 13:13:10 +0530

    try failures

--- packages/ssr/__tests__/integration/author-profile-default.js
+++ packages/ssr/__tests__/integration/author-profile-default.js
@@ -1,4 +1,4 @@
-import authorProfileTests from "../helpers/author-profile-helper";
+//import authorProfileTests from "../helpers/author-profile-helper";
 
 // default
 authorProfileTests({ stickyElements: ["#nav"], skipSnapshotTest: true });
