c8a9512d5a26953278511e5e329c9322979c7cd4
Avi247h <avinash.narayan@news.co.uk>
2024-09-26 00:20:10 +0530

    fix - bash

--- lib/cypress-rerun.sh
+++ lib/cypress-rerun.sh
@@ -8,46 +8,44 @@
 #  Team:        @tm-qa-engineers (QA team)
 #
 
-error_exit() {
-    echo "Error: Yarn command failure" >&2
-    exit 1
-}
+# error_exit() {
+#     echo "Error: Yarn command failure" >&2
+#     exit 1
+# }
 
 echo "Creating directory structure at './packages/ssr/test-results'..."
 mkdir -p "./packages/ssr/test-results"
 ln -s /home/circleci/project/node_modules/cypress-circleci-reporter "/home/circleci/project/packages/ssr/node_modules/cypress-circleci-reporter"
-echo "Running yarn command: yarn test:e2e:ci..."
-yarn test:e2e:ci || error_exit "Yarn command failed."
+# echo "Running yarn command: yarn test:e2e:ci..."
+# yarn test:e2e:ci || error_exit "Yarn command failed."
 
-# # Run Cypress tests and capture the exit code
-# npx cypress run --reporter cypress-circleci-reporter --spec "$1"
-# cypressExitCode=$?
-
-# # Stop the test server
-# yarn stop:testserver
-
-# # Exit with Cypress's exit code
-# exit $cypressExitCode
+####################################
 
 
+# Function to handle errors
+error_exit() {
+    echo "Error: $1" >&2
+    yarn stop:testserver 
+    exit 1
+}
 
+# Check if a spec is passed
+if [ -z "$1" ]; then
+    error_exit "No spec file provided."
+fi
 
-# yarn test:e2e:ci
+echo "Running Cypress tests for spec file: $1"
 
+npx cypress run --reporter cypress-circleci-reporter --spec "$1"
+cypressExitCode=$?
 
-# # Capture the exit code of the Cypress tests (from yarn test:ci)
-# cypressExitCode=$?
+# Stop the test server
+yarn stop:testserver
 
-# # Stop the test server (ignore any exit code from this command)
-# echo "Stopping the test server..."
-# yarn stop:testserver || true
+# Check if Cypress encountered an error
+if [ $cypressExitCode -ne 0 ]; then
+    error_exit "Cypress tests failed for spec file: $1"
+fi
 
-# # Check if Cypress tests failed
-# if [ $cypressExitCode -ne 0 ]; then
-#   echo "Tests failed with exit code $cypressExitCode."
-# else
-#   echo "Tests passed."
-# fi
-
-# # Exit with the Cypress exit code, not the server stop exit code
-# exit $cypressExitCode
+# Exit with Cypress's exit code
+exit $cypressExitCode
