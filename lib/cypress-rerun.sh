d12df99b4aa31d155dcb9984cde4e9774fa930f9
Avi247h <avinash.narayan@news.co.uk>
2024-09-25 22:04:47 +0530

    handling exitcode in bash file

--- lib/cypress-rerun.sh
+++ lib/cypress-rerun.sh
@@ -5,7 +5,7 @@
 #  Description: Check if arguments are passed correctly,create a test directory, add symbolic link and run the test command.
 #  Date:        Septemeber 2024
 #  Author:      avinash.narayan@news.co.uk
-#  Team:        @tm-discovery-qa (Discovery)
+#  Team:        @tm-qa-engineers (QA team)
 #
 
 
@@ -15,3 +15,21 @@ mkdir -p "./packages/ssr/test-results"
 ln -s /home/circleci/project/node_modules/cypress-circleci-reporter "/home/circleci/project/packages/ssr/node_modules/cypress-circleci-reporter"
 echo "Running yarn command: 'yarn test:e2e'..."
 yarn test:e2e:ci
+
+
+# Capture the exit code of the Cypress tests (from yarn test:ci)
+cypressExitCode=$?
+
+# Stop the test server (ignore any exit code from this command)
+echo "Stopping the test server..."
+yarn stop:testserver || true
+
+# Check if Cypress tests failed
+if [ $cypressExitCode -ne 0 ]; then
+  echo "Tests failed with exit code $cypressExitCode."
+else
+  echo "Tests passed."
+fi
+
+# Exit with the Cypress exit code, not the server stop exit code
+exit $cypressExitCode
