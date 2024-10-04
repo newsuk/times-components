02e905988dce6c88e94be619a6150f754085e96f
ivazlopasa2 <iva.zlopasa@news.co.uk>
2024-10-04 13:48:51 +0200

    fix lint

--- packages/ts-components/src/components/twitter-embed/TwitterEmbed.tsx
+++ packages/ts-components/src/components/twitter-embed/TwitterEmbed.tsx
@@ -1,22 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Placeholder } from '@times-components/image';
-import { useFetch } from '../../helpers/fetch/FetchProvider';
-import {
-  PlaceholderContainer,
-} from '../common-styles';
-import { DeckData } from '../../helpers/fetch/types';
-
-type TimelinesData = {
-  type: string;
-  data: {
-    date: string;
-    eventHeading: string;
-    image: string;
-    copy: string;
-  };
-};
-
-type TimelinesDeckData = DeckData<never, TimelinesData>;
+import React, { useEffect } from 'react';
 
 declare global {
   interface Window {
@@ -31,48 +13,28 @@ declare global {
 export const TwitterEmbed: React.FC<{
   sectionColour: string;
 }> = () => {
-  const { loading, error, data } = useFetch<TimelinesDeckData>();
-    console.log('window', window);
+  // tslint:disable-next-line:no-console
+  console.log('window', window);
 
-     useEffect(() => {
-     if (window.__tcfapi) {
-       window.__tcfapi('getCustomVendorConsents', 2, (data, success) => {
-         if (success) {
-           console.log('TCF API response:', data);
-         } else {
-           console.log('Error fetching TCF API data');
-         }
-       });
-     } else {
-       console.log('TCF API not available');
-     }
-   }, []);
-
-   console.log('window.__tcfapi', window.__tcfapi);
-
-  if (loading) {
-    return (
-      <PlaceholderContainer>
-        <Placeholder />
-      </PlaceholderContainer>
-    );
-  }
-
-  if (error || data === undefined) {
-    return null;
-  }
-
-  const showAllRef = useRef<HTMLDivElement>(null);
-  const [, setShowShowAll] = useState(false);
-  const maxHeight = 375;
   useEffect(() => {
-    const listContainer = showAllRef.current;
-    if (listContainer) {
-      setShowShowAll(listContainer.clientHeight > maxHeight);
+    if (window.__tcfapi) {
+      window.__tcfapi('getCustomVendorConsents', 2, (data, success) => {
+        if (success) {
+          // tslint:disable-next-line:no-console
+          console.log('TCF API response:', data);
+        } else {
+          // tslint:disable-next-line:no-console
+          console.log('Error fetching TCF API data');
+        }
+      });
+    } else {
+      // tslint:disable-next-line:no-console
+      console.log('TCF API not available');
     }
   }, []);
 
-  return (
-    <h1>Test</h1>
-  );
+  // tslint:disable-next-line:no-console
+  console.log('window.__tcfapi', window.__tcfapi);
+
+  return <h1>Test</h1>;
 };
