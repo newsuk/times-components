bb960f7a2b764296a440471d3c9fe1fcb45346f7
Avi247h <avinash.narayan@news.co.uk>
2024-10-09 11:42:46 +0530

    removed comments

--- packages/ssr/__tests__/cypress/e2e/article-indepth.cy.js
+++ packages/ssr/__tests__/cypress/e2e/article-indepth.cy.js
@@ -1,5 +1,4 @@
 import articleTests from "../../helpers/author-profile-helper";
-// const authorProfileTests = require('../../helpers/author-profile-helper')
 
 articleTests("indepth", {
   stickyElements: ["#nav"],
