d78ddcfdc32cd5c1e22031f0720dd5eb4d0d4af4
ivazlopasa2 <iva.zlopasa@news.co.uk>
2024-10-14 07:58:58 +0200

    fix lint

--- packages/ts-components/src/components/twitter-embed/__tests__/TwitterEmbed.test.tsx
+++ packages/ts-components/src/components/twitter-embed/__tests__/TwitterEmbed.test.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { TwitterEmbed } from '../TwitterEmbed'; 
+import { TwitterEmbed } from '../TwitterEmbed';
 import '@testing-library/jest-dom';
 import get from 'lodash.get';
 
 // Mocking external dependencies
-jest.mock('@times-components/interactive-wrapper', () => jest.fn(() => <div>InteractiveWrapper</div>));
+jest.mock('@times-components/interactive-wrapper', () =>
+  jest.fn(() => <div>InteractiveWrapper</div>)
+);
 jest.mock('lodash.get');
 
 const mockTcfApi = jest.fn();
 
 describe('TwitterEmbed', () => {
   beforeEach(() => {
+    // Reset mocks before each test
     mockTcfApi.mockReset();
     window.__tcfapi = mockTcfApi;
 
+    /* tslint:disable:no-empty */
     jest.spyOn(global.console, 'log').mockImplementation(() => {});
+    /* tslint:disable:no-empty */
     jest.spyOn(global.console, 'warn').mockImplementation(() => {});
   });
 
   afterEach(() => {
+    // Restore mocks after each test
     jest.restoreAllMocks();
   });
 
   it('renders Twitter content if consent is given for Twitter', () => {
-    // tslint:disable-next-line:no-unused-variable
-    mockTcfApi.mockImplementation((command, version, callback) => {
+    mockTcfApi.mockImplementation((_, __, callback) => {
       callback({ consentedVendors: [{ name: 'Twitter' }] }, true);
     });
 
     const mockElement = {
       attributes: {},
       value: 'Twitter content',
-      key: 'twitter-embed',
+      key: 'twitter-embed'
     };
     const url = 'https://twitter.com';
 
@@ -43,15 +48,14 @@ describe('TwitterEmbed', () => {
   });
 
   it('renders blocked content message if consent for Twitter is not given', () => {
-    // tslint:disable-next-line:no-unused-variable
-    mockTcfApi.mockImplementation((command, version, callback) => {
+    mockTcfApi.mockImplementation((_, __, callback) => {
       callback({ consentedVendors: [] }, true);
     });
 
     const mockElement = {
       attributes: {},
       value: 'Twitter content',
-      key: 'twitter-embed',
+      key: 'twitter-embed'
     };
     const url = 'https://twitter.com';
 
@@ -60,15 +64,19 @@ describe('TwitterEmbed', () => {
     // Assert that the blocked content message is rendered
     expect(screen.getByText('X (Twitter) content blocked')).toBeInTheDocument();
     expect(screen.getByText('privacy manager.')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /Enable cookies/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /Allow cookies once/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Enable cookies/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Allow cookies once/i })
+    ).toBeInTheDocument();
   });
 
   it('handles privacy manager link click', () => {
     const mockElement = {
       attributes: {},
       value: 'Twitter content',
-      key: 'twitter-embed',
+      key: 'twitter-embed'
     };
     const url = 'https://twitter.com';
 
@@ -78,25 +86,28 @@ describe('TwitterEmbed', () => {
     fireEvent.click(privacyManagerLink);
 
     // Ensure that the privacy modal load function was attempted
-    expect(global.console.warn).toHaveBeenCalledWith('Sourcepoint LoadPrivacyManagerModal is not available');
+    expect(global.console.warn).toHaveBeenCalledWith(
+      'Sourcepoint LoadPrivacyManagerModal is not available'
+    );
   });
 
   it('calls __tcfapi and logs an error if consent data is unavailable', () => {
-    // tslint:disable-next-line:no-unused-variable
-    mockTcfApi.mockImplementation((command, version, callback) => {
+    mockTcfApi.mockImplementation((_, __, callback) => {
       callback(null, false);
     });
 
     const mockElement = {
       attributes: {},
       value: 'Twitter content',
-      key: 'twitter-embed',
+      key: 'twitter-embed'
     };
     const url = 'https://twitter.com';
 
     render(<TwitterEmbed element={mockElement} url={url} />);
 
-    expect(global.console.log).toHaveBeenCalledWith('Error fetching consent data or Twitter not allowed');
+    expect(global.console.log).toHaveBeenCalledWith(
+      'Error fetching consent data or Twitter not allowed'
+    );
   });
 
   it('warns when privacy modal cannot be loaded', () => {
@@ -105,7 +116,7 @@ describe('TwitterEmbed', () => {
     const mockElement = {
       attributes: {},
       value: 'Twitter content',
-      key: 'twitter-embed',
+      key: 'twitter-embed'
     };
     const url = 'https://twitter.com';
 
@@ -115,6 +126,8 @@ describe('TwitterEmbed', () => {
     const privacyManagerLink = screen.getByText('privacy manager.');
     fireEvent.click(privacyManagerLink);
 
-    expect(global.console.warn).toHaveBeenCalledWith('Sourcepoint LoadPrivacyManagerModal is not available');
+    expect(global.console.warn).toHaveBeenCalledWith(
+      'Sourcepoint LoadPrivacyManagerModal is not available'
+    );
   });
 });
