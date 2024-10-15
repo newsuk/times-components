737163ddea5129b6d351899ea3bf74953ca35462
suneethanagella <105289286+suneethanagella@users.noreply.github.com>
2024-10-15 17:14:02 +0530

    updated .co.uk to .com (#3957)

--- packages/ts-components/src/components/article-sidebar/ArticleSidebar.stories.tsx
+++ packages/ts-components/src/components/article-sidebar/ArticleSidebar.stories.tsx
@@ -9,24 +9,24 @@ const getAttributes = () => {
   const data = [
     {
       title: 'Crossword',
-      url: 'https://www.thetimes.co.uk/puzzles/crossword',
+      url: 'https://www.thetimes.com/puzzles/crossword',
       imgUrl:
-        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/crossword-c7ae8934ef.png'
+        'https://www.thetimes.com/d/img/puzzles/new-illustrations/crossword-c7ae8934ef.png'
     },
     {
       title: 'Polygon',
-      url: 'https://www.thetimes.co.uk/puzzles/word-puzzles',
+      url: 'https://www.thetimes.com/puzzles/word-puzzles',
       imgUrl:
-        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/polygon-875ea55487.png'
+        'https://www.thetimes.com/d/img/puzzles/new-illustrations/polygon-875ea55487.png'
     },
     {
       title: 'Sudoku',
-      url: 'https://www.thetimes.co.uk/puzzles/sudoku',
+      url: 'https://www.thetimes.com/puzzles/sudoku',
       imgUrl:
-        'https://www.thetimes.co.uk/d/img/puzzles/new-illustrations/sudoku-ee2aea0209.png'
+        'https://www.thetimes.com/d/img/puzzles/new-illustrations/sudoku-ee2aea0209.png'
     }
   ];
-  const pageLink = 'https://www.thetimes.co.uk/puzzles';
+  const pageLink = 'https://www.thetimes.com/puzzles';
 
   return { sectionTitle, data, pageLink };
 };
