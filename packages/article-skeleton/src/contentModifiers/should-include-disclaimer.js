60d59d47d4291174e39278538a32b3b7d7d19d7a
simun-ivanac <simun.ivanac@news.co.uk>
2024-10-23 11:27:00 +0200

    use endpoint for list of domains

--- packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
+++ packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
@@ -34,7 +34,64 @@ const oldDisclaimerTexts = [
   "This article contains links from which we may earn revenue"
 ];
 
+const domainJson = [
+  "(clicks.)?trx-hub.com",
+  "go.skimresources.com",
+  "go.redirectingat.com",
+  "(www.)?fave.co",
+  "beavertownbrewery.pxf.io",
+  "wilko.sjv.io",
+  "fanatics.93n6tx.net",
+  "ticketmaster-uk.tm7559.net",
+  "(www.)?prf.hn",
+  "nastygal.prf.hn",
+  "selfridges.prf.hn",
+  "bloomwild.prf.hn",
+  "go.linkby.com",
+  "(www.)?awin1.com",
+  "track.webgains.com",
+  "click.linksynergy.com",
+  "(www.)?dpbolvw.net",
+  "(www.)?kqzyfj.com",
+  "(www.)?anrdoezrs.net",
+  "(www.)?veneficus.co.uk",
+  "(www.)?shareasale.com",
+  "(www.)?privadovpn.com",
+  "(www.)?amzn.to",
+  "(www.)?amazon.[\\S]*?\\/(dp|gp)\\/[\\S]*?tag=",
+  "(www.)?planethowl.com"
+];
+
+const getDomainListing = async () => {
+  let domainList = domainJson;
+
+  try {
+    const getDomains = await fetch(
+      "https://ads.thetimes.com/affiliates/domains.json"
+    ).then(response => response.text());
+    const formattedRes = getDomains.replaceAll("'", '"');
+    const parsedRes = JSON.parse(`{"response": ${formattedRes} }`);
+    domainList = parsedRes.response;
+  } catch (error) {
+    console.log("Fetching domain listing failed. Using default."); // eslint-disable-line
+  }
+
+  return domainList;
+};
+
+const isLinkAffiliate = async (url = "", adsDomains) => {
+  const affiliateStrPattern = `<a\\s[^>]*?href="https?:\\/\\/(${adsDomains.join(
+    "|"
+  )})`;
+  const anchorEl = `<a href="${url}"></a>`;
+
+  const affiliateLinkPattern = new RegExp(affiliateStrPattern, "i");
+  return affiliateLinkPattern.test(anchorEl);
+};
+
 const shouldIncludeDisclaimer = children => {
+  const adsDomains = getDomainListing();
+
   const clonedChildren = [...children];
   let affiliateLinkExist = false;
   let affiliateDisclaimerExist = false;
@@ -106,11 +163,7 @@ const shouldIncludeDisclaimer = children => {
 
         const href = linkComponentVal || ctaComponentVal;
 
-        if (
-          !href ||
-          href.startsWith("https://www.thetimes.co.uk") ||
-          href.startsWith("https://www.thetimes.com")
-        ) {
+        if (!href || !isLinkAffiliate(href, adsDomains)) {
           return false;
         }
 
