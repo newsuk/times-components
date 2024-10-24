05c1391222730273220a0812a3da3a0066e332ea
yukiinasawa <yuki.inasawa@news.co.uk>
2024-10-24 11:57:17 +0100

    feat(tmrs-488): read more

--- packages/ts-components/src/components/recommended-articles/RecommendedFetch.tsx
+++ packages/ts-components/src/components/recommended-articles/RecommendedFetch.tsx
@@ -5,14 +5,12 @@ declare global {
   }
 }
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { FetchProvider } from '../../helpers/fetch/FetchProvider';
 import { TrackingContextProvider } from '../../helpers/tracking/TrackingContextProvider';
 import { RecommendedArticles } from './RecommendedArticles';
 
-const isValidEnvironment = (name: string) =>
-  ['local-prod', 'pr', 'uat', 'staging', 'prod'].includes(name);
 
 export const RecommendedFetch: React.FC<{
   articleId: string;
