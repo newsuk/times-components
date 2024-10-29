eeefba1cdaa536c8cdbd9ddf416077ccd5d50a5f
simun-ivanac <simun.ivanac@news.co.uk>
2024-10-29 13:44:47 +0100

    remove backup domains

--- packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
+++ packages/article-skeleton/src/contentModifiers/should-include-disclaimer.js
@@ -36,33 +36,7 @@ const oldDisclaimerTexts = [
   "This article contains links from which we may earn revenue"
 ];
 
-const domainJson = [
-  "(clicks.)?trx-hub.com",
-  "go.skimresources.com",
-  "go.redirectingat.com",
-  "(www.)?fave.co",
-  "beavertownbrewery.pxf.io",
-  "wilko.sjv.io",
-  "fanatics.93n6tx.net",
-  "ticketmaster-uk.tm7559.net",
-  "(www.)?prf.hn",
-  "nastygal.prf.hn",
-  "selfridges.prf.hn",
-  "bloomwild.prf.hn",
-  "go.linkby.com",
-  "(www.)?awin1.com",
-  "track.webgains.com",
-  "click.linksynergy.com",
-  "(www.)?dpbolvw.net",
-  "(www.)?kqzyfj.com",
-  "(www.)?anrdoezrs.net",
-  "(www.)?veneficus.co.uk",
-  "(www.)?shareasale.com",
-  "(www.)?privadovpn.com",
-  "(www.)?amzn.to",
-  "(www.)?amazon.[\\S]*?\\/(dp|gp)\\/[\\S]*?tag=",
-  "(www.)?planethowl.com"
-];
+const domainJson = [];
 
 const isLinkAffiliate = (url = "", adsDomains) => {
   const affiliateStrPattern = `<a\\s[^>]*?href="https?:\\/\\/(${adsDomains.join(
