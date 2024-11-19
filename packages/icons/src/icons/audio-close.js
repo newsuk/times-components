ecc9eb07eec48325df4ca74272c864cbdd64378f
Domagoj <domagoj.buljan@news.co.uk>
2024-11-19 13:14:34 +0100

    feat/TMPZ-777-audio-player-icons

---
+++ packages/icons/src/icons/audio-close.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AudioCloseIcon = ({ fill }) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill={fill}
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label="Audio Close Icon"
+    role="img"
+  >
+    <mask
+      id="mask0_4612_9510"
+      style={{ maskType: "alpha" }}
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="24"
+      height="24"
+    >
+      <path
+        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+        fill={fill}
+      />
+    </mask>
+    <g mask="url(#mask0_4612_9510)">
+      <rect width="24" height="24" fill={fill} />
+    </g>
+  </svg>
+);
+
+export default AudioCloseIcon;
---
+++ packages/icons/src/icons/check.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const IconCheck = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <mask
+      id="mask0_4528_7566"
+      style={{ maskType: "alpha" }}
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="24"
+      height="25"
+    >
+      <path
+        d="M8.99991 16.1796L4.82991 12.0096L3.40991 13.4196L8.99991 19.0096L20.9999 7.00961L19.5899 5.59961L8.99991 16.1796Z"
+        fill="#0A0A0A"
+      />
+    </mask>
+    <g mask="url(#mask0_4528_7566)">
+      <rect y="0.00976562" width="24" height="24" fill="#01000D" />
+    </g>
+  </svg>
+);
+
+export default IconCheck;
