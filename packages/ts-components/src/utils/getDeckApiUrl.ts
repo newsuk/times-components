7d82178c9b444edcdb1a117d9c48712473931b6d
seldinpQ <seldin.puce@news.co.uk>
2024-12-02 16:08:23 +0100

    chore(TMP-1657): lint

--- packages/ts-components/src/utils/getDeckApiUrl.ts
+++ packages/ts-components/src/utils/getDeckApiUrl.ts
@@ -1,13 +1,14 @@
 export const getDeckApiUrl = (): string => {
-    if (typeof window !== 'undefined') {
-        const environmentName = window.__TIMES_CONFIG__ && window.__TIMES_CONFIG__.environmentName;
+  if (typeof window !== 'undefined') {
+    const environmentName =
+      window.__TIMES_CONFIG__ && window.__TIMES_CONFIG__.environmentName;
 
-        if (environmentName === 'prod') {
-            return 'https://editorial-tm.newsapis.co.uk/prod/deck-component-data-api';
-        } else {
-            return 'https://editorial-tm.staging.newsapis.co.uk/staging/deck-component-data-api';
-        }
+    if (environmentName === 'prod') {
+      return 'https://editorial-tm.newsapis.co.uk/prod/deck-component-data-api';
     } else {
-        return 'https://editorial-tm.staging.newsapis.co.uk/staging/deck-component-data-api';
+      return 'https://editorial-tm.staging.newsapis.co.uk/staging/deck-component-data-api';
     }
+  } else {
+    return 'https://editorial-tm.staging.newsapis.co.uk/staging/deck-component-data-api';
+  }
 };
