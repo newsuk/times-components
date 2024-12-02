2a4434a82ab4146fbeb47e6a1f45bc75a4e47e08
ha2605 <Harsh.Agarwal@news.co.uk>
2024-12-02 16:05:54 +0530

    added secrets and utilised orbs to fetch secrets

---
+++ terraform-secret-rotator/variables.tf
@@ -0,0 +1,18 @@
+# variable "service_catalogue_id" {
+#   type        = string
+#   description = "service catalogue id for times-components"
+#   default     = "31"
+# }
+
+variable "repository" {
+  type        = string
+  description = "repository name for times-components"
+  default     = "times-components"
+}
+
+variable "service_owner" {
+  type        = string
+  description = "service owner name for times-components"
+  default     = "Times Media"
+}
+
