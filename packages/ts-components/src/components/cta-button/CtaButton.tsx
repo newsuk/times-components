f3f72da06b6705967df10b25f4e44fb1a4ef2224
ivazlopasa2 <iva.zlopasa@news.co.uk>
2024-12-09 15:44:56 +0100

    fix button component

--- packages/ts-components/src/components/cta-button/CtaButton.tsx
+++ packages/ts-components/src/components/cta-button/CtaButton.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
-import { BackgroundContainer, ctaButtonStyles, TimesEmbed } from './styles';
-import Button from '@times-components/button';
+import React from 'react';
+import { Link } from './styles';
 
 interface AttributesProps {
   url: string;
@@ -16,52 +14,15 @@ interface RootProps {
 }
 
 export const CtaButton: React.FC<RootProps> = props => {
-  const [id, setId] = useState<string | null>(null);
-  const [src, setSrc] = useState<string | null>(null);
-  const [ratio, setRatio] = useState<string | null>(null);
-  const [stateLoaded, setStateLoaded] = useState<boolean>(false);
-
-  useEffect(
-    () => {
-      const decodedSrc = decodeURIComponent(props.src);
-      if (decodedSrc) {
-        setId(`iframe-${Math.floor(Math.random() * 99999)}`);
-        setSrc(decodedSrc);
-        setRatio(props.ratio || '');
-        setStateLoaded(true);
-      }
-    },
-    [props.src, props.ratio]
-  );
-
-  useEffect(
-    () => {
-      if (stateLoaded && src) {
-        const iframeEl = document.getElementById('iframeEl');
-        if (iframeEl) {
-          iframeEl.classList.add('deactivate-ratio');
-        }
-      }
-    },
-    [stateLoaded, src]
-  );
-
   const attributes = props.attributes;
 
-  return (
-    <BackgroundContainer ratio={ratio || ''} id="iframeEl">
-      <div>
-        {attributes && (
-          <Button
-            title={attributes.text}
-            style={ctaButtonStyles}
-            onPress={() => {
-              window.open(attributes.url, attributes.target || '_blank');
-            }}
-          />
-        )}
-        {id && <TimesEmbed id={id} />}
-      </div>
-    </BackgroundContainer>
-  );
+  return attributes ? (
+    <Link
+      href={attributes.url}
+      target={attributes.target || '_blank'}
+      rel="noopener noreferrer"
+    >
+      {attributes.text}
+    </Link>
+  ) : null;
 };
--- packages/ts-components/src/components/cta-button/styles.ts
+++ packages/ts-components/src/components/cta-button/styles.ts
@@ -1,62 +1,17 @@
 import styled from 'styled-components';
 
-interface BackgroundContainerProps {
-  ratio?: 'r21-9' | 'r16-9' | 'r4-3' | 'r1-1' | 'r2-3' | 'r1-2' | string;
-}
-
-export const BackgroundContainer = styled.div<BackgroundContainerProps>`
-  position: relative;
-  overflow: hidden;
-  overflow-y: auto;
-
-  color: blue;
-
-  -webkit-overflow-scrolling: touch;
-
-  padding-bottom: ${props =>
-    props.ratio === 'r21-9'
-      ? '42.85714%'
-      : props.ratio === 'r16-9'
-        ? '56.25%'
-        : props.ratio === 'r4-3'
-          ? '75%'
-          : props.ratio === 'r1-1'
-            ? '100%'
-            : props.ratio === 'r2-3'
-              ? '150%'
-              : props.ratio === 'r1-2'
-                ? '200%'
-                : '75'};
-  position: relative;
-
-  iframe {
-    position: absolute;
-    height: 100%;
-  }
-
-  &.deactivate-ratio {
-    padding: 0;
-    overflow: hidden !important;
-
-    iframe {
-      position: relative;
-    }
-  }
+export const Link = styled.a`
+  width: fit-content;
+  height: 48px;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  border: none;
+  border-radius: 0;
+  padding: 12px 16px;
+  background-color: #005c8a;
+  color: #ffffff;
+  display: flex;
+  text-decoration: none;
+  align-items: center;
 `;
-
-export const TimesEmbed = styled.div`
-  border: 0px;
-  width: 100%;
-  height: auto;
-`;
-
-export const ctaButtonStyles = {
-  width: 'fit-content',
-  height: '48px',
-  'font-family': 'Roboto, sans-serif',
-  'font-size': '16px',
-  'font-weight': '500',
-  border: 'none',
-  'border-radius': 0,
-  'padding-inline': '16px'
-};
