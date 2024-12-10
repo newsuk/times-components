21a8198368638071986235e7a481848498392119
ivazlopasa2 <iva.zlopasa@news.co.uk>
2024-12-10 15:12:02 +0100

    fix cta button test

--- packages/ts-components/src/components/cta-button/__tests__/CtaButton.test.tsx
+++ packages/ts-components/src/components/cta-button/__tests__/CtaButton.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { CtaButton } from '../CtaButton';
 
 describe('CtaButton Component', () => {
