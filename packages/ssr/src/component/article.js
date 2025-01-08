32538e59ee119868ea82b3dbef1f2dd22f374473
Luka Kerečeni <luka.kereceni@q.agency>
2025-01-08 22:12:12 +0100

    Fix lint issue

--- packages/ssr/src/component/article.js
+++ packages/ssr/src/component/article.js
@@ -60,7 +60,7 @@ module.exports = (client, analyticsStream, data, helmetContext) => {
             : providerData.article;
           const articleTemplate = article ? article.template : null;
 
-          article.ssrAttr = 'new-ssr-attribute';
+          article.ssrAttr = "new-ssr-attribute";
 
           // eslint-disable-next-line no-console
           console.log("Provider data: ", article);
