78d162a3e7405d2826e1d9042fa0907db0f1f17a
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2025-01-08 14:40:53 +0000

    add space

--- packages/article-summary/fixtures/article-multi.js
+++ packages/article-summary/fixtures/article-multi.js
@@ -4,8 +4,10 @@ import { ArticleSummaryHeadline, ArticleSummaryContent } from "..";
 const defaultByline = "Camilla Long, Environment Editor";
 const defaultHeadline =
   "Top medal for forces dog who took a bite out of the Taliban";
+
 const defaultParagraph1 =
   "The special forces dog fought on under fire, even after shrapnel from Taliban grenades tore into his belly and legs, blew out a front tooth and damaged his right ear.";
+  
 const defaultParagraph2 =
   "Mali sniffed out explosives and insurgents during a gunfight that lasted seven and a half hours to help a team of Special Boat Service (SBS) operators hunt down and kill more than a dozen";
 
