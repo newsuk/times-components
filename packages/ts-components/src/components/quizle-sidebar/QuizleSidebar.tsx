98c37cd85d37627f41a78609656adca24a0abc5d
suneethanagella <105289286+suneethanagella@users.noreply.github.com>
2025-01-09 15:21:31 +0530

    Quizle sidebar component added

---
+++ packages/ts-components/src/components/quizle-sidebar/QuizleSidebar.tsx
@@ -0,0 +1,116 @@
+import React, { FC, useEffect, useState } from 'react';
+import {
+  QuestionIcon,
+  Container,
+  Titletag,
+  Divider,
+  Link,
+  Title,
+  TitleIconContainer,
+  Bottom
+} from './styles';
+import { useTrackingContext } from '../../helpers/tracking/TrackingContextProvider';
+import { handleClick } from './tracking-helpers';
+
+export interface QuizleSideBarProps {
+  sectionTitle: string;
+  pageLink: string;
+}
+
+export const QuizleSidebar: FC<QuizleSideBarProps> = ({
+  sectionTitle,
+  pageLink
+}) => {
+  const { fireAnalyticsEvent } = useTrackingContext();
+  const [currentQuestion, setCurrentQuestion] = useState<string | null>(null);
+
+  const backupQuestion = [
+    {
+      id: 0,
+      publishDate: "2024-10-29T00:00:00.000Z",
+      question: "Which type of dog gets its name from the Welsh words for 'dwarf' and 'dog'?",
+      solution: "Corgi",
+    },
+  ];
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch(
+        'https://tnl-render.tools.news/production/content/puzzles/quizle/questions.json'
+      );
+      if (!response.ok) throw new Error('Failed to fetch');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+      return backupQuestion; // Return backup questions on error
+    }
+  };
+
+  const findTodaysQuestion = (questions: typeof backupQuestion) => {
+    const today = new Date().toDateString();
+    const todaysQuestion = questions.find(
+      (q) => new Date(q.publishDate).toDateString() === today
+    );
+    return (
+      todaysQuestion?.question ||
+      backupQuestion[0]?.question || 'No questions available.'
+    );
+  };
+
+  useEffect(() => {
+    const loadQuestion = async () => {
+      const questions = await fetchQuestions();
+      const question = findTodaysQuestion(questions);
+      setCurrentQuestion(question);
+    };
+
+    loadQuestion();
+  }, []);
+
+  return (
+    <Container>
+      <>
+        <TitleIconContainer>
+          <div>
+            <Titletag>BETA</Titletag>
+            <Link
+              href={pageLink}
+              onClick={() =>
+                handleClick(
+                  fireAnalyticsEvent,
+                  'quizle sidebar: header selected'
+                )
+              }
+              className="trigger"
+            >
+              <Title>{sectionTitle}</Title>
+            </Link>
+          </div>
+          <QuestionIcon />
+        </TitleIconContainer>
+      </>
+      <Divider />
+      <Bottom>
+        <p>
+          <span className="q-icon">Q: </span>
+          {currentQuestion}
+        </p>
+        <Link
+              href={pageLink}
+              onClick={() =>
+                handleClick(
+                  fireAnalyticsEvent,
+                  'quizle sidebar: link selected'
+                )
+              }
+              className="quizle-link"
+            >
+          Test your knowledge
+          <span className="q-arrow"></span>
+        </Link>
+      </Bottom>
+      <Divider />
+    </Container>
+  );
+};
---
+++ packages/ts-components/src/components/quizle-sidebar/styles.ts
@@ -0,0 +1,137 @@
+import styled from 'styled-components';
+import QuizleIcon from './assets/QuizleIcon';
+
+export const QuestionIcon = styled(QuizleIcon)`
+  width: 37.652px;
+  height: 40px;
+`;
+
+export const Container = styled.div`
+  padding-top: 12px;
+  border-style: solid none none none;
+  border-width: 3px;
+  border-color: #c05729;
+`;
+
+export const Description = styled.p`
+  display: block;
+  font-family: 'Roboto';
+  font-weight: 400;
+  line-height: 1.5;
+  font-size: 1.4rem;
+  letter-spacing: 0em;
+  margin: 7px 0 4px;
+`;
+
+export const Divider = styled.hr`
+  margin: 12px 0;
+  border-style: dashed none none none;
+  border-width: 1px;
+  border-color: #01000d;
+  &:first-of-type {
+    margin-top: 12px;
+  }
+`;
+
+export const Link = styled.a`
+  display: block;
+  text-decoration: none;
+
+  &:hover {
+    button {
+      background-color: #e4e4e4;
+    }
+    h3 {
+      color: #00527a;
+    }
+  }
+  h3 {
+    color: #c05729;
+  }
+`;
+
+export const TitleIconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h3`
+  font-family: 'Times Modern';
+  font-weight: 800;
+  line-height: 1.125;
+  font-size: 1.8rem;
+  letter-spacing: 0em;
+  margin: 2px 0 0;
+`;
+
+export const Titletag = styled.h3`
+  font-family: 'Roboto';
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 18px;
+  text-align: left;
+  margin-block: 0;
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 8px 0 0;
+  min-height: 170px;
+  justify-content: space-between;
+
+  & .q-icon {
+    position: absolute;
+    color: #C74600;
+    font-family: TimesModern-Regular, TimesModern-Regular-fallback, serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%;
+    left: -28px;
+    top: -7px;
+  }
+
+  & .q-arrow {
+    width: 16px;
+    height: 16px;
+    background-image: url('data:image/svg+xml; charset=utf-8, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M6.67999 4L5.73999 4.94L8.79332 8L5.73999 11.06L6.67999 12L10.68 8L6.67999 4Z" fill="%23C74600"/></svg>');
+    background-size: contain;
+    background-repeat: no-repeat;
+  	display: inline-block;
+  	background-position: -3px 4px;
+    margin-left: 4px;
+}
+
+  & p {
+    position: relative;
+    color: #333;
+    font-family: Roboto-Regular, Roboto-Regular-fallback, sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+    margin: 0 0 20px;
+    text-indent: 28px;
+}
+
+  & a {
+    position: relative;
+    width: 100%;
+    color: #C74600;
+    font-family: Roboto-Regular, Roboto-Regular-fallback, sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 112.5%;
+    text-decoration: none;
+    & :hover{
+      color:#00527A;
+
+      & .q-arrow {
+        background-image: url('data:image/svg+xml; charset=utf-8, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M6.67999 4L5.73999 4.94L8.79332 8L5.73999 11.06L6.67999 12L10.68 8L6.67999 4Z" fill="%2300527A"/></svg>')
+    }
+  }
+`;
