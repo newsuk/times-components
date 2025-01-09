8c6889e20874dc6127852a004bffe1420897c16d
Luka Kerečeni <luka.kereceni@q.agency>
2025-01-09 12:42:24 +0100

    Added paywall-ssr

--- packages/ssr/src/lib/add-attribute.js
+++ packages/ssr/src/lib/add-attribute.js
@@ -1,4 +1,23 @@
 module.exports.addAttribute = article => ({
   ...article,
+  content: [
+    ...article.content,
+    {
+      name: "paywall-ssr",
+      children: [
+        {
+          name: "text",
+          children: [],
+          attributes: {
+            value: "lalalala"
+          }
+        }
+      ],
+      attributes: {
+        href:
+          "https://www.awin1.com/cread.php?awinmid=12430&awinaffid=240415&clickref=times-austria-travel-guide-040722&ued=https%3A%2F%2Fwww.tiqets.com%2Fen%2Fbelvedere-palace-tickets-l145892%2F"
+      }
+    }
+  ],
   ssrAttr: "new-ssr-attr"
 });
