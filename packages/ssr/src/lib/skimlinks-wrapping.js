b23ab35cc87212c16eb98ddad2edf0305c29f52e
Luka Kerečeni <luka.kereceni@q.agency>
2025-01-20 21:15:49 +0100

    Debug fetchSkimlinksDomains

--- packages/ssr/src/lib/skimlinks-wrapping.js
+++ packages/ssr/src/lib/skimlinks-wrapping.js
@@ -1,25 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 const { flowRight } = require("lodash");
-const { publisherId } = require("../constants/affiliate-links-validation");
+/* const { publisherId } = require("../constants/affiliate-links-validation"); */
 
-const getSecondLevelDomain = str => str.replace(/^(https?:\/\/)?www\./, "");
+/* const getSecondLevelDomain = str => str.replace(/^(https?:\/\/)?www\./, "");
 
 const trim = str => str.trim();
 
 const removeTrailingSlash = str => str.replace(/\/$/, "");
 
-const stringifyArg = str => (typeof str === "string" ? str : "");
+const stringifyArg = str => (typeof str === "string" ? str : ""); */
 
 /**
  * Removes http scheme, `www.`, and trailing slash from the string
  * @param str
  * @returns Domain name without www
  */
-const sanitizeDomainName = flowRight(
+/* const sanitizeDomainName = flowRight(
   removeTrailingSlash,
   getSecondLevelDomain,
   trim,
   stringifyArg
-);
+); */
 
 const fetchSkimlinksDomains = async () => {
   const response = await fetch(
@@ -40,17 +41,17 @@ const fetchSkimlinksDomains = async () => {
  * @param domains List of domains
  * @returns Array of just retailer domains
  */
-const filterDomains = domains =>
+/* const filterDomains = domains =>
   Array.isArray(domains)
     ? domains.map(retailer => retailer.domain).map(sanitizeDomainName)
-    : [];
+    : []; */
 
-const createDomainRegex = domains => {
+/* const createDomainRegex = domains => {
   const domainRegexString = domains.join("|");
   const skimlinksRegex = new RegExp(domainRegexString);
 
   return skimlinksRegex;
-};
+}; */
 
 /**
  *
@@ -58,22 +59,25 @@ const createDomainRegex = domains => {
  * @param contentPageUrl The referring page
  * @returns Skimlinks url
  */
-const constructSkimlinksUrl = (merchantUrl, contentPageUrl) => {
+/* const constructSkimlinksUrl = (merchantUrl, contentPageUrl) => {
   const skimlinksWrapper = `https://go.skimresources.com/?id=${publisherId}&url=${encodeURIComponent(
     merchantUrl
   )}&sref=${encodeURIComponent(contentPageUrl)}`;
 
   return skimlinksWrapper;
-};
+}; */
 
-const wrapSkimlinks = async (url, contentPageUrl) => {
+const wrapSkimlinks = async url => {
   const data = await fetchSkimlinksDomains();
-  const filteredDomains = filterDomains(data);
+
+  // eslint-disable-next-line no-console
+  console.log("data: ", data);
+  /* const filteredDomains = filterDomains(data);
   const skimlinksRegex = createDomainRegex(filteredDomains);
 
   if (skimlinksRegex.test(url)) {
     return constructSkimlinksUrl(url, contentPageUrl);
-  }
+  } */
 
   return url;
 };
