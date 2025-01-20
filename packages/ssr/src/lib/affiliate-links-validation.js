8a04de32eee3778a7085269df86ce1617a9763fb
Luka Kerečeni <luka.kereceni@q.agency>
2025-01-20 13:21:28 +0100

    Implement logic for Skimlinks wrapping

---
+++ packages/ssr/src/lib/affiliate-links-validation.js
@@ -0,0 +1,93 @@
+const {
+  theTimesSiteCode,
+  travelSiteCode,
+  trackonomicsRegex
+} = require("../constants/affiliate-links-validation");
+const { wrapSkimlinks } = require("./skimlinks-wrapping");
+
+const isAffiliateLink = url => trackonomicsRegex.test(url);
+
+const wrapAffiliateLink = (affiliateLink, contentPageUrl) => {
+  const wrapTrackonomics = trackonomicsUrl => {
+    if (!isAffiliateLink(trackonomicsUrl)) {
+      return trackonomicsUrl;
+    }
+
+    const isTravel =
+      contentPageUrl.includes("https://www.thetimes.com/travel") ||
+      contentPageUrl.includes("https://www.thetimes.co.uk/travel");
+
+    const siteCode = isTravel ? travelSiteCode : theTimesSiteCode;
+    const affiliateWrapper = `https://clicks.trx-hub.com/xid/${siteCode}?q=${encodeURIComponent(
+      trackonomicsUrl
+    )}&p=${encodeURIComponent(contentPageUrl)}`;
+
+    return affiliateWrapper;
+  };
+
+  const skimlinksUrl = wrapSkimlinks(affiliateLink, contentPageUrl);
+  return wrapTrackonomics(skimlinksUrl);
+};
+
+module.exports.affiliateLinksValidation = (children, articleDataFromRender) => {
+  const clonedChildren = [...children];
+  const { canonicalUrl, hostName } = articleDataFromRender;
+  const contentPageUrl = `${hostName}${canonicalUrl}`;
+
+  const checkAndSetLinkTarget = elements =>
+    elements.map(el => {
+      let newElement = { ...el };
+
+      // Check if element is a link or an interactive element with a URL.
+      if (
+        newElement.name === "link" ||
+        (newElement.name === "interactive" &&
+          newElement.attributes &&
+          newElement.attributes.element &&
+          newElement.attributes.element.value === "times-travel-cta")
+      ) {
+        const { attributes } = newElement;
+        const { element } = attributes || {};
+        const { attributes: elementAttributes } = element || {};
+        const href =
+          newElement.name === "interactive"
+            ? elementAttributes.url || ""
+            : attributes.href || "";
+
+        // If the link is external, set target to _blank. Wrap affiliate link if necessary.
+        if (
+          href &&
+          !href.startsWith("https://www.thetimes.co.uk") &&
+          !href.startsWith("https://www.thetimes.com")
+        ) {
+          if (newElement.name === "interactive") {
+            newElement = {
+              ...newElement,
+              attributes: {
+                ...attributes,
+                target: "_blank",
+                url: wrapAffiliateLink(href, contentPageUrl)
+              }
+            };
+          } else {
+            newElement = {
+              ...newElement,
+              attributes: {
+                ...attributes,
+                target: "_blank",
+                href: wrapAffiliateLink(href, contentPageUrl)
+              }
+            };
+          }
+        }
+      }
+
+      if (newElement.children && newElement.children.length) {
+        newElement.children = checkAndSetLinkTarget(newElement.children);
+      }
+
+      return newElement;
+    });
+
+  return checkAndSetLinkTarget(clonedChildren);
+};
