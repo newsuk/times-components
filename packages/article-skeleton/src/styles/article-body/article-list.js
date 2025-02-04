ff983293c8da0b90ff0dc54a5e2e8c4a3e83cfc0
Milan Haria <milan.haria@news.co.uk>
2025-02-04 16:05:38 +0000

    remove spaces in file

--- packages/article-skeleton/src/styles/article-body/article-list.js
+++ packages/article-skeleton/src/styles/article-body/article-list.js
@@ -7,12 +7,9 @@ import {
   spacing
 } from "@times-components/ts-styleguide";
 
-
 export const StyledUl = styled.ul`
-
   padding-left: ${spacing(5)};
   padding-right: ${spacing(2)};
-
   @media (min-width: ${breakpoints.medium}px) {
     font-size: ${fontSizes.body}px;
     line-height: 30px;
@@ -21,7 +18,6 @@ export const StyledUl = styled.ul`
     padding-right: ${spacing(2)};
     padding-left: ${spacing(3)};
   }
-
   @media (min-width: ${breakpoints.wide}px) {
     width: 56.2%;
   }
@@ -30,17 +26,14 @@ export const StyledUl = styled.ul`
 export const StyledLi = styled.li`
   margin-bottom: 16px;
   list-style: square;
-
   color: ${colours.functional.primary};
   font-family: ${fontsWithFallback.bodyRegular};
   line-height: 26px;
   font-size: ${fontSizes.bodyMobile}px;
-
   p {
     width: 100% !important;
     margin-block-start: 16px;
     margin-block-end: 0;
-
     &:empty {
       display: none;
     }
