c3e1cdacaaf28888f3aebc150095a6f7b48846e8
Luka Kerečeni <luka.kereceni@q.agency>
2025-02-06 15:40:43 +0100

    Remove setExternalLinkTargets & disable automatic disclaimer func

--- packages/article-skeleton/src/article-skeleton.js
+++ packages/article-skeleton/src/article-skeleton.js
@@ -48,9 +48,7 @@ import StickySaveAndShareBar from "./sticky-save-and-share-bar";
 import insertDropcapIntoAST from "./contentModifiers/dropcap-util";
 import insertNewsletterPuff from "./contentModifiers/newsletter-puff";
 import insertInlineAd from "./contentModifiers/inline-ad";
-import shouldIncludeDisclaimer from "./contentModifiers/should-include-disclaimer";
 import { getIsLiveOrBreakingFlag } from "./data-helper";
-import setExternalLinkTargets from "./contentModifiers/setExternalLinkTargets";
 
 export const reduceArticleContent = (content, reducers) =>
   content &&
@@ -179,11 +177,9 @@ const ArticleSkeleton = ({
 
   const articleContentReducers = [
     insertDropcapIntoAST(template, dropcapsDisabled),
-    shouldIncludeDisclaimer,
     insertNewsletterPuff(section, isPreview, expirableFlags),
     insertInlineAd(isPreview),
-    tagLastParagraph,
-    setExternalLinkTargets
+    tagLastParagraph
   ];
 
   const newContent = reduceArticleContent(content, articleContentReducers);
