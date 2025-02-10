9a2f82c1f279f114ebe940e85f272df3ae80935e
Milan Haria <milan.haria@news.co.uk>
2025-02-10 14:15:52 +0000

    fix: linting

--- packages/article-skeleton/src/contentModifiers/map-list-elements.js
+++ packages/article-skeleton/src/contentModifiers/map-list-elements.js
@@ -1,10 +1,12 @@
-
-export default (children) => {
+export default children => {
   const newContent = children.map(child => {
-    if(child.name === "paragraph") {
-      if(child.children.length === 1 && child.children[0].name === "unorderedList") {
+    if (child.name === "paragraph") {
+      if (
+        child.children.length === 1 &&
+        child.children[0].name === "unorderedList"
+      ) {
         return {
-          ...child.children[0],
+          ...child.children[0]
         };
       }
     }
