2e53c1d64dc5f47a5a3a45b2a720a1d142db9f3f
Leona Ljubičić <Leona.Ljubicic@news.co.uk>
2025-02-18 12:28:38 +0100

    feature(TMC-339): Added logic which reorders interactive and ad elements in article (#4067)
    * added logic which reorders interactive and ad elements in article
    
    * pulled latest changes and updated condition
    
    * disabled rule for ++

---
+++ packages/article-skeleton/src/contentModifiers/reorder-interactive-before-ad.js
@@ -0,0 +1,29 @@
+const reorderInteractiveBeforeAd = content => {
+  const paywallElement = content.find(item => item.name === "paywall");
+
+  if (!paywallElement || !Array.isArray(paywallElement.children)) {
+    return content;
+  }
+
+  const paywallChildren = paywallElement.children;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < paywallChildren.length - 2; i++) {
+    if (
+      paywallChildren[i].name === "paragraph" &&
+      (paywallChildren[i + 1].name.includes("inlineAd") ||
+        paywallChildren[i + 1].name === "ad") &&
+      paywallChildren[i + 2].name === "interactive" &&
+      paywallChildren[i + 2].attributes.element.value === "times-travel-cta"
+    ) {
+      [paywallChildren[i + 1], paywallChildren[i + 2]] = [
+        paywallChildren[i + 2],
+        paywallChildren[i + 1]
+      ];
+    }
+  }
+
+  return content;
+};
+
+export default reorderInteractiveBeforeAd;
