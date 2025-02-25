2a1c327b16018192cec45834285396a435b5965e
suneethanagella <105289286+suneethanagella@users.noreply.github.com>
2025-02-25 16:27:21 +0530

    edition query to puzzle query

--- packages/article-skeleton/src/article-sidebar.js
+++ packages/article-skeleton/src/article-sidebar.js
@@ -1,68 +1,27 @@
 import GET_PUZZLE_SECTIONS from "@times-components/provider-queries/src/polygon-data.js";
 
-const generatePuzzleUrl = slice => {
-  const base = "/puzzles/word-puzzles";
-
-  if (slice.shortIdentifier) {
-    return `${base}/${slice.slug}-${slice.shortIdentifier}`;
-  }
-  return base;
-};
+const baseURL = "/puzzles/word-puzzles";
 
 const fetchPolygonData = async () => {
   try {
     const response = await fetch("https://api.thetimes.co.uk/graphql", {
       method: "POST",
+      headers: { "x-use-standalone-puzzle-data": "true" },
       body: JSON.stringify({
-        query: GET_PUZZLE_SECTIONS
+        query: GET_PUZZLE_SECTIONS,
+        variables: { type: "polygon" }
       })
     });
 
-    if (!response.ok) {
+    if (!response.ok)
       throw new Error(`Network response was not ok: ${response.status}`);
-    }
 
     const data = await response.json();
-
-    const puzzleSectionsData =
-      data && data.data && data.data.editions && data.data.editions.list
-        ? data.data.editions.list
-            .map(edition => {
-              const puzzleSection = edition.sections.find(
-                sec => sec && sec.__typename === "PuzzleSection"
-              );
-              return puzzleSection
-                ? {
-                    ...puzzleSection,
-                    slices: puzzleSection.slices.map(slice => ({
-                      ...slice,
-                      section: {
-                        title: puzzleSection.title,
-                        id: puzzleSection.id
-                      }
-                    }))
-                  }
-                : null;
-            })
-            .filter(Boolean)
-        : [];
-
-    const filteredPuzzleSlices = puzzleSectionsData.flatMap(sec =>
-      sec.slices
-        .filter(
-          slice => slice.__typename === "Puzzle" && slice.type === "polygon"
-        )
-        .map(({ slug, shortIdentifier }) => ({
-          slug,
-          shortIdentifier
-        }))
-    );
-
-    const polygonData = filteredPuzzleSlices.map(puzzle =>
-      generatePuzzleUrl(puzzle)
-    );
-
-    return polygonData[0];
+    const puzzleUrls =
+      data?.data?.puzzles?.list?.map(
+        ({ slug, shortIdentifier }) => `${baseURL}/${slug}-${shortIdentifier}`
+      ) || [];
+    return puzzleUrls;
   } catch (error) {
     // Handle error fetching puzzle data
     return null;
