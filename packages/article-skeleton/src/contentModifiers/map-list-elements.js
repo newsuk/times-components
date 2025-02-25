21332b6503380779bdc404380f6872adf505a8d7
Milan Haria <milan.haria@news.co.uk>
2025-02-25 12:40:34 +0000

    fix(TMRP-193): indents on unordered lists

--- packages/article-skeleton/src/contentModifiers/map-list-elements.js
+++ packages/article-skeleton/src/contentModifiers/map-list-elements.js
@@ -1,6 +1,13 @@
-export default children => {
+// This function maps over the content to remove list elements from
+// being rendered inside paragraphs as this is invalid HTML.
+// It returns an array with the list elements not being a child of a paragraph.
+
+function mapListElements(children) {
   const newContent = children.map(child => {
-    if (child.name === "paragraph") {
+    if (child.name === "paragraph" || child.name === "paywall") {
+      if (child.name === "paywall") {
+        mapListElements(child.children);
+      }
       if (
         child.children.length === 1 &&
         child.children[0].name === "unorderedList"
@@ -13,4 +20,6 @@ export default children => {
     return child;
   });
   return newContent;
-};
+}
+
+export default mapListElements;
