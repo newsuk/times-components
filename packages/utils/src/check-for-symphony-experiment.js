5685f4d776d8d04024f00fe34767693724c758bd
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-02-26 16:34:40 +0000

    feat(TMG-3003): add blue dot

---
+++ packages/utils/src/check-for-symphony-experiment.js
@@ -0,0 +1,21 @@
+export const getCookieValue = cookieName => {
+  const allCookies = document.cookie;
+  const cookiesArray = allCookies.split(";");
+  const targetCookie = cookiesArray.find(cookie => cookie.includes(cookieName));
+  return targetCookie && targetCookie.split("=")[1];
+};
+
+export const getBase64CookieValue = cookieName => {
+  const cookieValue = getCookieValue(cookieName);
+  if (!cookieValue) return undefined;
+  try {
+    return JSON.parse(atob(cookieValue));
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const checkForSymphonyExperiment = () => {
+  const zephrDecisions = getBase64CookieValue("nuk_zephr_decisions");
+  return !!zephrDecisions?.["project-symphony"]?.includes("ARTICLE_ACCESS");
+};
