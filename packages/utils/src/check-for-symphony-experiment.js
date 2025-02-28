cec8bd540595d5eb1a274e631264a0007c2bf840
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-02-28 13:25:01 +0000

    feat(TMG-3003): remove optional chaining in check for symphony experiment

--- packages/utils/src/check-for-symphony-experiment.js
+++ packages/utils/src/check-for-symphony-experiment.js
@@ -2,7 +2,10 @@ import getBase64CookieValue from "./get-base64-cookie-value";
 
 const checkForSymphonyExperiment = () => {
   const zephrDecisions = getBase64CookieValue("nuk_zephr_decisions");
-  return !!zephrDecisions?.["project-symphony"]?.includes("ARTICLE_ACCESS");
+  if (!zephrDecisions || !zephrDecisions["project-symphony"]) {
+    return false;
+  }
+  return zephrDecisions["project-symphony"].includes("ARTICLE_ACCESS");
 };
 
 export default checkForSymphonyExperiment;
