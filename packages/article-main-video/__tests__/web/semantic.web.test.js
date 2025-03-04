6ec2779382ca09032abe1c584a0b00a34bae459e
times-tools <tools@news.co.uk>
2025-03-04 16:31:30 +0000

    feat(TMRS-684): linting

--- packages/article-main-video/__tests__/web/semantic.web.test.js
+++ packages/article-main-video/__tests__/web/semantic.web.test.js
@@ -13,7 +13,10 @@ import {
 } from "@times-components/jest-serializer";
 import { ContextProviderWithDefaults } from "@times-components/context";
 import { scales } from "@times-components/ts-styleguide";
-import { fixtures, MockedProvider } from "@times-components/provider-test-tools";
+import {
+  fixtures,
+  MockedProvider
+} from "@times-components/provider-test-tools";
 import Article from "../../src/article-main-video";
 import { adConfig } from "../ad-mock";
 
