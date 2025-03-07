4dea78b138e0d068887d540118ea0c858409cc9d
Callum <callumchristie@hotmail.com>
2025-03-07 08:04:01 +0000

    feat(TMG-3003): Change symphony decision property

--- packages/utils/src/check-for-symphony-experiment.js
+++ packages/utils/src/check-for-symphony-experiment.js
@@ -2,10 +2,12 @@ import getBase64CookieValue from "./get-base64-cookie-value";
 
 const checkForSymphonyExperiment = () => {
   const zephrDecisions = getBase64CookieValue("nuk_zephr_decisions");
-  if (!zephrDecisions || !zephrDecisions["project-symphony"]) {
+  if (!zephrDecisions || !zephrDecisions["project-symphony-reg-users"]) {
     return false;
   }
-  return zephrDecisions["project-symphony"].includes("ARTICLE_ACCESS");
+  return zephrDecisions["project-symphony-reg-users"].includes(
+    "ARTICLE_ACCESS"
+  );
 };
 
 export default checkForSymphonyExperiment;
