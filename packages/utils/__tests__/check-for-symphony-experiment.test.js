44530fea61d4fc9b0eb1cca3fce4d093d9f698c4
Callum <callumchristie@hotmail.com>
2025-03-07 08:04:01 +0000

    feat(TMG-3003): Change symphony decision property

--- packages/utils/__tests__/check-for-symphony-experiment.test.js
+++ packages/utils/__tests__/check-for-symphony-experiment.test.js
@@ -3,14 +3,16 @@ import checkForSymphonyExperiment from "../src/check-for-symphony-experiment";
 describe("checkForSymphonyExperiment", () => {
   it("returns true if the Symphony experiment is active", () => {
     const base64Value = btoa(
-      JSON.stringify({ "project-symphony": ["ARTICLE_ACCESS"] })
+      JSON.stringify({ "project-symphony-reg-users": ["ARTICLE_ACCESS"] })
     );
     document.cookie = `nuk_zephr_decisions=${base64Value}`;
     expect(checkForSymphonyExperiment()).toBe(true);
   });
 
   it("returns false if the Symphony experiment is not active", () => {
-    const base64Value = btoa(JSON.stringify({ "project-symphony": [] }));
+    const base64Value = btoa(
+      JSON.stringify({ "project-symphony-reg-users": [] })
+    );
     document.cookie = `nuk_zephr_decisions=${base64Value}`;
     expect(checkForSymphonyExperiment()).toBe(false);
   });
