348d3c3720846dc0dca862f8aa797ddcae0d88bf
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-03-10 09:15:30 +0000

    feat(TMG-3003): update Zephr slug

--- packages/utils/__tests__/check-for-symphony-experiment.test.js
+++ packages/utils/__tests__/check-for-symphony-experiment.test.js
@@ -3,16 +3,14 @@ import checkForSymphonyExperiment from "../src/check-for-symphony-experiment";
 describe("checkForSymphonyExperiment", () => {
   it("returns true if the Symphony experiment is active", () => {
     const base64Value = btoa(
-      JSON.stringify({ "project-symphony-reg-users": ["ARTICLE_ACCESS"] })
+      JSON.stringify({ "free-access-symphony": ["ARTICLE_ACCESS"] })
     );
     document.cookie = `nuk_zephr_decisions=${base64Value}`;
     expect(checkForSymphonyExperiment()).toBe(true);
   });
 
   it("returns false if the Symphony experiment is not active", () => {
-    const base64Value = btoa(
-      JSON.stringify({ "project-symphony-reg-users": [] })
-    );
+    const base64Value = btoa(JSON.stringify({ "free-access-symphony": [] }));
     document.cookie = `nuk_zephr_decisions=${base64Value}`;
     expect(checkForSymphonyExperiment()).toBe(false);
   });
