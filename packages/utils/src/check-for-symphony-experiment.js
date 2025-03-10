88755d4a60919016b408580837463e8a011a454f
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-03-10 09:15:30 +0000

    feat(TMG-3003): update Zephr slug

--- packages/utils/src/check-for-symphony-experiment.js
+++ packages/utils/src/check-for-symphony-experiment.js
@@ -2,12 +2,10 @@ import getBase64CookieValue from "./get-base64-cookie-value";
 
 const checkForSymphonyExperiment = () => {
   const zephrDecisions = getBase64CookieValue("nuk_zephr_decisions");
-  if (!zephrDecisions || !zephrDecisions["project-symphony-reg-users"]) {
+  if (!zephrDecisions || !zephrDecisions["free-access-symphony"]) {
     return false;
   }
-  return zephrDecisions["project-symphony-reg-users"].includes(
-    "ARTICLE_ACCESS"
-  );
+  return zephrDecisions["free-access-symphony"].includes("ARTICLE_ACCESS");
 };
 
 export default checkForSymphonyExperiment;
