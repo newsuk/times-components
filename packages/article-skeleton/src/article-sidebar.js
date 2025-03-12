4bb38a4d8e0623c1ce5cf2afb7c9331ba5384011
Suneetha Nagella <105289286+suneethanagella@users.noreply.github.com>
2025-03-12 15:38:37 +0530

    feat(TMRP-171): Change Articles Puzzle Sidebar query from an EditionQuery to a PuzzlesQuery (#4075)
    * edition query to puzzle query
    
    * fix
    
    * edition query to puzzle query
    
    * fix
    
    * uni tests
    
    * renamed file

--- packages/article-skeleton/src/article-sidebar.js
+++ packages/article-skeleton/src/article-sidebar.js
@@ -1,20 +1,15 @@
-import GET_PUZZLE_SECTIONS from "@times-components/provider-queries/src/polygon-data.js";
+import GET_PUZZLE_SECTIONS from "@times-components/provider-queries/src/sidebar-data.js";
 
-const generatePuzzleUrl = slice => {
-  const base = "/puzzles/word-puzzles";
-
-  if (slice.shortIdentifier) {
-    return `${base}/${slice.slug}-${slice.shortIdentifier}`;
-  }
-  return base;
-};
+const baseURL = "/puzzles/word-puzzles";
 
 const fetchPolygonData = async () => {
   try {
     const response = await fetch("https://api.thetimes.co.uk/graphql", {
       method: "POST",
+      headers: { "x-use-standalone-puzzle-data": "true" },
       body: JSON.stringify({
-        query: GET_PUZZLE_SECTIONS
+        query: GET_PUZZLE_SECTIONS,
+        variables: { type: "polygon" }
       })
     });
 
@@ -23,49 +18,18 @@ const fetchPolygonData = async () => {
     }
 
     const data = await response.json();
+    const puzzles =
+      data && data.data && data.data.puzzles && data.data.puzzles.list;
 
-    const puzzleSectionsData =
-      data && data.data && data.data.editions && data.data.editions.list
-        ? data.data.editions.list
-            .map(edition => {
-              const puzzleSection = edition.sections.find(
-                sec => sec && sec.__typename === "PuzzleSection"
-              );
-              return puzzleSection
-                ? {
-                    ...puzzleSection,
-                    slices: puzzleSection.slices.map(slice => ({
-                      ...slice,
-                      section: {
-                        title: puzzleSection.title,
-                        id: puzzleSection.id
-                      }
-                    }))
-                  }
-                : null;
-            })
-            .filter(Boolean)
-        : [];
-
-    const filteredPuzzleSlices = puzzleSectionsData.flatMap(sec =>
-      sec.slices
-        .filter(
-          slice => slice.__typename === "Puzzle" && slice.type === "polygon"
+    const puzzleUrl = puzzles
+      ? puzzles.map(
+          ({ slug, shortIdentifier }) => `${baseURL}/${slug}-${shortIdentifier}`
         )
-        .map(({ slug, shortIdentifier }) => ({
-          slug,
-          shortIdentifier
-        }))
-    );
-
-    const polygonData = filteredPuzzleSlices.map(puzzle =>
-      generatePuzzleUrl(puzzle)
-    );
-
-    return polygonData[0];
+      : [];
+    return puzzleUrl;
   } catch (error) {
-    // Handle error fetching puzzle data
-    return null;
+    console.error("Error fetching puzzle data:", error);
+    return [];
   }
 };
 
