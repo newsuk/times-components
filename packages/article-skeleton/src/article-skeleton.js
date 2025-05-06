fca5042bfd06fa593bc9b04d9f08e3fdb3e77a8e
Leona Ljubicic <Leona.Ljubicic@news.co.uk>
2025-05-06 12:51:54 +0200

    added util function

--- packages/article-skeleton/src/article-skeleton.js
+++ packages/article-skeleton/src/article-skeleton.js
@@ -5,6 +5,7 @@ import ArticleExtras from "@times-components/article-extras";
 import LazyLoad from "@times-components/lazy-load";
 import { StickyProvider } from "@times-components/sticky";
 import { withTrackScrollDepth } from "@times-components/tracking";
+import isExcludedPage from "@times-components/utils/src/excluded-paths";
 import {
   TrackingContextProvider,
   WelcomeBanner,
@@ -176,7 +177,7 @@ const ArticleSkeleton = ({
   const articleUrl =
     hostName && canonicalUrl ? `${hostName}${canonicalUrl}` : url;
 
-  const isObituariesArticle = articleUrl.includes("obituaries")
+    const isExcludedArticlePath = isExcludedPage(articleUrl);
 
   const categoryPath = url ? url.split("/").filter(Boolean)[0] || null : null;
   const quizCategories = ["culture", "life-style"];
@@ -359,7 +360,7 @@ const ArticleSkeleton = ({
             <Content content={rendererdContent} SaveAndShare={SaveAndShare} />
           ) : (
             <Fragment>
-              {!isObituariesArticle && <HeaderAdContainer key="headerAd">
+              {!isExcludedArticlePath && <HeaderAdContainer key="headerAd">
                 <AdContainer slotName="header" style={styles.adMarginStyle} />
               </HeaderAdContainer>}
               <MainContainer>
