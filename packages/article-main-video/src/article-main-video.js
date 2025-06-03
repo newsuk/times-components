a7bedd73f450a3240d9980168b268efe2b5044ce
douglasmik <48333391+douglasmik@users.noreply.github.com>
2025-06-03 13:25:01 +0100

    feat(TMRA-206): video article comments (#4131)

--- packages/article-main-video/src/article-main-video.js
+++ packages/article-main-video/src/article-main-video.js
@@ -183,7 +183,7 @@ class ArticlePage extends Component {
               </BreadcrumbContainer>
             )}
           <ArticleTopics topics={topics} />
-          {renderAd("inline-ad")}
+          {renderAd("videoAd1")}
         </ContentFooterContainer>
         <UserState state={UserState.showArticleExtras}>
           <CommentContainer>
@@ -196,9 +196,7 @@ class ArticlePage extends Component {
             />
           </CommentContainer>
         </UserState>
-        <ContentFooterContainer>
-          {renderAd("ad-article-inline-1")}
-        </ContentFooterContainer>
+        <ContentFooterContainer>{renderAd("videoAd2")}</ContentFooterContainer>
       </>
     );
   }
