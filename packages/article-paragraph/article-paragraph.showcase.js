8dc914633adbd5a01a2f0de3faf4530e0d9bf07f
times-tools <tools@news.co.uk>
2025-07-17 10:09:19 +0100

    feat(TMRA-652): resolve circular dependency

--- packages/article-paragraph/article-paragraph.showcase.js
+++ packages/article-paragraph/article-paragraph.showcase.js
@@ -3,7 +3,6 @@ import React, { Fragment } from "react";
 import coreRenderers from "@times-components/markup";
 import { renderTree } from "@times-components/markup-forest";
 import { colours, themeFactory } from "@times-components/ts-styleguide";
-import { ArticleLink } from "@times-components/article-skeleton";
 import paragraphData from "./fixtures/paragraph-showcase.json";
 import dropCapData from "./fixtures/drop-cap-showcase.json";
 import dropCapShortTextData from "./fixtures/drop-cap-short-text-showcase.json";
