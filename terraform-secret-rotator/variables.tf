8dd21b54771e81ca79fb39ced16cb63541b2fa5e
news-emil-davidkov <emil.davidkov@news.co.uk>
2025-08-05 13:09:57 +0300

    removed commented out var

--- terraform-secret-rotator/variables.tf
+++ terraform-secret-rotator/variables.tf
@@ -1,9 +1,3 @@
-# variable "service_catalogue_id" {
-#   type        = string
-#   description = "service catalogue id for times-components"
-#   default     = "31"
-# }
-
 variable "repository" {
   type        = string
   description = "repository name for times-components"
