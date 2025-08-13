13de525d33632aef8298b6299b477ac97e2dd6ea
Ibrahim kurhan <ibrahim.kurhan@news.co.uk>
2025-08-13 09:30:00 +0100

    feat(TMPR-527): add profile metadata to profile pages

--- packages/author-profile/src/head.js
+++ packages/author-profile/src/head.js
@@ -4,8 +4,9 @@ import { Helmet } from "react-helmet-async";
 import { renderTreeArrayAsText } from "@times-components/markup-forest";
 
 import { propTypes as authorProfileHeadPropTypes } from "./author-profile-head-prop-types";
+import DualMasthead from "../assets/dual-masthead.png";
 
-function Head({ metaDescription, description, name }) {
+function Head({ metaDescription, description, name, slug, articles }) {
   let content = `Get up to date information and read all the latest articles from ${name}.`;
 
   if (metaDescription) {
@@ -13,11 +14,42 @@ function Head({ metaDescription, description, name }) {
   } else if (description && description.length) {
     content = renderTreeArrayAsText(description).substring(0, 200);
   }
+  const hostName = "https://www.thetimes.com";
 
   return (
     <Helmet>
       <title>{name} | The Times &amp; The Sunday Times</title>
       <meta content={content} name="description" />
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "http://schema.org",
+          "@type": "CollectionPage",
+          publisher: {
+            "@type": "Organization",
+            name: "The Times",
+            logo: {
+              "@type": "ImageObject",
+              url: DualMasthead,
+              width: "600",
+              height: "315"
+            }
+          },
+
+          headline: name,
+          description: content,
+          url: `${hostName}/profile/${slug}`,
+          mainEntity: {
+            "@type": "ItemList",
+            itemListElement: articles?.map((article, index) => ({
+              "@type": "ListItem",
+              url: article?.url,
+              position: index + 1
+            }))
+          },
+          isAccessibleForFree: true,
+          articleSection: "author profile page"
+        })}
+      </script>
     </Helmet>
   );
 }
@@ -25,11 +57,19 @@ function Head({ metaDescription, description, name }) {
 Head.propTypes = {
   metaDescription: PropTypes.string,
   description: authorProfileHeadPropTypes.biography.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  slug: PropTypes.string,
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string
+    })
+  )
 };
 
 Head.defaultProps = {
-  metaDescription: null
+  metaDescription: null,
+  slug: "",
+  articles: []
 };
 
 export default Head;
